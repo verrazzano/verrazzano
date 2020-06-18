a5f2c4884b50719f3b514a7f50861dfd5e333496
Michael Gianatassio <michael.gianatassio@oracle.com>
2020-06-18 15:32:18 -0400

    Pick up new helm chart for updated admission-controller

--- install/3-install-verrazzano.sh
+++ install/3-install-verrazzano.sh
@@ -100,6 +100,7 @@ function install_verrazzano()
       --set clusterOperator.rancherUserName="${TOKEN_ARRAY[0]}" \
       --set clusterOperator.rancherPassword="${TOKEN_ARRAY[1]}" \
       --set clusterOperator.rancherHostname=${RANCHER_HOSTNAME} \
+      --set verrazzanoAdmissionController.imageVersion="v0.0.7" \
       --set verrazzanoAdmissionController.caBundle="$(kubectl -n ${VERRAZZANO_NS} get secret verrazzano-validation -o json | jq -r '.data."ca.crt"' | base64 --decode)"
 }
 
