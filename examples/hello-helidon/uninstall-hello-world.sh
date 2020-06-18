7a110126be1e5b8a7a66a1df1d26f1b26035174f
guzhang <guoyong.zhang@oracle.com>
2020-06-17 23:38:43 -0400

    add timeout to deleting vm and vb

--- examples/hello-helidon/uninstall-hello-world.sh
+++ examples/hello-helidon/uninstall-hello-world.sh
@@ -2,5 +2,5 @@
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd -P)
 
-kubectl delete -f ${SCRIPT_DIR}/hello-world-binding.yaml
-kubectl delete -f ${SCRIPT_DIR}/hello-world-model.yaml
+kubectl delete -f ${SCRIPT_DIR}/hello-world-binding.yaml --timeout 5m
+kubectl delete -f ${SCRIPT_DIR}/hello-world-model.yaml --timeout 2m
