86d32bd7fbc6aa28bd4f42dfdf807b0205f5d78f
Deva Sagar <deva.sagar@oracle.com>
2020-06-19 13:08:08 -0400

    Add Copyright Notices (#14)

--- install/2b-install-system-components-ocidns.sh
+++ install/2b-install-system-components-ocidns.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-
+#
+# Copyright (c) 2020, Oracle Corporation and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+#
 INGRESS_VERSION=1.27.0
 CERT_MANAGER_VERSION=0.13.1
 EXTERNAL_DNS_VERSION=2.20.0
--- install/common.sh
+++ install/common.sh
@@ -1,3 +1,7 @@
+#
+# Copyright (c) 2020, Oracle Corporation and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+#
 if [ -z "{SCRIPT_DIR}" ] ; then
     echo "SCRIPT_DIR not set"
     exit 1
