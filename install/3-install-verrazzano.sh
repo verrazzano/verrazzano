b1e9ad913b37189bcd3ba3c380f53a5764e9ed11
Robert Donat <robert.donat@oracle.com>
2020-06-22 21:06:36 -0400

    Update to helm chart version v0.0.24 (#21)
    * update keycloak image to built from source version
    
    * update keycloak image for Kind
    
    * Update to helm chart version v0.0.24

--- install/3-install-verrazzano.sh
+++ install/3-install-verrazzano.sh
@@ -18,7 +18,7 @@ function set_INGRESS_IP() {
 }
 
 VERRAZZANO_NS=verrazzano-system
-VERRAZZANO_VERSION=v0.0.23
+VERRAZZANO_VERSION=v0.0.24
 RancherAdminPassword=${RancherAdminPassword:=admin}
 set_INGRESS_IP
 
