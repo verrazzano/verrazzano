f045e498b518c3da291dfbbb13c7c6a9154c41ed
Mark Cram <mark.cram@oracle.com>
2020-07-01 13:18:39 +1000

    Check for LoadBalancer status address for xip use
    Signed-off-by: Mark Cram <mark.cram@oracle.com>

--- install/2a-install-system-components-magicdns.sh
+++ install/2a-install-system-components-magicdns.sh
@@ -21,6 +21,9 @@ function set_INGRESS_IP() {
     INGRESS_IP=$(kubectl get svc ingress-controller-nginx-ingress-controller -n ingress-nginx -o json | jq -r '.status.loadBalancer.ingress[0].ip')
   elif [ ${CLUSTER_TYPE} == "KIND" ]; then
     INGRESS_IP=$(kubectl get node ${KIND_CLUSTER_NAME}-control-plane -o json | jq -r '.status.addresses[] | select (.type == "InternalIP") | .address')
+  elif [ "${CLUSTER_TYPE}" == "OLCNE" ]; then
+    # Will not always be present, only return non-null when LoadBalancer providers are available.
+    INGRESS_IP=$(kubectl get svc ingress-controller-nginx-ingress-controller -n ingress-nginx -o json | jq -r '.status.loadBalancer.ingress[0].ip')
   fi
 }
 
