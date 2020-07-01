5c8e9b8e78c0631ff57521ae0581eb1db61bca40
Mark Cram <mark.cram@oracle.com>
2020-07-01 10:07:05 +1000

    Add a wrapper around install stage two to prevent changing install instructions
    Signed-off-by: Mark Cram <mark.cram@oracle.com>

---
+++ install/2-install-system-components.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2020, Oracle Corporation and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+#
+SCRIPT_DIR=$(cd $(dirname "$0"); pwd -P)
+
+set -e
+
+DNS_TYPE="xip.io"
+
+while getopts d: flag
+do
+    case "${flag}" in
+        d) DNS_TYPE=${OPTARG};;
+    esac
+done
+
+if [ ${DNS_TYPE} == "oci" ]; then
+  "$SCRIPT_DIR"/2b-install-system-components-ocidns.sh "$@"
+else
+  "$SCRIPT_DIR"/2a-install-system-components-magicdns.sh "$@"
+fi
