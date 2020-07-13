d454a4a15be6bd942e46ec7753502736772e2e36
Shih-Chang Chen[ <shih-chang.chen@oracle.com>
2020-07-13 13:14:54 -0400

    upper case RETRIES

--- install/1-install-istio.sh
+++ install/1-install-istio.sh
@@ -154,24 +154,24 @@ function verify_ocr_secret()
 {
     kubectl get secret ocr -n default || fail -e "ERROR: Secret named ocr is required to pull images from ${GLOBAL_HUB_REPO}.\nCreate the secret in the default namespace and then rerun this script.\ne.g. kubectl create secret docker-registry ocr --docker-username=<username> --docker-password=<password> --docker-server=container-registry.oracle.com"
     kubectl apply -f $CONFIG_DIR/ocrtest.yaml
-    ocr_verified=false
+    OCR_VERIFIED=false
     retries=0
     until [ "$retries" -ge 24 ]
     do
        OCRTEST=$(kubectl get pod -l job-name=ocrtest | grep ocrtest)       
        if [[ "$OCRTEST" == *"Running"* || "$OCRTEST" == *"Completed"* ]]; then
-           ocr_verified=true
+           OCR_VERIFIED=true
            break
        fi
        if [[ "$OCRTEST" == *"ImagePullBackOff"* || "$OCRTEST" == *"ErrImagePull"* ]]; then
-           ocr_verified=false
+           OCR_VERIFIED=false
            break
        fi
        retries=$(($retries+1))
        sleep 5
     done
     kubectl delete job ocrtest
-    if [ $ocr_verified = false ]; then
+    if [ $OCR_VERIFIED = false ]; then
         fail -e "ERROR: Secret named ocr has invalid username or password.\nDelete and recreate the secret in the default namespace and then rerun this script.\ne.g. kubectl create secret docker-registry ocr --docker-username=<username> --docker-password=<password> --docker-server=container-registry.oracle.com"
     fi
 }
