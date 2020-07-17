f8f92d66ba4d307b9ea85b4d84f7ef2b04c149f5
Tom Beerbower <tom.beerbower@oracle.com>
2020-07-17 12:19:23 -0400

    Curl error handling should be consistent

--- install/3-install-verrazzano.sh
+++ install/3-install-verrazzano.sh
@@ -45,9 +45,11 @@ function check_ingress_ports() {
 
       # Attempt to access the port on the $INGRESS_IP
       if [ $TARGETPORT == "https" ]; then
-        curl -k https://$INGRESS_IP:$PORT
+        ARGS=(-k https://$INGRESS_IP:$PORT)
+        call_curl 0 response http_code ARGS
       else
-        curl http://$INGRESS_IP:$PORT
+        ARGS=(http://$INGRESS_IP:$PORT)
+        call_curl 0 response http_code ARGS
       fi
 
       # Check the result of the curl call
