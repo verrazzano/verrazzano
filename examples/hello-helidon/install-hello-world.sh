e4c47c1c956396e77cf8bb3f001a0e3264c75203
Kevin Minder <kevin.minder@oracle.com>
2020-07-22 09:56:14 -0400

    VZ-824: multiple failures in helidon upgrade (pod timeout, delete binding)

--- examples/hello-helidon/install-hello-world.sh
+++ examples/hello-helidon/install-hello-world.sh
@@ -22,40 +22,36 @@ kubectl apply -f ${SCRIPT_DIR}/hello-world-binding.yaml
 echo "Wait for application namespace to be active."
 attempt=1
 while true; do
-    status=$(kubectl get ns -o=jsonpath='{.items[?(@.metadata.name=="greet")].status.phase}' || true)
-    if [ "${status}" == "Active" ]; then
-      echo "Application namespace found and active on attempt ${attempt}."
-      break
-    elif [ ${attempt} -gt 60 ]; then
-      echo "ERROR: Application namespace not found on final attempt ${attempt}. Exiting."
-      exit 1
-    else
-      if [ ${attempt} -eq 1 ]; then
-        echo "Application namespace not found on initial attempt, status \"${status}\". Retrying after delay."
-      fi
-      attempt=$(($attempt+1))
-      sleep .5
-    fi
+  status=$(kubectl get ns -o=jsonpath='{.items[?(@.metadata.name=="greet")].status.phase}' || true)
+  if [ "${status}" == "Active" ]; then
+    echo "Application namespace found and active on attempt ${attempt}."
+    break
+  elif [ ${attempt} -ge 60 ]; then
+    echo "ERROR: Application namespace not found on final attempt ${attempt}. Exiting."
+    exit 1
+  elif [ ${attempt} -eq 1 ]; then
+    echo "Application namespace not found on initial attempt, status \"${status}\". Retrying after delay."
+  fi
+  attempt=$(($attempt+1))
+  sleep .5
 done
 
-echo "Wait for application pods to be running."
+echo "Wait for application pods to be running."s
 attempt=1
 while true; do
   # Can't use kubectl wait with timeout as this fails immediately if there are no pods.
-  count=$( ( kubectl get pods -n greet -o=jsonpath='{.items[?(@.status.phase=="Running")].metadata.name}' || true ) | wc -w)
+  count=$( (kubectl get pods -n greet -o=jsonpath='{.items[?(@.status.phase=="Running")].metadata.name}' || true) | wc -w)
   if [ ${count} -ge 2 ]; then
     echo "Application pods found and running on attempt ${attempt}."
     break
-  elif [ ${attempt} -gt 60 ]; then
+  elif [ ${attempt} -ge 60 ]; then
     echo "ERROR: Application pods not found on final attempt ${attempt}. Exiting."
     exit 1
-  else
-    if [ ${attempt} -eq 1 ]; then
-      echo "Application pods not found on initial attempt, count ${count}. Retrying after delay."
-    fi
-    attempt=$((attempt+1))
-    sleep .5
+  elif [ ${attempt} -eq 1 ]; then
+    echo "Application pods not found on initial attempt, count ${count}. Retrying after delay."
   fi
+  attempt=$((attempt+1))
+  sleep .5
 done
 
 echo "Determine application endpoint."
