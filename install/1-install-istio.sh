40c0f4b43344b8e6f67e3dc834283da44ee05a57
Ravi Singhal <ravi.singhal.cse.21@gmail.com>
2020-07-29 00:16:11 +0530

    fix retry for ocr and print pod log (#148)
    * fix retry for ocr and print pod log
    
    * remove test exit

--- install/1-install-istio.sh
+++ install/1-install-istio.sh
@@ -172,7 +172,7 @@ function verify_ocr_secret()
     sed -e "s/OCR_TEST_JOB_NAME/${OCR_TEST_JOB_NAME}/" $CONFIG_DIR/ocrtest.yaml | kubectl apply -f -
     OCR_VERIFIED=false
     RETRIES=0
-    until [ "$RETRIES" -ge 60 ]
+    until [ "$RETRIES" -ge 30 ]
     do
        OCRTEST=$(kubectl get pod -l job-name=$OCR_TEST_JOB_NAME | grep ocrtest)
        if [[ "$OCRTEST" == *"Running"* || "$OCRTEST" == *"Completed"* ]]; then
@@ -184,15 +184,15 @@ function verify_ocr_secret()
        if [[ "$OCRTEST" == *"ImagePullBackOff"* || "$OCRTEST" == *"ErrImagePull"* ]]; then
            log "OCR Secret verification failed at attempt $RETRIES, job status is below"
            echo $OCRTEST
+           kubectl describe pod `echo $OCRTEST | awk '{ print $1 }'` | grep "Failed" | head -n 1
            OCR_VERIFIED=false
-           break
        fi
        RETRIES=$(($RETRIES+1))
-       sleep 5
+       sleep 1
     done
     kubectl delete job $OCR_TEST_JOB_NAME
     if [ "$OCR_VERIFIED" == false ]; then
-        fail "ERROR: Secret named ocr has invalid username or password.\nDelete and recreate the secret in the default namespace and then rerun this script.\ne.g. kubectl create secret docker-registry ocr --docker-username=<username> --docker-password=<password> --docker-server=container-registry.oracle.com"
+        fail "ERROR: OCR Secret verification failed. See log file for more details."
     fi
 }
 
