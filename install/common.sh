e69cf727e7da8b787c8cec6bab3d5c6b193c6e05
Mike Gilbode <mike.gilbode@oracle.com>
2020-08-03 09:53:50 -0400

    Pickup keycloak from OCR (#166)

--- install/common.sh
+++ install/common.sh
@@ -222,7 +222,7 @@ ISTIO_CORE_DNS_IMAGE=container-registry.oracle.com/olcne/coredns
 ISTIO_CORE_DNS_TAG=1.6.2
 ISTIO_VERSION=1.4.6
 
-KEYCLOAK_IMAGE=phx.ocir.io/stevengreenberginc/bfs/keycloak
+KEYCLOAK_IMAGE=container-registry.oracle.com/verrazzano/keycloak
 KEYCLOAK_IMAGE_TAG=10.0.1-30d98b0-5
 KEYCLOAK_CHART_VERSION=8.2.2
 
