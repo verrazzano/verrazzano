e33d570cf6d469e794f3ef538e38c18270de24b2
Davis Gomes <davis.gomes@oracle.com>
2020-08-12 14:40:19 -0700

    Corrected spelling and added cert manager deletion

--- uninstall/2a-uninstall-system-components-magicdns.sh
+++ uninstall/2a-uninstall-system-components-magicdns.sh
@@ -10,14 +10,15 @@ INSTALL_DIR=$SCRIPT_DIR/../install
 
 function delete_nginx() {
   # uninstall ingress-nginx
-  helm delete ingress_controller -n ingress-nginx
+  log "Uninstalling ingress-nginx"
+  helm delete ingress_controller -n ingress-nginx || 2>/dev/null
 
   # delete the nginx clusterrole and clusterrolebinding
-  if [ "$(kubectl get clusterole ingress-controller-nginx-ingress)" ] ; then
+  if [ "$(kubectl get clusterrole ingress-controller-nginx-ingress)" ] ; then
     kubectl delete clusterrole ingress-controller-nginx-ingress
   fi
 
-  if [ "$(kubectl get clusterolebinding ingress-controller-nginx-ingress)" ] ; then
+  if [ "$(kubectl get clusterrolebinding ingress-controller-nginx-ingress)" ] ; then
     kubectl delete clusterrolebinding ingress-controller-nginx-ingress
   fi
 
@@ -27,4 +28,17 @@ function delete_nginx() {
   fi
 }
 
+function delete_cert_manager() {
+    # uninstall cert manager deployment
+  log "Uninstalling cert-manager"
+  helm delete cert-manager -n cert-manager || 2>/dev/null
+
+  # delete the custom resource definition for cert manager
+  kubectl delete -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.13/deploy/manifests/00-crds.yaml
+
+  # delete namespace
+  kubectl delete namespace cert-manager
+}
+
 action "Deleting Nginx Components" delete_nginx
+action "Delete Cert Manager Components" delete_cert_manager
