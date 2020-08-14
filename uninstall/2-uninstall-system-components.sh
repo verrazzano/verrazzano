44ddfd12047b1b4424883e429f7c423482016c3e
Davis Gomes <davis.gomes@oracle.com>
2020-08-14 09:10:42 -0700

    Added rancher uninstall

--- uninstall/2-uninstall-system-components.sh
+++ uninstall/2-uninstall-system-components.sh
@@ -9,6 +9,7 @@ INSTALL_DIR=$SCRIPT_DIR/../install
 . $INSTALL_DIR/common.sh
 
 function delete_external_dns() {
+  log "Uninstalling external-dns"
   helm delete external-dns -n cert-manager || 2>/dev/null
 
   # delete clusterro;e and clusterrolebinding
@@ -24,7 +25,7 @@ function delete_external_dns() {
 function delete_nginx() {
   # uninstall ingress-nginx
   log "Uninstalling ingress-nginx"
-  helm delete ingress_controller -n ingress-nginx || 2>/dev/null
+  helm delete ingress-controller -n ingress-nginx || 2>/dev/null
 
   # delete the nginx clusterrole and clusterrolebinding
   if [ "$(kubectl get clusterrole ingress-controller-nginx-ingress)" ] ; then
@@ -60,6 +61,64 @@ function delete_cert_manager() {
   fi
 }
 
+function delete_rancher() {
+  log "Uninstalling rancher"
+  helm delete rancher -n cattle-system || 2>/dev/null
+
+  while [ "$(kubectl get crds --no-headers -o custom-columns=":metadata.name" | grep -E 'coreos.com|.cattle.io')" ]
+  do
+    # remove finalizers from crds
+    log "Deleting CRDs"
+    kubectl get crds --no-headers -o custom-columns=":metadata.name" \
+      | grep -E 'coreos.com|.cattle.io' \
+      | xargs kubectl patch crd -p '{"metadata":{"finalizers":null}}' --type=merge
+
+    # delete crds (include timeout for undiscovered finalizer problem)
+    kubectl get crds --no-headers -o custom-columns=":metadata.name" \
+      | grep -E 'coreos.com|.cattle.io' \
+      | xargs kubectl delete crd &
+    sleep 30
+    kill $!
+  done
+
+  # delete clusterrolebindings deployed by rancher
+  log "Deleting ClusterRoleBindings"
+  kubectl get clusterrolebinding --no-headers -o custom-columns=":metadata.name" \
+    | grep -E 'clusterrolebinding-|cattle-globalrolebinding-|globaladmin-user|grb-u|rancher' \
+    | xargs kubectl delete clusterrolebinding
+
+  # delete clusterroles
+  log "Deleting ClusterRoles"
+  kubectl get clusterrole --no-headers -o custom-columns=":metadata.name" \
+    | grep -E 'p-|project-|user-|cluster-owner|create-ns' \
+    | xargs kubectl delete clusterrole
+
+  # delete rolebinding
+  log "Deleting RoleBindings"
+  local default_names=("default" "kube-node-lease" "kube-public" "kube-system")
+  for namespace in "${default_names[@]}"
+  do
+    kubectl get rolebinding --no-headers -o custom-columns=":metadata.name" -n "${namespace}"\
+      | grep 'clusterrolebinding-' \
+      | xargs kubectl delete rolebinding -n "${namespace}"
+  done
+
+  # delete configmap in kube-system
+  if [ "$(kubectl get configmap cattle-controllers -n kube-system)" ] ; then
+    kubectl delete configmap cattle-controllers -n kube-system
+  fi
+
+  log "Deleting cattle namespaces"
+  # delete namespace finalizers
+  kubectl get namespaces --no-headers -o custom-columns=":metadata.name" \
+    | grep 'cattle' \
+    | xargs kubectl patch namespace -p '{"metadata":{"finalizers":null}}' --type=merge
+
+  # delete cattle namespaces
+  kubectl get namespaces --no-headers -o custom-columns=":metadata.name" | grep 'cattle' | xargs kubectl delete namespaces
+}
+
 action "Deleting External DNS Components" delete_external_dns
 action "Deleting Nginx Components" delete_nginx
 action "Delete Cert Manager Components" delete_cert_manager
+action "Delete Rancher Components" delete_rancher
