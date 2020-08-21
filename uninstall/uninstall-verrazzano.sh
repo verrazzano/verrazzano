140c2c9b1a85eb9374f5ff761b1a4f29ce48c1e1
Davis Gomes <davis.gomes@oracle.com>
2020-08-21 09:23:45 -0700

    Warn user about applications specifics and add force uninstall

--- uninstall/uninstall-verrazzano.sh
+++ uninstall/uninstall-verrazzano.sh
@@ -8,16 +8,56 @@ INSTALL_DIR=$SCRIPT_DIR/../install
 
 . $INSTALL_DIR/common.sh
 
-while true
+function usage() {
+  error
+    error "usage: $0 [-f] [-h]"
+    error " -f    Force the uninstall and suppress warnings"
+    error " -h    Help"
+    error
+    exit
+}
+
+FORCE=false
+while getopts fh flag
 do
-  echo -n "$(tput bold)All Verrazzano resources and applications will be permanently removed from the cluster. Do you wish to proceed? [y/n]:$(tput sgr0)" >&4
-  read -r -t 30 resp
-  case $resp in
-    [Yy]* ) break;;
-    [Nn]* ) exit;;
-    * ) status 'Please answer yes or no'
-  esac
+    case "${flag}" in
+        f) FORCE=true;;
+        h) usage;;
+        *) usage;;
+    esac
 done
+
+if [ "$FORCE" = false ] ; then
+  while true
+  do
+    echo -n "$(tput bold)All Verrazzano resources and applications will be permanently removed from the cluster. Do you wish to proceed? [y/n]:$(tput sgr0)" >&4
+    read -r -t 30 resp
+    case $resp in
+      [Yy]* ) break;;
+      [Nn]* ) exit;;
+      * ) status 'Please answer yes or no'
+    esac
+  done
+
+  if [ "$(kubectl get vb -A)" ] || [ "$(kubectl get vm -A)" ] ; then
+    echo "$(tput bold)The following applications will be deleted upon uninstall:$(tput sgr0)" >&4
+    echo "Verrazzano Models:" >&4
+    echo "$(kubectl get vm -A --no-headers -o custom-columns=":metadata.name")" >&4
+    echo "Verrazzano Bindings:" >&4
+    echo "$(kubectl get vb -A --no-headers -o custom-columns=":metadata.name")" >&4
+
+    while true
+    do
+      echo -n "$(tput bold)Are you sure you want to delete these applications? [y/n]:$(tput sgr0)" >&4
+      read -r -t 30 resp
+      case $resp in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) status 'Please answer yes or no'
+      esac
+    done
+  fi
+fi
 
 section "Uninstalling Verrazzano Applications"
 $SCRIPT_DIR/uninstall-steps/0-uninstall-applications.sh
