fd29703c4f9c57ac496238e2584eae39346a6fac
Davis Gomes <67442368+davis-gomes@users.noreply.github.com>
2020-09-10 10:24:50 -0700

    Clean up error and logging output (#252)
    * error output for uninstall
    
    * Update logging
    
    * Add missing error and log
    
    * Created err_exit function to exit upon errors with a message
    
    * MySQL spelling
    
    * Update method of error output
    
    * Updating collect to retrieve
    
    * Typo updates
    
    * backgrounded warning

--- uninstall/uninstall-steps/1-uninstall-istio.sh
+++ uninstall/uninstall-steps/1-uninstall-istio.sh
@@ -20,73 +20,75 @@ CONFIG_DIR=$INSTALL_DIR/config
 function uninstall_istio() {
   # delete webhook configurations
   log "Removing Istio Webhook Configurations"
-  kubectl delete MutatingWebhookConfiguration istio-sidecar-injector --ignore-not-found=true || return $?
-  kubectl delete ValidatingWebhookConfiguration istio-galley --ignore-not-found=true || return $?
+  kubectl delete MutatingWebhookConfiguration istio-sidecar-injector --ignore-not-found=true || err_return $? "Could not delete MutatingWebhookConfiguration from Istio" || return $?
+  kubectl delete ValidatingWebhookConfiguration istio-galley --ignore-not-found=true || err_return $? "Could not delete ValidatingWebhookConfiguration from Istio" || return $?
 
   # delete istio crds
   log "Deleting Istio Custom Resource Definitions"
   kubectl get crd --no-headers -o custom-columns=":metadata.name" \
     | awk '/istio.io/ {print $1}' \
     | xargsr kubectl delete crd \
-    || return $? # return on pipefail
+    || err_return $? "Could not delete CustomResourceDefinition from Istio" || return $? # return on pipefail
 
   # delete istio api services
   log "Deleting Istio API Services"
   kubectl get apiservice --no-headers -o custom-columns=":metadata.name" \
     | awk '/istio.io/ {print $1}' \
     | xargsr kubectl delete apiservice \
-    || return $? # return on pipefail
+    || err_return $? "Could not delete APIServices from Istio" || return $? # return on pipefail
 
   # delete istio cluster role bindings
   log "Deleting Istio Cluster Role Bindings"
   kubectl get clusterrolebinding --no-headers -o custom-columns=":metadata.name" \
     | awk '/istio-system|istio-multi/ {print $1}' \
     | xargsr kubectl delete clusterrolebinding \
-    || return $? # return on pipefail
+    || err_return $? "Could not delete ClusterRoleBindings from Istio" || return $? # return on pipefail
 
   # delete istio cluster roles
   log "Deleting Istio Cluster Roles"
   kubectl get clusterrole --no-headers -o custom-columns=":metadata.name" \
     | awk '/istio-system|istio-reader|istiocoredns/ {print $1}' \
     | xargsr kubectl delete clusterrole \
-    || return $? # return on pipefail
+    || err_return $? "Could not delete ClusterRoles from Istio" || return $? # return on pipefail
 }
 
 function delete_secrets() {
   # Delete istio.default in all namespaces
-  log "Collecting istio secrets for deletion"
-  kubectl delete secret istio.default --ignore-not-found=true || return $?
-  kubectl delete secret istio.default -n kube-public --ignore-not-found=true || return $?
-  kubectl delete secret istio.default -n kube-node-lease --ignore-not-found=true || return $?
+  log "Retrieving istio secrets for deletion"
+  kubectl delete secret istio.default --ignore-not-found=true || err_return $? "Could not delete secret from Istio in namespace default" || return $?
+  kubectl delete secret istio.default -n kube-public --ignore-not-found=true || err_return $? "Could not delete secret from Istio in namespace kube-public" || return $?
+  kubectl delete secret istio.default -n kube-node-lease --ignore-not-found=true || err_return $? "Could not delete secret from Istio in namespace kube-node-lease" || return $?
 
   # delete secrets left over in kube-system
   kubectl get secrets -n kube-system --no-headers -o custom-columns=":metadata.name,:metadata.annotations" \
   | awk '/istio./ {print $1}' \
   | xargsr kubectl delete secret -n kube-system \
-  || return $? # return on pipefail
+  || err_return $? "Could not delete secrets from Istio in namespace kube-system" || return $? # return on pipefail
 }
 
 function delete_istio_namepsace() {
+  log "Deleting istio-system finalizers"
   kubectl get namespaces --no-headers -o custom-columns=":metadata.name" \
     | awk '/istio-system/ {print $1}' \
     | xargsr kubectl patch namespace -p '{"metadata":{"finalizers":null}}' --type=merge  \
-    || return $? # return on pipefail
+    || err_return $? "Could not remove finalizers from namespace istio-system" || return $? # return on pipefail
 
   log "Deleting istio-system namespace"
-  kubectl delete namespace istio-system --ignore-not-found=true || return $?
+  kubectl delete namespace istio-system --ignore-not-found=true || err_return $? "Could not delete namespace istio-system" || return $?
 }
 
 function finalize() {
   # Removing possible reference to verrazzano in clusterroles and clusterrolebindings
+  log "Removing Verrazzano ClusterRoles and ClusterRoleBindings"
   kubectl get clusterrolebinding --no-headers -o custom-columns=":metadata.name" \
     | awk '/verrazzano/' \
     | xargsr kubectl delete clusterrolebinding \
-    || return $? # return on pipefail
+    || err_return $? "Could not delete ClusterRoleBindings" || return $? # return on pipefail
 
   kubectl get clusterrole --no-headers -o custom-columns=":metadata.name" \
     | awk '/verrazzano/' \
     | xargsr kubectl delete clusterrole \
-    || return $? # return on pipefail
+    || err_return $? "Could not delete ClusterRoles" || return $? # return on pipefail
 
   # Grab all leftover Helm repos and delete resources
   log "Deleting Helm repos"
--- uninstall/uninstall-utils.sh
+++ uninstall/uninstall-utils.sh
@@ -3,6 +3,10 @@
 # Copyright (c) 2020, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 #
+SCRIPT_DIR=$(cd $(dirname "$0"); pwd -P)
+INSTALL_DIR=$SCRIPT_DIR/../../install
+
+. $INSTALL_DIR/common.sh
 
 # wrapper for xargs
 # $@ commands - xargs flags and commands
@@ -20,4 +24,22 @@ function xargsr() {
   else
     xargs "$@"
   fi
+}
+
+# error return command
+# $1 exit_code - code given by the command that triggered this function
+# $2 message - error message given to the user when an error is reached
+# Usage:
+# err_return $? "message"
+function err_return() {
+  exit_code=$1
+  message=$2
+
+  if (($exit_code < 0 && $exit_code > 255)) ; then
+    error "the exit code given is not a valid integer"
+    exit 1
+  fi
+
+  error "$message"
+  return "$exit_code"
 }
