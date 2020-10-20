5a029484fa338223e285137a15183d701e34d517
jonathan.maron@oracle.com <jonathan.maron@oracle.com>
2020-10-20 10:15:31 -0400

    add hello world name change

--- util/create_image_list.sh
+++ util/create_image_list.sh
@@ -3,12 +3,17 @@
 # Copyright (c) 2020, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 #
+
 SCRIPT_DIR=$(cd $(dirname "$0"); pwd -P)
 
-source ../install/common.sh
+source ${SCRIPT_DIR}/../install/common.sh
 
 # get image list from cluster and persist to output file
-kubectl get pods --all-namespaces -o jsonpath="{..image}" |\tr -s '[[:space:]]' '\n' |\sort |\uniq -c | grep verrazzano | cut -c 6- | grep / | cut -d/ -f2- > verrazzano_img_list.txt
+kubectl get pods --all-namespaces -o jsonpath="{..image}" |\tr -s '[[:space:]]' '\n' |\sort |\uniq -c | grep verrazzano | cut -c 6- | grep / | cut -d/ -f2- > ${SCRIPT_DIR}/verrazzano_img_list.txt
 
 # add the acme solver (short lived container image)
-echo $CERT_MANAGER_SOLVER_IMAGE:$CERT_MANAGER_SOLVER_TAG | grep / | cut -d/ -f2- >> verrazzano_img_list.txt
+echo $CERT_MANAGER_SOLVER_IMAGE:$CERT_MANAGER_SOLVER_TAG | grep / | cut -d/ -f2- >> ${SCRIPT_DIR}/verrazzano_img_list.txt
+
+# handle helidon hello world image name mapping
+cd ${SCRIPT_DIR}
+sed -i 's/example-hello-world-helidon/example-helidon-greet-app-v1/' ${SCRIPT_DIR}/verrazzano_img_list.txt
