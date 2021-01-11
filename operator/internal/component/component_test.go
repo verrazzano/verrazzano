21663464eb02b0a81528439d57f44903edf5b6b3
Paul Mackin <paul.mackin@oracle.com>
2021-01-11 11:04:58 -0500

    fix unit test

--- operator/internal/component/component_test.go
+++ operator/internal/component/component_test.go
@@ -16,8 +16,8 @@ import (
 func TestGetComponents(t *testing.T) {
 	assert := assert.New(t)
 	comps := GetComponents()
-	assert.Len(comps, 2, "Wrong number of components")
+	assert.Len(comps, 3, "Wrong number of components")
 	assert.Equal(comps[0].Name(), "verrazzano")
-	assert.Equal(comps[2].Name(), "ingress-nginx")
-	assert.Equal(comps[1].Name(), "external-dns")
+	assert.Equal(comps[1].Name(), "ingress-nginx")
+	assert.Equal(comps[2].Name(), "external-dns")
 }
