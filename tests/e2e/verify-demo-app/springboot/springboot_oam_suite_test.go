e1e6b4a4d49aea41bca26bf592d9e86656391210
Padmanabha Bhat <padmanabha.bhat@oracle.com>
2021-01-29 11:29:48 +0530

    Initial commit

---
+++ tests/e2e/verify-demo-app/springboot/springboot_oam_suite_test.go
@@ -0,0 +1,20 @@
+// Copyright (c) 2020, 2021, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package oam
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/onsi/ginkgo"
+	"github.com/onsi/ginkgo/config"
+	"github.com/onsi/ginkgo/reporters"
+	"github.com/onsi/gomega"
+)
+
+func TestOAMInfra(t *testing.T) {
+	RegisterFailHandler(Fail)
+	junitReporter := reporters.NewJUnitReporter(fmt.Sprintf("verify-demo-app-springboot-oam-%d-test-result.xml", config.GinkgoConfig.ParallelNode))
+	RunSpecsWithDefaultAndCustomReporters(t, "Springboot OAM  Test Suite", []Reporter{junitReporter})
+}
