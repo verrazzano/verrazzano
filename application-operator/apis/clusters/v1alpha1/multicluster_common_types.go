d3a151b22e5749fd7171913537eb4db7136ec53f
Deva Sagar <deva.sagar@oracle.com>
2021-02-02 15:26:16 -0500

    lint fix

--- application-operator/apis/clusters/v1alpha1/multicluster_common_types.go
+++ application-operator/apis/clusters/v1alpha1/multicluster_common_types.go
@@ -1,5 +1,6 @@
 // Copyright (c) 2021 Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
 package v1alpha1
 
 import corev1 "k8s.io/api/core/v1"
