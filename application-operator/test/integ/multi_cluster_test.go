b21b59b4b7094b47b4f39758b16c4db7de81d14a
Deva Sagar <deva.sagar@oracle.com>
2021-02-03 13:33:29 -0500

    Add MultiClusterSecret CRD

--- application-operator/test/integ/multi_cluster_test.go
+++ application-operator/test/integ/multi_cluster_test.go
@@ -8,15 +8,26 @@ import (
 	"github.com/verrazzano/verrazzano/application-operator/test/integ/util"
 )
 
+var _ = BeforeSuite(func() {
+	_, stderr := util.Kubectl("create ns multiclustertest")
+	if stderr != "" {
+		Fail("could not create namespace multiclustertest")
+	}
+})
+
 var _ = Describe("Testing Multi-Cluster Namespace CRD", func() {
-	It("MultiClusterNamespace CRD can be applied", func() {
+	It("MultiCluster CRDs can be applied", func() {
 		_, stderr := util.Kubectl("apply -f ../../config/crd/bases/clusters.verrazzano.io_multiclusternamespaces.yaml")
+		Expect(stderr).To(Equal(""))
+		_, stderr = util.Kubectl("apply -f ../../config/crd/bases/clusters.verrazzano.io_multiclustersecret.yaml")
 		Expect(stderr).To(Equal(""))
 	})
 	It("MultiClusterNamespace can be created ", func() {
-		_, stderr := util.Kubectl("create ns multicluster")
-		Expect(stderr).To(Equal(""), "could not create multicluster namespace")
-		_, stderr = util.Kubectl("apply -f testdata/multicluster_namespace_sample.yaml")
+		_, stderr := util.Kubectl("apply -f testdata/multi-cluster/multicluster_namespace_sample.yaml")
+		Expect(stderr).To(Equal(""))
+	})
+	It("MultiClusterSecret can be created ", func() {
+		_, stderr := util.Kubectl("apply -f testdata/multi-cluster/multicluster_secret_sample.yaml")
 		Expect(stderr).To(Equal(""))
 	})
 })
