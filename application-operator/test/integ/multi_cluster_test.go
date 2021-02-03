759b1d30d61e255571381951925c5bf2e83b7062
Deva Sagar <deva.sagar@oracle.com>
2021-02-03 16:54:49 -0500

    minor fixes after review

--- application-operator/test/integ/multi_cluster_test.go
+++ application-operator/test/integ/multi_cluster_test.go
@@ -4,8 +4,8 @@ package integ
 
 import (
 	"fmt"
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
+	"github.com/onsi/ginkgo"
+	"github.com/onsi/gomega"
 	"github.com/verrazzano/verrazzano/application-operator/test/integ/util"
 )
 
@@ -21,21 +21,21 @@ var (
 	}
 )
 
-var _ = Describe("Testing Multi-Cluster Namespace CRD", func() {
-	It("MultiCluster CRDs can be applied", func() {
+var _ = ginkgo.Describe("Testing Multi-Cluster Namespace CRD", func() {
+	ginkgo.It("MultiCluster CRDs can be applied", func() {
 		for _, crd := range multiclusterCrds {
 			_, stderr := util.Kubectl(fmt.Sprintf("apply -f %v", crd))
-			Expect(stderr).To(Equal(""), fmt.Sprintf("Failed to apply CRD %v", crd))
+			gomega.Expect(stderr).To(gomega.Equal(""), fmt.Sprintf("Failed to apply CRD %v", crd))
 		}
 		_, stderr := util.Kubectl("create ns " + multiclusterTestNamespace)
-		Expect(stderr).To(Equal(""), fmt.Sprintf("failed to create namespace %v", multiclusterTestNamespace))
+		gomega.Expect(stderr).To(gomega.Equal(""), fmt.Sprintf("failed to create namespace %v", multiclusterTestNamespace))
 	})
-	It("MultiClusterNamespace can be created ", func() {
+	ginkgo.It("MultiClusterNamespace can be created ", func() {
 		_, stderr := util.Kubectl("apply -f testdata/multi-cluster/multicluster_namespace_sample.yaml")
-		Expect(stderr).To(Equal(""))
+		gomega.Expect(stderr).To(gomega.Equal(""))
 	})
-	It("MultiClusterSecret can be created ", func() {
+	ginkgo.It("MultiClusterSecret can be created ", func() {
 		_, stderr := util.Kubectl("apply -f testdata/multi-cluster/multicluster_secret_sample.yaml")
-		Expect(stderr).To(Equal(""))
+		gomega.Expect(stderr).To(gomega.Equal(""))
 	})
 })
