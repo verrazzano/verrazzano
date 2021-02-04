5fb45e0e57dd0809b9662129922cc0c23d85b5c1
Michael Gianatassio <michael.gianatassio@oracle.com>
2021-02-04 09:10:58 -0800

    Create a clients folder with clusters and verrazzano under it

--- platform-operator/clients/verrazzano/clientset/versioned/typed/verrazzano/v1alpha1/fake/fake_verrazzano_client.go
+++ platform-operator/clients/verrazzano/clientset/versioned/typed/verrazzano/v1alpha1/fake/fake_verrazzano_client.go
@@ -6,7 +6,7 @@
 package fake
 
 import (
-	v1alpha1 "github.com/verrazzano/verrazzano/platform-operator/client/clientset/versioned/typed/verrazzano/v1alpha1"
+	v1alpha1 "github.com/verrazzano/verrazzano/platform-operator/clients/verrazzano/clientset/versioned/typed/verrazzano/v1alpha1"
 	rest "k8s.io/client-go/rest"
 	testing "k8s.io/client-go/testing"
 )
--- platform-operator/clients/verrazzano/clientset/versioned/typed/verrazzano/v1alpha1/verrazzano.go
+++ platform-operator/clients/verrazzano/clientset/versioned/typed/verrazzano/v1alpha1/verrazzano.go
@@ -10,7 +10,7 @@ import (
 	"time"
 
 	v1alpha1 "github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1alpha1"
-	scheme "github.com/verrazzano/verrazzano/platform-operator/client/clientset/versioned/scheme"
+	scheme "github.com/verrazzano/verrazzano/platform-operator/clients/verrazzano/clientset/versioned/scheme"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	types "k8s.io/apimachinery/pkg/types"
 	watch "k8s.io/apimachinery/pkg/watch"
