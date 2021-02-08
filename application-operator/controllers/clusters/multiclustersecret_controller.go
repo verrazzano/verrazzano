41e9f9191362099c72d3a3104ab0f7980da76f86
Deva Sagar <deva.sagar@oracle.com>
2021-02-08 13:50:38 -0500

    Status update for MultiClusterSecret

--- application-operator/controllers/clusters/multiclustersecret_controller.go
+++ application-operator/controllers/clusters/multiclustersecret_controller.go
@@ -5,10 +5,12 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"time"
 
 	"github.com/go-logr/logr"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -31,21 +33,68 @@ type MultiClusterSecretReconciler struct {
 func (r *MultiClusterSecretReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	_ = context.Background()
 	logger := r.Log.WithValues("multiclustersecret", req.NamespacedName)
-
 	var mcSecret clustersv1alpha1.MultiClusterSecret
+	result := reconcile.Result{}
 	ctx := context.Background()
 	err := r.fetchMultiClusterSecret(ctx, req.NamespacedName, &mcSecret)
 	if err != nil {
 		logger.Info("Failed to fetch MultiClusterSecret", "err", err)
-		return reconcile.Result{}, client.IgnoreNotFound(err)
+		return result, client.IgnoreNotFound(err)
 	}
 
 	logger.Info("MultiClusterSecret create or update with underlying secret",
 		"secret", mcSecret.Spec.Template.Metadata.Name,
 		"placement", mcSecret.Spec.Placement.Clusters[0].Name)
-	r.createOrUpdateSecret(ctx, mcSecret)
+	opResult, err := r.createOrUpdateSecret(ctx, mcSecret, &result)
 
-	return ctrl.Result{}, nil
+	return r.updateStatus(ctx, &mcSecret, opResult, err)
+}
+
+func (r *MultiClusterSecretReconciler) updateStatus(ctx context.Context, mcSecret *clustersv1alpha1.MultiClusterSecret, opResult controllerutil.OperationResult, err error) (ctrl.Result, error) {
+	var condition clustersv1alpha1.Condition
+	var state clustersv1alpha1.StateType
+	if err != nil {
+		condition = clustersv1alpha1.Condition{Type: clustersv1alpha1.DeployFailed,
+			Status: corev1.ConditionTrue,
+			Message: err.Error(),
+			LastTransitionTime: time.Now().Format(time.RFC3339),
+		}
+		state = clustersv1alpha1.Failed
+	} else {
+		msg := fmt.Sprintf("Secret %v", opResult)
+		condition = clustersv1alpha1.Condition{Type: clustersv1alpha1.DeployComplete,
+			Status: corev1.ConditionTrue,
+			Message: msg,
+			LastTransitionTime: time.Now().Format(time.RFC3339),
+		}
+		state = clustersv1alpha1.Ready
+	}
+	if statusNeedsUpdate(mcSecret.Status.Conditions, mcSecret.Status.State, condition, state) {
+		mcSecret.Status.Conditions = append(mcSecret.Status.Conditions, condition)
+		mcSecret.Status.State = state
+		return reconcile.Result{Requeue: true}, r.Status().Update(ctx, mcSecret)
+	} else {
+		return reconcile.Result{}, nil
+	}
+}
+
+func statusNeedsUpdate(curConditions []clustersv1alpha1.Condition, curState clustersv1alpha1.StateType,
+	newCondition clustersv1alpha1.Condition, newState clustersv1alpha1.StateType) bool {
+	if newState == clustersv1alpha1.Failed {
+		return true
+	}
+	if newState != curState {
+		return true
+	}
+	foundStatus := false
+	for _, existingCond := range curConditions {
+		if existingCond.Status == newCondition.Status &&
+			existingCond.Message == newCondition.Message &&
+			existingCond.Type == newCondition.Type {
+			foundStatus = true
+		}
+	}
+	return !foundStatus
 }
 
 func (r *MultiClusterSecretReconciler) SetupWithManager(mgr ctrl.Manager) error {
@@ -58,17 +107,18 @@ func (r *MultiClusterSecretReconciler) fetchMultiClusterSecret(ctx context.Conte
 	return r.Get(ctx, name, mcSecretRef)
 }
 
-func (r *MultiClusterSecretReconciler) createOrUpdateSecret(ctx context.Context, mcSecret clustersv1alpha1.MultiClusterSecret) {
+func (r *MultiClusterSecretReconciler) createOrUpdateSecret(ctx context.Context, mcSecret clustersv1alpha1.MultiClusterSecret, recRes *reconcile.Result) (controllerutil.OperationResult, error) {
 	var secret corev1.Secret
 	secret.Namespace = mcSecret.Namespace
 	secret.Name = mcSecret.Name
 
-	controllerutil.CreateOrUpdate(ctx, r.Client, &secret, func() error {
+	return controllerutil.CreateOrUpdate(ctx, r.Client, &secret, func() error {
 		r.MutateSecret(mcSecret, &secret)
 		// This SetControllerReference call will trigger garbage collection i.e. the secret
 		// will automatically get deleted when the mcSecret is deleted
 		return controllerutil.SetControllerReference(&mcSecret, &secret, r.Scheme)
 	})
+
 }
 
 func (r *MultiClusterSecretReconciler) MutateSecret(mcSecret clustersv1alpha1.MultiClusterSecret, secret *corev1.Secret) {
