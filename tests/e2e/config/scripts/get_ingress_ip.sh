ac947433a73d5606206f682d5d90ea82e69cc1fd
Padmanabha Bhat <padmanabha.bhat@oracle.com>
2021-02-08 13:43:35 +0530

    Fix Copyright issue

--- tests/e2e/config/scripts/get_ingress_ip.sh
+++ tests/e2e/config/scripts/get_ingress_ip.sh
@@ -1,4 +1,9 @@
 
+#
+# Copyright (c) 2021, Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+#
+
 TEST_CONFIG_FILE=$1
 INGRESS_IP=$(kubectl get svc ingress-controller-ingress-nginx-controller -n ingress-nginx -o json | jq -r '.status.loadBalancer.ingress[0].ip')
 sed -i "s/XX_DNS_ZONE_XX/${INGRESS_IP}.xip.io/" ${TEST_CONFIG_FILE}
