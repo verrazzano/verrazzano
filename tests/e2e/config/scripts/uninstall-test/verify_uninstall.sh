97ee3d874c6db56a95d0562084f199607b7cce84
Padmanabha Bhat <padmanabha.bhat@oracle.com>
2021-02-09 20:40:17 +0530

    Initial version of unitial tests

--- tests/e2e/config/scripts/uninstall-test/verify_uninstall.sh
+++ tests/e2e/config/scripts/uninstall-test/verify_uninstall.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 # Copyright (c) 2020, 2021, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
@@ -10,9 +10,12 @@ DIFF_FOUND=false
 DIFF=$(diff ${SCRIPT_DIR}/pre-install-resources/default.txt ${SCRIPT_DIR}/post-uninstall-resources/default.txt | grep "^>")
 echo "Remaining resources:"
 echo $DIFF
+echo "Expected resources:"
+EXPECTED_RESOURCES=`cat $SCRIPT_DIR/expected-resources.txt`
+echo $EXPECTED_RESOURCES
 
-if [ -z "$DIFF" ] ; then
-  echo "No resources found as expected"
+if [ "$DIFF" == "$EXPECTED_RESOURCES" ] ; then
+  echo "expected resources found"
 else
   DIFF_FOUND=true
 fi
