62fd69e06f7289c04b0eb7f0f68eb853a9389157
Deva Sagar <deva.sagar@oracle.com>
2021-02-09 15:29:32 -0500

    more unit test

--- application-operator/controllers/clusters/multiclustersecret_controller_test.go
+++ application-operator/controllers/clusters/multiclustersecret_controller_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/verrazzano/verrazzano/application-operator/mocks"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
@@ -67,16 +68,10 @@ func TestReconcileCreateSecret(t *testing.T) {
 
 	mcSecretSample := getSampleMCSecret(namespace, crName, secretData)
 
-	// expect a call to fetch the MultiClusterSecret (and another to fetch and update corev1.Secret)
-	cli.EXPECT().
-		Get(gomock.Any(), types.NamespacedName{Namespace: namespace, Name: crName}, gomock.AssignableToTypeOf(&mcSecretSample)).
-		DoAndReturn(func(ctx context.Context, name types.NamespacedName, mcSecret *clustersv1alpha1.MultiClusterSecret) error {
-				mcSecret.ObjectMeta = mcSecretSample.ObjectMeta
-				mcSecret.TypeMeta = mcSecretSample.TypeMeta
-				mcSecret.Spec = mcSecretSample.Spec
-			return nil
-		})
+	// expect a call to fetch the MultiClusterSecret
+	doExpectGetMultiClusterSecret(cli, mcSecretSample)
 
+	// expect a call to fetch existing corev1.Secret and return not found error, to test create case
 	cli.EXPECT().
 		Get(gomock.Any(), types.NamespacedName{Namespace: namespace, Name: crName}, gomock.Not(gomock.Nil())).
 		Return(errors.NewNotFound(schema.GroupResource{Group: namespace, Resource: "Secret"}, crName))
@@ -85,26 +80,12 @@ func TestReconcileCreateSecret(t *testing.T) {
 	cli.EXPECT().
 		Create(gomock.Any(), gomock.Any()).
 		DoAndReturn(func(ctx context.Context, s *v1.Secret, opts ...client.CreateOption) error {
-			assert.Equal(v1.SecretTypeOpaque, s.Type)
-			assert.Equal(namespace, s.ObjectMeta.Namespace)
-			assert.Equal(crName, s.ObjectMeta.Name)
-			assert.Equal(secretData, s.Data)
-			assert.Equal(1, len(s.OwnerReferences))
-			assert.Equal("MultiClusterSecret", s.OwnerReferences[0].Kind)
-			assert.Equal(clustersv1alpha1.GroupVersion.String(), s.OwnerReferences[0].APIVersion)
-			assert.Equal(crName, s.OwnerReferences[0].Name)
+			assertSecretValid(assert, s, secretData)
 			return nil
 		})
 
 	// expect a call to update the status of the multicluster secret
-	cli.EXPECT().Status().Return(mockStatusWriter)
-
-	mockStatusWriter.EXPECT().
-		Update(gomock.Any(), gomock.AssignableToTypeOf(&mcSecretSample)).
-		DoAndReturn(func(ctx context.Context, mcSecret *clustersv1alpha1.MultiClusterSecret) error {
-			assertMultiClusterSecretStatus(assert, mcSecret, clustersv1alpha1.Ready, clustersv1alpha1.DeployComplete, v1.ConditionTrue)
-			return nil
-		})
+	doExpectStatusUpdateSucceeded(cli, mockStatusWriter, assert)
 
 	// create a request and reconcile it
 	request := newRequest(namespace, crName)
@@ -114,13 +95,6 @@ func TestReconcileCreateSecret(t *testing.T) {
 	mocker.Finish()
 	assert.NoError(err)
 	assert.Equal(false, result.Requeue)
-}
-
-func assertMultiClusterSecretStatus(assert *asserts.Assertions, mcSecret *clustersv1alpha1.MultiClusterSecret, state clustersv1alpha1.StateType, condition clustersv1alpha1.ConditionType, conditionStatus v1.ConditionStatus) {
-	assert.Equal(state, mcSecret.Status.State)
-	assert.Equal(1, len(mcSecret.Status.Conditions))
-	assert.Equal(conditionStatus, mcSecret.Status.Conditions[0].Status)
-	assert.Equal(condition, mcSecret.Status.Conditions[0].Type)
 }
 
 // TestReconcileUpdateSecret tests the path of reconciling a MultiClusterSecret when the underlying
@@ -141,32 +115,17 @@ func TestReconcileUpdateSecret(t *testing.T) {
 
 	mcSecretSample := getSampleMCSecret(namespace, crName, newSecretData)
 
-	// expect a call to fetch the MultiClusterSecret (and another to fetch and update corev1.Secret)
-	cli.EXPECT().
-		Get(gomock.Any(), types.NamespacedName{Namespace: namespace, Name: crName}, gomock.Not(gomock.Nil())).
-		DoAndReturn(func(ctx context.Context, name types.NamespacedName, mcSecret *clustersv1alpha1.MultiClusterSecret) error {
-			mcSecret.ObjectMeta = mcSecretSample.ObjectMeta
-			mcSecret.TypeMeta = mcSecretSample.TypeMeta
-			mcSecret.Spec = mcSecretSample.Spec
-			return nil
-		})
+	// expect a call to fetch the MultiClusterSecret
+	doExpectGetMultiClusterSecret(cli, mcSecretSample)
 
-	cli.EXPECT().
-		Get(gomock.Any(), types.NamespacedName{Namespace: namespace, Name: crName}, gomock.Not(gomock.Nil())).
-		DoAndReturn(func(ctx context.Context, name types.NamespacedName, secret *v1.Secret) error {
-			secret.Data = existingSecretData
-			secret.ObjectMeta = mcSecretSample.ObjectMeta
-			return nil
-		})
+	// expect a call to fetch underlying secret, and return an existing secret
+	doExpectGetSecretExists(cli, mcSecretSample.ObjectMeta, existingSecretData)
 
 	// expect a call to update the K8S secret with the new secret data
 	cli.EXPECT().
 		Update(gomock.Any(), gomock.Any()).
 		DoAndReturn(func(ctx context.Context, s *v1.Secret, opts ...client.CreateOption) error {
-			assert.Equal(v1.SecretTypeOpaque, s.Type)
-			assert.Equal(namespace, s.ObjectMeta.Namespace)
-			assert.Equal(crName, s.ObjectMeta.Name)
-			assert.Equal(newSecretData, s.Data)
+			assertSecretValid(assert, s, newSecretData)
 			return nil
 		})
 
@@ -187,6 +146,165 @@ func TestReconcileUpdateSecret(t *testing.T) {
 	assert.Equal(false, result.Requeue)
 }
 
+// TestReconcileCreateSecretFailed tests the path of reconciling a MultiClusterSecret
+// when the underlying secret does not exist and fails to be created due to some error condition
+// GIVEN a MultiClusterSecret resource is created
+// WHEN the controller Reconcile function is called and create underlying secret fails
+// THEN expect the status of the MultiClusterSecret to be updated with failure information
+func TestReconcileCreateSecretFailed(t *testing.T) {
+	assert := asserts.New(t)
+
+	mocker := gomock.NewController(t)
+	cli := mocks.NewMockClient(mocker)
+	mockStatusWriter := mocks.NewMockStatusWriter(mocker)
+
+	secretData := map[string][]byte{"username": []byte("aaaaa")}
+
+	mcSecretSample := getSampleMCSecret(namespace, crName, secretData)
+
+	// expect a call to fetch the MultiClusterSecret
+	doExpectGetMultiClusterSecret(cli, mcSecretSample)
+
+	// expect a call to fetch existing corev1.Secret and return not found error, to simulate create case
+	cli.EXPECT().
+		Get(gomock.Any(), types.NamespacedName{Namespace: namespace, Name: crName}, gomock.Not(gomock.Nil())).
+		Return(errors.NewNotFound(schema.GroupResource{Group: namespace, Resource: "Secret"}, crName))
+
+	// expect a call to create the K8S secret and fail the call
+	cli.EXPECT().
+		Create(gomock.Any(), gomock.Any()).
+		DoAndReturn(func(ctx context.Context, s *v1.Secret, opts ...client.CreateOption) error {
+			return errors.NewBadRequest("will not create it")
+		})
+
+	// expect that the status of MultiClusterSecret is updated to failed because we
+	// failed the underlying secret's creation
+	doExpectStatusUpdateFailed(cli, mockStatusWriter, assert)
+
+	// create a request and reconcile it
+	request := newRequest(namespace, crName)
+	reconciler := newReconciler(cli)
+	result, err := reconciler.Reconcile(request)
+
+	mocker.Finish()
+	assert.NoError(err)
+	assert.Equal(false, result.Requeue)
+}
+
+func TestReconcileUpdateSecretFailed(t *testing.T) {
+	assert := asserts.New(t)
+
+	mocker := gomock.NewController(t)
+	cli := mocks.NewMockClient(mocker)
+	mockStatusWriter := mocks.NewMockStatusWriter(mocker)
+
+	secretData := map[string][]byte{"username": []byte("aaaaa")}
+	existingSecretData := map[string][]byte{"username": []byte("existing secret data")}
+
+	mcSecretSample := getSampleMCSecret(namespace, crName, secretData)
+
+	// expect a call to fetch the MultiClusterSecret
+	doExpectGetMultiClusterSecret(cli, mcSecretSample)
+
+	// expect a call to fetch existing corev1.Secret (simulate update case)
+	doExpectGetSecretExists(cli, mcSecretSample.ObjectMeta, existingSecretData)
+
+	// expect a call to update the K8S secret and fail the call
+	cli.EXPECT().
+		Update(gomock.Any(), gomock.Any()).
+		DoAndReturn(func(ctx context.Context, s *v1.Secret, opts ...client.CreateOption) error {
+			return errors.NewBadRequest("will not update it")
+		})
+
+	// expect that the status of MultiClusterSecret is updated to failed because we
+	// failed the underlying secret's creation
+	doExpectStatusUpdateFailed(cli, mockStatusWriter, assert)
+
+	// create a request and reconcile it
+	request := newRequest(namespace, crName)
+	reconciler := newReconciler(cli)
+	result, err := reconciler.Reconcile(request)
+
+	mocker.Finish()
+	assert.NoError(err)
+	assert.Equal(false, result.Requeue)
+}
+
+// doExpectGetSecretExists expects a call to get a corev1.Secret, and return an "existing" secret
+func doExpectGetSecretExists(cli *mocks.MockClient, metadata metav1.ObjectMeta, existingSecretData map[string][]byte) {
+	cli.EXPECT().
+		Get(gomock.Any(), types.NamespacedName{Namespace: namespace, Name: crName}, gomock.Not(gomock.Nil())).
+		DoAndReturn(func(ctx context.Context, name types.NamespacedName, secret *v1.Secret) error {
+			secret.Data = existingSecretData
+			secret.ObjectMeta = metadata
+			return nil
+		})
+}
+
+// doExpectStatusUpdateFailed expects a call to update status of MultiClusterSecret to failure
+func doExpectStatusUpdateFailed(cli *mocks.MockClient, mockStatusWriter *mocks.MockStatusWriter, assert *asserts.Assertions) {
+	// expect a call to update the status of the multicluster secret
+	cli.EXPECT().Status().Return(mockStatusWriter)
+
+	// the status update should be to failure status/conditions on the multicluster secret
+	mockStatusWriter.EXPECT().
+		Update(gomock.Any(), gomock.AssignableToTypeOf(&clustersv1alpha1.MultiClusterSecret{})).
+		DoAndReturn(func(ctx context.Context, mcSecret *clustersv1alpha1.MultiClusterSecret) error {
+			assertMultiClusterSecretStatus(assert, mcSecret, clustersv1alpha1.Failed, clustersv1alpha1.DeployFailed, v1.ConditionTrue)
+			return nil
+		})
+}
+
+// doExpectStatusUpdateSucceeded expects a call to update status of MultiClusterSecret to success
+func doExpectStatusUpdateSucceeded(cli *mocks.MockClient, mockStatusWriter *mocks.MockStatusWriter, assert *asserts.Assertions) {
+	// expect a call to update the status of the multicluster secret
+	cli.EXPECT().Status().Return(mockStatusWriter)
+
+	// the status update should be to success status/conditions on the multicluster secret
+	mockStatusWriter.EXPECT().
+		Update(gomock.Any(), gomock.AssignableToTypeOf(&clustersv1alpha1.MultiClusterSecret{})).
+		DoAndReturn(func(ctx context.Context, mcSecret *clustersv1alpha1.MultiClusterSecret) error {
+			assertMultiClusterSecretStatus(assert, mcSecret, clustersv1alpha1.Ready, clustersv1alpha1.DeployComplete, v1.ConditionTrue)
+			return nil
+		})
+}
+
+// doExpectGetMultiClusterSecret adds an expectation to the given MockClient to expect a Get
+// call for a MultiClusterSecret, and populate the multi cluster secret with given data
+func doExpectGetMultiClusterSecret(cli *mocks.MockClient, mcSecretSample clustersv1alpha1.MultiClusterSecret) {
+	cli.EXPECT().
+		Get(gomock.Any(), types.NamespacedName{Namespace: namespace, Name: crName}, gomock.AssignableToTypeOf(&mcSecretSample)).
+		DoAndReturn(func(ctx context.Context, name types.NamespacedName, mcSecret *clustersv1alpha1.MultiClusterSecret) error {
+			mcSecret.ObjectMeta = mcSecretSample.ObjectMeta
+			mcSecret.TypeMeta = mcSecretSample.TypeMeta
+			mcSecret.Spec = mcSecretSample.Spec
+			return nil
+		})
+}
+
+// assertMultiClusterSecretStatus asserts that the status and conditions on the MultiClusterSecret
+// are as expected
+func assertMultiClusterSecretStatus(assert *asserts.Assertions, mcSecret *clustersv1alpha1.MultiClusterSecret, state clustersv1alpha1.StateType, condition clustersv1alpha1.ConditionType, conditionStatus v1.ConditionStatus) {
+	assert.Equal(state, mcSecret.Status.State)
+	assert.Equal(1, len(mcSecret.Status.Conditions))
+	assert.Equal(conditionStatus, mcSecret.Status.Conditions[0].Status)
+	assert.Equal(condition, mcSecret.Status.Conditions[0].Type)
+}
+
+// assertSecretValid asserts that the metadata and content of the created/updated K8S secret
+// are valid
+func assertSecretValid(assert *asserts.Assertions, s *v1.Secret, secretData map[string][]byte) {
+	assert.Equal(v1.SecretTypeOpaque, s.Type)
+	assert.Equal(namespace, s.ObjectMeta.Namespace)
+	assert.Equal(crName, s.ObjectMeta.Name)
+	assert.Equal(secretData, s.Data)
+	assert.Equal(1, len(s.OwnerReferences))
+	assert.Equal("MultiClusterSecret", s.OwnerReferences[0].Kind)
+	assert.Equal(clustersv1alpha1.GroupVersion.String(), s.OwnerReferences[0].APIVersion)
+	assert.Equal(crName, s.OwnerReferences[0].Name)
+}
+
+// getSampleMCSecret creates and returns a sample MultiClusterSecret used in tests
 func getSampleMCSecret(ns string, name string, secretData map[string][]byte) clustersv1alpha1.MultiClusterSecret {
 	var mcSecret clustersv1alpha1.MultiClusterSecret
 	mcSecret.Spec.Template = clustersv1alpha1.SecretTemplate{Type: v1.SecretTypeOpaque, Data: secretData}
