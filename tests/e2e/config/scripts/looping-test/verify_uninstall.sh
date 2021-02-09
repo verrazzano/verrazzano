a16f85c51ea6965e69e850990fdce70a6b3419b8
Padmanabha Bhat <padmanabha.bhat@oracle.com>
2021-02-10 00:17:31 +0530

    Define variable for tenancy name, fix verification of resources after uninstall

--- tests/e2e/config/scripts/looping-test/verify_uninstall.sh
+++ tests/e2e/config/scripts/looping-test/verify_uninstall.sh
@@ -3,8 +3,12 @@
 # Copyright (c) 2020, 2021, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-SCRIPT_DIR=$(cd $(dirname "$0"); pwd -P)
+if [ -z "$1" ] ; then
+  echo "Please provide the directory containing the resources"
+  exit 1
+fi
 
+SCRIPT_DIR="$1"
 DIFF_FOUND=false
 
 DIFF=$(diff ${SCRIPT_DIR}/pre-install-resources/default.txt ${SCRIPT_DIR}/post-uninstall-resources/default.txt | grep "^>")
