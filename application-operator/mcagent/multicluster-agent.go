18873ddd7e7fe081deace0c87ff042ad34944b8a
Michael Gianatassio <michael.gianatassio@oracle.com>
2021-02-12 14:08:12 -0800

    save

--- application-operator/mcagent/multicluster-agent.go
+++ application-operator/mcagent/multicluster-agent.go
@@ -8,11 +8,11 @@ import (
 	"time"
 
 	"github.com/go-logr/logr"
+	clustersv1alpha1 "github.com/verrazzano/verrazzano/application-operator/apis/clusters/v1alpha1"
 	"github.com/verrazzano/verrazzano/application-operator/constants"
 	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -21,6 +21,7 @@ import (
 func StartAgent(client client.Client, log logr.Logger) {
 	// Wait for the existence of the verrazzano-cluster secret.  It contains the credentials
 	// for connecting to a managed cluster.
+	log.Info("Starting multi-cluster agent")
 	secret := corev1.Secret{}
 
 	for {
@@ -42,35 +43,38 @@ func StartAgent(client client.Client, log logr.Logger) {
 	log.Info(fmt.Sprintf("Found secret named %s in namespace %s for cluster named %q", secret.Name, secret.Namespace, clusterName))
 
 	// Create the client for accessing the managed cluster
-	k8sClient, err := getMCClient(&secret)
+	clientset, err := getMCClient(&secret)
 	if err != nil {
-		log.Error(err, fmt.Sprintf("Failed to get the Kubernetes client for cluster %q", clusterName))
+		log.Error(err, fmt.Sprintf("Failed to get the client for cluster %q", clusterName))
 		return
 	}
 
-	// Start the thread for syncing multi-cluster objects
-	go StartSync(k8sClient, log)
-
-	// Test listing a resource
-	_, err = k8sClient.CoreV1().Namespaces().List(context.Background(), metav1.ListOptions{})
+	// test
+	var mcSecret clustersv1alpha1.MultiClusterSecret
+	err = clientset.Get(context.TODO(), types.NamespacedName{
+		Namespace: "hello",
+		Name:      "admin-elasticsearch",
+	}, &mcSecret)
 	if err != nil {
-		log.Error(err, "Failed to list namespaces")
+		log.Error(err, fmt.Sprintf("Failed to get the MCSecret: %v", err))
 	}
-	log.Info("Successfully listed namespaces")
+	log.Info(fmt.Sprintf("MC placement name", mcSecret.Spec.Placement.Clusters[0]))
 
+	// Start the thread for syncing multi-cluster objects
+	go StartSync(clientset, log)
 }
 
 // StartSync - start the thread for syncing multi-cluster objects
-func StartSync(k8sClient *kubernetes.Clientset, log logr.Logger) {
+func StartSync(clientset client.Client, log logr.Logger) {
 	// Periodically loop looking for multi-cluster objects
 	for {
-		getMCSecretObjects(k8sClient, log)
+		getMCSecretObjects(clientset, log)
 		time.Sleep(5 * time.Minute)
 	}
 }
 
 // Synchronize MCSecret objects to the local cluster
-func getMCSecretObjects(k8sClient *kubernetes.Clientset, log logr.Logger) error {
+func getMCSecretObjects(clientset client.Client, log logr.Logger) error {
 	// Get all the MCSecret objects from the admin cluster
 
 	return nil
@@ -93,8 +97,8 @@ func validateClusterSecret(secret *corev1.Secret) error {
 	return nil
 }
 
-// Get the Kubernetes client for accessing the managed cluster
-func getMCClient(secret *corev1.Secret) (*kubernetes.Clientset, error) {
+// Get the clientset for accessing the managed cluster
+func getMCClient(secret *corev1.Secret) (client.Client, error) {
 	// Create a temp file that contains the kubeconfig
 	tmpFile, err := ioutil.TempFile("/tmp", "kubeconfig")
 	if err != nil {
@@ -111,12 +115,12 @@ func getMCClient(secret *corev1.Secret) (*kubernetes.Clientset, error) {
 	if err != nil {
 		return nil, err
 	}
+	scheme := runtime.NewScheme()
+	_ = clustersv1alpha1.AddToScheme(scheme)
 
-	// create the clientset
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		return nil, err
-	}
+	clientset, err := client.New(config, client.Options{
+		Scheme: scheme,
+	})
 
 	return clientset, nil
 }
