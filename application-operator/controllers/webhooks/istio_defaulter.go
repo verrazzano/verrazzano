e4a78469da98966593391561367355d351d592cc
robert.donat <robert.donat@oracle.com>
2021-02-14 17:24:05 -0500

    save

--- application-operator/controllers/webhooks/istio_defaulter.go
+++ application-operator/controllers/webhooks/istio_defaulter.go
@@ -9,6 +9,8 @@ import (
 	"net/http"
 
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
@@ -23,23 +25,33 @@ type IstioWebhook struct {
 	decoder *admission.Decoder
 }
 
-// Handle identifies OAM created pods and mutates pods and adds additional resources as needed.
+// Handle identifies OAM created pods with a name selecter that matches istio-enabled:true,
+// mutates pods and adds additional resources as needed.
 func (a *IstioWebhook) Handle(ctx context.Context, req admission.Request) admission.Response {
 	var log = ctrl.Log.WithName("webhooks.istio-defaulter")
 
 	pod := &corev1.Pod{}
 
+	log.Info("Request %s:%s", req.Namespace, req.Name)
 	err := a.decoder.Decode(req, pod)
 	if err != nil {
 		return admission.Errored(http.StatusBadRequest, err)
 	}
 
-	log.Info(fmt.Sprintf("Created pod: %s:%s", pod.Namespace, pod.Name))
+	obj, err := runtime.DefaultUnstructuredConverter.ToUnstructured(pod)
+	if err != nil {
+		return admission.Errored(http.StatusBadRequest, err)
+	}
+	u := unstructured.Unstructured{Object: obj}
+
+	log.Info(fmt.Sprintf("Unstructured pod: %s:%s:%s", u.GetNamespace(), u.GetName(), u.GetGenerateName()))
 
 	//	marshaledPod, err := json.Marshal(pod)
 	//	if err != nil {
 	//		return admission.Errored(http.StatusInternalServerError, err)
 	//	}
+
+	// TODO: set label that says this is an istio enabled OAM pod
 
 	return admission.Allowed("No action required")
 	//	return admission.PatchResponseFromRaw(req.Object.Raw, marshaledPod)
