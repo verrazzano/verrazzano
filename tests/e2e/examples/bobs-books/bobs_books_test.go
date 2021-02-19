bd365c9097deb8c9bc70b15ee0642981d971579c
Fred Tibbitts <fred.tibbitts@oracle.com>
2021-02-19 17:33:12 -0500

    Fix ingress trait substring for bobs orders app

--- tests/e2e/examples/bobs-books/bobs_books_test.go
+++ tests/e2e/examples/bobs-books/bobs_books_test.go
@@ -165,7 +165,7 @@ var _ = ginkgo.Describe("Verify Bobs Books example application.", func() {
 				ingress := pkg.Ingress()
 				pkg.Log(pkg.Info, fmt.Sprintf("Ingress: %s", ingress))
 				url := fmt.Sprintf("http://%s/bobs-bookstore-order-manager/orders", ingress)
-				host := pkg.GetHostnameFromGateway("bobs-books", "bobs")
+				host := pkg.GetHostnameFromGateway("bobs-books", "bobs-orders")
 				status, content := pkg.GetWebPageWithCABundle(url, host)
 				return pkg.WebResponse{
 					Status:  status,
