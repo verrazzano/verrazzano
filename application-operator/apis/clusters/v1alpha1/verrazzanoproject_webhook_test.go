5b822b0e71823170161cb669c9f742ea072415c5
Michael Gianatassio <michael.gianatassio@oracle.com>
2021-02-25 13:19:08 -0800

    add more tests

--- application-operator/apis/clusters/v1alpha1/verrazzanoproject_webhook_test.go
+++ application-operator/apis/clusters/v1alpha1/verrazzanoproject_webhook_test.go
@@ -4,6 +4,7 @@
 package v1alpha1
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -21,25 +22,61 @@ var testProject = VerrazzanoProject{
 	},
 }
 
-// TestCreateVerrazzanoProject tests the validation of VerrazzanoProject resource
-// GIVEN a call validate VerrazzanoProject create
+// TestVerrazzanoProject tests the validation of VerrazzanoProject resource
+// GIVEN a call validate VerrazzanoProject on create or update
 // WHEN the VerrazzanoProject is properly formed
 // THEN the validation should succeed
-func TestCreateVerrazzanoProject(t *testing.T) {
+func TestVerrazzanoProject(t *testing.T) {
 	// Test data
 	testVP := testProject
+
+	// Test create
 	err := testVP.ValidateCreate()
+	assert.NoError(t, err, "Error validating VerrazzanoMultiCluster resource")
+
+	// Test update
+	err = testVP.ValidateUpdate(&VerrazzanoProject{})
 	assert.NoError(t, err, "Error validating VerrazzanoMultiCluster resource")
 }
 
-// TestUpdateVerrazzanoProject tests the validation of VerrazzanoProject resource
-// GIVEN a call validate VerrazzanoProject update
-// WHEN the VerrazzanoProject is properly formed
-// THEN the validation should succeed
-func TestUpdateVerrazzanoProject(t *testing.T) {
+// TestInvalidNamespace tests the validation of VerrazzanoProject resource
+// GIVEN a call validate VerrazzanoProject on create or update
+// WHEN the VerrazzanoProject contains an invalid namespace
+// THEN the validation should fail
+func TestInvalidNamespace(t *testing.T) {
 
 	// Test data
 	testVP := testProject
-	err := testVP.ValidateUpdate(&VerrazzanoProject{})
-	assert.NoError(t, err, "Error validating VerrazzanoMultiCluster resource")
+	testVP.Namespace = "invalid-namespace"
+
+	// Test create
+	err := testVP.ValidateCreate()
+	assert.Error(t, err, "Expected failure for invalid namespace")
+	assert.Containsf(t, err.Error(), fmt.Sprintf("resource must be %q", constants.VerrazzanoMultiClusterNamespace), "unexpected failure string")
+
+	// Test update
+	err = testVP.ValidateUpdate(&VerrazzanoProject{})
+	assert.Error(t, err, "Expected failure for invalid namespace")
+	assert.Containsf(t, err.Error(), fmt.Sprintf("resource must be %q", constants.VerrazzanoMultiClusterNamespace), "unexpected failure string")
+}
+
+// TestInvalidNamespaces tests the validation of VerrazzanoProject resource
+// GIVEN a call validate VerrazzanoProject on create or update
+// WHEN the VerrazzanoProject contains an invalid namespace list
+// THEN the validation should fail
+func TestInvalidNamespaces(t *testing.T) {
+
+	// Test data
+	testVP := testProject
+	testVP.Spec.Namespaces = []string{}
+
+	// Test create
+	err := testVP.ValidateCreate()
+	assert.Error(t, err, "Expected failure for invalid namespace list")
+	assert.Containsf(t, err.Error(), "One or more namespaces must be provided", "unexpected failure string")
+
+	// Test update
+	err = testVP.ValidateUpdate(&VerrazzanoProject{})
+	assert.Error(t, err, "Expected failure for invalid namespace list")
+	assert.Containsf(t, err.Error(), "One or more namespaces must be provided", "unexpected failure string")
 }
