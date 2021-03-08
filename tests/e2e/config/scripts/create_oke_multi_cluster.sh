3cfb87e13816ec5c85ec24c8c2aeac3ff2a152c8
Padmanabha Bhat <35250350+bhatpmk@users.noreply.github.com>
2021-03-08 10:15:32 +0530

    OKE multi-cluster setup (#774)
    * Initial commit
    
    * Fix the issues with OKE multi-cluster setup
    
    * Fix the variable usage for OKE cluster
    
    * Adjust the debug statements
    
    * Remove redirecting console output
    
    * Test change to increase the timeout and print pod status at the end
    
    * Add missing newline at the end of defaultresource_test.go

---
+++ tests/e2e/config/scripts/create_oke_multi_cluster.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+#
+# Copyright (c) 2020, 2021, Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+#
+
+SCRIPT_DIR=$(cd $(dirname "$0"); pwd -P)
+CLUSTER_COUNT=${1:-1}
+KUBECONFIG_DIR=$2
+REQUIRED_VNC_COUNT=0
+REQUIRED_LB_COUNT=0
+CLUSTER_NAME_PREFIX="oke"
+MODULE_NAME_PREFIX="oke"
+
+check_for_resources() {
+  local resource_type=$1
+  local service_name=$2
+  local limit_name=$3
+  local min_required=$4
+
+  local count=$(${SCRIPT_DIR}/get_resource_availability.sh $service_name $limit_name)
+  local status_code=$?
+  if [ ${status_code:-1} -eq 0 ]; then
+    # OCI query succeeded, proceed with value evaluation
+    if [ $count -lt $min_required ]; then
+      echo "ERROR: Not enough ${resource_type}s available to create the OKE cluster : ${count}"
+      exit 1
+    elif [ $count -lt 5 ]; then
+      echo "WARNING: Critically low number of ${resource_type}s available in tenancy: ${count}. Proceeding with creating the OKE cluster ..."
+    else
+      echo "Sufficient number of ${resource_type}s available for creating the OKE cluster: ${count}"
+    fi
+  else
+    echo "ERROR: Query for available number of ${resource_type}s in tenancy failed."
+    exit 1
+  fi
+}
+
+if [ -z "$TF_VAR_compartment_id" ] ; then
+    echo "TF_VAR_compartment_id env var must be set!"
+    exit 1
+fi
+
+echo "Check OCI CLI is working..."
+# If OCI CLI is not configured correctly, the following command will have a non-zero return code
+# which will cause the job to fail at this point
+oci ce cluster list --compartment-id=${TF_VAR_compartment_id} --region=${TF_VAR_region} > /dev/null
+
+if [ -f /tmp/main.tf ]; then
+  rm /tmp/main.tf
+fi
+
+# Create main.tf for the multi-cluster configuration, and evaluate the VNC and LB counts
+for i in $(seq 1 $CLUSTER_COUNT)
+do
+  let REQUIRED_VNC_COUNT=$REQUIRED_VNC_COUNT+1
+  let REQUIRED_LB_COUNT=$REQUIRED_LB_COUNT+2
+  cp ${SCRIPT_DIR}/terraform/cluster/multi_cluster_main_tf_template /tmp/main.tf.tmp
+  sed -i "s/MODULE_NAME/$MODULE_NAME_PREFIX-$i/g" /tmp/main.tf.tmp
+  sed -i "s/CLUSTER_NAME/$CLUSTER_NAME_PREFIX-$i/g" /tmp/main.tf.tmp
+  cat /tmp/main.tf.tmp >> /tmp/main.tf
+done
+
+echo "Minimum required VNC count: ${REQUIRED_VNC_COUNT}"
+echo "Minimum required Load Balancer count: ${REQUIRED_LB_COUNT}"
+
+# Copy the temporary file as main.tf
+cat /tmp/main.tf
+cp /tmp/main.tf ${SCRIPT_DIR}/terraform/cluster/main.tf
+
+rm /tmp/main.tf
+rm -rf ${KUBECONFIG_DIR}/*
+
+# check available resources
+check_for_resources VCN vcn vcn-count $REQUIRED_VNC_COUNT
+check_for_resources LB load-balancer lb-100mbps-count $REQUIRED_LB_COUNT
+
+echo 'Create OKE cluster...'
+cd ${SCRIPT_DIR}/terraform/cluster
+./create-multi-cluster.sh $CLUSTER_COUNT $CLUSTER_NAME_PREFIX
+status_code=$?
+
+if [ ${status_code:-1} -eq 0 ]; then
+    echo "OKE Cluster creation request submitted."
+    for i in $(seq 1 $CLUSTER_COUNT)
+    do
+      echo "Create kube config for cluster ${TF_VAR_label_prefix}-$CLUSTER_NAME_PREFIX-$i ..."
+      CLUSTER_OCID=$(oci ce cluster list --compartment-id "${TF_VAR_compartment_id}" --name "${TF_VAR_label_prefix}-${CLUSTER_NAME_PREFIX}-$i" --lifecycle-state "ACTIVE" | jq -r '.data[0]."id"')
+      echo "OCID of the cluster ${TF_VAR_label_prefix}-$CLUSTER_NAME_PREFIX-$i : ${CLUSTER_OCID}."
+      mkdir -p "${KUBECONFIG_DIR}/$i"
+      oci ce cluster create-kubeconfig --cluster-id ${CLUSTER_OCID} --file "${KUBECONFIG_DIR}/$i/kube_config" --region "${TF_VAR_region}" --token-version 2.0.0
+      export KUBECONFIG="${KUBECONFIG_DIR}/$i/kube_config"
+      # Adding a Service Account Authentication Token to kubeconfig
+      # https://docs.cloud.oracle.com/en-us/iaas/Content/ContEng/Tasks/contengaddingserviceaccttoken.htm
+      ${SCRIPT_DIR}/update_oke_kubeconfig.sh
+      # Right after oke cluster is provisioned, it takes a while before any node is added to the cluster
+      # The next command will wait for node to come up before continue
+      echo "Waiting for nodes to be added to cluster..."
+      timeout 15m bash -c 'until kubectl get nodes | grep NAME; do sleep 10; done'
+      echo "Waiting for nodes to transition to 'READY'..."
+      kubectl wait --for=condition=ready nodes --timeout=5m --all
+    done
+else
+    echo "OKE Cluster creation request failed!"
+    exit 1
+fi
