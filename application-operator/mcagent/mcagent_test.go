f0672e295c47b8f8571f0e7b07da19b1a0603d88
Michael Gianatassio <michael.gianatassio@oracle.com>
2021-03-08 13:17:35 -0800

    save

--- application-operator/mcagent/mcagent_test.go
+++ application-operator/mcagent/mcagent_test.go
@@ -5,15 +5,35 @@ package mcagent
 
 import (
 	"context"
+	"fmt"
+	"os/exec"
 	"testing"
 
 	"github.com/golang/mock/gomock"
 	asserts "github.com/stretchr/testify/assert"
+	clustersv1alpha1 "github.com/verrazzano/verrazzano/application-operator/apis/clusters/v1alpha1"
+	"github.com/verrazzano/verrazzano/application-operator/constants"
 	"github.com/verrazzano/verrazzano/application-operator/mocks"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-func TestProcessAgentThread(t *testing.T) {
+var validSecret = corev1.Secret{
+	ObjectMeta: metav1.ObjectMeta{
+		Name:      constants.MCAgentSecret,
+		Namespace: constants.VerrazzanoSystemNamespace,
+	},
+	Data: map[string][]byte{constants.ClusterNameData: []byte("cluster1"), constants.AdminKubeconfigData: []byte("kubeconfig")},
+}
+
+// TestProcessAgentThreadNoProjects tests agent thread when no projects exist
+// GIVEN a request to process the agent loop
+// WHEN the a new VerrazzanoProjects resources exists
+// THEN ensure that there are no calls to sync any multi-cluste resources
+func TestProcessAgentThreadNoProjects(t *testing.T) {
 	assert := asserts.New(t)
 	log := ctrl.Log.WithName("test")
 
@@ -25,6 +45,22 @@ func TestProcessAgentThread(t *testing.T) {
 	adminMocker := gomock.NewController(t)
 	adminMock := mocks.NewMockClient(adminMocker)
 
+	// Managed Cluster - expect call to get the cluster registration secret.
+	mcMock.EXPECT().
+		Get(gomock.Any(), types.NamespacedName{Namespace: constants.VerrazzanoSystemNamespace, Name: constants.MCAgentSecret}, gomock.Not(gomock.Nil())).
+		DoAndReturn(func(ctx context.Context, name types.NamespacedName, secret *corev1.Secret) error {
+			secret.ObjectMeta = validSecret.ObjectMeta
+			secret.Data = validSecret.Data
+			return nil
+		})
+
+	// Admin Cluster - expect call to list VerrazzanoProject objects - return an empty list
+	adminMock.EXPECT().
+		List(gomock.Any(), &clustersv1alpha1.VerrazzanoProjectList{}, gomock.Not(gomock.Nil())).
+		DoAndReturn(func(ctx context.Context, list *clustersv1alpha1.VerrazzanoProjectList, opts ...*client.ListOptions) error {
+			return fmt.Errorf("no project resources found %s", exec.ErrNotFound)
+		})
+
 	// Make the request
 	s := &Syncer{
 		AdminClient:        adminMock,
@@ -32,6 +68,48 @@ func TestProcessAgentThread(t *testing.T) {
 		Log:                log,
 		ManagedClusterName: testClusterName,
 		Context:            context.TODO(),
+	}
+	err := s.ProcessAgentThread()
+
+	// Validate the results
+	adminMocker.Finish()
+	mcMocker.Finish()
+	assert.NoError(err)
+}
+
+// TestProcessAgentThreadSecretDeleted tests agent thread when the registration secret is deleted
+// GIVEN a request to process the agent loop
+// WHEN the registration secret has been deleted
+// THEN ensure that there are no calls to get VerrazzanoProject resources
+func TestProcessAgentThreadSecretDeleted(t *testing.T) {
+	assert := asserts.New(t)
+	log := ctrl.Log.WithName("test")
+
+	// Managed cluster mocks
+	mcMocker := gomock.NewController(t)
+	mcMock := mocks.NewMockClient(mcMocker)
+
+	// Admin cluster mocks
+	adminMocker := gomock.NewController(t)
+	adminMock := mocks.NewMockClient(adminMocker)
+
+	// Managed Cluster - expect call to get the cluster registration secret.
+	mcMock.EXPECT().
+		Get(gomock.Any(), types.NamespacedName{Namespace: constants.VerrazzanoSystemNamespace, Name: constants.MCAgentSecret}, gomock.Not(gomock.Nil())).
+		DoAndReturn(func(ctx context.Context, name types.NamespacedName, secret *corev1.Secret) error {
+			return fmt.Errorf("no project resources found %s", exec.ErrNotFound)
+		})
+
+	// Do not expect any further calls because the registration secret no longer exists
+
+	// Make the request
+	s := &Syncer{
+		AdminClient:        adminMock,
+		LocalClient:        mcMock,
+		Log:                log,
+		ManagedClusterName: testClusterName,
+		Context:            context.TODO(),
+		AgentSecretFound:   true,
 	}
 	err := s.ProcessAgentThread()
 
