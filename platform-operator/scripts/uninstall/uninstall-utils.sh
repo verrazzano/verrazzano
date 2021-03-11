c93af94e663cd9362f7c89318c868cc6628ba577
Padmanabha Bhat <padmanabha.bhat@oracle.com>
2021-03-11 11:54:03 +0530

    Checkout 0-uninstall-applications.sh from master

--- platform-operator/scripts/uninstall/uninstall-utils.sh
+++ platform-operator/scripts/uninstall/uninstall-utils.sh
@@ -56,7 +56,7 @@ function delete_k8s_resource_from_all_namespaces() {
       fi
     done
   fi
-  # Delete the CRD
+  # Delete the CRDs, without any CRs based on that, from all the namespaces
   if kubectl get crd "${res}"> /dev/null 2>&1 ; then
     kubectl delete crd "${res}" --ignore-not-found > /dev/null 2>&1
   fi
