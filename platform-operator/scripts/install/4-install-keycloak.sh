84b450467a171f491c2c9ab24b7cadb9d260e3d4
Ravi Singhal <ravi.r.singhal@oracle.com>
2021-03-11 18:05:44 +0530

    implement review comments

--- platform-operator/scripts/install/4-install-keycloak.sh
+++ platform-operator/scripts/install/4-install-keycloak.sh
@@ -69,8 +69,9 @@ function install_keycloak {
   # Replace strings in keycloak.json file
   VZ_PW_SALT=$(kubectl get secret -n ${VERRAZZANO_NS} verrazzano -o jsonpath="{.data.salt}")
   VZ_PW_HASH=$(kubectl get secret -n ${VERRAZZANO_NS} verrazzano -o jsonpath="{.data.hash}")
+  VZ_ADMIN_GROUP=$(helm show values ${VZ_CHARTS_DIR}/verrazzano --jsonpath {.userrolebindings.admin.name})
 
-  sed "s|ENV_NAME|${ENV_NAME}|g;s|DNS_SUFFIX|${DNS_SUFFIX}|g;s|VZ_SYS_REALM|${VZ_SYS_REALM}|g;s|VZ_USERNAME|${VZ_USERNAME}|g;s|VZ_PW_SALT|${VZ_PW_SALT}|g;s|VZ_PW_HASH|${VZ_PW_HASH}|g" $SCRIPT_DIR/config/keycloak.json > ${TMP_DIR}/keycloak-sed.json
+  sed "s|ENV_NAME|${ENV_NAME}|g;s|DNS_SUFFIX|${DNS_SUFFIX}|g;s|VZ_SYS_REALM|${VZ_SYS_REALM}|g;s|VZ_USERNAME|${VZ_USERNAME}|g;s|VZ_PW_SALT|${VZ_PW_SALT}|g;s|VZ_PW_HASH|${VZ_PW_HASH}|g;s|VZ_ADMIN_GROUP|${VZ_ADMIN_GROUP}|g" $SCRIPT_DIR/config/keycloak.json > ${TMP_DIR}/keycloak-sed.json
 
   # Create keycloak secret
   kubectl create secret generic keycloak-realm-cacert \
