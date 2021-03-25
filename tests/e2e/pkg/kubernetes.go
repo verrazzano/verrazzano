738923650053382016dafa690d19fb7727b37571
ddrechse <doug.drechsel@oracle.com>
2021-03-25 17:04:13 -0400

    RBAC Acceptance Tests for new verrazzano cluster roles

--- tests/e2e/pkg/kubernetes.go
+++ tests/e2e/pkg/kubernetes.go
@@ -6,6 +6,8 @@ package pkg
 import (
 	"context"
 	"fmt"
+	"k8s.io/api/authorization/v1beta1"
+	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
@@ -33,6 +35,22 @@ import (
 )
 
 const dockerconfigjsonTemplate string = "{\"auths\":{\"%v\":{\"username\":\"%v\",\"password\":\"%v\",\"auth\":\"%v\"}}}"
+
+// headerAdder is an http.RoundTripper that adds additional headers to the request
+type headerAdder struct {
+	headers map[string][]string
+
+	rt http.RoundTripper
+}
+
+func (h *headerAdder) RoundTrip(req *http.Request) (*http.Response, error) {
+	for k, vv := range h.headers {
+		for _, v := range vv {
+			req.Header.Add(k, v)
+		}
+	}
+	return h.rt.RoundTrip(req)
+}
 
 // GetKubeConfig will get the kubeconfig from the environment variable KUBECONFIG, if set, or else from $HOME/.kube/config
 func GetKubeConfig() *restclient.Config {
@@ -479,6 +497,43 @@ func DoesRoleBindingContainSubject(namespace, name, subjectKind, subjectName str
 	return false
 }
 
+func CreateRoleBinding(userOCID string, namespace string, rolebindingname string, clusterrolename string) error {
+
+	subject1 := rbacv1.Subject{
+		Kind:      "User",
+		APIGroup:  "rbac.authorization.k8s.io",
+		Name:      userOCID,
+		Namespace: "",
+	}
+	subjects := []rbacv1.Subject{0: subject1}
+
+	rb := rbacv1.RoleBinding{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "RoleBinding",
+			APIVersion: "rbac.authorization.k8s.io/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: rolebindingname,
+		},
+		Subjects: subjects,
+		RoleRef: rbacv1.RoleRef{
+			APIGroup: "rbac.authorization.k8s.io",
+			Kind:     "ClusterRole",
+			Name:     clusterrolename,
+		},
+	}
+
+	// Get the Kubernetes clientset
+	clientset := GetKubernetesClientset()
+
+	_, err := clientset.RbacV1().RoleBindings(namespace).Create(context.TODO(), &rb, metav1.CreateOptions{})
+	if err != nil {
+		Log(Info, fmt.Sprintf("Failed to create role binding: %v", err))
+	}
+
+	return err
+}
+
 // GetIstioClientset returns the clientset object for Istio
 func GetIstioClientset() *istioClient.Clientset {
 	cs, err := istioClient.NewForConfig(GetKubeConfig())
@@ -498,4 +553,55 @@ func GetConfigMap(configMapName string, namespace string) *corev1.ConfigMap {
 		ginkgo.Fail(fmt.Sprintf("Failed to get Config Map %v from namespace %v:  Error = %v ", configMapName, namespace, err))
 	}
 	return configMap
+}
+
+func CanI(userOCID string, namespace string, verb string, resource string) (bool, string) {
+	return CanIGroup(userOCID, namespace, verb, resource, "")
+}
+
+func CanIGroup(userOCID string, namespace string, verb string, resource string, group string) (bool, string) {
+
+	canI := &v1beta1.SelfSubjectAccessReview{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "SelfSubjectAccessReview",
+			APIVersion: "authorization.k8s.io/v1",
+		},
+		Spec: v1beta1.SelfSubjectAccessReviewSpec{
+			ResourceAttributes: &v1beta1.ResourceAttributes{
+				Namespace:   namespace,
+				Verb:        verb,
+				Group:       group,
+				Version:     "",
+				Resource:    resource,
+				Subresource: "",
+				Name:        "",
+			},
+		},
+	}
+
+	config := GetKubeConfig()
+
+	wt := config.WrapTransport // Config might already have a transport wrapper
+	config.WrapTransport = func(rt http.RoundTripper) http.RoundTripper {
+		if wt != nil {
+			rt = wt(rt)
+		}
+		return &headerAdder{
+			headers: map[string][]string{"Impersonate-User": {userOCID}},
+			rt:      rt,
+		}
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		ginkgo.Fail("Could not get Kubernetes clientset")
+	}
+
+	auth, err := clientset.AuthorizationV1beta1().SelfSubjectAccessReviews().Create(context.TODO(), canI, metav1.CreateOptions{})
+	if err != nil {
+		Log(Info, fmt.Sprintf("Failed to check perms: %v", err))
+	}
+
+	return auth.Status.Allowed, auth.Status.Reason
+
 }
---
+++ tests/e2e/security/rbac/rbac_test.go
@@ -0,0 +1,240 @@
+// Copyright (c) 2021, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package rbac_test
+
+import (
+	"fmt"
+	"github.com/onsi/gomega"
+	"github.com/verrazzano/verrazzano/tests/e2e/pkg"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"time"
+
+	"github.com/onsi/ginkgo"
+)
+
+var (
+	expectedPodsOperator = []string{"verrazzano-application-operator"}
+	expectedPodsOam      = []string{"oam-kubernetes-runtime"}
+	waitTimeout          = 10 * time.Minute
+	pollingInterval      = 30 * time.Second
+)
+
+const (
+	verrazzanoSystemNS = "verrazzano-system"
+	rbacTestNamespace  = "rbactest"
+	v80ProjectAdmin    = "ocid1.user.oc1..aaaaaaaallodotxfvg0g1antsyq3gonyyhblya66kiqjnp2kogonykvjwi19"
+	v80ProjectMonitor  = "ocid1.user.oc1..aaaaaaaallodotxfvg0yank33sq3gonyghblya66kiqjnp2kogonykvjwi19"
+)
+
+var _ = ginkgo.BeforeSuite(func() {
+	pkg.Log(pkg.Info, "Create namespace")
+	if _, err := pkg.CreateNamespace(rbacTestNamespace, map[string]string{"verrazzano-managed": "true", "istio-injection": "enabled"}); err != nil {
+		ginkgo.Fail(fmt.Sprintf("Failed to create namespace: %v", err))
+	}
+})
+
+var _ = ginkgo.AfterSuite(func() {
+	pkg.Log(pkg.Info, "Delete namespace")
+	if err := pkg.DeleteNamespace(rbacTestNamespace); err != nil {
+		pkg.Log(pkg.Error, fmt.Sprintf("Failed to delete the namespace: %v", err))
+	}
+	gomega.Eventually(func() bool {
+		ns, err := pkg.GetNamespace(rbacTestNamespace)
+		return ns == nil && err != nil && errors.IsNotFound(err)
+	}, 3*time.Minute, 15*time.Second).Should(gomega.BeFalse())
+})
+
+var _ = ginkgo.Describe("Test RBAC Permission", func() {
+	ginkgo.Context("for user with role verrazzano-project-admin", func() {
+
+		ginkgo.It("Fail getting Pods in namespace rbactest", func() {
+			pkg.Log(pkg.Info, "Can User List Pods in NameSpace rbactest?  No")
+			if allowed, reason := pkg.CanI(v80ProjectAdmin, rbacTestNamespace, "list", "pods"); allowed != false {
+				ginkgo.Fail(fmt.Sprintf("FAIL: Passed Authorization on user list pods: Allowed = %t, reason = %s", allowed, reason))
+			}
+		})
+
+		ginkgo.It("Create RoleBinding Admin for verrazzano-project-admin", func() {
+			pkg.Log(pkg.Info, "Create RoleBinding Admin for verrazzano-project-admin")
+			if err := pkg.CreateRoleBinding(v80ProjectAdmin, rbacTestNamespace, "admin-binding", "admin"); err != nil {
+				ginkgo.Fail(fmt.Sprintf("FAIL: RoleBinding creation failed: reason = %s", err))
+			}
+		})
+
+		ginkgo.It("Succeed getting Pods in namespace rbactest", func() {
+			pkg.Log(pkg.Info, "Can User List Pods in NameSpace rbactest?  no")
+			if allowed, reason := pkg.CanI(v80ProjectAdmin, rbacTestNamespace, "list", "pods"); allowed == false {
+				ginkgo.Fail(fmt.Sprintf("FAIL: Did Not Pass Authorization on user list pods: Allowed = %t, reason = %s", allowed, reason))
+			}
+		})
+
+		ginkgo.It("Fail getting Pods in namespace verrazzano-system", func() {
+			pkg.Log(pkg.Info, "Can User List Pods in NameSpace verrazzano-system?  No")
+			if allowed, reason := pkg.CanI(v80ProjectAdmin, verrazzanoSystemNS, "list", "pods"); allowed != false {
+				ginkgo.Fail(fmt.Sprintf("FAIL: Passed Authorization on user list pods: Allowed = %t, reason = %s", allowed, reason))
+			}
+		})
+
+		ginkgo.It("Fail create ApplicationConfiguration in namespace rbactest", func() {
+			pkg.Log(pkg.Info, "Can User create ApplicationConfiguration in NameSpace rbactest?  No")
+			if allowed, reason := pkg.CanIGroup(v80ProjectAdmin, rbacTestNamespace, "create", "applicationconfigurations", "core.oam.dev"); allowed != false {
+				ginkgo.Fail(fmt.Sprintf("FAIL: Passed Authorization on user create ApplicationConfiguration: Allowed = %t, reason = %s", allowed, reason))
+			}
+		})
+
+		ginkgo.It("Fail list ApplicationConfiguration in namespace rbactest", func() {
+			pkg.Log(pkg.Info, "Can User list ApplicationConfiguration in NameSpace rbactest?  No")
+			if allowed, reason := pkg.CanIGroup(v80ProjectAdmin, rbacTestNamespace, "list", "applicationconfigurations", "core.oam.dev"); allowed != false {
+				ginkgo.Fail(fmt.Sprintf("FAIL: Passed Authorization on user list ApplicationConfiguration: Allowed = %t, reason = %s", allowed, reason))
+			}
+		})
+
+		ginkgo.It("Fail create OAM Component in namespace rbactest", func() {
+			pkg.Log(pkg.Info, "Can User create OAM Components in NameSpace rbactest?  No")
+			if allowed, reason := pkg.CanIGroup(v80ProjectAdmin, rbacTestNamespace, "create", "components", "core.oam.dev"); allowed != false {
+				ginkgo.Fail(fmt.Sprintf("FAIL: Passed Authorization on user create OAM Components: Allowed = %t, reason = %s", allowed, reason))
+			}
+		})
+
+		ginkgo.It("Fail list OAM Component in namespace rbactest", func() {
+			pkg.Log(pkg.Info, "Can User list OAM Components in NameSpace rbactest?  No")
+			if allowed, reason := pkg.CanIGroup(v80ProjectAdmin, rbacTestNamespace, "list", "components", "core.oam.dev"); allowed != false {
+				ginkgo.Fail(fmt.Sprintf("FAIL: Passed Authorization on user list OAM Components: Allowed = %t, reason = %s", allowed, reason))
+			}
+		})
+
+		ginkgo.It("Create RoleBinding verrazzano-project-admin-binding for verrazzano-project-admin", func() {
+			pkg.Log(pkg.Info, "Create RoleBinding verrazzano-project-admin-binding for verrazzano-project-admin")
+			if err := pkg.CreateRoleBinding(v80ProjectAdmin, rbacTestNamespace, "verrazzano-project-admin-binding", "verrazzano-project-admin"); err != nil {
+				ginkgo.Fail(fmt.Sprintf("FAIL: RoleBinding creation failed: reason = %s", err))
+			}
+		})
+
+		ginkgo.It("Succeed create ApplicationConfiguration in namespace rbactest", func() {
+			pkg.Log(pkg.Info, "Can User create ApplicationConfiguration in NameSpace rbactest?  Yes")
+			if allowed, reason := pkg.CanIGroup(v80ProjectAdmin, rbacTestNamespace, "create", "applicationconfigurations", "core.oam.dev"); allowed == false {
+				ginkgo.Fail(fmt.Sprintf("FAIL: Did Not Pass Authorization on user create ApplicationConfiguration: Allowed = %t, reason = %s", allowed, reason))
+			}
+		})
+
+		ginkgo.It("Succeed list ApplicationConfiguration in namespace rbactest", func() {
+			pkg.Log(pkg.Info, "Can User list ApplicationConfiguration in NameSpace rbactest?  Yes")
+			if allowed, reason := pkg.CanIGroup(v80ProjectAdmin, rbacTestNamespace, "list", "applicationconfigurations", "core.oam.dev"); allowed == false {
+				ginkgo.Fail(fmt.Sprintf("FAIL: Did Not Pass Authorization on user list ApplicationConfiguration: Allowed = %t, reason = %s", allowed, reason))
+			}
+		})
+
+		ginkgo.It("Succeed create OAM Components in namespace rbactest", func() {
+			pkg.Log(pkg.Info, "Can User create OAM Components in NameSpace rbactest?  Yes")
+			if allowed, reason := pkg.CanIGroup(v80ProjectAdmin, rbacTestNamespace, "create", "components", "core.oam.dev"); allowed == false {
+				ginkgo.Fail(fmt.Sprintf("FAIL: Did Not Pass Authorization on user create OAM Components: Allowed = %t, reason = %s", allowed, reason))
+			}
+		})
+
+		ginkgo.It("Succeed list OAM Components in namespace rbactest", func() {
+			pkg.Log(pkg.Info, "Can User list OAM Components in NameSpace rbactest?  Yes")
+			if allowed, reason := pkg.CanIGroup(v80ProjectAdmin, rbacTestNamespace, "list", "components", "core.oam.dev"); allowed == false {
+				ginkgo.Fail(fmt.Sprintf("FAIL: Did Not Pass Authorization on user list OAM Components: Allowed = %t, reason = %s", allowed, reason))
+			}
+		})
+
+	})
+})
+
+var _ = ginkgo.Describe("Test RBAC Permission", func() {
+	ginkgo.Context("for user with role verrazzano-project-monitor", func() {
+
+		ginkgo.It("Fail getting Pods in namespace rbactest", func() {
+			pkg.Log(pkg.Info, "Can User List Pods in NameSpace rbactest?  No")
+			if allowed, reason := pkg.CanI(v80ProjectMonitor, rbacTestNamespace, "list", "pods"); allowed != false {
+				ginkgo.Fail(fmt.Sprintf("FAIL: Passed Authorization on user list pods: Allowed = %t, reason = %s", allowed, reason))
+			}
+		})
+
+		ginkgo.It("Create RoleBinding Admin for verrazzano-project-monitor", func() {
+			pkg.Log(pkg.Info, "Create RoleBinding Admin for verrazzano-project-monitor")
+			if err := pkg.CreateRoleBinding(v80ProjectMonitor, rbacTestNamespace, "monitor-binding", "admin"); err != nil {
+				ginkgo.Fail(fmt.Sprintf("FAIL: RoleBinding creation failed: reason = %s", err))
+			}
+		})
+
+		ginkgo.It("Succeed getting Pods in namespace rbactest", func() {
+			pkg.Log(pkg.Info, "Can User List Pods in NameSpace rbactest?  no")
+			if allowed, reason := pkg.CanI(v80ProjectMonitor, rbacTestNamespace, "list", "pods"); allowed == false {
+				ginkgo.Fail(fmt.Sprintf("FAIL: Did Not Pass Authorization on user list pods: Allowed = %t, reason = %s", allowed, reason))
+			}
+		})
+
+		ginkgo.It("Fail getting Pods in namespace verrazzano-system", func() {
+			pkg.Log(pkg.Info, "Can User List Pods in NameSpace verrazzano-system?  No")
+			if allowed, reason := pkg.CanI(v80ProjectMonitor, verrazzanoSystemNS, "list", "pods"); allowed != false {
+				ginkgo.Fail(fmt.Sprintf("FAIL: Passed Authorization on user list pods: Allowed = %t, reason = %s", allowed, reason))
+			}
+		})
+
+		ginkgo.It("Fail create ApplicationConfiguration in namespace rbactest", func() {
+			pkg.Log(pkg.Info, "Can User create ApplicationConfiguration in NameSpace rbactest?  No")
+			if allowed, reason := pkg.CanIGroup(v80ProjectMonitor, rbacTestNamespace, "create", "applicationconfigurations", "core.oam.dev"); allowed != false {
+				ginkgo.Fail(fmt.Sprintf("FAIL: Passed Authorization on user create ApplicationConfiguration: Allowed = %t, reason = %s", allowed, reason))
+			}
+		})
+
+		ginkgo.It("Fail list ApplicationConfiguration in namespace rbactest", func() {
+			pkg.Log(pkg.Info, "Can User list ApplicationConfiguration in NameSpace rbactest?  No")
+			if allowed, reason := pkg.CanIGroup(v80ProjectMonitor, rbacTestNamespace, "list", "applicationconfigurations", "core.oam.dev"); allowed != false {
+				ginkgo.Fail(fmt.Sprintf("FAIL: Passed Authorization on user list ApplicationConfiguration: Allowed = %t, reason = %s", allowed, reason))
+			}
+		})
+
+		ginkgo.It("Fail create OAM Component in namespace rbactest", func() {
+			pkg.Log(pkg.Info, "Can User create OAM Components in NameSpace rbactest?  No")
+			if allowed, reason := pkg.CanIGroup(v80ProjectMonitor, rbacTestNamespace, "create", "components", "core.oam.dev"); allowed != false {
+				ginkgo.Fail(fmt.Sprintf("FAIL: Passed Authorization on user create OAM Components: Allowed = %t, reason = %s", allowed, reason))
+			}
+		})
+
+		ginkgo.It("Fail list OAM Component in namespace rbactest", func() {
+			pkg.Log(pkg.Info, "Can User list OAM Components in NameSpace rbactest?  No")
+			if allowed, reason := pkg.CanIGroup(v80ProjectMonitor, rbacTestNamespace, "list", "components", "core.oam.dev"); allowed != false {
+				ginkgo.Fail(fmt.Sprintf("FAIL: Passed Authorization on user list OAM Components: Allowed = %t, reason = %s", allowed, reason))
+			}
+		})
+
+		ginkgo.It("Create RoleBinding verrazzano-project-monitor-binding for cluster role verrazzano-project-monitor", func() {
+			pkg.Log(pkg.Info, "Create RoleBinding verrazzano-project-monitor-binding for verrazzano-project-monitor")
+			if err := pkg.CreateRoleBinding(v80ProjectMonitor, rbacTestNamespace, "verrazzano-project-monitor-binding", "verrazzano-project-monitor"); err != nil {
+				ginkgo.Fail(fmt.Sprintf("FAIL: RoleBinding creation failed: reason = %s", err))
+			}
+		})
+
+		ginkgo.It("Fail create ApplicationConfiguration in namespace rbactest", func() {
+			pkg.Log(pkg.Info, "Can User create ApplicationConfiguration in NameSpace rbactest?  No")
+			if allowed, reason := pkg.CanIGroup(v80ProjectMonitor, rbacTestNamespace, "create", "applicationconfigurations", "core.oam.dev"); allowed != false {
+				ginkgo.Fail(fmt.Sprintf("FAIL: Passed Authorization on user create ApplicationConfiguration: Allowed = %t, reason = %s", allowed, reason))
+			}
+		})
+
+		ginkgo.It("Succeed list ApplicationConfiguration in namespace rbactest", func() {
+			pkg.Log(pkg.Info, "Can User list ApplicationConfiguration in NameSpace rbactest?  Yes")
+			if allowed, reason := pkg.CanIGroup(v80ProjectMonitor, rbacTestNamespace, "list", "applicationconfigurations", "core.oam.dev"); allowed == false {
+				ginkgo.Fail(fmt.Sprintf("FAIL: Did Not Pass Authorization on user list ApplicationConfiguration: Allowed = %t, reason = %s", allowed, reason))
+			}
+		})
+
+		ginkgo.It("Fail create OAM Components in namespace rbactest", func() {
+			pkg.Log(pkg.Info, "Can User create OAM Components in NameSpace rbactest?  No")
+			if allowed, reason := pkg.CanIGroup(v80ProjectMonitor, rbacTestNamespace, "create", "components", "core.oam.dev"); allowed != false {
+				ginkgo.Fail(fmt.Sprintf("FAIL: Passed Authorization on user create OAM Components: Allowed = %t, reason = %s", allowed, reason))
+			}
+		})
+
+		ginkgo.It("Succeed list OAM Components in namespace rbactest", func() {
+			pkg.Log(pkg.Info, "Can User list OAM Components in NameSpace rbactest?  Yes")
+			if allowed, reason := pkg.CanIGroup(v80ProjectMonitor, rbacTestNamespace, "list", "components", "core.oam.dev"); allowed == false {
+				ginkgo.Fail(fmt.Sprintf("FAIL: Did Not Pass Authorization on user list OAM Components: Allowed = %t, reason = %s", allowed, reason))
+			}
+		})
+
+	})
+})
