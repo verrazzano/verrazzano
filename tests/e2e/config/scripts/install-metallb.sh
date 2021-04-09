7ed96556fc59dac047eea2cb4122f173b6e8c929
tony.vlatas <tony.vlatas@oracle.com>
2021-04-09 12:45:33 -0400

    Update metal-lb to latest which removes the Always image pull policy, it should now default to a more sane IfNotPresent policy

--- tests/e2e/config/scripts/install-metallb.sh
+++ tests/e2e/config/scripts/install-metallb.sh
@@ -8,8 +8,8 @@ set -e
 
 ADDRESS_RANGE=${1:-"172.18.0.230-172.18.0.254"}
 
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/metallb.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 kubectl apply -f - <<-EOF
 apiVersion: v1
