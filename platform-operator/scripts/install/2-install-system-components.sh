49e024374ce7e831d46156f307b4cf8cfe821bc3
Mike Cico <michael.cico@oracle.com>
2021-04-19 18:54:11 -0400

    Use call_curl and do error checks around cert downloads

--- platform-operator/scripts/install/2-install-system-components.sh
+++ platform-operator/scripts/install/2-install-system-components.sh
@@ -305,9 +305,28 @@ function install_rancher()
     if [ "$useAdditionalCAs" == "true" ]; then
       log "Using ACME staging, create staging certs secret for Rancher"
       local acme_staging_certs=${TMP_DIR}/ca-additional.pem
-      curl https://letsencrypt.org/certs/staging/letsencrypt-stg-int-r3.pem > ${acme_staging_certs}
-      curl https://letsencrypt.org/certs/staging/letsencrypt-stg-int-e1.pem >> ${acme_staging_certs}
-      curl https://letsencrypt.org/certs/staging/letsencrypt-stg-root-x1.pem >> ${acme_staging_certs}
+      echo -n "" > ${acme_staging_certs}
+      curl_args=(--output ${TMP_DIR}/int-r3.pem "https://letsencrypt.org/certs/staging/letsencrypt-stg-int-r3.pem")
+      call_curl 200 http_response http_status curl_args || true
+      if [ ${http_status:--1} -ne 200 ]; then
+        log "Error downloading LetsEncrypt Staging intermediate R3 cert"
+      else
+        cat ${TMP_DIR}/int-r3.pem >> ${acme_staging_certs}
+      fi
+      curl_args=(--output ${TMP_DIR}/int-e1.pem "https://letsencrypt.org/certs/staging/letsencrypt-stg-int-e1.pem")
+      call_curl 200 http_response http_status curl_args || true
+      if [ ${http_status:--1} -ne 200 ]; then
+        log "Error downloading LetsEncrypt Staging intermediate E1 cert"
+      else
+        cat ${TMP_DIR}/int-e1.pem >> ${acme_staging_certs}
+      fi
+      curl_args=(--output ${TMP_DIR}/root-x1.pem "https://letsencrypt.org/certs/staging/letsencrypt-stg-root-x1.pem")
+      call_curl 200 http_response http_status curl_args || true
+      if [ ${http_status:--1} -ne 200 ]; then
+        log "Error downloading LetsEncrypt Staging X1 Root cert"
+      else
+        cat ${TMP_DIR}/root-x1.pem >> ${acme_staging_certs}
+      fi
       kubectl -n cattle-system create secret generic tls-ca-additional --from-file=ca-additional.pem=${acme_staging_certs}
     fi
 
