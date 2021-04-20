aef78b30a0f1f3d63041ca9c5bd8fcb5bc04fecc
Guoyong Zhang <guoyong.zhang@oracle.com>
2021-04-20 11:39:46 -0400

    disable multicluster system logging tests

--- tests/e2e/config/scripts/register_managed_cluster.sh
+++ tests/e2e/config/scripts/register_managed_cluster.sh
@@ -119,4 +119,6 @@ if [[ "${ES_HOST}" == *.xip.io ]]; then
   patch_data='{"spec":{"template":{"spec":{"hostAliases":[{"hostnames":["'"${ES_HOST}"'"],"ip":"'"${ES_IP}"'"}]}}}}'
   kubectl --kubeconfig ${MANAGED_KUBECONFIG} -n logging patch daemonset filebeat --patch ${patch_data}
   kubectl --kubeconfig ${MANAGED_KUBECONFIG} -n logging patch daemonset journalbeat --patch ${patch_data}
+  kubectl --kubeconfig ${MANAGED_KUBECONFIG} -n logging get daemonset filebeat -o json | jq .spec.template.spec.hostAliases
+  kubectl --kubeconfig ${MANAGED_KUBECONFIG} -n logging get daemonset journalbeat -o json | jq .spec.template.spec.hostAliases
 fi
