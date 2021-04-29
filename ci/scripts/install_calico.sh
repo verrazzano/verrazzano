379b35677132c8cc19929944e5c42dc90ff861e9
Padmanabha Bhat <padmanabha.bhat@oracle.com>
2021-04-29 21:23:21 +0530

    Change for the directory for calico directory in the well-known location

--- ci/scripts/install_calico.sh
+++ ci/scripts/install_calico.sh
@@ -8,27 +8,34 @@ CALICO_DIR=$(cd $(dirname "$0"); pwd -P)
 CLUSTER_NAME=${1:-"kind"}
 CALICO_VERSION=${2:-"3.18.1"}
 
+download_calico() {
+  mkdir -p ${CALICO_DIR}/calico/${CALICO_VERSION}
+  curl -LJo ${CALICO_DIR}/calico/"${CALICO_VERSION}".tgz https://github.com/projectcalico/calico/releases/download/v"${CALICO_VERSION}"/release-v"${CALICO_VERSION}".tgz
+  cd ${CALICO_DIR}/calico
+  tar xzvf "${CALICO_VERSION}".tgz -C ${CALICO_DIR}/calico/${CALICO_VERSION}
+  # TODO : Remove this ls
+  ls ${CALICO_DIR}/calico/${CALICO_VERSION}
+  rm release-v${CALICO_VERSION}.tgz
+}
+
 # Install Calico using the release bundle under CALICO_HOME. When the environment variable CALICO_HOME is set, the script
-# expects the release-v3.18.1.tgz inside it. When the environment variable is not set, the script downloads the release
-# bundle from the Calico release location.
+# expects the directory CALICO_VERSION inside it. When the environment variable is not set, the script downloads the
+# bundle for version CALICO_VERSION from the Calico release location.
 #
 if [ -z "$CALICO_HOME" ]; then
-  echo "CALICO_HOME is not set, downloading Calico release bundle ..."
-  mkdir -p ${CALICO_DIR}/calico
+  echo "CALICO_HOME is not set, downloading Calico release bundle."
+  download_calico
   export CALICO_HOME=${CALICO_DIR}/calico
-  curl -LJo ${CALICO_DIR}/calico/release-v"${CALICO_VERSION}".tgz https://github.com/projectcalico/calico/releases/download/v"${CALICO_VERSION}"/release-v"${CALICO_VERSION}".tgz
 fi
 
 # Download the release bundle, if $CALICO_HOME doesn't contain the requested/required version
-if [[ ! -f "${CALICO_HOME}/release-v${CALICO_VERSION}.tgz" ]]; then
-  echo "CALICO_HOME doesn't contain the release bundle for version ${CALICO_VERSION}, downloading it ..."
-  curl -LJo ${CALICO_DIR}/calico/release-v"${CALICO_VERSION}".tgz https://github.com/projectcalico/calico/releases/download/v"${CALICO_VERSION}"/release-v"${CALICO_VERSION}".tgz
+if [ -d "${CALICO_HOME}/${CALICO_VERSION}" ]; then
+  echo "CALICO_HOME doesn't contain the release bundle for version ${CALICO_VERSION}, downloading it."
+  download_calico
 fi
 
-echo "Extract Calico release bundle, load the images and apply calico.yaml"
-cd ${CALICO_HOME}
-tar -xzvf release-v${CALICO_VERSION}.tgz --strip-components=1
-cd ${CALICO_HOME}/images
+echo "Load the docker image from Calico archives from ${CALICO_HOME}/${CALICO_VERSION}/images."
+cd ${CALICO_HOME}/${CALICO_VERSION}/images
 kind load image-archive calico-cni.tar --name ${CLUSTER_NAME}
 kind load image-archive calico-dikastes.tar --name ${CLUSTER_NAME}
 kind load image-archive calico-flannel-migration-controller.tar --name ${CLUSTER_NAME}
@@ -36,5 +43,7 @@ kind load image-archive calico-kube-controllers.tar --name ${CLUSTER_NAME}
 kind load image-archive calico-node.tar --name ${CLUSTER_NAME}
 kind load image-archive calico-pod2daemon-flexvol.tar --name ${CLUSTER_NAME}
 kind load image-archive calico-typha.tar --name ${CLUSTER_NAME}
-cd ${CALICO_HOME}/k8s-manifests
+
+echo "Apply ${CALICO_HOME}/${CALICO_VERSION}/k8s-manifests/calico.yaml."
+cd ${CALICO_HOME}/${CALICO_VERSION}/k8s-manifests
 kubectl apply -f calico.yaml
