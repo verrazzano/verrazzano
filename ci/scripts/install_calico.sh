a3d9ff7675a985c866cc5453c4fd0dcbfad4caa7
Padmanabha Bhat <padmanabha.bhat@oracle.com>
2021-04-29 17:44:31 +0530

    Support calico version

--- ci/scripts/install_calico.sh
+++ ci/scripts/install_calico.sh
@@ -1,38 +1,39 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #
 # Copyright (c) 2021, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 #
 
 CALICO_DIR=$(cd $(dirname "$0"); pwd -P)
+CALICO_VERSION=${1:-"3.18.1"}
 
 # Install Calico using the release bundle under CALICO_HOME. When the environment variable CALICO_HOME is set, the script
 # expects the release-v3.18.1.tgz inside it. When the environment variable is not set, the script downloads the release
 # bundle from the Calico release location.
 #
 if [ -z "$CALICO_HOME" ]; then
-  echo "CALICO_HOME is not set, downloading Calico release bundles"
+  echo "CALICO_HOME is not set, downloading Calico release bundle ..."
   mkdir -p ${CALICO_DIR}/calico
   export CALICO_HOME=${CALICO_DIR}/calico
-  curl -LJo ${CALICO_DIR}/calico/release-v3.18.1.tgz https://github.com/projectcalico/calico/releases/download/v3.18.1/release-v3.18.1.tgz
+  curl -LJo ${CALICO_DIR}/calico/release-v"${CALICO_VERSION}".tgz https://github.com/projectcalico/calico/releases/download/v"${CALICO_VERSION}"/release-v"${CALICO_VERSION}".tgz
 fi
 
-# The $CALICO_HOME should have release-v3.18.1.tgz
-if [ -f "${CALICO_HOME}/release-v3.18.1.tgz" ]; then
-  echo "Extract Calico release bundle, load the images and apply calico.yaml"
-  cd ${CALICO_HOME}
-  tar -xzvf release-v3.18.1.tgz --strip-components=1
-  cd ${CALICO_HOME}/images
-  kind load image-archive calico-cni.tar
-  kind load image-archive calico-dikastes.tar
-  kind load image-archive calico-flannel-migration-controller.tar
-  kind load image-archive calico-kube-controllers.tar
-  kind load image-archive calico-node.tar
-  kind load image-archive calico-pod2daemon-flexvol.tar
-  kind load image-archive calico-typha.tar
-  cd ${CALICO_HOME}/k8s-manifests
-  kubectl apply -f calico.yaml
-else
-  echo "File ${CALICO_HOME}/release-v3.18.1.tgz does not exist, Calico installation failed."
-  exit 1
+# Download the release bundle, if $CALICO_HOME doesn't contain the requested/required version
+if [[ ! -f "${CALICO_HOME}/release-v${CALICO_VERSION}.tgz" ]]; then
+  echo "CALICO_HOME doesn't contain the release bundle for version ${CALICO_VERSION}, downloading it ..."
+  curl -LJo ${CALICO_DIR}/calico/release-v"${CALICO_VERSION}".tgz https://github.com/projectcalico/calico/releases/download/v"${CALICO_VERSION}"/release-v"${CALICO_VERSION}".tgz
 fi
+
+echo "Extract Calico release bundle, load the images and apply calico.yaml"
+cd ${CALICO_HOME}
+tar -xzvf release-v${CALICO_VERSION}.tgz --strip-components=1
+cd ${CALICO_HOME}/images
+kind load image-archive calico-cni.tar
+kind load image-archive calico-dikastes.tar
+kind load image-archive calico-flannel-migration-controller.tar
+kind load image-archive calico-kube-controllers.tar
+kind load image-archive calico-node.tar
+kind load image-archive calico-pod2daemon-flexvol.tar
+kind load image-archive calico-typha.tar
+cd ${CALICO_HOME}/k8s-manifests
+kubectl apply -f calico.yaml
--- ci/scripts/prepare_jenkins_at_environment.sh
+++ ci/scripts/prepare_jenkins_at_environment.sh
@@ -17,6 +17,7 @@ fi
 
 INSTALL_CALICO=${1:-false}
 WILDCARD_DNS_DOMAIN=${2:-"x=nip.io"}
+CALICO_VERSION=${3:-"3.18.1"}
 
 cd ${GO_REPO_PATH}/verrazzano
 echo "tests will execute" > ${TESTS_EXECUTED_FILE}
@@ -26,8 +27,9 @@ cd ${TEST_SCRIPTS_DIR}
 
 if [ $INSTALL_CALICO == true ]; then
     echo "Install Calico"
-    cd ${GO_REPO_PATH}/verrazzano
-    ./ci/scripts/install-calico.sh
+    cd ${GO_REPO_PATH}/verrazzano/ci/scripts
+    ls
+    ./install_calico.sh ${CALICO_VERSION}
 fi
 
 echo "Install metallb"
