1962697c3d64d7ea9edbe4c2bcf9c84de758fa80
Paul Mackin <paul.mackin@oracle.com>
2021-05-04 16:04:46 -0400

    skip-at testing upgrade

--- tests/e2e/config/scripts/process_upgrade_yaml.sh
+++ tests/e2e/config/scripts/process_upgrade_yaml.sh
@@ -6,12 +6,5 @@
 # This script is used to add the version: field to the verrazzano custom resource .yaml file
 # It is needed to test upgrade
 VERSION=$1
-CR_IN_FILE=$2
-CR_OUT_FILE=$3
-
-# Create the output file and add the version field, and spec: field if missing
-cp $CR_IN_FILE $CR_OUT_FILE
-if ! grep -q 'spec:' $CR_IN_FILE; then
-  echo 'spec:' >> $CR_OUT_FILE
-fi
-echo ' v' $VERSION >> $CR_OUT_FILE
+CR_FILE=$2
+yq -i eval ".spec.version = \"v${VERSION}\"" ${CR_FILE}
