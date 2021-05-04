81ba47bc7f9e6d8b6d95aa5cd43480c13a395778
Padmanabha Bhat <padmanabha.bhat@oracle.com>
2021-05-04 16:58:56 +0530

    Change LIST_PODS and isNodeReady to contain boolean

--- ci/scripts/wait_for_control_plane.sh
+++ ci/scripts/wait_for_control_plane.sh
@@ -12,27 +12,26 @@ SCRIPT_DIR=$(cd $(dirname "$0"); pwd -P)
 CLUSTER_NAME=${1:-"kind"}
 MAX_WAIT=${2:-300}
 POLLING_INTERVAL=${3:-10}
-LIST_PODS=${4:-"true"}
+LIST_PODS=${4:-true}
 
-SECONDS=0
-isNodeReady="False"
+isNodeReady=false
 
 for ((loopCount=0; loopCount<$MAX_WAIT; loopCount+=$POLLING_INTERVAL)); do
-  isNodeReady="$(kubectl get node "${CLUSTER_NAME}"-control-plane -o 'jsonpath={.status.conditions[?(@.type=="Ready")].status}')"
-  if [[ $isNodeReady == "True" ]]; then
+  if [[ "$(kubectl get node "${CLUSTER_NAME}"-control-plane -o 'jsonpath={.status.conditions[?(@.type=="Ready")].status}')" == 'True' ]]; then
     echo "The node "${CLUSTER_NAME}"-control-plane is ready."
+    isNodeReady=true
     break
   fi
   echo "Wait for "$POLLING_INTERVAL seconds before checking the status of "${CLUSTER_NAME}"-control-plane" ..."
   sleep $POLLING_INTERVAL
 done
 
-if [[ $LIST_PODS == "true" ]]; then
+if [ "$LIST_PODS" = true ] ; then
   echo "Listing pods in kube-system namespace ..."
   kubectl get pods -n kube-system
 fi
 
-if [[ $isNodeReady != "True" ]]; then
+if [[ $isNodeReady != true ]]; then
   echo "The node "${CLUSTER_NAME}"-control-plane is not ready after $MAX_WAIT seconds, exiting ..."
   exit 1
 fi
