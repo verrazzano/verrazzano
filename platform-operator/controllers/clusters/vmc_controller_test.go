ba11a86f9fde724dd04c8478b2d4d69acbec5e88
ddrechse <doug.drechsel@oracle.com>
2021-05-04 10:07:02 -0400

    Fix up Product Name Capitalization

--- platform-operator/controllers/clusters/vmc_controller_test.go
+++ platform-operator/controllers/clusters/vmc_controller_test.go
@@ -1126,7 +1126,7 @@ func expectSyncRegistration(t *testing.T, mock *mocks.MockClient, name string) {
 			return nil
 		})
 
-	// Expect a call to get the Verrazzano Elastic Search/FluentD secret, return the secret with the fields set
+	// Expect a call to get the Verrazzano Elasticsearch/FluentD secret, return the secret with the fields set
 	mock.EXPECT().
 		Get(gomock.Any(), types.NamespacedName{Namespace: constants.VerrazzanoSystemNamespace, Name: constants.VerrazzanoESInternal}, gomock.Not(gomock.Nil())).
 		DoAndReturn(func(ctx context.Context, name types.NamespacedName, secret *corev1.Secret) error {
