c31033365b4c81d122d1a1a9c632e3bea789aa15
Paul Mackin <paul.mackin@oracle.com>
2021-05-05 09:02:44 -0400

    skip-at testing upgrade

--- ci/scripts/prepare_jenkins_at_environment.sh
+++ ci/scripts/prepare_jenkins_at_environment.sh
@@ -10,6 +10,8 @@ set -o pipefail
 
 set -xv
 
+env
+
 if [ -z "$JENKINS_URL" ] || [ -z "$GO_REPO_PATH" ] || [ -z "$TESTS_EXECUTED_FILE" ] || [ -z "$WORKSPACE" ] || [ -z "$VERRAZZANO_OPERATOR_IMAGE" ] || [ -z "$INSTALL_CONFIG_FILE_KIND" ] || [ -z "$OCI_OS_LOCATION" ] || [ -z "$TEST_SCRIPTS_DIR" ]; then
   echo "This script must only be called from Jenkins and requires a number of environment variables are set"
   exit 1
