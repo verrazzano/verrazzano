4af779811b01e52c6b2d5c5e8d8b615c9b0c4299
Tom Beerbower <tom.beerbower@oracle.com>
2021-05-11 16:09:48 -0400

    Automated tests for network security policies

---
+++ tests/e2e/security/network-policies/network_policy_suite_test.go
@@ -0,0 +1,20 @@
+// Copyright (c) 2021, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package network_policy_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/onsi/ginkgo"
+	"github.com/onsi/ginkgo/config"
+	"github.com/onsi/ginkgo/reporters"
+	"github.com/onsi/gomega"
+)
+
+func TestSecurityNetworkPolicies(t *testing.T) {
+	gomega.RegisterFailHandler(ginkgo.Fail)
+	junitReporter := reporters.NewJUnitReporter(fmt.Sprintf("security-network-policy-%d-test-result.xml", config.GinkgoConfig.ParallelNode))
+	ginkgo.RunSpecsWithDefaultAndCustomReporters(t, "Verrazzano Network Policy Suite", []ginkgo.Reporter{junitReporter})
+}
