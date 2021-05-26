a14907408973c528a23e97d673a7557df6361a12
Padmanabha Bhat <padmanabha.bhat@oracle.com>
2021-05-27 00:00:06 +0530

    Update the vmo image

---
+++ tests/e2e/metrics/nginx/nginx_test.go
@@ -0,0 +1,31 @@
+// Copyright (c) 2021, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package nginx_test
+
+import (
+	"time"
+
+	"github.com/onsi/ginkgo"
+	"github.com/onsi/gomega"
+	"github.com/verrazzano/verrazzano/tests/e2e/pkg"
+)
+
+const (
+	longPollingInterval = 20 * time.Second
+	longWaitTimeout     = 10 * time.Minute
+)
+
+var _ = ginkgo.Describe("Verify Verrazzano component metrics", func() {
+	ginkgo.Context("Verify NGINX ingress controller metrics", func() {
+		isManagedClusterProfile := pkg.IsManagedClusterProfile()
+		ginkgo.It("Verify sample NGINX metrics can be queried from Prometheus", func() {
+			if !isManagedClusterProfile {
+				gomega.Eventually(func() bool {
+					return pkg.MetricsExist("nginx_ingress_controller_success", "app_kubernetes_io_instance", "ingress-controller")
+				}, longWaitTimeout, longPollingInterval).Should(gomega.BeTrue())
+			}
+
+		})
+	})
+})
