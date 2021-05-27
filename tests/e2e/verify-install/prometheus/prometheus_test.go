f03de64d19a9f496711da87dd2572b803f0739db
Padmanabha Bhat <padmanabha.bhat@oracle.com>
2021-05-27 21:24:32 +0530

    Exclude all the tests from the managed cluster run

--- tests/e2e/verify-install/prometheus/prometheus_test.go
+++ tests/e2e/verify-install/prometheus/prometheus_test.go
@@ -27,42 +27,41 @@ var _ = ginkgo.Describe("Verify default component metrics", func() {
 				}, longWaitTimeout, longPollingInterval).Should(gomega.BeTrue())
 			})
 		})
+		ginkgo.Context("Verify metrics from Container Advisor", func() {
+			ginkgo.It("Verify sample Container Advisor metrics can be queried from Prometheus", func() {
+				gomega.Eventually(func() bool {
+					return pkg.MetricsExist("container_start_time_seconds", "namespace", "verrazzano-system")
+				}, longWaitTimeout, longPollingInterval).Should(gomega.BeTrue())
+			})
+		})
+
+		ginkgo.Context("Verify metrics from Node Exporter", func() {
+			ginkgo.It("Verify sample Node Exporter metrics can be queried from Prometheus", func() {
+				gomega.Eventually(func() bool {
+					return pkg.MetricsExist("go_gc_duration_seconds", "job", "node-exporter")
+				}, longWaitTimeout, longPollingInterval).Should(gomega.BeTrue())
+			})
+		})
+
+		ginkgo.Context("Verify metrics from Istio mesh and istiod", func() {
+			ginkgo.It("Verify sample mesh metrics can be queried from Prometheus", func() {
+				gomega.Eventually(func() bool {
+					return pkg.MetricsExist("istio_tcp_connections_opened_total", "namespace", "verrazzano-system")
+				}, longWaitTimeout, longPollingInterval).Should(gomega.BeTrue())
+			})
+			ginkgo.It("Verify sample istiod metrics can be queried from Prometheus", func() {
+				gomega.Eventually(func() bool {
+					return pkg.MetricsExist("sidecar_injection_requests_total", "app", "istiod")
+				}, longWaitTimeout, longPollingInterval).Should(gomega.BeTrue())
+			})
+		})
+
+		ginkgo.Context("Verify metrics from Prometheus", func() {
+			ginkgo.It("Verify sample metrics can be queried from Prometheus", func() {
+				gomega.Eventually(func() bool {
+					return pkg.MetricsExist("prometheus_target_interval_length_seconds", "job", "prometheus")
+				}, longWaitTimeout, longPollingInterval).Should(gomega.BeTrue())
+			})
+		})
 	}
-
-	ginkgo.Context("Verify metrics from Container Advisor", func() {
-		ginkgo.It("Verify sample Container Advisor metrics can be queried from Prometheus", func() {
-			gomega.Eventually(func() bool {
-				return pkg.MetricsExist("container_start_time_seconds", "namespace", "verrazzano-system")
-			}, longWaitTimeout, longPollingInterval).Should(gomega.BeTrue())
-		})
-	})
-
-	ginkgo.Context("Verify metrics from Node Exporter", func() {
-		ginkgo.It("Verify sample Node Exporter metrics can be queried from Prometheus", func() {
-			gomega.Eventually(func() bool {
-				return pkg.MetricsExist("go_gc_duration_seconds", "job", "node-exporter")
-			}, longWaitTimeout, longPollingInterval).Should(gomega.BeTrue())
-		})
-	})
-
-	ginkgo.Context("Verify metrics from Istio mesh and istiod", func() {
-		ginkgo.It("Verify sample mesh metrics can be queried from Prometheus", func() {
-			gomega.Eventually(func() bool {
-				return pkg.MetricsExist("istio_tcp_connections_opened_total", "namespace", "verrazzano-system")
-			}, longWaitTimeout, longPollingInterval).Should(gomega.BeTrue())
-		})
-		ginkgo.It("Verify sample istiod metrics can be queried from Prometheus", func() {
-			gomega.Eventually(func() bool {
-				return pkg.MetricsExist("sidecar_injection_requests_total", "app", "istiod")
-			}, longWaitTimeout, longPollingInterval).Should(gomega.BeTrue())
-		})
-	})
-
-	ginkgo.Context("Verify metrics from Prometheus", func() {
-		ginkgo.It("Verify sample metrics can be queried from Prometheus", func() {
-			gomega.Eventually(func() bool {
-				return pkg.MetricsExist("prometheus_target_interval_length_seconds", "job", "prometheus")
-			}, longWaitTimeout, longPollingInterval).Should(gomega.BeTrue())
-		})
-	})
 })
