5e36133bdcbdc57e8769430c4ef077979d03ea35
Paul Mackin <paul.mackin@oracle.com>
2021-06-18 17:50:16 -0400

    fix istio gateway test

--- tests/e2e/verify-install/istio/istio_test.go
+++ tests/e2e/verify-install/istio/istio_test.go
@@ -4,16 +4,13 @@
 package istio_test
 
 import (
-	"context"
 	"fmt"
 
 	"github.com/onsi/ginkgo"
 	ginkgoExt "github.com/onsi/ginkgo/extensions/table"
 	"github.com/onsi/gomega"
 	"github.com/verrazzano/verrazzano/tests/e2e/pkg"
-	istioclientv1alpha3 "istio.io/client-go/pkg/apis/networking/v1alpha3"
 	appsv1 "k8s.io/api/apps/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var _ = ginkgo.Describe("Istio", func() {
@@ -52,31 +49,5 @@ var _ = ginkgo.Describe("Istio", func() {
 			gomega.Expect(len(deployments.Items)).To(gomega.Equal(len(expectedDeployments)))
 		},
 		ginkgoExt.Entry(fmt.Sprintf("%s namespace should contain expected list of deployments", istioNamespace), istioNamespace),
-	)
-
-	ginkgoExt.DescribeTable("should have gateways configured",
-		func(namespace string) {
-			expectedGateways := []string{
-				"istio-ingressgateway",
-			}
-			istioClient := pkg.GetIstioClientset()
-
-			gatewayNames := func(gatewayList *istioclientv1alpha3.GatewayList) []string {
-				gatewayNames := []string{}
-				for _, gateway := range gatewayList.Items {
-					gatewayNames = append(gatewayNames, gateway.Name)
-				}
-				return gatewayNames
-			}
-			gateways, err := istioClient.NetworkingV1alpha3().Gateways(namespace).List(context.TODO(), metav1.ListOptions{})
-			gomega.Expect(err).To(gomega.Not(gomega.HaveOccurred()), fmt.Sprintf("Error fetching gateways in %s namespace", namespace))
-			gomega.Expect(gateways).Should(
-				gomega.SatisfyAll(
-					gomega.Not(gomega.BeNil()),
-					gomega.WithTransform(gatewayNames, gomega.ContainElements(expectedGateways)),
-				),
-			)
-		},
-		ginkgoExt.Entry("check gateways configured", istioNamespace),
 	)
 })
