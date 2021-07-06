3be2cc1543075c139df2deb8ad9b47f51643c341
John Speidel <john.p.speidel@oracle.com>
2021-07-06 08:53:07 -0400

    During uninstall, only delete application resources from namespaces which are managed by Verrazzano (#1325)
    * Dont delete coherence and weblogic CRs
    
    * Changed uninstall to only delete applications from Verrazzano managed namespaces
    
    * Don't delete application crd's
    
    * Don't include crd's in cluster dump for uninstall test
    
    * Minor doc change
    
    * Fix call to dump_cluster for all uninstall tests to not dump crd's

--- tests/e2e/config/scripts/looping-test/dump_cluster.sh
+++ tests/e2e/config/scripts/looping-test/dump_cluster.sh
@@ -4,10 +4,15 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 SCRIPT_DIR=$(cd $(dirname "$0"); pwd -P)
+INCLUDE_CRDS=true
 
 if [ -z "$1" ] ; then
   echo "Please provide directory to place resource dump"
   exit 1
+fi
+
+if [ -n "$2" ] ; then
+  INCLUDE_CRDS=$2
 fi
 
 mkdir -p "$1"
@@ -16,13 +21,13 @@ touch default.txt kube-node-lease.txt kube-public.txt kube-system.txt
 cd "$SCRIPT_DIR"
 
 echo "retrieving default resources"
-"${SCRIPT_DIR}"/get_resources.sh default > "$1"/default.txt
+"${SCRIPT_DIR}"/get_resources.sh default "${INCLUDE_CRDS}" > "$1"/default.txt
 
 echo "retrieving kube-node-lease resources"
-"${SCRIPT_DIR}"/get_resources.sh kube-node-lease > "$1"/kube-node-lease.txt
+"${SCRIPT_DIR}"/get_resources.sh kube-node-lease "${INCLUDE_CRDS}" > "$1"/kube-node-lease.txt
 
 echo "retrieving kube-public resources"
-"${SCRIPT_DIR}"/get_resources.sh kube-public > "$1"/kube-public.txt
+"${SCRIPT_DIR}"/get_resources.sh kube-public "${INCLUDE_CRDS}" > "$1"/kube-public.txt
 
 echo "retrieving kube-system resources"
-"${SCRIPT_DIR}"/get_resources.sh kube-system > "$1"/kube-system.txt
+"${SCRIPT_DIR}"/get_resources.sh kube-system "${INCLUDE_CRDS}" > "$1"/kube-system.txt
