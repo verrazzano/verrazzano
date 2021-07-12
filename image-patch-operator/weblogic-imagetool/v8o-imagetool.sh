cf4dd1c864a5f257a54ec6f2484f708751e9ac84
Marco Gellecano <marco.gellecanao@oracle.com>
2021-07-12 13:15:41 -0700

    added wrapper to imagetool script

---
+++ image-patch-operator/weblogic-imagetool/v8o-imagetool.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Copyright (C) 2021, Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+printf "\n=====v8o-imagetool.sh has been called!=====\n\n"
+./imagetool/bin/imagetool.sh "$@"
+
