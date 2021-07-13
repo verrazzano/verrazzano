a1b71aa366a59a61e7fe00fc88afe4a7852def1a
TheBullseye23 <karan.kumar.sekar@oracle.com>
2021-07-13 17:54:50 +0000

    Created the namespace delete.go file.

---
+++ tools/cli/vz/cmd/namespace/delete.go
@@ -0,0 +1,39 @@
+package namespace
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/verrazzano/verrazzano/tools/cli/vz/pkg/helpers"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+)
+
+type NamespaceDeleteOptions struct {
+	args []string
+	genericclioptions.IOStreams
+}
+
+func NewNamespaceDeleteOptions(streams genericclioptions.IOStreams) *NamespaceDeleteOptions {
+	return &NamespaceDeleteOptions{
+		IOStreams: streams,
+	}
+}
+
+func NewNamespaceDelete(streams genericclioptions.IOStreams, kubernetesInterface helpers.Kubernetes) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "delete",
+		Short: "delete namespace",
+		Long:  "delete namespace",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if err := deleteNamespace(streams, args, kubernetesInterface); err != nil {
+				return err
+			}
+			return nil
+		},
+	}
+	return cmd
+}
+
+func deleteNamespace(streams genericclioptions.IOStreams, args []string, kubernetesInterface helpers.Kubernetes) error {
+	// business logic here
+	return nil
+}
