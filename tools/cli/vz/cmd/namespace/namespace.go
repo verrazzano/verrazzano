6c2c91685f3bed7bae502bbea56d707f755e1b91
TheBullseye23 <karan.kumar.sekar@oracle.com>
2021-07-13 16:46:54 +0000

    Created the namespace.go root file.

---
+++ tools/cli/vz/cmd/namespace/namespace.go
@@ -0,0 +1,31 @@
+package namespace
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/verrazzano/verrazzano/tools/cli/vz/pkg/helpers"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+)
+
+type NamespaceOptions struct {
+	args []string
+	genericclioptions.IOStreams
+}
+
+func NewNamespaceOptions(streams genericclioptions.IOStreams) *NamespaceOptions {
+	return &NamespaceOptions{
+		IOStreams: streams,
+	}
+}
+
+func NewCmdNamespace(streams genericclioptions.IOStreams, kubernetesInterface helpers.Kubernetes) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "namespace",
+		Short: "Work with namespaces",
+		Long:  "Work with namespaces",
+		// TODO : Description needs to be rewritten
+	}
+	//cmd.AddCommand(NewCmdNamespaceAdd(streams,kubernetesInterface))
+	//cmd.AddCommand(NewCmdNamespaceList(streams,kubernetesInterface))
+	//cmd.AddCommand(NewCmdNamespaceMove(streams,kubernetesInterface))
+	return cmd
+}
