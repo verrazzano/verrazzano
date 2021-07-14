ec24293f2bde2ece9ab3d02285fe4a99788ed1f1
Marco Gellecano <marco.gellecanao@oracle.com>
2021-07-14 14:43:47 -0700

    moved addInstallers to wrapper script; added tag before push

--- image-patch-operator/weblogic-imagetool/v8o-imagetool.sh
+++ image-patch-operator/weblogic-imagetool/v8o-imagetool.sh
@@ -5,10 +5,15 @@
 
 printf "\n=====v8o-imagetool.sh has been called!=====\n\n"
 
-# TODO: move imagetool cache addInstaller here
-
-cat /registry-cred/password | podman login $(cat /registry-creds/registry) --username $(cat /registry-cred/username) --password-stdin
+export WLSIMG_CACHEDIR="/home/verrazzano/cache"
+./imagetool/bin/imagetool.sh cache addInstaller --type wls --version 12.2.1.4.0 --path ./${WLS_BINARY}
+./imagetool/bin/imagetool.sh cache addInstaller --type jdk --version 8u281 --path ./${JDK_BINARY}
+./imagetool/bin/imagetool.sh cache addInstaller --type wdt --version latest --path ./${WDT_BINARY}
+chmod +x ./v8o-imagetool.sh
 
 ./imagetool/bin/imagetool.sh create --tag ${IMAGE_NAME}:${IMAGE_TAG} --builder podman --jdkVersion ${JDK_INSTALLER_VERSION} --version ${WEBLOGIC_INSTALLER_VERSION}
 
+cat /registry-creds/password | podman login $(cat /registry-creds/registry) --username $(cat /registry-creds/username) --password-stdin
+podman tag ${IMAGE_NAME}:${IMAGE_TAG} ${IMAGE_REGISTRY}/${IMAGE_REPOSITORY}/${IMAGE_NAME}:${IMAGE_TAG}
 podman image push ${IMAGE_REGISTRY}/${IMAGE_REPOSITORY}/${IMAGE_NAME}:${IMAGE_TAG}
+
