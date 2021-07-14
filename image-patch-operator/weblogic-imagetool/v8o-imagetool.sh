a10bec501d83b1153e363f3f6d3f4095f214b36d
Marco Gellecano <marco.gellecanao@oracle.com>
2021-07-14 12:34:12 -0700

    added tentative podman login and push

--- image-patch-operator/weblogic-imagetool/v8o-imagetool.sh
+++ image-patch-operator/weblogic-imagetool/v8o-imagetool.sh
@@ -4,5 +4,11 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 printf "\n=====v8o-imagetool.sh has been called!=====\n\n"
-./imagetool/bin/imagetool.sh "$@"
 
+# TODO: move imagetool cache addInstaller here
+
+cat /registry-cred/password | podman login $(cat /registry-creds/registry) --username $(cat /registry-cred/username) --password-stdin
+
+./imagetool/bin/imagetool.sh create --tag ${IMAGE_NAME}:${IMAGE_TAG} --builder podman --jdkVersion ${JDK_INSTALLER_VERSION} --version ${WEBLOGIC_INSTALLER_VERSION}
+
+podman image push ${IMAGE_REGISTRY}/${IMAGE_REPOSITORY}/${IMAGE_NAME}:${IMAGE_TAG}
