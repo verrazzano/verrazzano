3a37534f80e2d4655847a0d1073232d0553599b0
Marco Gellecano <marco.gellecanao@oracle.com>
2021-07-14 14:51:49 -0700

    removed chmod in wrapper script; fixed podman tag command

--- image-patch-operator/weblogic-imagetool/v8o-imagetool.sh
+++ image-patch-operator/weblogic-imagetool/v8o-imagetool.sh
@@ -9,11 +9,10 @@ export WLSIMG_CACHEDIR="/home/verrazzano/cache"
 ./imagetool/bin/imagetool.sh cache addInstaller --type wls --version 12.2.1.4.0 --path ./${WLS_BINARY}
 ./imagetool/bin/imagetool.sh cache addInstaller --type jdk --version 8u281 --path ./${JDK_BINARY}
 ./imagetool/bin/imagetool.sh cache addInstaller --type wdt --version latest --path ./${WDT_BINARY}
-chmod +x ./v8o-imagetool.sh
 
 ./imagetool/bin/imagetool.sh create --tag ${IMAGE_NAME}:${IMAGE_TAG} --builder podman --jdkVersion ${JDK_INSTALLER_VERSION} --version ${WEBLOGIC_INSTALLER_VERSION}
 
 cat /registry-creds/password | podman login $(cat /registry-creds/registry) --username $(cat /registry-creds/username) --password-stdin
-podman tag ${IMAGE_NAME}:${IMAGE_TAG} ${IMAGE_REGISTRY}/${IMAGE_REPOSITORY}/${IMAGE_NAME}:${IMAGE_TAG}
+podman tag ${IMAGE_NAME}:${IMAGE_TAG} ${IMAGE_REGISTRY}/${IMAGE_REPOSITORY}:{IMAGE_TAG}
 podman image push ${IMAGE_REGISTRY}/${IMAGE_REPOSITORY}/${IMAGE_NAME}:${IMAGE_TAG}
 
