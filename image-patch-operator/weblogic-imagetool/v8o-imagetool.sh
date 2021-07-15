3fa7b000fa4665b1c1255d5aa4b385bdc592a511
Marco Gellecano <marco.gellecanao@oracle.com>
2021-07-15 12:52:04 -0700

    removed debugging prints

--- image-patch-operator/weblogic-imagetool/v8o-imagetool.sh
+++ image-patch-operator/weblogic-imagetool/v8o-imagetool.sh
@@ -3,17 +3,6 @@
 # Copyright (C) 2021, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-printf "\n=====v8o-imagetool.sh has been called!=====\n\n"
-
-echo "Environment variables:"
-echo $IMAGE_REGISTRY
-echo $IMAGE_REPOSITORY
-echo $IMAGE_TENANCY
-echo $IMAGE_NAME
-echo $IMAGE_TAG
-echo ${IMAGE_REGISTRY}/${IMAGE_TENANCY}/${IMAGE_REPOSITORY}:${IMAGE_TAG}
-echo ""
-
 export WLSIMG_CACHEDIR="/home/verrazzano/cache"
 ./imagetool/bin/imagetool.sh cache addInstaller --type wls --version 12.2.1.4.0 --path ./installers/${WEBLOGIC_INSTALLER_BINARY}
 ./imagetool/bin/imagetool.sh cache addInstaller --type jdk --version 8u281 --path ./installers/${JDK_INSTALLER_BINARY}
@@ -23,6 +12,5 @@ export WLSIMG_CACHEDIR="/home/verrazzano/cache"
 
 cat /registry-creds/password | podman login $(cat /registry-creds/registry) --username $(cat /registry-creds/username) --password-stdin
 podman tag ${IMAGE_NAME}:${IMAGE_TAG} ${IMAGE_REGISTRY}/${IMAGE_TENANCY}/${IMAGE_REPOSITORY}:${IMAGE_TAG}
-podman images # FIXME: remove this later
 podman image push ${IMAGE_REGISTRY}/${IMAGE_TENANCY}/${IMAGE_REPOSITORY}:${IMAGE_TAG}
 
