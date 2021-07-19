a1ed9f3ea45d024abc67b4b40c8b79e0b0aacb22
TheBullseye23 <karan.kumar.sekar@oracle.com>
2021-07-19 08:36:10 +0000

    Implemented namespace add, a few minor changes and bug fix for creation timestamp in project add.go

--- tools/cli/vz/cmd/namespace/create.go
+++ tools/cli/vz/cmd/namespace/create.go
@@ -8,10 +8,11 @@ import (
 	"github.com/verrazzano/verrazzano/tools/cli/vz/pkg/helpers"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 )
 
-var projectID = []string{""}
+var projectID []string
 var description []string
 
 type NamespaceCreateOptions struct {
@@ -39,7 +40,7 @@ func NewCmdNamespaceCreate(streams genericclioptions.IOStreams, kubernetesInterf
 		},
 	}
 	cmd.Flags().StringSliceVar(&description, "description", []string{}, "Description about the namespace")
-	cmd.Flags().StringSliceVarP(&projectID, "project-id", "p", []string{}, "ID of project this namespace belongs to")
+	cmd.Flags().StringSliceVarP(&projectID, "project-id", "p", []string{""}, "ID of project this namespace belongs to")
 	// TODO : Throw a more graceful error when project flag is not specified - give a default project?
 	return cmd
 }
@@ -47,57 +48,59 @@ func NewCmdNamespaceCreate(streams genericclioptions.IOStreams, kubernetesInterf
 func createNamespace(streams genericclioptions.IOStreams, args []string, kubernetesInterface helpers.Kubernetes) error {
 	nsName := args[0]
 
-	//preparing namespace resources
-	// TODO : Isolate error in project about createTimeStamp, or pass it here.
-	
-	// old namespace
-	oldNamespace := v1alpha1.NamespaceTemplate{
-		Metadata: metav1.ObjectMeta{
-			Name: nsName,
-		},
-	}
-	
+	// adding label to register namespace as verrazzano namespace
+	vzLabel := make(map[string]string)
+	vzLabel["verrazzano-managed"] = "true"
+
+	// preparing the namespace object
 	newNamespace := v1.Namespace{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: nsName,
+			Name:              nsName,
+			CreationTimestamp: metav1.Now(),
+			Labels:            vzLabel,
 		},
 	}
+
 	// fetching the clientsets
-	projectClientset, err := kubernetesInterface.NewProjectClientSet()
-	if err!=nil{
-		fmt.Fprintln(streams.ErrOut,err)
-	}
-	clustersClientset:= kubernetesInterface.NewClientSet()
-	newns, err := clustersClientset.CoreV1().Namespaces().Create(context.Background(),&newNamespace,metav1.CreateOptions{})
-	
-	// fetching the project
-	project, err := projectClientset.ClustersV1alpha1().VerrazzanoProjects("verrazzano-mc").Get(context.Background(), projectID[0], metav1.GetOptions{})
-	//project, err := projectClientset.ClustersV1alpha1().VerrazzanoProjects("verrazzano-mc").Update(context.Background(),projectID[0],metav1.UpdateOptions{})
+	clustersClientset := kubernetesInterface.NewClientSet()
+	_, err := clustersClientset.CoreV1().Namespaces().Create(context.Background(), &newNamespace, metav1.CreateOptions{})
 	if err != nil {
+		fmt.Fprintln(streams.ErrOut, err)
 		return err
 	}
 
-	project.Spec.Template.Namespaces = append(project.Spec.Template.Namespaces,newNamespace.Spec.)
-
-	// appending the namespace to the project
-	var isDuplicate bool
-	// TODO : Parse the project's namespaces to check if it already exists. Look how to do it.
-	for _, template := range project.Spec.Template.Namespaces {
-		if template.Metadata.Name == projectID[0] {
-			isDuplicate = true
-			fmt.Fprintln(streams.Out, "duplicate namespace")
-		}
-	}
-
-	// if the project doesn't already contain the namespace
-	if !isDuplicate {
-		// adding the namespace to the project
-		project.Spec.Template.Namespaces = append(project.Spec.Template.Namespaces, oldNamespace)
-
-		// updating the project
-		if _, err := projectClientset.ClustersV1alpha1().VerrazzanoProjects("verrazzano-mc").Update(context.Background(), project, metav1.UpdateOptions{}); err != nil {
+	// if project flag is passed
+	if projectID[0] != "" {
+		isDuplicate := false
+		projectClientset, err := kubernetesInterface.NewProjectClientSet()
+		project, err := projectClientset.ClustersV1alpha1().VerrazzanoProjects("verrazzano-mc").Get(context.Background(), projectID[0], metav1.GetOptions{})
+		if err != nil {
+			fmt.Fprintln(streams.ErrOut, err)
 			return err
 		}
+
+		// checking if project already contains the namespace.
+		for _, projectNamespaces := range project.Spec.Template.Namespaces {
+			if projectNamespaces.Metadata.Name == projectID[0] {
+				isDuplicate = true
+			}
+		}
+		if !isDuplicate {
+			// adding the namespace to the project
+			newNsTemplate := v1alpha1.NamespaceTemplate{
+				Metadata: metav1.ObjectMeta{
+					Name:              newNamespace.GetName(),
+					CreationTimestamp: newNamespace.GetCreationTimestamp(),
+				},
+			}
+			project.Spec.Template.Namespaces = append(project.Spec.Template.Namespaces, newNsTemplate)
+			// updating the project
+			if _, err := projectClientset.ClustersV1alpha1().VerrazzanoProjects("verrazzano-mc").Update(context.Background(), project, metav1.UpdateOptions{}); err != nil {
+				fmt.Fprintln(streams.ErrOut, err)
+				return err
+			}
+		}
 	}
+	fmt.Fprintln(streams.Out, "namespace/"+nsName+" created")
 	return nil
 }
