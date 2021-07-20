0e3189ec065c84125e002f63bd5106772d121c6e
Marco Gellecano <marco.gellecanao@oracle.com>
2021-07-20 11:13:12 -0700

    added conditionals to stop script given failures

--- image-patch-operator/weblogic-imagetool/v8o-imagetool.sh
+++ image-patch-operator/weblogic-imagetool/v8o-imagetool.sh
@@ -9,16 +9,35 @@ if ! (cat /registry-creds/password | podman login $(cat /registry-creds/registry
   exit 1
 fi
 
-# Add installers to the imagetool cache
+# Add installers to the Image Tool cache
 export WLSIMG_CACHEDIR="/home/verrazzano/cache"
-./imagetool/bin/imagetool.sh cache addInstaller --type wls --version ${WEBLOGIC_INSTALLER_VERSION} --path ./installers/${WEBLOGIC_INSTALLER_BINARY}
-./imagetool/bin/imagetool.sh cache addInstaller --type jdk --version ${JDK_INSTALLER_VERSION} --path ./installers/${JDK_INSTALLER_BINARY}
-./imagetool/bin/imagetool.sh cache addInstaller --type wdt --version ${WDT_INSTALLER_VERSION} --path ./installers/${WDT_INSTALLER_BINARY}
+if ! ./imagetool/bin/imagetool.sh cache addInstaller --type wls --version ${WEBLOGIC_INSTALLER_VERSION} --path ./installers/${WEBLOGIC_INSTALLER_BINARY}; then
+  echo "Adding WebLogic installer to Image Tool cache failed."
+  exit 1
+fi
+if ! ./imagetool/bin/imagetool.sh cache addInstaller --type jdk --version ${JDK_INSTALLER_VERSION} --path ./installers/${JDK_INSTALLER_BINARY}; then
+  echo "Adding JDK installer to Image Tool cache failed."
+  exit 1
+fi
+if ! ./imagetool/bin/imagetool.sh cache addInstaller --type wdt --version ${WDT_INSTALLER_VERSION} --path ./installers/${WDT_INSTALLER_BINARY}; then
+  echo "Adding WebLogic Deploy Tooling installer to Image Tool cache failed."
+  exit 1
+fi
 
 # Create the image
-./imagetool/bin/imagetool.sh create --tag ${IMAGE_NAME}:${IMAGE_TAG} --builder podman --jdkVersion ${JDK_INSTALLER_VERSION} --version ${WEBLOGIC_INSTALLER_VERSION}
+if ! ./imagetool/bin/imagetool.sh create --tag ${IMAGE_NAME}:${IMAGE_TAG} --builder podman --jdkVersion ${JDK_INSTALLER_VERSION} --version ${WEBLOGIC_INSTALLER_VERSION}; then
+  echo "Creating the image using WebLogic Image Tool failed."
+  exit 1
+fi
 
 # Tag and push the image to the registry
-podman tag ${IMAGE_NAME}:${IMAGE_TAG} ${IMAGE_REGISTRY}/${IMAGE_REPOSITORY}/${IMAGE_NAME}:${IMAGE_TAG}
-podman image push ${IMAGE_REGISTRY}/${IMAGE_REPOSITORY}/${IMAGE_NAME}:${IMAGE_TAG}
+if ! podman tag ${IMAGE_NAME}:${IMAGE_TAG} ${IMAGE_REGISTRY}/${IMAGE_REPOSITORY}/${IMAGE_NAME}:${IMAGE_TAG}; then
+  echo "Tagging image failed."
+  exit 1
+fi
+if ! podman image push ${IMAGE_REGISTRY}/${IMAGE_REPOSITORY}/${IMAGE_NAME}:${IMAGE_TAG}; then
+  echo "Pushing image failed."
+  exit 1
+fi
 
+echo "Successfully pushed image!"
