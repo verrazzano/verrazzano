d1a72a9c449fcd5852cbd6397ebbce6e5534cd61
TheBullseye23 <karan.kumar.sekar@oracle.com>
2021-07-23 11:26:53 +0000

    Added a argument checking unit test for create namespace go file.

---
+++ tools/cli/vz/cmd/namespace/delete_test.go
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
+
+package namespace
+
+// unit test for checking arguments
+// unit test for deleting non-existent namespaces
+// unit test for ensuring namespace is deleted i.e by calling namespace command repeatedly.
