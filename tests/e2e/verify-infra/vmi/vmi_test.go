ce76871a56d0f5a83b18ac7a630559519e98f980
tony.vlatas <tony.vlatas@oracle.com>
2021-08-31 14:14:04 -0400

    add unstable test

--- tests/e2e/verify-infra/vmi/vmi_test.go
+++ tests/e2e/verify-infra/vmi/vmi_test.go
@@ -1,6 +1,8 @@
 // Copyright (c) 2020, 2021, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
+// +build unstable_test
+
 package vmi_test
 
 import (
