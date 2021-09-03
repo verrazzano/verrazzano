d0089ddf06baad8876afcbd3fcf3cbb409761435
Mike Cico <michael.cico@oracle.com>
2021-09-03 17:00:32 -0400

    VZ-3241.  Move WKO install to platform operator.

--- platform-operator/scripts/install/3-install-verrazzano.sh
+++ platform-operator/scripts/install/3-install-verrazzano.sh
@@ -313,6 +313,6 @@ fi
 
 action "Installing Verrazzano system components" install_verrazzano || exit 1
 action "Installing Coherence Kubernetes operator" install_coherence_operator || exit 1
-action "Installing WebLogic Kubernetes operator" install_weblogic_operator || exit 1
+platform_operator_install_message "WebLogic Kubernetes operator"
 platform_operator_install_message "OAM Kubernetes operator"
 platform_operator_install_message "Verrazzano Application Kubernetes operator"
