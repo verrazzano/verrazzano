15323d4f6b54a168dc9a536cb0fcdfcd132d7de5
Michael Cico <46466506+michael-cico@users.noreply.github.com>
2021-09-03 08:21:19 -0400

    VZ-3384. Move install of OAM/App operators VPO (#1471)
    - Add component level status reporting to CR status field
    - Gate overall install status on combination of install job status and individual component status'
    - Changes to Component, component registry, HelmComponent, and Helm CLI code to accommodate installs
    - still need to update unit tests
    - Copy global image pull secret to target namespaces as needed

---
+++ platform-operator/controllers/verrazzano/install.go
@@ -0,0 +1,44 @@
+// Copyright (c) 2020, 2021, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package verrazzano
+
+import (
+	installv1alpha1 "github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1alpha1"
+
+	"go.uber.org/zap"
+	ctrl "sigs.k8s.io/controller-runtime"
+
+	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component"
+)
+
+// Reconcile upgrade will upgrade the components as required
+func (r *Reconciler) reconcileInstall(log *zap.SugaredLogger, _ ctrl.Request, cr *installv1alpha1.Verrazzano) error {
+
+	// Loop through all of the Verrazzano components and upgrade each one sequentially for now; will parallelize later
+	for _, comp := range component.GetComponents() {
+		if !comp.IsOperatorInstallSupported() {
+			continue
+		}
+		// If component is enabled -- need to replicate scripts' config merging logic here
+		// If component is in deployed state, continue
+		if comp.IsInstalled() {
+			log.Infof("Component %s already installed")
+			if err := r.updateComponentStatus(log, cr, comp.Name(), "Update ready status", installv1alpha1.InstallComplete); err != nil {
+				return err
+			}
+			continue
+		}
+		if err := r.updateComponentStatus(log, cr, comp.Name(), "Install starting", installv1alpha1.InstallStarted); err != nil {
+			return err
+		}
+		// If component is not installed,install it
+		if err := comp.Install(log, r, cr.Namespace, r.DryRun); err != nil {
+			return err
+		}
+		if err := r.updateComponentStatus(log, cr, comp.Name(), "Install complete", installv1alpha1.InstallComplete); err != nil {
+			return err
+		}
+	}
+	return nil
+}
