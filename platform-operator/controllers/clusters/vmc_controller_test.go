d90ba58239c355d6dd9275b53e42871e484cc6c1
Kevin Minder <kevin.minder@oracle.com>
2021-09-09 15:59:35 -0400

    VZ-3256: Fix default elastic search URL

--- platform-operator/controllers/clusters/vmc_controller_test.go
+++ platform-operator/controllers/clusters/vmc_controller_test.go
@@ -1265,7 +1265,7 @@ func expectSyncRegistration(t *testing.T, mock *mocks.MockClient, name string, e
 	const externalPasswordData = "external-pw"
 	const externalCaData = "external-ca"
 
-	fluentdESURL := "http://vmi-system-es-ingest-oidc:8775"
+	fluentdESURL := "http://verrazzano-authproxy-elasticsearch:9200"
 	fluentdESSecret := "verrazzano"
 	esSecret := constants.VerrazzanoESInternal
 	if externalES {
