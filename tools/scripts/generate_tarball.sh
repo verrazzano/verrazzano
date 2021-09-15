364f235bd9d4c3bf5d37831a63c518b4969ccc27
Mark Nelson <mark.x.nelson@oracle.com>
2021-09-14 22:17:21 -0400

    Revert "VZ-3462 BOM to image list (#1565)"
    This reverts commit c99262a38100f6c0280800948d5fe5a6ddbcf84b.

--- tools/scripts/generate_tarball.sh
+++ tools/scripts/generate_tarball.sh
@@ -26,7 +26,50 @@ if [ -d "$3" ]; then
   exit 1
 fi
 
-source $SCRIPT_DIR/bom_utils.sh
+# Get the global Docker registry specified in the BOM
+function get_registry() {
+  cat ${BOM_FILE} | jq -r '.registry'
+}
+
+# Get the list of component names in the BOM
+function list_components() {
+  cat ${BOM_FILE} | jq -r '.components[].name'
+}
+
+# Get the repository name for a component in the BOM
+function get_component_repo() {
+  local compName=$1
+  cat ${BOM_FILE} | jq -r --arg comp ${compName} '.components[] | select(.name==$comp) | .repository'
+}
+
+# Get the subcomponent registry
+function get_subcomponent_registry() {
+  local compName=$1
+  local subCompName=$2
+  cat ${BOM_FILE} | jq -r --arg comp ${compName} --arg subcomp ${subCompName} '.components[] | select(.name==$comp) | .subcomponents[] | select(.name==$subcomp) | .registry'
+}
+
+# Get the repository name for a subcomponent in the BOM
+function get_subcomponent_repo() {
+  local compName=$1
+  local subCompName=$2
+  cat ${BOM_FILE} | jq -r --arg comp ${compName} --arg subcomp ${subCompName} '.components[] | select(.name==$comp) | .subcomponents[] | select(.name==$subcomp) | .repository'
+}
+
+# List the subcomponents names within a component in the BOM
+function list_subcomponent_names() {
+  local compName=$1
+  cat ${BOM_FILE} | jq -r --arg comp ${compName} \
+    '.components[] | select(.name==$comp) | .subcomponents[] | .name '
+}
+
+# List the base image names for all subcomponents of a component in the BOM, in the form <image-name>:<tag>
+function list_subcomponent_images() {
+  local compName=$1
+  local subCompName=$2
+  cat ${BOM_FILE} | jq -r --arg comp ${compName} --arg subcomp ${subCompName} \
+    '.components[] | select(.name==$comp) | .subcomponents[] | select(.name==$subcomp) | .images[] | "\(.image):\(.tag)"'
+}
 
 # Main driver for pulling/saving images based on the Verrazzano bill of materials (BOM)
 function pull_and_save_images() {
