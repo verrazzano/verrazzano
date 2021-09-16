20c14a2348aa3b32c39e05dd29651ec0773bba14
Kevin Minder <kevin.minder@oracle.com>
2021-09-16 10:11:50 -0400

    VZ-3529: Patch Prometheus deployment during upgrade. Epic VZ-3435. Add unit test.

--- platform-operator/controllers/verrazzano/component/prometheus.go
+++ platform-operator/controllers/verrazzano/component/prometheus.go
@@ -14,9 +14,16 @@ import (
 	clipkg "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
+// FixupPrometheusDeployment will update the pod template annotations of the Prometheus deployment.
+// This is required to configure the network path taken by Prometheus when scraping metrics.
+// In general metrics endpoints are not part of the Istio mesh so metrics scrape requests
+// should avoid the Istio sidecar.  However if Keycloak is being used then requests to Keycloak
+// should go through the Istio sidecar.
 func FixupPrometheusDeployment(log *zap.SugaredLogger, client clipkg.Client) error {
 	ctx := context.TODO()
-	// If Prometheus isn't deployed don't do anything.
+	// If Prometheus isn't deployed no changes are required so return without doing anything.
+	// The name of the Prometheus deployment should ideally come from profile or manifests
+	// but that information is not available to upgrade.
 	promKey := clipkg.ObjectKey{Namespace: constants.VerrazzanoSystemNamespace, Name: "vmi-system-prometheus-0"}
 	promObj := k8sapps.Deployment{}
 	err := client.Get(ctx, promKey, &promObj)
@@ -30,12 +37,18 @@ func FixupPrometheusDeployment(log *zap.SugaredLogger, client clipkg.Client) err
 	}
 
 	// If Keycloak isn't deployed configure Prometheus to avoid the Istio sidecar for metrics scraping.
+	// This is done by adding the traffic.sidecar.istio.io/excludeOutboundIPRanges: 0.0.0.0/0 annotation.
+	// The namespace and name of the Keycloak statefulset should ideally come from the profile or
+	// manifests but that information is not available to upgrade.
 	kcKey := clipkg.ObjectKey{Namespace: "keycloak", Name: "keycloak"}
 	kcObj := k8sapps.StatefulSet{}
 	err = client.Get(ctx, kcKey, &kcObj)
 	if errors.IsNotFound(err) {
 		// Set the Istio annotation on Prometheus to exclude all IP addresses.
-		promObj.Spec.Template.Annotations["traffic.sidecar.istio.io/excludeOutboundIPRanges"] = "0.0.0.0/0"
+		promObj.Spec.Template.Annotations = setAnnotation(
+			promObj.Spec.Template.Annotations,
+			"traffic.sidecar.istio.io/excludeOutboundIPRanges",
+			"0.0.0.0/0")
 		err = client.Update(ctx, &promObj)
 		if err != nil {
 			log.Errorf("Failed to update Istio annotations of Prometheus deployment: %s", err)
@@ -50,6 +63,9 @@ func FixupPrometheusDeployment(log *zap.SugaredLogger, client clipkg.Client) err
 
 	// Set the Istio annotation on Prometheus to exclude Keycloak HTTP Service IP address.
 	// The includeOutboundIPRanges implies all others are excluded.
+	// This is done by adding the traffic.sidecar.istio.io/includeOutboundIPRanges=<Keycloak IP>/32 annotation.
+	// The namespace and name of the Keycloak statefulset should ideally come from the profile or
+	// manifests but that information is not available to upgrade.
 	svcKey := clipkg.ObjectKey{Namespace: "keycloak", Name: "keycloak-http"}
 	svcObj := k8score.Service{}
 	err = client.Get(ctx, svcKey, &svcObj)
@@ -57,11 +73,22 @@ func FixupPrometheusDeployment(log *zap.SugaredLogger, client clipkg.Client) err
 		log.Errorf("Failed to find HTTP Service for Keycloak: %s", err)
 		return err
 	}
-	promObj.Spec.Template.Annotations["traffic.sidecar.istio.io/includeOutboundIPRanges"] = fmt.Sprintf("%s/32", svcObj.Spec.ClusterIP)
+	promObj.Spec.Template.Annotations = setAnnotation(
+		promObj.Spec.Template.Annotations,
+		"traffic.sidecar.istio.io/includeOutboundIPRanges",
+		fmt.Sprintf("%s/32", svcObj.Spec.ClusterIP))
 	err = client.Update(ctx, &promObj)
 	if err != nil {
 		log.Errorf("Failed to update Istio annotations of Prometheus deployment: %s", err)
 		return err
 	}
 	return nil
+}
+
+func setAnnotation(annotations map[string]string, name string, value string) map[string]string {
+	if annotations == nil {
+		annotations = map[string]string{}
+	}
+	annotations[name] = value
+	return annotations
 }
