1b533357c8cca2f55b6a463c744f91f1fbda86b7
Davis Gomes <davis.gomes@oracle.com>
2021-09-16 12:23:50 -0400

    VZ-3547 fix logging error in helm

--- platform-operator/scripts/install/config.sh
+++ platform-operator/scripts/install/config.sh
@@ -276,10 +276,6 @@ function config_array_to_helm_args {
       param_name=$(echo "$arg" | jq -r '.name')
       param_value=$(echo "$arg" | jq -r '.value')
       param_set_string=$(echo "$arg" | jq -r '.setString')
-
-      log "name: $param_name"
-      log "value: $param_value"
-
       if [ ! -z "$param_name" ]; then
         if [ "$param_set_string" == "true" ]; then
           helm_args="$helm_args --set-string $param_name=$param_value"
