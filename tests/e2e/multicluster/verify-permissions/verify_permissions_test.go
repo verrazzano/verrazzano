0ee4357e3cf070ee290af729e2aa457222313dd2
Michael Gianatassio <michael.gianatassio@oracle.com>
2021-09-16 08:34:05 -0400

    vz-3521 put back imports of ginkgo and gomega

--- tests/e2e/multicluster/verify-permissions/verify_permissions_test.go
+++ tests/e2e/multicluster/verify-permissions/verify_permissions_test.go
@@ -12,6 +12,8 @@ import (
 	"time"
 
 	oamv1alpha2 "github.com/crossplane/oam-kubernetes-runtime/apis/core/v1alpha2"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 	clustersv1alpha1 "github.com/verrazzano/verrazzano/application-operator/apis/clusters/v1alpha1"
 	"github.com/verrazzano/verrazzano/application-operator/apis/oam/v1alpha1"
 	"github.com/verrazzano/verrazzano/pkg/k8sutil"
