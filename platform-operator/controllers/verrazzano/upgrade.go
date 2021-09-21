2d0148cc19da5e29cedcc6d87d1e3b3824555dab
Michael Cico <46466506+michael-cico@users.noreply.github.com>
2021-09-21 07:18:00 -0400

    Create Component pre/post install and upgrade hooks (#1590)

--- platform-operator/controllers/verrazzano/upgrade.go
+++ platform-operator/controllers/verrazzano/upgrade.go
@@ -38,13 +38,22 @@ func (r *Reconciler) reconcileUpgrade(log *zap.SugaredLogger, cr *installv1alpha
 	}
 
 	// Loop through all of the Verrazzano components and upgrade each one sequentially
+	// - for now, upgrade is blocking
 	for _, comp := range registry.GetComponents() {
+		if err := comp.PreUpgrade(log, r, cr.Namespace, r.DryRun); err != nil {
+			// for now, this will be fatal until upgrade is retry-able
+			return ctrl.Result{}, err
+		}
 		err := comp.Upgrade(log, r, cr.Namespace, r.DryRun)
 		if err != nil {
 			log.Errorf("Error upgrading component %s: %v", comp.Name(), err)
 			msg := fmt.Sprintf("Error upgrading component %s - %s\".  Error is %s", comp.Name(),
 				fmtGeneration(cr.Generation), err.Error())
 			err := r.updateStatus(log, cr, msg, installv1alpha1.UpgradeFailed)
+			return ctrl.Result{}, err
+		}
+		if err := comp.PostUpgrade(log, r, cr.Namespace, r.DryRun); err != nil {
+			// for now, this will be fatal until upgrade is retry-able
 			return ctrl.Result{}, err
 		}
 	}
