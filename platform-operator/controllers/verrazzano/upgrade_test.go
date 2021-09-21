1f4173a304d30514827ea52669b26bbe46b425a6
Kevin Minder <kevin.minder@oracle.com>
2021-09-21 11:56:25 -0400

    VZ-3529: Patch Prometheus deployment during upgrade. Fix unit tests. Epic VZ-3435.

--- platform-operator/controllers/verrazzano/upgrade_test.go
+++ platform-operator/controllers/verrazzano/upgrade_test.go
@@ -606,6 +606,12 @@ func TestUpgradeCompletedStatusReturnsError(t *testing.T) {
 	defer config.Set(config.Get())
 	config.Set(config.OperatorConfig{VersionCheckEnabled: false})
 
+	// Expect a call to get the Prometheus deployment and return a NotFound error.
+	mock.EXPECT().
+		Get(gomock.Any(), types.NamespacedName{Namespace: "verrazzano-system", Name: "vmi-system-prometheus-0"}, gomock.Not(gomock.Nil())).
+		DoAndReturn(func(ctx context.Context, name types.NamespacedName, deployment *k8sapps.Deployment) error {
+			return errors2.NewNotFound(schema.GroupResource{Group: "apps", Resource: "Deployment"}, name.Name)
+		})
 	// Expect a call to get the verrazzano resource.  Return resource with version
 	mock.EXPECT().
 		Get(gomock.Any(), types.NamespacedName{Namespace: namespace, Name: name}, gomock.Not(gomock.Nil())).
