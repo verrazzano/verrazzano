0015a202fd38f403a7d1c15dec021645c229b169
Jonathan Maron <jonathan.maron@oracle.com>
2021-09-21 15:40:55 -0400

    configure credentials helper script

---
+++ release/scripts/credential_helper.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2021, Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+#
+echo username=$GITHUB_CREDENTIALS_USR
+echo password=$GITHUB_CREDENTIALS_PWD
