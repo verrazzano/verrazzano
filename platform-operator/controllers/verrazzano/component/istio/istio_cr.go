99dfda3c4ede01993bc6b037f0c63dd4484495bb
Paul Mackin <paul.mackin@oracle.com>
2021-09-23 20:30:19 -0400

    skip-at

--- platform-operator/controllers/verrazzano/component/istio/istio_cr.go
+++ platform-operator/controllers/verrazzano/component/istio/istio_cr.go
@@ -45,20 +45,49 @@ spec:
 {{.Values}}
 `
 
-// templateValues needed for template rendering
+// Template for merging externalIp YAML
+const externalIpTemplate = `
+apiVersion: install.istio.io/v1alpha1
+kind: IstioOperator
+spec:
+  components:
+    ingressGateways:
+      - name: istio-ingressgateway
+        enabled: true
+        k8s:
+          service:
+            type: ClusterIP
+            externalIPs:
+{{.ExternalIps}}
+`
+
+// templateValues is needed for template rendering of helm values
 type templateValues struct {
 	Values string
 }
 
+// templateExternalIP needed for template rendering of external IPs.
+type templateExternalIP struct {
+	ExternalIps string
+}
 // BuildIstioOperatorYaml builds the IstioOperator CR YAML that will be passed as an override to istioctl
 // Transform the Verrazzano CR IstioComponent provided by the user onto an IstioOperator formatted YAML
 func BuildIstioOperatorYaml(comp *vzapi.IstioComponent) (string, error) {
-	//
+	// All generated YAML will be indented 6 spaces
 	const leftMargin = 6
+
+	// This is a special case where Istio helm chart no logner supports ExternalIPs
+	// So we need to put it into the IstioOperator YAML, which does support it
+	const ExternalIpKey = "gateways.istio-ingressgateway.externalIPs"
+	var externalIpArg *vzapi.InstallArgs
 
 	// Build a list of YAML strings from the IstioComponent initargs, one for each arg.
 	var yamls []string
-	for _, arg := range comp.IstioInstallArgs {
+	for i, arg := range comp.IstioInstallArgs {
+		if arg.Name == ExternalIpKey {
+			externalIpArg = &comp.IstioInstallArgs[i]
+			continue
+		}
 		values := arg.ValueList
 		if len(values) == 0 {
 			values = []string{arg.Value}
@@ -89,5 +118,49 @@ func BuildIstioOperatorYaml(comp *vzapi.IstioComponent) (string, error) {
 		return "", err
 	}
 
+	// This the merged YAML
+	merged = rendered.String()
+
+	// If the externalIPs exists, the render the YAML and merge it
+	if externalIpArg != nil {
+		extYaml, err := renderExternalIpYAML(externalIpArg)
+		if err != nil {
+			return "", err
+		}
+		merged, err = vzyaml.ReplacementMerge(merged, extYaml)
+		if err != nil {
+			return "", err
+		}
+	}
+	return merged, nil
+}
+
+// Render the helm values using the template, return the YAML
+func renderHelmValues(yam string) (string, error) {
+	t, err := template.New("image").Parse(istioCrTempate)
+	if err != nil {
+		return "", err
+	}
+	var rendered bytes.Buffer
+	tInput := templateValues{Values: yam}
+	err = t.Execute(&rendered, tInput)
+	if err != nil {
+		return "", err
+	}
+	return rendered.String(), nil
+}
+
+// Render the externalIP values using the template, return the YAML
+func renderExternalIpYAML(arg *vzapi.InstallArgs) (string, error) {
+	t, err := template.New("image").Parse(istioCrTempate)
+	if err != nil {
+		return "", err
+	}
+	var rendered bytes.Buffer
+	tInput := templateValues{Values: valuesYaml}
+	err = t.Execute(&rendered, tInput)
+	if err != nil {
+		return "", err
+	}
 	return rendered.String(), nil
 }
