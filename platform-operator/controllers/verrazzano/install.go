d515c451b817e11355d44f157fa3dd3e6440b508
Paul Mackin <paul.mackin@oracle.com>
2021-09-27 13:59:26 -0400

    change VZCR WebLogia and Coherence name

--- platform-operator/controllers/verrazzano/install.go
+++ platform-operator/controllers/verrazzano/install.go
@@ -98,9 +98,9 @@ func (r *Reconciler) reconcileComponents(_ context.Context, log *zap.SugaredLogg
 func isComponentEnabled(cr *vzapi.Verrazzano, componentName string) bool {
 	switch componentName {
 	case coherence.ComponentName:
-		return coherence.IsEnabled(cr.Spec.Components.Coherence)
+		return coherence.IsEnabled(cr.Spec.Components.CoherenceOperator)
 	case weblogic.ComponentName:
-		return weblogic.IsEnabled(cr.Spec.Components.WebLogic)
+		return weblogic.IsEnabled(cr.Spec.Components.WebLogicOperator)
 	}
 	return true
 }
