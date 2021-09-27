e61f064abb34f4939962fe08a7f5971660b73bbe
ben.mclean <ben.mclean@oracle.com>
2021-09-27 10:37:29 -0400

    added delete for new mwc

--- platform-operator/scripts/uninstall/uninstall-steps/1-uninstall-istio.sh
+++ platform-operator/scripts/uninstall/uninstall-steps/1-uninstall-istio.sh
@@ -23,6 +23,7 @@ function uninstall_istio() {
   log "Removing Istio Webhook Configurations"
   kubectl delete MutatingWebhookConfiguration istio-sidecar-injector --ignore-not-found=true || err_return $? "Could not delete MutatingWebhookConfiguration from Istio" || return $?
   kubectl delete ValidatingWebhookConfiguration istiod-istio-system  --ignore-not-found=true || err_return $? "Could not delete ValidatingWebhookConfiguration from Istio" || return $?
+  kubectl delete MutatingWebhookConfiguration istio-sidecar-injector-1-10-2 --ignore-not-found=true || err_return $? "Could not delete MutatingWebhookConfiguration from Istio" || return $?
 
   # delete istio crds
   log "Deleting Istio Custom Resource Definitions"
