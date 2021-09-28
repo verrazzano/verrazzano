8bcace862b278d4a8b2d06c6deb6c5a8091f0567
Michael Gianatassio <31552226+mgianatagh@users.noreply.github.com>
2021-09-28 09:17:45 -0400

    VZ-3604 - add wait for VMC to be deleted in CLI tests (#1644)

--- tools/cli/scripts/cli_test.sh
+++ tools/cli/scripts/cli_test.sh
@@ -37,6 +37,25 @@ fi
 echo "vz cluster deregister ${MANAGED_CLUSTER_NAME}"
 vz cluster deregister ${MANAGED_CLUSTER_NAME}
 
+# wait for the VMC to be deleted
+retries=0
+echo "wait for VMC ${MANAGED_CLUSTER_NAME} to be deleted"
+set +e
+until [ "$retries" -ge 10 ]
+do
+  kubectl --kubeconfig ${ADMIN_KUBECONFIG} get vmc ${MANAGED_CLUSTER_NAME} -n verrazzano-mc
+  if [ $? -ne 0 ]; then
+    break
+  fi
+  retries=$(($retries+1))
+  sleep 5
+done
+set -e
+if [ "$retries" -ge 10 ] ; then
+  echo "failed to delete VMC ${MANAGED_CLUSTER_NAME}"
+  exit 1
+fi
+
 #create VerrazzanoMangedCLuster on admin
 echo "vz cluster register ${MANAGED_CLUSTER_NAME}"
 vz cluster register ${MANAGED_CLUSTER_NAME} -d "VerrazzanoManagedCluster object for ${MANAGED_CLUSTER_NAME}" -c "ca-secret-${MANAGED_CLUSTER_NAME}"
