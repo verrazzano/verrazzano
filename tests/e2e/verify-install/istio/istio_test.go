a3c2072f0c8892daf8a85bbca44b8a99844ea062
Paul Mackin <paul.mackin@oracle.com>
2021-10-04 11:48:23 -0400

    revert test changes

--- tests/e2e/verify-install/istio/istio_test.go
+++ tests/e2e/verify-install/istio/istio_test.go
@@ -37,6 +37,7 @@ var _ = Describe("Istio", func() {
 				"istio-egressgateway",
 				"istio-ingressgateway",
 				"istiod",
+				"istiocoredns",
 			}
 
 			deploymentNames := func(deploymentList *appsv1.DeploymentList) []string {
