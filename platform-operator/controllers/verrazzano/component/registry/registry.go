5375abc9fa192d720eb1bcb0a99126997a798e35
Paul Mackin <paul.mackin@oracle.com>
2021-10-04 11:56:16 -0400

    fix istio version in bom

--- platform-operator/controllers/verrazzano/component/registry/registry.go
+++ platform-operator/controllers/verrazzano/component/registry/registry.go
@@ -90,15 +90,6 @@ func getComponents() []spi.Component {
 			SkipUpgrade:             true,
 		},
 		helm.HelmComponent{
-			ReleaseName:             "istiocoredns",
-			ChartDir:                filepath.Join(thirdPartyChartsDir, "istio/istiocoredns"),
-			ChartNamespace:          "istio-system",
-			IgnoreNamespaceOverride: true,
-			ValuesFile:              filepath.Join(overridesDir, "istio-values.yaml"),
-			AppendOverridesFunc:     istio.AppendIstioOverrides,
-			SkipUpgrade:             true,
-		},
-		helm.HelmComponent{
 			ReleaseName:             nginx.ComponentName,
 			ChartDir:                filepath.Join(thirdPartyChartsDir, "ingress-nginx"), // Note name is different than release name
 			ChartNamespace:          "ingress-nginx",
