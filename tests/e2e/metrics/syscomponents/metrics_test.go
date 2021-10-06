58d51acdc4b813e1f824242a6ece1be352110871
Paul Mackin <paul.mackin@oracle.com>
2021-10-06 15:02:34 -0400

    change polling time

--- tests/e2e/metrics/syscomponents/metrics_test.go
+++ tests/e2e/metrics/syscomponents/metrics_test.go
@@ -16,7 +16,7 @@ import (
 )
 
 const (
-	longPollingInterval = 15 * time.Second
+	longPollingInterval = 8 * time.Second
 	longWaitTimeout     = 10 * time.Minute
 
 	// Constants for sample metrics of system components validated by the test
