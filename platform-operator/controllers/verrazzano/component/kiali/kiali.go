e924600f884cff3c9c8c7e02ae0713650aa269c9
ddrechse <doug.drechsel@oracle.com>
2021-10-06 12:24:41 -0400

    Add Enable/Disable functionality.  Diasabled is the default

--- platform-operator/controllers/verrazzano/component/kiali/kiali.go
+++ platform-operator/controllers/verrazzano/component/kiali/kiali.go
@@ -4,6 +4,7 @@
 package kiali
 
 import (
+	"github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1alpha1"
 	"github.com/verrazzano/verrazzano/platform-operator/internal/k8s/status"
 	"go.uber.org/zap"
 	"k8s.io/apimachinery/pkg/types"
@@ -20,4 +21,12 @@ func IsKialiReady(log *zap.SugaredLogger, c clipkg.Client, _ string, namespace s
 		{Name: kialiDeploymentName, Namespace: namespace},
 	}
 	return status.DeploymentsReady(log, c, deployments, 1)
+}
+
+// IsEnabled returns true if the component is enabled, which is the default
+func IsEnabled(comp *v1alpha1.KialiComponent) bool {
+	if comp == nil || comp.Enabled == nil {
+		return false
+	}
+	return *comp.Enabled
 }
--- platform-operator/controllers/verrazzano/install.go
+++ platform-operator/controllers/verrazzano/install.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	vzapi "github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1alpha1"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/coherence"
+	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/kiali"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/registry"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/spi"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/weblogic"
@@ -104,6 +105,8 @@ func isComponentEnabled(cr *vzapi.Verrazzano, componentName string) bool {
 		return coherence.IsEnabled(cr.Spec.Components.CoherenceOperator)
 	case weblogic.ComponentName:
 		return weblogic.IsEnabled(cr.Spec.Components.WebLogicOperator)
+	case kiali.ComponentName:
+		return kiali.IsEnabled(cr.Spec.Components.Kiali)
 	}
 	return true
 }
