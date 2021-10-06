4e2e789eca801b304f4a2f3ca64da1e184def89e
Davis Gomes <davis.gomes@oracle.com>
2021-10-06 12:26:53 -0400

    stripped back ssock shop

---
+++ tests/e2e/loggingtrait/coherenceworkload/coherence_loggingtrait_test.go
@@ -0,0 +1,129 @@
+// Copyright (c) 2021, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package coherencelogging
+
+import (
+	"k8s.io/apimachinery/pkg/types"
+	"os"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/verrazzano/verrazzano/tests/e2e/pkg"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+)
+
+const (
+	shortWaitTimeout     = 10 * time.Minute
+	shortPollingInterval = 10 * time.Second
+	longWaitTimeout      = 15 * time.Minute
+	longPollingInterval  = 20 * time.Second
+	namespace            = "hello-helidon-logging"
+	componentsPath       = "testdata/loggingtrait/helidonworkload/helidon-logging-components.yaml"
+	applicationPath      = "testdata/loggingtrait/helidonworkload/helidon-logging-application.yaml"
+	applicationPodName   = "hello-helidon-deployment-"
+	configMapName		 = "logging-stdout-hello-helidon-deployment-deployment"
+)
+
+var kubeConfig = os.Getenv("KUBECONFIG")
+
+var _ = BeforeSuite(func() {
+	deployApplication()
+})
+
+var _ = AfterSuite(func() {
+	undeployApplication()
+})
+
+func deployApplication() {
+	pkg.Log(pkg.Info, "Deploy test application")
+	// Wait for namespace to finish deletion possibly from a prior run.
+	Eventually(func() bool {
+		_, err := pkg.GetNamespace(namespace)
+		return err != nil && errors.IsNotFound(err)
+	}, shortWaitTimeout, shortPollingInterval).Should(BeTrue())
+
+	pkg.Log(pkg.Info, "Create namespace")
+	Eventually(func() (*v1.Namespace, error) {
+		nsLabels := map[string]string{
+			"verrazzano-managed": "true",
+			"istio-injection":    "enabled"}
+		return pkg.CreateNamespace(namespace, nsLabels)
+	}, shortWaitTimeout, shortPollingInterval).ShouldNot(BeNil())
+
+	pkg.Log(pkg.Info, "Create component resources")
+	Eventually(func() error {
+		return pkg.CreateOrUpdateResourceFromFile(componentsPath)
+	}, shortWaitTimeout, shortPollingInterval).ShouldNot(HaveOccurred())
+
+	pkg.Log(pkg.Info, "Create application resources")
+	Eventually(func() error {
+		return pkg.CreateOrUpdateResourceFromFile(applicationPath)
+	}, shortWaitTimeout, shortPollingInterval).ShouldNot(HaveOccurred())
+}
+
+func undeployApplication() {
+	pkg.Log(pkg.Info, "Delete application")
+	Eventually(func() error {
+		return pkg.DeleteResourceFromFile(applicationPath)
+	}, shortWaitTimeout, shortPollingInterval).ShouldNot(HaveOccurred())
+
+	pkg.Log(pkg.Info, "Delete components")
+	Eventually(func() error {
+		return pkg.DeleteResourceFromFile(componentsPath)
+	}, shortWaitTimeout, shortPollingInterval).ShouldNot(HaveOccurred())
+
+	pkg.Log(pkg.Info, "Verify ConfigMap is Deleted")
+	Eventually(func() bool {
+		configMap, _ := pkg.GetConfigMap(configMapName, namespace)
+		return (configMap == nil)
+	}, shortWaitTimeout, shortPollingInterval).Should(BeTrue())
+
+	pkg.Log(pkg.Info, "Delete namespace")
+	Eventually(func() error {
+		return pkg.DeleteNamespace(namespace)
+	}, shortWaitTimeout, shortPollingInterval).ShouldNot(HaveOccurred())
+
+	Eventually(func() bool {
+		_, err := pkg.GetNamespace(namespace)
+		return err != nil && errors.IsNotFound(err)
+	}, shortWaitTimeout, shortPollingInterval).Should(BeTrue())
+}
+
+var _ = Describe("Verify application.", func() {
+
+	Context("Deployment.", func() {
+		// GIVEN the app is deployed
+		// WHEN the running pods are checked
+		// THEN the adminserver and mysql pods should be found running
+		It("Verify 'hello-helidon-deployment' pod is running", func() {
+			Eventually(func() bool {
+				return pkg.PodsRunning(namespace, []string{applicationPodName})
+			}, longWaitTimeout, longPollingInterval).Should(BeTrue())
+		})
+	})
+
+	Context("LoggingTrait.", func() {
+		// GIVEN the app is deployed and the pods are running
+		// WHEN the app pod is inspected
+		// THEN the container for the logging trait should exist
+		It("Verify that 'logging-stdout' container exists in the 'hello-helidon-deployment' pod", func() {
+			Eventually(func() bool {
+				containerExists, err := pkg.DoesLoggingSidecarExist(kubeConfig, types.NamespacedName{Name: applicationPodName, Namespace: namespace}, "logging-stdout")
+				return containerExists && (err == nil)
+			}, shortWaitTimeout, shortPollingInterval).Should(BeTrue())
+		})
+
+		// GIVEN the app is deployed and the pods are running
+		// WHEN the configmaps in the app namespace are retrieved
+		// THEN the configmap for the logging trait should exist
+		It("Verify that 'logging-stdout-hello-helidon-deployment-deployment' ConfigMap exists in the 'hello-helidon-logging' namespace", func() {
+			Eventually(func() bool {
+				configMap, err := pkg.GetConfigMap(configMapName, namespace)
+				return (configMap != nil) && (err == nil)
+			}, shortWaitTimeout, shortPollingInterval).Should(BeTrue())
+		})
+	})
+})
