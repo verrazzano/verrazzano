115a475c630d6fed4975700b5a67c16384eaf5cc
Davis Gomes <davis.gomes@oracle.com>
2021-10-06 11:46:55 -0400

    helidon initial test -- doesn't work

---
+++ tests/e2e/loggingtrait/helidonworkload/helidon_loggingtrait_suite_test.go
@@ -0,0 +1,21 @@
+// Copyright (c) 2021, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package helidonlogging
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/onsi/ginkgo"
+	"github.com/onsi/ginkgo/config"
+	"github.com/onsi/ginkgo/reporters"
+	"github.com/onsi/gomega"
+)
+
+// TestWebLogicLoggingTrait tests an ingress trait setup for console access.
+func TestHelidonLoggingTrait(t *testing.T) {
+	gomega.RegisterFailHandler(ginkgo.Fail)
+	junitReporter := reporters.NewJUnitReporter(fmt.Sprintf("helidon-loggingtrait-%d-test-result.xml", config.GinkgoConfig.ParallelNode))
+	ginkgo.RunSpecsWithDefaultAndCustomReporters(t, "Helidon Logging Trait Test Suite", []ginkgo.Reporter{junitReporter})
+}
