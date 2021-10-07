a41218e9b92a54225af91b029aafb9107467c8dd
Paul Mackin <paul.mackin@oracle.com>
2021-10-07 13:47:29 -0400

    testing

--- platform-operator/controllers/verrazzano/component/istio/istio_install.go
+++ platform-operator/controllers/verrazzano/component/istio/istio_install.go
@@ -128,75 +128,10 @@ type installFuncSig func(log *zap.SugaredLogger, imageOverridesString string, ov
 // installFunc is the default install function
 var installFunc installFuncSig = istio.Install
 
-func SetIstioInstallFunction(fn installFuncSig) {
-	installFunc = fn
-}
-
-func ResetIstioInstallFunction() {
-	installFunc = istio.Install
-}
-
-func setInstallFunc(f installFuncSig) {
-	installFunc = f
-}
-
-func setDefaultInstallFunc() {
-	installFunc = istio.Install
-}
-
-// # Create
-//if ! kubectl get secret cacerts -n istio-system > /dev/null 2>&1 ; then
-//  action "Generating Istio CA bundle" create_secret || exit 1
-//fi
-
-// function create_secret {
-//  CERTS_OUT=$SCRIPT_DIR/build/istio-certs
-//
-//  rm -rf $CERTS_OUT || true
-//  rm -f ./index.txt* serial serial.old || true
-//
-//  mkdir -p $CERTS_OUT
-//  touch ./index.txt
-//  echo 1000 > ./serial
-//
-//  if ! kubectl get secret cacerts -n istio-system > /dev/null 2>&1; then
-//    log "Generating CA bundle for Istio"
-//
-//    # Create the private key for the root CA
-//    openssl genrsa -out $CERTS_OUT/root-key.pem 4096 || return $?
-//
-//    # Generate a root CA with the private key
-//    openssl req -config $CONFIG_DIR/istio_root_ca_config.txt -key $CERTS_OUT/root-key.pem -new -x509 -days 7300 -sha256 -extensions v3_ca -out $CERTS_OUT/root-cert.pem || return $?
-//
-//    # Create the private key for the intermediate CA
-//    openssl genrsa -out $CERTS_OUT/ca-key.pem 4096 || return $?
-//
-//    # Generate certificate signing request (CSR)
-//    openssl req -config $CONFIG_DIR/istio_intermediate_ca_config.txt -new -sha256 -key $CERTS_OUT/ca-key.pem -out $CERTS_OUT/intermediate-csr.pem || return $?
-//
-//    # create intermediate cert using the root CA
-//    openssl ca -batch -config $CONFIG_DIR/istio_root_ca_config.txt -extensions v3_intermediate_ca -days 3650 -notext -md sha256 \
-//        -keyfile $CERTS_OUT/root-key.pem \
-//        -cert $CERTS_OUT/root-cert.pem \
-//        -in $CERTS_OUT/intermediate-csr.pem \
-//        -out $CERTS_OUT/ca-cert.pem \
-//        -outdir $CERTS_OUT || return $?
-//
-//    # Create certificate chain file
-//    cat $CERTS_OUT/ca-cert.pem $CERTS_OUT/root-cert.pem > $CERTS_OUT/cert-chain.pem || return $?
-//
-//    kubectl create secret generic cacerts -n istio-system \
-//        --from-file=$CERTS_OUT/ca-cert.pem \
-//        --from-file=$CERTS_OUT/ca-key.pem  \
-//        --from-file=$CERTS_OUT/root-cert.pem \
-//        --from-file=$CERTS_OUT/cert-chain.pem || return $?
-//  else
-//    log "Istio CA Certs bundle and secret already created"
-//  fi
-//
-//  rm -rf $CERTS_OUT
-//  rm -f ./index.txt* serial serial.old
-//
-//  return 0
+//func setInstallFunc(f installFuncSig) {
+//	installFunc = f
 //}
 //
+//func setDefaultInstallFunc() {
+//	installFunc = istio.Install
+//}
