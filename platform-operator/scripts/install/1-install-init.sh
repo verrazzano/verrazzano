771293b595f881e02badce0a85ad6ca5bff2c494
Paul Mackin <paul.mackin@oracle.com>
2021-10-07 22:26:07 -0400

    more init

--- platform-operator/scripts/install/1-install-init.sh
+++ platform-operator/scripts/install/1-install-init.sh
@@ -12,6 +12,16 @@ trap 'rc=$?; rm -rf ${TMP_DIR} || true; _logging_exit_handler $rc' EXIT
 
 set -eu
 
+action "Checking Kubernetes version" log_kube_version || exit 1
+action "Checking Helm version" check_helm_version || (error "Helm version must be v3.x! Your Helm version is: $(helm version --short)"; exit 1)
+
+# Wait for all cluster nodes to exist, and then to be ready
+action "Waiting for all Kubernetes nodes to exist in cluster" wait_for_nodes_to_exist || exit 1
+
+log "Kubernetes nodes exist"
+action "Waiting for all Kubernetes nodes to be ready" \
+    kubectl wait --for=condition=ready nodes --all || exit 1
+
 # Label the kube-system namespace so that we can apply network policies
 log "Adding label needed by network policies to kube-system namespace"
 kubectl label namespace kube-system "verrazzano.io/namespace=kube-system" --overwrite
