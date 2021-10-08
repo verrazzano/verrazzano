b353c004f0e537ff26530d19f005a17a45e488cb
Padmanabha Bhat <padmanabha.bhat@oracle.com>
2021-10-08 16:52:52 +0530

    Set SkipUpgrade for mysql

--- platform-operator/controllers/verrazzano/component/registry/registry.go
+++ platform-operator/controllers/verrazzano/component/registry/registry.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/coherence"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/externaldns"
+	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/mysql"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/nginx"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/oam"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/rancher"
@@ -178,13 +179,14 @@ func getComponents() []spi.Component {
 			ReadyStatusFunc:         appoper.IsApplicationOperatorReady,
 			Dependencies:            []string{"oam-kubernetes-runtime"},
 		},
-		//helm.HelmComponent{
-		//	ReleaseName:             mysql.ComponentName,
-		//	ChartDir:                filepath.Join(thirdPartyChartsDir, mysql.ComponentName),
-		//	ChartNamespace:          "keycloak",
-		//	IgnoreNamespaceOverride: true,
-		//	ValuesFile:              filepath.Join(overridesDir, "mysql-values.yaml"),
-		//},
+		helm.HelmComponent{
+			ReleaseName:             mysql.ComponentName,
+			ChartDir:                filepath.Join(thirdPartyChartsDir, mysql.ComponentName),
+			ChartNamespace:          "keycloak",
+			IgnoreNamespaceOverride: true,
+			ValuesFile:              filepath.Join(overridesDir, "mysql-values.yaml"),
+			SkipUpgrade:             true,
+		},
 		helm.HelmComponent{
 			ReleaseName:             "keycloak",
 			ChartDir:                filepath.Join(thirdPartyChartsDir, "keycloak"),
