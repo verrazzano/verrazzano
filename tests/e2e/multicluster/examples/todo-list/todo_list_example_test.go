755ae3b4d0779afca1f5bb354755524ab734a216
stshapir <steven.shapiro@oracle.com>
2021-10-19 15:23:00 -0400

    first iteration of deployment
    Signed-off-by: stshapir <steven.shapiro@oracle.com>

--- tests/e2e/multicluster/examples/todo-list/todo_list_example_test.go
+++ tests/e2e/multicluster/examples/todo-list/todo_list_example_test.go
@@ -4,7 +4,6 @@
 package todo_list
 
 import (
-	"encoding/json"
 	"fmt"
 	"os"
 	"time"
@@ -12,137 +11,63 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/verrazzano/verrazzano/tests/e2e/pkg"
-	v1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
 )
 
 const (
-	shortWaitTimeout     = 10 * time.Minute
-	shortPollingInterval = 10 * time.Second
-	longWaitTimeout      = 15 * time.Minute
-	longPollingInterval  = 20 * time.Second
-	sourceDir            = "todo-list"
+	pollingInterval     = 5 * time.Second
+	waitTimeout         = 5 * time.Minute
+	longWaitTimeout     = 15 * time.Minute
+	longPollingInterval = 20 * time.Second
+	sourceDir           = "todo-list"
+	testNamespace       = "mc-todo-list"
+	testProjectName     = "todo-list"
+	testCluster         = "TodoList"
 )
 
 var clusterName = os.Getenv("MANAGED_CLUSTER_NAME")
 var adminKubeconfig = os.Getenv("ADMIN_KUBECONFIG")
 var managedKubeconfig = os.Getenv("MANAGED_KUBECONFIG")
 
-var _ = BeforeSuite(func() {
-	deployToDoListExample()
-})
-
+// failed indicates whether any of the tests has failed
 var failed = false
+
 var _ = AfterEach(func() {
 	failed = failed || CurrentGinkgoTestDescription().Failed
+})
+
+var _ = BeforeSuite(func() {
+	// deploy the VerrazzanoProject
+	Eventually(func() error {
+		return DeployTodoListProject(adminKubeconfig, sourceDir)
+	}, waitTimeout, pollingInterval).ShouldNot(HaveOccurred())
+
+	// wait for the namespace to be created on the cluster before deploying app
+	Eventually(func() bool {
+		return TodoListNamespaceExists(adminKubeconfig, testNamespace)
+	}, waitTimeout, pollingInterval).Should(BeTrue())
+
+	Eventually(func() error {
+		return DeployTodoListApp(adminKubeconfig, sourceDir)
+	}, waitTimeout, pollingInterval).ShouldNot(HaveOccurred())
 })
 
 var _ = AfterSuite(func() {
 	if failed {
 		pkg.ExecuteClusterDumpWithEnvVarConfig()
 	}
-	//undeployToDoListExample()
 })
 
-func deployToDoListExample() {
-	pkg.Log(pkg.Info, "Deploy ToDoList example")
-	wlsUser := "weblogic"
-	wlsPass := pkg.GetRequiredEnvVarOrFail("WEBLOGIC_PSW")
-	dbPass := pkg.GetRequiredEnvVarOrFail("DATABASE_PSW")
-	regServ := pkg.GetRequiredEnvVarOrFail("OCR_REPO")
-	regUser := pkg.GetRequiredEnvVarOrFail("OCR_CREDS_USR")
-	regPass := pkg.GetRequiredEnvVarOrFail("OCR_CREDS_PSW")
-
-	pkg.Log(pkg.Info, "Create namespace")
-	Eventually(func() (*v1.Namespace, error) {
-		nsLabels := map[string]string{
-			"verrazzano-managed": "true",
-			"istio-injection":    "enabled"}
-		return pkg.CreateNamespace("todo-list", nsLabels)
-	}, shortWaitTimeout, shortPollingInterval).ShouldNot(BeNil())
-
-	pkg.Log(pkg.Info, "Create Docker repository secret")
-	Eventually(func() (*v1.Secret, error) {
-		return pkg.CreateDockerSecret("todo-list", "tododomain-repo-credentials", regServ, regUser, regPass)
-	}, shortWaitTimeout, shortPollingInterval).ShouldNot(BeNil())
-
-	pkg.Log(pkg.Info, "Create WebLogic credentials secret")
-	Eventually(func() (*v1.Secret, error) {
-		return pkg.CreateCredentialsSecret("todo-list", "tododomain-weblogic-credentials", wlsUser, wlsPass, nil)
-	}, shortWaitTimeout, shortPollingInterval).ShouldNot(BeNil())
-
-	pkg.Log(pkg.Info, "Create database credentials secret")
-	Eventually(func() (*v1.Secret, error) {
-		return pkg.CreateCredentialsSecret("todo-list", "tododomain-jdbc-tododb", wlsUser, dbPass, map[string]string{"weblogic.domainUID": "tododomain"})
-	}, shortWaitTimeout, shortPollingInterval).ShouldNot(BeNil())
-
-	pkg.Log(pkg.Info, "Create component resources")
-	Eventually(func() error {
-		return pkg.CreateOrUpdateResourceFromFile("examples/todo-list/todo-list-components.yaml")
-	}, shortWaitTimeout, shortPollingInterval).ShouldNot(HaveOccurred())
-
-	pkg.Log(pkg.Info, "Create application resources")
-	Eventually(func() error {
-		return pkg.CreateOrUpdateResourceFromFile("examples/todo-list/todo-list-application.yaml")
-	}, shortWaitTimeout, shortPollingInterval, "Failed to create application resource").ShouldNot(HaveOccurred())
-}
-
-func undeployToDoListExample() {
-	pkg.Log(pkg.Info, "Undeploy ToDoList example")
-	pkg.Log(pkg.Info, "Delete application")
-	Eventually(func() error {
-		return pkg.DeleteResourceFromFile("examples/todo-list/todo-list-application.yaml")
-	}, shortWaitTimeout, shortPollingInterval).ShouldNot(HaveOccurred())
-
-	pkg.Log(pkg.Info, "Delete components")
-	Eventually(func() error {
-		return pkg.DeleteResourceFromFile("examples/todo-list/todo-list-components.yaml")
-	}, shortWaitTimeout, shortPollingInterval).ShouldNot(HaveOccurred())
-
-	pkg.Log(pkg.Info, "Delete namespace")
-	Eventually(func() error {
-		return pkg.DeleteNamespace("todo-list")
-	}, shortWaitTimeout, shortPollingInterval).ShouldNot(HaveOccurred())
-
-	pkg.Log(pkg.Info, "Deleted namespace check")
-	Eventually(func() bool {
-		_, err := pkg.GetNamespace("todo-list")
-		return err != nil && errors.IsNotFound(err)
-	}, shortWaitTimeout, shortPollingInterval).Should(BeTrue())
-
-	// GIVEN the ToDoList app is undeployed
-	// WHEN the app config certificate generated to support secure gateways is fetched
-	// THEN the certificate should have been cleaned up
-	pkg.Log(pkg.Info, "Deleted certificate check")
-	Eventually(func() bool {
-		_, err := pkg.GetCertificate("istio-system", "todo-list-todo-appconf-cert")
-		return err != nil && errors.IsNotFound(err)
-	}, shortWaitTimeout, shortPollingInterval).Should(BeTrue())
-
-	// GIVEN the ToDoList app is undeployed
-	// WHEN the app config secret generated to support secure gateways is fetched
-	// THEN the secret should have been cleaned up
-	pkg.Log(pkg.Info, "Waiting for secret containing certificate to be deleted")
-	var secret *v1.Secret
-	var err error
-	for i := 0; i < 30; i++ {
-		secret, err = pkg.GetSecret("istio-system", "todo-list-todo-appconf-cert-secret")
-		if err != nil && errors.IsNotFound(err) {
-			pkg.Log(pkg.Info, "Secret deleted")
-			return
-		}
-		if err != nil {
-			pkg.Log(pkg.Error, fmt.Sprintf("Error attempting to get secret: %v", err))
-		}
-		time.Sleep(shortPollingInterval)
+func cleanUp(kubeconfigPath string) error {
+	if err := pkg.DeleteResourceFromFileInCluster(fmt.Sprintf("examples/multicluster/%s/mc-todo-list-application.yaml", sourceDir), kubeconfigPath); err != nil {
+		return fmt.Errorf("failed to delete multi-cluster sock-shop application resource: %v", err)
 	}
 
-	pkg.Log(pkg.Error, "Secret could not be deleted. Secret data:")
-	if secret != nil {
-		if b, err := json.Marshal(secret); err == nil {
-			pkg.Log(pkg.Info, string(b))
-		}
+	if err := pkg.DeleteResourceFromFileInCluster(fmt.Sprintf("examples/multicluster/%s/todo-list-components.yaml", sourceDir), kubeconfigPath); err != nil {
+		return fmt.Errorf("failed to delete multi-cluster sock-shop component resources: %v", err)
 	}
-	pkg.ExecuteClusterDumpWithEnvVarConfig()
-	Fail("Unable to delete secret")
+
+	if err := pkg.DeleteResourceFromFileInCluster(fmt.Sprintf("examples/multicluster/%s/verrazzano-project.yaml", sourceDir), kubeconfigPath); err != nil {
+		return fmt.Errorf("failed to delete sock-shop project resource: %v", err)
+	}
+	return nil
 }
--- tests/e2e/multicluster/examples/todo-list/todo_list_example_utils.go
+++ tests/e2e/multicluster/examples/todo-list/todo_list_example_utils.go
@@ -2,3 +2,80 @@
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 package todo_list
+
+import (
+	"fmt"
+	"time"
+
+	. "github.com/onsi/gomega"
+	"github.com/verrazzano/verrazzano/tests/e2e/pkg"
+	v1 "k8s.io/api/core/v1"
+)
+
+const (
+	multiclusterNamespace = "verrazzano-mc"
+	appConfigName         = "todo-appconf"
+	shortWaitTimeout      = 10 * time.Minute
+	shortPollingInterval  = 10 * time.Second
+)
+
+var (
+	expectedCompsTodoList = []string{
+		"todo-domain",
+		"todo-jdbc-config",
+		"mysql-initdb-config",
+		"todo-mysql-service",
+		"todo-mysql-deployment"}
+	expectedPodsTodoList = []string{
+		"mysql",
+		"tododomain-adminserver"}
+)
+
+// DeployTodoListProject deploys the sock-shop example's VerrazzanoProject to the cluster with the given kubeConfigPath
+func DeployTodoListProject(kubeconfigPath string, sourceDir string) error {
+	if err := pkg.CreateOrUpdateResourceFromFileInCluster(fmt.Sprintf("examples/multicluster/%s/verrazzano-project.yaml", sourceDir), kubeconfigPath); err != nil {
+		return fmt.Errorf("failed to create %s project resource: %v", sourceDir, err)
+	}
+	return nil
+}
+
+// TodoListNamespaceExists SockShopExists - returns true if the sock-shop namespace exists in the given cluster
+func TodoListNamespaceExists(kubeconfigPath string, namespace string) bool {
+	_, err := pkg.GetNamespaceInCluster(namespace, kubeconfigPath)
+	return err == nil
+}
+
+// DeployTodoListApp deploys the sock-shop example application to the cluster with the given kubeConfigPath
+func DeployTodoListApp(kubeconfigPath string, sourceDir string) error {
+
+	pkg.Log(pkg.Info, "Deploy ToDoList example")
+	wlsUser := "weblogic"
+	wlsPass := pkg.GetRequiredEnvVarOrFail("WEBLOGIC_PSW")
+	dbPass := pkg.GetRequiredEnvVarOrFail("DATABASE_PSW")
+	regServ := pkg.GetRequiredEnvVarOrFail("OCR_REPO")
+	regUser := pkg.GetRequiredEnvVarOrFail("OCR_CREDS_USR")
+	regPass := pkg.GetRequiredEnvVarOrFail("OCR_CREDS_PSW")
+
+	// create Docker repository secret
+	Eventually(func() (*v1.Secret, error) {
+		return pkg.CreateDockerSecret("todo-list", "tododomain-repo-credentials", regServ, regUser, regPass)
+	}, shortWaitTimeout, shortPollingInterval).ShouldNot(BeNil())
+
+	// create Weblogic credentials secret
+	Eventually(func() (*v1.Secret, error) {
+		return pkg.CreateCredentialsSecret("todo-list", "tododomain-weblogic-credentials", wlsUser, wlsPass, nil)
+	}, shortWaitTimeout, shortPollingInterval).ShouldNot(BeNil())
+
+	// create database credentials secret
+	Eventually(func() (*v1.Secret, error) {
+		return pkg.CreateCredentialsSecret("todo-list", "tododomain-jdbc-tododb", wlsUser, dbPass, map[string]string{"weblogic.domainUID": "tododomain"})
+	}, shortWaitTimeout, shortPollingInterval).ShouldNot(BeNil())
+
+	if err := pkg.CreateOrUpdateResourceFromFileInCluster(fmt.Sprintf("examples/multicluster/%s/todo-list-components.yaml", sourceDir), kubeconfigPath); err != nil {
+		return fmt.Errorf("failed to create multi-cluster %s component resources: %v", sourceDir, err)
+	}
+	if err := pkg.CreateOrUpdateResourceFromFileInCluster(fmt.Sprintf("examples/multicluster/%s/mc-todo-list-application.yaml", sourceDir), kubeconfigPath); err != nil {
+		return fmt.Errorf("failed to create multi-cluster %s application resource: %v", sourceDir, err)
+	}
+	return nil
+}
