fcec6003ef820aa9dd23e503aa00bacb6e99e9c3
Paul Mackin <paul.mackin@oracle.com>
2021-10-27 13:00:49 -0400

    remove test

--- tests/e2e/verify-install/kubernetes/kubernetes_test.go
+++ tests/e2e/verify-install/kubernetes/kubernetes_test.go
@@ -169,7 +169,6 @@ var _ = Describe("Kubernetes Cluster",
 			ginkgoExt.Entry("includes es-kibana", "vmi-system-kibana", !isManagedClusterProfile),
 			ginkgoExt.Entry("includes es-grafana", "vmi-system-grafana", !isManagedClusterProfile),
 			ginkgoExt.Entry("includes verrazzano-console", "verrazzano-console", !isManagedClusterProfile),
-			ginkgoExt.Entry("includes kiali", "vmi-system-kiali", !isManagedClusterProfile),
 		)
 
 		It("Expected pods are running", func() {
