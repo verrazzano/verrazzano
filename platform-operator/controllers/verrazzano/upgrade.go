3d5eb8cdb1b5ae3cc272c1e98e949e468886bacc
bbmclean <ben.mclean@oracle.com>
2021-10-28 20:05:43 +0000

    upgrade failure limit 10

--- platform-operator/controllers/verrazzano/upgrade.go
+++ platform-operator/controllers/verrazzano/upgrade.go
@@ -19,7 +19,7 @@ import (
 )
 
 // The max upgrade failures for a given upgrade attempt is 2
-const failedUpgradeLimit = 2
+const failedUpgradeLimit = 10
 
 // Reconcile upgrade will upgrade the components as required
 func (r *Reconciler) reconcileUpgrade(log *zap.SugaredLogger, cr *installv1alpha1.Verrazzano) (ctrl.Result, error) {
