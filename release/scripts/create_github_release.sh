9fc0624b271053be390fecf7d8f53a64490ceb44
Fred Tibbitts <fred.tibbitts@oracle.com>
2021-11-08 17:52:18 -0500

    Fixes/enhancements and move verification of release artifacts to separate script

--- release/scripts/create_github_release.sh
+++ release/scripts/create_github_release.sh
@@ -17,7 +17,7 @@ usage() {
     $(basename $0) <new version for the release> <hash of commit to release> <directory containing the release binaries> <a boolean to indicate test run, defaults to true>
 
   Example:
-    $(basename $0) v.1.0.1 aa94949a4e8e9b50bc0674035898f2579f2519cb ~/go/src/github.com/verrazzano/verrazzano/release
+    $(basename $0) v1.0.1 aa94949a4e8e9b50bc0674035898f2579f2519cb ~/go/src/github.com/verrazzano/verrazzano/release
 
   The script assumes Github CLI is installed and login is performed to authenticate the Github account.
 
@@ -32,37 +32,7 @@ RELEASE_COMMIT=${2}
 RELEASE_BINARIES_DIR=${3}
 TEST_RUN=${4:-true}
 
-function verify_released_artifacts() {
-  local releaseVersionDir=$RELEASE_BINARIES_DIR/release
-  mkdir -p $releaseVersionDir
-  cd $releaseVersionDir
-
-  # Iterate the array containing the release artifacts and download all of them
-  for i in "${releaseArtifacts[@]}"
-  do
-    wget -O $i https://github.com/verrazzano/verrazzano/releases/download/v$VERSION/$i
-  done
-  sha256sum -c k8s-dump-cluster.sh.sha256
-  sha256sum -c verrazzano-analysis-darwin-amd64.tar.gz.sha256
-  sha256sum -c verrazzano-analysis-linux-amd64.tar.gz.sha256
-
-  # Latest tag is automatic, do we really need to check ? If required, better compare the files from the two directories
-  local latestVersionDir=$RELEASE_BINARIES_DIR/latest
-  mkdir -p $latestVersionDir
-  cd $latestVersionDir
-
-  # Iterate the array containing the release artifacts and download all of them
-  for i in "${releaseArtifacts[@]}"
-  do
-    wget -O $i https://github.com/verrazzano/verrazzano/releases/latest/download/$i
-  done
-  sha256sum -c k8s-dump-cluster.sh.sha256
-  sha256sum -c verrazzano-analysis-darwin-amd64.tar.gz.sha256
-  sha256sum -c verrazzano-analysis-linux-amd64.tar.gz.sha256
-}
-
 function verify_release_binaries_exist() {
-  cd ${RELEASE_BINARIES_DIR}
   for i in "${releaseArtifacts[@]}"
   do
     if [ ! -f $i ];then
@@ -82,7 +52,14 @@ verify_release_binaries_exist || exit 1
 validate_github_cli || exit 1
 
 if [ $TEST_RUN == true ] ; then
-    echo "TEST_RUN is set to true, not doing a github release."
+    echo "TEST_RUN is set to true, NOT doing a github release. This is the command that would be executed:"
+    echo ""
+    echo gh release create \"${VERSION}\" \
+      --draft \
+      --target \"${RELEASE_COMMIT}\" \
+      --notes \"\" \
+      --title \"Verrazzano release ${VERSION}\" \
+    ${releaseArtifacts[*]}
 else
     echo "TEST_RUN is set to false, doing a github release now."
     # Setting an empty string for notes, as the release notes will be prepared separately
@@ -93,4 +70,3 @@ else
       --title "Verrazzano release ${VERSION}" \
     ${releaseArtifacts[*]}
 fi
-verify_released_artifacts || exit 1
