bfd902245f5202927c3c3404e25ba1038cb8ea4d
Paul Mackin <paul.mackin@oracle.com>
2021-11-10 19:27:36 -0500

    testing

--- tests/e2e/config/scripts/dump_pods.sh
+++ tests/e2e/config/scripts/dump_pods.sh
@@ -3,5 +3,11 @@
 # Copyright (c) 2021, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 #
-NAMESPACE=${1}
-kubectl get pods -n ${NAMESPACE} -o jsonpath='{range .items[*]}{.metadata.name}{" "}{..uid}{"\n"}{end}'
+set -x
+NAMESPACES=${1}
+OUTFILE=${2}
+rm ${OUTFILE}
+for ns in ${NAMESPACES[@]}
+do
+  kubectl get pods -n ${ns} -o jsonpath='{range .items[*]}{.metadata.name}{" "}{..uid}{"\n"}{end}' >> ${OUTFILE}
+done
