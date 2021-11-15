7a05ca95ada5d4e0c046290f1f4dfe87f50c3290
Michael Gianatassio <31552226+mgianatagh@users.noreply.github.com>
2021-11-15 16:41:00 -0500

    vz-4021 Initial setup of ginkgo wrappers for testing framework (#1974)

--- build/coverage.sh
+++ build/coverage.sh
@@ -13,7 +13,8 @@ go test -coverpkg=./... -coverprofile ./coverage.raw.cov $(go list ./... | \
   grep -Ev github.com/verrazzano/verrazzano/platform-operator/test/integ | \
   grep -Ev github.com/verrazzano/verrazzano/platform-operator/clients | \
   grep -Ev github.com/verrazzano/verrazzano/tools | \
-  grep -Ev github.com/verrazzano/verrazzano/tests)
+  grep -Ev github.com/verrazzano/verrazzano/tests | \
+  grep -Ev github.com/verrazzano/verrazzano/pkg/framework)
 
 TEST_STATUS=$?
 
