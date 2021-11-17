2c900b4516f46f3840d93893a040616b33cf5664
Michael Gianatassio <31552226+mgianatagh@users.noreply.github.com>
2021-11-17 11:25:16 -0500

    VZ-4021 refactor framework package to be under pkg/test (#1987)

--- tests/e2e/verify-install/verrazzano/verrazzano_test.go
+++ tests/e2e/verify-install/verrazzano/verrazzano_test.go
@@ -8,7 +8,7 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"github.com/verrazzano/verrazzano/pkg/framework"
+	"github.com/verrazzano/verrazzano/pkg/test/framework"
 	"github.com/verrazzano/verrazzano/tests/e2e/pkg"
 	rbacv1 "k8s.io/api/rbac/v1"
 )
