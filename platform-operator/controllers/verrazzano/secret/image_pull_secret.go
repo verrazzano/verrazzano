1a7686320ea22b7af07de89ff19a2ec7a6c378e5
ben.mclean <ben.mclean@oracle.com>
2021-11-18 17:51:21 -0500

    Revert "logs"
    This reverts commit 5f3337da62a2a92ea4e5c9818b8902edfbba21a9.

--- platform-operator/controllers/verrazzano/secret/image_pull_secret.go
+++ platform-operator/controllers/verrazzano/secret/image_pull_secret.go
@@ -18,13 +18,11 @@ const DefaultImagePullSecretKeyName = "imagePullSecrets[0].name"
 
 // CheckImagePullSecret Checks if the global image pull secret exists and copies it into the specified namespace; returns
 // true if the image pull secret exists and was copied successfully.
-func CheckImagePullSecret(client client.Client, targetNamespace string, log *zap.SugaredLogger) (bool, error) {
+func CheckImagePullSecret(client client.Client, targetNamespace string) (bool, error) {
 	var targetSecret v1.Secret
 	if err := client.Get(context.TODO(), types.NamespacedName{Namespace: targetNamespace, Name: constants.GlobalImagePullSecName}, &targetSecret); err == nil {
-
 		return true, nil
 	} else if !errors.IsNotFound(err) {
-
 		// Unexpected error
 		return false, err
 	}
@@ -56,7 +54,7 @@ func CheckImagePullSecret(client client.Client, targetNamespace string, log *zap
 
 // AddGlobalImagePullSecretHelmOverride Adds a helm override Key if the global image pull secret exists and was copied successfully to the target namespace
 func AddGlobalImagePullSecretHelmOverride(log *zap.SugaredLogger, client client.Client, ns string, kvs []bom.KeyValue, keyName string) ([]bom.KeyValue, error) {
-	secretExists, err := CheckImagePullSecret(client, ns, log)
+	secretExists, err := CheckImagePullSecret(client, ns)
 	if err != nil {
 		log.Errorf("Error copying global image pull secret %s to %s namespace", constants.GlobalImagePullSecName, ns)
 		return kvs, err
