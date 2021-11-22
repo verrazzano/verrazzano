963ce7785dd13afd5781572601aeca39927209e4
anders-swanson <91502735+anders-swanson@users.noreply.github.com>
2021-11-22 06:45:00 -0800

    VZ 3513: Rancher install to Go code (#1989)
    Rancher install to Go code

--- platform-operator/scripts/install/2-install-system-components.sh
+++ platform-operator/scripts/install/2-install-system-components.sh
@@ -20,6 +20,21 @@ function wait_for_nginx() {
   return $?
 }
 
+function wait_for_rancher() {
+  wait_for_deployment cattle-system rancher
+  return $?
+}
+
+function create_cattle_system_namespace()
+{
+    if ! kubectl get namespace cattle-system > /dev/null 2>&1; then
+        kubectl create namespace cattle-system
+    fi
+
+    log "Adding label needed by Rancher network policies to cattle-system namespace"
+    kubectl label namespace cattle-system "verrazzano.io/namespace=cattle-system" --overwrite
+}
+
 function install_nginx_ingress_controller()
 {
     local ingress_nginx_ns=ingress-nginx
@@ -122,300 +137,6 @@ function install_external_dns()
     fi
 }
 
-function ensure_rancher_admin_user() {
-  log "Ensure default Rancher admin user is present"
-  local STDERROR_FILE="${TMP_DIR}/rancher_ensureadminuser.err"
-  kubectl --kubeconfig $KUBECONFIG -n cattle-system exec $(kubectl --kubeconfig $KUBECONFIG -n cattle-system get pods -l app=rancher | grep '1/1' | head -1 | awk '{ print $1 }') -- ensure-default-admin > /dev/null 2>$STDERROR_FILE
-  local max_retries=5
-  local retries=0
-  while true ; do
-    RANCHER_ADMIN_USERNAME=$(kubectl get users -l authz.management.cattle.io/bootstrapping=admin-user -o jsonpath={'.items[].username'} || true)
-    if [ -z "${RANCHER_ADMIN_USERNAME}" ] ; then
-      sleep 10
-    else
-      log "Rancher admin user: ${RANCHER_ADMIN_USERNAME}"
-      break
-    fi
-    ((retries+=1))
-    if [ "$retries" -ge "$max_retries" ] ; then
-      echo "Could not detect default Rancher admin user"
-      local std_error_file=$(cat $STDERROR_FILE)
-      log "${std_error_file}"
-      rm "$STDERROR_FILE"
-      return 1
-    fi
-    log "Retry Rancher admin user lookup..."
-  done
-  return 0
-}
-
-function reset_rancher_admin_password() {
-  if kubectl get secret cattle-system -n rancher-admin-secret 2>&1 > /dev/null ; then
-    log "Rancher admin secret exists, skipping"
-    return 0
-  fi
-
-  ensure_rancher_admin_user || return $?
-
-  log "Reset Rancher admin password and create secrets"
-  local STDERROR_FILE="${TMP_DIR}/rancher_resetpwd.err"
-  local max_retries=5
-  local retries=0
-  while true ; do
-    RANCHER_DATA=$(kubectl --kubeconfig $KUBECONFIG -n cattle-system exec $(kubectl --kubeconfig $KUBECONFIG -n cattle-system get pods -l app=rancher | grep '1/1' | head -1 | awk '{ print $1 }') -- reset-password 2>$STDERROR_FILE)
-    ADMIN_PW=$(echo -n $RANCHER_DATA | awk 'END{ print $NF }')
-
-    if [ -z "$ADMIN_PW" ] ; then
-      sleep 10
-    else
-      break
-    fi
-    ((retries+=1))
-    if [ "$retries" -ge "$max_retries" ] ; then
-      error "ERROR: Failed to reset Rancher password"
-      local std_error_file=$(cat $STDERROR_FILE)
-      log "${std_error_file}"
-      rm "$STDERROR_FILE"
-      return 1
-    fi
-    log "Retry Rancher admin password reset..."
-  done
-
-  update_secret_from_literal rancher-admin-secret cattle-system "$ADMIN_PW"
-}
-
-function create_cattle_system_namespace()
-{
-    if ! kubectl get namespace cattle-system > /dev/null 2>&1; then
-        kubectl create namespace cattle-system
-    fi
-
-    log "Adding label needed by Rancher network policies to cattle-system namespace"
-    kubectl label namespace cattle-system "verrazzano.io/namespace=cattle-system" --overwrite
-}
-
-function install_rancher()
-{
-    local RANCHER_CHART_DIR=${CHARTS_DIR}/rancher
-
-    # Create the rancher-operator-system namespace so we can create network policies
-    if ! kubectl get namespace rancher-operator-system > /dev/null 2>&1; then
-        kubectl create namespace rancher-operator-system
-    fi
-
-    local INGRESS_TLS_SOURCE=""
-    local EXTRA_RANCHER_ARGUMENTS=""
-    local RANCHER_PATCH_DATA=""
-    local useAdditionalCAs=false
-    if ! is_chart_deployed rancher cattle-system ${RANCHER_CHART_DIR} ; then
-      if [ "$CERT_ISSUER_TYPE" == "acme" ]; then
-        INGRESS_TLS_SOURCE="letsEncrypt"
-        if [ "$(get_acme_environment)" != "production" ]; then
-          log "Using ACME staging, enable use of additional trusted CAs for Rancher"
-          useAdditionalCAs=true
-        fi
-        EXTRA_RANCHER_ARGUMENTS="--set letsEncrypt.ingress.class=rancher --set letsEncrypt.email=$(get_config_value ".certificates.acme.emailAddress") --set letsEncrypt.environment=$(get_acme_environment) --set additionalTrustedCAs=${useAdditionalCAs}"
-        RANCHER_PATCH_DATA="{\"metadata\":{\"annotations\":{\"kubernetes.io/tls-acme\":\"true\",\"nginx.ingress.kubernetes.io/auth-realm\":\"${DNS_SUFFIX} auth\",\"external-dns.alpha.kubernetes.io/target\":\"verrazzano-ingress.${NAME}.${DNS_SUFFIX}\",\"cert-manager.io/issuer\":null,\"cert-manager.io/issuer-kind\":null,\"external-dns.alpha.kubernetes.io/ttl\":\"60\"}}}"
-      elif [ "$CERT_ISSUER_TYPE" == "ca" ]; then
-        INGRESS_TLS_SOURCE="secret"
-        if [ $(get_config_value ".certificates.ca.secretName") == "$VERRAZZANO_DEFAULT_SECRET_NAME" ] &&
-           [ $(get_config_value ".certificates.ca.clusterResourceNamespace") == "$VERRAZZANO_DEFAULT_SECRET_NAMESPACE" ]; then
-          EXTRA_RANCHER_ARGUMENTS="--set privateCA=true"
-          local retries=0
-          local max_retries=60
-          until [ "$retries" -ge "$max_retries" ]
-          do
-            log "Waiting for secret $VERRAZZANO_DEFAULT_SECRET_NAME in namespace $VERRAZZANO_DEFAULT_SECRET_NAMESPACE to be created."
-            if kubectl -n $VERRAZZANO_DEFAULT_SECRET_NAMESPACE get secret $VERRAZZANO_DEFAULT_SECRET_NAME >/dev/null 2>&1 ; then
-              kubectl -n $VERRAZZANO_DEFAULT_SECRET_NAMESPACE get secret $VERRAZZANO_DEFAULT_SECRET_NAME -o jsonpath='{.data.ca\.crt}' | base64 --decode > ${TMP_DIR}/cacerts.pem || return $?
-              break
-            fi
-            retries=$(($retries+1))
-            sleep 1
-          done
-          if [ "$retries" -ge "$max_retries" ]; then
-            fail "Failed to get secret $VERRAZZANO_DEFAULT_SECRET_NAME in namespace $VERRAZZANO_DEFAULT_SECRET_NAMESPACE.";
-          fi
-          log "Copy CA certificate which is used by the Rancher Agent to validate the connection to the server."
-          kubectl -n cattle-system create secret generic tls-ca --from-file=cacerts.pem=${TMP_DIR}/cacerts.pem || return $?
-        fi
-        RANCHER_PATCH_DATA="{\"metadata\":{\"annotations\":{\"kubernetes.io/tls-acme\":\"true\",\"nginx.ingress.kubernetes.io/auth-realm\":\"${NAME}.${DNS_SUFFIX} auth\",\"cert-manager.io/cluster-issuer\":\"verrazzano-cluster-issuer\"}}}"
-      else
-        fail "certificates issuerType $CERT_ISSUER_TYPE is not supported.";
-      fi
-
-      log "Install Rancher"
-
-      IMAGE_PULL_SECRETS_ARGUMENT=""
-      if [ ${REGISTRY_SECRET_EXISTS} == "TRUE" ]; then
-        IMAGE_PULL_SECRETS_ARGUMENT=" --set imagePullSecrets[0].name=${GLOBAL_IMAGE_PULL_SECRET}"
-      fi
-
-      # Settings required to point Rancher at a registry for background helm install
-      if [ -n "${REGISTRY}" ]; then
-        local sys_default_reg=${REGISTRY}
-
-        if [ -n "${IMAGE_REPO}" ]; then
-          sys_default_reg=${REGISTRY}/${IMAGE_REPO}
-        fi
-
-        EXTRA_RANCHER_ARGUMENTS="${EXTRA_RANCHER_ARGUMENTS} --set systemDefaultRegistry=${sys_default_reg} --set useBundledSystemChart=true"
-      fi
-
-      if [ "$useAdditionalCAs" = "true" ] && ! kubectl -n cattle-system get secret tls-ca-additional 2>&1 > /dev/null ; then
-        log "Using ACME staging, create staging certs secret for Rancher"
-        local acme_staging_certs=${TMP_DIR}/ca-additional.pem
-        echo -n "" > ${acme_staging_certs}
-        curl_args=(--output ${TMP_DIR}/int-r3.pem "https://letsencrypt.org/certs/staging/letsencrypt-stg-int-r3.pem")
-        call_curl 200 http_response http_status curl_args || true
-        if [ ${http_status:--1} -ne 200 ]; then
-          log "Error downloading LetsEncrypt Staging intermediate R3 cert"
-        else
-          cat ${TMP_DIR}/int-r3.pem >> ${acme_staging_certs}
-        fi
-        curl_args=(--output ${TMP_DIR}/int-e1.pem "https://letsencrypt.org/certs/staging/letsencrypt-stg-int-e1.pem")
-        call_curl 200 http_response http_status curl_args || true
-        if [ ${http_status:--1} -ne 200 ]; then
-          log "Error downloading LetsEncrypt Staging intermediate E1 cert"
-        else
-          cat ${TMP_DIR}/int-e1.pem >> ${acme_staging_certs}
-        fi
-        curl_args=(--output ${TMP_DIR}/root-x1.pem "https://letsencrypt.org/certs/staging/letsencrypt-stg-root-x1.pem")
-        call_curl 200 http_response http_status curl_args || true
-        if [ ${http_status:--1} -ne 200 ]; then
-          log "Error downloading LetsEncrypt Staging X1 Root cert"
-        else
-          cat ${TMP_DIR}/root-x1.pem >> ${acme_staging_certs}
-        fi
-        kubectl -n cattle-system create secret generic tls-ca-additional --from-file=ca-additional.pem=${acme_staging_certs}
-      fi
-
-      local chart_name=rancher
-      build_image_overrides rancher ${chart_name}
-
-      # Check if this install is using a dns type "external".
-      if [ $(is_external_dns) == "true" ]; then
-        log "Installing cattle-system/${chart_name}"
-        # Do not add --wait since helm install will not fully work in OLCNE until MKNOD is added in the next command
-        helm upgrade ${chart_name} ${RANCHER_CHART_DIR} \
-          --install --namespace cattle-system \
-          --set hostname=rancher.${NAME}.${DNS_SUFFIX} \
-          --set ingress.tls.source=${INGRESS_TLS_SOURCE} \
-          ${HELM_IMAGE_ARGS} \
-          ${IMAGE_PULL_SECRETS_ARGUMENT} \
-          ${EXTRA_RANCHER_ARGUMENTS} \
-          || return $?
-      else
-        helm_install_retry ${chart_name} ${RANCHER_CHART_DIR} cattle-system \
-          --set hostname=rancher.${NAME}.${DNS_SUFFIX} \
-          --set ingress.tls.source=${INGRESS_TLS_SOURCE} \
-          ${HELM_IMAGE_ARGS} \
-          ${IMAGE_PULL_SECRETS_ARGUMENT} \
-          ${EXTRA_RANCHER_ARGUMENTS} \
-          || return $?
-      fi
-    fi
-
-    # CRI-O does not deliver MKNOD by default, until https://github.com/rancher/rancher/pull/27582 is merged we must add the capability
-    # OLCNE uses CRI-O and needs this change, and it doesn't hurt other cases
-    kubectl patch deployments -n cattle-system rancher -p '{"spec":{"template":{"spec":{"containers":[{"name":"rancher","securityContext":{"capabilities":{"add":["MKNOD"]}}}]}}}}'
-
-    log "Patch Rancher ingress"
-    kubectl patch ingress rancher -n cattle-system -p "$RANCHER_PATCH_DATA" --type=merge
-
-    log "Rollout Rancher"
-    kubectl -n cattle-system rollout status -w deploy/rancher || return $?
-
-    log "Waiting for Rancher TLS cert to reach ready state"
-    kubectl wait --for=condition=ready cert tls-rancher-ingress -n cattle-system
-
-    # Make sure rancher ingress has an IP
-    wait_for_ingress_ip rancher cattle-system || exit 1
-
-    reset_rancher_admin_password || return $?
-}
-
-function set_rancher_server_url
-{
-    echo "Get Rancher admin password."
-    rancher_admin_password=$(kubectl get secret --namespace cattle-system rancher-admin-secret -o jsonpath={.data.password})
-    if [ $? -ne 0 ]; then
-      echo "Failed to get Rancher admin password. Continuing without setting Rancher server URL."
-      return 0
-    fi
-    rancher_admin_password=$(echo ${rancher_admin_password} | base64 --decode)
-    if [ $? -ne 0 ]; then
-      echo "Failed to decode Rancher admin password. Continuing without setting Rancher server URL."
-      return 0
-    fi
-    echo "Get Rancher access token."
-    get_rancher_access_token "${RANCHER_HOSTNAME}" "${rancher_admin_password}"
-    if [ $? -ne 0 ] ; then
-      echo "Failed to get Rancher access token. Continuing without setting Rancher server URL."
-      return 0
-    fi
-
-    if [ -z "${RANCHER_ACCESS_TOKEN}" ]; then
-      echo "Failed to get valid Rancher access token. Continuing without setting Rancher server URL."
-      return 0
-    fi
-    echo "Set Rancher server URL to https://${RANCHER_HOSTNAME}"
-    curl_args=("https://${RANCHER_HOSTNAME}:$(get_nginx_nodeport)/v3/settings/server-url" $(get_rancher_resolve ${RANCHER_HOSTNAME}) \
-          -H 'content-type: application/json' \
-          -H "Authorization: Bearer ${RANCHER_ACCESS_TOKEN}" \
-          -X PUT \
-          --data-binary '{"name":"server-url","value":"'https://${RANCHER_HOSTNAME}'"}' \
-          --insecure)
-    call_curl 200 http_response http_status curl_args || true
-    if [ ${http_status:--1} -ne 200 ]; then
-      echo "Failed to set Rancher server URL. Continuing without setting Rancher server URL."
-      return 0
-    else
-      echo "Successfully set Rancher server URL."
-    fi
-}
-
-function wait_for_rancher_agent_to_exist {
-    retries=0
-    until kubectl -n cattle-system get deploy | grep cattle-cluster-agent; do
-      retries=$(($retries+1))
-      sleep 2
-      if [ "$retries" -ge 30 ] ; then
-        break
-      fi
-    done
-}
-
-function patch_rancher_agents() {
-    local rancher_in_cluster_host=$(get_rancher_in_cluster_host ${RANCHER_HOSTNAME})
-
-    if [ ${RANCHER_HOSTNAME} != ${rancher_in_cluster_host} ]; then
-        local patch_data='{"spec":{"template":{"spec":{"hostAliases":[{"hostnames":["'"${RANCHER_HOSTNAME}"'"],"ip":"'"${rancher_in_cluster_host}"'"}]}}}}'
-
-        wait_for_rancher_agent_to_exist
-
-        # only when cattle-cluster-agent is deployed
-        kubectl -n cattle-system get deploy/cattle-cluster-agent
-        if [ $? -eq 0 ]; then
-            echo "cattle-cluster-agent is deployed.  Continue with patching cattle-cluster-agent."
-            kubectl -n cattle-system patch deployments cattle-cluster-agent --patch ${patch_data}
-        else
-            echo "cattle-cluster-agent is not deployed.  Skip patching."
-        fi
-
-        # only when cattle-node-agent is deployed
-        kubectl -n cattle-system get daemonset/cattle-node-agent
-        if [ $? -eq 0 ]; then
-            echo "cattle-node-agent is deployed.  Continue with patching cattle-node-agent."
-            kubectl -n cattle-system patch daemonsets cattle-node-agent --patch ${patch_data}
-        else
-            echo "cattle-node-agent is not deployed.  Skip patching."
-        fi
-    else
-        echo "Rancher host is the same from inside and outside the cluster.  No need to patch agents."
-    fi
-}
-
 function kubectl_apply_with_retry() {
   local count=0
   local ret=0
@@ -456,21 +177,10 @@ set -eu
 # We can only know the ingress IP after installing nginx ingress controller
 INGRESS_IP=$(get_verrazzano_ingress_ip)
 
-# DNS_SUFFIX is only used by install_rancher
 DNS_SUFFIX=$(get_dns_suffix ${INGRESS_IP})
-
-RANCHER_HOSTNAME=rancher.${NAME}.${DNS_SUFFIX}
 
 # Always create the cattle-system namespace so we can create network policies
 action "Creating cattle-system namespace" create_cattle_system_namespace || exit 1
-
-# Copy the optional global registry secret to the cattle-system namespace for pulling images from a private registry
-if [ "${REGISTRY_SECRET_EXISTS}" == "TRUE" ]; then
-  if ! kubectl get secret ${GLOBAL_IMAGE_PULL_SECRET} -n cattle-system > /dev/null 2>&1 ; then
-    action "Copying ${GLOBAL_IMAGE_PULL_SECRET} secret to cattle-system namespace" \
-    copy_registry_secret "cattle-system"
-  fi
-fi
 
 platform_operator_install_message "Installing cert-manager"
 if [ "$DNS_TYPE" == "oci" ]; then
@@ -478,7 +188,6 @@ if [ "$DNS_TYPE" == "oci" ]; then
 fi
 
 if [ $(is_rancher_enabled) == "true" ]; then
-  action "Installing Rancher" install_rancher || exit 1
-  action "Setting Rancher Server URL" set_rancher_server_url || true
-  action "Patching Rancher Agents" patch_rancher_agents || true
+  platform_operator_install_message "Installing Rancher"
+  action "Wait for Rancher availability" wait_for_rancher || exit 1
 fi
--- platform-operator/scripts/install/reset-rancher-password.sh
+++ platform-operator/scripts/install/reset-rancher-password.sh
@@ -1,2 +1,7 @@
+#!/usr/bin/env bash
+#
 # Copyright (c) 2021, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+#
+
+kubectl exec "$1" -n "$2" -- reset-password | tail -1
