b35d915b4961235024e49a1324d115e9231f990f
Shashank Kumar <shashank.z.kumar@oracle.com>
2021-11-26 06:37:55 +0000

    Updated scripts to push metrics

---
+++ tools/scripts/create_lre_tests_job.sh
@@ -0,0 +1,206 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2021, Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+#
+# This script can be used to execute compiled test binaries as a kubernetes job in a cluster. 
+# The target cluster where the tests will run can be different than where the job is being executed.
+
+SCRIPT_DIR=$(cd $(dirname "$0"); pwd -P)
+
+function usage {
+    echo
+    echo "Usage:"
+    echo " $0 [-f TEST_BINARIES] [-i JOB_IMAGE]"
+    echo "Arguments:"
+    echo "  -i, --job-image               The docker image of of the job."
+    echo "  -j, --job-name                (Optional) The name of of the job."
+    echo "  -n, --job-namespace           (Optional) The namespace of of the job."
+    echo "  -s, --source-kubeconfig       (Optional) Path to Kubeconfig of cluster where job will run. If not provided, env variable KUBECONFIG is used."
+    echo "  -t, --target-kubeconfig       (Optional) Path to Kubeconfig of cluster where tests wll be run. If not provided, --source-kubeconfig is used."
+    echo "  -b, --oci-bucket              (Optional) Test log bucket. Required for pushing logs to object storage. Please make sure OCI CLI env variables are set."
+    echo "  -p, --prometheus-pushgateway  (Optional) URL to prometheus pushgateway for pushing metrics. It MUST not have a trailing slash ('/')." 
+    echo "  -o, --prometheus-job          (Optional) Job name for prometheus metrics."
+    echo "  -m, --prometheus-instace      (Optional) Instace name for prometheus metrics."
+    echo "  -u, --sauron-cred             (Optional) Sauron credentials in the format: <username>:<password>"
+    echo "  -r, --regex-filter            (Optional) The regex filter for tests used by Ginkgo."
+    echo "  -l, --test-log-archive        (Optional) The name of test log archive file."
+    echo "  -v, --verbose                 (Optional) Turn on verbose output."
+    echo "  -h, --help                    Print usage information."
+    echo
+    exit 1
+}
+
+SOURCE_KUBECONFIG=""
+TARGET_KUBECONFIG=""
+JOB_NAME=""
+JOB_NAMESPACE=""
+JOB_IMAGE=""
+TEST_BINARIES="/tmp/verrazzano/test-binaries"
+PROMETHEUS_GW_URL=""
+PROMETHEUS_JOB=""
+PROMETHEUS_INSTANCE=""
+SAURON_CRED=""
+OCI_BUCKET=""
+TEST_REGEX=""
+TEST_LOG_ARCHIVE=""
+VERBOSE=""
+TS=`date "+%Y%m%d-%H%M%S%s"`
+JOB_NAME_DEFAULT="testjob-${TS}"
+JOB_NAMESPACE_DEFAULT="lre-tests"
+
+# Parse the command line arguments
+while [ $# -gt 0 ]
+do
+    key="$1"
+    case ${key} in 
+        -s|--source-kubeconfig)        SOURCE_KUBECONFIG="$2"; shift; shift;;
+        -t|--target-kubeconfig)        TARGET_KUBECONFIG="$2"; shift; shift;;
+        -j|--job-name)                 JOB_NAME="$2"; shift; shift;;
+        -n|--job-namespace)            JOB_NAMESPACE="$2"; shift; shift;;
+        -i|--job-image)                JOB_IMAGE="$2"; shift; shift;;
+        -b|--oci-bucket)               OCI_BUCKET="$2"; shift; shift;;
+        -p|--prometheus-pushgateway)   PROMETHEUS_GW_URL="$2"; shift; shift;;
+        -o|--prometheus-job)           PROMETHEUS_JOB="$2"; shift; shift;;
+        -m|--prometheus-instance)      PROMETHEUS_INSTANCE="$2"; shift; shift;;
+        -u|--sauron-cred)              SAURON_CRED="$2"; shift; shift;;
+        -r|--regex-filter)             TEST_REGEX="$2"; shift; shift;;
+        -l|--test-log-archive)         TEST_LOG_ARCHIVE="$2"; shift; shift;;
+        -v|--verbose)                  VERBOSE=true; shift;;
+        -h|--help)                     usage;;
+        *)                             usage;;
+    esac
+done
+
+SOURCE_KUBECONFIG=${SOURCE_KUBECONFIG:-$KUBECONFIG}
+if [ -z "${SOURCE_KUBECONFIG}" ]
+then
+    echo "ERROR: Either --source-kubeconfig should be provided or env variable KUBECONFIG should be set."
+    exit 1
+elif [ -z "${JOB_IMAGE}" ]
+then
+    echo "ERROR: Missing required argument: -i, --job-image"
+    usage
+fi
+
+# Create namespace
+if [ -z "${TARGET_KUBECONFIG}" ]
+then
+    TARGET_KUBECONFIG=${SOURCE_KUBECONFIG}
+fi
+if [ -z "${JOB_NAME}" ]
+then
+    JOB_NAME=${JOB_NAME_DEFAULT}
+fi
+if [ -z "${JOB_NAMESPACE}" ]
+then
+    JOB_NAMESPACE=${JOB_NAMESPACE_DEFAULT}
+fi
+export KUBECONFIG=${SOURCE_KUBECONFIG}
+kubectl create namespace ${JOB_NAMESPACE}
+
+# Create configmap
+mkdir -p /tmp/${JOB_NAME}
+cp ${TARGET_KUBECONFIG} /tmp/${JOB_NAME}/kubeconfig
+cp ${SCRIPT_DIR}/run_compiled_tests.sh /tmp/${JOB_NAME}
+CONFIGMAP_NAME="${JOB_NAME}-configmap"
+kubectl create configmap ${CONFIGMAP_NAME} -n ${JOB_NAMESPACE} --from-file=/tmp/${JOB_NAME}
+
+# Command to execute in the pod
+CMDLINE="sleep 5s && \
+        cd /tmp && \
+        git clone https://github.com/verrazzano/verrazzano && \
+        mkdir -p ${TEST_BINARIES} && \
+        ./lre-config/run_compiled_tests.sh --test-binaries ${TEST_BINARIES} --kubeconfig-location /tmp/lre-config/kubeconfig"
+
+# Add arguments for the script - run_lre_tests.sh
+if [ ! -z "${PROMETHEUS_GW_URL}" ]
+then
+    CMDLINE="${CMDLINE} --prometheus-pushgateway ${PROMETHEUS_GW_URL}"
+fi
+if [ ! -z "${PROMETHEUS_JOB}" ]
+then
+    CMDLINE="${CMDLINE} --prometheus-job ${PROMETHEUS_JOB}"
+fi
+if [ ! -z "${PROMETHEUS_INSTANCE}" ]
+then
+    CMDLINE="${CMDLINE} --prometheus-instance ${PROMETHEUS_INSTANCE}"
+fi
+if [ ! -z "${SAURON_CRED}" ]
+then
+    CMDLINE="${CMDLINE} --sauron-cred ${SAURON_CRED}"
+fi
+if [ ! -z "${TEST_REGEX}" ]
+then
+    CMDLINE="${CMDLINE} --test-regex ${TEST_REGEX}"
+fi
+if [ ! -z "${TEST_LOG_ARCHIVE}" ]
+then
+    CMDLINE="${CMDLINE} --test-log-archive ${TEST_LOG_ARCHIVE}"
+fi
+if [ ! -z "${VERBOSE}" ]
+then
+    CMDLINE="${CMDLINE} --verbose"
+fi
+
+# Create OCI secret
+SECRET_NAME="oci"
+if [ ! -z "${OCI_BUCKET}" ]
+then
+    kubectl get secret ${SECRET_NAME} -n ${JOB_NAMESPACE} > /dev/null 2>&1
+    if [ $? -ne 0 ]
+    then
+      ${SCRIPT_DIR}/../../platform-operator/scripts/install/create_oci_config_secret.sh ${JOB_NAMESPACE}
+    fi
+    CMDLINE="${CMDLINE} --oci-bucket ${OCI_BUCKET}"
+fi
+
+kubectl apply -f - <<-EOF
+apiVersion: batch/v1
+kind: Job
+metadata:
+  name: ${JOB_NAME}
+  namespace: ${JOB_NAMESPACE}
+spec:
+  template:
+    metadata:
+      name: ${JOB_NAME}
+    spec:
+      containers:
+        - name: ${JOB_NAME}
+          image: ${JOB_IMAGE}
+          volumeMounts:
+            - name: ocisecret
+              mountPath: /var/run/secrets/ocisecret
+            - name: lre-config
+              mountPath: /tmp/lre-config
+          command: [ "/bin/sh", "-c"]
+          args: [${CMDLINE}]
+      restartPolicy: Never
+      volumes:
+      - name: ocisecret
+        secret:
+          secretName: ${SECRET_NAME}
+          optional: true
+      - name: lre-config
+        projected:
+          sources:
+            - configMap:
+                name: ${CONFIGMAP_NAME}
+                items:
+                  - key: kubeconfig
+                    path: kubeconfig
+                    mode: 0755
+                  - key: run_compiled_tests.sh
+                    path: run_compiled_tests.sh
+                    mode: 0755
+EOF
+
+kubectl wait -n ${JOB_NAMESPACE} --for=condition=ContainersReady --timeout=100s pod --selector job-name=${JOB_NAME}
+
+POD_NAME=$(kubectl get pod -n ${JOB_NAMESPACE} -l job-name=${JOB_NAME} -o jsonpath="{.items[0].metadata.name}") 
+echo
+echo "Copy test binaries using the following command:"
+echo "kubectl cp -n ${JOB_NAMESPACE} <path-to-binaries-folder> ${POD_NAME}:${TEST_BINARIES}"
+echo
+kubectl logs -f -n ${JOB_NAMESPACE} -f ${POD_NAME}
--- tools/scripts/run_compiled_tests.sh
+++ tools/scripts/run_compiled_tests.sh
@@ -3,167 +3,321 @@
 # Copyright (c) 2021, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 #
-# This script can be used to execute a compiled test binary against a verrazzano cluster and (optionally) push the test logs
-# and report to objectstorage.
-#
+# This script can be used to execute a compiled test binary against a verrazzano cluster, extract and send the metrics to prometheus, 
+# and (optionally) push the test logs and report to objectstorage.
 
 SCRIPT_DIR=$(cd $(dirname "$0"); pwd -P)
+
 function usage {
     echo
-    echo "usage: $0 -t test_binary_url_or_file -k kubeconfig_text -c kubeconfig_url_or_file -r regex_filter_for_tests -o test_logs_os_bucket -l test_log_archive_name"
-    echo "  -t test_binary_url_or_file The url or path of test binary file"
-    echo "  -k kubeconfig_text         kubeconfig text"
-    echo "  -c kubeconfig_url_or_file  The url or path of kubeconfig"
-    echo "  -r regex_filter_for_tests  The regex filter for tests"
-    echo "  -o test_logs_os_bucket     The OS Bucket name for test logs, please make sure OCI CLI env variables are set."
-    echo "  -l test_log_archive_name   The name of test log archive file"
-    echo "  -h help                    show this help"
+    echo "Usage:"
+    echo " $0 [-f TEST_BINARIES]"
+    echo "Arguments:"
+    echo "  -f, --test-binaries           The path to the compiled test file or the folder containing compiled test binaries."
+    echo "  -p, --prometheus-pushgateway  (Optional) URL to prometheus pushgateway for pushing metrics. It MUST not have a trailing slash ('/')." 
+    echo "  -o, --prometheus-job          (Optional) Job name for prometheus metrics."
+    echo "  -m, --prometheus-instace      (Optional) Instace name for prometheus metrics."
+    echo "  -u, --sauron-cred             (Optional) Sauron credentials in the format: <username>:<password>"
+    echo "  -b, --oci-bucket              (Optional) Test log bucket. Required for pushing logs to object storage. Please make sure OCI CLI env variables are set."
+    echo "  -k, --kubeconfig-text         (Optional) Kubeconfig text."
+    echo "  -c, --kubeconfig-location     (Optional) The url or path to the kubeconfig file."
+    echo "  -r, --regex-filter            (Optional) The regex filter for tests used by Ginkgo."
+    echo "  -l, --test-log-archive        (Optional) The name of test log archive file."
+    echo "  -v, --verbose                 (Optional) Turn on verbose output."
+    echo "  -h, --help                    Print usage information."
     echo
     exit 1
 }
 
-is_url() {
+function is_url {
     regex='^(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]\.[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]$'
     [[ $1 =~ $regex ]]
 }
 
-file_exists_at_url() {
+function file_exists_at_url {
     wget --spider $1 2>/dev/null
 }
 
-file_exists() {
+function file_exists {
     [[ -f "$1" && -s "$1" ]]
 }
 
-TEST_BINARY=""
+function print {
+    if [ ${VERBOSE} ]
+    then
+        echo "$1" | tee -a "${TEST_LOG_FILE}"
+    else
+        echo "$1" | cat >> "${TEST_LOG_FILE}"
+    fi
+}
+
+function execute_tests_recursively {
+    for FILE_NAME in `ls $1`
+    do
+        local FILE_PATH="$1/${FILE_NAME}"
+        local FILE_NAME_NO_EXTENSION=${FILE_NAME%.*}
+        local EXTENSION=${FILE_PATH##*.}
+        if [ -d "${FILE_PATH}" ]
+        then 
+            execute_tests_recursively ${FILE_PATH}
+        elif file_exists "${FILE_PATH}" && [[ -x ${FILE_PATH} && ${EXTENSION} = "test" ]]
+        then
+            local LOG_FILE="/tmp/${TEST_LOG_ARCHIVE}/${FILE_NAME_NO_EXTENSION}.log"
+            ginkgo -v --no-color --keep-going --trace --focus="${TEST_REGEX}" ${FILE_PATH} > ${LOG_FILE} &
+            local PID="$!"
+            PIDS+=(${PID})
+            LOG_FILES+=(${LOG_FILE})
+            print "INFO: Running test file (PID: ${PID}): ${FILE_PATH}"
+            print "INFO: Created log file: ${LOG_FILE}"
+        fi
+    done
+}
+
+function update_prometheus_metricsfile {
+    local METRICS=($(cat ${TEST_TEMP_FILE} | grep -E 'Ran [0-9]+ of [0-9]+ Specs' | awk -F ' ' '{sum_specs_run+=$2; sum_specs_total+=$4;} END {print sum_specs_run, sum_specs_total;}'))
+    echo "#HELP lre_tests_specs_total Total number of specs in the test suites" > ${TEST_METRICS_FILE}
+    echo "#TYPE lre_tests_specs_total gauge" >> ${TEST_METRICS_FILE}
+    echo "lre_tests_specs_total ${METRICS[1]}" >> ${TEST_METRICS_FILE}
+    
+    echo "#HELP lre_tests_specs_run Total number of specs run in the test suites" >> ${TEST_METRICS_FILE}
+    echo "#TYPE lre_tests_specs_run gauge" >> ${TEST_METRICS_FILE}
+    echo "lre_tests_specs_run ${METRICS[0]}" >> ${TEST_METRICS_FILE}
+
+    METRICS=($(cat ${TEST_TEMP_FILE} | grep -E '(SUCCESS|FAIL)! -- [0-9]+ Passed \| [0-9]+ Failed \| [0-9]+ Pending \| [0-9]+ Skipped' | awk '{split($0, a, "! -- "); gsub(/\|/, "", a[2]); print a[2]}' | awk -F ' ' '{sum_passed+=$1; sum_failed+=$3; sum_pending+=$5; sum_skipped+=$7}  END {print sum_passed, sum_failed, sum_pending, sum_skipped;}')) 
+    echo "#HELP lre_tests_specs_passed Total number of specs passed" >> ${TEST_METRICS_FILE}
+    echo "#TYPE lre_tests_specs_passed gauge" >> ${TEST_METRICS_FILE}
+    echo "lre_tests_specs_passed ${METRICS[0]}" >> ${TEST_METRICS_FILE}
+    
+    echo "#HELP lre_tests_specs_failed Total number of specs failed" >> ${TEST_METRICS_FILE}
+    echo "#TYPE lre_tests_specs_failed gauge" >> ${TEST_METRICS_FILE}
+    echo "lre_tests_specs_failed ${METRICS[1]}" >> ${TEST_METRICS_FILE}
+    
+    echo "#HELP lre_tests_specs_pending Total number of specs pending" >> ${TEST_METRICS_FILE}
+    echo "#TYPE lre_tests_specs_pending gauge" >> ${TEST_METRICS_FILE}
+    echo "lre_tests_specs_pending ${METRICS[2]}" >> ${TEST_METRICS_FILE}
+    
+    echo "#HELP lre_tests_specs_skipped Total number of specs skipped" >> ${TEST_METRICS_FILE}
+    echo "#TYPE lre_tests_specs_skipped gauge" >> ${TEST_METRICS_FILE}
+    echo "lre_tests_specs_skipped ${METRICS[3]}" >> ${TEST_METRICS_FILE}
+
+    METRICS=$(cat ${TEST_TEMP_FILE} | grep -E 'Ginkgo ran [0-9]+ suite' | awk -F ' ' '{sum_suite+=$3;} END {print sum_suite;}')
+    echo "#HELP lre_tests_suites_run Total number of test suites run" >> ${TEST_METRICS_FILE}
+    echo "#TYPE lre_tests_suites_run gauge" >> ${TEST_METRICS_FILE}
+    echo "lre_tests_suites_run ${METRICS}" >> ${TEST_METRICS_FILE}
+}
+
+TEST_BINARIES=""
+PROMETHEUS_GW_URL=""
+PROMETHEUS_JOB=""
+PROMETHEUS_INSTANCE=""
+PROMETHEUS_PUSH_METRICS=""
+SAURON_CRED=""
 KUBECONFIG_TEXT=""
 KUBECONFIG_LOCATION=""
+OCI_BUCKET=""
+OCI_CONFIG_LOCATION="/var/run/secrets/ocisecret/oci.yaml"
+OCI_PUSH_LOGS=false
 TEST_REGEX=""
-TEST_LOG_BUCKET=""
-TEST_LOG_ARCHIVE=""
 TEST_REGEX_DEFAULT=".*"
+TEST_LOG_ARCHIVE=""
 TS=`date "+%Y%m%d-%H%M%S%ss"`
-TEST_LOG_ARCHIVE_DEFAULT="test-output-${TS}"
+TEST_LOG_ARCHIVE_DEFAULT="lre-tests-logs-${TS}"
+VERBOSE=""
+SLEEP_DURATION="1m"
 
-
-while getopts t:k:c:r:o:l:h flag
+# Parse the command line arguments
+while [ $# -gt 0 ]
 do
-    case "${flag}" in
-        t) TEST_BINARY=${OPTARG};;
-        k) KUBECONFIG_TEXT=${OPTARG};;
-        c) KUBECONFIG_LOCATION=${OPTARG};;
-        r) TEST_REGEX=${OPTARG};;
-        o) TEST_LOG_BUCKET=${OPTARG};;
-        l) TEST_LOG_ARCHIVE=${OPTARG};;
-        h) usage;;
-        *) usage;;
+    key="$1"
+    case ${key} in 
+        -f|--test-binaries)            TEST_BINARIES="${2%\/}"; shift; shift;;
+        -b|--oci-bucket)               OCI_BUCKET="$2"; shift; shift;;
+        -p|--prometheus-pushgateway)   PROMETHEUS_GW_URL="$2"; shift; shift;;
+        -o|--prometheus-job)           PROMETHEUS_JOB="$2"; shift; shift;;
+        -m|--prometheus-instance)      PROMETHEUS_INSTANCE="$2"; shift; shift;;
+        -u|--sauron-cred)              SAURON_CRED="$2"; shift; shift;;
+        -k|--kubeconfig-text)          KUBECONFIG_TEXT="$2"; shift; shift;;
+        -c|--kubeconfig-location)      KUBECONFIG_LOCATION="$2"; shift; shift;;
+        -r|--regex-filter)             TEST_REGEX="$2"; shift; shift;;
+        -l|--test-log-archive)         TEST_LOG_ARCHIVE="$2"; shift; shift;;
+        -v|--verbose)                  VERBOSE=true; shift;;
+        -h|--help)                     usage;;
+        *)                             usage;;
     esac
 done
 
-if [ -z "${TEST_BINARY}" ] ; then
-    echo "TEST_BINARY must be set!"
-    exit 1
-fi
-
-if is_url "${TEST_BINARY}"
+if [ -z "${TEST_BINARIES}" ]
 then
-    if file_exists_at_url "${TEST_BINARY}"
+    echo "ERROR: Missing required argument: -f, --test-binaries"
+    usage
+elif [[ ! -d "${TEST_BINARIES}" ]] && ! file_exists "${TEST_BINARIES}"
+then 
+    echo "ERROR: No such file or folder exists: ${TEST_BINARIES}"
+    exit 1
+elif [[ ! -z "${PROMETHEUS_GW_URL}" || ! -z "${PROMETHEUS_JOB}" || ! -z "${PROMETHEUS_INSTANCE}" || ! -z "${SAURON_CRED}" ]]
+then
+    if [ -z "${PROMETHEUS_GW_URL}" ]
     then
-      FILE_NAME=`basename "${TEST_BINARY}"`
-      rm -rf /tmp/$FILE_NAME
-      wget "${TEST_BINARY}" -O "/tmp/$FILE_NAME"
-      TEST_BINARY="/tmp/$FILE_NAME"
+        echo "ERROR: Missing required argument: -p, --prometheus-pushgateway"
+        usage
+    elif [ -z "${PROMETHEUS_JOB}" ]
+    then
+        echo "ERROR: Missing required argument: -o, --prometheus-job"
+        usage
+    elif [ -z "${PROMETHEUS_INSTANCE}" ]
+    then
+        echo "ERROR: Missing required argument: -m, --prometheus-instance"
+        usage
+    elif [ -z "${SAURON_CRED}" ]
+    then
+        echo "ERROR: Missing required argument: -u, --sauron-cred"
+        usage
     else
-      echo "No file exists at url specified by ${TEST_BINARY}!"
-      exit 1
+        PROMETHEUS_PUSH_METRICS=true
     fi
 fi
 
-if file_exists "${TEST_BINARY}"
-then
-  echo "Using TEST Binary fie at ${TEST_BINARY}"
-else
-  echo "No file exists at ${TEST_BINARY} or is empty!"
-  exit 1
+if [ -z "${TEST_REGEX}" ]; then
+    TEST_REGEX="${TEST_REGEX_DEFAULT}"
 fi
 
+# Fetch kubeconfig from the given URL
 if is_url "${KUBECONFIG_LOCATION}"
 then
     if file_exists_at_url "${KUBECONFIG_LOCATION}"
     then
-      FILE_NAME=`basename "${KUBECONFIG_LOCATION}"`
-      rm -rf /tmp/$FILE_NAME
-      wget "${KUBECONFIG_LOCATION}" -O "/tmp/$FILE_NAME"
-      KUBECONFIG_LOCATION="/tmp/$FILE_NAME"
+        local FILE_NAME=`basename "${KUBECONFIG_LOCATION}"`
+        rm -rf /tmp/${FILE_NAME}
+        wget "${KUBECONFIG_LOCATION}" -O "/tmp/${FILE_NAME}"
+        KUBECONFIG_LOCATION="/tmp/${FILE_NAME}"
     else
-      echo "No file exists at url specified by ${KUBECONFIG_LOCATION}!"
-      exit 1
+        echo "ERROR: No file exists at the URL specified: ${KUBECONFIG_LOCATION}"
+        exit 1
     fi
 fi
 
+# Get kubeconfig from local path
 if file_exists "${KUBECONFIG_LOCATION}"
 then
-  echo "Using Kubeconfig at ${KUBECONFIG_LOCATION}"
+    echo "INFO: Found Kubeconfig: ${KUBECONFIG_LOCATION}"
 else
-  if [ -z "${KUBECONFIG_TEXT}" ]
-  then
-    if [ ! -z "${KUBECONFIG}" ]
+    if [ -z "${KUBECONFIG_TEXT}" ]
     then
-      echo "Using KUBECONFIG from env variable with value ${KUBECONFIG}"
-      KUBECONFIG_LOCATION="${KUBECONFIG}"
+        if [ ! -z "${KUBECONFIG}" ]
+        then
+            echo "INFO: Using KUBECONFIG value from env variable: ${KUBECONFIG}"
+            KUBECONFIG_LOCATION="${KUBECONFIG}"
+        else
+            echo "ERROR: Either provide --kubeconfig-text, or a valid --kubeconfig-location, or set the KUBECONFIG env variable to a valid kubeconfig"
+            exit 1
+        fi
     else
-      echo "KUBECONFIG_TEXT must be set or KUBECONFIG_LOCATION should point to a valid kubeconfig or there should be a valid KUBECONFIG env variable!"
-      exit 1
+        KUBECONFIG_LOCATION="/tmp/kubeconfig"
+        rm -rf "${KUBECONFIG_LOCATION}"
+        touch "${KUBECONFIG_LOCATION}"
+        echo "${KUBECONFIG_TEXT}" > "${KUBECONFIG_LOCATION}"
     fi
-  else
-    KUBECONFIG_LOCATION="/tmp/kubeconfig"
-    rm -rf "${KUBECONFIG_LOCATION}"
-    touch "${KUBECONFIG_LOCATION}"
-    echo "${KUBECONFIG_TEXT}" > "${KUBECONFIG_LOCATION}"
-  fi  
 fi
 
-if [ -z "${TEST_REGEX}" ]; then
-  TEST_REGEX="${TEST_REGEX_DEFAULT}"
-fi
-
-if [ -z "${TEST_LOG_ARCHIVE}" ]; then
-  TEST_LOG_ARCHIVE="${TEST_LOG_ARCHIVE_DEFAULT}"
-fi
-
-rm -rf /tmp/${TEST_LOG_ARCHIVE}
-mkdir -p /tmp/${TEST_LOG_ARCHIVE}
-touch /tmp/${TEST_LOG_ARCHIVE}/test.log
 chmod a+x "${KUBECONFIG_LOCATION}"
-chmod a+x "${TEST_BINARY}"
 export KUBECONFIG="${KUBECONFIG_LOCATION}"
-# Remove a warning that suggests using Ginkgo 2.0
-export ACK_GINKGO_DEPRECATIONS=1.16.5
-ginkgo -v -keepGoing --reportFile="/tmp/${TEST_LOG_ARCHIVE}/test.report" -outputdir="/tmp/${TEST_LOG_ARCHIVE}" --trace --focus="${TEST_REGEX}" "${TEST_BINARY}" | tee /tmp/${TEST_LOG_ARCHIVE}/test.log
 
-if [ ! -z "${TEST_LOG_BUCKET}" ]; then
-  tar -czvf /tmp/${TEST_LOG_ARCHIVE}.tgz /tmp/${TEST_LOG_ARCHIVE}/*
-  # When this script is run in a k8s job, it expects the OCI creds to be present in /var/run/secrets/ocisecret/oci.yaml. 
-  if file_exists "/var/run/secrets/ocisecret/oci.yaml"
-  then
-    export OCI_CLI_PROFILE="DEFAULT"
-    export OCI_CLI_USER=`yq e .auth.user /var/run/secrets/ocisecret/oci.yaml`
-    export OCI_CLI_FINGERPRINT=`yq e .auth.fingerprint /var/run/secrets/ocisecret/oci.yaml`
-    yq e .auth.key /var/run/secrets/ocisecret/oci.yaml > /tmp/ocikey.pem
-    export OCI_CLI_KEY_FILE=/tmp/ocikey.pem
-    export OCI_CLI_TENANCY=`yq e .auth.tenancy /var/run/secrets/ocisecret/oci.yaml`
-    export OCI_CLI_REGION=`yq e .auth.region /var/run/secrets/ocisecret/oci.yaml`
-    export OCI_CLI_SUPPRESS_FILE_PERMISSIONS_WARNING="True"
-  fi
-  oci os object put --bucket-name "${TEST_LOG_BUCKET}" --file /tmp/${TEST_LOG_ARCHIVE}.tgz
-fi
-
-status=`sed -n -e '$p' /tmp/${TEST_LOG_ARCHIVE}/test.log`
-if [ "$status" == "Test Suite Failed" ]
+# Set OCI config env variables 
+if [ ! -z "${OCI_BUCKET}" ]
 then
-  echo "Tests Failed."
-  exit 1
-else
-  echo "Tests Passed"
-  exit 0
+    if [ ! -f "${OCI_CONFIG_LOCATION}" ]
+    then
+        echo "ERROR: OCI config file does not exist at: ${OCI_CONFIG_LOCATION}"
+        exit 1
+    else
+        echo "INFO: Setting OCI env variables"
+        export OCI_CLI_PROFILE="DEFAULT"
+        export OCI_CLI_USER=`yq e .auth.user ${OCI_CONFIG_LOCATION}`
+        export OCI_CLI_FINGERPRINT=`yq e .auth.fingerprint ${OCI_CONFIG_LOCATION}`
+        yq e .auth.key ${OCI_CONFIG_LOCATION} > /tmp/ocikey.pem
+        export OCI_CLI_KEY_FILE=/tmp/ocikey.pem
+        export OCI_CLI_TENANCY=`yq e .auth.tenancy ${OCI_CONFIG_LOCATION}`
+        export OCI_CLI_REGION=`yq e .auth.region ${OCI_CONFIG_LOCATION}`
+        export OCI_CLI_SUPPRESS_FILE_PERMISSIONS_WARNING="True"
+        OCI_PUSH_LOGS=true
+    fi
 fi
+
+# Create directory for logs 
+if [ -z "${TEST_LOG_ARCHIVE}" ]
+then
+    TEST_LOG_ARCHIVE=${TEST_LOG_ARCHIVE_DEFAULT}
+fi
+rm -rf "/tmp/${TEST_LOG_ARCHIVE}"
+mkdir -p "/tmp/${TEST_LOG_ARCHIVE}"
+
+TEST_LOG_FILE="/tmp/${TEST_LOG_ARCHIVE}/logs.txt"
+TEST_TEMP_FILE="/tmp/${TEST_LOG_ARCHIVE}/tmp.txt"
+TEST_METRICS_FILE="/tmp/${TEST_LOG_ARCHIVE}/metrics.txt"
+echo -n "" > ${TEST_LOG_FILE}
+echo -n "" > ${TEST_TEMP_FILE}
+echo -n "" > ${TEST_METRICS_FILE}
+
+# Run the tests
+ITERATION=0
+while true
+do
+    print "----------------------------------"
+    print "INFO: Iteration: ${ITERATION}"
+
+    # Array to keep track of process IDs of the spawned background processes
+    PIDS=()
+    LOG_FILES=()
+
+    # Execute tests
+    execute_tests_recursively ${TEST_BINARIES}
+
+    if [[ ${#PIDS[@]} -eq 0 ]]
+    then
+        print "INFO: No test files found."
+    else
+        # Wait for all the spawned background processes to finish
+        print "INFO: Waiting for the tests to finish ..."
+        for PID in ${PIDS[@]}
+        do 
+            wait ${PID}
+        done
+
+        # Extract metrics from the logfiles
+        for LOG_FILE in ${LOG_FILES[@]}
+        do
+            cat ${LOG_FILE} \
+            | grep -E 'Running Suite:|Ran [0-9]+ of [0-9]+ Specs|(SUCCESS|FAIL)! -- [0-9]+ Passed \| [0-9]+ Failed \| [0-9]+ Pending \| [0-9]+ Skipped|Ginkgo ran [0-9]+ suite' \
+            >> ${TEST_TEMP_FILE}
+        done
+        cat ${TEST_TEMP_FILE} >> ${TEST_LOG_FILE}
+
+        # Send metrics to prometheus pushgateway
+        if [ "${PROMETHEUS_PUSH_METRICS}" = true ]
+        then
+            # Update prometheus metrics file
+            print "INFO: Updating prometheus metrics file: ${TEST_METRICS_FILE}"
+            update_prometheus_metricsfile
+            print "INFO: Sending metrics to prometheus pushgateway: ${PROMETHEUS_GW_URL}/metrics/job/${PROMETHEUS_JOB}"
+            cat ${TEST_METRICS_FILE} \
+            | curl -i --data-binary @- ${PROMETHEUS_GW_URL}/metrics/job/${PROMETHEUS_JOB}/instance/${PROMETHEUS_INSTANCE} -u ${SAURON_CRED}
+        fi
+
+        # Push logs to object store
+        if [ "${OCI_PUSH_LOGS}" = true ]
+        then
+            print "INFO: Compressing directory /tmp/${TEST_LOG_ARCHIVE}"
+            tar -czvf "/tmp/${TEST_LOG_ARCHIVE}.tgz" "/tmp/${TEST_LOG_ARCHIVE}"
+            print "INFO: Pushing ${TEST_LOG_ARCHIVE}.tgz to object storage"
+            yes | oci os object put --bucket-name "${OCI_BUCKET}" --file "/tmp/${TEST_LOG_ARCHIVE}.tgz"
+        fi
+    fi
+
+    echo -n "" > ${TEST_TEMP_FILE}
+    ((ITERATION++))
+
+    # Sleep after every iteration
+    print "INFO: Sleeping for ${SLEEP_DURATION}"
+    print "----------------------------------"
+    sleep ${SLEEP_DURATION}
+done
