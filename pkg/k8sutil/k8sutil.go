0599d8d3bec0c2ef60a82ea360d1d29642bff175
anders-swanson <91502735+anders-swanson@users.noreply.github.com>
2021-11-29 07:40:46 -0800

    VZ-4124: Remove remaining Rancher scripts (#2030)
    VZ 4124: Rancher shell script removal

--- pkg/k8sutil/k8sutil.go
+++ pkg/k8sutil/k8sutil.go
@@ -4,9 +4,14 @@
 package k8sutil
 
 import (
+	"bytes"
 	"context"
 	"errors"
 	"fmt"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/tools/remotecommand"
+	"net/url"
 	"os"
 	"path/filepath"
 
@@ -151,4 +156,66 @@ func GetHostnameFromGatewayInCluster(namespace string, appConfigName string, kub
 	// keep retrying and eventually we should get a gateway with a host
 	fmt.Printf("Could not find host in application ingress gateways in namespace: %s\n", namespace)
 	return "", nil
+}
+
+// NewPodExecutor is to be overridden during unit tests
+var NewPodExecutor = remotecommand.NewSPDYExecutor
+
+// FakePodSTDOUT can be used to output arbitrary strings during unit testing
+var FakePodSTDOUT = ""
+
+//NewFakePodExecutor should be used instead of remotecommand.NewSPDYExecutor in unit tests
+func NewFakePodExecutor(config *rest.Config, method string, url *url.URL) (remotecommand.Executor, error) {
+	return &fakeExecutor{method: method, url: url}, nil
+}
+
+// fakeExecutor is for unit testing
+type fakeExecutor struct {
+	method string
+	url    *url.URL
+}
+
+// Stream on a fakeExecutor sets stdout to FakePodSTDOUT
+func (f *fakeExecutor) Stream(options remotecommand.StreamOptions) error {
+	if options.Stdout != nil {
+		buf := new(bytes.Buffer)
+		buf.WriteString(FakePodSTDOUT)
+		if _, err := options.Stdout.Write(buf.Bytes()); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+//ExecPod runs a remote command a pod, returning the stdout and stderr of the command.
+func ExecPod(cfg *rest.Config, restClient rest.Interface, pod *v1.Pod, container string, command []string) (string, string, error) {
+	stdout := &bytes.Buffer{}
+	stderr := &bytes.Buffer{}
+	request := restClient.
+		Post().
+		Namespace(pod.Namespace).
+		Resource("pods").
+		Name(pod.Name).
+		SubResource("exec").
+		VersionedParams(&v1.PodExecOptions{
+			Container: container,
+			Command:   command,
+			Stdin:     false,
+			Stdout:    true,
+			Stderr:    true,
+			TTY:       true,
+		}, scheme.ParameterCodec)
+	executor, err := NewPodExecutor(cfg, "POST", request.URL())
+	if err != nil {
+		return "", "", err
+	}
+	err = executor.Stream(remotecommand.StreamOptions{
+		Stdout: stdout,
+		Stderr: stderr,
+	})
+	if err != nil {
+		return "", "", fmt.Errorf("error running command %s on %v/%v: %v", command, pod.Namespace, pod.Name, err)
+	}
+
+	return stdout.String(), stderr.String(), nil
 }
