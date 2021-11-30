724fe06a97d535bc4326ad239275511dd0eb4690
ddrechse <doug.drechsel@oracle.com>
2021-11-30 18:51:10 -0500

    Refactor to use Go Client for Exec commands.  Turn off unit test.  Needs to be reworked

--- platform-operator/controllers/verrazzano/component/keycloak/keycloak_component.go
+++ platform-operator/controllers/verrazzano/component/keycloak/keycloak_component.go
@@ -4,13 +4,15 @@ package keycloak
 
 import (
 	"context"
+	"fmt"
+	certmanager "github.com/jetstack/cert-manager/pkg/apis/certmanager/v1"
 	vzpassword "github.com/verrazzano/verrazzano/pkg/security/password"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/helm"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/istio"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/spi"
 	"github.com/verrazzano/verrazzano/platform-operator/internal/config"
-	"github.com/verrazzano/verrazzano/tests/e2e/pkg"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"path/filepath"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -89,21 +91,19 @@ func (c KeycloakComponent) PostInstall(ctx spi.ComponentContext) error {
 	if err != nil {
 		return err
 	}
-	pkg.CreatePasswordSecret("verrazzano-system", "verrazzano-prom-internal", prompw, nil)
+	err = createOrUpdateAuthSecret(ctx, "verrazzano-system", "verrazzano-prom-internal", "verrazzano-prom-internal", prompw)
 	if err != nil {
 		return err
 	}
-
 	// Create secret for the verrazzano-es-internal user
 	espw, err := vzpassword.GeneratePassword(15)
 	if err != nil {
 		return err
 	}
-	pkg.CreatePasswordSecret("verrazzano-system", "verrazzano-es-internal", espw, nil)
+	err = createOrUpdateAuthSecret(ctx, "verrazzano-system", "verrazzano-es-internal", "verrazzano-es-internal", espw)
 	if err != nil {
 		return err
 	}
-
 	// Create the verrazzano-system realm and populate it with users, groups, clients, etc.
 	err = configureKeycloakRealms(ctx, prompw, espw)
 	if err != nil {
@@ -122,22 +122,31 @@ func (c KeycloakComponent) PostUpgrade(ctx spi.ComponentContext) error {
 	return updateKeycloakUris(ctx)
 }
 
+func (c KeycloakComponent) IsEnabled(ctx spi.ComponentContext) bool {
+	return isKeycloakEnabled(ctx)
+}
+
+func (c KeycloakComponent) IsReady(ctx spi.ComponentContext) bool {
+	// TLS cert from Cert Manager should be in Ready state
+	certName := fmt.Sprintf("%s-secret", getEnvironmentName(ctx.EffectiveCR().Spec.EnvironmentName))
+	certificate := &certmanager.Certificate{}
+	namespacedName := types.NamespacedName{Name: certName, Namespace: ComponentName}
+	if err := ctx.Client().Get(context.TODO(), namespacedName, certificate); err != nil {
+		ctx.Log().Infof("CDD Keycloak Failed to get Keycloak Certificate: %s", err)
+		return false
+	}
+	if certificate.Status.Conditions == nil {
+		ctx.Log().Infof("CDD Keycloak No Certificate Status conditions")
+		return false
+	}
+	condition := certificate.Status.Conditions[0]
+	return condition.Type == "Ready"
+}
+
 func isKeycloakEnabled(ctx spi.ComponentContext) bool {
 	comp := ctx.EffectiveCR().Spec.Components.Keycloak
 	if comp == nil || comp.Enabled == nil {
 		return true
 	}
 	return *comp.Enabled
-}
-
-func (c KeycloakComponent) IsReady(ctx spi.ComponentContext) bool {
-	//   Wait for TLS cert from Cert Manager to go into a ready state   USE isREady
-	certName := "cert/" + getEnvironmentName(ctx.EffectiveCR().Spec.EnvironmentName) + "-secret"
-	cmd := execCommand("kubectl", "wait", certName, "-n", "keycloak", "--for=condition=Ready", "--timeout=5s")
-	out, err := cmd.Output()
-	if err != nil {
-		ctx.Log().Errorf("Keycloak component isReady returned false, TLS Cert not in Ready state: Error  = %s", out)
-		return false
-	}
-	return true
 }
