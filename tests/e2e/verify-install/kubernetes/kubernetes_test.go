0ce9095e80be08821ca8f106232ce2acd412bdb5
Bhavani Ravichandran <31928633+bhavaniravichandran@users.noreply.github.com>
2021-12-01 08:38:04 -0500

    Bhavani/useginkgorc2 (#2051)
    Update to ginkgo 2.0-rc2
    
    Co-authored-by: BHAVANI.RAVICHANDRAN@ORACLE.COM <bravicha>
    Co-authored-by: Mark Nelson <mark.x.nelson@oracle.com>

--- tests/e2e/verify-install/kubernetes/kubernetes_test.go
+++ tests/e2e/verify-install/kubernetes/kubernetes_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/verrazzano/verrazzano/pkg/k8sutil"
 	"time"
 
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/verrazzano/verrazzano/tests/e2e/pkg"
 	v1 "k8s.io/api/core/v1"
