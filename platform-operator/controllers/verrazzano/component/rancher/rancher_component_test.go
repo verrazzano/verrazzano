37965025b9166941195e9901007874f7a5e04be1
Paul Mackin <paul.mackin@oracle.com>
2021-12-01 09:26:52 -0500

    minor code move

--- platform-operator/controllers/verrazzano/component/rancher/rancher_component_test.go
+++ platform-operator/controllers/verrazzano/component/rancher/rancher_component_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/verrazzano/verrazzano/pkg/k8sutil"
 	vzapi "github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1alpha1"
 	"github.com/verrazzano/verrazzano/platform-operator/constants"
-	"github.com/verrazzano/verrazzano/platform-operator/controllers/common"
+	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/common"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/spi"
 	"io"
 	appsv1 "k8s.io/api/apps/v1"
