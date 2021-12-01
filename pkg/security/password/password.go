c0b11c81649a743dd97632b5352fa52d4491fa28
anders-swanson <91502735+anders-swanson@users.noreply.github.com>
2021-12-01 13:07:15 -0800

    VZ 3503: Tests  (#2061)
    Keycloak UTs

--- pkg/security/password/password.go
+++ pkg/security/password/password.go
@@ -1,14 +1,19 @@
 // Copyright (c) 2021, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
 package password
 
 import (
 	"crypto/rand"
 	b64 "encoding/base64"
+	"fmt"
 )
 
 // GeneratePassword will generate a password of length
 func GeneratePassword(length int) (string, error) {
+	if length < 1 {
+		return "", fmt.Errorf("cannot create password of length %d", length)
+	}
 	b := make([]byte, length)
 	_, err := rand.Read(b)
 	if err != nil {
