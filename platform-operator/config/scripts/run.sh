c3b3cdd2bc82eaabc7066e4687675bbc2d614477
ddrechse <doug.drechsel@oracle.com>
2021-12-07 09:23:50 -0500

    Remove script 4

--- platform-operator/config/scripts/run.sh
+++ platform-operator/config/scripts/run.sh
@@ -55,7 +55,6 @@ elif [ "${MODE}" == "INSTALL" ]; then
   create-kubeconfig
   ./platform-operator/scripts/install/1-install-init.sh || dump-install-logs 1
   ./platform-operator/scripts/install/2-install-system-components.sh || dump-install-logs 1
-  ./platform-operator/scripts/install/4-install-keycloak.sh || dump-install-logs 1
   dump-install-logs 0
 elif [ "${MODE}" == "UNINSTALL" ]; then
   # Create a kubeconfig and run the installation
