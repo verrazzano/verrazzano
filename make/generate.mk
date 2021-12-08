dc7cbeff57f059921e7e2a7374c6eb5b886b99d1
ben.mclean <ben.mclean@oracle.com>
2021-12-08 17:28:56 -0500

    new poko stuff

--- make/generate.mk
+++ make/generate.mk
@@ -15,10 +15,12 @@ platform-manifests: controller-gen
 application-manifests: controller-gen
 	$(CONTROLLER_GEN) $(CRD_OPTIONS) rbac:roleName=manager-role paths="./apis/clusters/..." output:crd:artifacts:config=$(CRD_PATH)
 	$(CONTROLLER_GEN) $(CRD_OPTIONS) rbac:roleName=manager-role paths="./apis/oam/..." output:crd:artifacts:config=$(CRD_PATH)
+	$(CONTROLLER_GEN) $(CRD_OPTIONS) rbac:roleName=manager-role paths="./apis/poko/..." output:crd:artifacts:config=$(CRD_PATH)
 	# Add copyright headers to the kubebuilder generated CRDs
 	./hack/add-crd-header.sh
 	./hack/update-codegen.sh "clusters:v1alpha1" "clusters" "boilerplate.go.txt"
 	./hack/update-codegen.sh "oam:v1alpha1" "oam" "boilerplate.go.txt"
+	./hack/update-codegen.sh "poko:v1alpha1" "poko" "boilerplate.go.txt"
 	# Add copyright headers to the kubebuilder generated manifests
 	./hack/add-yml-header.sh PROJECT
 
