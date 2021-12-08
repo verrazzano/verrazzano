c37a38d7f34cfdd15135a6675708de531f251afe
Fred Tibbitts <fred.tibbitts@oracle.com>
2021-12-08 15:16:56 -0500

    Fix bug with empty OCI Logging settings

--- platform-operator/controllers/verrazzano/component/verrazzano/verrazzano.go
+++ platform-operator/controllers/verrazzano/component/verrazzano/verrazzano.go
@@ -310,7 +310,6 @@ func findStorageOverride(effectiveCR *vzapi.Verrazzano) (*resourceRequestValues,
 func appendFluentdOverrides(effectiveCR *vzapi.Verrazzano, overrides *verrazzanoValues) {
 	overrides.Fluentd = &fluentdValues{
 		Enabled: vzconfig.IsFluentdEnabled(effectiveCR),
-		OCI:     &ociLoggingSettings{},
 	}
 
 	fluentd := effectiveCR.Spec.Components.Fluentd
