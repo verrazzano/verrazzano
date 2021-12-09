1e4cc027b168f6e883cfe4c6b701962adf338997
Abhishek Mitra <abhishek.a.mitra@oracle.com>
2021-12-09 12:06:16 -0800

    Private dns zone for oci

--- platform-operator/controllers/verrazzano/component/externaldns/external_dns.go
+++ platform-operator/controllers/verrazzano/component/externaldns/external_dns.go
@@ -5,6 +5,7 @@ package externaldns
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"github.com/verrazzano/verrazzano/pkg/bom"
 	"github.com/verrazzano/verrazzano/platform-operator/constants"
@@ -12,9 +13,11 @@ import (
 	"github.com/verrazzano/verrazzano/platform-operator/internal/k8s/status"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"os"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -24,6 +27,9 @@ const (
 	externalDNSNamespace      = "cert-manager"
 	externalDNSDeploymentName = "external-dns"
 	ociSecretFileName         = "oci.yaml"
+	dnsGlobal    = "global" //default
+	dnsPrivate   = "private"
+	dnsScopeKey  = "DNSScope"
 )
 
 func preInstall(compContext spi.ComponentContext) error {
@@ -39,6 +45,14 @@ func preInstall(compContext spi.ComponentContext) error {
 	if err := compContext.Client().Get(context.TODO(), client.ObjectKey{Name: dns.OCI.OCIConfigSecret, Namespace: constants.VerrazzanoInstallNamespace}, &dnsSecret); err != nil {
 		compContext.Log().Errorf("Could not find secret %s in the %s namespace: %s", dns.OCI.OCIConfigSecret, constants.VerrazzanoInstallNamespace, err)
 		return err
+	}
+
+	//check if scope value is valid
+	scope := dns.OCI.DNSScope
+	if strings.ToLower(scope) != dnsGlobal && strings.ToLower(scope) != dnsPrivate && scope != "" {
+		message := fmt.Sprintf("Invalid oci dns scope value: %s. If set ,value can only be 'Global' or 'Private", dns.OCI.DNSScope)
+		compContext.Log().Errorf(message)
+		return errors.New(message)
 	}
 
 	// Attach compartment field to secret and apply it in the external DNS namespace
@@ -85,6 +99,7 @@ func isEnabled(compContext spi.ComponentContext) bool {
 // AppendOverrides builds the set of external-dns overrides for the helm install
 func AppendOverrides(compContext spi.ComponentContext, _ string, _ string, _ string, kvs []bom.KeyValue) ([]bom.KeyValue, error) {
 	// Append all helm overrides for external DNS
+	dns := compContext.EffectiveCR().Spec.Components.DNS
 	nameTimeString := fmt.Sprintf("v8o-local-%s-%s", compContext.EffectiveCR().Spec.EnvironmentName, strconv.FormatInt(time.Now().Unix(), 10))
 	arguments := []bom.KeyValue{
 		{Key: "domainFilters[0]", Value: compContext.EffectiveCR().Spec.Components.DNS.OCI.DNSZoneName},
@@ -95,7 +110,18 @@ func AppendOverrides(compContext spi.ComponentContext, _ string, _ string, _ str
 		{Key: "extraVolumes[0].secret.secretName", Value: compContext.EffectiveCR().Spec.Components.DNS.OCI.OCIConfigSecret},
 		{Key: "extraVolumeMounts[0].name", Value: "config"},
 		{Key: "extraVolumeMounts[0].mountPath", Value: "/etc/kubernetes/"},
+		{Key: "extraEnv[0].name", Value: dnsScopeKey},
+		{Key: "extraEnv[0].value", Value: strings.ToLower(dns.OCI.DNSScope)},
 	}
 	kvs = append(kvs, arguments...)
 	return kvs, nil
+}
+
+// getEnvValueWithDefault fetches value from env variable and returns a default value if not set
+func getEnvValueWithDefault(key, defaultValue string) string {
+	_, ok := os.LookupEnv(key)
+	if !ok {
+		return defaultValue
+	}
+	return os.Getenv(key)
 }
