f2ac46d5bf923d97a5f351ba5ab89ce1b51dd407
Michael Gianatassio <michael.gianatassio@oracle.com>
2021-12-10 06:33:53 -0500

    fix order of imports

--- application-operator/controllers/webhooks/scrape_generator_webhook_test.go
+++ application-operator/controllers/webhooks/scrape_generator_webhook_test.go
@@ -8,10 +8,9 @@ import (
 	"encoding/json"
 	"testing"
 
-	appsv1 "k8s.io/api/apps/v1"
-
 	"github.com/stretchr/testify/assert"
 	admissionv1beta1 "k8s.io/api/admission/v1beta1"
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
