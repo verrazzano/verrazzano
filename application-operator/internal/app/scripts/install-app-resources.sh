8b8425ef02ba188a9ef19f87130275a8860c34fc
ben.mclean <ben.mclean@oracle.com>
2021-12-10 12:59:20 -0500

    add crd to script

--- application-operator/internal/app/scripts/install-app-resources.sh
+++ application-operator/internal/app/scripts/install-app-resources.sh
@@ -2,4 +2,4 @@
 # Copyright (c) 2020, 2021, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-kubectl apply -f ../crds/
+kubectl apply -f ../crds/app.verrazzano.io_metricstemplates.yaml
