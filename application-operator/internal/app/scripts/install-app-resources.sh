2dba35612fc7d705f586b2bce305e3503d373077
Michael Gianatassio <michael.gianatassio@oracle.com>
2021-12-10 13:36:44 -0500

    move create of MutatingWebhookConfiguration to script

--- application-operator/internal/app/scripts/install-app-resources.sh
+++ application-operator/internal/app/scripts/install-app-resources.sh
@@ -3,3 +3,41 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 kubectl apply -f ../crds/app.verrazzano.io_metricstemplates.yaml
+
+# Create the MutatingWebhookConfiguration object.  This object will eventually be
+# moved to platform-operator/helm_config/charts/verrazzano-application-operator/templates/verrazzano-application-operator.yaml
+kubectl apply -f - <<-EOF
+apiVersion: admissionregistration.k8s.io/v1
+kind: MutatingWebhookConfiguration
+metadata:
+  name: verrazzano-application-scrape-generator
+  namespace: verrazzano-system
+  labels:
+    app: verrazzano-application-operator
+webhooks:
+  - name: verrazzano-application-scrape-generator.verrazzano.io
+    namespaceSelector:
+      matchExpressions:
+        - key: kubernetes.io/metadata.name
+          operator: NotIn
+          values: ["kube-system", "verrazzano-system", "verrazzano-mc"]
+    clientConfig:
+      service:
+        name: verrazzano-application-operator
+        namespace: verrazzano-system
+        path: "/scrape-generator"
+    rules:
+      - operations: ["CREATE","UPDATE"]
+        apiGroups: ["*"]
+        apiVersions: ["*"]
+        resources: ["deployments","coherences","domains","pods","replicasets","statefulsets"]
+        scope: "Namespaced"
+    sideEffects: None
+    failurePolicy: Fail
+    matchPolicy: Equivalent
+    timeoutSeconds: 30
+    admissionReviewVersions:
+      - v1beta1
+      - v1
+EOF
+
