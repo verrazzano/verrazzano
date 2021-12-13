9413b343e63f524705fa916094bc396f28e49012
davisgomes <davis.gomes@oracle.com>
2021-12-13 16:54:38 -0500

    VZ-4268 update build tag to metrics_template_test

--- tests/e2e/scrapegenerator/helidon/helidon_scrape_generator_test.go
+++ tests/e2e/scrapegenerator/helidon/helidon_scrape_generator_test.go
@@ -1,7 +1,7 @@
 // Copyright (c) 2021, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-// +build unstable_test
+// +build metrics_template_test
 
 package helidonscrapegenerator
 
