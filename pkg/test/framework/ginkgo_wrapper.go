2c846a9369e1d97d2e1612114131ce8b630dbb39
BHAVANI.RAVICHANDRAN@ORACLE.COM <bhavani.ravichandran@oracle.com>
2021-12-15 04:04:26 +0000

    fixing mistake when resolving conflict

--- pkg/test/framework/ginkgo_wrapper.go
+++ pkg/test/framework/ginkgo_wrapper.go
@@ -82,6 +82,7 @@ func VzAfterEach(body interface{}) bool {
 // VzDescribe - wrapper function for ginkgo Describe
 func VzDescribe(text string, body func()) bool {
 	ginkgo.Describe(text, func() {
+		startTime := time.Now()
 		pkg.Log(pkg.Debug, fmt.Sprintf("Describe block %q started - placeholder for making API call to emit test related metric(s)", VzCurrentGinkgoTestDescription().LeafNodeText))
 		reflect.ValueOf(body).Call([]reflect.Value{})
 		pkg.Log(pkg.Info, fmt.Sprintf("Describe block %q ended - placeholder for making API call to emit test related metric(s)", ginkgo.CurrentSpecReport().LeafNodeText))
