cfd43ecac39e50f6dcfe30260c410c21c95cd1cc
Paul Mackin <paul.mackin@oracle.com>
2021-12-16 12:39:50 -0500

    testing max backoff

--- application-operator/controllers/ingresstrait/ingresstrait_controller.go
+++ application-operator/controllers/ingresstrait/ingresstrait_controller.go
@@ -8,6 +8,7 @@ import (
 	"errors"
 	"fmt"
 	vzstring "github.com/verrazzano/verrazzano/pkg/string"
+	"k8s.io/client-go/util/workqueue"
 	"reflect"
 	"strings"
 
@@ -35,6 +36,7 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
@@ -75,7 +77,14 @@ type Reconciler struct {
 
 // SetupWithManager creates a controller and adds it to the manager
 func (r *Reconciler) SetupWithManager(mgr ctrl.Manager) error {
+	const (
+		baseDelay = 5
+		maxDelay  = 40
+	)
 	return ctrl.NewControllerManagedBy(mgr).
+		WithOptions(controller.Options{
+			RateLimiter: workqueue.NewItemExponentialFailureRateLimiter(baseDelay, maxDelay),
+		}).
 		For(&vzapi.IngressTrait{}).
 		Complete(r)
 }
