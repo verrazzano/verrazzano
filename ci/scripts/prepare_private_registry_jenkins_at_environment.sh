de6173e7649c8761a225cde7ea21fcefd1eb332e
Prasad Shirodkar <prasad.shirodkar@oracle.com>
2021-12-21 15:48:40 -0800

    VZ-4272
    WIP
    
    Signed-off-by: Prasad Shirodkar <prasad.shirodkar@oracle.com>

--- ci/scripts/prepare_private_registry_jenkins_at_environment.sh
+++ ci/scripts/prepare_private_registry_jenkins_at_environment.sh
@@ -76,7 +76,7 @@ deploy_harbor() {
 
   cd ${TEST_SCRIPTS_DIR}
   # Create the Harbor project if it does not exist
-  ${TEST_SCRIPTS_DIR}/create_harbor_project.sh -a "https://${REGISTRY}/api/v2.0" -u ${PRIVATE_REGISTRY_USR} -p ${PRIVATE_REGISTRY_PSW} -m ${IMAGE_REPO_SUBPATH_PREFIX} -l false
+  ./create_harbor_project.sh -a "https://${REGISTRY}/api/v2.0" -u ${PRIVATE_REGISTRY_USR} -p ${PRIVATE_REGISTRY_PSW} -m ${IMAGE_REPO_SUBPATH_PREFIX} -l false
 }
 
 start_installation() {
