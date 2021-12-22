9ce0b4c268400976c03b0848e5597ec4b1b0e378
Abhishek Mitra <abhishek.a.mitra@oracle.com>
2021-12-21 21:31:48 -0800

    Update script to support instance principal

--- platform-operator/scripts/install/create_oci_config_secret.sh
+++ platform-operator/scripts/install/create_oci_config_secret.sh
@@ -12,6 +12,7 @@ fi
 
 TMP_DIR=$(mktemp -d)
 trap 'rc=$?; rm -rf ${TMP_DIR} || true' EXIT
+#trap 'rm -rf ${TMP_DIR} || true' EXIT
 
 # read a config item from a specified section of an oci config file
 function read_config() {
@@ -54,7 +55,7 @@ function usage {
     echo "  -s config_file_section     The properties section within the OCI configuration file.  Default is DEFAULT"
     echo "  -k secret_name             The secret name containing the OCI configuration.  Default is oci"
     echo "  -c context_name            The kubectl context to use"
-    echo "  -i instance_principal      Use OCI instance principal (true/false)"
+    echo "  -a auth_type               The auth_type to be used to access OCI. Valid values are user_principal/instance_principal. Default is user_principal."
     echo "  -h                         Help"
     echo
     exit 1
@@ -67,46 +68,40 @@ SECTION=DEFAULT
 OCI_CONFIG_SECRET_NAME=oci
 K8SCONTEXT=""
 VERRAZZANO_INSTALL_NS=verrazzano-install
-INSTANCE_PRINCIPAL_ENABLED=false
+OCI_AUTH_TYPE="user_principal"
 
-while getopts c:o:s:k:i:h flag
+while getopts c:o:s:k:a:h flag
 do
     case "${flag}" in
         o) OCI_CONFIG_FILE=${OPTARG};;
         s) SECTION=${OPTARG};;
         k) OCI_CONFIG_SECRET_NAME=${OPTARG};;
         c) K8SCONTEXT="--context=${OPTARG}";;
-        i) INSTANCE_PRINCIPAL_INPUT=${OPTARG};;
+        a) OCI_AUTH_TYPE_INPUT=${OPTARG};;
         h) usage;;
         *) usage;;
     esac
 done
 
-if [ "${INSTANCE_PRINCIPAL_INPUT:-}" ] ; then
-  if [ ${INSTANCE_PRINCIPAL_INPUT} == "true" ] || [ ${INSTANCE_PRINCIPAL_INPUT} == "false" ]; then
-    INSTANCE_PRINCIPAL_ENABLED=${INSTANCE_PRINCIPAL_INPUT}
+if [ "${OCI_AUTH_TYPE_INPUT:-}" ] ; then
+  if [ ${OCI_AUTH_TYPE_INPUT} == "user_principal" ] || [ ${OCI_AUTH_TYPE_INPUT} == "instance_principal" ]; then
+    OCI_AUTH_TYPE=${OCI_AUTH_TYPE_INPUT}
   fi
 fi
 
 #create the yaml file
-if [ ${INSTANCE_PRINCIPAL_ENABLED} == "true" ]; then
-  echo "auth:" > $OUTPUT_FILE
-  echo "  useInstancePrincipals: ${INSTANCE_PRINCIPAL_ENABLED}" >> $OUTPUT_FILE
-else
-  SECTION_PROPS=$(read_config $OCI_CONFIG_FILE $SECTION *)
-  eval $SECTION_PROPS
-
-  echo "auth:" > $OUTPUT_FILE
-  echo "  region: $region" >> $OUTPUT_FILE
-  echo "  tenancy: $tenancy" >> $OUTPUT_FILE
-  echo "  user: $user" >> $OUTPUT_FILE
-  echo "  key: |" >> $OUTPUT_FILE
-  cat $key_file | sed 's/^/    /' >> $OUTPUT_FILE
-  echo "  fingerprint: $fingerprint" >> $OUTPUT_FILE
-  echo "  useInstancePrincipals: ${INSTANCE_PRINCIPAL_ENABLED}" >> $OUTPUT_FILE
-  if [[ ! -z "$pass_phrase" ]]; then
-    echo "  passphrase: $pass_phrase" >> $OUTPUT_FILE
-  fi
+SECTION_PROPS=$(read_config $OCI_CONFIG_FILE $SECTION *)
+eval $SECTION_PROPS
+echo "auth:" > $OUTPUT_FILE
+echo "  region: $region" >> $OUTPUT_FILE
+echo "  tenancy: $tenancy" >> $OUTPUT_FILE
+echo "  user: $user" >> $OUTPUT_FILE
+echo "  key: |" >> $OUTPUT_FILE
+cat $key_file | sed 's/^/    /' >> $OUTPUT_FILE
+echo "  fingerprint: $fingerprint" >> $OUTPUT_FILE
+echo "  authtype: ${OCI_AUTH_TYPE}" >> $OUTPUT_FILE
+if [[ ! -z "$pass_phrase" ]]; then
+  echo "  passphrase: $pass_phrase" >> $OUTPUT_FILE
 fi
 
 # create the secret in verrazzano-install namespace
@@ -118,7 +113,6 @@ if [ $? -eq 0 ]; then
    exit 1
 fi
 kubectl ${K8SCONTEXT} create secret -n $VERRAZZANO_INSTALL_NS  generic $OCI_CONFIG_SECRET_NAME --from-file=$OUTPUT_FILE
-
 
 
 
