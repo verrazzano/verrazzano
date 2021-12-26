ee9e8f3146364f2ce31904f3082ce2750aa1d29f
Abhishek Mitra <abhishek.a.mitra@oracle.com>
2021-12-25 23:40:31 -0800

    Debug fetching fcn id from dns script

--- tests/e2e/config/scripts/oci_dns_ops.sh
+++ tests/e2e/config/scripts/oci_dns_ops.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# added for debugging purpose
+set -x
+
 #
 # Copyright (c) 2020, 2021, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
