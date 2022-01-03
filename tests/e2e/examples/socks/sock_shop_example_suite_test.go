180b855ece8a54953bac759378614a1cb726a9de
anders-swanson <91502735+anders-swanson@users.noreply.github.com>
2022-01-03 14:50:25 -0800

    ToDo cluster dump (#2226)
    cluster dump

--- tests/e2e/examples/socks/sock_shop_example_suite_test.go
+++ tests/e2e/examples/socks/sock_shop_example_suite_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2021, Oracle and/or its affiliates.
+// Copyright (c) 2021, 2022, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 package socks
@@ -19,6 +19,6 @@ func init() {
 }
 
 func TestSockShopApplication(t *testing.T) {
-	gomega.RegisterFailHandler(FailHandler)
+	gomega.RegisterFailHandler(ginkgo.Fail)
 	ginkgo.RunSpecs(t, "Sock Shop Suite")
 }
