a3d41089148fe2e48489f93d6def438c84295145
Abhishek Mitra <abhishek.a.mitra@oracle.com>
2022-01-04 15:24:25 -0800

    update DNS scope and default value to GLOBAL

--- tests/e2e/config/scripts/process_oci_dns_install_yaml.sh
+++ tests/e2e/config/scripts/process_oci_dns_install_yaml.sh
@@ -7,16 +7,17 @@ INSTALL_CONFIG_TO_EDIT=$1
 CERT_MGR=${2:-"acme"}
 ACME_ENV=${3:-"staging"}
 DNS_SCOPE=${4:-"GLOBAL"}
-
+if [ "${DNS_SCOPE}" != "PRIVATE" ] || [ "${DNS_SCOPE}" != "GLOBAL" ]; then
+  # setting scope to global even for invalid values. This should not happen in jenkins flow
+  DNS_SCOPE="GLOBAL"
+fi
 echo "Editing install config file for OCI DNS ${INSTALL_CONFIG_TO_EDIT}"
 yq -i eval ".spec.environmentName = \"${VZ_ENVIRONMENT_NAME}\"" ${INSTALL_CONFIG_TO_EDIT}
 yq -i eval ".spec.profile = \"${INSTALL_PROFILE}\"" ${INSTALL_CONFIG_TO_EDIT}
 yq -i eval ".spec.components.dns.oci.dnsZoneCompartmentOCID = \"${OCI_DNS_COMPARTMENT_OCID}\"" ${INSTALL_CONFIG_TO_EDIT}
 yq -i eval ".spec.components.dns.oci.dnsZoneOCID = \"${OCI_DNS_ZONE_OCID}\"" ${INSTALL_CONFIG_TO_EDIT}
 yq -i eval ".spec.components.dns.oci.dnsZoneName = \"${OCI_DNS_ZONE_NAME}\"" ${INSTALL_CONFIG_TO_EDIT}
-if [ "${DNS_SCOPE}" == "PRIVATE" ]; then
-  yq -i eval ".spec.components.dns.oci.dnsScope = \"${DNS_SCOPE}\"" ${INSTALL_CONFIG_TO_EDIT}
-fi
+yq -i eval ".spec.components.dns.oci.dnsScope = \"${DNS_SCOPE}\"" ${INSTALL_CONFIG_TO_EDIT}
 if [ "${CERT_MGR}" == "acme" ]; then
   yq -i eval ".spec.components.certManager.certificate.acme.provider = \"letsEncrypt\"" ${INSTALL_CONFIG_TO_EDIT}
   yq -i eval ".spec.components.certManager.certificate.acme.emailAddress = \"emailAddress@domain.com\"" ${INSTALL_CONFIG_TO_EDIT}
