ebd68a59c130185db041c4bc92e23fb9eb10ab7a
Abhishek Mitra <abhishek.a.mitra@oracle.com>
2022-01-05 15:05:14 -0800

    set -x for debug

--- tests/e2e/config/scripts/terraform/cluster/create-cluster.sh
+++ tests/e2e/config/scripts/terraform/cluster/create-cluster.sh
@@ -4,6 +4,9 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 #
 
+# added for debug
+set -x
+
 . ./init.sh
 
 $SCRIPT_DIR/terraform init -no-color -reconfigure
