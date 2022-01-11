b71ae85632c2d93db85fe4a88da30c425763733d
Bryan Tidd <bryan.tidd@oracle.com>
2022-01-11 11:11:54 -0500

    VZ-4411 update copyright year

--- pkg/test/framework/metrics/ginkgo_metrics_suite_test.go
+++ pkg/test/framework/metrics/ginkgo_metrics_suite_test.go
@@ -1,11 +1,9 @@
-// Copyright (c) 2021, Oracle and/or its affiliates.
+// Copyright (c) 2021, 2022, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 package metrics_test
 
 import (
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
 	"testing"
 )
 
