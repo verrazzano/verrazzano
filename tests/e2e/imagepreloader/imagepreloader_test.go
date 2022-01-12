f44d8d53a321a60df6b86bc23fff970f8ba65c11
Michael Gianatassio <michael.gianatassio@oracle.com>
2022-01-12 13:33:51 -0800

    more refactoring of code

--- tests/e2e/imagepreloader/imagepreloader_test.go
+++ tests/e2e/imagepreloader/imagepreloader_test.go
@@ -24,7 +24,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-const namespace string = "image-preloader"
+const testNamespace string = "image-preloader"
 const testName string = "preloader-test"
 const bomPath string = "../../../platform-operator/verrazzano-bom.json"
 
@@ -41,9 +41,9 @@ var shortPollingInterval = 10 * time.Second
 var t = framework.NewTestFramework("imagepreloader")
 
 var _ = t.BeforeSuite(func() {
-	pkg.Log(pkg.Info, fmt.Sprintf("Create namespace %s", namespace))
+	pkg.Log(pkg.Info, fmt.Sprintf("Create namespace %s", testNamespace))
 	Eventually(func() (*v1.Namespace, error) {
-		return pkg.CreateNamespace(namespace, map[string]string{"verrazzano-managed": "true", "istio-injection": "enabled"})
+		return pkg.CreateNamespace(testNamespace, map[string]string{"verrazzano-managed": "true", "istio-injection": "enabled"})
 	}, waitTimeout, shortPollingInterval).ShouldNot(BeNil())
 
 	pkg.Log(pkg.Info, "Parse Verrazzano bom file")
@@ -54,14 +54,14 @@ var _ = t.BeforeSuite(func() {
 })
 
 var _ = t.AfterSuite(func() {
-	pkg.Log(pkg.Info, fmt.Sprintf("Delete the namespace %s", namespace))
+	pkg.Log(pkg.Info, fmt.Sprintf("Delete the namespace %s", testNamespace))
 	Eventually(func() error {
-		return pkg.DeleteNamespace(namespace)
+		return pkg.DeleteNamespace(testNamespace)
 	}, waitTimeout, shortPollingInterval).ShouldNot(HaveOccurred())
 
-	pkg.Log(pkg.Info, fmt.Sprintf("Wait for the namespace %s to be terminated", namespace))
+	pkg.Log(pkg.Info, fmt.Sprintf("Wait for the namespace %s to be terminated", testNamespace))
 	Eventually(func() bool {
-		_, err := pkg.GetNamespace(namespace)
+		_, err := pkg.GetNamespace(testNamespace)
 		if err != nil {
 			return errors.IsNotFound(err)
 		}
@@ -78,14 +78,14 @@ var _ = t.Describe("Load Verrazzano Container Images", func() {
 		})
 		t.It("wait for the pod to be ready", func() {
 			Eventually(func() bool {
-				return pkg.PodsRunning(namespace, []string{testName})
+				return pkg.PodsRunning(testNamespace, []string{testName})
 			}, shortWaitTimeout, pollingInterval).Should(BeTrue(), fmt.Sprintf("%s failed to deploy", testName))
 		})
 		t.It("get the pod name", func() {
 			var podsList []corev1.Pod
 			Eventually(func() error {
 				var err error
-				podsList, err = pkg.GetPodsFromSelector(&metav1.LabelSelector{MatchLabels: map[string]string{"name": testName}}, namespace)
+				podsList, err = pkg.GetPodsFromSelector(&metav1.LabelSelector{MatchLabels: map[string]string{"name": testName}}, testNamespace)
 				if err != nil && errors.IsNotFound(err) {
 					// Ignore pods not found
 					return nil
@@ -109,15 +109,20 @@ var _ = t.Describe("Load Verrazzano Container Images", func() {
 			var err error
 			kubeconfig, err = k8sutil.GetKubeConfigLocation()
 			Expect(err).ToNot(HaveOccurred())
-			Expect(kubeconfig).ToNot(HaveLen(0))
-			err = injectImages(kubeconfig, imageList)
-			Expect(err).ToNot(HaveOccurred())
 
+			// Loop through the image list and use ephemeral containers to inject each image into the test deployment.
+			// This will initiate the download of each image into the cluster (if not already present)
+			for name, image := range imageList {
+				// Use Eventually block because the kubectl commands can get a conflict error when executed so quickly in succession.
+				Eventually(func() error {
+					return injectImage(kubeconfig, name, image)
+				}, shortWaitTimeout, pollingInterval).ShouldNot(HaveOccurred(), fmt.Sprintf("failed to inject image %s", image))
+			}
 		})
 		t.It(fmt.Sprintf("wait for all ephemeral containers in pod %s to complete", podName), func() {
 			Eventually(func() bool {
-				return areImagesToLoaded()
-			}, shortWaitTimeout, pollingInterval).Should(BeTrue(), "timed out waiting for images to load")
+				return areImagesToLoaded(testName, testNamespace)
+			}, waitTimeout, pollingInterval).Should(BeTrue(), "timed out waiting for images to load")
 		})
 	})
 	metrics.Emit(t.Metrics.With("images_load_elapsed_time", time.Since(start).Milliseconds()))
@@ -128,7 +133,7 @@ func deployDaemonSet() error {
 	templateName := "imagepreloader-template.yaml"
 	params := map[string]string{
 		"test_name":      testName,
-		"test_namespace": namespace,
+		"test_namespace": testNamespace,
 	}
 	testTemplate, err := template.New(templateName).ParseFiles(fmt.Sprintf("testdata/%s", templateName))
 	if err != nil {
@@ -170,38 +175,30 @@ func createImageList(bom v8obom.Bom) (map[string]string, error) {
 	return imageMap, nil
 }
 
-// injectImages - inject the container images into the test pod using ephemeral containers
-func injectImages(kubeconfig string, imageList map[string]string) error {
-	// Loop through the image list and use ephemeral containers to inject each image into the test deployment.
-	// This will initiate the download of each image into the cluster (if not already present)
-	for name, image := range imageList {
-		// Use Eventually block because the kubectl commands can get a conflict error when executed so quickly in succession.
-		Eventually(func() error {
-			cmd := exec.Command("kubectl", "debug", "--namespace", namespace, podName,
-				"--container", name, "--target", testName, "--image", image, "--image-pull-policy", "IfNotPresent",
-				"--", "pwd")
-			pkg.Log(pkg.Info, fmt.Sprintf("kubectl command to inject image %s: %s", image, cmd.String()))
-			cmd.Env = append(os.Environ(), fmt.Sprintf("KUBECONFIG=%s", kubeconfig))
-			cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
-			err := cmd.Start()
-			if err != nil {
-				return err
-			}
-			err = cmd.Wait()
-			if err != nil {
-				return err
-			}
-			return nil
-		}, shortWaitTimeout, pollingInterval).ShouldNot(HaveOccurred(), fmt.Sprintf("failed to inject image %s", image))
+// injectImage - inject a container image into the test pod using ephemeral containers
+func injectImage(kubeconfig string, name string, image string) error {
+	cmd := exec.Command("kubectl", "debug", "--namespace", testNamespace, podName,
+		"--container", name, "--target", testName, "--image", image, "--image-pull-policy", "IfNotPresent",
+		"--", "pwd")
+	pkg.Log(pkg.Info, fmt.Sprintf("kubectl command to inject image %s: %s", image, cmd.String()))
+	cmd.Env = append(os.Environ(), fmt.Sprintf("KUBECONFIG=%s", kubeconfig))
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Start()
+	if err != nil {
+		return err
+	}
+	err = cmd.Wait()
+	if err != nil {
+		return err
 	}
 	return nil
 }
 
 // areImagesToLoaded - wait for the images in the ephemeral containers to load
-func areImagesToLoaded() bool {
+func areImagesToLoaded(name string, namespace string) bool {
 	// Get the pod
-	podsList, err := pkg.GetPodsFromSelector(&metav1.LabelSelector{MatchLabels: map[string]string{"name": testName}}, namespace)
+	podsList, err := pkg.GetPodsFromSelector(&metav1.LabelSelector{MatchLabels: map[string]string{"name": name}}, namespace)
 	if err != nil {
 		return false
 	}
