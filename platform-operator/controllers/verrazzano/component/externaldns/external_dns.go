f22cbdb062516a916cbf7a9988472e8e120a4b6a
Abhishek Mitra <abhishek.a.mitra@oracle.com>
2022-01-15 12:09:34 -0800

    Cherry pick and fix conflicts for VZ-4525

--- platform-operator/controllers/verrazzano/component/externaldns/external_dns.go
+++ platform-operator/controllers/verrazzano/component/externaldns/external_dns.go
@@ -16,7 +16,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"strconv"
-	"strings"
 	"time"
 )
 
@@ -26,9 +25,8 @@ const (
 	externalDNSNamespace      = "cert-manager"
 	externalDNSDeploymentName = "external-dns"
 	ociSecretFileName         = "oci.yaml"
-	dnsGlobal                 = "global" //default
-	dnsPrivate                = "private"
-	dnsScopeKey               = "DNSScope"
+	dnsGlobal                 = "GLOBAL" //default
+	dnsPrivate                = "PRIVATE"
 	imagePullSecretHelmKey    = "global.imagePullSecrets[0]"
 )
 
@@ -49,8 +47,8 @@ func preInstall(compContext spi.ComponentContext) error {
 
 	//check if scope value is valid
 	scope := dns.OCI.DNSScope
-	if strings.ToLower(scope) != dnsGlobal && strings.ToLower(scope) != dnsPrivate && scope != "" {
-		message := fmt.Sprintf("Invalid OCI DNS scope value: %s. If set, value can only be 'Global' or 'Private", dns.OCI.DNSScope)
+	if scope != dnsGlobal && scope != dnsPrivate && scope != "" {
+		message := fmt.Sprintf("Invalid OCI DNS scope value: %s. If set, value can only be 'GLOBAL' or 'PRIVATE", dns.OCI.DNSScope)
 		compContext.Log().Errorf(message)
 		return errors.New(message)
 	}
@@ -91,7 +89,6 @@ func isReady(compContext spi.ComponentContext) bool {
 // AppendOverrides builds the set of external-dns overrides for the helm install
 func AppendOverrides(compContext spi.ComponentContext, _ string, _ string, _ string, kvs []bom.KeyValue) ([]bom.KeyValue, error) {
 	// Append all helm overrides for external DNS
-	dns := compContext.EffectiveCR().Spec.Components.DNS
 	nameTimeString := fmt.Sprintf("v8o-local-%s-%s", compContext.EffectiveCR().Spec.EnvironmentName, strconv.FormatInt(time.Now().Unix(), 10))
 	arguments := []bom.KeyValue{
 		{Key: "domainFilters[0]", Value: compContext.EffectiveCR().Spec.Components.DNS.OCI.DNSZoneName},
@@ -103,8 +100,6 @@ func AppendOverrides(compContext spi.ComponentContext, _ string, _ string, _ str
 		{Key: "extraVolumes[0].secret.secretName", Value: compContext.EffectiveCR().Spec.Components.DNS.OCI.OCIConfigSecret},
 		{Key: "extraVolumeMounts[0].name", Value: "config"},
 		{Key: "extraVolumeMounts[0].mountPath", Value: "/etc/kubernetes/"},
-		{Key: "extraEnv[0].name", Value: dnsScopeKey},
-		{Key: "extraEnv[0].value", Value: strings.ToLower(dns.OCI.DNSScope)},
 	}
 	kvs = append(kvs, arguments...)
 	return kvs, nil
