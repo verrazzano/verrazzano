4a0c223878b16d093df3b6722a3613a195673dde
Robert Donat <robert.donat@oracle.com>
2022-01-18 10:16:16 -0500

    VZ-4435: POKO: Implement acceptance tests for all standard Kubernetes workloads (#2317)
    * initial changes
    
    * update acceptance tests
    
    * fix typo
    
    * updates per review comments
    
    * fix build checks

--- tests/e2e/metricsbinding/helidon-deployment/helidon_deployment_test.go
+++ tests/e2e/metricsbinding/helidon-deployment/helidon_deployment_test.go
@@ -1,40 +1,39 @@
 // Copyright (c) 2021, 2022, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-// +build metrics_template_test
-
-package helidonscrapegenerator
+package deploymentworkload
 
 import (
-	"github.com/verrazzano/verrazzano/tests/e2e/pkg"
-	"k8s.io/apimachinery/pkg/types"
 	"time"
 
 	. "github.com/onsi/gomega"
-	"github.com/verrazzano/verrazzano/tests/e2e/scrapegenerator"
+	"github.com/verrazzano/verrazzano/pkg/test/framework"
+	"github.com/verrazzano/verrazzano/pkg/test/framework/metrics"
+	"github.com/verrazzano/verrazzano/tests/e2e/metricsbinding"
+	"github.com/verrazzano/verrazzano/tests/e2e/pkg"
 )
 
 const (
 	longWaitTimeout      = 15 * time.Minute
 	longPollingInterval  = 20 * time.Second
-	namespace            = "hello-helidon-scrape"
+	namespace            = "hello-helidon-namespace"
 	applicationPodPrefix = "hello-helidon-deployment-"
-	metricsTemplateName  = "hello-helidon-metrics-template"
-	yamlPath             = "testdata/scrapegenerator/helidon/helidon-scrape-generator.yaml"
+	yamlPath             = "application-operator/internal/app/resources/workloads/hello-helidon-deployment.yaml"
+	promConfigJobName    = "hello-helidon-namespace_hello-helidon-deployment_apps_v1_Deployment"
 )
 
-var t = framework.NewTestFramework("helidonscrapegenerator")
+var t = framework.NewTestFramework("deploymentworkload")
 
 var _ = t.BeforeSuite(func() {
 	start := time.Now()
-	scrapegenerator.DeployApplication(namespace, yamlPath)
+	metricsbinding.DeployApplication(namespace, yamlPath)
 	metrics.Emit(t.Metrics.With("deployment_elapsed_time", time.Since(start).Milliseconds()))
 })
 
 var clusterDump = pkg.NewClusterDumpWrapper()
 var _ = clusterDump.AfterEach(func() {}) // Dump cluster if spec fails
 var _ = clusterDump.AfterSuite(func() {  // Dump cluster if aftersuite fails
-	scrapegenerator.UndeployApplication(namespace, yamlPath)
+	metricsbinding.UndeployApplication(namespace, yamlPath, promConfigJobName)
 })
 
 var _ = t.AfterEach(func() {})
@@ -44,25 +43,23 @@ var _ = t.Describe("Verify", func() {
 		// GIVEN the app is deployed
 		// WHEN the running pods are checked
 		// THEN the Helidon pod should exist
-		t.It("'hello-helidon-scrape' pod is running", func() {
+		t.It("Verify 'hello-helidon-deployment' pod is running", func() {
 			Eventually(func() bool {
 				return pkg.PodsRunning(namespace, []string{applicationPodPrefix})
-			}, longWaitTimeout, longPollingInterval).Should(BeTrue())
-		})
-		t.It("metrics template exists", func() {
-			Eventually(func() bool {
-				return pkg.DoesMetricsTemplateExist(types.NamespacedName{Name: metricsTemplateName, Namespace: namespace})
 			}, longWaitTimeout, longPollingInterval).Should(BeTrue())
 		})
 	})
 
 	// GIVEN the Helidon app is deployed and the pods are running
 	// WHEN the Prometheus metrics in the app namespace are scraped
-	// THEN the Helidon application metrics should exist
-	t.Context("Retrieve", func() {
-		t.It("Prometheus scraped metrics for Helidon Pod", func() {
+	// THEN the Helidon application metrics should exist using the default metrics template for deployments
+	t.Context("Verify Prometheus scraped metrics.", func() {
+		t.It("Retrieve Prometheus scraped metrics for 'hello-helidon-deployment' Pod", func() {
 			Eventually(func() bool {
-				return pkg.MetricsExist("base_jvm_uptime_seconds", "app", "hello-helidon-scrape-application")
+				return pkg.MetricsExist("base_jvm_uptime_seconds", "app_verrazzano_io_workload", "hello-helidon-deployment-apps-v1-deployment")
+			}, longWaitTimeout, longPollingInterval).Should(BeTrue(), "Expected to find Prometheus scraped metrics for Helidon application.")
+			Eventually(func() bool {
+				return pkg.MetricsExist("base_jvm_uptime_seconds", "job", promConfigJobName)
 			}, longWaitTimeout, longPollingInterval).Should(BeTrue(), "Expected to find Prometheus scraped metrics for Helidon application.")
 		})
 	})
