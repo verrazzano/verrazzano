19abc1071dfa9c6240b424460e9d13c36736354c
Prasad Shirodkar <prasad.shirodkar@oracle.com>
2022-01-18 11:00:41 -0800

    Fixed the test by correcting merge conflict commit.
    Signed-off-by: Prasad Shirodkar <prasad.shirodkar@oracle.com>

--- tests/e2e/registry/registry_test.go
+++ tests/e2e/registry/registry_test.go
@@ -78,18 +78,6 @@ var _ = t.Describe("Private Registry Verification",
 						pod = pods.Items[j]
 						pkg.Log(pkg.Info, fmt.Sprintf("%d. Validating the registry url prefix for pod: %s in namespace: %s", i, pod.Name, ns))
 						for k := range pod.Spec.Containers {
-							if imageRegistrySelection == harborEphemeral && !strings.HasPrefix(pod.Spec.Containers[k].Image, goHarborImagePrefix) {
-								// Assertion only when the image registry selection is ephemeral harbor and excluding the images originating from harbor installation
-								Expect(strings.HasPrefix(pod.Spec.Containers[k].Image, imagePrefix)).To(BeTrue(),
-									fmt.Sprintf("FAIL: The image for the pod %s in containers, doesn't starts with expected registry URL prefix %s, image name %s", pod.Name, imagePrefix, pod.Spec.Containers[k].Image))
-							} else {
-								Expect(strings.HasPrefix(pod.Spec.Containers[k].Image, imagePrefix)).To(BeTrue(),
-									fmt.Sprintf("FAIL: The image for the pod %s in containers, doesn't starts with expected registry URL prefix %s, image name %s", pod.Name, imagePrefix, pod.Spec.Containers[k].Image))
-							}
-						}
-						for k := range pod.Spec.InitContainers {
-							Expect(strings.HasPrefix(pod.Spec.InitContainers[k].Image, imagePrefix)).To(BeTrue(),
-								fmt.Sprintf("FAIL: The image for the pod %s in initContainers, doesn't starts with expected registry URL prefix %s, image name %s", pod.Name, imagePrefix, pod.Spec.InitContainers[k].Image))
 							assertImageRegistryURLOfPod(pod.Name, pod.Spec.Containers[k].Image, imagePrefix, "containers")
 						}
 						for k := range pod.Spec.InitContainers {
