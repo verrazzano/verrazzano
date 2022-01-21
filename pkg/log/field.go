0d1eec68142e52fd1f74bf4be34272fc78f5a3c2
Paul Mackin <paul.mackin@oracle.com>
2022-01-21 09:07:08 -0500

    VZ-4619 VPOI logging cleanup (#2347)

---
+++ pkg/log/field.go
@@ -0,0 +1,10 @@
+// Copyright (C) 2022, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package log
+
+const (
+	FieldResourceNamespace = "resource_namespace"
+	FieldResourceName      = "resource_name"
+	FieldController
+)
