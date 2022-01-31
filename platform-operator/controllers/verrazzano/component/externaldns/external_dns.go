ef8051a5c77fc9147dbd1634940a58ed16fb6469
Michael Cico <46466506+michael-cico@users.noreply.github.com>
2022-01-31 07:08:24 -0500

    VZ-4616.  Re-introduce validator, and fix instance-principal case (#2398)
    * Revert "Revert "VZ-4616.  Re-introduce changes, and add temp file cleanup hooks on container restart (#2373)" (#2396)"
    This reverts commit d4376d3a6bd647e14d2c2bbfe2f6128994c101f6.
    * Fix OCI DNS instance_principal validation
    * touch up unit test

--- platform-operator/controllers/verrazzano/component/externaldns/external_dns.go
+++ platform-operator/controllers/verrazzano/component/externaldns/external_dns.go
@@ -10,6 +10,9 @@ import (
 	"strconv"
 	"time"
 
+	ctrlerrrors "github.com/verrazzano/verrazzano/pkg/controller/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
 	"github.com/verrazzano/verrazzano/pkg/bom"
 	"github.com/verrazzano/verrazzano/platform-operator/constants"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/spi"
@@ -36,6 +39,17 @@ func preInstall(compContext spi.ComponentContext) error {
 	if compContext.IsDryRun() {
 		compContext.Log().Debug("cert-manager PreInstall dry run")
 		return nil
+	}
+
+	compContext.Log().Debug("Creating namespace %s namespace if necessary", externalDNSNamespace)
+	ns := v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: externalDNSNamespace}}
+	if _, err := controllerutil.CreateOrUpdate(context.TODO(), compContext.Client(), &ns, func() error {
+		return nil
+	}); err != nil {
+		return ctrlerrrors.RetryableError{
+			Source: compContext.GetComponent(),
+			Cause:  fmt.Errorf("Failed to create or update the cert-manager namespace: %v", err),
+		}
 	}
 
 	// Get OCI DNS secret from the verrazzano-install namespace
