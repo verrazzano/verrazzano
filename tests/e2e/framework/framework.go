174efdce9f221d4dbbbd729894d7bc1e168442ca
Steven Shapiro <31927916+stevshap@users.noreply.github.com>
2022-02-07 15:59:31 -0500

    Stshapir/vz 4822 (#2471)
    * BeforeEach interface (kinda)
    
    Signed-off-by: stshapir <steven.shapiro@oracle.com>

--- tests/e2e/framework/framework.go
+++ tests/e2e/framework/framework.go
@@ -2,3 +2,112 @@
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 package framework
+
+import (
+	"context"
+	"fmt"
+	"k8s.io/client-go/discovery"
+	"math/rand"
+	"os"
+	"path"
+	"strings"
+	"sync"
+	"time"
+
+	"k8s.io/apimachinery/pkg/runtime"
+
+	"github.com/verrazzano/verrazzano/pkg/k8sutil"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/dynamic"
+	clientset "k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+
+	"github.com/onsi/ginkgo"
+	"github.com/onsi/gomega"
+)
+
+const (
+	// DefaultNamespaceDeletionTimeout is timeout duration for waiting for a namespace deletion.
+	DefaultNamespaceDeletionTimeout = 5 * time.Minute
+)
+
+// Framework supports common operations used by e2e tests; it will keep a client & a namespace for you.
+// Eventual goal is to merge this with integration test framework.
+type Framework struct {
+	BaseName string
+
+	// Set together with creating the ClientSet and the namespace.
+	// Guaranteed to be unique in the cluster even when running the same
+	// test multiple times in parallel.
+	UniqueName string
+
+	clientConfig			*rest.Config
+	ClientSet               clientset.Interface
+
+	DynamicClient 			dynamic.Interface
+
+	SkipNamespaceCreation    bool            // Whether to skip creating a namespace
+	Namespace                *v1.Namespace   // Every test has at least one namespace unless creation is skipped
+	namespacesToDelete       []*v1.Namespace // Some tests have more than one.
+
+	// afterEaches is a map of name to function to be called after each test.  These are not
+	// cleared.  The call order is randomized so that no dependencies can grow between
+	// the various afterEaches
+	afterEaches map[string]AfterEachActionFunc
+
+	// beforeEachStarted indicates that BeforeEach has started
+	beforeEachStarted bool
+}
+
+// AfterEachActionFunc is a function that can be called after each test
+type AfterEachActionFunc func(f *Framework, failed bool)
+
+// NewDefaultFramework makes a new framework and sets up a BeforeEach/AfterEach for
+// you (you can write additional before/after each functions).
+func NewDefaultFramework(baseName string) *Framework {
+	return NewFramework(baseName, nil)
+}
+
+// NewFramework creates a test framework.
+func NewFramework(baseName string, client clientset.Interface) *Framework {
+	f := &Framework{
+		BaseName:                 baseName,
+		ClientSet:                client,
+	}
+
+	f.AddAfterEach("dumpNamespaceInfo", func(f *Framework, failed bool) {
+		if !failed {
+			return
+		}
+	})
+
+	ginkgo.BeforeEach(f.BeforeEach)
+	ginkgo.AfterEach(f.AfterEach)
+
+	return f
+}
+
+// BeforeEach gets a client and makes a namespace.
+func (f *Framework) BeforeEach() {
+	f.beforeEachStarted = true
+
+	if f.ClientSet == nil {
+		ginkgo.By("Creating a kubernetes client")
+		config, err := k8sutil.GetKubeConfig()
+
+		f.clientConfig = rest.CopyConfig(config)
+		f.ClientSet, err = k8sutil.GetKubernetesClientset()
+		f.DynamicClient, err = dynamic.NewForConfig(config)
+	}
+}
+
+// AddAfterEach is a way to add a function to be called after every test.  The execution order is intentionally random
+// to avoid growing dependencies.  If you register the same name twice, it is a coding error and will panic.
+func (f *Framework) AddAfterEach(name string, fn AfterEachActionFunc) {
+
+}
+
+// AfterEach deletes the namespace, after reading its events.
+func (f *Framework) AfterEach() {
+
+}
