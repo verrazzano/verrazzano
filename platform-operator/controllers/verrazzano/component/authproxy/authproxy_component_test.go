0e96b6cbdb3fa58d08ecf945bfd031aa071f2d5d
Michael Gianatassio <michael.gianatassio@oracle.com>
2022-02-08 13:32:55 -0800

    add tests for IsReady

--- platform-operator/controllers/verrazzano/component/authproxy/authproxy_component_test.go
+++ platform-operator/controllers/verrazzano/component/authproxy/authproxy_component_test.go
@@ -1,34 +1,76 @@
+// Copyright (c) 2022, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
 package authproxy
 
 import (
 	"testing"
 
+	vzapi "github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1alpha1"
+
+	globalconst "github.com/verrazzano/verrazzano/pkg/constants"
+	appsv1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+
 	"github.com/stretchr/testify/assert"
-	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/helm"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/spi"
 )
 
-func Test_authProxyComponent_IsReady(t *testing.T) {
-	type fields struct {
-		HelmComponent helm.HelmComponent
-	}
-	type args struct {
-		ctx spi.ComponentContext
-	}
+// TestIsReady tests the AuthProxy IsReady call
+// GIVEN a AuthProxy component
+//  WHEN I call IsReady when all requirements are met
+//  THEN true or false is returned
+func TestIsReady(t *testing.T) {
 	tests := []struct {
-		name   string
-		fields fields
-		args   args
-		want   bool
+		name      string
+		client    client.Client
+		wantError bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Test IsReady when AuthProxy is successfully deployed",
+			client: fake.NewFakeClientWithScheme(testScheme,
+				&appsv1.Deployment{
+					ObjectMeta: metav1.ObjectMeta{
+						Namespace: globalconst.VerrazzanoSystemNamespace,
+						Name:      "verrazzano-authproxy",
+					},
+					Status: appsv1.DeploymentStatus{
+						Replicas:            1,
+						ReadyReplicas:       1,
+						AvailableReplicas:   1,
+						UnavailableReplicas: 0,
+					},
+				}),
+			wantError: false,
+		},
+		{
+			name: "Test IsReady when AuthProxy deployment is not ready",
+			client: fake.NewFakeClientWithScheme(testScheme,
+				&appsv1.Deployment{
+					ObjectMeta: metav1.ObjectMeta{
+						Namespace: globalconst.VerrazzanoSystemNamespace,
+						Name:      "verrazzano-authproxy",
+					},
+					Status: appsv1.DeploymentStatus{
+						Replicas:            1,
+						ReadyReplicas:       1,
+						AvailableReplicas:   0,
+						UnavailableReplicas: 1,
+					},
+				}),
+			wantError: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			c := authProxyComponent{
-				HelmComponent: tt.fields.HelmComponent,
+			ctx := spi.NewFakeContext(tt.client, &vzapi.Verrazzano{}, false)
+			if tt.wantError {
+				assert.False(t, NewComponent().IsReady(ctx))
+			} else {
+				assert.True(t, NewComponent().IsReady(ctx))
 			}
-			assert.Equalf(t, tt.want, c.IsReady(tt.args.ctx), "IsReady(%v)", tt.args.ctx)
 		})
 	}
 }
