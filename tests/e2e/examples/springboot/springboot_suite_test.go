b5ce65bf687ab8cd0584ad2dead0ac9dc6cdf056
Mark Nelson <mark.x.nelson@oracle.com>
2022-02-07 19:23:10 -0500

    fix copyright notices -refs touched by revert
    Signed-off-by: Mark Nelson <mark.x.nelson@oracle.com>

--- tests/e2e/examples/springboot/springboot_suite_test.go
+++ tests/e2e/examples/springboot/springboot_suite_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2020, 2021, Oracle and/or its affiliates.
+// Copyright (c) 2020, 2022, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 package springboot
@@ -7,7 +7,6 @@ import (
 	"flag"
 	"testing"
 
-	"github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
 )
 
