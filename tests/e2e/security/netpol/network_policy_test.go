8b0645cae92a8ecb5f8cf2cd0960dcf5aef157c0
Robert Donat <robert.donat@oracle.com>
2022-02-08 14:19:18 +0000

    fix imports

--- tests/e2e/security/netpol/network_policy_test.go
+++ tests/e2e/security/netpol/network_policy_test.go
@@ -9,10 +9,9 @@ import (
 	"strings"
 	"time"
 
-	"github.com/verrazzano/verrazzano/pkg/k8sutil"
-
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/verrazzano/verrazzano/pkg/k8sutil"
 	"github.com/verrazzano/verrazzano/pkg/test/framework"
 	"github.com/verrazzano/verrazzano/pkg/test/framework/metrics"
 	"github.com/verrazzano/verrazzano/tests/e2e/pkg"
