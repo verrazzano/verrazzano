62c31ab3bc0cf5dccba1806a4205dcff94215cfe
Michael Gianatassio <michael.gianatassio@oracle.com>
2022-02-08 13:34:58 -0800

    add more tests for IsReady

--- platform-operator/controllers/verrazzano/component/authproxy/authproxy_component_test.go
+++ platform-operator/controllers/verrazzano/component/authproxy/authproxy_component_test.go
@@ -62,6 +62,11 @@ func TestIsReady(t *testing.T) {
 				}),
 			wantError: true,
 		},
+		{
+			name:      "Test IsReady when AuthProxy deployment does not exist",
+			client:    fake.NewFakeClientWithScheme(testScheme),
+			wantError: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
