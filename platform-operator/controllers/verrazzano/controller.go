20116de5c87d96c394125418a0f00d9236f17b1e
adiforluls <aditya.b.bharadwaj@oracle.com>
2022-02-09 14:01:25 +0530

    fix golint error

--- platform-operator/controllers/verrazzano/controller.go
+++ platform-operator/controllers/verrazzano/controller.go
@@ -1010,9 +1010,7 @@ func (r *Reconciler) procDelete(ctx context.Context, log vzlog.VerrazzanoLogger,
 		if condition.Type == installv1alpha1.UninstallComplete || condition.Type == installv1alpha1.UninstallFailed {
 			if condition.Type == installv1alpha1.UninstallComplete {
 				log.Once("Successfully uninstalled Verrrazzano")
-				if _, ok := initializedSet[vz.Name]; ok {
-					delete(initializedSet, vz.Name)
-				}
+				delete(initializedSet, vz.Name)
 			} else {
 				log.Once("Failed uninstalling Verraazzano")
 			}
