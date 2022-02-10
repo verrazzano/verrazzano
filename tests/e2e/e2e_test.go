2523f96e062ae134b840aeb05c2ed381afdb7b13
stshapir <steven.shapiro@oracle.com>
2022-02-10 14:16:54 -0500

    refactor
    Signed-off-by: stshapir <steven.shapiro@oracle.com>

--- tests/e2e/e2e_test.go
+++ tests/e2e/e2e_test.go
@@ -11,7 +11,7 @@ import (
 
 	//import tests
 	_ "github.com/verrazzano/verrazzano/tests/e2e/examples/add"
-	_ "github.com/verrazzano/verrazzano/tests/e2e/framework/poc"
+	_ "github.com/verrazzano/verrazzano/tests/e2e/poc"
 )
 
 func TestMain(m *testing.M) {
