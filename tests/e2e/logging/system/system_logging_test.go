65d1c19002279eb57b551c5bc8866f3c4c3fbe7d
Paul Mackin <paul.mackin@oracle.com>
2022-02-15 16:20:07 -0500

    fix index test

--- tests/e2e/logging/system/system_logging_test.go
+++ tests/e2e/logging/system/system_logging_test.go
@@ -261,7 +261,7 @@ func validateVPOLogs() bool {
 func validateVAOLogs() bool {
 	return validateElasticsearchRecords(
 		allElasticsearchRecordValidator,
-		installIndex,
+		systemIndex,
 		"kubernetes.labels.app.keyword",
 		"verrazzano-application-operator",
 		searchTimeWindow,
@@ -271,7 +271,7 @@ func validateVAOLogs() bool {
 func validateVMOLogs() bool {
 	return validateElasticsearchRecords(
 		allElasticsearchRecordValidator,
-		installIndex,
+		systemIndex,
 		"kubernetes.labels.app.keyword",
 		"verrazzano-monitoring-operator",
 		searchTimeWindow,
@@ -281,7 +281,7 @@ func validateVMOLogs() bool {
 func validateVOLogs() bool {
 	return validateElasticsearchRecords(
 		allElasticsearchRecordValidator,
-		installIndex,
+		systemIndex,
 		"kubernetes.labels.app.keyword",
 		"verrazzano-operator",
 		searchTimeWindow,
