16d1b46bc9d6b5fe64e2d82d109dd7d28bde0728
Ravi Singhal <ravi.r.singhal@oracle.com>
2022-02-17 09:32:04 +0530

    remove traits specific to component only

--- application-operator/controllers/metricstrait/metricstrait_controller.go
+++ application-operator/controllers/metricstrait/metricstrait_controller.go
@@ -1127,7 +1127,7 @@ func (r *Reconciler) removedTraitReferencesFromOwner(ctx context.Context, ownerR
 						continue
 					}
 
-					if componentTraitUnstructured.GetAPIVersion() == trait.APIVersion && componentTraitUnstructured.GetKind() == trait.Kind {
+					if componentTraitUnstructured.GetAPIVersion() == trait.APIVersion && componentTraitUnstructured.GetKind() == trait.Kind && trait.Spec.WorkloadReference.Name == component.ComponentName {
 						log.Infof("Removing trait %s/%s for component: %s of application configuration: %s/%s", componentTraitUnstructured.GetAPIVersion(), componentTraitUnstructured.GetKind(), component.ComponentName, appConfig.GetNamespace(), appConfig.GetName())
 					} else {
 						remainingTraits = append(remainingTraits, componentTrait)
