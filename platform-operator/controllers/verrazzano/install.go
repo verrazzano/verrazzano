6c532131594078abcd9ba7809ef5ab0043ddc8fb
Abhishek Mitra <abhishek.a.mitra@oracle.com>
2022-03-02 09:52:42 -0800

    VZ-4869: Restore urls after vz restore op when comp ready

--- platform-operator/controllers/verrazzano/install.go
+++ platform-operator/controllers/verrazzano/install.go
@@ -52,6 +52,10 @@ func (r *Reconciler) reconcileComponents(_ context.Context, spiCtx spi.Component
 			if err := comp.Reconcile(spiCtx); err != nil {
 				return newRequeueWithDelay(), err
 			}
+			// After restore '.status.instance' is empty and not updated. Below change will populate the correct values when comp state is Ready
+			if err := r.updateComponentStatus(compContext, "Component is Ready", vzapi.CondInstallComplete); err != nil {
+				return ctrl.Result{Requeue: true}, err
+			}
 			continue
 		case vzapi.CompStateDisabled:
 			if !comp.IsEnabled(compContext) {
