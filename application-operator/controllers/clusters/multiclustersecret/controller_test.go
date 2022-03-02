67fc1d1a3408a4e22ffb5a0150fa0e6b7e89d60b
Steven Shapiro <31927916+stevshap@users.noreply.github.com>
2022-03-02 11:21:12 -0500

    Stshapir/VZ-3458 (#2654)
    * Adding nameselector to exclude kube-system from all verrazzano webhooks
    * Modifying reconciler to ignore kube-system namespace for all operators
    * Updating controller tests to test kube-system reconcile
    * Fixes VZ-3458
    
    Signed-off-by: stshapir <steven.shapiro@oracle.com>

--- application-operator/controllers/clusters/multiclustersecret/controller_test.go
+++ application-operator/controllers/clusters/multiclustersecret/controller_test.go
@@ -436,3 +436,21 @@ func newSecretReconciler(c client.Client) Reconciler {
 		Scheme: clusters.NewScheme(),
 	}
 }
+
+// TestReconcileKubeSystem tests to make sure we do not reconcile
+// Any resource that belong to the kube-system namespace
+func TestReconcileKubeSystem(t *testing.T) {
+	assert := asserts.New(t)
+
+	var mocker = gomock.NewController(t)
+	var cli = mocks.NewMockClient(mocker)
+
+	// create a request and reconcile it
+	request := clusterstest.NewRequest(kubeSystem, "unit-test-verrazzano-helidon-workload")
+	reconciler := newSecretReconciler(cli)
+	result, err := reconciler.Reconcile(request)
+
+	mocker.Finish()
+	assert.Nil(err)
+	assert.True(result.IsZero())
+}
