2a51a7ef98e3d2eba3f612cdeff23ae24d0882f2
Padmanabha Bhat <padmanabha.bhat@oracle.com>
2022-03-11 17:41:49 +0530

    Include a simple service

---
+++ tests/testdata/test-applications/weblogic/hello-verrazzano/src/main/java/com/oracle/verrazzano/hello/resource/Greetings.java
@@ -0,0 +1,20 @@
+// Copyright (c) 2020, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package com.oracle.verrazzano.hello.resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+public class Greetings {
+
+    @GET
+    @Path("/greet")
+    @Produces((MediaType.TEXT_PLAIN))
+    public Response sayHello() {
+	  return Response.status(200).entity("Hello Verrazzano").build();
+    }
+}
