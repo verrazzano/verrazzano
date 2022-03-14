41947f149fcf7264af993dfd5c892699897c244b
Deva Sagar <deva.sagar@oracle.com>
2022-03-14 11:55:09 -0400

    Rebase, fix copyright

--- tests/e2e/verify-install/web/nginx_errors_test.go
+++ tests/e2e/verify-install/web/nginx_errors_test.go
@@ -25,9 +25,19 @@ const (
 	expected401    = "<html>\n<head><title>401 Unauthorized</title></head>\n<body>\n<center><h1>401 Unauthorized</h1></center>\n</body>\n</html>"
 )
 
+var kubeconfigPath string
+
+var _ = t.BeforeSuite(func() {
+	var err error
+	kubeconfigPath, err = k8sutil.GetKubeConfigLocation()
+	if err != nil {
+		Fail(fmt.Sprintf("Failed to get default kubeconfig path: %s", err.Error()))
+	}
+})
+
 var _ = t.Describe("nginx error pages", Label("f:mesh.ingress", "f:mesh.traffic-mgmt"), func() {
 	t.Context("test that an", func() {
-		t.ItMinimumVersion("Incorrect path returns a 404", minimumVersion, func() {
+		t.ItMinimumVersion("Incorrect path returns a 404", minimumVersion, kubeconfigPath, func() {
 			if !pkg.IsManagedClusterProfile() {
 				Eventually(func() (string, error) {
 					kubeConfigPath, err := k8sutil.GetKubeConfigLocation()
@@ -62,7 +72,7 @@ var _ = t.Describe("nginx error pages", Label("f:mesh.ingress", "f:mesh.traffic-
 			}
 		})
 
-		t.ItMinimumVersion("Incorrect password returns a 401", minimumVersion, func() {
+		t.ItMinimumVersion("Incorrect password returns a 401", minimumVersion, kubeconfigPath, func() {
 			if !pkg.IsManagedClusterProfile() {
 				Eventually(func() (string, error) {
 					kubeConfigPath, err := k8sutil.GetKubeConfigLocation()
@@ -92,7 +102,7 @@ var _ = t.Describe("nginx error pages", Label("f:mesh.ingress", "f:mesh.traffic-
 			}
 		})
 
-		t.ItMinimumVersion("Incorrect host returns a 404", minimumVersion, func() {
+		t.ItMinimumVersion("Incorrect host returns a 404", minimumVersion, kubeconfigPath, func() {
 			if !pkg.IsManagedClusterProfile() && os.Getenv("TEST_ENV") != "ocidns_oke" {
 				Eventually(func() (string, error) {
 					kubeConfigPath, err := k8sutil.GetKubeConfigLocation()
