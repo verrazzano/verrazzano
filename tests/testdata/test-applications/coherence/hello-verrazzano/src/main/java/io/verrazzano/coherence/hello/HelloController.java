4f2df867f57d4328a7955a1852d6aebdeb110504
Padmanabha Bhat <35250350+bhatpmk@users.noreply.github.com>
2022-03-15 09:39:29 +0530

    Update maven group for coherence sample (#2729)
    * Rename maven group and java package
    
    * Rename maven group and java package

--- tests/testdata/test-applications/coherence/hello-verrazzano/src/main/java/io/verrazzano/coherence/hello/HelloController.java
+++ tests/testdata/test-applications/coherence/hello-verrazzano/src/main/java/io/verrazzano/coherence/hello/HelloController.java
@@ -1,7 +1,7 @@
 // Copyright (c) 2022, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-package org.coherence.springboot.hello;
+package io.verrazzano.coherence.hello;
 
 import com.oracle.coherence.spring.configuration.annotation.CoherenceMap;
 
