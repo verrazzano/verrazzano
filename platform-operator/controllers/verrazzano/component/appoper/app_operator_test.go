3632c9d2eabb1132d968cd61633742bb717e23ab
Guoyong Zhang <guoyong.zhang@oracle.com>
2022-03-22 12:17:58 -0400

    VZ-5319 Support weblogic-monitoring-exporter in private registry configurations (#2765)

--- platform-operator/controllers/verrazzano/component/appoper/app_operator_test.go
+++ platform-operator/controllers/verrazzano/component/appoper/app_operator_test.go
@@ -35,14 +35,17 @@ func TestAppendAppOperatorOverrides(t *testing.T) {
 
 	const expectedFluentdImage = "ghcr.io/verrazzano/fluentd-kubernetes-daemonset:v1.12.3-20210517195222-f345ec2"
 	const expectedIstioProxyImage = "ghcr.io/verrazzano/proxyv2:1.7.3"
+	const expectedWeblogicMonitoringExporterImage = "ghcr.io/oracle/weblogic-monitoring-exporter:2.0.4"
 
 	kvs, err := AppendApplicationOperatorOverrides(nil, "", "", "", nil)
 	assert.NoError(err, "AppendApplicationOperatorOverrides returned an error ")
-	assert.Len(kvs, 2, "AppendApplicationOperatorOverrides returned an unexpected number of Key:Value pairs")
+	assert.Len(kvs, 3, "AppendApplicationOperatorOverrides returned an unexpected number of Key:Value pairs")
 	assert.Equalf("fluentdImage", kvs[0].Key, "Did not get expected fluentdImage Key")
 	assert.Equalf(expectedFluentdImage, kvs[0].Value, "Did not get expected fluentdImage Value")
 	assert.Equalf("istioProxyImage", kvs[1].Key, "Did not get expected istioProxyImage Key")
 	assert.Equalf(expectedIstioProxyImage, kvs[1].Value, "Did not get expected istioProxyImage Value")
+	assert.Equalf("weblogicMonitoringExporterImage", kvs[2].Key, "Did not get expected weblogicMonitoringExporterImage Key")
+	assert.Equalf(expectedWeblogicMonitoringExporterImage, kvs[2].Value, "Did not get expected weblogicMonitoringExporterImage Value")
 
 	customImage := "myreg.io/myrepo/v8o/verrazzano-application-operator-dev:local-20210707002801-b7449154"
 	os.Setenv(constants.VerrazzanoAppOperatorImageEnvVar, customImage)
@@ -50,13 +53,15 @@ func TestAppendAppOperatorOverrides(t *testing.T) {
 
 	kvs, err = AppendApplicationOperatorOverrides(nil, "", "", "", nil)
 	assert.NoError(err, "AppendApplicationOperatorOverrides returned an error ")
-	assert.Len(kvs, 3, "AppendApplicationOperatorOverrides returned wrong number of Key:Value pairs")
+	assert.Len(kvs, 4, "AppendApplicationOperatorOverrides returned wrong number of Key:Value pairs")
 	assert.Equalf("image", kvs[0].Key, "Did not get expected image Key")
 	assert.Equalf(customImage, kvs[0].Value, "Did not get expected image Value")
 	assert.Equalf("fluentdImage", kvs[1].Key, "Did not get expected fluentdImage Key")
 	assert.Equalf(expectedFluentdImage, kvs[1].Value, "Did not get expected fluentdImage Value")
 	assert.Equalf("istioProxyImage", kvs[2].Key, "Did not get expected istioProxyImage Key")
 	assert.Equalf(expectedIstioProxyImage, kvs[2].Value, "Did not get expected istioProxyImage Value")
+	assert.Equalf("weblogicMonitoringExporterImage", kvs[3].Key, "Did not get expected weblogicMonitoringExporterImage Key")
+	assert.Equalf(expectedWeblogicMonitoringExporterImage, kvs[3].Value, "Did not get expected weblogicMonitoringExporterImage Value")
 }
 
 // TestIsApplicationOperatorReady tests the isApplicationOperatorReady function
