a1e4769f6900b887f45c13cc890eebc2e69781c9
Fred Tibbitts <fred.tibbitts@oracle.com>
2022-03-23 09:40:25 -0400

    Fix an import

--- platform-operator/controllers/verrazzano/component/verrazzano/verrazzano_test.go
+++ platform-operator/controllers/verrazzano/component/verrazzano/verrazzano_test.go
@@ -19,7 +19,6 @@ import (
 	"time"
 
 	certv1 "github.com/jetstack/cert-manager/pkg/apis/certmanager/v1"
-
 	"github.com/stretchr/testify/assert"
 	vmov1 "github.com/verrazzano/verrazzano-monitoring-operator/pkg/apis/vmcontroller/v1"
 	"github.com/verrazzano/verrazzano/pkg/bom"
