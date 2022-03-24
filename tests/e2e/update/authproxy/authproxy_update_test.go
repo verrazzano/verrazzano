017f574451b1c57adb94dd08c3d2ed4c0eab2d0f
Guoyong Zhang <guoyong.zhang@oracle.com>
2022-03-24 15:09:02 -0400

    authproxy update test

---
+++ tests/e2e/update/authproxy/authproxy_update_test.go
@@ -0,0 +1,170 @@
+// Copyright (c) 2022, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package authproxy
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/verrazzano/verrazzano/pkg/k8sutil"
+	vzapi "github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1alpha1"
+	vpoClient "github.com/verrazzano/verrazzano/platform-operator/clients/verrazzano/clientset/versioned"
+
+	"github.com/verrazzano/verrazzano/pkg/constants"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/verrazzano/verrazzano/pkg/test/framework"
+	"github.com/verrazzano/verrazzano/tests/e2e/pkg"
+)
+
+const (
+	authProxyName   = "verrazzano-authproxy"
+	waitTimeout     = 3 * time.Minute
+	pollingInterval = 5 * time.Second
+)
+
+var t = framework.NewTestFramework("update authproxy")
+
+var vzcr *vzapi.Verrazzano
+var kubeconfigPath string
+var nodeCount uint32 = 1
+
+var _ = t.BeforeSuite(func() {
+	kindNodeCount := os.Getenv("KIND_NODE_COUNT")
+	if len(kindNodeCount) > 0 {
+		u, err := strconv.ParseUint(kindNodeCount, 10, 32)
+		if err != nil {
+			nodeCount = uint32(u)
+		}
+	}
+
+	var err error
+	kubeconfigPath, err = k8sutil.GetKubeConfigLocation()
+	if err != nil {
+		Fail(fmt.Sprintf("Failed to get default kubeconfig path: %s", err.Error()))
+	}
+
+	// Get the Verrazzano CR
+	Eventually(func() error {
+		var err error
+		vzcr, err = pkg.GetVerrazzano()
+		if err != nil {
+			return err
+		}
+		return err
+	}, waitTimeout, pollingInterval).Should(BeNil(), "Expected to get Verrazzano CR")
+})
+var _ = t.AfterSuite(func() {})
+var _ = t.AfterEach(func() {})
+
+var _ = t.Describe("Update authProxy", Label("f:platform-lcm.update"), func() {
+	t.Describe("verrazzano-authproxy verify", Label("f:platform-lcm.authproxy-verify"), func() {
+		t.It("authproxy default replicas", func() {
+			expectedRunning := uint32(1)
+			expectedPending := uint32(0)
+			if vzcr.Spec.Profile == "production" || vzcr.Spec.Profile == "" {
+				expectedRunning = 2
+			}
+			validatePods(authProxyName, constants.VerrazzanoSystemNamespace, expectedRunning, expectedPending)
+		})
+	})
+
+	t.Describe("verrazzano-authproxy update replicas", Label("f:platform-lcm.authproxy-update-replicas"), func() {
+		t.It("authproxy explicit replicas", func() {
+			vzcr.Spec.Components.AuthProxy.Kubernetes.Replicas = nodeCount
+			err := updateCR(vzcr)
+			if err != nil {
+				Fail(err.Error())
+			}
+
+			expectedRunning := nodeCount
+			expectedPending := uint32(0)
+			validatePods(authProxyName, constants.VerrazzanoSystemNamespace, expectedRunning, expectedPending)
+		})
+	})
+
+	t.Describe("verrazzano-authproxy update affinity", Label("f:platform-lcm.authproxy-update-affinity"), func() {
+		t.It("authproxy explicit affinity", func() {
+			vzcr.Spec.Components.AuthProxy.Kubernetes.Affinity = &corev1.Affinity{
+				PodAntiAffinity: &corev1.PodAntiAffinity{
+					RequiredDuringSchedulingIgnoredDuringExecution: []corev1.PodAffinityTerm{
+						{
+							LabelSelector: &metav1.LabelSelector{
+								MatchLabels: nil,
+								MatchExpressions: []metav1.LabelSelectorRequirement{
+									{
+										Key:      "app",
+										Operator: "In",
+										Values: []string{
+											authProxyName,
+										},
+									},
+								},
+							},
+							TopologyKey: "kubernetes.io/hostname",
+						},
+					},
+				},
+			}
+			err := updateCR(vzcr)
+			if err != nil {
+				Fail(err.Error())
+			}
+
+			expectedRunning := nodeCount - 1
+			expectedPending := uint32(2)
+			if nodeCount == 1 {
+				expectedRunning = nodeCount
+				expectedPending = uint32(0)
+			}
+			validatePods(authProxyName, constants.VerrazzanoSystemNamespace, expectedRunning, expectedPending)
+		})
+	})
+})
+
+func validatePods(deployName string, nameSpace string, expectedPodsRunning uint32, expectedPodsPending uint32) {
+	var pods []corev1.Pod
+	Eventually(func() bool {
+		var err error
+		pods, err = pkg.GetPodsFromSelector(&metav1.LabelSelector{MatchLabels: map[string]string{"app": deployName}}, nameSpace)
+		if err != nil {
+			return false
+		}
+		// Compare the number of running/pending pods to the expected numbers
+		var runningPods uint32 = 0
+		var pendingPods uint32 = 0
+		for _, pod := range pods {
+			if pod.Status.Phase == corev1.PodRunning {
+				runningPods++
+			}
+			if pod.Status.Phase == corev1.PodPending {
+				pendingPods++
+			}
+		}
+		return runningPods == expectedPodsRunning && pendingPods == expectedPodsPending
+	}, waitTimeout, pollingInterval).Should(BeTrue())
+}
+
+func updateCR(cr *vzapi.Verrazzano) error {
+	config, err := k8sutil.GetKubeConfigGivenPath(kubeconfigPath)
+	if err != nil {
+		return err
+	}
+	client, err := vpoClient.NewForConfig(config)
+	if err != nil {
+		return err
+	}
+	vzClient := client.VerrazzanoV1alpha1().Verrazzanos("")
+	_, err = vzClient.Update(context.TODO(), cr, metav1.UpdateOptions{})
+	if err != nil {
+		return fmt.Errorf("error updating Verrazzano instance: %v", err)
+	}
+	return nil
+}
