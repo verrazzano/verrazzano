17add37fdceb22d3947745ec742cdb1df28efb68
adiforluls <aditya.b.bharadwaj@oracle.com>
2022-03-25 15:56:59 +0530

    remove timestamp field from metrics

--- ci/scripts/metric_emit.sh
+++ ci/scripts/metric_emit.sh
@@ -17,12 +17,11 @@ TIMESTAMP=$(date +%s)
 TIME_METRIC=""
 if [ $DURATION -gt 0 ]
 then
-    TIME_METRIC="${JOB}_time{${LABELS}} $DURATION $TIMESTAMP"
+    TIME_METRIC="${JOB}_time{${LABELS}} $DURATION"
 fi
 
-echo "aditya_test_job_metric 123" | curl -i --data-binary @- ${PROMETHEUS_GW_URL}/metrics/job/${JOB}/instance/${INSTANCE} -u $PROMETHEUS_CRED
 echo "Sending to ${PROMETHEUS_GW_URL}/metrics/job/${JOB}"
 cat <<EOF | curl -i --data-binary @- ${PROMETHEUS_GW_URL}/metrics/job/${JOB}/instance/${INSTANCE} -u $PROMETHEUS_CRED
-${JOB}_status{${LABELS}} $STATUS $TIMESTAMP
+${JOB}_status{${LABELS}} $STATUS
 ${TIME_METRIC}
 EOF
