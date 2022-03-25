e35b1218a44d1b4546303b8c7544325047f5b6b1
adiforluls <aditya.b.bharadwaj@oracle.com>
2022-03-25 14:29:07 +0530

    check pgw push

--- ci/scripts/metric_emit.sh
+++ ci/scripts/metric_emit.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright (c) 2021, Oracle and/or its affiliates.
+# Copyright (c) 2021, 2022, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 #
 PROMETHEUS_GW_URL=$1
@@ -19,6 +19,8 @@ if [ $DURATION -gt 0 ]
 then
     TIME_METRIC="${JOB}_time{${LABELS}} $DURATION $TIMESTAMP"
 fi
+
+echo "aditya_test_job_metric 123" | curl -i --data-binary @- ${PROMETHEUS_GW_URL}/metrics/job/${JOB}/instance/${INSTANCE} -u $PROMETHEUS_CRED
 echo "Sending to ${PROMETHEUS_GW_URL}/metrics/job/${JOB}"
 cat <<EOF | curl -i --data-binary @- ${PROMETHEUS_GW_URL}/metrics/job/${JOB}/instance/${INSTANCE} -u $PROMETHEUS_CRED
 ${JOB}_status{${LABELS}} $STATUS $TIMESTAMP
