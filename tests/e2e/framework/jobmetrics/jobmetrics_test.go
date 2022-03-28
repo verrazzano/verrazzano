301a1c6d7c83ea02c1eb4e7f283cff62d5c28b14
ben.mclean <ben.mclean@oracle.com>
2022-03-28 13:59:45 -0400

    add context

--- tests/e2e/framework/jobmetrics/jobmetrics_test.go
+++ tests/e2e/framework/jobmetrics/jobmetrics_test.go
@@ -13,11 +13,13 @@ var t = framework.NewTestFramework("jobmetrics")
 
 var _ = t.AfterEach(func() {})
 
-var _ = t.Describe("Emit job metrics,", func() {
-	t.It("at the end of each job", func() {
-		t.Metrics.With("job_duration", os.Getenv("DURATION")).
-			With("job_status", os.Getenv("JOB_STATUS")).
-			Info()
-		metrics.Emit(t.Metrics)
+var _ = t.Describe("Emit job metrics", func() {
+	t.Context("for the job", func() {
+		t.It("at the end of each job", func() {
+			t.Metrics.With("job_duration", os.Getenv("DURATION")).
+				With("job_status", os.Getenv("JOB_STATUS")).
+				Info()
+			metrics.Emit(t.Metrics)
+		})
 	})
 })
