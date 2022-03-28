6e8095b699ce8d18494ae721fe493dd55b76b876
ben.mclean <ben.mclean@oracle.com>
2022-03-28 10:09:31 -0400

    after each

--- tests/e2e/framework/jobmetrics/jobmetrics_test.go
+++ tests/e2e/framework/jobmetrics/jobmetrics_test.go
@@ -10,6 +10,8 @@ import (
 
 var t = framework.NewTestFramework("jobmetrics")
 
+var _ = t.AfterEach(func() {})
+
 var _ = t.Describe("Emit job metrics,", func() {
 	t.It("at the end of each job", func() {
 		t.Metrics.With("job_duration", os.Getenv("DURATION"))
