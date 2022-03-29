0417009df419b93f324b8b55f44a8cf8d27fa6d6
ben.mclean <ben.mclean@oracle.com>
2022-03-29 11:37:23 -0400

    no eventually

--- tests/e2e/framework/jobmetrics/jobmetrics_test.go
+++ tests/e2e/framework/jobmetrics/jobmetrics_test.go
@@ -5,17 +5,7 @@ package jobmetrics
 
 import (
 	"github.com/verrazzano/verrazzano/pkg/test/framework"
-	"github.com/verrazzano/verrazzano/tests/e2e/pkg"
-	v1 "k8s.io/api/core/v1"
 	"os"
-	"time"
-
-	. "github.com/onsi/gomega"
-)
-
-const (
-	longWaitTimeout     = 10 * time.Minute
-	longPollingInterval = 20 * time.Second
 )
 
 var t = framework.NewTestFramework("jobmetrics")
@@ -23,13 +13,8 @@ var t = framework.NewTestFramework("jobmetrics")
 var _ = t.AfterEach(func() {})
 
 var _ = t.Describe("Emit job metrics,", func() {
-	t.Context("application Deployment.", func() {
-		t.It("at the end of each job", func() {
-			t.Metrics = t.Metrics.With("job_duration", os.Getenv("DURATION")).
-				With("job_status", os.Getenv("JOB_STATUS"))
-			Eventually(func() (*v1.SecretList, error) {
-				return pkg.ListSecrets("verrazzano-system")
-			}, longWaitTimeout, longPollingInterval).ShouldNot(BeNil())
-		})
+	t.It("at the end of each job", func() {
+		t.Metrics = t.Metrics.With("job_duration", os.Getenv("DURATION")).
+			With("job_status", os.Getenv("JOB_STATUS"))
 	})
 })
