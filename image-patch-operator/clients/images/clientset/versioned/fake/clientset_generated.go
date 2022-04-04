782125f3cb310d053a91cf394f5c96ebfc061577
Bryan Tidd <bryan.tidd@oracle.com>
2022-04-04 16:22:45 -0400

    VZ-5560 updating from image-patch-operator

--- image-patch-operator/clients/images/clientset/versioned/fake/clientset_generated.go
+++ image-patch-operator/clients/images/clientset/versioned/fake/clientset_generated.go
@@ -61,7 +61,10 @@ func (c *Clientset) Tracker() testing.ObjectTracker {
 	return c.tracker
 }
 
-var _ clientset.Interface = &Clientset{}
+var (
+	_ clientset.Interface = &Clientset{}
+	_ testing.FakeClient  = &Clientset{}
+)
 
 // ImagesV1alpha1 retrieves the ImagesV1alpha1Client
 func (c *Clientset) ImagesV1alpha1() imagesv1alpha1.ImagesV1alpha1Interface {
--- image-patch-operator/clients/images/clientset/versioned/typed/images/v1alpha1/fake/fake_imagebuildrequest.go
+++ image-patch-operator/clients/images/clientset/versioned/typed/images/v1alpha1/fake/fake_imagebuildrequest.go
@@ -104,7 +104,7 @@ func (c *FakeImageBuildRequests) UpdateStatus(ctx context.Context, imageBuildReq
 // Delete takes name of the imageBuildRequest and deletes it. Returns an error if one occurs.
 func (c *FakeImageBuildRequests) Delete(ctx context.Context, name string, opts v1.DeleteOptions) error {
 	_, err := c.Fake.
-		Invokes(testing.NewDeleteAction(imagebuildrequestsResource, c.ns, name), &v1alpha1.ImageBuildRequest{})
+		Invokes(testing.NewDeleteActionWithOptions(imagebuildrequestsResource, c.ns, name, opts), &v1alpha1.ImageBuildRequest{})
 
 	return err
 }
