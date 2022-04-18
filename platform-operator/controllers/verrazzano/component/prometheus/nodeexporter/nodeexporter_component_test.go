0add53466854598935a1f7c81fc4c4dcf5024498
Deva Sagar <deva.sagar@oracle.com>
2022-04-18 14:08:30 -0400

    add appendoverrides unit test

--- platform-operator/controllers/verrazzano/component/prometheus/nodeexporter/nodeexporter_component_test.go
+++ platform-operator/controllers/verrazzano/component/prometheus/nodeexporter/nodeexporter_component_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/verrazzano/verrazzano/pkg/bom"
 	vzapi "github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1alpha1"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/spi"
 )
@@ -72,6 +73,70 @@ func TestIsEnabled(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			ctx := spi.NewFakeContext(nil, &tests[i].actualCR, false, profilesRelativePath)
 			assert.Equal(t, tt.expectTrue, NewComponent().IsEnabled(ctx.EffectiveCR()))
+		})
+	}
+}
+
+// TestAppendOverrides tests whether the prometheus.monitor.enabled setting for enabling
+// service monitor, is overridden and set to true when Prometheus Operator is also enabled, but
+// not otherwise
+func TestAppendOverrides(t *testing.T) {
+	trueValue := true
+	tests := []struct {
+		name              string
+		actualCR          vzapi.Verrazzano
+		expectedOverrides []bom.KeyValue
+	}{
+		{
+			// GIVEN a Verrazzano custom resource with the Prometheus Node-Exporter enabled, but not Prometheus Operator
+			// WHEN we call AppendOverrides on the Prometheus Node-Exporter component
+			// THEN prometheus.monitor.enabled is NOT set
+			name: "Test AppendOverrides when Prometheus operator is not also enabled",
+			actualCR: vzapi.Verrazzano{
+				Spec: vzapi.VerrazzanoSpec{
+					Components: vzapi.ComponentSpec{
+						PrometheusNodeExporter: &vzapi.PrometheusNodeExporterComponent{
+							MonitoringComponent: vzapi.MonitoringComponent{
+								Enabled: &trueValue,
+							},
+						},
+					},
+				},
+			},
+			expectedOverrides: []bom.KeyValue{},
+		},
+		{
+			// GIVEN a Verrazzano custom resource with the Prometheus Node-Exporter AND Prometheus Operator enabled
+			// WHEN we call AppendOverrides on the Prometheus Node-Exporter component
+			// THEN prometheus.monitor.enabled is set to true
+			name: "Test AppendOverrides when Prometheus operator is also enabled",
+			actualCR: vzapi.Verrazzano{
+				Spec: vzapi.VerrazzanoSpec{
+					Components: vzapi.ComponentSpec{
+						PrometheusNodeExporter: &vzapi.PrometheusNodeExporterComponent{
+							MonitoringComponent: vzapi.MonitoringComponent{
+								Enabled: &trueValue,
+							},
+						},
+						PrometheusOperator: &vzapi.PrometheusOperatorComponent{
+							Enabled: &trueValue,
+						},
+					},
+				},
+			},
+			expectedOverrides: []bom.KeyValue{bom.KeyValue{Key: "prometheus.monitor.enabled", Value: "true"}},
+		},
+	}
+
+	for i, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := spi.NewFakeContext(nil, &tests[i].actualCR, false, profilesRelativePath)
+			var err error
+			kvs := make([]bom.KeyValue, 0)
+			kvs, err = AppendOverrides(ctx, "", "", "", kvs)
+			assert.NoError(t, err)
+			assert.Len(t, kvs, len(tt.expectedOverrides))
+			assert.Equal(t, tt.expectedOverrides, kvs)
 		})
 	}
 }
