110dbc8b666702a8ca2c899358fe428fc04aa1ad
ben.mclean <ben.mclean@oracle.com>
2022-04-19 13:36:03 -0400

    fixed more unit tests

--- platform-operator/controllers/verrazzano/component/registry/registry_test.go
+++ platform-operator/controllers/verrazzano/component/registry/registry_test.go
@@ -4,6 +4,7 @@
 package registry
 
 import (
+	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/opensearch"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -52,7 +53,7 @@ func TestGetComponents(t *testing.T) {
 	a := assert.New(t)
 	comps := GetComponents()
 
-	a.Len(comps, 18, "Wrong number of components")
+	a.Len(comps, 19, "Wrong number of components")
 	a.Equal(comps[0].Name(), oam.ComponentName)
 	a.Equal(comps[1].Name(), appoper.ComponentName)
 	a.Equal(comps[2].Name(), istio.ComponentName)
@@ -62,15 +63,16 @@ func TestGetComponents(t *testing.T) {
 	a.Equal(comps[6].Name(), externaldns.ComponentName)
 	a.Equal(comps[7].Name(), rancher.ComponentName)
 	a.Equal(comps[8].Name(), verrazzano.ComponentName)
-	a.Equal(comps[9].Name(), authproxy.ComponentName)
-	a.Equal(comps[10].Name(), coherence.ComponentName)
-	a.Equal(comps[11].Name(), mysql.ComponentName)
-	a.Equal(comps[12].Name(), keycloak.ComponentName)
-	a.Equal(comps[13].Name(), kiali.ComponentName)
-	a.Equal(comps[14].Name(), promoperator.ComponentName)
-	a.Equal(comps[15].Name(), promadapter.ComponentName)
-	a.Equal(comps[16].Name(), kubestatemetrics.ComponentName)
-	a.Equal(comps[17].Name(), pushgateway.ComponentName)
+	a.Equal(comps[9].Name(), opensearch.ComponentName)
+	a.Equal(comps[10].Name(), authproxy.ComponentName)
+	a.Equal(comps[11].Name(), coherence.ComponentName)
+	a.Equal(comps[12].Name(), mysql.ComponentName)
+	a.Equal(comps[13].Name(), keycloak.ComponentName)
+	a.Equal(comps[14].Name(), kiali.ComponentName)
+	a.Equal(comps[15].Name(), promoperator.ComponentName)
+	a.Equal(comps[16].Name(), promadapter.ComponentName)
+	a.Equal(comps[17].Name(), kubestatemetrics.ComponentName)
+	a.Equal(comps[18].Name(), pushgateway.ComponentName)
 }
 
 // TestFindComponent tests FindComponent
