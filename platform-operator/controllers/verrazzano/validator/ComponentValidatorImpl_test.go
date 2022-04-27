9fc247722d79d9892a7659fcd25732b6320d4271
Robert Donat <robert.donat@oracle.com>
2022-04-27 22:06:02 +0000

    fix unit tests

--- platform-operator/controllers/verrazzano/validator/ComponentValidatorImpl_test.go
+++ platform-operator/controllers/verrazzano/validator/ComponentValidatorImpl_test.go
@@ -43,7 +43,7 @@ func TestComponentValidatorImpl_ValidateInstall(t *testing.T) {
 					},
 				},
 			},
-			numberOfErrors: 9,
+			numberOfErrors: 8,
 		},
 	}
 	config.TestProfilesDir = "../../../manifests/profiles"
@@ -119,7 +119,7 @@ func TestComponentValidatorImpl_ValidateUpdate(t *testing.T) {
 					},
 				},
 			},
-			numberOfErrors: 11,
+			numberOfErrors: 10,
 		},
 	}
 	config.TestProfilesDir = "../../../manifests/profiles"
@@ -160,7 +160,7 @@ func Test_dependencyValidation(t *testing.T) {
 					},
 				},
 			},
-			numberOfErrors: 9,
+			numberOfErrors: 8,
 		},
 		{
 			name: "disabled istio",
