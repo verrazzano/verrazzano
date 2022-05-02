7b6127c384ac1e1dd6a189d52e3a6018e280be0b
adiforluls <aditya.b.bharadwaj@oracle.com>
2022-05-02 16:44:59 +0530

    Simple test

--- tests/e2e/overrides/overrides_test.go
+++ tests/e2e/overrides/overrides_test.go
@@ -1,71 +1,124 @@
 package overrides
 
 import (
+	"context"
+	"fmt"
+	"github.com/onsi/ginkgo/v2"
+	"github.com/onsi/gomega"
+	"github.com/verrazzano/verrazzano/pkg/k8sutil"
 	"github.com/verrazzano/verrazzano/pkg/test/framework"
 	vzapi "github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1alpha1"
-	"github.com/verrazzano/verrazzano/tests/e2e/update"
-	v1 "k8s.io/api/core/v1"
+	vpoClient "github.com/verrazzano/verrazzano/platform-operator/clients/verrazzano/clientset/versioned"
+	"github.com/verrazzano/verrazzano/platform-operator/constants"
+	"github.com/verrazzano/verrazzano/tests/e2e/pkg"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"time"
+)
+
+const (
+	waitTimeOut     = 5 * time.Minute
+	pollingInterval = 5 * time.Second
 )
 
 var (
-	t              = framework.NewTestFramework("overrides")
-	enableData     = `{"prometheusOperator": {"enable": true}}`
-	disableData    = `{"prometheusOperator": {"enable": false}}`
-	deploymentName = "prometheus-operator-kube-p-operator"
+	t             = framework.NewTestFramework("overrides")
+	overrideData0 = `{"prometheusOperator":{"podLabels":{"override": "true"}}}`
+	overrideData1 = `{"prometheusOperator":{"podLabels":{"override": "false"}}}`
 )
 
-type OverrideDisableModifier struct {
-}
+type OverrideModifier struct{}
 
-type OverrideEnableModifier struct {
-}
+var _ = t.BeforeSuite(func() {
+	t.Logs.Info("Create ConfigMap")
+	gomega.Eventually(func() error {
+		configMap := corev1.ConfigMap{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "po-val",
+				Namespace: "default",
+			},
+			Immutable:  nil,
+			Data:       map[string]string{"values.yaml": overrideData0},
+			BinaryData: nil,
+		}
+		err := CreateConfigMap(&configMap)
+		return err
+	}, waitTimeOut, pollingInterval).Should(gomega.BeNil())
 
-var _ = t.Describe("Test helm overrides post install", func() {
-	t.BeforeEach(func() {
-		update.GetCR()
-	})
+	t.Logs.Info("Update VZ with overrides and wait for Re-Install to complete")
+	gomega.Eventually(func() bool {
+		cr, err := pkg.GetVerrazzano()
+		if err != nil {
+			ginkgo.AbortSuite("Couldn't get the Verrazzano resource")
+		}
+		overrides := []vzapi.Overrides{
+			{
+				ConfigMapRef: &corev1.ConfigMapKeySelector{
+					LocalObjectReference: corev1.LocalObjectReference{
+						Name: "po-val",
+					},
+					Key:      "values.yaml",
+					Optional: nil,
+				},
+			},
+		}
+		watchValues := true
+		cr.Spec.Components.PrometheusOperator.ValueOverrides = overrides
+		cr.Spec.Components.PrometheusOperator.MonitorChanges = &watchValues
+		UpdateVZ(cr)
 
-	t.AfterEach(func() {
-
-	})
-
-	t.Context("disable prometheus operator", func() {
-		// TODO: disable prometheus operator and check that deployment is not found
-	})
-
-	t.Context("re-enable prometheus operator", func() {
-		// TODO: re-enable prometheus operator and check that re-install is successful
-	})
+		cr, err = pkg.GetVerrazzano()
+		return cr.Status.State == "Ready"
+	}, waitTimeOut, pollingInterval).Should(gomega.BeTrue())
+	beforeSuitePassed = true
 })
 
-func (o OverrideDisableModifier) ModifyCR(vz *vzapi.Verrazzano) {
-	overrides := []vzapi.Overrides{
-		{
-			ConfigMapRef: &v1.ConfigMapKeySelector{
-				LocalObjectReference: v1.LocalObjectReference{
-					Name: "po-values",
-				},
-				Key:      "values.yaml",
-				Optional: nil,
-			},
-		},
-	}
-	WatchValues := true
-	vz.Spec.Components.PrometheusOperator.ValueOverrides = overrides
-	vz.Spec.Components.PrometheusOperator.MonitorChanges = &WatchValues
-}
+var failed = false
+var beforeSuitePassed = false
+var _ = t.AfterEach(func() {
+	failed = failed || ginkgo.CurrentSpecReport().Failed()
+})
 
-func (o OverrideEnableModifier) ModifyCR(vz *vzapi.Verrazzano) {
-	overrides := vz.Spec.Components.PrometheusOperator.ValueOverrides
-	overrides = append([]vzapi.Overrides{
-		{
-			ConfigMapRef: &v1.ConfigMapKeySelector{
-				LocalObjectReference: v1.LocalObjectReference{
-					Name: "po-values",
-				},
-				Key:      "values1.yaml",
-				Optional: nil,
-			},
-		},
-	}, overrides...)
+var _ = t.AfterSuite(func() {
+	if failed || !beforeSuitePassed {
+		pkg.ExecuteClusterDumpWithEnvVarConfig()
+	}
+})
+
+var _ = t.Describe("Verify Override Post Upgrade", func() {
+	t.It("Prometheus Operator pod should have the overriden label",
+		gomega.Eventually(
+			func() bool {
+				pods, err := pkg.GetPodsFromSelector(&metav1.LabelSelector{
+					MatchLabels: map[string]string{"override": "true"},
+				}, constants.VerrazzanoMonitoringNamespace)
+
+				if err != nil {
+					ginkgo.AbortSuite(fmt.Sprintf("Label override not found with the given error: %v", err))
+				}
+
+				return len(pods) == 1
+			}, waitTimeOut, pollingInterval).Should(gomega.BeTrue()))
+})
+
+func UpdateVZ(cr *vzapi.Verrazzano) {
+	var err error
+	kubeconfigPath, err := k8sutil.GetKubeConfigLocation()
+	if err != nil {
+		ginkgo.AbortSuite(fmt.Sprintf("Failed to get kubeconfig location with error: %v", err))
+	}
+	config, err := k8sutil.GetKubeConfigGivenPath(kubeconfigPath)
+	if err != nil {
+		ginkgo.AbortSuite(fmt.Sprintf("Failed to get kubeconfig with error: %v", err))
+	}
+
+	vzClient, err := vpoClient.NewForConfig(config)
+	if err != nil {
+		ginkgo.AbortSuite(fmt.Sprintf("Failed to get verrazzano client with error: %v", err))
+	}
+
+	_, err = vzClient.VerrazzanoV1alpha1().Verrazzanos(cr.Namespace).Update(context.TODO(), cr, metav1.UpdateOptions{})
+	if err != nil {
+		ginkgo.AbortSuite(fmt.Sprintf("Failed to update Verrazzano with error: %v", err))
+	}
 }
--- tests/e2e/overrides/values.go
+++ tests/e2e/overrides/values.go
@@ -4,13 +4,12 @@ import (
 	"context"
 	ginkgov2 "github.com/onsi/ginkgo/v2"
 	"github.com/verrazzano/verrazzano/pkg/k8sutil"
-	appsv1 "k8s.io/api/apps/v1"
+	"github.com/verrazzano/verrazzano/tests/e2e/pkg"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/types"
 )
 
-func CreateConfigMap(configMap *v1.ConfigMap) {
+func CreateConfigMap(configMap *v1.ConfigMap) error {
 	kubeconfigPath, err := k8sutil.GetKubeConfigLocation()
 	if err != nil {
 		ginkgov2.Fail(err.Error())
@@ -26,35 +25,15 @@ func CreateConfigMap(configMap *v1.ConfigMap) {
 		ginkgov2.Fail(err.Error())
 	}
 
-	clientset.CoreV1().ConfigMaps(configMap.Namespace).Create(context.TODO(), configMap, metav1.CreateOptions{})
-}
-
-func GetConfigMap(namespace string, name string) (*v1.ConfigMap, error) {
-	kubeconfigPath, err := k8sutil.GetKubeConfigLocation()
+	configMap, err = clientset.CoreV1().ConfigMaps(configMap.Namespace).Create(context.TODO(), configMap, metav1.CreateOptions{})
 	if err != nil {
-		ginkgov2.Fail(err.Error())
+		return err
 	}
-
-	kubeconfig, err := k8sutil.GetKubeConfigGivenPath(kubeconfigPath)
-	if err != nil {
-		ginkgov2.Fail(err.Error())
-	}
-
-	clientset, err := k8sutil.GetKubernetesClientsetWithConfig(kubeconfig)
-	if err != nil {
-		ginkgov2.Fail(err.Error())
-	}
-
-	configMap, err := clientset.CoreV1().ConfigMaps(namespace).Get(context.TODO(), name, metav1.GetOptions{})
-	if err != nil {
-		ginkgov2.Fail(err.Error())
-	}
-
-	return configMap, nil
+	return nil
 }
 
 func UpdateConfigMap(namespace string, name string, data []byte) {
-	configMap, err := GetConfigMap(namespace, name)
+	configMap, err := pkg.GetConfigMap(name, namespace)
 	if err != nil {
 		ginkgov2.Fail(err.Error())
 	}
@@ -78,29 +57,4 @@ func UpdateConfigMap(namespace string, name string, data []byte) {
 
 	clientset.CoreV1().ConfigMaps(namespace).Update(context.TODO(), configMap, metav1.UpdateOptions{})
 
-}
-
-func GetDeployment(nsn types.NamespacedName) (*appsv1.Deployment, error) {
-	kubeconfigPath, err := k8sutil.GetKubeConfigLocation()
-	if err != nil {
-		ginkgov2.Fail(err.Error())
-	}
-
-	kubeconfig, err := k8sutil.GetKubeConfigGivenPath(kubeconfigPath)
-	if err != nil {
-		ginkgov2.Fail(err.Error())
-	}
-
-	clientset, err := k8sutil.GetKubernetesClientsetWithConfig(kubeconfig)
-	if err != nil {
-		ginkgov2.Fail(err.Error())
-	}
-
-	deployment, err := clientset.AppsV1().Deployments(nsn.Namespace).Get(context.TODO(), nsn.Name, metav1.GetOptions{})
-
-	if err != nil {
-		return nil, err
-	}
-
-	return deployment, err
 }
