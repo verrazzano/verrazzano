65828502f1908e213423e8961a0c9dfe7f2436d3
Davis Gomes <49332517+davisgomes@users.noreply.github.com>
2022-05-02 10:51:24 -0400

    VZ-5716 Arbitrary Helm value implementation with new YAML parser (#3036)
    Co-authored-by: davisgomes <davis.gomes@oracle.com>
    Co-authored-by: adiforluls <aditya.b.bharadwaj@oracle.com>

--- platform-operator/apis/verrazzano/v1alpha1/validate_test.go
+++ platform-operator/apis/verrazzano/v1alpha1/validate_test.go
@@ -1631,31 +1631,27 @@ func TestValidateHelmValueOverrides(t *testing.T) {
 	testNoOverride := []Overrides{{}}
 
 	testBadOverride := []Overrides{
-		{ConfigMapRef: &ConfigMapRef{
-			ConfigMapKeySelector: &corev1.ConfigMapKeySelector{
+		{
+			ConfigMapRef: &corev1.ConfigMapKeySelector{
 				LocalObjectReference: corev1.LocalObjectReference{},
 				Key:                  "",
 				Optional:             nil,
 			},
-		},
-			SecretRef: &SecretRef{
-				SecretKeySelector: &corev1.SecretKeySelector{
-					LocalObjectReference: corev1.LocalObjectReference{},
-					Key:                  "",
-					Optional:             nil,
-				},
+			SecretRef: &corev1.SecretKeySelector{
+				LocalObjectReference: corev1.LocalObjectReference{},
+				Key:                  "",
+				Optional:             nil,
 			},
 		},
 	}
 
 	testGoodOverride := []Overrides{
-		{ConfigMapRef: &ConfigMapRef{
-			ConfigMapKeySelector: &corev1.ConfigMapKeySelector{
+		{
+			ConfigMapRef: &corev1.ConfigMapKeySelector{
 				LocalObjectReference: corev1.LocalObjectReference{},
 				Key:                  "",
 				Optional:             nil,
 			},
-		},
 		},
 	}
 
---
+++ platform-operator/internal/yaml/helm.go
@@ -0,0 +1,36 @@
+// Copyright (c) 2021, 2022, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package yaml
+
+import (
+	"fmt"
+	"github.com/verrazzano/verrazzano/pkg/bom"
+	"helm.sh/helm/v3/pkg/strvals"
+	"sigs.k8s.io/yaml"
+	"strings"
+)
+
+// HelmValueFileConstructor creates a YAML file from a set of key value pairs
+func HelmValueFileConstructor(kvs []bom.KeyValue) (string, error) {
+	yamlObject := map[string]interface{}{}
+	for _, kv := range kvs {
+		// replace unwanted characters in the value to avoid splitting
+		ignoreChars := ",[.{}"
+		for _, char := range ignoreChars {
+			kv.Value = strings.Replace(kv.Value, string(char), "\\"+string(char), -1)
+		}
+
+		composedStr := fmt.Sprintf("%s=%s", kv.Key, kv.Value)
+		err := strvals.ParseInto(composedStr, yamlObject)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	yamlFile, err := yaml.Marshal(yamlObject)
+	if err != nil {
+		return "", err
+	}
+	return string(yamlFile), nil
+}
