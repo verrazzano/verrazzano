3642165732e8984240224b323df06f3a78ac8982
Abhishek Mitra <abhishek.a.mitra@oracle.com>
2022-05-03 08:32:32 -0700

    VZ-5799: mysql backup hook

---
+++ platform-operator/scripts/hooks/mysql-hook.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+BACKUP_DIR="/var/lib/mysql/data-backup"
+backup() {
+  FILE_PATH=${BACKUP_DIR}/$1
+  mysqldump --all-databases --single-transaction --quick --lock-tables=false > ${FILE_PATH} -u root -p${MYSQL_ROOT_PASSWORD}
+  if [ $? -eq 0 ]; then
+         echo "Mysqldump  successful"
+         exit 0
+    else
+        echo "Mysqldump failed"
+        exit 1
+  fi
+}
+
+restore() {
+  FILE_PATH=${BACKUP_DIR}/$1
+
+  if test -f "${FILE_PATH}"; then
+      echo "'${FILE_PATH}' exists."
+  else
+     echo "'${FILE_PATH}' does not exist"
+     exit 1
+  fi
+
+  while ! mysqladmin ping -u root -p${MYSQL_ROOT_PASSWORD} --silent; do
+          sleep 5
+  done
+  sleep 10
+
+  mysql -u root -p${MYSQL_ROOT_PASSWORD} < ${FILE_PATH}
+  if [ $? -eq 0 ]; then
+       echo "Mysql restore successful"
+       exit 0
+  else
+      echo "Mysql restore failed"
+      exit  1
+  fi
+}
+
+mkdir -p ${BACKUP_DIR}
+# backup <fileName>
+# restore <fileName>
+$1 $2
