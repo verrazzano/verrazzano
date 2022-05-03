d85e1deb55b7bf665ef8bcf108108e6c1c4120f5
Robert Donat <robert.donat@oracle.com>
2022-05-03 10:01:03 -0400

    Update ATs for k8s 1.23 (#3056)

--- tests/e2e/config/scripts/create_kind_cluster.sh
+++ tests/e2e/config/scripts/create_kind_cluster.sh
@@ -19,14 +19,14 @@ KIND_IMAGE=""
 CALICO_SUFFIX=""
 
 create_kind_cluster() {
-  if [ ${K8S_VERSION} == 1.19 ]; then
-    KIND_IMAGE="v1.19.11@sha256:07db187ae84b4b7de440a73886f008cf903fcf5764ba8106a9fd5243d6f32729"
-  elif [ ${K8S_VERSION} == 1.20 ]; then
+  if [ ${K8S_VERSION} == 1.20 ]; then
     KIND_IMAGE="v1.20.7@sha256:cbeaf907fc78ac97ce7b625e4bf0de16e3ea725daf6b04f930bd14c67c671ff9"
   elif [ ${K8S_VERSION} == 1.21 ]; then
     KIND_IMAGE="v1.21.1@sha256:69860bda5563ac81e3c0057d654b5253219618a22ec3a346306239bba8cfa1a6"
   elif [ ${K8S_VERSION} == 1.22 ]; then
     KIND_IMAGE="v1.22.0@sha256:b8bda84bb3a190e6e028b1760d277454a72267a5454b57db34437c34a588d047"
+  elif [ ${K8S_VERSION} == 1.23 ]; then
+    KIND_IMAGE="v1.23.4@sha256:0e34f0d0fd448aa2f2819cfd74e99fe5793a6e4938b328f657c8e3f81ee0dfb9"
   else
     echo "ERROR: Invalid value for Kubernetes Version ${K8S_VERSION}."
     exit 1
