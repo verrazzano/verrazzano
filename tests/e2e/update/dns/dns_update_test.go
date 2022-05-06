29f77f628b5801186be008321490902dfcaeb1f4
Shashank Kumar <shashank.z.kumar@oracle.com>
2022-05-06 11:01:38 +0000

    Added functions to get and validate ingresses

--- tests/e2e/update/dns/dns_update_test.go
+++ tests/e2e/update/dns/dns_update_test.go
@@ -5,12 +5,11 @@ package dns
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	vzapi "github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1alpha1"
 
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
 	"github.com/verrazzano/verrazzano/pkg/test/framework"
 	"github.com/verrazzano/verrazzano/tests/e2e/pkg"
 	"github.com/verrazzano/verrazzano/tests/e2e/update"
@@ -29,6 +28,13 @@ type WildcardDnsModifier struct {
 	Domain string
 }
 
+type Ingress struct {
+	Name           string
+	Namespace      string
+	Hostname       string
+	LoadBalancerIP string
+}
+
 func (u EnvironmentNameModifier) ModifyCR(cr *vzapi.Verrazzano) {
 	cr.Spec.EnvironmentName = u.EnvironmentName
 }
@@ -43,28 +49,35 @@ func (u WildcardDnsModifier) ModifyCR(cr *vzapi.Verrazzano) {
 	cr.Spec.Components.DNS.Wildcard.Domain = u.Domain
 }
 
+var ingressList []Ingress
 var t = framework.NewTestFramework("update dns")
 
 var _ = t.Describe("Update environment name", func() {
 	t.It("Verify the environment name", func() {
 		cr := update.GetCR()
-		environmentName := GetEnvironmentName(cr)
-		ValidateIngresses(environmentName)
+		getIngressList()
+		validateIngressList(cr.Spec.EnvironmentName, "nip.io")
 	})
 })
 
-func getEnvironmentName(cr *vzapi.Verrazzano) string {
-	if cr.Spec.EnvironmentName == "" {
-		return "default"
-	} else {
-		return cr.Spec.EnvironmentName
+func getIngressList() error {
+	// Fetch the ingresses for the Verrazzano components
+	ingresses, err := pkg.GetIngressList("")
+	if err == nil {
+		for _, ingress := range ingresses.Items {
+			ingressList = append(ingressList, Ingress{ingress.Name, ingress.Namespace, ingress.Spec.Rules[0].Host, ingress.Status.LoadBalancer.Ingress[0].IP})
+		}
 	}
+	return nil
 }
 
-func validateIngresses() {
-	// Fetch the ingresses
-	list, _ := pkg.GetIngressList("")
-	for _, ingress := range list.Items {
-		fmt.Println(ingress.Name, ingress.Namespace)
+func validateIngressList(environmentName string, domain string) error {
+	// Validate that the ingresses contain the expected environment name and domain name
+	for _, ingress := range ingressList {
+		str := fmt.Sprintf("%s.%s.%s", environmentName, ingress.LoadBalancerIP, domain)
+		if !strings.Contains(ingress.Hostname, str) {
+			return fmt.Errorf("Ingress %s in namespace %s  with hostname %s must contain %s", ingress.Name, ingress.Namespace, ingress.Hostname, str)
+		}
 	}
+	return nil
 }
