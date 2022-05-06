e8438b2b70865d1938ad8122b9f7a1792727ef16
Paul Mackin <paul.mackin@oracle.com>
2022-05-06 18:13:27 -0400

    add finalizer test

--- platform-operator/controllers/verrazzano/controller_test.go
+++ platform-operator/controllers/verrazzano/controller_test.go
@@ -101,9 +101,10 @@ func TestSuccessfulInstall(t *testing.T) {
 		APIVersion: "install.verrazzano.io/v1alpha1",
 		Kind:       "Verrazzano"}
 	verrazzanoToUse.ObjectMeta = metav1.ObjectMeta{
-		Namespace: namespace,
-		Name:      name,
-		Labels:    labels}
+		Namespace:  namespace,
+		Name:       name,
+		Labels:     labels,
+		Finalizers: []string{finalizerName}}
 	verrazzanoToUse.Spec.Components.DNS = &vzapi.DNSComponent{External: &vzapi.External{Suffix: "mydomain.com"}}
 
 	verrazzanoToUse.Status.State = vzapi.VzStateReady
@@ -182,9 +183,10 @@ func TestInstallInitComponents(t *testing.T) {
 		APIVersion: "install.verrazzano.io/v1alpha1",
 		Kind:       "Verrazzano"}
 	verrazzanoToUse.ObjectMeta = metav1.ObjectMeta{
-		Namespace: namespace,
-		Name:      name,
-		Labels:    labels}
+		Namespace:  namespace,
+		Name:       name,
+		Labels:     labels,
+		Finalizers: []string{finalizerName}}
 	verrazzanoToUse.Status.State = vzapi.VzStateReady
 
 	// Sample bom file for version validation functions
@@ -328,9 +330,10 @@ func TestCreateVerrazzano(t *testing.T) {
 		APIVersion: "install.verrazzano.io/v1alpha1",
 		Kind:       "Verrazzano"}
 	vzToUse.ObjectMeta = metav1.ObjectMeta{
-		Namespace: namespace,
-		Name:      name,
-		Labels:    labels}
+		Namespace:  namespace,
+		Name:       name,
+		Labels:     labels,
+		Finalizers: []string{finalizerName}}
 
 	vzToUse.Status.Components = makeVerrazzanoComponentStatusMap()
 	vzToUse.Status.State = vzapi.VzStateReady
@@ -453,9 +456,10 @@ func TestCreateVerrazzanoWithOCIDNS(t *testing.T) {
 		APIVersion: "install.verrazzano.io/v1alpha1",
 		Kind:       "Verrazzano"}
 	vzToUse.ObjectMeta = metav1.ObjectMeta{
-		Namespace: namespace,
-		Name:      name,
-		Labels:    labels}
+		Namespace:  namespace,
+		Name:       name,
+		Labels:     labels,
+		Finalizers: []string{finalizerName}}
 	vzToUse.Spec.Components.DNS = &vzapi.DNSComponent{
 		OCI: &vzapi.OCI{
 			OCIConfigSecret:        "test-oci-config-secret",
@@ -1042,9 +1046,10 @@ func TestServiceAccountGetError(t *testing.T) {
 		APIVersion: "install.verrazzano.io/v1alpha1",
 		Kind:       "Verrazzano"}
 	verrazzanoToUse.ObjectMeta = metav1.ObjectMeta{
-		Namespace: namespace,
-		Name:      name,
-		Labels:    labels}
+		Namespace:  namespace,
+		Name:       name,
+		Labels:     labels,
+		Finalizers: []string{finalizerName}}
 	verrazzanoToUse.Status = vzapi.VerrazzanoStatus{
 		State: vzapi.VzStateReady}
 
@@ -1088,9 +1093,10 @@ func TestServiceAccountCreateError(t *testing.T) {
 		APIVersion: "install.verrazzano.io/v1alpha1",
 		Kind:       "Verrazzano"}
 	verrazzanoToUse.ObjectMeta = metav1.ObjectMeta{
-		Namespace: namespace,
-		Name:      name,
-		Labels:    labels}
+		Namespace:  namespace,
+		Name:       name,
+		Labels:     labels,
+		Finalizers: []string{finalizerName}}
 	verrazzanoToUse.Status = vzapi.VerrazzanoStatus{
 		State: vzapi.VzStateReady}
 
@@ -1139,9 +1145,10 @@ func TestClusterRoleBindingGetError(t *testing.T) {
 		APIVersion: "install.verrazzano.io/v1alpha1",
 		Kind:       "Verrazzano"}
 	verrazzanoToUse.ObjectMeta = metav1.ObjectMeta{
-		Namespace: namespace,
-		Name:      name,
-		Labels:    labels}
+		Namespace:  namespace,
+		Name:       name,
+		Labels:     labels,
+		Finalizers: []string{finalizerName}}
 	verrazzanoToUse.Status = vzapi.VerrazzanoStatus{
 		State: vzapi.VzStateReady}
 
@@ -1188,9 +1195,10 @@ func TestClusterRoleBindingCreateError(t *testing.T) {
 		APIVersion: "install.verrazzano.io/v1alpha1",
 		Kind:       "Verrazzano"}
 	verrazzanoToUse.ObjectMeta = metav1.ObjectMeta{
-		Namespace: namespace,
-		Name:      name,
-		Labels:    labels}
+		Namespace:  namespace,
+		Name:       name,
+		Labels:     labels,
+		Finalizers: []string{finalizerName}}
 	verrazzanoToUse.Status = vzapi.VerrazzanoStatus{
 		State: vzapi.VzStateReady}
 
@@ -1242,9 +1250,10 @@ func TestVZSystemNamespaceGetError(t *testing.T) {
 		APIVersion: "install.verrazzano.io/v1alpha1",
 		Kind:       "Verrazzano"}
 	verrazzanoToUse.ObjectMeta = metav1.ObjectMeta{
-		Namespace: namespace,
-		Name:      name,
-		Labels:    labels}
+		Namespace:  namespace,
+		Name:       name,
+		Labels:     labels,
+		Finalizers: []string{finalizerName}}
 	verrazzanoToUse.Status = vzapi.VerrazzanoStatus{
 		State: vzapi.VzStateReady}
 	verrazzanoToUse.Status.Components = makeVerrazzanoComponentStatusMap()
@@ -1299,9 +1308,10 @@ func TestVZSystemNamespaceCreateError(t *testing.T) {
 		APIVersion: "install.verrazzano.io/v1alpha1",
 		Kind:       "Verrazzano"}
 	verrazzanoToUse.ObjectMeta = metav1.ObjectMeta{
-		Namespace: namespace,
-		Name:      name,
-		Labels:    labels}
+		Namespace:  namespace,
+		Name:       name,
+		Labels:     labels,
+		Finalizers: []string{finalizerName}}
 	verrazzanoToUse.Status = vzapi.VerrazzanoStatus{
 		State: vzapi.VzStateReady}
 	verrazzanoToUse.Status.Components = makeVerrazzanoComponentStatusMap()
@@ -1361,9 +1371,10 @@ func TestGetOCIConfigSecretError(t *testing.T) {
 		APIVersion: "install.verrazzano.io/v1alpha1",
 		Kind:       "Verrazzano"}
 	verrazzanoToUse.ObjectMeta = metav1.ObjectMeta{
-		Namespace: namespace,
-		Name:      name,
-		Labels:    labels}
+		Namespace:  namespace,
+		Name:       name,
+		Labels:     labels,
+		Finalizers: []string{finalizerName}}
 	verrazzanoToUse.Spec.Components.DNS = &vzapi.DNSComponent{
 		OCI: &vzapi.OCI{
 			OCIConfigSecret:        "test-oci-config-secret",
@@ -1403,6 +1414,92 @@ func TestGetOCIConfigSecretError(t *testing.T) {
 	asserts.NoError(err)
 	asserts.Equal(true, result.Requeue)
 	asserts.NotZero(result.RequeueAfter)
+}
+
+// TestSetFinalizer tests the Reconcile method for the following use case
+// GIVEN a request to reconcile a Verrazzano resource
+// WHEN a Verrazzano resource doesn't have a finalizer
+// THEN ensure the finalizer is added
+func TestSetFinalizer(t *testing.T) {
+	unitTesting = true
+	namespace := "verrazzano"
+	name := "test"
+	labels := map[string]string{"label1": "test"}
+	var verrazzanoToUse vzapi.Verrazzano
+	asserts := assert.New(t)
+	mocker := gomock.NewController(t)
+	mock := mocks.NewMockClient(mocker)
+	mockStatus := mocks.NewMockStatusWriter(mocker)
+	asserts.NotNil(mockStatus)
+
+	config.TestHelmConfigDir = "../../helm_config"
+	defer func() { config.TestHelmConfigDir = "" }()
+
+	config.TestProfilesDir = "../../manifests/profiles"
+	defer func() { config.TestProfilesDir = "" }()
+
+	verrazzanoToUse.TypeMeta = metav1.TypeMeta{
+		APIVersion: "install.verrazzano.io/v1alpha1",
+		Kind:       "Verrazzano"}
+	verrazzanoToUse.ObjectMeta = metav1.ObjectMeta{
+		Namespace: namespace,
+		Name:      name,
+		Labels:    labels}
+	verrazzanoToUse.Spec.Components.DNS = &vzapi.DNSComponent{External: &vzapi.External{Suffix: "mydomain.com"}}
+
+	verrazzanoToUse.Status.State = vzapi.VzStateReady
+	verrazzanoToUse.Status.Components = makeVerrazzanoComponentStatusMap()
+
+	// Sample bom file for version validation functions
+	config.SetDefaultBomFilePath(testBomFilePath)
+	defer config.SetDefaultBomFilePath("")
+
+	registry.OverrideGetComponentsFn(func() []spi.Component {
+		return []spi.Component{
+			fakeComponent{},
+		}
+	})
+	defer registry.ResetGetComponentsFn()
+
+	// Expect a call to get the Verrazzano resource.
+	expectGetVerrazzanoExists(mock, verrazzanoToUse, namespace, name, labels)
+
+	// Expect a call to get the service account
+	expectGetServiceAccountExists(mock, name, labels)
+
+	// Expect a call to get the ClusterRoleBinding
+	expectClusterRoleBindingExists(mock, verrazzanoToUse, namespace, name)
+
+	// Expect a call to get the Verrazzano system namespace (return exists)
+	expectGetVerrazzanoSystemNamespaceExists(mock, asserts)
+
+	// Expect a call to update the finalizers - return success
+	mock.EXPECT().Update(gomock.Any(), gomock.Any()).Return(nil).AnyTimes()
+
+	// Expect a call to get the status writer and return a mock.
+	mock.EXPECT().Status().Return(mockStatus).AnyTimes()
+
+	// Expect a call to update the status of the Verrazzano resource
+	mockStatus.EXPECT().
+		Update(gomock.Any(), gomock.Any()).
+		DoAndReturn(func(ctx context.Context, verrazzano *vzapi.Verrazzano, opts ...client.UpdateOption) error {
+			asserts.Len(verrazzano.Status.Conditions, 1)
+			return nil
+		}).Times(1)
+
+	// Expect local registration calls
+	expectSyncLocalRegistration(t, mock, name)
+
+	// Create and make the request
+	request := newRequest(namespace, name)
+	reconciler := newVerrazzanoReconciler(mock)
+	result, err := reconciler.Reconcile(nil, request)
+	asserts.NoError(err)
+
+	// Validate the results
+	mocker.Finish()
+	asserts.Equal(false, result.Requeue)
+	asserts.Equal(time.Duration(0), result.RequeueAfter)
 }
 
 // newScheme creates a new scheme that includes this package's object to use for testing
