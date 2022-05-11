09e2bd2a1e81a1ba70d6531322716c0dacb173fa
Michael Gianatassio <michael.gianatassio@oracle.com>
2022-05-11 09:51:09 -0700

    convert TestDeleteDuringUpgrade to use mocks

--- platform-operator/controllers/verrazzano/upgrade_test.go
+++ platform-operator/controllers/verrazzano/upgrade_test.go
@@ -37,9 +37,7 @@ import (
 	batchv1 "k8s.io/api/batch/v1"
 	v1 "k8s.io/api/core/v1"
 	networkingv1 "k8s.io/api/networking/v1"
-	errors2 "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes"
 	gofake "k8s.io/client-go/kubernetes/fake"
@@ -382,30 +380,22 @@ func TestDeleteDuringUpgrade(t *testing.T) {
 
 	config.SetDefaultBomFilePath(unitTestBomFile)
 	asserts := assert.New(t)
-	mocker := gomock.NewController(t)
-	mock := mocks.NewMockClient(mocker)
-	mockStatus := mocks.NewMockStatusWriter(mocker)
-	asserts.NotNil(mockStatus)
 
 	defer config.Set(config.Get())
 	config.Set(config.OperatorConfig{VersionCheckEnabled: false})
 
-	// Expect a call to get the verrazzano resource.  Return resource with version
-	mock.EXPECT().
-		Get(gomock.Any(), types.NamespacedName{Namespace: namespace, Name: name}, gomock.Not(gomock.Nil())).
-		DoAndReturn(func(ctx context.Context, name types.NamespacedName, verrazzano *vzapi.Verrazzano) error {
-			verrazzano.TypeMeta = metav1.TypeMeta{
-				APIVersion: "install.verrazzano.io/v1alpha1",
-				Kind:       "Verrazzano"}
-			verrazzano.ObjectMeta = metav1.ObjectMeta{
-				Name:              name.Name,
-				Namespace:         name.Namespace,
+	_ = vzapi.AddToScheme(k8scheme.Scheme)
+	c := fake.NewClientBuilder().WithScheme(k8scheme.Scheme).WithObjects(
+		&vzapi.Verrazzano{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace:         namespace,
+				Name:              name,
 				DeletionTimestamp: &metav1.Time{Time: time.Now()},
-				Finalizers:        []string{finalizerName},
-			}
-			verrazzano.Spec = vzapi.VerrazzanoSpec{
-				Version: "0.2.0"}
-			verrazzano.Status = vzapi.VerrazzanoStatus{
+				Finalizers:        []string{finalizerName}},
+			Spec: vzapi.VerrazzanoSpec{
+				Version: "0.2.0",
+			},
+			Status: vzapi.VerrazzanoStatus{
 				State: vzapi.VzStateUpgrading,
 				Conditions: []vzapi.Condition{
 					{
@@ -415,56 +405,38 @@ func TestDeleteDuringUpgrade(t *testing.T) {
 						Type: vzapi.CondUpgradeStarted,
 					},
 				},
-				Components: makeVerrazzanoComponentStatusMap(),
-			}
-			return nil
-		})
-
-	// Expect a call to get the service account
-	expectGetServiceAccountExists(mock, name, nil)
-
-	// Expect a call to get the ClusterRoleBinding
-	expectClusterRoleBindingExists(mock, verrazzanoToUse, namespace, name)
-
-	// Expect a call to get the status writer and return a mock.
-	mock.EXPECT().Status().Return(mockStatus).AnyTimes()
-
-	// Expect a call to get the uninstall Job - return that it does not exist
-	mock.EXPECT().
-		Get(gomock.Any(), types.NamespacedName{Namespace: getInstallNamespace(), Name: buildUninstallJobName(name)}, gomock.Not(gomock.Nil())).
-		Return(errors2.NewNotFound(schema.GroupResource{Group: namespace, Resource: "Job"}, buildUninstallJobName(name)))
+				Components: makeVerrazzanoComponentStatusMap()},
+		},
+		rbac.NewServiceAccount(namespace, name, []string{}, nil),
+		rbac.NewClusterRoleBinding(&verrazzanoToUse, name, getInstallNamespace(), buildServiceAccountName(name)),
+	).Build()
 
 	// Expect a call to create the uninstall Job - return success
-	mock.EXPECT().
-		Create(gomock.Any(), gomock.Any()).
-		DoAndReturn(func(ctx context.Context, job *batchv1.Job, opts ...client.CreateOption) error {
-			asserts.Equalf(getInstallNamespace(), job.Namespace, "Job namespace did not match")
-			asserts.Equalf(buildUninstallJobName(name), job.Name, "Job name did not match")
-			return nil
-		})
-
-	// Expect a call to update the job - return success
-	mock.EXPECT().Update(gomock.Any(), gomock.Any()).Return(nil).AnyTimes()
-
-	// Expect a status update on the job
-	mockStatus.EXPECT().Update(gomock.Any(), gomock.Any()).Return(nil)
-
-	// Expect a call to get the status writer and return a mock.
-	mock.EXPECT().Status().Return(mockStatus).AnyTimes()
+	/*
+		mock.EXPECT().
+			Create(gomock.Any(), gomock.Any()).
+			DoAndReturn(func(ctx context.Context, job *batchv1.Job, opts ...client.CreateOption) error {
+				asserts.Equalf(getInstallNamespace(), job.Namespace, "Job namespace did not match")
+				asserts.Equalf(buildUninstallJobName(name), job.Name, "Job name did not match")
+				return nil
+			})
+	*/
 
 	config.TestProfilesDir = "../../manifests/profiles"
 	defer func() { config.TestProfilesDir = "" }()
 
 	// Create and make the request
 	request := newRequest(namespace, name)
-	reconciler := newVerrazzanoReconciler(mock)
+	reconciler := newVerrazzanoReconciler(c)
 	result, err := reconciler.Reconcile(nil, request)
 
 	// Validate the results
-	mocker.Finish()
 	asserts.NoError(err)
 	asserts.Equal(true, result.Requeue)
 	asserts.Equal(time.Duration(2)*time.Second, result.RequeueAfter)
+	uninstallJob := batchv1.Job{}
+	err = c.Get(context.TODO(), types.NamespacedName{Namespace: getInstallNamespace(), Name: buildUninstallJobName(name)}, &uninstallJob)
+	asserts.NoError(err)
 }
 
 // TestUpgradeStartedWhenPrevFailures tests the reconcileUpgrade method for the following use case
