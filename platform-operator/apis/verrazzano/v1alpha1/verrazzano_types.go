e04fbe1d718ce8f80d5a7e1e8fefc0a6ec2363dd
ben.mclean <ben.mclean@oracle.com>
2022-05-17 11:10:36 -0400

    comment

--- platform-operator/apis/verrazzano/v1alpha1/verrazzano_types.go
+++ platform-operator/apis/verrazzano/v1alpha1/verrazzano_types.go
@@ -770,11 +770,13 @@ type OciLoggingConfiguration struct {
 	APISecret       string `json:"apiSecret,omitempty"`
 }
 
+// InstallOverrides are used to pass install overrides to components
 type InstallOverrides struct {
 	MonitorChanges *bool       `json:"monitorChanges,omitempty"`
 	ValueOverrides []Overrides `json:"overrides,omitempty"`
 }
 
+// Overrides stores the specified InstallOverrides
 type Overrides struct {
 	ConfigMapRef *corev1.ConfigMapKeySelector `json:"configMapRef,omitempty"`
 	SecretRef    *corev1.SecretKeySelector    `json:"secretRef,omitempty"`
