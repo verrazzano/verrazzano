8f445629f7743c3aa3a1dc1f76d016573e2fd874
adiforluls <aditya.b.bharadwaj@oracle.com>
2022-05-18 11:05:16 +0530

    fix kubectl create

--- tests/e2e/update/overrides/update_overrides.sh
+++ tests/e2e/update/overrides/update_overrides.sh
@@ -9,14 +9,14 @@ UPDATE_OVERRIDE_CONFIGMAP_FILE=$SCRIPT_DIR/../../config/scripts/post-install-ove
 UPDATE_OVERRIDE_SECRET_FILE=$SCRIPT_DIR/../../config/scripts/post-install-overrides/test-overrides-secret.yaml
 
 echo "Update overrides ConfigMap"
-kubectl create cm test-overrides --from-file=$UPDATE_OVERRIDE_CONFIGMAP_FILE --dry-run=client | kubectl apply -f -
+kubectl create cm test-overrides --from-file=$UPDATE_OVERRIDE_CONFIGMAP_FILE -o yaml --dry-run=client | kubectl apply -f -
 if [ $? -ne 0 ]; then
   echo "Could not update ConfigMap"
   exit 1
 fi
 
 echo "Update overrides Secret"
-kubectl create secret generic test-overrides --from-file=$UPDATE_OVERRIDE_SECRET_FILE --dry-run=client | kubectl apply -f -
+kubectl create secret generic test-overrides --from-file=$UPDATE_OVERRIDE_SECRET_FILE -o yaml --dry-run=client | kubectl apply -f -
 if [ $? -ne 0 ]; then
   echo "Could not update Secret"
   exit 1
