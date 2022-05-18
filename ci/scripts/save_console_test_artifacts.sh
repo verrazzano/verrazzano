19148e941d7bf7a30cd7e6b0e12862200bb71207
Deva Sagar <deva.sagar@oracle.com>
2022-05-18 17:57:44 -0400

    Jenkinsfiles should save console test browser logs, in addition to screenshots

--- ci/scripts/save_console_test_artifacts.sh
+++ ci/scripts/save_console_test_artifacts.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright (c) 2021, Oracle and/or its affiliates.
+# Copyright (c) 2021, 2022, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 #
 if [ -z "${GO_REPO_PATH}" ] || [ -z "${WORKSPACE}" ]; then
@@ -11,5 +11,8 @@ fi
 cd ${GO_REPO_PATH}/console
 # Copy artifacts to workspace, ignore if copy fails
 mkdir -p ${WORKSPACE}/console/screenshots
+mkdir -p ${WORKSPACE}/console/logs
 ls Screenshot*.png || true
 cp Screenshot*.png ${WORKSPACE}/console/screenshots || true
+ls ConsoleLog*.log || true
+cp ConsoleLog*.log ${WORKSPACE}/console/logs || true
