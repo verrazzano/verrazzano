96cf9a193a6a4b13f79498c8596010b2cbbb05e3
davisgomes <davisgomes@g.ucla.edu>
2022-05-20 16:16:04 -0400

    VZ-5910 create pod monitor endpoints for each port

--- application-operator/controllers/metricstrait/monitor.go
+++ application-operator/controllers/metricstrait/monitor.go
@@ -58,36 +58,151 @@ func (r *Reconciler) updatePodMonitor(ctx context.Context, trait *vzapi.MetricsT
 // the Pod Monitor reflects the specifications of the trait and the trait defaults
 func (r *Reconciler) mutatePodMonitorFromTrait(ctx context.Context, podMonitor *promoperapi.PodMonitor, trait *vzapi.MetricsTrait, workload *unstructured.Unstructured, traitDefaults *vzapi.MetricsTraitSpec, log vzlog.VerrazzanoLogger) error {
 	// Create the Pod monitor name from the trait if the label exists
-	// Create the Pod Monitor selector from the trait label i fit exists
+	// Create the Pod Monitor selector from the trait label if it exists
 	if podMonitor.ObjectMeta.Labels == nil {
 		podMonitor.ObjectMeta.Labels = map[string]string{}
 	}
 	podMonitor.ObjectMeta.Labels["name"] = podMonitor.GetName()
-	podMonitor.Spec.Selector = metav1.LabelSelector{MatchLabels: map[string]string{appObjectMetaLabel: podMonitor.GetName()}}
+	podMonitor.Spec.Selector = metav1.LabelSelector{MatchLabels: map[string]string{appObjectMetaLabel: trait.Labels[appObjectMetaLabel]}}
 
 	// Loop through ports in the trait and create scrape targets for each
 	ports := getPortSpecs(trait, traitDefaults)
 	for i := range ports {
-		err := r.createPodMetricsEndpoint(ctx, podMonitor, trait, workload, traitDefaults, i)
+		endpoint, err := r.createPodMetricsEndpoint(ctx, trait, workload, traitDefaults, i)
 		if err != nil {
 			return log.ErrorfNewErr("Failed to create the pod metrics endpoint for the pod monitor: %v", err)
 		}
+		podMonitor.Spec.PodMetricsEndpoints = append(podMonitor.Spec.PodMetricsEndpoints, endpoint)
 	}
 
 	return nil
 }
 
-func (r *Reconciler) createPodMetricsEndpoint(ctx context.Context, podMonitor *promoperapi.PodMonitor, trait *vzapi.MetricsTrait, workload *unstructured.Unstructured, traitDefaults *vzapi.MetricsTraitSpec, portIncrement int) error {
-	job, err := createPodMonitorName(trait, portIncrement)
-	if err != nil {
-		return err
+func (r *Reconciler) createPodMetricsEndpoint(ctx context.Context, trait *vzapi.MetricsTrait, workload *unstructured.Unstructured, traitDefaults *vzapi.MetricsTraitSpec, portIncrement int) (promoperapi.PodMetricsEndpoint, error) {
+	var endpoint promoperapi.PodMetricsEndpoint
+
+	endpoint.Scheme = "http"
+	endpoint.Path = "/metrics"
+	if trait.Spec.Path != nil {
+		endpoint.Path = *trait.Spec.Path
 	}
 
-	podMonitor.Spec.PodMetricsEndpoints = append(podMonitor.Spec.PodMetricsEndpoints, promoperapi.PodMetricsEndpoint{
-		Port: "__meta_kubernetes_pod_annotation_verrazzano_io_metricsPort",
-		Path: job,
+	// Set up the port appendix if necessary
+	var portString string
+	if portIncrement > 0 {
+		portString = fmt.Sprintf("_%d", portIncrement)
+	}
+
+	// If Istio is enabled, use the tls config
+	useHTTPS, err := useHTTPSForScrapeTarget(ctx, r.Client, trait)
+	if err != nil {
+		return endpoint, err
+	}
+	if useHTTPS {
+		certSecName := "istio-certs"
+		endpoint.Scheme = "https"
+		endpoint.TLSConfig = &promoperapi.PodMetricsEndpointTLSConfig{
+			SafeTLSConfig: promoperapi.SafeTLSConfig{
+				CA: promoperapi.SecretOrConfigMap{
+					Secret: &k8score.SecretKeySelector{
+						LocalObjectReference: k8score.LocalObjectReference{
+							Name: certSecName,
+						},
+						Key: "root-cert.pem",
+					},
+				},
+				Cert: promoperapi.SecretOrConfigMap{
+					Secret: &k8score.SecretKeySelector{
+						LocalObjectReference: k8score.LocalObjectReference{
+							Name: certSecName,
+						},
+						Key: "cert-chain.pem",
+					},
+				},
+				KeySecret: &k8score.SecretKeySelector{
+					LocalObjectReference: k8score.LocalObjectReference{
+						Name: certSecName,
+					},
+					Key: "key.pem",
+				},
+				InsecureSkipVerify: true,
+			},
+		}
+	}
+
+	// Relabel the cluster name
+	endpoint.RelabelConfigs = append(endpoint.RelabelConfigs, &promoperapi.RelabelConfig{
+		Action:      "replace",
+		Replacement: "local",
+		TargetLabel: prometheusClusterNameLabel,
 	})
-	return nil
+
+	// Relabel to match the expected labels
+	endpoint.RelabelConfigs = append(endpoint.RelabelConfigs, &promoperapi.RelabelConfig{
+		Action: "keep",
+		Regex:  fmt.Sprintf("true;%s;%s", trait.Labels[appObjectMetaLabel], trait.Labels[compObjectMetaLabel]),
+		SourceLabels: []promoperapi.LabelName{
+			promoperapi.LabelName(fmt.Sprintf("__meta_kubernetes_pod_annotation_verrazzano_io_metricsEnabled%s", portString)),
+			"__meta_kubernetes_pod_label_app_oam_dev_name",
+			"__meta_kubernetes_pod_label_app_oam_dev_component",
+		},
+	})
+
+	// Relabel the address of the metrics endpoint
+	endpoint.RelabelConfigs = append(endpoint.RelabelConfigs, &promoperapi.RelabelConfig{
+		Action:      "replace",
+		Regex:       `([^:]+)(?::\d+)?;(\d+)`,
+		Replacement: "$1:$2",
+		SourceLabels: []promoperapi.LabelName{
+			"__address__",
+			promoperapi.LabelName(fmt.Sprintf("__meta_kubernetes_pod_annotation_verrazzano_io_metricsPort%s", portString)),
+		},
+	})
+
+	// Relabel the namespace label
+	endpoint.RelabelConfigs = append(endpoint.RelabelConfigs, &promoperapi.RelabelConfig{
+		Action:      "replace",
+		Regex:       `(.*)`,
+		Replacement: "$1",
+		SourceLabels: []promoperapi.LabelName{
+			"__meta_kubernetes_namespace",
+		},
+		TargetLabel: "namespace",
+	})
+
+	// Relabel the pod label
+	endpoint.RelabelConfigs = append(endpoint.RelabelConfigs, &promoperapi.RelabelConfig{
+		Action: "labelmap",
+		Regex:  `__meta_kubernetes_pod_label_(.+)`,
+	})
+
+	// Relabel the pod name label
+	endpoint.RelabelConfigs = append(endpoint.RelabelConfigs, &promoperapi.RelabelConfig{
+		Action: "replace",
+		SourceLabels: []promoperapi.LabelName{
+			"__meta_kubernetes_pod_name",
+		},
+		TargetLabel: "pod_name",
+	})
+
+	// Drop the controller revision hash label
+	endpoint.RelabelConfigs = append(endpoint.RelabelConfigs, &promoperapi.RelabelConfig{
+		Action: "labeldrop",
+		Regex:  `(controller_revision_hash)`,
+	})
+
+	// Relabel the webapp label
+	endpoint.RelabelConfigs = append(endpoint.RelabelConfigs, &promoperapi.RelabelConfig{
+		Action:      "replace",
+		Regex:       `.*/(.*)$`,
+		Replacement: "$1",
+		SourceLabels: []promoperapi.LabelName{
+			"name",
+		},
+		TargetLabel: "webapp",
+	})
+
+	return endpoint, nil
 }
 
 // fetchSourceCredentialsSecretIfRequired fetches the metrics endpoint authentication credentials if a secret is provided.
