1093e52ffc8990f3b098d43f77dacd9cb5e41959
Michael Cico <46466506+michael-cico@users.noreply.github.com>
2022-05-23 10:00:32 -0400

    VZ-5835.  Re-integrate the update-on-upgrade tests into the upgrade-paths pipeline (#3166)
    * VZ-5835.  Add version check to verify-upgrade-required tests
    - Exec's into the VPO pod and retrieves in the installed BOM file, and gets the version
    - Verifies that the available version in the BOM is >= v1.3.0 before continuing
    - Re-add verify-upgrade-required stage
    - Add conditionalized update-on-upgrade scenario,limited to v1.3.0+

--- tests/e2e/config/scripts/process_upgrade_yaml.sh
+++ tests/e2e/config/scripts/process_upgrade_yaml.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-# Copyright (c) 2021, Oracle and/or its affiliates.
+# Copyright (c) 2021, 2022, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+function version_ge() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" == "$1"; }
 
 # This script is used to add the version: field to the verrazzano custom resource .yaml file
 # It is needed to test upgrade
 VERSION=$1
 CR_FILE=$2
 yq -i eval ".spec.version = \"v${VERSION}\"" ${CR_FILE}
+
+if version_ge $VERSION "1.3.0"; then
+  echo "$VERSION supports updates, testing update on upgrade scenario"
+  # Add some simple additional updates to validate update during an upgrade
+  yq -i eval '.spec.components.istio.ingress.kubernetes.replicas = 3' ${CR_FILE}
+  yq -i eval '.spec.components.istio.egress.kubernetes.replicas = 3' ${CR_FILE}
+fi
