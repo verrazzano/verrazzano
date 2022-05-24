2fb1acd007db4b8b17a7aca7c634af3921fc0515
anajosep <anand.francis.joseph.augustin@oracle.com>
2022-05-24 06:35:03 +0000

    Added skip logic in before suit
    Signed-off-by: anajosep <anand.francis.joseph.augustin@oracle.com>

--- tests/e2e/upgrade/post-upgrade/prometheus/prometheus_test.go
+++ tests/e2e/upgrade/post-upgrade/prometheus/prometheus_test.go
@@ -23,28 +23,24 @@ const (
 
 var t = framework.NewTestFramework("prometheus")
 
+var _ = t.BeforeSuite(func() {
+	kubeconfigPath, err := k8sutil.GetKubeConfigLocation()
+	if err != nil {
+		Fail(fmt.Sprintf("Failed to get default kubeconfig path: %s", err.Error()))
+	}
+	supported := pkg.IsPrometheusEnabled(kubeconfigPath)
+	// Only run tests if Prometheus component is enabled in Verrazzano CR
+	if !supported {
+		Skip("Prometheus component is not enabled")
+	}
+})
 
 var _ = t.Describe("Post upgrade Prometheus", Label("f:observability.logging.es"), func() {
-	PrometheusSupportedIt := func(description string, f func()) {
-		kubeconfigPath, err := k8sutil.GetKubeConfigLocation()
-		if err != nil {
-			t.It(description, func() {
-				Fail(fmt.Sprintf("Failed to get default kubeconfig path: %s", err.Error()))
-			})
-		}
-		supported := pkg.IsPrometheusEnabled(kubeconfigPath)
-		// Only run tests if Prometheus component is enabled in Verrazzano CR
-		if supported {
-			t.It(description, f)
-		} else {
-			pkg.Log(pkg.Info, fmt.Sprintf("Skipping check '%v', Prometheus component is not enabled in current Verrazzano Installation", description))
-		}
-	}
 
 	// GIVEN a running Prometheus instance,
 	// WHEN a scrape config is created,
 	// THEN verify that the scrape config is created correctly
-	PrometheusSupportedIt("Old indices are deleted", func() {
+	It("Old indices are deleted", func() {
 		Eventually(func() bool {
 
 			return true
@@ -55,7 +51,7 @@ var _ = t.Describe("Post upgrade Prometheus", Label("f:observability.logging.es"
 	// GIVEN a running Prometheus instance,
 	// WHEN the data streams are retrieved
 	// THEN verify that they have data streams
-	PrometheusSupportedIt("Data streams are created", func() {
+	It("Data streams are created", func() {
 		Eventually(func() bool {
 			return true
 		}).WithPolling(pollingInterval).WithTimeout(threeMinutes).Should(BeTrue(),
@@ -65,7 +61,7 @@ var _ = t.Describe("Post upgrade Prometheus", Label("f:observability.logging.es"
 	// GIVEN a running Prometheus instance,
 	// WHEN
 	// THEN verify that the data can be retrieved successfully
-	PrometheusSupportedIt("OpenSearch get old data", func() {
+	It("OpenSearch get old data", func() {
 		Eventually(func() bool {
 			return true
 		}).WithPolling(pollingInterval).WithTimeout(threeMinutes).Should(BeTrue(),
@@ -76,7 +72,7 @@ var _ = t.Describe("Post upgrade Prometheus", Label("f:observability.logging.es"
 	// WHEN VZ custom resource is upgraded
 	// THEN only the system logs that are as old as the retention period
 	//      is migrated and older logs are purged
-	PrometheusSupportedIt("OpenSearch system logs older than retention period is not available post upgrade", func() {
+	It("OpenSearch system logs older than retention period is not available post upgrade", func() {
 		Eventually(func() bool {
 			return true
 		}).WithPolling(pollingInterval).WithTimeout(threeMinutes).Should(BeTrue(),
@@ -87,7 +83,7 @@ var _ = t.Describe("Post upgrade Prometheus", Label("f:observability.logging.es"
 	// WHEN VZ custom resource is upgraded
 	// THEN only the application logs that are as old as the retention period
 	//      is migrated and older logs are purged
-	PrometheusSupportedIt("OpenSearch application logs older than retention period is not available post upgrade", func () {
+	It("OpenSearch application logs older than retention period is not available post upgrade", func () {
 		Eventually(func() bool {
 			return true
 		}).WithPolling(pollingInterval).WithTimeout(threeMinutes).Should(BeTrue(),
--- tests/e2e/upgrade/pre-upgrade/prometheus/prometheus_test.go
+++ tests/e2e/upgrade/pre-upgrade/prometheus/prometheus_test.go
@@ -23,28 +23,24 @@ const (
 
 var t = framework.NewTestFramework("prometheus")
 
+var _ = t.BeforeSuite(func() {
+	kubeconfigPath, err := k8sutil.GetKubeConfigLocation()
+	if err != nil {
+		Fail(fmt.Sprintf("Failed to get default kubeconfig path: %s", err.Error()))
+	}
+	supported := pkg.IsPrometheusEnabled(kubeconfigPath)
+	// Only run tests if Prometheus component is enabled in Verrazzano CR
+	if !supported {
+		Skip("Prometheus component is not enabled")
+	}
+})
 
 var _ = t.Describe("Pre upgrade Prometheus", Label("f:observability.logging.es"), func() {
-	PrometheusSupportedIt := func(description string, f func()) {
-		kubeconfigPath, err := k8sutil.GetKubeConfigLocation()
-		if err != nil {
-			t.It(description, func() {
-				Fail(fmt.Sprintf("Failed to get default kubeconfig path: %s", err.Error()))
-			})
-		}
-		supported := pkg.IsPrometheusEnabled(kubeconfigPath)
-		// Only run tests if Prometheus component is enabled in Verrazzano CR
-		if supported {
-			t.It(description, f)
-		} else {
-			pkg.Log(pkg.Info, fmt.Sprintf("Skipping check '%v', Prometheus component is not enabled in current Verrazzano Installation", description))
-		}
-	}
 
 	// GIVEN a running Prometheus instance,
 	// WHEN a scrape config is created,
 	// THEN verify that the scrape config is created correctly
-	PrometheusSupportedIt("Scrape targets can be listed and there is atleast 1 scrape target", func() {
+	It("Scrape targets can be listed and there is atleast 1 scrape target", func() {
 		Eventually(func() bool {
 			scrapeTargets, err  := pkg.ScrapeTargets()
 			if err != nil {
@@ -59,7 +55,7 @@ var _ = t.Describe("Pre upgrade Prometheus", Label("f:observability.logging.es")
 	// GIVEN a running Prometheus instance,
 	// WHEN the data streams are retrieved
 	// THEN verify that they have data streams
-	PrometheusSupportedIt("Data streams are created", func() {
+	It("Data streams are created", func() {
 		Eventually(func() bool {
 			return true
 		}).WithPolling(pollingInterval).WithTimeout(threeMinutes).Should(BeTrue(),
@@ -69,7 +65,7 @@ var _ = t.Describe("Pre upgrade Prometheus", Label("f:observability.logging.es")
 	// GIVEN a running Prometheus instance,
 	// WHEN
 	// THEN verify that the data can be retrieved successfully
-	PrometheusSupportedIt("OpenSearch get old data", func() {
+	It("OpenSearch get old data", func() {
 		Eventually(func() bool {
 			return true
 		}).WithPolling(pollingInterval).WithTimeout(threeMinutes).Should(BeTrue(),
@@ -80,7 +76,7 @@ var _ = t.Describe("Pre upgrade Prometheus", Label("f:observability.logging.es")
 	// WHEN VZ custom resource is upgraded
 	// THEN only the system logs that are as old as the retention period
 	//      is migrated and older logs are purged
-	PrometheusSupportedIt("OpenSearch system logs older than retention period is not available post upgrade", func() {
+	It("OpenSearch system logs older than retention period is not available post upgrade", func() {
 		Eventually(func() bool {
 			return true
 		}).WithPolling(pollingInterval).WithTimeout(threeMinutes).Should(BeTrue(),
@@ -91,7 +87,7 @@ var _ = t.Describe("Pre upgrade Prometheus", Label("f:observability.logging.es")
 	// WHEN VZ custom resource is upgraded
 	// THEN only the application logs that are as old as the retention period
 	//      is migrated and older logs are purged
-	PrometheusSupportedIt("OpenSearch application logs older than retention period is not available post upgrade", func () {
+	It("OpenSearch application logs older than retention period is not available post upgrade", func () {
 		Eventually(func() bool {
 			return true
 		}).WithPolling(pollingInterval).WithTimeout(threeMinutes).Should(BeTrue(),
