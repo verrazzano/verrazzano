4423a8878fa62991d4d634eb86c7dd3df7062334
Shashank Kumar <shashank.z.kumar@oracle.com>
2022-05-26 08:32:07 +0000

    Added utils as env var

--- ci/utils.groovy
+++ ci/utils.groovy
@@ -3,17 +3,16 @@
 
 // File containing groovy functions to be used by Jenkinsfile in this directory
 
-import java.text.SimpleDateFormat
-
-def dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss")
+// import java.text.SimpleDateFormat
+// def dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss")
 
 def testUtilsFile() {
     return "success"
 }
 
 def log(status, msg) {
-    timestamp = dateFormat.format(new Date())
-    println("${timestamp} ${status} - ${msg}")
+    // timestamp = dateFormat.format(new Date())
+    println("${status} - ${msg}")
 }
 
 return this
