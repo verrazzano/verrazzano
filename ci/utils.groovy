caf6ffe2eb4b0f0788992687b4b62258bc565844
Shashank Kumar <shashank.z.kumar@oracle.com>
2022-05-26 04:53:45 +0000

    Restructured Jenkinsfile and added utils.groovy

---
+++ ci/utils.groovy
@@ -0,0 +1,19 @@
+// Copyright (c) 2022, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+// File containing groovy functions to be used by Jenkinsfile in this directory
+
+import java.text.SimpleDateFormat
+
+def dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss")
+
+def testUtilsFile() {
+    return "success"
+}
+
+def log(status, msg) {
+    timestamp = dateFormat.format(new Date())
+    println("${timestamp} ${status} - ${msg}")
+}
+
+return this
