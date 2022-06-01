a55dfd9296372a046ee480bd0ad41d78312bd93f
Michael Cico <46466506+michael-cico@users.noreply.github.com>
2022-06-01 16:03:51 -0400

    Use makefile driver for tests; add default dump directory init logic to test framework constructor

--- pkg/test/framework/ginkgo_wrapper.go
+++ pkg/test/framework/ginkgo_wrapper.go
@@ -4,8 +4,10 @@
 package framework
 
 import (
+	"fmt"
 	"github.com/verrazzano/verrazzano/pkg/test/framework/metrics"
 	"go.uber.org/zap"
+	"os"
 	"reflect"
 
 	"github.com/onsi/ginkgo/v2"
@@ -23,7 +25,21 @@ func NewTestFramework(pkg string) *TestFramework {
 	t.Pkg = pkg
 	t.Metrics, _ = metrics.NewLogger(pkg, metrics.MetricsIndex)
 	t.Logs, _ = metrics.NewLogger(pkg, metrics.TestLogIndex, "stdout")
+	t.initDumpDirectoryIfNecessary()
 	return t
+}
+
+// initDumpDirectoryIfNecessary - sets the DUMP_DIRECTORY env variable to a default if not set externally
+func (t *TestFramework) initDumpDirectoryIfNecessary() {
+	if dumpDirectory, dumpDirIsSet := os.LookupEnv(DumpDirectoryEnvVarName); !dumpDirIsSet {
+		dumpDirectory = t.Pkg
+		dumpRoot, exists := os.LookupEnv(DumpRootDirectoryEnvVarName)
+		if exists {
+			dumpDirectory = fmt.Sprintf("%s/%s", dumpRoot, t.Pkg)
+		}
+		t.Logs.Infof("Defaulting %s to %s", DumpDirectoryEnvVarName, dumpDirectory)
+		os.Setenv(DumpDirectoryEnvVarName, dumpDirectory)
+	}
 }
 
 // AfterEach wraps Ginkgo AfterEach to emit a metric
