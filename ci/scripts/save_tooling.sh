3585f8dd9db3d4d4b5b06ad10f3267ee87adceb1
stshapir <steven.shapiro@oracle.com>
2022-06-02 17:00:22 -0400

    updated tooling
    Signed-off-by: stshapir <steven.shapiro@oracle.com>

--- ci/scripts/save_tooling.sh
+++ ci/scripts/save_tooling.sh
@@ -32,6 +32,8 @@ cd ${WORKSPACE}
 sha256sum k8s-dump-cluster.sh > k8s-dump-cluster.sh.sha256
 sha256sum verrazzano-analysis-linux-amd64.tar.gz > verrazzano-analysis-linux-amd64.tar.gz.sha256
 sha256sum verrazzano-analysis-darwin-amd64.tar.gz > verrazzano-analysis-darwin-amd64.tar.gz.sha256
+sha256sum verrazzano-cli-linux-amd64.tar.gz > verrazzano-cli-linux-amd64.tar.gz.sha256
+sha256sum verrazzano-cli-darwin-amd64.tar.gz > verrazzano-cli-darwin-amd64.tar.gz.sha256
 oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_BUCKET} --name ${CURRENT_BRANCH_NAME}/k8s-dump-cluster.sh --file k8s-dump-cluster.sh
 oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_BUCKET} --name ${CURRENT_BRANCH_NAME}/k8s-dump-cluster.sh.sha256 --file k8s-dump-cluster.sh.sha256
 oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_BUCKET} --name ${CURRENT_BRANCH_NAME}/verrazzano-analysis-linux-amd64.tar.gz --file verrazzano-analysis-linux-amd64.tar.gz
