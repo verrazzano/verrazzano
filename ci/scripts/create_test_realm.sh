bb606fde1e9a0504b080397b73b5bb11dc645d4f
Jonathan Maron <jonathan.maron@oracle.com>
2022-06-06 16:30:15 -0400

    template relam creation script

--- ci/scripts/create_test_realm.sh
+++ ci/scripts/create_test_realm.sh
@@ -4,30 +4,26 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 #
 
-userpass="${REALM_USER_PASSWORD:-testuserpass}"
-
-realmName="${REALM_NAME:-test-realm}"
-
 # login
-/opt/jboss/keycloak/bin/kcadm.sh config credentials --server http://localhost:8080/auth --realm master --user keycloakadmin --password ${KEYCLOAK_PASSWORD}
+/opt/jboss/keycloak/bin/kcadm.sh config credentials --server http://localhost:8080/auth --realm master --user keycloakadmin --password ##KEYCLOAK_PASSWORD##
 
 #create realm
-/opt/jboss/keycloak/bin/kcadm.sh create realms -s realm=${realmName} -s enabled=false
+/opt/jboss/keycloak/bin/kcadm.sh create realms -s realm=##REALM_NAME## -s enabled=false
 
 # create a user
-/opt/jboss/keycloak/bin/kcadm.sh create users -r ${realmName} -s username=testuser -s enabled=true
+/opt/jboss/keycloak/bin/kcadm.sh create users -r ##REALM_NAME## -s username=testuser -s enabled=true
 
 # set user password
-/opt/jboss/keycloak/bin/kcadm.sh set-password -r ${realmName} --username testuser --new-password ${userpass}
+/opt/jboss/keycloak/bin/kcadm.sh set-password -r ##REALM_NAME## --username testuser --new-password ##REALM_USER_PASSWORD##
 
 # create a keycloak client
-/opt/jboss/keycloak/bin/kcadm.sh create clients -r ${realmName} -s clientId=appsclient -s enabled=true -s directAccessGrantsEnabled=true -s publicClient=true
+/opt/jboss/keycloak/bin/kcadm.sh create clients -r ##REALM_NAME## -s clientId=appsclient -s enabled=true -s directAccessGrantsEnabled=true -s publicClient=true
 
 # create a role
-/opt/jboss/keycloak/bin/kcadm.sh create roles -r ${realmName} -s name=customer
+/opt/jboss/keycloak/bin/kcadm.sh create roles -r ##REALM_NAME## -s name=customer
 
 # map user to role
-/opt/jboss/keycloak/bin/kcadm.sh add-roles -r ${realmName} --uusername testuser --rolename customer
+/opt/jboss/keycloak/bin/kcadm.sh add-roles -r ##REALM_NAME## --uusername testuser --rolename customer
 
 # enable realm
-/opt/jboss/keycloak/bin/kcadm.sh update realms/${realmName} -s enabled=true
+/opt/jboss/keycloak/bin/kcadm.sh update realms/##REALM_NAME## -s enabled=true
