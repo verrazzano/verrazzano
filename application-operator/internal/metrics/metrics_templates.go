5185a4427337edef851f89a4d686e021388a9745
davisgomes <davisgomes@g.ucla.edu>
2022-06-09 13:41:21 -0700

    VZ-6049 default template implementation

--- application-operator/internal/metrics/metrics_templates.go
+++ application-operator/internal/metrics/metrics_templates.go
@@ -6,21 +6,31 @@ package metrics
 import (
 	"context"
 	"fmt"
+	promoperapi "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
+	"github.com/verrazzano/verrazzano/pkg/log/vzlog"
+	k8scorev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 
 	vzapi "github.com/verrazzano/verrazzano/application-operator/apis/app/v1alpha1"
-	"github.com/verrazzano/verrazzano/pkg/constants"
+	"github.com/verrazzano/verrazzano/application-operator/constants"
+	vzconst "github.com/verrazzano/verrazzano/pkg/constants"
 	"go.uber.org/zap"
 	k8smetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	k8sclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
+const (
+	workloadSourceLabel = "__meta_kubernetes_pod_label_app_verrazzano_io_workload"
+)
+
 // GetMetricsTemplate returns the MetricsTemplate given in the MetricsBinding
 func GetMetricsTemplate(ctx context.Context, client k8sclient.Client, metricsBinding *vzapi.MetricsBinding, log *zap.SugaredLogger) (*vzapi.MetricsTemplate, error) {
 	template := vzapi.MetricsTemplate{
 		TypeMeta: k8smetav1.TypeMeta{
-			Kind:       constants.MetricsTemplateKind,
-			APIVersion: constants.MetricsTemplateAPIVersion,
+			Kind:       vzconst.MetricsTemplateKind,
+			APIVersion: vzconst.MetricsTemplateAPIVersion,
 		},
 	}
 
@@ -37,23 +47,25 @@ func GetMetricsTemplate(ctx context.Context, client k8sclient.Client, metricsBin
 }
 
 // HandleDefaultMetricsTemplate handles pre-Verrazzano 1.4 metrics bindings that use the default
-// metrics template, by creating/updating a service monitor that does the same work as the default
-// template, and deleting the metrics binding.
-func HandleDefaultMetricsTemplate(ctx context.Context, client k8sclient.Client, metricsBinding *vzapi.MetricsBinding, log *zap.SugaredLogger) error {
+// metrics template, by creating/updating a service monitor that does the same work as the default template
+func HandleDefaultMetricsTemplate(ctx context.Context, client k8sclient.Client, metricsBinding *vzapi.MetricsBinding, log vzlog.VerrazzanoLogger) error {
 	log.Infof("Default metrics template used by metrics binding %s/%s, service monitor time!", metricsBinding.Namespace, metricsBinding.Name)
-	// GetMetricsTemplate(metricsBinding)
-	// createScrapeInfoFromMetricsTemplate()
-	// createservicemonitor
+
+	// Create the Service monitor from information gathered from the Metrics Binding
+	scrapeInfo, err := createScrapeInfo(ctx, client, metricsBinding, log)
+	if err != nil {
+		return err
+	}
+	serviceMonitor := promoperapi.ServiceMonitor{}
+	serviceMonitor.SetName(metricsBinding.Name)
+	serviceMonitor.SetNamespace(metricsBinding.Namespace)
+	_, err = controllerutil.CreateOrUpdate(ctx, client, &serviceMonitor, func() error {
+		return PopulateServiceMonitor(scrapeInfo, &serviceMonitor, log)
+	})
+	if err != nil {
+		return log.ErrorfNewErr("Failed to create or update the service monitor for the Metrics Binding %s/%s: %v", metricsBinding.Namespace, metricsBinding.Name, err)
+	}
 	return nil
-	// if create service monitor succeeded, our conversion of legacy MetricsBinding is
-	// done. Keep the MetricsBinding in the custom metrics template use case so we know this is a "legacy" app, update it with the
-	// additionalScrapeConfig config map instead of the promConfigMap name
-	// metricsBinding.Spec.ServiceMonitor = serviceMonitor.Name
-	// err := a.Client.Update(ctx, metricsBinding)
-	// if err != nil {
-	// 	log.Errorf("Failed to update MetricsBinding with service monitor information: %v", err)
-	// 	return admission.Errored(http.StatusInternalServerError, err)
-	// }
 }
 
 // HandleCustomMetricsTemplate handles pre-Verrazzano 1.4 metrics bindings that use a custom
@@ -64,6 +76,41 @@ func HandleCustomMetricsTemplate(ctx context.Context, client k8sclient.Client, m
 	return nil
 }
 
-// func (r *Reconciler) createOrUpdateServiceMonitor(metricsBinding vzapi.MetricsBinding) (*ServiceMonitor, error) {
-//
-// }
+func createScrapeInfo(ctx context.Context, client k8sclient.Client, metricsBinding *vzapi.MetricsBinding, log vzlog.VerrazzanoLogger) (ScrapeInfo, error) {
+	var scrapeInfo ScrapeInfo
+
+	// Get the workload object from the Metrics Binding to populate the Service Monitor
+	workload := metricsBinding.Spec.Workload
+	workloadObject := unstructured.Unstructured{}
+	workloadObject.SetKind(workload.TypeMeta.Kind)
+	workloadObject.SetAPIVersion(workload.TypeMeta.APIVersion)
+	workloadName := types.NamespacedName{Namespace: metricsBinding.Namespace, Name: workload.Name}
+	err := client.Get(ctx, workloadName, &workloadObject)
+	if err != nil {
+		return scrapeInfo, log.ErrorfNewErr("Failed to get the workload %s from the Metrics Binding %s/%s: %v", workload.Name, metricsBinding.Namespace, metricsBinding.Name, err)
+	}
+
+	// Get the namespace for the Metrics Binding to check if Istio is enabled
+	workloadNamespace := k8scorev1.Namespace{}
+	err = client.Get(context.TODO(), k8sclient.ObjectKey{Name: metricsBinding.GetNamespace()}, &workloadNamespace)
+	if err != nil {
+		return scrapeInfo, log.ErrorfNewErr("Failed to get metrics binding namespace %s: %v", metricsBinding.GetName(), err)
+	}
+
+	// Verify if Istio is enabled from the Namespace annotations
+	value, ok := workloadNamespace.Labels[constants.LabelIstioInjection]
+	istioEnabled := ok && value == "enabled"
+	scrapeInfo.IstioEnabled = &istioEnabled
+
+	// Match the Verrazzano workload application labels that get applied by the Metrics Binding labeler
+	value, ok = workloadObject.GetLabels()[constants.MetricsWorkloadLabel]
+	if !ok {
+		return scrapeInfo, log.ErrorfNewErr("Failed to find the annotation %s on the target workload", constants.MetricsWorkloadLabel)
+	}
+	scrapeInfo.KeepLabels = map[string]string{workloadSourceLabel: value}
+
+	// Add a port to the Service Monitor endpoints
+	scrapeInfo.Ports = 1
+
+	return scrapeInfo, nil
+}
--- application-operator/internal/metrics/metrics_templates_test.go
+++ application-operator/internal/metrics/metrics_templates_test.go
@@ -2,12 +2,17 @@ package metrics
 
 import (
 	"context"
+	promoperapi "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
+	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"testing"
 
 	asserts "github.com/stretchr/testify/assert"
 	vzapi "github.com/verrazzano/verrazzano/application-operator/apis/app/v1alpha1"
-	"github.com/verrazzano/verrazzano/pkg/constants"
+	"github.com/verrazzano/verrazzano/application-operator/constants"
+	vzconst "github.com/verrazzano/verrazzano/pkg/constants"
 	"github.com/verrazzano/verrazzano/pkg/log/vzlog"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
@@ -26,8 +31,8 @@ const (
 
 var metricsTemplate = vzapi.MetricsTemplate{
 	TypeMeta: metav1.TypeMeta{
-		Kind:       constants.MetricsTemplateKind,
-		APIVersion: constants.MetricsTemplateAPIVersion,
+		Kind:       vzconst.MetricsTemplateKind,
+		APIVersion: vzconst.MetricsTemplateAPIVersion,
 	},
 	ObjectMeta: metav1.ObjectMeta{
 		Namespace: testMetricsTemplateNamespace,
@@ -52,7 +57,7 @@ var metricsBinding = vzapi.MetricsBinding{
 		Workload: vzapi.Workload{
 			Name: testDeploymentName,
 			TypeMeta: metav1.TypeMeta{
-				Kind:       constants.DeploymentWorkloadKind,
+				Kind:       vzconst.DeploymentWorkloadKind,
 				APIVersion: deploymentGroup + "/" + deploymentVersion,
 			},
 		},
@@ -76,4 +81,109 @@ func TestGetMetricsTemplate(t *testing.T) {
 	template, err := GetMetricsTemplate(context.Background(), c, localMetricsBinding, log.GetZapLogger())
 	assert.NoError(err, "Expected no error getting the MetricsTemplate from the MetricsBinding")
 	assert.NotNil(template)
+}
+
+// TestGetMetricsTemplate tests the retrieval process of the metrics template
+// GIVEN a metrics binding
+// WHEN the function receives the binding
+// THEN a scrape config gets generated for the target workload
+func TestHandleDefaultMetricsTemplate(t *testing.T) {
+	assert := asserts.New(t)
+
+	scheme := runtime.NewScheme()
+	_ = vzapi.AddToScheme(scheme)
+	_ = corev1.AddToScheme(scheme)
+	_ = appsv1.AddToScheme(scheme)
+	_ = promoperapi.AddToScheme(scheme)
+
+	plainNs := &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: testMetricsBindingNamespace,
+		},
+	}
+	labeledNs := plainNs.DeepCopy()
+	labeledNs.Labels = map[string]string{constants.LabelIstioInjection: "enabled"}
+
+	plainWorkload := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: testMetricsBindingNamespace,
+			Name:      testDeploymentName,
+		},
+	}
+	labeledWorkload := plainWorkload.DeepCopy()
+	labeledWorkload.Labels = map[string]string{constants.MetricsWorkloadLabel: testDeploymentName}
+
+	serviceMonitorNSN := types.NamespacedName{Namespace: testMetricsBindingNamespace, Name: testMetricsBindingName}
+
+	tests := []struct {
+		name        string
+		workload    *appsv1.Deployment
+		namespace   *corev1.Namespace
+		expectError bool
+	}{
+		{
+			name:        "test no workload",
+			workload:    &appsv1.Deployment{},
+			namespace:   labeledNs,
+			expectError: true,
+		},
+		{
+			name:        "test no namespace",
+			workload:    labeledWorkload,
+			namespace:   &corev1.Namespace{},
+			expectError: true,
+		},
+		{
+			name:        "test workload no label",
+			workload:    plainWorkload,
+			namespace:   labeledNs,
+			expectError: true,
+		},
+		{
+			name:        "test workload and namespace label",
+			workload:    labeledWorkload,
+			namespace:   labeledNs,
+			expectError: false,
+		},
+		{
+			name:        "test workload label only",
+			workload:    labeledWorkload,
+			namespace:   plainNs,
+			expectError: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := fake.NewClientBuilder().WithScheme(scheme).WithRuntimeObjects([]runtime.Object{
+				&metricsTemplate,
+				tt.workload,
+				tt.namespace,
+			}...).Build()
+
+			localMetricsBinding := metricsBinding.DeepCopy()
+
+			log := vzlog.DefaultLogger()
+			err := HandleDefaultMetricsTemplate(context.Background(), c, localMetricsBinding, log)
+			if tt.expectError {
+				assert.Error(err, "Expected error handling the default MetricsTemplate")
+				return
+			} else {
+				assert.NoError(err, "Expected no error handling the default MetricsTemplate")
+			}
+
+			// Get the service monitor for analysis
+			serviceMonitor := promoperapi.ServiceMonitor{}
+			err = c.Get(context.TODO(), serviceMonitorNSN, &serviceMonitor)
+			assert.NoError(err, "Expected no error getting the Service Monitor")
+
+			assert.Equal(1, len(serviceMonitor.Spec.Endpoints))
+			assert.Contains(serviceMonitor.Spec.Endpoints[0].RelabelConfigs[1].SourceLabels, promoperapi.LabelName(workloadSourceLabel))
+			if _, ok := tt.namespace.Labels[constants.LabelIstioInjection]; ok {
+				assert.Equal("https", serviceMonitor.Spec.Endpoints[0].Scheme)
+			} else {
+				assert.Equal("http", serviceMonitor.Spec.Endpoints[0].Scheme)
+			}
+		})
+	}
+
 }
