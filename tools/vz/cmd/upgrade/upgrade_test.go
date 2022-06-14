18ec2d20465732346a31cc56302a8a412a1956d1
Robert Donat <robert.donat@oracle.com>
2022-06-14 21:44:51 +0000

    start adding unit tests

--- tools/vz/cmd/upgrade/upgrade_test.go
+++ tools/vz/cmd/upgrade/upgrade_test.go
@@ -5,29 +5,70 @@ package upgrade
 
 import (
 	"bytes"
+	"github.com/stretchr/testify/assert"
+	vzconstants "github.com/verrazzano/verrazzano/pkg/constants"
+	vzapi "github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1alpha1"
+	"github.com/verrazzano/verrazzano/tools/vz/pkg/constants"
+	"github.com/verrazzano/verrazzano/tools/vz/test/helpers"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
 	k8scheme "k8s.io/client-go/kubernetes/scheme"
 	"os"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
-	"github.com/verrazzano/verrazzano/tools/vz/test/helpers"
-	"k8s.io/cli-runtime/pkg/genericclioptions"
 )
 
-// TestUninstallCmd - check that command reports not implemented yet
-func TestUninstallCmd(t *testing.T) {
-	c := fake.NewClientBuilder().WithScheme(k8scheme.Scheme).Build()
+// TestUpgradeCmdDefaultNoWait
+// GIVEN a CLI upgrade command with all defaults and --wait==false
+//  WHEN I call cmd.Execute for upgrade
+//  THEN the CLI upgrade command is successful
+func TestUpgradeCmdDefaultNoWait(t *testing.T) {
+	vpo := &corev1.Pod{
+		TypeMeta: metav1.TypeMeta{},
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: vzconstants.VerrazzanoInstallNamespace,
+			Name:      constants.VerrazzanoPlatformOperator,
+			Labels: map[string]string{
+				"app": constants.VerrazzanoPlatformOperator,
+			},
+		},
+		Status: corev1.PodStatus{
+			InitContainerStatuses: []corev1.ContainerStatus{
+				{
+					Name:  "webhook-init",
+					Ready: true,
+				},
+			},
+			ContainerStatuses: []corev1.ContainerStatus{
+				{
+					Name:  "verrazzano-platform-operator",
+					Ready: true,
+				},
+			},
+		},
+	}
+	vz := &vzapi.Verrazzano{
+		TypeMeta: metav1.TypeMeta{},
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "default",
+			Name:      "verrazzano",
+		},
+	}
+	_ = vzapi.AddToScheme(k8scheme.Scheme)
+	c := fake.NewClientBuilder().WithScheme(k8scheme.Scheme).WithObjects(vpo, vz).Build()
 
-	// Send the command output to a byte buffer
+	// Send stdout stderr to a byte buffer
 	buf := new(bytes.Buffer)
 	errBuf := new(bytes.Buffer)
 	rc := helpers.NewFakeRootCmdContext(genericclioptions.IOStreams{In: os.Stdin, Out: buf, ErrOut: errBuf})
 	rc.SetClient(c)
 	cmd := NewCmdUpgrade(rc)
 	assert.NotNil(t, cmd)
+	cmd.PersistentFlags().Set(constants.WaitFlag, "false")
 
-	// Run upgrade command, check for the expected status results to be displayed
-	//	err := cmd.Execute()
-	//	assert.NoError(t, err)
+	// Run upgrade command
+	err := cmd.Execute()
+	assert.NoError(t, err)
+	assert.Equal(t, "", errBuf.String())
 }
