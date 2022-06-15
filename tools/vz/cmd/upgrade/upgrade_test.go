15d6ac43ff174fc4c441bf3ab4ceb8f4f2d81628
Robert Donat <robert.donat@oracle.com>
2022-06-15 01:07:09 +0000

    more unit tests

--- tools/vz/cmd/upgrade/upgrade_test.go
+++ tools/vz/cmd/upgrade/upgrade_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	vzconstants "github.com/verrazzano/verrazzano/pkg/constants"
 	vzapi "github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1alpha1"
+	cmdHelpers "github.com/verrazzano/verrazzano/tools/vz/cmd/helpers"
 	"github.com/verrazzano/verrazzano/tools/vz/pkg/constants"
 	"github.com/verrazzano/verrazzano/tools/vz/test/helpers"
 	corev1 "k8s.io/api/core/v1"
@@ -71,4 +72,77 @@ func TestUpgradeCmdDefaultNoWait(t *testing.T) {
 	err := cmd.Execute()
 	assert.NoError(t, err)
 	assert.Equal(t, "", errBuf.String())
+}
+
+// TestUpgradeCmdDefaultTimeout
+// GIVEN a CLI upgrade command with all defaults and --timeout=2s
+//  WHEN I call cmd.Execute for upgrade
+//  THEN the CLI upgrade command times out
+func TestUpgradeCmdDefaultTimeout(t *testing.T) {
+	vpo := &corev1.Pod{
+		TypeMeta: metav1.TypeMeta{},
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: vzconstants.VerrazzanoInstallNamespace,
+			Name:      constants.VerrazzanoPlatformOperator,
+			Labels: map[string]string{
+				"app": constants.VerrazzanoPlatformOperator,
+			},
+		},
+	}
+	vz := &vzapi.Verrazzano{
+		TypeMeta: metav1.TypeMeta{},
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "default",
+			Name:      "verrazzano",
+		},
+	}
+	_ = vzapi.AddToScheme(k8scheme.Scheme)
+	c := fake.NewClientBuilder().WithScheme(k8scheme.Scheme).WithObjects(vpo, vz).Build()
+
+	// Send stdout stderr to a byte buffer
+	buf := new(bytes.Buffer)
+	errBuf := new(bytes.Buffer)
+	rc := helpers.NewFakeRootCmdContext(genericclioptions.IOStreams{In: os.Stdin, Out: buf, ErrOut: errBuf})
+	rc.SetClient(c)
+	cmd := NewCmdUpgrade(rc)
+	assert.NotNil(t, cmd)
+	cmd.PersistentFlags().Set(constants.TimeoutFlag, "2s")
+
+	// Run upgrade command
+	err := cmd.Execute()
+	assert.NoError(t, err)
+	assert.Equal(t, "", errBuf.String())
+	assert.Contains(t, buf.String(), "Timeout 2s exceeded waiting for upgrade to complete")
+}
+
+// TestUpgradeCmdDefaultNoVPO
+// GIVEN a CLI upgrade command with all defaults and no VPO found
+//  WHEN I call cmd.Execute for upgrade
+//  THEN the CLI upgrade command fails
+func TestUpgradeCmdDefaultNoVPO(t *testing.T) {
+	vz := &vzapi.Verrazzano{
+		TypeMeta: metav1.TypeMeta{},
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "default",
+			Name:      "verrazzano",
+		},
+	}
+	_ = vzapi.AddToScheme(k8scheme.Scheme)
+	c := fake.NewClientBuilder().WithScheme(k8scheme.Scheme).WithObjects(vz).Build()
+
+	// Send stdout stderr to a byte buffer
+	buf := new(bytes.Buffer)
+	errBuf := new(bytes.Buffer)
+	rc := helpers.NewFakeRootCmdContext(genericclioptions.IOStreams{In: os.Stdin, Out: buf, ErrOut: errBuf})
+	rc.SetClient(c)
+	cmd := NewCmdUpgrade(rc)
+	assert.NotNil(t, cmd)
+
+	// Run upgrade command
+	cmdHelpers.SetVpoWaitRetries(1) // override for unit testing
+	err := cmd.Execute()
+	cmdHelpers.ResetVpoWaitRetries()
+	assert.Error(t, err)
+	assert.EqualError(t, err, "verrazzano-platform-operator pod not found in namespace verrazzano-install")
+	assert.Equal(t, errBuf.String(), "Error: verrazzano-platform-operator pod not found in namespace verrazzano-install\n")
 }
