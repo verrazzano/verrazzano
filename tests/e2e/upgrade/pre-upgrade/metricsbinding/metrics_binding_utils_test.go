de12253dc0ee7a85e5ff54b000e24bcf34c90598
davisgomes <davisgomes@g.ucla.edu>
2022-06-15 10:42:38 -0700

    VZ-4069 add suite test

---
+++ tests/e2e/upgrade/pre-upgrade/metricsbinding/metrics_binding_utils_test.go
@@ -0,0 +1,100 @@
+// Copyright (c) 2021, 2022, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package metricsbinding
+
+import (
+	"fmt"
+	v1 "k8s.io/api/core/v1"
+	"time"
+
+	"github.com/onsi/ginkgo/v2"
+	"github.com/onsi/gomega"
+	"github.com/verrazzano/verrazzano/pkg/test/framework"
+	"github.com/verrazzano/verrazzano/tests/e2e/pkg"
+	"k8s.io/apimachinery/pkg/api/errors"
+)
+
+const (
+	shortWaitTimeout     = 10 * time.Minute
+	shortPollingInterval = 10 * time.Second
+	longWaitTimeout      = 15 * time.Minute
+	longPollingInterval  = 20 * time.Second
+)
+
+func createNamespace(namespace, istioInjection string, t framework.TestFramework) {
+	t.Logs.Info("Create namespace")
+	gomega.Eventually(func() (*v1.Namespace, error) {
+		nsLabels := map[string]string{"verrazzano-managed": "true", "istio-injeciton": istioInjection}
+		nsExists, err := pkg.DoesNamespaceExist(namespace)
+		if err != nil {
+			return nil, err
+		}
+		if !nsExists {
+			return pkg.CreateNamespace(namespace, nsLabels)
+		}
+		return pkg.GetNamespace(namespace)
+	}, shortWaitTimeout, shortPollingInterval).ShouldNot(gomega.BeNil())
+}
+
+// deployApplication deploys an application and namespace given the application parameters
+func deployApplication(namespace, yamlPath, podPrefix string, t framework.TestFramework) {
+	t.Logs.Info("Deploy test application")
+	t.Logs.Info("Create application from yaml path")
+	gomega.Eventually(func() error {
+		return pkg.CreateOrUpdateResourceFromFileInGeneratedNamespace(yamlPath, namespace)
+	}, shortWaitTimeout, shortPollingInterval).ShouldNot(gomega.HaveOccurred())
+
+	t.Logs.Info("Check application pods are running")
+	gomega.Eventually(func() bool {
+		result, err := pkg.PodsRunning(namespace, []string{podPrefix})
+		if err != nil {
+			ginkgo.AbortSuite(fmt.Sprintf("One or more pods are not running in the namespace: %v, error: %v", namespace, err))
+		}
+		return result
+	}, longWaitTimeout, longPollingInterval).Should(gomega.BeTrue())
+}
+
+func UndeployApplication(namespace string, yamlPath string, promConfigJobName string, t framework.TestFramework) {
+	t.Logs.Info("Delete application")
+	gomega.Eventually(func() error {
+		return pkg.DeleteResourceFromFileInGeneratedNamespace(yamlPath, namespace)
+	}, shortWaitTimeout, shortPollingInterval).ShouldNot(gomega.HaveOccurred())
+
+	t.Logs.Info("Remove application from Prometheus Config")
+	gomega.Eventually(func() bool {
+		return pkg.IsAppInPromConfig(promConfigJobName)
+	}, shortWaitTimeout, shortPollingInterval).Should(gomega.BeFalse(), "Expected application to be removed from Prometheus config")
+
+	t.Logs.Info("Delete namespace")
+	gomega.Eventually(func() error {
+		return pkg.DeleteNamespace(namespace)
+	}, shortWaitTimeout, shortPollingInterval).ShouldNot(gomega.HaveOccurred())
+
+	t.Logs.Info("Wait for namespace finalizer to be removed")
+	gomega.Eventually(func() bool {
+		return pkg.CheckNamespaceFinalizerRemoved(namespace)
+	}, shortWaitTimeout, shortPollingInterval).Should(gomega.BeTrue())
+
+	t.Logs.Info("Wait for namespace to be deleted")
+	gomega.Eventually(func() bool {
+		_, err := pkg.GetNamespace(namespace)
+		return err != nil && errors.IsNotFound(err)
+	}, shortWaitTimeout, shortPollingInterval).Should(gomega.BeTrue())
+}
+
+// deployConfigMap deploys a ConfigMap from a file path
+func deployConfigMap(namespace, configMapYamlPath string, t framework.TestFramework) {
+	t.Logs.Info("Create ConfigMap resource")
+	gomega.Eventually(func() error {
+		return pkg.CreateOrUpdateResourceFromFileInGeneratedNamespace(configMapYamlPath, namespace)
+	}, shortWaitTimeout, shortPollingInterval).ShouldNot(gomega.HaveOccurred())
+}
+
+// deployTemplate deploys a Metrics Template from a file path
+func deployTemplate(namespace, templateYamlPath string, t framework.TestFramework) {
+	t.Logs.Info("Create template resource")
+	gomega.Eventually(func() error {
+		return pkg.CreateOrUpdateResourceFromFileInGeneratedNamespace(templateYamlPath, namespace)
+	}, shortWaitTimeout, shortPollingInterval).ShouldNot(gomega.HaveOccurred())
+}
