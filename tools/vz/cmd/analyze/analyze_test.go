a76d1fd0d307114d357cf1f4d3e0bfb54e4aceea
Saidu Dosapati <saidu.babu.dosapati@oracle.com>
2022-06-16 14:30:47 -0400

    Added more UT's for analyze command

--- tools/vz/cmd/analyze/analyze_test.go
+++ tools/vz/cmd/analyze/analyze_test.go
@@ -3,11 +3,14 @@ package analyze
 import (
 	"bytes"
 	"github.com/stretchr/testify/assert"
+	"github.com/verrazzano/verrazzano/tools/vz/pkg/constants"
 	"github.com/verrazzano/verrazzano/tools/vz/test/helpers"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 	"os"
 	"testing"
 )
+
+var imagePullCase1 = "/Users/sdosapat/src/github.com/verrazzano/verrazzano/tools/vz/pkg/analysis/test/cluster/image-pull-case1"
 
 func TestAnalyzeCommandDefault(t *testing.T) {
 	// Send stdout stderr to a byte buffer
@@ -18,4 +21,80 @@ func TestAnalyzeCommandDefault(t *testing.T) {
 	assert.NotNil(t, cmd)
 	err := cmd.Execute()
 	assert.Contains(t, err.Error(), "Analyze failed")
+}
+
+func TestAnalyzeCommandValidCapturedDir(t *testing.T) {
+	buf := new(bytes.Buffer)
+	errBuf := new(bytes.Buffer)
+	rc := helpers.NewFakeRootCmdContext(genericclioptions.IOStreams{In: os.Stdin, Out: buf, ErrOut: errBuf})
+	cmd := NewCmdAnalyze(rc)
+	assert.NotNil(t, cmd)
+	cmd.PersistentFlags().Set(constants.DirectoryFlagName, imagePullCase1)
+	err := cmd.Execute()
+	assert.Nil(t, err)
+	assert.Contains(t, buf.String(), "pulling images have been detected due to the image not being found")
+}
+
+func TestAnalyzeCommandInvalidReportFormat(t *testing.T) {
+	buf := new(bytes.Buffer)
+	errBuf := new(bytes.Buffer)
+	rc := helpers.NewFakeRootCmdContext(genericclioptions.IOStreams{In: os.Stdin, Out: buf, ErrOut: errBuf})
+	cmd := NewCmdAnalyze(rc)
+	assert.NotNil(t, cmd)
+	cmd.PersistentFlags().Set(constants.DirectoryFlagName, imagePullCase1)
+	cmd.PersistentFlags().Set(constants.ReportFormatFlagName, "invalid-report-format")
+	err := cmd.Execute()
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), "unsupported output format")
+}
+
+func TestAnalyzeCommandValidReportFormat(t *testing.T) {
+	buf := new(bytes.Buffer)
+	errBuf := new(bytes.Buffer)
+	rc := helpers.NewFakeRootCmdContext(genericclioptions.IOStreams{In: os.Stdin, Out: buf, ErrOut: errBuf})
+	cmd := NewCmdAnalyze(rc)
+	assert.NotNil(t, cmd)
+	cmd.PersistentFlags().Set(constants.DirectoryFlagName, imagePullCase1)
+	cmd.PersistentFlags().Set(constants.ReportFormatFlagName, "simple")
+	err := cmd.Execute()
+	assert.Nil(t, err)
+}
+
+func TestAnalyzeCommandReportFileOutputSuccess(t *testing.T) {
+	buf := new(bytes.Buffer)
+	errBuf := new(bytes.Buffer)
+	rc := helpers.NewFakeRootCmdContext(genericclioptions.IOStreams{In: os.Stdin, Out: buf, ErrOut: errBuf})
+	cmd := NewCmdAnalyze(rc)
+	assert.NotNil(t, cmd)
+	cmd.PersistentFlags().Set(constants.DirectoryFlagName, imagePullCase1)
+	cmd.PersistentFlags().Set(constants.ReportFormatFlagName, "simple")
+	cmd.PersistentFlags().Set(constants.ReportFileFlagName, "TestAnalyzeCommandReportFileOutput")
+	err := cmd.Execute()
+	assert.Nil(t, err)
+	assert.FileExists(t, "TestAnalyzeCommandReportFileOutput")
+	os.Remove("TestAnalyzeCommandReportFileOutput")
+	assert.NoFileExists(t, "TestAnalyzeCommandReportFileOutput")
+}
+
+func TestAnalyzeCommandCapturedDirRequired(t *testing.T) {
+	buf := new(bytes.Buffer)
+	errBuf := new(bytes.Buffer)
+	rc := helpers.NewFakeRootCmdContext(genericclioptions.IOStreams{In: os.Stdin, Out: buf, ErrOut: errBuf})
+	cmd := NewCmdAnalyze(rc)
+	assert.NotNil(t, cmd)
+	err := cmd.Execute()
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), "required flag(s)")
+}
+
+func TestAnalyzeCommandCapturedDirInvalid(t *testing.T) {
+	buf := new(bytes.Buffer)
+	errBuf := new(bytes.Buffer)
+	rc := helpers.NewFakeRootCmdContext(genericclioptions.IOStreams{In: os.Stdin, Out: buf, ErrOut: errBuf})
+	cmd := NewCmdAnalyze(rc)
+	assert.NotNil(t, cmd)
+	cmd.PersistentFlags().Set(constants.DirectoryFlagName, "../")
+	err := cmd.Execute()
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), "Cluster Analyzer runAnalysis didn't find any clusters")
 }
