bc6b5d6010081ba82ebbc1b5303a8a1059f15f4e
Watsh Rajneesh <watsh.rajneesh@oracle.com>
2022-06-22 20:59:15 +0000

    go fmt'd the file metrics_test.go

--- tests/e2e/metrics/syscomponents/metrics_test.go
+++ tests/e2e/metrics/syscomponents/metrics_test.go
@@ -9,10 +9,10 @@ import (
 	"strings"
 	"time"
 
-	"github.com/verrazzano/verrazzano/pkg/test/framework"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/verrazzano/verrazzano/pkg/k8sutil"
+	"github.com/verrazzano/verrazzano/pkg/test/framework"
 	"github.com/verrazzano/verrazzano/tests/e2e/pkg"
 )
 
