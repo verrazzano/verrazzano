9d509020e0bd8f717f354377ab4757b609013ce4
GeorgeAeillo <george.aeillo@gmail.com>
2022-06-23 15:21:05 -0400

    Made edits that were requested in pull requests

--- platform-operator/metricsExporter/metricsExporter.go
+++ platform-operator/metricsExporter/metricsExporter.go
@@ -1,13 +1,14 @@
 // Copyright (c) 2022, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
-package metrics
+package metricsExporter
 
 import (
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-func MetricsEndpointInitalizer() {
+func InitalizeMetricsEndpoint() {
 	http.Handle("/metrics", promhttp.Handler())
 	http.ListenAndServe(":9100", nil)
 
