3e22ce8de5c01a5714af32b1f7cb6f72c3841ccc
Raaghavcodes <raaghavwadhawan@gmail.com>
2022-06-24 12:30:51 -0700

    goroutine handler edit

--- application-operator/metricsexporter/metricsexporter.go
+++ application-operator/metricsexporter/metricsexporter.go
@@ -1,5 +1,6 @@
 // Copyright (c) 2022, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
 package metricsexporter
 
 import (
@@ -8,7 +9,13 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+// InitalizeMetricsEndpoint creates a goroutine so this process will not halt execcution of the code
 func InitalizeMetricsEndpoint() {
+	go InitalizeMetricsEndpointHelper()
+}
+
+//InitalizeMetricsEndpointHelper creates and serves a /metrics endpoint at 9100 for Prometheus to scrape metrics from
+func InitalizeMetricsEndpointHelper() {
 	http.Handle("/metrics", promhttp.Handler())
 	http.ListenAndServe(":9100", nil)
 
