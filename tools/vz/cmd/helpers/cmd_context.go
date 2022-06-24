8caa50131637be435cdf489d4860e2bb17e75fab
Steven Shapiro <31927916+stevshap@users.noreply.github.com>
2022-06-24 11:54:37 -0400

    VZ-5952 CLI Uninstall Command (#3419)
    Adding uninstall functionality to Verrazzano CLI
    
    Signed-off-by: stshapir <steven.shapiro@oracle.com>

--- tools/vz/cmd/helpers/cmd_context.go
+++ tools/vz/cmd/helpers/cmd_context.go
@@ -11,8 +11,10 @@ import (
 	"github.com/verrazzano/verrazzano/pkg/k8sutil"
 	vzapi "github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1alpha1"
 	"github.com/verrazzano/verrazzano/tools/vz/pkg/constants"
+	adminv1 "k8s.io/api/admissionregistration/v1"
 	appv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 	"k8s.io/client-go/kubernetes"
@@ -60,6 +62,8 @@ func (rc *RootCmdContext) GetClient(cmd *cobra.Command) (client.Client, error) {
 	scheme := runtime.NewScheme()
 	_ = vzapi.AddToScheme(scheme)
 	_ = corev1.SchemeBuilder.AddToScheme(scheme)
+	_ = adminv1.SchemeBuilder.AddToScheme(scheme)
+	_ = rbacv1.SchemeBuilder.AddToScheme(scheme)
 	_ = appv1.SchemeBuilder.AddToScheme(scheme)
 
 	return client.New(config, client.Options{Scheme: scheme})
