0b6a5893dd796d58ba9c25eaf4af666ab6c5dfbe
GeorgeAeillo <george.aeillo@gmail.com>
2022-06-27 09:59:59 -0400

    Changed error message to lowercase
    Signed-off-by: GeorgeAeillo <george.aeillo@gmail.com>

--- platform-operator/metricsexporter/metricsexporter.go
+++ platform-operator/metricsexporter/metricsexporter.go
@@ -11,13 +11,15 @@ import (
 	"k8s.io/apimachinery/pkg/util/wait"
 )
 
+// PreInstall called multiple times but only successfully completed once
+//
 //InitalizeMetricsEndpoint creates and serves a /metrics endpoint at 9100 for Prometheus to scrape metrics from
 func InitalizeMetricsEndpoint() {
 	go wait.Until(func() {
 		http.Handle("/metrics", promhttp.Handler())
 		err := http.ListenAndServe(":9100", nil)
 		if err != nil {
-			zap.S().Errorf("Failed to start metrics server for Verrazzano-Platform-Operator: %v", err)
+			zap.S().Errorf("Failed to start metrics server for verrazzano-platform-operator: %v", err)
 		}
 	}, time.Second*3, wait.NeverStop)
 }
