daff08ef4ed2b7dfa28096ffbc661ec9ec964760
Vicky Sarkar <vicky.sarkar@oracle.com>
2022-07-05 20:13:23 +0530

    new bom validator ginkgo

---
+++ tests/e2e/verify-install/bom-validator/bom_validator_suite_test.go
@@ -0,0 +1,15 @@
+// Copyright (c) 2020, 2022, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package bomvalidator
+
+import (
+	"testing"
+	"github.com/onsi/ginkgo/v2"
+	"github.com/onsi/gomega"
+)
+
+func TestIstio(t *testing.T) {
+	gomega.RegisterFailHandler(ginkgo.Fail)
+	ginkgo.RunSpecs(t, "Bom Validator Suite")
+}
---
+++ tests/e2e/verify-install/bom-validator/bom_validator_test.go
@@ -0,0 +1,23 @@
+// Copyright (c) 2022, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package bomvalidator
+
+import (
+
+)
+
+const (
+	waitTimeout        = 3 * time.Minute
+	pollingInterval    = 10 * time.Second
+)
+
+var t = framework.NewTestFramework("bom validator")
+
+var _ = t.AfterEach(func() {})
+
+var _ = t.Describe("Bom Validator", Label("f:platform-lcm.install"), func() {
+	t.Context("after successful validation", func() {
+
+	})
+})
