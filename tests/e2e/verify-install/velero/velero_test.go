73e60ba570e987861d94799f48aba8c9f61a9860
Abhishek Mitra <abhishek.a.mitra@oracle.com>
2022-07-05 21:54:09 -0700

    VZ-5698: Update verify test check with appropriare image check

--- tests/e2e/verify-install/velero/velero_test.go
+++ tests/e2e/verify-install/velero/velero_test.go
@@ -20,7 +20,7 @@ const (
 	waitTimeout                  = 3 * time.Minute
 	pollingInterval              = 10 * time.Second
 	veleroRestoreHelperConfigMap = "restic-restore-action-config"
-	resticHelperImage            = "ghcr.io/verrazzano/velero-restic-restore-helper"
+	resticHelperImage            = "velero-restic-restore-helper"
 )
 
 var (
@@ -96,8 +96,8 @@ var _ = t.Describe("Velero", Label("f:platform-lcm.install"), func() {
 						pkg.Log(pkg.Error, fmt.Sprintf("Unable to retrieve configmap %s in the namespace: %s, error: %v", veleroRestoreHelperConfigMap, constants.VeleroNameSpace, err))
 						return false
 					}
-					if !strings.HasPrefix(cfgMap.Data["image"], resticHelperImage) {
-						pkg.Log(pkg.Error, fmt.Sprintf("Configmap %s does not have the image prefix %s in the namespace: %s", veleroRestoreHelperConfigMap, resticHelperImage, constants.VeleroNameSpace))
+					if !strings.Contains(cfgMap.Data["image"], resticHelperImage) {
+						pkg.Log(pkg.Error, fmt.Sprintf("Configmap %s does not have the image %s in the namespace: %s. Image found = %s ", veleroRestoreHelperConfigMap, resticHelperImage, constants.VeleroNameSpace, cfgMap.Data["image"]))
 						return false
 					}
 				}
