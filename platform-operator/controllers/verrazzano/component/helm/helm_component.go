cca740b0cc83ade009bb12f87cc8e93b6c758074
Paul Mackin <paul.mackin@oracle.com>
2022-07-06 15:35:42 -0400

    revert helm component change

--- platform-operator/controllers/verrazzano/component/helm/helm_component.go
+++ platform-operator/controllers/verrazzano/component/helm/helm_component.go
@@ -329,10 +329,6 @@ func (h HelmComponent) Uninstall(context spi.ComponentContext) error {
 }
 
 func (h HelmComponent) PostUninstall(context spi.ComponentContext) error {
-
-	//	kubectl delete validatingwebhookconfigurations.admissionregistration.k8s.io coherence-operator-validating-webhook-configuration --ignore-not-found
-	//	kubectl delete mutatingwebhookconfigurations.admissionregistration.k8s.io coherence-operator-mutating-webhook-configuration --ignore-not-found
-
 	return nil
 }
 
