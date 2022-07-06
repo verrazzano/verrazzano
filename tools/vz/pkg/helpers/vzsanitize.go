32c7f80680c96dea86e708c642e55db50cfdcbef
Saidu Dosapati <saidu.babu.dosapati@oracle.com>
2022-07-06 15:09:54 -0400

    Cleaned up dead code

--- tools/vz/pkg/helpers/vzsanitize.go
+++ tools/vz/pkg/helpers/vzsanitize.go
@@ -4,13 +4,8 @@
 package helpers
 
 import (
-	"bufio"
 	"crypto/sha256"
 	"encoding/hex"
-	"fmt"
-	"io"
-	"os"
-	"path/filepath"
 	"regexp"
 )
 
@@ -24,76 +19,6 @@ func InitRegexToReplacementMap() {
 	regexToReplacementList = append(regexToReplacementList, ipv4Regex)
 }
 
-// GetMatchingFiles returns the filenames for files that match a regular expression.
-func GetMatchingFiles(rootDirectory string, fileMatchRe *regexp.Regexp) (fileMatches []string, err error) {
-	if len(rootDirectory) == 0 {
-		return nil, fmt.Errorf("GetMatchingFiles requires a rootDirectory")
-	}
-
-	if fileMatchRe == nil {
-		return nil, fmt.Errorf("GetMatchingFiles requires a regular expression")
-	}
-
-	walkFunc := func(fileName string, fileInfo os.FileInfo, err error) error {
-		if !fileMatchRe.MatchString(fileName) {
-			return nil
-		}
-		if !fileInfo.IsDir() {
-			fileMatches = append(fileMatches, fileName)
-		}
-		return nil
-	}
-
-	err = filepath.Walk(rootDirectory, walkFunc)
-	if err != nil {
-		return nil, err
-	}
-	return fileMatches, err
-}
-
-// SanitizeDirectory will iterate over the given root directory to sanitize each file
-// ReplaceFile is to whether replace the file with sanitized content or create a new file with _tmpfoo suffix
-func SanitizeDirectory(rootDirectory string, replaceFile bool) {
-	InitRegexToReplacementMap()
-	fileMatches, err := GetMatchingFiles(rootDirectory, regexp.MustCompile(".*"))
-	check(err)
-	for _, eachFile := range fileMatches {
-		SanitizeFile(eachFile, replaceFile)
-	}
-}
-
-// SanitizeFile the given file, replaceFile boolean is to whether inplace replacement or to a new file
-func SanitizeFile(path string, replaceFile bool) error {
-	fmt.Println("path provided is: ", path)
-	input, err := os.Open(path)
-	check(err)
-	defer input.Close()
-
-	outFile, err := os.Create(path + "_tmpfoo")
-	check(err)
-	defer outFile.Close()
-
-	br := bufio.NewReader(input)
-	for {
-		l, _, err := br.ReadLine()
-
-		if err == io.EOF || l == nil {
-			break
-		}
-		check(err)
-
-		sanitizedLine := SanitizeALine(string(l))
-		outFile.WriteString(sanitizedLine + "\n")
-	}
-
-	if replaceFile {
-		check(os.Remove(path))
-		check(os.Rename(path+"_tmpfoo", path))
-	}
-
-	return nil
-}
-
 // SanitizeALine sanitizes each line in a given file,
 // Sanitizes based on the regex map initialized above
 func SanitizeALine(l string) string {
@@ -104,13 +29,6 @@ func SanitizeALine(l string) string {
 		l = regexp.MustCompile(eachRegex).ReplaceAllString(l, getSha256Hash(l))
 	}
 	return l
-}
-
-func check(e error) error {
-	if e != nil {
-		return e
-	}
-	return nil
 }
 
 // getSha256Hash generates the one way hash for the input string
--- tools/vz/pkg/helpers/vzsanitize_test.go
+++ tools/vz/pkg/helpers/vzsanitize_test.go
@@ -2,8 +2,6 @@ package helpers
 
 import (
 	"github.com/stretchr/testify/assert"
-	"os"
-	"regexp"
 	"testing"
 )
 
@@ -11,42 +9,7 @@ var testDir = "../../pkg/analysis/test/files"
 
 const testIP = "132.23.234.24"
 
-func TestSanitizeDirectoryMatch(t *testing.T) {
-	SanitizeDirectory(testDir, false)
-	testFile := testDir + "/sanity_test.txt"
-	testFiletmp := testFile + "_tmpfoo"
-	file, err := os.ReadFile(testFiletmp)
-	assert.Nil(t, err)
-	//assert.Contains(t, string(file), "REDACT")
-	assert.NotContains(t, string(file), testIP)
-	testCleanup(testDir)
-}
-
-func TestSanitizeDirectoryNoMatch(t *testing.T) {
-	SanitizeDirectory(testDir, false)
-	testFile := testDir + "/sanity_test_no_match.txt"
-	testFiletmp := testFile + "_tmpfoo"
-	file, err := os.ReadFile(testFiletmp)
-	assert.Nil(t, err)
-	//assert.NotContains(t, string(file), "REDACT")
-	assert.Contains(t, string(file), "2134.46.75689.235464356768")
-	testCleanup(testDir)
-}
-
-func testCleanup(path string) error {
-	files, err := GetMatchingFiles(path, regexp.MustCompile(".*_tmpfoo"))
-	if err != nil {
-		return err
-	}
-	for _, eachFile := range files {
-		println("deleting temp file: ", eachFile)
-		os.Remove(eachFile)
-	}
-	return nil
-}
-
 func TestSanitizeALine(t *testing.T) {
 	assert.NotContains(t, SanitizeALine(testIP), testIP)
 	assert.Contains(t, SanitizeALine("test.me.test.me"), "test")
-	testCleanup(testDir)
 }
