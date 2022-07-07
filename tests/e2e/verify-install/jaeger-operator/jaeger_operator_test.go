2aaf19500b18d917bd34e80cc22766fb7bbf2bd5
Asha Yarangatta <asha.yarangatta@oracle.com>
2022-07-07 09:42:51 +0530

    NO JIRA: Update Jaeger Operator verify-install E2E tests for private registry

--- tests/e2e/verify-install/jaeger-operator/jaeger_operator_test.go
+++ tests/e2e/verify-install/jaeger-operator/jaeger_operator_test.go
@@ -20,18 +20,19 @@ const (
 	waitTimeout        = 3 * time.Minute
 	pollingInterval    = 10 * time.Second
 	jaegerOperatorName = "jaeger-operator"
-	operatorImage      = "ghcr.io/verrazzano/jaeger-operator"
 )
 
 var (
 	jaegerOperatorCrds = []string{
 		"jaegers.jaegertracing.io",
 	}
+	imagePrefix          = pkg.GetImagePrefix()
+	operatorImage        = imagePrefix + "/verrazzano/" + jaegerOperatorName
 	expectedJaegerImages = map[string]string{
-		"JAEGER-AGENT-IMAGE":     "ghcr.io/verrazzano/jaeger-agent",
-		"JAEGER-COLLECTOR-IMAGE": "ghcr.io/verrazzano/jaeger-collector",
-		"JAEGER-QUERY-IMAGE":     "ghcr.io/verrazzano/jaeger-query",
-		"JAEGER-INGESTER-IMAGE":  "ghcr.io/verrazzano/jaeger-ingester",
+		"JAEGER-AGENT-IMAGE":     imagePrefix + "/verrazzano/jaeger-agent",
+		"JAEGER-COLLECTOR-IMAGE": imagePrefix + "/verrazzano/jaeger-collector",
+		"JAEGER-QUERY-IMAGE":     imagePrefix + "/verrazzano/jaeger-query",
+		"JAEGER-INGESTER-IMAGE":  imagePrefix + "/verrazzano/jaeger-ingester",
 	}
 )
 
