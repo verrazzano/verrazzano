184b54ed933de6779ca85002b17d47206cc8b799
davisgomes <49332517+davisgomes@users.noreply.github.com>
2022-07-07 16:14:59 -0400

    VZ-6321 fix command executor

--- platform-operator/controllers/verrazzano/component/rancher/rancher_uninstall.go
+++ platform-operator/controllers/verrazzano/component/rancher/rancher_uninstall.go
@@ -4,15 +4,21 @@
 package rancher
 
 import (
+	osexec "os/exec"
+
 	"github.com/verrazzano/verrazzano/pkg/os"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/spi"
 )
 
 // postUninstall removes the objects after the Helm uninstall process finishes
 func postUninstall(ctx spi.ComponentContext) error {
-	_, stdErr, err := os.RunBash("/usr/local/bin/system-tools", "remove", "-c", "/home/verrazzano/kubeconfig", "--force")
+	ctx.Log().Oncef("Running the Rancher uninstall system tool")
+
+	cmd := osexec.Command("Rancher uninstall binary", "/usr/local/bin/system-tools", "remove", "-c", "/home/verrazzano/kubeconfig", "--force")
+	_, stdErr, err := os.DefaultRunner{}.Run(cmd)
 	if err != nil {
 		return ctx.Log().ErrorNewErr("Failed to run system tools  for Rancher deletion: %s: %v", stdErr, err)
 	}
+
 	return nil
 }
