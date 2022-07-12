d972cd696c24e8b9910d2365358805f55c5e0d32
Robert Donat <robert.donat@oracle.com>
2022-07-12 18:48:22 -0400

    VZ-6320: Move nginx uninstall from scripts to Go code (#3554)

--- platform-operator/scripts/uninstall/uninstall-steps/2-uninstall-system-components.sh
+++ platform-operator/scripts/uninstall/uninstall-steps/2-uninstall-system-components.sh
@@ -13,28 +13,6 @@ UNINSTALL_DIR=$SCRIPT_DIR/..
 
 set -o pipefail
 
-function delete_nginx() {
-  # uninstall ingress-nginx
-  log "Deleting ingress-nginx"
-  helm ls -n ingress-nginx \
-    | awk '/ingress-controller/ {print $1}' \
-    | xargsr helm uninstall -n ingress-nginx \
-    || err_return $? "Could not delete ingress-controller from helm" || return $? # return on pipefail
-
-  # delete the nginx clusterrole and clusterrolebinding
-  log "Deleting ClusterRoles and ClusterRoleBindings for ingress-nginx"
-  kubectl delete clusterrole ingress-controller-ingress-nginx --ignore-not-found=true || err_return $? "Could not delete ClusterRole ingress-controller-ingress-nginx" || return $?
-  kubectl delete clusterrolebinding ingress-controller-ingress-nginx --ignore-not-found=true || err_return $? "Could not delete ClusterRoleBinding ingress-controller-ingress-nginx" || return $?
-
-  # delete ingress-nginx namespace
-  log "Deleting ingress-nginx namespace finalizers"
-  patch_k8s_resources namespaces ":metadata.name" "Could not remove finalizer from namespace ingress-nginx" '/ingress-nginx/ {print $1}' '{"metadata":{"finalizers":null}}' \
-    || return $? # return on pipefail
-
-  log "Deleting ingress-nginx namespace"
-  kubectl delete namespace ingress-nginx --ignore-not-found=true || err_return $? "Could not delete namespace ingress-nginx" || return $?
-}
-
 function delete_rancher() {
   local rancher_exists=$(kubectl get namespace cattle-system --ignore-not-found)
   if [ -z "$rancher_exists" ] ; then
@@ -181,4 +159,3 @@ function delete_rancher() {
 }
 
 action "Deleting Rancher Components" delete_rancher || exit 1
-action "Deleting NGINX Components" delete_nginx || exit 1
