ee49bb1c79217c6551995bb07f61f3f1dee98637
Raaghavcodes <raaghavwadhawan@gmail.com>
2022-07-12 13:37:09 -0700

    taking out error metric test

--- application-operator/metricsexporter/metricsexporter_test.go
+++ application-operator/metricsexporter/metricsexporter_test.go
@@ -8,13 +8,8 @@ import (
 
 	"github.com/prometheus/client_golang/prometheus/testutil"
 	asserts "github.com/stretchr/testify/assert"
-	//"github.com/verrazzano/verrazzano/application-operator/metricsexporter"
 )
 
-//TestCollectReconcileMetrics tests the CollectReconcileMetrics fn
-//GIVEN a call to CollectReconcileMetrics
-//WHEN A starting time is passed into the function
-//THEN the function updates the reconcileCounterMetric by 1 and creates a new time for that reconcile in the reconcileLastDurationMetric
 func TestCollectReconcileSuccessfulMetric(t *testing.T) {
 	assert := asserts.New(t)
 	tests := []struct {
@@ -34,22 +29,23 @@ func TestCollectReconcileSuccessfulMetric(t *testing.T) {
 		})
 	}
 }
-func TestCollectReconcileErrorMetric(t *testing.T) {
-	assert := asserts.New(t)
-	tests := []struct {
-		name string
-	}{
-		{
-			name: "Test that reoncile counter is incremented by one when function has failed",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			r := reconcileMap["appconfig"]
-			reconcileFailedCounterBefore := testutil.ToFloat64(r.reconcileFailed)
-			r.reconcileSuccessful.Inc()
-			reconcileFailedCounterAfter := testutil.ToFloat64(r.reconcileFailed)
-			assert.Equal(reconcileFailedCounterBefore, reconcileFailedCounterAfter-1)
-		})
-	}
-}
+
+// func TestCollectReconcileErrorMetric(t *testing.T) {
+// 	assert := asserts.New(t)
+// 	tests := []struct {
+// 		name string
+// 	}{
+// 		{
+// 			name: "Test that reoncile counter is incremented by one when function has failed",
+// 		},
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			r := reconcileMap["appconfig"]
+// 			reconcileFailedCounterBefore := testutil.ToFloat64(r.reconcileFailed)
+// 			r.reconcileSuccessful.Inc()
+// 			reconcileFailedCounterAfter := testutil.ToFloat64(r.reconcileFailed)
+// 			assert.Equal(reconcileFailedCounterBefore, reconcileFailedCounterAfter-1)
+// 		})
+// 	}
+// }
