a00dea81d91c08e2ddaf5894bf76f9e0b83a1d69
tony.vlatas <tony.vlatas@oracle.com>
2022-07-12 13:44:41 -0400

    They changed the name in 4.0.0-BETA4 from tags to freeform_tags, we are using 3.3.0 so I need to switch it to use tags

--- tests/e2e/config/scripts/terraform/cluster/main.tf
+++ tests/e2e/config/scripts/terraform/cluster/main.tf
@@ -55,7 +55,7 @@ module "oke" {
 
   node_pool_image_id = var.node_pool_image_id
 
-  freeform_tags = {
+  tags = {
     vcn = { "verrazzano-infra/Branch" = var.branch_tag, "verrazzano-infra/Pipeline" = var.pipeline_tag, "verrazzano-infra/JobScenario" = var.job_scenario_tag }
     bastion = { "verrazzano-infra/Branch" = var.branch_tag, "verrazzano-infra/Pipeline" = var.pipeline_tag, "verrazzano-infra/JobScenario" = var.job_scenario_tag }
     operator = { "verrazzano-infra/Branch" = var.branch_tag, "verrazzano-infra/Pipeline" = var.pipeline_tag, "verrazzano-infra/JobScenario" = var.job_scenario_tag }
