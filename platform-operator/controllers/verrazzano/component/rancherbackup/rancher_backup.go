509e0e09c0dbe1d2aad4663e5cac957d67eda88e
Abhishek Mitra <abhishek.a.mitra@oracle.com>
2022-07-18 14:41:47 -0700

    VZ-5771: Remove append overrides

--- platform-operator/controllers/verrazzano/component/rancherbackup/rancher_backup.go
+++ platform-operator/controllers/verrazzano/component/rancherbackup/rancher_backup.go
@@ -5,7 +5,6 @@ package rancherbackup
 
 import (
 	"context"
-	"github.com/verrazzano/verrazzano/pkg/bom"
 	"github.com/verrazzano/verrazzano/pkg/k8sutil"
 	vzapi "github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1alpha1"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/spi"
@@ -26,13 +25,6 @@ const (
 // isRancherBackupOperatorReady checks if the Rancher Backup deployment is ready
 func isRancherBackupOperatorReady(context spi.ComponentContext) bool {
 	return status.DeploymentsAreReady(context.Log(), context.Client(), deployments, 1, componentPrefix)
-}
-
-// AppendOverrides appends Helm value overrides for the Rancher Backup component's Helm chart
-func AppendOverrides(compContext spi.ComponentContext, _ string, _ string, _ string, kvs []bom.KeyValue) ([]bom.KeyValue, error) {
-	arguments := []bom.KeyValue{}
-	kvs = append(kvs, arguments...)
-	return kvs, nil
 }
 
 // GetOverrides gets the install overrides
