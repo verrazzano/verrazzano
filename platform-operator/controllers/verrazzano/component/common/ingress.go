7592ad5823294ed4ba6f45404e99971835c8ae5c
Anders Swanson <91502735+anders-swanson@users.noreply.github.com>
2022-07-19 12:22:27 -0700

    VZ 6271, VZ 6475: Prometheus HA (#3599)
    VZ 6271, VZ 6475: Prometheus HA

--- platform-operator/controllers/verrazzano/component/common/ingress.go
+++ platform-operator/controllers/verrazzano/component/common/ingress.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 
 	ctrlerrors "github.com/verrazzano/verrazzano/pkg/controller/errors"
+	"github.com/verrazzano/verrazzano/pkg/vzmap"
 	"github.com/verrazzano/verrazzano/platform-operator/constants"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/spi"
 	"github.com/verrazzano/verrazzano/platform-operator/internal/vzconfig"
@@ -16,11 +17,30 @@ import (
 	controllerruntime "sigs.k8s.io/controller-runtime"
 )
 
+type IngressProperties struct {
+	IngressName      string
+	HostName         string
+	TLSSecretName    string
+	ExtraAnnotations map[string]string
+}
+
+//SameSiteCookieAnnotations creates annotations for same site cookies to enable sticky sessions on an ingress
+func SameSiteCookieAnnotations(cookieName string) map[string]string {
+	return map[string]string{
+		"nginx.ingress.kubernetes.io/affinity":                                 "cookie",
+		"nginx.ingress.kubernetes.io/session-cookie-conditional-samesite-none": "true",
+		"nginx.ingress.kubernetes.io/session-cookie-expires":                   "86400",
+		"nginx.ingress.kubernetes.io/session-cookie-max-age":                   "86400",
+		"nginx.ingress.kubernetes.io/session-cookie-name":                      cookieName,
+		"nginx.ingress.kubernetes.io/session-cookie-samesite":                  "Strict",
+	}
+}
+
 // CreateOrUpdateSystemComponentIngress creates or updates an ingress for a Verrazzano system component
-func CreateOrUpdateSystemComponentIngress(ctx spi.ComponentContext, ingressName string, hostName string, tlsSecretName string) error {
+func CreateOrUpdateSystemComponentIngress(ctx spi.ComponentContext, props IngressProperties) error {
 	// create the ingress in the same namespace as Auth Proxy, note that we cannot use authproxy.ComponentNamespace here because it creates an import cycle
 	ingress := netv1.Ingress{
-		ObjectMeta: metav1.ObjectMeta{Name: ingressName, Namespace: constants.VerrazzanoSystemNamespace},
+		ObjectMeta: metav1.ObjectMeta{Name: props.IngressName, Namespace: constants.VerrazzanoSystemNamespace},
 	}
 
 	_, err := controllerruntime.CreateOrUpdate(context.TODO(), ctx.Client(), &ingress, func() error {
@@ -29,7 +49,7 @@ func CreateOrUpdateSystemComponentIngress(ctx spi.ComponentContext, ingressName 
 			return ctx.Log().ErrorfNewErr("Failed building DNS domain name: %v", err)
 		}
 		ingressClassName := vzconfig.GetIngressClassName(ctx.EffectiveCR())
-		qualifiedHostName := fmt.Sprintf("%s.%s", hostName, dnsSubDomain)
+		qualifiedHostName := fmt.Sprintf("%s.%s", props.HostName, dnsSubDomain)
 		pathType := netv1.PathTypeImplementationSpecific
 
 		ingRule := netv1.IngressRule{
@@ -57,7 +77,7 @@ func CreateOrUpdateSystemComponentIngress(ctx spi.ComponentContext, ingressName 
 		ingress.Spec.TLS = []netv1.IngressTLS{
 			{
 				Hosts:      []string{qualifiedHostName},
-				SecretName: tlsSecretName,
+				SecretName: props.TLSSecretName,
 			},
 		}
 		ingress.Spec.Rules = []netv1.IngressRule{ingRule}
@@ -76,11 +96,11 @@ func CreateOrUpdateSystemComponentIngress(ctx spi.ComponentContext, ingressName 
 			ingress.Annotations["external-dns.alpha.kubernetes.io/target"] = ingressTarget
 			ingress.Annotations["external-dns.alpha.kubernetes.io/ttl"] = "60"
 		}
-
+		ingress.Annotations = vzmap.UnionStringMaps(ingress.Annotations, props.ExtraAnnotations)
 		return nil
 	})
 	if ctrlerrors.ShouldLogKubenetesAPIError(err) {
-		return ctx.Log().ErrorfNewErr("Failed creating/updating ingress %s: %v", ingressName, err)
+		return ctx.Log().ErrorfNewErr("Failed creating/updating ingress %s: %v", props.IngressName, err)
 	}
 	return err
 }
