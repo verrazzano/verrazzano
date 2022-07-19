2d74b5910351d94a6f5c5ec8ddcdbf95f8e7746e
Shashank Kumar <shashank.z.kumar@oracle.com>
2022-07-19 09:04:49 +0000

    oci-ccm cleanup

--- tests/e2e/config/scripts/get_ingress_ip.sh
+++ tests/e2e/config/scripts/get_ingress_ip.sh
@@ -6,7 +6,7 @@
 TEST_CONFIG_FILE=$1
 DNS_DOMAIN=${2:-"nip.io"}
 INGRESS_IP=$(kubectl get svc ingress-controller-ingress-nginx-controller -n ingress-nginx -o json | jq -r '.status.loadBalancer.ingress[0].ip')
-if [[ $DNS_DOMAIN == "nip.io" || $DNS_DOMAIN == "sslip.io" ]]; then
+if [[ -z $DNS_DOMAIN || $DNS_DOMAIN == "nip.io" || $DNS_DOMAIN == "sslip.io" ]]; then
     sed -i "s/XX_DNS_ZONE_XX/${INGRESS_IP}.${DNS_DOMAIN}/" ${TEST_CONFIG_FILE}
 else 
     sed -i "s/XX_DNS_ZONE_XX/${DNS_DOMAIN}/" ${TEST_CONFIG_FILE}
