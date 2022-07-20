248a639b4f1d4f41b8e5b2edcd2977860e1742ef
Shashank Kumar <shashank.z.kumar@oracle.com>
2022-07-20 14:15:09 +0000

    delete scripts

--- tests/e2e/config/scripts/get_private_ip_address.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-# Copyright (c) 2022, Oracle and/or its affiliates.
-# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
-#
-# Provide private-ip-ocid to fetch the associated ip-address 
-oci network private-ip get --private-ip-id $1 | jq -r '.data."ip-address"'
