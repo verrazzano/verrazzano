c670962041d01d74a7221235b987219b8772784d
Anders Swanson <anders.swanson@oracle.com>
2022-07-22 07:54:53 -0700

    WIP

--- tests/e2e/ha/monitor/prometheus_ha_test.go
+++ tests/e2e/ha/monitor/prometheus_ha_test.go
@@ -5,12 +5,12 @@ package monitor
 
 import (
 	. "github.com/onsi/ginkgo/v2"
-	"time"
+	. "github.com/onsi/gomega"
+	"github.com/verrazzano/verrazzano/tests/e2e/pkg"
 )
 
 var _ = t.Describe("Prometheus HA Monitoring", Label("f:platform-lcm:ha"), func() {
 	RunningUntilShutdownIt("verifies Prometheus is ready and running", func() {
-		t.Logs.Info("running prometheus tests!")
-		time.Sleep(5 * time.Second)
+		Expect(pkg.VerifyPrometheusComponent(t.Logs, api, vzHTTPClient, vmiCredentials)).To(BeTrue())
 	})
 })
