82a1bc6cd7d05c5176be2527279cc4fb4d706bcf
Michael Cico <46466506+michael-cico@users.noreply.github.com>
2022-07-22 11:27:41 -0400

    Fix upgrade pipeline usage of prepare script

--- ci/scripts/prepare_jenkins_at_environment.sh
+++ ci/scripts/prepare_jenkins_at_environment.sh
@@ -65,21 +65,21 @@ cd ${GO_REPO_PATH}/verrazzano
 echo "Determine which yaml file to use to install the Verrazzano Platform Operator"
 cd ${GO_REPO_PATH}/verrazzano
 
-OPERATOR_FILE=${OPERATOR_FILE:-"${WORKSPACE}/acceptance-test-operator.yaml"}
+TARGET_OPERATOR_FILE=${TARGET_OPERATOR_FILE:-"${WORKSPACE}/acceptance-test-operator.yaml"}
 if [ -z "$OPERATOR_YAML" ] && [ "" = "${OPERATOR_YAML}" ]; then
   # Derive the name of the operator.yaml file, copy or generate the file, then install
   if [ "NONE" = "${VERRAZZANO_OPERATOR_IMAGE}" ]; then
       echo "Using operator.yaml from object storage"
       oci --region us-phoenix-1 os object get --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_BUCKET} --name ${OCI_OS_LOCATION}/operator.yaml --file ${WORKSPACE}/downloaded-operator.yaml
-      cp -v ${WORKSPACE}/downloaded-operator.yaml ${OPERATOR_FILE}
+      cp -v ${WORKSPACE}/downloaded-operator.yaml ${TARGET_OPERATOR_FILE}
   else
       echo "Generating operator.yaml based on image name provided: ${VERRAZZANO_OPERATOR_IMAGE}"
-      env IMAGE_PULL_SECRETS=verrazzano-container-registry DOCKER_IMAGE=${VERRAZZANO_OPERATOR_IMAGE} ./tools/scripts/generate_operator_yaml.sh > ${OPERATOR_FILE}
+      env IMAGE_PULL_SECRETS=verrazzano-container-registry DOCKER_IMAGE=${VERRAZZANO_OPERATOR_IMAGE} ./tools/scripts/generate_operator_yaml.sh > ${TARGET_OPERATOR_FILE}
   fi
 else
   # The operator.yaml filename was provided, install using that file.
   echo "Using provided operator.yaml file: " ${OPERATOR_YAML}
-  cp -v ${OPERATOR_YAML} ${OPERATOR_FILE}
+  TARGET_OPERATOR_FILE=${OPERATOR_YAML}
 fi
 
 # Create the verrazzano-install namespace
@@ -114,7 +114,7 @@ fi
 
 echo "Installing Verrazzano on Kind"
 cd ${GO_REPO_PATH}/verrazzano/tools/vz
-GO111MODULE=on GOPRIVATE=github.com/verrazzano go run main.go install --filename ${VZ_INSTALL_FILE} --operator-file ${OPERATOR_FILE}
+GO111MODULE=on GOPRIVATE=github.com/verrazzano go run main.go install --filename ${VZ_INSTALL_FILE} --operator-file ${TARGET_OPERATOR_FILE}
 result=$?
 ${GO_REPO_PATH}/verrazzano/tools/scripts/k8s-dump-cluster.sh -d ${WORKSPACE}/post-vz-install-cluster-dump -r ${WORKSPACE}/post-vz-install-cluster-dump/analysis.report
 if [[ $result -ne 0 ]]; then
