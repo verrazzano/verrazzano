4a4bd2954ec8720641bbc103f6ac32c6b3e36db7
Shashank Kumar <shashank.z.kumar@oracle.com>
2022-07-26 06:34:59 +0000

    updated copyright header

--- tests/e2e/config/scripts/process_olcne_install_yaml.sh
+++ tests/e2e/config/scripts/process_olcne_install_yaml.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #
-# Copyright (c), 2022, Oracle and/or its affiliates.
+# Copyright (c) 2022, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+#
 # Usage: ./process_olcne_install_yaml.sh <CONFIG_FILE_TO_EDIT> [ENVIRONMENT_NAME INSTALL_PROFILE]
 # - Updates the given yaml file
 
