385f6098709466a341b206def9569b22bf2d0511
Anand Francis Joseph <anand.francis.joseph.augustin@oracle.com>
2022-07-27 00:02:18 +0530

    Preliminary check in for tests
    Signed-off-by: Anand Francis Joseph <anand.francis.joseph.augustin@oracle.com>

---
+++ tests/e2e/jaeger/jaeger_test.go
@@ -0,0 +1,199 @@
+// Copyright (c) 2022, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package jaeger
+
+import (
+	"fmt"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/verrazzano/verrazzano/pkg/k8sutil"
+	"github.com/verrazzano/verrazzano/pkg/test/framework"
+	"github.com/verrazzano/verrazzano/pkg/test/framework/metrics"
+	"github.com/verrazzano/verrazzano/tests/e2e/pkg"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"time"
+)
+
+const (
+	longWaitTimeout          = 20 * time.Minute
+	longPollingInterval      = 20 * time.Second
+	shortPollingInterval     = 10 * time.Second
+	shortWaitTimeout         = 5 * time.Minute
+	imagePullWaitTimeout     = 40 * time.Minute
+	imagePullPollingInterval = 30 * time.Second
+)
+
+var (
+	t                        = framework.NewTestFramework("jaeger")
+	generatedNamespace       = pkg.GenerateNamespace("jaeger-tracing")
+	expectedPodsHelloHelidon = []string{"hello-helidon-workload"}
+	waitTimeout              = 10 * time.Minute
+	pollingInterval          = 30 * time.Second
+	failed                   = false
+	beforeSuitePassed        = false
+)
+
+var _ = t.BeforeSuite(func() {
+	start := time.Now()
+	Eventually(func() (*v1.Namespace, error) {
+		nsLabels := map[string]string{
+			"verrazzano-managed": "true",
+			"istio-injection":    istioInjection}
+		return pkg.CreateNamespace(namespace, nsLabels)
+	}).WithPolling(shortPollingInterval).WithTimeout(shortWaitTimeout).ShouldNot(BeNil())
+
+	Eventually(func() error {
+		return pkg.CreateOrUpdateResourceFromFileInGeneratedNamespace("testdata/jaeger/helidon/helidon-logging-comp.yaml", namespace)
+	}).WithPolling(shortPollingInterval).WithTimeout(shortWaitTimeout).ShouldNot(HaveOccurred())
+
+	Eventually(func() error {
+		return pkg.CreateOrUpdateResourceFromFileInGeneratedNamespace("testdata/jaeger/helidon/helidon-logging-app.yaml", namespace)
+	}).WithPolling(shortPollingInterval).WithTimeout(shortWaitTimeout).ShouldNot(HaveOccurred())
+
+	Eventually(func() bool {
+		return pkg.ContainerImagePullWait(namespace, expectedPodsHelloHelidon)
+	}, imagePullWaitTimeout, imagePullPollingInterval).Should(BeTrue())
+	// Verify hello-helidon-workload pod is running
+	Eventually(helloHelidonPodsRunning, waitTimeout, pollingInterval).Should(BeTrue())
+	beforeSuitePassed = true
+	metrics.Emit(t.Metrics.With("deployment_elapsed_time", time.Since(start).Milliseconds()))
+})
+
+var _ = t.AfterEach(func() {
+	failed = failed || CurrentSpecReport().Failed()
+})
+
+var _ = t.AfterSuite(func() {
+	if failed || !beforeSuitePassed {
+		pkg.ExecuteClusterDumpWithEnvVarConfig()
+	}
+	// undeploy the application here
+	start := time.Now()
+
+	t.Logs.Info("Delete application")
+	Eventually(func() error {
+		return pkg.DeleteResourceFromFileInGeneratedNamespace("testdata/jaeger/helidon/helidon-logging-app.yaml", namespace)
+	}).WithPolling(shortPollingInterval).WithTimeout(shortWaitTimeout).ShouldNot(HaveOccurred())
+
+	t.Logs.Info("Delete components")
+	Eventually(func() error {
+		return pkg.DeleteResourceFromFileInGeneratedNamespace("testdata/jaeger/helidon/helidon-logging-comp.yaml", namespace)
+	}).WithPolling(shortPollingInterval).WithTimeout(shortWaitTimeout).ShouldNot(HaveOccurred())
+
+	t.Logs.Info("Wait for application pods to terminate")
+	Eventually(func() bool {
+		podsTerminated, _ := pkg.PodsNotRunning("helidon-logging", expectedPodsHelloHelidon)
+		return podsTerminated
+	}).WithPolling(shortPollingInterval).WithTimeout(shortWaitTimeout).Should(BeTrue())
+
+	t.Logs.Info("Delete namespace")
+	Eventually(func() error {
+		return pkg.DeleteNamespace(namespace)
+	}).WithPolling(shortPollingInterval).WithTimeout(shortWaitTimeout).ShouldNot(HaveOccurred())
+
+	t.Logs.Info("Wait for Finalizer to be removed")
+	Eventually(func() bool {
+		return pkg.CheckNamespaceFinalizerRemoved("helidon-logging")
+	}).WithPolling(shortPollingInterval).WithTimeout(shortWaitTimeout).Should(BeTrue())
+
+	t.Logs.Info("Wait for namespace to be deleted")
+	Eventually(func() bool {
+		_, err := pkg.GetNamespace("helidon-logging")
+		return err != nil && errors.IsNotFound(err)
+	}).WithPolling(shortPollingInterval).WithTimeout(shortWaitTimeout).Should(BeTrue())
+
+	metrics.Emit(t.Metrics.With("undeployment_elapsed_time", time.Since(start).Milliseconds()))
+})
+
+func isJaegerOperatorEnabled() bool {
+	kubeconfigPath, err := k8sutil.GetKubeConfigLocation()
+	if err != nil {
+		AbortSuite(fmt.Sprintf("Failed to get default kubeconfig path: %s", err.Error()))
+	}
+	return pkg.IsJaegerOperatorEnabled(kubeconfigPath)
+}
+
+// 'It' Wrapper to only run spec if the Jaeger operator is supported on the current Verrazzano version
+func WhenJaegerOperatorInstalledIt(description string, f func()) {
+	kubeconfigPath, err := k8sutil.GetKubeConfigLocation()
+	if err != nil {
+		t.It(description, func() {
+			Fail(fmt.Sprintf("Failed to get default kubeconfig path: %s", err.Error()))
+		})
+	}
+	supported, err := pkg.IsVerrazzanoMinVersion("1.3.0", kubeconfigPath)
+	if err != nil {
+		t.It(description, func() {
+			Fail(fmt.Sprintf("Failed to check Verrazzano version 1.3.0: %s", err.Error()))
+		})
+	}
+	if supported {
+		t.It(description, f)
+	} else {
+		t.Logs.Infof("Skipping check '%v', the Jaeger Operator is not supported", description)
+	}
+}
+
+var _ = t.Describe("Jaeger Operator", Label("f:platform-lcm.install"), func() {
+	t.Context("after successful installation", func() {
+		// GIVEN the Jaeger Operator is enabled and a sample application is installed,
+		// WHEN we check for traces for that service,
+		// THEN we are able to get the traces
+		WhenJaegerOperatorInstalledIt("should have a verrazzano-monitoring namespace", func() {
+			Eventually(func() (bool, error) {
+				if !isJaegerOperatorEnabled() {
+					return true, nil
+				}
+				return true, nil
+			}).WithPolling(shortPollingInterval).WithTimeout(shortWaitTimeout).Should(BeTrue())
+		})
+
+		// GIVEN the Jaeger Operator component is enabled,
+		// WHEN a sample application is installed,
+		// THEN the traces are found in OpenSearch Backend
+		WhenJaegerOperatorInstalledIt("should have running pods", func() {
+			Eventually(func() (bool, error) {
+				if !isJaegerOperatorEnabled() {
+					return true, nil
+				}
+				return true, nil
+			}).WithPolling(shortPollingInterval).WithTimeout(shortWaitTimeout).Should(BeTrue())
+		})
+
+		// GIVEN the Jaeger Operator component is enabled,
+		// WHEN we check for metrics related to Jaeger operator
+		// THEN we see that the metrics are present in prometheus
+		WhenJaegerOperatorInstalledIt("should have the correct default Jaeger images", func() {
+			Eventually(func() (bool, error) {
+				if !isJaegerOperatorEnabled() {
+					return true, nil
+				}
+				return true, nil
+			}).WithPolling(shortPollingInterval).WithTimeout(shortWaitTimeout).Should(BeTrue())
+
+		})
+
+		// GIVEN the Jaeger Operator component is installed with default Jaeger CR enabled
+		// WHEN we check for metrics related to Jaeger Components (jaeger-query, jaeger-collector, jaeger-agent)
+		// THEN we see that the metrics are present in prometheus
+		WhenJaegerOperatorInstalledIt("should have the correct Jaeger Operator CRDs", func() {
+			Eventually(func() (bool, error) {
+				if !isJaegerOperatorEnabled() {
+					return true, nil
+				}
+				return true, nil
+			}).WithPolling(shortPollingInterval).WithTimeout(shortWaitTimeout).Should(BeTrue())
+		})
+	})
+
+})
+
+func helloHelidonPodsRunning() bool {
+	result, err := pkg.PodsRunning(namespace, expectedPodsHelloHelidon)
+	if err != nil {
+		AbortSuite(fmt.Sprintf("One or more pods are not running in the namespace: %v, error: %v", namespace, err))
+	}
+	return result
+}
--- tests/e2e/pkg/jaeger.go
+++ tests/e2e/pkg/jaeger.go
@@ -4,14 +4,73 @@
 package pkg
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/hashicorp/go-retryablehttp"
 	"github.com/verrazzano/verrazzano/platform-operator/constants"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"net/http"
 	"strings"
 )
 
 const (
 	jaegerServiceIndexPrefix = "verrazzano-jaeger-jaeger-service"
 	jaegerSpanIndexPrefix    = "verrazzano-jaeger-jaeger-span"
+	jaegerClusterNameLabel   = "verrazzano_local"
+	jaegerClusterName        = "local"
 )
+
+type JaegerTraceData struct {
+	TraceID string `json:"traceID"`
+	Spans   []struct {
+		TraceID       string `json:"traceID"`
+		SpanID        string `json:"spanID"`
+		Flags         int    `json:"flags"`
+		OperationName string `json:"operationName"`
+		References    []struct {
+			RefType string `json:"refType"`
+			TraceID string `json:"traceID"`
+			SpanID  string `json:"spanID"`
+		} `json:"references"`
+		StartTime int64 `json:"startTime"`
+		Duration  int   `json:"duration"`
+		Tags      []struct {
+			Key   string      `json:"key"`
+			Type  string      `json:"type"`
+			Value interface{} `json:"value"`
+		} `json:"tags"`
+		Logs []struct {
+			Timestamp int64 `json:"timestamp"`
+			Fields    []struct {
+				Key   string `json:"key"`
+				Type  string `json:"type"`
+				Value string `json:"value"`
+			} `json:"fields"`
+		} `json:"logs"`
+		ProcessID string      `json:"processID"`
+		Warnings  interface{} `json:"warnings"`
+	} `json:"spans"`
+	Processes struct {
+		P1 struct {
+			ServiceName string `json:"serviceName"`
+			Tags        []struct {
+				Key   string `json:"key"`
+				Type  string `json:"type"`
+				Value string `json:"value"`
+			} `json:"tags"`
+		} `json:"p1"`
+	} `json:"processes"`
+	Warnings interface{} `json:"warnings"`
+}
+
+type JaegerTraceDataWrapper struct {
+	Data   []JaegerTraceData `json:"data"`
+	Total  int               `json:"total"`
+	Limit  int               `json:"limit"`
+	Offset int               `json:"offset"`
+	Errors interface{}       `json:"errors"`
+}
 
 func VerifyJaegerSpans(service string) bool {
 	return false
@@ -28,13 +87,136 @@ func IsJaegerInstanceCreated() (bool, error) {
 	return false, nil
 }
 
-func GetJaegerIndicesInElasticSearch(kubeconfigPath string) []string {
-	jaegerIndices := []string{}
+func GetJaegerSpanIndexInElasticSearch(kubeconfigPath string) []string {
+	var jaegerIndices []string
 	for _, indexName := range listSystemElasticSearchIndices(kubeconfigPath) {
-		if strings.HasPrefix(indexName, jaegerServiceIndexPrefix) ||
-			strings.HasPrefix(indexName, jaegerSpanIndexPrefix) {
+		if strings.HasPrefix(indexName, jaegerSpanIndexPrefix) {
 			jaegerIndices = append(jaegerIndices, indexName)
 		}
 	}
 	return jaegerIndices
+}
+
+func GetJaegerServiceIndexInElasticSearch(kubeconfigPath string) []string {
+	var jaegerIndices []string
+	for _, indexName := range listSystemElasticSearchIndices(kubeconfigPath) {
+		if strings.HasPrefix(indexName, jaegerServiceIndexPrefix) {
+			jaegerIndices = append(jaegerIndices, indexName)
+		}
+	}
+	return jaegerIndices
+}
+
+// IsJaegerMetricFound validates if the given jaeger metrics contain the labels with values specified as key-value pairs of the map
+func IsJaegerMetricFound(kubeconfigPath, metricName string, kv map[string]string) bool {
+	compMetrics, err := QueryMetricWithLabel(metricName, kubeconfigPath, jaegerClusterNameLabel, jaegerClusterName)
+	if err != nil {
+		return false
+	}
+	metrics := JTq(compMetrics, "data", "result").([]interface{})
+	for _, metric := range metrics {
+		metricFound := true
+		for key, value := range kv {
+			if Jq(metric, "metric", key) != value {
+				metricFound = false
+				break
+			}
+		}
+		return metricFound
+	}
+	return false
+}
+
+func listJaegerTraces(kubeconfigPath string, serviceName string) []string {
+	var traces []string
+	url := fmt.Sprintf("%s/api/traces?service=%s", getJaegerURL(kubeconfigPath), serviceName)
+	username, password, err := getJaegerUsernamePassword(kubeconfigPath)
+	if err != nil {
+		return traces
+	}
+	resp, err := getJaegerWithBasicAuth(url, "", username, password, kubeconfigPath)
+	if err != nil {
+		Log(Error, fmt.Sprintf("Error getting Jaeger traces: url=%s, error=%v", url, err))
+		return traces
+	}
+	if resp.StatusCode != http.StatusOK {
+		Log(Error, fmt.Sprintf("Error retrieving Jaeger traces: url=%s, status=%d", url, resp.StatusCode))
+		return traces
+	}
+	Log(Debug, fmt.Sprintf("traces: %s", resp.Body))
+	var jaegerTraceDataWrapper JaegerTraceDataWrapper
+	json.Unmarshal(resp.Body, &jaegerTraceDataWrapper)
+	for _, traceObj := range jaegerTraceDataWrapper.Data {
+		traces = append(traces, traceObj.TraceID)
+	}
+	return traces
+}
+
+func listServicesInJaeger(kubeconfigPath string) []string {
+	var services []string
+	url := fmt.Sprintf("%s/api/services", getJaegerURL(kubeconfigPath))
+	username, password, err := getJaegerUsernamePassword(kubeconfigPath)
+	if err != nil {
+		return services
+	}
+	resp, err := getJaegerWithBasicAuth(url, "", username, password, kubeconfigPath)
+	if err != nil {
+		Log(Error, fmt.Sprintf("Error getting Jaeger traces: url=%s, error=%v", url, err))
+		return services
+	}
+	if resp.StatusCode != http.StatusOK {
+		Log(Error, fmt.Sprintf("Error retrieving Jaeger traces: url=%s, status=%d", url, resp.StatusCode))
+		return services
+	}
+	Log(Debug, fmt.Sprintf("traces: %s", resp.Body))
+	var serviceMap map[string][]string
+	json.Unmarshal(resp.Body, &serviceMap)
+	for _, name := range serviceMap["data"] {
+		services = append(services, name)
+	}
+	return services
+}
+
+// getJaegerWithBasicAuth access Jaeger with GET using basic auth, using a given kubeconfig
+func getJaegerWithBasicAuth(url string, hostHeader string, username string, password string, kubeconfigPath string) (*HTTPResponse, error) {
+	retryableClient, err := getJaegerClient(kubeconfigPath)
+	if err != nil {
+		return nil, err
+	}
+	return doReq(url, "GET", "", hostHeader, username, password, nil, retryableClient)
+}
+
+// getJaegerClient returns the Jaeger client which can be used for GET/POST operations using a given kubeconfig
+func getJaegerClient(kubeconfigPath string) (*retryablehttp.Client, error) {
+	client, err := GetVerrazzanoHTTPClient(kubeconfigPath)
+	if err != nil {
+		return nil, err
+	}
+	return client, err
+}
+
+// getJaegerURL returns Jaeger URL from the corresponding ingress resource using the given kubeconfig
+func getJaegerURL(kubeconfigPath string) string {
+	clientset, err := GetKubernetesClientsetForCluster(kubeconfigPath)
+	if err != nil {
+		Log(Error, fmt.Sprintf("Failed to get clientset for cluster %v", err))
+		return ""
+	}
+	ingressList, _ := clientset.NetworkingV1().Ingresses(VerrazzanoNamespace).List(context.TODO(), metav1.ListOptions{})
+	for _, ingress := range ingressList.Items {
+		if ingress.Name == "verrazzano-jaeger" {
+			Log(Info, fmt.Sprintf("Found Jaeger Ingress %v, host %s", ingress.Name, ingress.Spec.Rules[0].Host))
+			return fmt.Sprintf("https://%s", ingress.Spec.Rules[0].Host)
+		}
+	}
+	return ""
+}
+
+// getJaegerUsernamePassword returns the username/password for connecting to Jaeger
+func getJaegerUsernamePassword(kubeconfigPath string) (username, password string, err error) {
+	password, err = GetVerrazzanoPasswordInCluster(kubeconfigPath)
+	if err != nil {
+		return "", "", err
+	}
+	return "verrazzano", password, err
 }
