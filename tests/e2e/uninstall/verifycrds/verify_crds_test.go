79a1ecfee55fb21551a87ccccc6ecc0f1ac3c904
Robert Donat <robert.donat@oracle.com>
2022-07-29 19:14:09 +0000

    don't check kiali CRDS

--- tests/e2e/uninstall/verifycrds/verify_crds_test.go
+++ tests/e2e/uninstall/verifycrds/verify_crds_test.go
@@ -127,14 +127,6 @@ var _ = t.Describe("Verify CRDs after uninstall.", Label("f:platform-lcm.unnstal
 		checkCrds(crds, map[string]bool{"coherence.coherence.oracle.com": false}, "coherence.coherence.oracle.com")
 	})
 
-	t.It("Check for expected monitoringdashboards.monitoring.kiali.io CRD", func() {
-		if !pkg.IsManagedClusterProfile() {
-			checkCrds(crds, map[string]bool{"monitoringdashboards.monitoring.kiali.io": false}, "monitoringdashboards.monitoring.kiali.io")
-		} else {
-			pkg.Log(pkg.Info, "monitoringdashboards.monitoring.kiali.io CRD is not checked on a managed-cluster profile")
-		}
-	})
-
 	t.It("Check for unexpected CRDs", func() {
 		var crdsFound = make(map[string]bool)
 		for _, crd := range crds.Items {
