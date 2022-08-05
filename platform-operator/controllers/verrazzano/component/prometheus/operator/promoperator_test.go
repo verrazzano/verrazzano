a1d9f9b02b9647bf9a5a7a7a15286233c9401130
Fred Tibbitts III <44442080+fredtibbitts@users.noreply.github.com>
2022-08-05 09:45:30 -0400

    Move auth proxy ServiceMonitor so it gets installed with Prometheus Operator (#3777)

--- platform-operator/controllers/verrazzano/component/prometheus/operator/promoperator_test.go
+++ platform-operator/controllers/verrazzano/component/prometheus/operator/promoperator_test.go
@@ -479,8 +479,8 @@ func TestApplySystemMonitors(t *testing.T) {
 	monitors.SetGroupVersionKind(schema.GroupVersionKind{Group: "monitoring.coreos.com", Version: "v1", Kind: "ServiceMonitor"})
 	err = client.List(context.TODO(), monitors)
 	assert.NoError(t, err)
-	// expect that 7 ServiceMonitors are created
-	assert.Len(t, monitors.Items, 7)
+	// expect that 8 ServiceMonitors are created
+	assert.Len(t, monitors.Items, 8)
 }
 
 // TestValidatePrometheusOperator tests the validation of the Prometheus Operator installation and the Verrazzano CR
