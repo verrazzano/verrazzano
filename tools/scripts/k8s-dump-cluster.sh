d3991da813ab45a79d5038f73b6cb5e3e50833ac
Saidu Dosapati <saidu.babu.dosapati@oracle.com>
2022-08-10 10:28:08 -0400

    tweaks to k8s-cluster-dump-script

--- tools/scripts/k8s-dump-cluster.sh
+++ tools/scripts/k8s-dump-cluster.sh
@@ -283,17 +283,26 @@ function analyze_dump() {
       cd $SCRIPT_DIR/../vz
       # To enable debug, add  -zap-log-level debug
       if [ -z $REPORT_FILE ]; then
-        $GO_REPO_PATH/vz analyze --capture-dir $FULL_PATH_CAPTURE_DIR || true
-#        GO111MODULE=on GOPRIVATE=github.com/verrazzano go run main.go analyze --capture-dir $FULL_PATH_CAPTURE_DIR || true
+        if [[ -x $GOPATH/bin/vz ]]; then
+          $GOPATH/vz analyze --capture-dir $FULL_PATH_CAPTURE_DIR || true
+        else
+          GO111MODULE=on GOPRIVATE=github.com/verrazzano go run main.go analyze --capture-dir $FULL_PATH_CAPTURE_DIR || true
+        fi
       else
         # Since we have to change the current working directory to run go, we need to take into account if the reportFile specified was relative to the original
         # working directory. If it was absolute then we just use it directly
         if [[ $REPORT_FILE = /* ]]; then
-          ${GO_REPO_PATH}/vz analyze --capture-dir $FULL_PATH_CAPTURE_DIR --report-file $REPORT_FILE || true
-#          GO111MODULE=on GOPRIVATE=github.com/verrazzano go run main.go analyze --capture-dir $FULL_PATH_CAPTURE_DIR --report-file $REPORT_FILE || true
+          if [[ -x $GOPATH/bin/vz ]]; then
+            $GOPATH/vz analyze --capture-dir $FULL_PATH_CAPTURE_DIR --report-file $REPORT_FILE || true
+          else
+            GO111MODULE=on GOPRIVATE=github.com/verrazzano go run main.go analyze --capture-dir $FULL_PATH_CAPTURE_DIR --report-file $REPORT_FILE || true
+          fi
         else
-          ${GO_REPO_PATH}/vz analyze --capture-dir $FULL_PATH_CAPTURE_DIR --report-file $SAVE_DIR/$REPORT_FILE || true
-#          GO111MODULE=on GOPRIVATE=github.com/verrazzano go run main.go analyze --capture-dir $FULL_PATH_CAPTURE_DIR --report-file $SAVE_DIR/$REPORT_FILE || true
+          if [[ -x $GOPATH/bin/vz ]]; then
+            $GOPATH/vz analyze --capture-dir $FULL_PATH_CAPTURE_DIR --report-file $SAVE_DIR/$REPORT_FILE || true
+          else
+            GO111MODULE=on GOPRIVATE=github.com/verrazzano go run main.go analyze --capture-dir $FULL_PATH_CAPTURE_DIR --report-file $SAVE_DIR/$REPORT_FILE || true
+          fi
         fi
       fi
       cd $SAVE_DIR
