6b0bf7282ccdb44450663c9ff8a0fe766bb89d70
adiforluls <aditya.b.bharadwaj@oracle.com>
2022-08-12 10:42:05 +0530

    Squashed commit of the following:
    commit e01430d948cb8ff5cdeca4c665d8f5a476c0e657
    Author: adiforluls <aditya.b.bharadwaj@oracle.com>
    Date:   Fri Aug 12 10:41:11 2022 +0530
    
        a
    
    commit 8e7c541a5f1e0c541a56eaaa00d74d8b9a1b2843
    Author: adiforluls <aditya.b.bharadwaj@oracle.com>
    Date:   Fri Aug 12 10:40:52 2022 +0530
    
        hhh
    
    commit 6b797320d0b8de15efc4ea3e57a15b229cdf19bf
    Author: adiforluls <aditya.b.bharadwaj@oracle.com>
    Date:   Fri Aug 12 10:40:06 2022 +0530
    
        test

--- platform-operator/apis/verrazzano/v1alpha1/verrazzano_webhook.go
+++ platform-operator/apis/verrazzano/v1alpha1/verrazzano_webhook.go
@@ -1,5 +1,4 @@
-// Copyright (c) 2020, 2022, Oracle and/or its affiliates.
-// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+// hhhhhh aaaaa
 
 package v1alpha1
 
