6d93c41843ec356257eddbe5319cab060e0c8fb6
Saidu Dosapati <saidu.babu.dosapati@oracle.com>
2022-08-16 13:51:57 -0400

    UTs fix

--- tools/vz/cmd/analyze/analyze.go
+++ tools/vz/cmd/analyze/analyze.go
@@ -42,7 +42,6 @@ func NewCmdAnalyze(vzHelper helpers.VZHelper) *cobra.Command {
 	cmd.PersistentFlags().String(constants.DirectoryFlagName, constants.DirectoryFlagValue, constants.DirectoryFlagUsage)
 	cmd.PersistentFlags().String(constants.ReportFileFlagName, constants.ReportFileFlagValue, constants.ReportFileFlagUsage)
 	cmd.PersistentFlags().String(constants.ReportFormatFlagName, constants.SummaryReport, constants.ReportFormatFlagUsage)
-	cmd.PersistentFlags().String("help", "", "custom help")
 	cmd.PersistentFlags().BoolP(constants.VerboseFlag, constants.VerboseFlagShorthand, constants.VerboseFlagDefault, constants.VerboseFlagUsage)
 	return cmd
 }
