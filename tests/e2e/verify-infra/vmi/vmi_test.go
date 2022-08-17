6b79bc888c21082713cb5d4f3e434e70cb668183
Michael Gianatassio <michael.gianatassio@oracle.com>
2022-08-17 15:36:20 -0400

    adjust vmi test, add more cluster snapshots

--- tests/e2e/verify-infra/vmi/vmi_test.go
+++ tests/e2e/verify-infra/vmi/vmi_test.go
@@ -349,7 +349,7 @@ var _ = t.Describe("VMI", Label("f:infra-lcm"), func() {
 					assertPersistentVolume("vmi-system-grafana", size)
 					assertPersistentVolume(esMaster0, size)
 
-					Expect(len(vzMonitoringVolumeClaims)).To(Equal(expectedPromReplicas))
+					Expect(len(vzMonitoringVolumeClaims)).To(Equal(int(expectedPromReplicas)))
 					assertPrometheusVolume(size)
 				} else {
 					Expect(len(volumeClaims)).To(Equal(3))
@@ -365,7 +365,7 @@ var _ = t.Describe("VMI", Label("f:infra-lcm"), func() {
 		t.It("Check persistent volumes for managed cluster profile", func() {
 			if minVer14 {
 				Expect(len(volumeClaims)).To(Equal(0))
-				Expect(len(vzMonitoringVolumeClaims)).To(Equal(expectedPromReplicas))
+				Expect(len(vzMonitoringVolumeClaims)).To(Equal(int(expectedPromReplicas)))
 				assertPrometheusVolume(size)
 			} else {
 				Expect(len(volumeClaims)).To(Equal(1))
@@ -376,7 +376,7 @@ var _ = t.Describe("VMI", Label("f:infra-lcm"), func() {
 		t.It("Check persistent volumes for prod cluster profile", func() {
 			if minVer14 {
 				Expect(len(volumeClaims)).To(Equal(7))
-				Expect(len(vzMonitoringVolumeClaims)).To(Equal(expectedPromReplicas))
+				Expect(len(vzMonitoringVolumeClaims)).To(Equal(int(expectedPromReplicas)))
 				assertPrometheusVolume(size)
 			} else {
 				Expect(len(volumeClaims)).To(Equal(8))
