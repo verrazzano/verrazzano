bd0bd3131f2009abc607bbf1448b1d59906574cf
ben.mclean <ben.mclean@oracle.com>
2022-08-19 14:59:59 -0400

    don't update keycloak

--- platform-operator/controllers/verrazzano/component/keycloak/keycloak.go
+++ platform-operator/controllers/verrazzano/component/keycloak/keycloak.go
@@ -1251,9 +1251,9 @@ func getKeycloakClients(ctx spi.ComponentContext) (KeycloakClients, error) {
 		return nil, err
 	}
 	// Get the Client ID JSON array
-	out, stderr, err := k8sutil.ExecPod(cli, cfg, keycloakPod(), ComponentName, bashCMD("/opt/jboss/keycloak/bin/kcadm.sh get clients -r "+vzSysRealm+" --fields id,clientId"))
+	out, _, err := k8sutil.ExecPod(cli, cfg, keycloakPod(), ComponentName, bashCMD("/opt/jboss/keycloak/bin/kcadm.sh get clients -r "+vzSysRealm+" --fields id,clientId"))
 	if err != nil {
-		ctx.Log().Errorf("Component Keycloak failed retrieving clients: %s", stderr)
+		ctx.Log().Errorf("Component Keycloak failed retrieving clients: %s", err)
 		return nil, err
 	}
 	if len(out) == 0 {
