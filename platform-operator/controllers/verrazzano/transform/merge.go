62086480c60ac355ad8ddf43992df6b1bb4a628a
Anand Francis Joseph <anand.francis.joseph.augustin@oracle.com>
2022-08-22 11:57:25 +0530

    Added new GetEffectiveCR method for v1beta1
    Signed-off-by: Anand Francis Joseph <anand.francis.joseph.augustin@oracle.com>

--- platform-operator/controllers/verrazzano/transform/merge.go
+++ platform-operator/controllers/verrazzano/transform/merge.go
@@ -4,21 +4,62 @@
 package transform
 
 import (
-	"github.com/verrazzano/verrazzano/pkg/constants"
 	"os"
 	"strings"
+
+	"github.com/verrazzano/verrazzano/pkg/constants"
 
 	"github.com/verrazzano/verrazzano/platform-operator/internal/config"
 	"sigs.k8s.io/yaml"
 
 	vzyaml "github.com/verrazzano/verrazzano/pkg/yaml"
 	vzapi "github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1alpha1"
+	vzv1beta1 "github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1beta1"
 )
 
 const (
 	// implicit base profile (defaults)
 	baseProfile = "base"
 )
+
+// MergeProfiles merges a list of Verrazzano profile files with an existing Verrazzano CR.
+// The profiles must be in the Verrazzano CR format
+func MergeProfilesForV1beta1(actualCR *vzv1beta1.Verrazzano, profileFiles ...string) (*vzv1beta1.Verrazzano, error) {
+	// First merge the profiles
+	profileStrings, err := appendProfileComponentOverrides(profileFiles...)
+	if err != nil {
+		return nil, err
+	}
+	merged, err := vzyaml.StrategicMerge(vzv1beta1.Verrazzano{}, profileStrings...)
+	if err != nil {
+		return nil, err
+	}
+
+	profileVerrazzano := &vzv1beta1.Verrazzano{}
+	if err := yaml.Unmarshal([]byte(merged), profileVerrazzano); err != nil {
+		return nil, err
+	}
+	cr := actualCR.DeepCopy()
+	appendComponentOverridesV1beta1(cr, profileVerrazzano)
+
+	// Now merge the the profiles on top of the Verrazzano CR
+	crYAML, err := yaml.Marshal(cr)
+	if err != nil {
+		return nil, err
+	}
+
+	// merge all profiles together into a single yaml
+	merged, err = vzyaml.StrategicMerge(vzv1beta1.Verrazzano{}, merged, string(crYAML))
+	if err != nil {
+		return nil, err
+	}
+
+	// Return a new CR
+	var newCR vzv1beta1.Verrazzano
+	yaml.Unmarshal([]byte(merged), &newCR)
+
+	return &newCR, nil
+}
 
 // MergeProfiles merges a list of Verrazzano profile files with an existing Verrazzano CR.
 // The profiles must be in the Verrazzano CR format
@@ -86,6 +127,39 @@ func appendProfileComponentOverrides(profileFiles ...string) ([]string, error) {
 	}
 	profileStrings = append(profileStrings, string(data))
 	return profileStrings, nil
+}
+
+// GetEffectiveCR Creates an "effective" Verrazzano CR based on the user defined resource merged with the profile definitions
+// - Effective CR == base profile + declared profiles + ActualCR (in order)
+// - last definition wins
+func GetEffectiveV1beta1CR(actualCR *vzv1beta1.Verrazzano) (*vzv1beta1.Verrazzano, error) {
+	if actualCR == nil {
+		return nil, nil
+	}
+	// Identify the set of profiles, base + declared
+	profiles := []string{baseProfile, string(vzapi.Prod)}
+	if len(actualCR.Spec.Profile) > 0 {
+		profiles = append([]string{baseProfile}, strings.Split(string(actualCR.Spec.Profile), ",")...)
+	}
+	var profileFiles []string
+	for _, profile := range profiles {
+		profileFiles = append(profileFiles, config.GetProfile(profile))
+	}
+	// Merge the profile files into an effective profile YAML string
+	effectiveCR, err := MergeProfilesForV1beta1(actualCR, profileFiles...)
+	if err != nil {
+		return nil, err
+	}
+	effectiveCR.Status = vzv1beta1.VerrazzanoStatus{} // Don't replicate the CR status in the effective config
+	// if Certificate in CertManager is empty, set it to default CA
+	var emptyCertConfig = vzv1beta1.Certificate{}
+	if effectiveCR.Spec.Components.CertManager.Certificate == emptyCertConfig {
+		effectiveCR.Spec.Components.CertManager.Certificate.CA = vzv1beta1.CA{
+			SecretName:               constants.DefaultVerrazzanoCASecretName,
+			ClusterResourceNamespace: constants.CertManagerNamespace,
+		}
+	}
+	return effectiveCR, nil
 }
 
 // GetEffectiveCR Creates an "effective" Verrazzano CR based on the user defined resource merged with the profile definitions
@@ -257,6 +331,148 @@ func appendComponentOverrides(actual, profile *vzapi.Verrazzano) {
 	if actualVelero != nil && profileVelero != nil {
 		actualVelero.ValueOverrides = mergeOverrides(actualVelero.ValueOverrides, profileVelero.ValueOverrides)
 	}
+}
+
+//appendComponentOverrides copies the profile overrides over to the actual overrides. Any component that has overrides should be included here.
+// Because overrides lacks a proper merge key, a strategic merge will replace the array instead of merging it. This function stops that replacement from occurring.
+// The profile CR overrides must be appended to the actual CR overrides to preserve the precedence order in the way HelmComponent consumes them.
+func appendComponentOverridesV1beta1(actual, profile *vzv1beta1.Verrazzano) {
+	actualKubeStateMetrics := actual.Spec.Components.KubeStateMetrics
+	profileKubeStateMetrics := profile.Spec.Components.KubeStateMetrics
+	if actualKubeStateMetrics != nil && profileKubeStateMetrics != nil {
+		actualKubeStateMetrics.ValueOverrides = mergeOverridesV1beta1(actualKubeStateMetrics.ValueOverrides, profileKubeStateMetrics.ValueOverrides)
+	}
+
+	actualPrometheusAdapter := actual.Spec.Components.PrometheusAdapter
+	profilePrometheusAdapter := profile.Spec.Components.PrometheusAdapter
+	if actualPrometheusAdapter != nil && profilePrometheusAdapter != nil {
+		actualPrometheusAdapter.ValueOverrides = mergeOverridesV1beta1(actualPrometheusAdapter.ValueOverrides, profilePrometheusAdapter.ValueOverrides)
+	}
+
+	actualPrometheusNodeExporter := actual.Spec.Components.PrometheusNodeExporter
+	profilePrometheusNodeExporter := profile.Spec.Components.PrometheusNodeExporter
+	if actualPrometheusNodeExporter != nil && profilePrometheusNodeExporter != nil {
+		actualPrometheusNodeExporter.ValueOverrides = mergeOverridesV1beta1(actualPrometheusNodeExporter.ValueOverrides, profilePrometheusNodeExporter.ValueOverrides)
+	}
+
+	actualPrometheusOperator := actual.Spec.Components.PrometheusOperator
+	profilePrometheusOperator := profile.Spec.Components.PrometheusOperator
+	if actualPrometheusOperator != nil && profilePrometheusOperator != nil {
+		actualPrometheusOperator.ValueOverrides = mergeOverridesV1beta1(actualPrometheusOperator.ValueOverrides, profilePrometheusOperator.ValueOverrides)
+	}
+
+	actualPrometheusPushgateway := actual.Spec.Components.PrometheusPushgateway
+	profilePrometheusPushgateway := profile.Spec.Components.PrometheusPushgateway
+	if actualPrometheusPushgateway != nil && profilePrometheusPushgateway != nil {
+		actualPrometheusPushgateway.ValueOverrides = mergeOverridesV1beta1(actualPrometheusPushgateway.ValueOverrides, profilePrometheusPushgateway.ValueOverrides)
+	}
+
+	actualCertManager := actual.Spec.Components.CertManager
+	profileCertManager := profile.Spec.Components.CertManager
+	if actualCertManager != nil && profileCertManager != nil {
+		actualCertManager.ValueOverrides = mergeOverridesV1beta1(actualCertManager.ValueOverrides, profileCertManager.ValueOverrides)
+	}
+
+	actualCoherenceOperator := actual.Spec.Components.CoherenceOperator
+	profileCoherenceOperator := profile.Spec.Components.CoherenceOperator
+	if actualCoherenceOperator != nil && profileCoherenceOperator != nil {
+		actualCoherenceOperator.ValueOverrides = mergeOverridesV1beta1(actualCoherenceOperator.ValueOverrides, profileCoherenceOperator.ValueOverrides)
+	}
+
+	actualApplicationOperator := actual.Spec.Components.ApplicationOperator
+	profileApplicationOperator := profile.Spec.Components.ApplicationOperator
+	if actualApplicationOperator != nil && profileApplicationOperator != nil {
+		actualApplicationOperator.ValueOverrides = mergeOverridesV1beta1(actualApplicationOperator.ValueOverrides, profileApplicationOperator.ValueOverrides)
+	}
+
+	actualAuthProxy := actual.Spec.Components.AuthProxy
+	profileAuthProxy := profile.Spec.Components.AuthProxy
+	if actualAuthProxy != nil && profileAuthProxy != nil {
+		actualAuthProxy.ValueOverrides = mergeOverridesV1beta1(actualAuthProxy.ValueOverrides, profileAuthProxy.ValueOverrides)
+	}
+
+	actualOAM := actual.Spec.Components.OAM
+	profileOAM := profile.Spec.Components.OAM
+	if actualOAM != nil && profileOAM != nil {
+		actualOAM.ValueOverrides = mergeOverridesV1beta1(actualOAM.ValueOverrides, profileOAM.ValueOverrides)
+	}
+
+	actualVerrazzano := actual.Spec.Components.Verrazzano
+	profileVerrazzano := profile.Spec.Components.Verrazzano
+	if actualVerrazzano != nil && profileVerrazzano != nil {
+		actualVerrazzano.ValueOverrides = mergeOverridesV1beta1(actualVerrazzano.ValueOverrides, profileVerrazzano.ValueOverrides)
+	}
+
+	actualKiali := actual.Spec.Components.Kiali
+	profileKiali := profile.Spec.Components.Kiali
+	if actualKiali != nil && profileKiali != nil {
+		actualKiali.ValueOverrides = mergeOverridesV1beta1(actualKiali.ValueOverrides, profileKiali.ValueOverrides)
+	}
+
+	actualConsole := actual.Spec.Components.Console
+	profileConsole := profile.Spec.Components.Console
+	if actualConsole != nil && profileConsole != nil {
+		actualConsole.ValueOverrides = mergeOverridesV1beta1(actualConsole.ValueOverrides, profileConsole.ValueOverrides)
+	}
+
+	actualDNS := actual.Spec.Components.DNS
+	profileDNS := profile.Spec.Components.DNS
+	if actualDNS != nil && profileDNS != nil {
+		actualDNS.ValueOverrides = mergeOverridesV1beta1(actualDNS.ValueOverrides, profileDNS.ValueOverrides)
+	}
+
+	actualIngress := actual.Spec.Components.Ingress
+	profileIngress := profile.Spec.Components.Ingress
+	if actualIngress != nil && profileIngress != nil {
+		actualIngress.ValueOverrides = mergeOverridesV1beta1(actualIngress.ValueOverrides, profileIngress.ValueOverrides)
+	}
+
+	actualIstio := actual.Spec.Components.Istio
+	profileIstio := profile.Spec.Components.Istio
+	if actualIstio != nil && profileIstio != nil {
+		actualIstio.ValueOverrides = mergeOverridesV1beta1(actualIstio.ValueOverrides, profileIstio.ValueOverrides)
+	}
+
+	actualJaegerOperator := actual.Spec.Components.JaegerOperator
+	profileJaegerOperator := profile.Spec.Components.JaegerOperator
+	if actualJaegerOperator != nil && profileJaegerOperator != nil {
+		actualJaegerOperator.ValueOverrides = mergeOverridesV1beta1(actualJaegerOperator.ValueOverrides, profileJaegerOperator.ValueOverrides)
+	}
+
+	actualKeycloak := actual.Spec.Components.Keycloak
+	profileKeycloak := profile.Spec.Components.Keycloak
+	if actualKeycloak != nil && profileKeycloak != nil {
+		actualKeycloak.ValueOverrides = mergeOverridesV1beta1(actualKeycloak.ValueOverrides, profileKeycloak.ValueOverrides)
+		actualKeycloak.MySQL.ValueOverrides = mergeOverridesV1beta1(actualKeycloak.MySQL.ValueOverrides, profileKeycloak.MySQL.ValueOverrides)
+	}
+
+	actualRancher := actual.Spec.Components.Rancher
+	profileRancher := profile.Spec.Components.Rancher
+	if actualRancher != nil && profileRancher != nil {
+		actualRancher.ValueOverrides = mergeOverridesV1beta1(actualRancher.ValueOverrides, profileRancher.ValueOverrides)
+	}
+
+	actualFluentd := actual.Spec.Components.Fluentd
+	profileFluentd := profile.Spec.Components.Fluentd
+	if actualFluentd != nil && profileFluentd != nil {
+		actualFluentd.ValueOverrides = mergeOverridesV1beta1(actualFluentd.ValueOverrides, profileFluentd.ValueOverrides)
+	}
+
+	actualWebLogicOperator := actual.Spec.Components.WebLogicOperator
+	profileWebLogicOperator := profile.Spec.Components.WebLogicOperator
+	if actualWebLogicOperator != nil && profileWebLogicOperator != nil {
+		actualWebLogicOperator.ValueOverrides = mergeOverridesV1beta1(actualWebLogicOperator.ValueOverrides, profileWebLogicOperator.ValueOverrides)
+	}
+
+	actualVelero := actual.Spec.Components.Velero
+	profileVelero := profile.Spec.Components.Velero
+	if actualVelero != nil && profileVelero != nil {
+		actualVelero.ValueOverrides = mergeOverridesV1beta1(actualVelero.ValueOverrides, profileVelero.ValueOverrides)
+	}
+}
+
+func mergeOverridesV1beta1(actual, profile []vzv1beta1.Overrides) []vzv1beta1.Overrides {
+	return append(actual, profile...)
 }
 
 func mergeOverrides(actual, profile []vzapi.Overrides) []vzapi.Overrides {
