1358cf9310311e895290bcc282b3cac7fee3e406
Anders Swanson <anders.swanson@oracle.com>
2022-08-23 16:19:02 -0700

    fixup tests

--- platform-operator/internal/vzconfig/enabled_test.go
+++ platform-operator/internal/vzconfig/enabled_test.go
@@ -3,11 +3,18 @@
 package vzconfig
 
 import (
+	installv1beta1 "github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1beta1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"testing"
 
 	vzapi "github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1alpha1"
 
 	"github.com/stretchr/testify/assert"
+)
+
+var (
+	enabled  = true
+	disabled = false
 )
 
 // TestIsExternalDNSEnabledDefault tests the IsExternalDNSEnabled function
@@ -514,4 +521,209 @@ func TestIsVeleroEnabled(t *testing.T) {
 				},
 			},
 		}}))
+}
+
+func TestIsComponentEnabled(t *testing.T) {
+	var tests = []struct {
+		name      string
+		cr        runtime.Object
+		enabled   bool
+		isEnabled func(object runtime.Object) bool
+	}{
+		// WKO
+		{
+			"wko enabled when empty v1alpha1 CR",
+			&vzapi.Verrazzano{},
+			true,
+			IsWebLogicOperatorEnabled,
+		},
+		{
+			"wko enabled when empty v1beta1 CR",
+			&installv1beta1.Verrazzano{},
+			true,
+			IsWebLogicOperatorEnabled,
+		},
+		{
+			"wko enabled when component enabled, v1alpha1 CR",
+			&vzapi.Verrazzano{Spec: vzapi.VerrazzanoSpec{Components: vzapi.ComponentSpec{WebLogicOperator: &vzapi.WebLogicOperatorComponent{Enabled: &enabled}}}},
+			true,
+			IsWebLogicOperatorEnabled,
+		},
+		{
+			"wko enabled when component enabled, v1beta1 CR",
+			&installv1beta1.Verrazzano{Spec: installv1beta1.VerrazzanoSpec{Components: installv1beta1.ComponentSpec{WebLogicOperator: &installv1beta1.WebLogicOperatorComponent{Enabled: &enabled}}}},
+			true,
+			IsWebLogicOperatorEnabled,
+		},
+		{
+			"wko disabled when component disabled, v1alpha1 CR",
+			&vzapi.Verrazzano{Spec: vzapi.VerrazzanoSpec{Components: vzapi.ComponentSpec{WebLogicOperator: &vzapi.WebLogicOperatorComponent{Enabled: &disabled}}}},
+			false,
+			IsWebLogicOperatorEnabled,
+		},
+		{
+			"wko disabled when component disabled, v1beta1 CR",
+			&installv1beta1.Verrazzano{Spec: installv1beta1.VerrazzanoSpec{Components: installv1beta1.ComponentSpec{WebLogicOperator: &installv1beta1.WebLogicOperatorComponent{Enabled: &disabled}}}},
+			false,
+			IsWebLogicOperatorEnabled,
+		},
+
+		// COH
+		{
+			"coh enabled when empty v1alpha1 CR",
+			&vzapi.Verrazzano{},
+			true,
+			IsCoherenceOperatorEnabled,
+		},
+		{
+			"coh enabled when empty v1beta1 CR",
+			&installv1beta1.Verrazzano{},
+			true,
+			IsCoherenceOperatorEnabled,
+		},
+		{
+			"coh enabled when component enabled, v1alpha1 CR",
+			&vzapi.Verrazzano{Spec: vzapi.VerrazzanoSpec{Components: vzapi.ComponentSpec{CoherenceOperator: &vzapi.CoherenceOperatorComponent{Enabled: &enabled}}}},
+			true,
+			IsCoherenceOperatorEnabled,
+		},
+		{
+			"coh enabled when component enabled, v1beta1 CR",
+			&installv1beta1.Verrazzano{Spec: installv1beta1.VerrazzanoSpec{Components: installv1beta1.ComponentSpec{CoherenceOperator: &installv1beta1.CoherenceOperatorComponent{Enabled: &enabled}}}},
+			true,
+			IsCoherenceOperatorEnabled,
+		},
+		{
+			"coh disabled when component disabled, v1alpha1 CR",
+			&vzapi.Verrazzano{Spec: vzapi.VerrazzanoSpec{Components: vzapi.ComponentSpec{CoherenceOperator: &vzapi.CoherenceOperatorComponent{Enabled: &disabled}}}},
+			false,
+			IsCoherenceOperatorEnabled,
+		},
+		{
+			"coh disabled when component disabled, v1beta1 CR",
+			&installv1beta1.Verrazzano{Spec: installv1beta1.VerrazzanoSpec{Components: installv1beta1.ComponentSpec{CoherenceOperator: &installv1beta1.CoherenceOperatorComponent{Enabled: &disabled}}}},
+			false,
+			IsCoherenceOperatorEnabled,
+		},
+
+		// Verrazzano Component
+		{
+			"vz enabled when empty v1alpha1 CR",
+			&vzapi.Verrazzano{},
+			true,
+			IsVerrazzanoComponentEnabled,
+		},
+		{
+			"vz enabled when empty v1beta1 CR",
+			&installv1beta1.Verrazzano{},
+			true,
+			IsVerrazzanoComponentEnabled,
+		},
+		{
+			"vz enabled when component enabled, v1alpha1 CR",
+			&vzapi.Verrazzano{Spec: vzapi.VerrazzanoSpec{Components: vzapi.ComponentSpec{Verrazzano: &vzapi.VerrazzanoComponent{Enabled: &enabled}}}},
+			true,
+			IsVerrazzanoComponentEnabled,
+		},
+		{
+			"vz enabled when component enabled, v1beta1 CR",
+			&installv1beta1.Verrazzano{Spec: installv1beta1.VerrazzanoSpec{Components: installv1beta1.ComponentSpec{Verrazzano: &installv1beta1.VerrazzanoComponent{Enabled: &enabled}}}},
+			true,
+			IsVerrazzanoComponentEnabled,
+		},
+		{
+			"vz disabled when component disabled, v1alpha1 CR",
+			&vzapi.Verrazzano{Spec: vzapi.VerrazzanoSpec{Components: vzapi.ComponentSpec{Verrazzano: &vzapi.VerrazzanoComponent{Enabled: &disabled}}}},
+			false,
+			IsVerrazzanoComponentEnabled,
+		},
+		{
+			"vz disabled when component disabled, v1beta1 CR",
+			&installv1beta1.Verrazzano{Spec: installv1beta1.VerrazzanoSpec{Components: installv1beta1.ComponentSpec{Verrazzano: &installv1beta1.VerrazzanoComponent{Enabled: &disabled}}}},
+			false,
+			IsVerrazzanoComponentEnabled,
+		},
+
+		// OAM
+		{
+			"oam enabled when empty v1alpha1 CR",
+			&vzapi.Verrazzano{},
+			true,
+			IsCoherenceOperatorEnabled,
+		},
+		{
+			"oam enabled when empty v1beta1 CR",
+			&installv1beta1.Verrazzano{},
+			true,
+			IsOAMEnabled,
+		},
+		{
+			"oam enabled when component enabled, v1alpha1 CR",
+			&vzapi.Verrazzano{Spec: vzapi.VerrazzanoSpec{Components: vzapi.ComponentSpec{OAM: &vzapi.OAMComponent{Enabled: &enabled}}}},
+			true,
+			IsOAMEnabled,
+		},
+		{
+			"oam enabled when component enabled, v1beta1 CR",
+			&installv1beta1.Verrazzano{Spec: installv1beta1.VerrazzanoSpec{Components: installv1beta1.ComponentSpec{OAM: &installv1beta1.OAMComponent{Enabled: &enabled}}}},
+			true,
+			IsOAMEnabled,
+		},
+		{
+			"oam disabled when component disabled, v1alpha1 CR",
+			&vzapi.Verrazzano{Spec: vzapi.VerrazzanoSpec{Components: vzapi.ComponentSpec{OAM: &vzapi.OAMComponent{Enabled: &disabled}}}},
+			false,
+			IsOAMEnabled,
+		},
+		{
+			"oam disabled when component disabled, v1beta1 CR",
+			&installv1beta1.Verrazzano{Spec: installv1beta1.VerrazzanoSpec{Components: installv1beta1.ComponentSpec{OAM: &installv1beta1.OAMComponent{Enabled: &disabled}}}},
+			false,
+			IsOAMEnabled,
+		},
+
+		// MySQL Operator
+		{
+			"mysqlop enabled when empty v1alpha1 CR",
+			&vzapi.Verrazzano{},
+			true,
+			IsMySQLOperatorEnabled,
+		},
+		{
+			"mysqlop enabled when empty v1beta1 CR",
+			&installv1beta1.Verrazzano{},
+			true,
+			IsMySQLOperatorEnabled,
+		},
+		{
+			"mysqlop enabled when component enabled, v1alpha1 CR",
+			&vzapi.Verrazzano{Spec: vzapi.VerrazzanoSpec{Components: vzapi.ComponentSpec{MySQLOperator: &vzapi.MySQLOperatorComponent{Enabled: &enabled}}}},
+			true,
+			IsMySQLOperatorEnabled,
+		},
+		{
+			"mysqlop enabled when component enabled, v1beta1 CR",
+			&installv1beta1.Verrazzano{Spec: installv1beta1.VerrazzanoSpec{Components: installv1beta1.ComponentSpec{MySQLOperator: &installv1beta1.MySQLOperatorComponent{Enabled: &enabled}}}},
+			true,
+			IsMySQLOperatorEnabled,
+		},
+		{
+			"mysqlop disabled when component disabled, v1alpha1 CR",
+			&vzapi.Verrazzano{Spec: vzapi.VerrazzanoSpec{Components: vzapi.ComponentSpec{MySQLOperator: &vzapi.MySQLOperatorComponent{Enabled: &disabled}}}},
+			false,
+			IsMySQLOperatorEnabled,
+		},
+		{
+			"mysqlop disabled when component disabled, v1beta1 CR",
+			&installv1beta1.Verrazzano{Spec: installv1beta1.VerrazzanoSpec{Components: installv1beta1.ComponentSpec{MySQLOperator: &installv1beta1.MySQLOperatorComponent{Enabled: &disabled}}}},
+			false,
+			IsMySQLOperatorEnabled,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.enabled, tt.isEnabled(tt.cr))
+		})
+	}
 }
