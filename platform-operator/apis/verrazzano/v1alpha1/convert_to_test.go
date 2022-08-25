597f860653e9b6b0e20b50f334bdec9deb695116
abnadago <abijitha.nadagouda@oracle.com>
2022-08-25 12:51:01 -0700

    Fix tests and update base profile

--- platform-operator/apis/verrazzano/v1alpha1/convert_to_test.go
+++ platform-operator/apis/verrazzano/v1alpha1/convert_to_test.go
@@ -11,7 +11,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/json"
+	"sigs.k8s.io/yaml"
 	"sort"
 	"testing"
 )
@@ -438,8 +438,8 @@ func TestConvertToV1Beta1(t *testing.T) {
 					sortNodes(v1beta1Actual.Spec.Components.OpenSearch.Nodes)
 				}
 				// expected and actual v1beta1 CRs must be equal
-				v1a, _ := json.Marshal(v1beta1Expected.Spec)
-				v1b, _ := json.Marshal(v1beta1Actual.Spec)
+				v1a, _ := yaml.Marshal(v1beta1Expected.Spec)
+				v1b, _ := yaml.Marshal(v1beta1Actual.Spec)
 				fmt.Println("v1beta1Expected", string(v1a))
 				fmt.Println("v1beta1Actual", string(v1b))
 				assert.EqualValues(t, v1beta1Expected.ObjectMeta, v1beta1Actual.ObjectMeta)
