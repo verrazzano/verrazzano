53a2157827b418ccaf09262946b494366f670957
Robert Donat <robert.donat@oracle.com>
2022-08-26 19:51:47 +0000

    cleanup

--- ci/scripts/update_lre_kubeconfig.sh
+++ ci/scripts/update_lre_kubeconfig.sh
@@ -4,8 +4,6 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 #
 
-kubectl -n kube-system create serviceaccount kubeconfig-sa
-kubectl create clusterrolebinding add-on-cluster-admin --clusterrole=cluster-admin --serviceaccount=kube-system:kubeconfig-sa
 TOKENNAME=`kubectl -n kube-system get serviceaccount/kubeconfig-sa -o jsonpath='{.secrets[0].name}'`
 TOKEN=`kubectl -n kube-system get secret $TOKENNAME -o jsonpath='{.data.token}'| base64 --decode`
 kubectl config set-credentials kubeconfig-sa --token=$TOKEN
