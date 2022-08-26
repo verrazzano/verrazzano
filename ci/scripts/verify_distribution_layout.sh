35954efec7693f443a70dd038973bd9624bec233
Saidu Dosapati <saidu.babu.dosapati@oracle.com>
2022-08-26 13:35:57 -0400

    Added exit codes

--- ci/scripts/verify_distribution_layout.sh
+++ ci/scripts/verify_distribution_layout.sh
@@ -3,6 +3,7 @@
 # Copyright (c) 2022, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 #
+export SAVEDIR=$(pwd)
 export DIRECTORY="/Users/sdosapat/Downloads/pabhat%2Fvz-6801-last-clean-periodic-test_verrazzano-1.4.0-open-source/verrazzano-1.4.0-linux-amd64"
 cd ${DIRECTORY}
 
@@ -11,6 +12,7 @@ then
   echo 'License found'
 else
   echo 'ERROR: Missing LICENSE file'
+  exit 1
 fi
 
 cd ${DIRECTORY}/bin
@@ -19,6 +21,7 @@ then
   echo 'All files found under bin'
 else
   echo 'ERROR: Missing files for bin'
+  exit 1
 fi
 
 cd ${DIRECTORY}/manifests
@@ -27,6 +30,7 @@ then
   echo 'All files found under manifests'
 else
   echo 'ERROR: Missing files for manifests'
+  exit 1
 fi
 
 if [ -e "k8s/verrazzano-platform-operator.yaml" ]
@@ -34,6 +38,7 @@ then
   echo 'All files found under manifests/k8s'
 else
   echo 'ERROR: Missing files for manifests/k8s'
+  exit 1
 fi
 
 if [ -e "profiles/default.yaml" ] && [ -e "profiles/dev.yaml" ] && [ -e "profiles/managed-cluster.yaml" ] && [ -e "profiles/oci.yaml" ] && [ -e "profiles/ocne.yaml" ]
@@ -41,6 +46,7 @@ then
   echo 'All files found under manifests/profiles'
 else
   echo 'ERROR: Missing files for manifests/profiles'
+  exit 1
 fi
 
 cd ${DIRECTORY}/manifests/charts/verrazzano-platform-operator
@@ -49,6 +55,7 @@ then
   echo 'All files found under manifests/charts/verrazzano-platform-operator'
 else
   echo 'ERROR: Missing files for manifests/charts/verrazzano-platform-operator'
+  exit 1
 fi
 
 if [ -e "crds/clusters.verrazzano.io_verrazzanomanagedclusters.yaml" ] && [ -e "crds/install.verrazzano.io_verrazzanos.yaml" ]
@@ -56,6 +63,7 @@ then
   echo 'All files found under manifests/charts/verrazzano-platform-operator/crds'
 else
   echo 'ERROR: Missing files for manifests/charts/verrazzano-platform-operator/crds'
+  exit 1
 fi
 
 if [ -e "templates/clusterrole.yaml" ] && [ -e "templates/clusterrolebinding.yaml" ] && [ -e "templates/deployment.yaml" ] && [ -e "templates/namespace.yaml" ] && \
@@ -64,6 +72,10 @@ then
   echo 'All files found under manifests/charts/verrazzano-platform-operator/bin'
 else
   echo 'ERROR: Missing files for manifests/charts/verrazzano-platform-operator/bin'
+  exit 1
 fi
+
+# Chanding back to original directory
+cd ${SAVEDIR}
 
 
