1052e4ce9ddf63a097e89b404d4aa8e93667c593
Vicky Sarkar <vicky.sarkar@oracle.com>
2022-08-26 13:27:04 +0530

    VZ-6740 grafana add component changes into v1beta1 version (#3923)
    * grafana add component changes

--- platform-operator/apis/verrazzano/v1beta1/verrazzano_types.go
+++ platform-operator/apis/verrazzano/v1beta1/verrazzano_types.go
@@ -425,10 +425,18 @@ type KubeStateMetricsComponent struct {
 	InstallOverrides `json:",inline"`
 }
 
+// DatabaseInfo specifies the database host, name, and username/password secret for Grafana DB instance
+type DatabaseInfo struct {
+	Host string `json:"host,omitempty"`
+	Name string `json:"name,omitempty"`
+}
+
 // GrafanaComponent specifies the Grafana configuration.
 type GrafanaComponent struct {
 	// +optional
-	Enabled *bool `json:"enabled,omitempty"`
+	Enabled  *bool         `json:"enabled,omitempty"`
+	Replicas *int32        `json:"replicas,omitempty"`
+	Database *DatabaseInfo `json:"database,omitempty"`
 }
 
 // PrometheusComponent specifies the Prometheus configuration.
