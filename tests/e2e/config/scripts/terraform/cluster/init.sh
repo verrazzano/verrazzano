5a35b25e47c79d97c5f0cef60b8445b87d864e5a
tony.vlatas <tony.vlatas@oracle.com>
2022-08-29 16:20:06 -0400

    needs TF 1.0.0 min

--- tests/e2e/config/scripts/terraform/cluster/init.sh
+++ tests/e2e/config/scripts/terraform/cluster/init.sh
@@ -16,9 +16,9 @@ done
 #download terraform
 if [ ! -f $SCRIPT_DIR/terraform ];
 then
-  curl https://releases.hashicorp.com/terraform/0.14.10/terraform_0.14.10_$(uname -s | tr '[:upper:]' '[:lower:]')_amd64.zip -o $SCRIPT_DIR/terraform_0.14.10.zip
-  unzip $SCRIPT_DIR/terraform_0.14.10.zip -d $SCRIPT_DIR/
-  rm $SCRIPT_DIR/terraform_0.14.10.zip
+  curl https://releases.hashicorp.com/terraform/1.0.0/terraform_1.0.0_$(uname -s | tr '[:upper:]' '[:lower:]')_amd64.zip -o $SCRIPT_DIR/terraform_1.0.0.zip
+  unzip $SCRIPT_DIR/terraform_1.0.0.zip -d $SCRIPT_DIR/
+  rm $SCRIPT_DIR/terraform_1.0.0.zip
 fi
 
 if [ ! -f $SCRIPT_DIR/terraform ];
