22ccf91a6c489da7cb3a87dd146e66ee013de6ad
Kate Nelson <kate.s.nelson@oracle.com>
2022-08-29 15:28:30 +0000

    edits: impl, test files, singleton, scheme. todo: move testdata folder and change paths, validateOCIsecrets test failing, write more tests for validate go if needed

--- platform-operator/apis/verrazzano/v1alpha1/verrazzano_webhook.go
+++ platform-operator/apis/verrazzano/v1alpha1/verrazzano_webhook.go
@@ -5,9 +5,7 @@ package v1alpha1
 
 import (
 	"context"
-	goerrors "errors"
 	"fmt"
-	"github.com/onsi/gomega/gstruct/errors"
 	"github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/validators"
 	"github.com/verrazzano/verrazzano/platform-operator/constants"
 	"github.com/verrazzano/verrazzano/platform-operator/internal/config"
@@ -22,7 +20,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 )
 
-var getControllerRuntimeClient = getClient
+var getControllerRuntimeClient = validators.GetClient
 
 // SetupWebhookWithManager is used to let the controller manager know about the webhook
 func (v *Verrazzano) SetupWebhookWithManager(mgr ctrl.Manager, log *zap.SugaredLogger) error {
@@ -65,7 +63,7 @@ func (v *Verrazzano) ValidateCreate() error {
 		return err
 	}
 
-	client, err := getControllerRuntimeClient()
+	client, err := getControllerRuntimeClient(newScheme())
 	if err != nil {
 		return err
 	}
@@ -90,7 +88,7 @@ func (v *Verrazzano) ValidateCreate() error {
 	// hand the Verrazzano to component validator to validate
 	if componentValidator != nil {
 		if errs := componentValidator.ValidateInstall(v); len(errs) > 0 {
-			return combineErrors(errs)
+			return validators.CombineErrors(errs)
 		}
 	}
 	return nil
@@ -135,7 +133,7 @@ func (v *Verrazzano) ValidateUpdate(old runtime.Object) error {
 		return err
 	}
 
-	client, err := getControllerRuntimeClient()
+	client, err := getControllerRuntimeClient(newScheme())
 	if err != nil {
 		return err
 	}
@@ -146,7 +144,7 @@ func (v *Verrazzano) ValidateUpdate(old runtime.Object) error {
 	// hand the old and new Verrazzano to component validator to validate
 	if componentValidator != nil {
 		if errs := componentValidator.ValidateUpdate(oldResource, v); len(errs) > 0 {
-			return combineErrors(errs)
+			return validators.CombineErrors(errs)
 		}
 	}
 
@@ -158,7 +156,7 @@ func (v *Verrazzano) ValidateUpdate(old runtime.Object) error {
 // updates before terminating.  In the longer term we may want some kind of leader-election strategy to support
 // multiple instances, if that makes sense.
 func (v *Verrazzano) verifyPlatformOperatorSingleton() error {
-	runtimeClient, err := getControllerRuntimeClient()
+	runtimeClient, err := getControllerRuntimeClient(newScheme())
 	if err != nil {
 		return err
 	}
@@ -177,25 +175,6 @@ func (v *Verrazzano) ValidateDelete() error {
 
 	// Webhook is not configured for deletes so function will not be called.
 	return nil
-}
-
-// combineErrors combines multiple errors into one error, nil if no error
-func combineErrors(errs []error) error {
-	if len(errs) > 0 {
-		return goerrors.New(errors.AggregateError(errs).Error())
-	}
-	return nil
-}
-
-// getClient returns a controller runtime client for the Verrazzano resource
-func getClient() (client.Client, error) {
-
-	config, err := ctrl.GetConfig()
-	if err != nil {
-		return nil, err
-	}
-
-	return client.New(config, client.Options{Scheme: newScheme()})
 }
 
 // newScheme creates a new scheme that includes this package's object for use by client
--- platform-operator/apis/verrazzano/v1beta1/validate.go
+++ platform-operator/apis/verrazzano/v1beta1/validate.go
@@ -1,3 +1,6 @@
+// Copyright (c) 2022, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
 package v1beta1
 
 import (
@@ -68,5 +71,29 @@ func validateOCISecrets(client client.Client, spec *VerrazzanoSpec) error {
 	}
 
 	//spec does not have ocidns secret nor fluentd oci auth secret
+	return nil
+}
+
+// ValidateInstallOverrides checks that the overrides slice has only one override type per slice item
+func ValidateInstallOverrides(Overrides []Overrides) error {
+	overridePerItem := 0
+	for _, override := range Overrides {
+		if override.ConfigMapRef != nil {
+			overridePerItem++
+		}
+		if override.SecretRef != nil {
+			overridePerItem++
+		}
+		if override.Values != nil {
+			overridePerItem++
+		}
+		if overridePerItem > 1 {
+			return fmt.Errorf("Invalid install overrides. Cannot specify more than one override type in the same list element")
+		}
+		if overridePerItem == 0 {
+			return fmt.Errorf("Invalid install overrides. No override specified")
+		}
+		overridePerItem = 0
+	}
 	return nil
 }
