b52feb5af0c3739f9a08ce0dbc038951338ec8c3
tony.vlatas <tony.vlatas@oracle.com>
2022-08-30 13:21:11 -0400

    add home provider

--- tests/e2e/config/scripts/terraform/cluster/provider.tf
+++ tests/e2e/config/scripts/terraform/cluster/provider.tf
@@ -15,6 +15,15 @@ provider "oci" {
   region               = var.region
 }
 
+provider "oci" {
+  tenancy_ocid         = var.tenancy_id
+  user_ocid            = var.user_id
+  fingerprint          = var.api_fingerprint
+  private_key_path     = var.api_private_key_path
+  region               = "us-phoenix-1"
+  alias                = "home"
+}
+
 terraform {
   required_version = ">= 1.0.0"
 }
