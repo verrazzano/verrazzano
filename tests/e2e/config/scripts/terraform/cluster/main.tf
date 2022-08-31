84e209e9ea0350b80dd31e3cbc3c1c0074fbd649
tony.vlatas <tony.vlatas@oracle.com>
2022-08-31 14:18:01 -0400

    Move to new variable name refactorings

--- tests/e2e/config/scripts/terraform/cluster/main.tf
+++ tests/e2e/config/scripts/terraform/cluster/main.tf
@@ -7,6 +7,7 @@ module "oke" {
 
   tenancy_id = var.tenancy_id
   user_id = var.user_id
+  home_region = "us-phoenix-1"
   region = var.region
   api_fingerprint = var.api_fingerprint
   api_private_key_path =var.api_private_key_path
@@ -15,32 +16,32 @@ module "oke" {
   compartment_id = var.compartment_id
   kubernetes_version = var.kubernetes_version
   allow_worker_ssh_access = var.allow_worker_ssh_access
-  worker_mode = var.worker_mode
-  cluster_access = var.cluster_access
+  worker_type = var.worker_mode
+  control_plane_access = var.cluster_access
   ssh_private_key_path = var.ssh_private_key_path
   ssh_public_key_path = var.ssh_public_key_path
   node_pools =var.node_pools
   allow_node_port_access = var.allow_node_port_access
-  operator_enabled = var.operator_enabled
-  bastion_enabled = var.bastion_enabled
   username = var.username
 
-  calico_enabled = var.calico_enabled
+  enable_calico = var.calico_enabled
   calico_version = var.calico_version
 
   vcn_name = "${var.cluster_name}-vcn"
   vcn_dns_label = var.cluster_name
   label_prefix = var.label_prefix
 
+  create_operator = var.operator_enabled
   operator_shape = { shape="VM.Standard.E3.Flex", ocpus=1, memory=4, boot_volume_size=50 }
   operator_notification_endpoint = ""
-  operator_instance_principal = false
+  enable_operator_instance_principal = false
   operator_notification_enabled = false
   operator_timezone = "UTC"
 
+  create_bastion_host = var.bastion_enabled
   bastion_shape = { shape="VM.Standard.E3.Flex", ocpus=1, memory=4, boot_volume_size=50 }
   bastion_timezone = "UTC"
-  bastion_notification_enabled = false
+  enable_bastion_notification = false
   bastion_notification_endpoint = ""
 
   email_address = ""
@@ -48,7 +49,7 @@ module "oke" {
   create_service_account = false
   service_account_cluster_role_binding = ""
 
-  existing_key_id = ""
+  kms_key_id = ""
 
   use_signed_images = false
   image_signing_keys = []
