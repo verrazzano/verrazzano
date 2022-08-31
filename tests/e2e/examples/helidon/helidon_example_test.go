c1fcccb54d515656b798fe89dd60dc5138fd9193
Ravi Singhal <ravi.r.singhal@oracle.com>
2022-08-31 13:07:55 +0530

    check for resp being null in helidon example test

--- tests/e2e/examples/helidon/helidon_example_test.go
+++ tests/e2e/examples/helidon/helidon_example_test.go
@@ -208,7 +208,7 @@ func appEndpointAccessible(url string, hostname string) bool {
 	resp, err := httpClient.Do(req)
 	if err != nil {
 		t.Logs.Errorf("Unexpected error=%v", err)
-		if resp.Body != nil {
+		if resp != nil && resp.Body != nil {
 			bodyRaw, err := ioutil.ReadAll(resp.Body)
 			if err != nil {
 				t.Logs.Errorf("Unexpected error while marshallling error response=%v", err)
