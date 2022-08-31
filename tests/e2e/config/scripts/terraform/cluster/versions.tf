293430fb16aab6f0c623f301cf4b471453d72f40
tony.vlatas <tony.vlatas@oracle.com>
2022-08-31 08:04:19 -0400

    add in versions.tf

---
+++ tests/e2e/config/scripts/terraform/cluster/versions.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_providers {
+    oci = {
+      source                = "oracle/oci"
+      configuration_aliases = [oci.home]
+      version               = ">= 4.67.3"
+    }
+  }
+  required_version = ">= 1.0.0"
+}
