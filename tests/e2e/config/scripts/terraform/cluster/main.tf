8cc166e0d07921923ad11d7e631f41f47a7d426e
tony.vlatas <tony.vlatas@oracle.com>
2022-08-31 16:00:05 -0400

    try addressing tag issue

--- tests/e2e/config/scripts/terraform/cluster/main.tf
+++ tests/e2e/config/scripts/terraform/cluster/main.tf
@@ -70,27 +70,25 @@ module "oke" {
         "verrazzano-infra/Pipeline" = var.pipeline_tag
         "verrazzano-infra/JobScenario" = var.job_scenario_tag
     }
-    oke = {
-      cluster = {
+    cluster = {
         "verrazzano-infra/Branch" = var.branch_tag
         "verrazzano-infra/Pipeline" = var.pipeline_tag
         "verrazzano-infra/JobScenario" = var.job_scenario_tag
-      }
-      service_lb = {
+    }
+    service_lb = {
         "verrazzano-infra/Branch" = var.branch_tag
         "verrazzano-infra/Pipeline" = var.pipeline_tag
         "verrazzano-infra/JobScenario" = var.job_scenario_tag
-      }
-      persistent_volume = {
+    }
+    persistent_volume = {
         "verrazzano-infra/Branch" = var.branch_tag
         "verrazzano-infra/Pipeline" = var.pipeline_tag
         "verrazzano-infra/JobScenario" = var.job_scenario_tag
-      }
-      node_pool = {
+    }
+    node_pool = {
         "verrazzano-infra/Branch" = var.branch_tag
         "verrazzano-infra/Pipeline" = var.pipeline_tag
         "verrazzano-infra/JobScenario" = var.job_scenario_tag
-      }
     }
   }
 
