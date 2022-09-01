96db90d24e4e56b52532f16f25284aa43b7e19eb
Anders Swanson <anders.swanson@oracle.com>
2022-09-01 09:51:40 -0700

    Fix copyright

--- tools/url_linter/invalid_url_linter.sh
+++ tools/url_linter/invalid_url_linter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (C) 2021, Oracle and/or its affiliates.
+# Copyright (C) 2021, 2022, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 #
 SCRIPT_DIR=$(cd $(dirname "$0"); pwd -P)
