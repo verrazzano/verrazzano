0ab158d48b833f8a143e1c6dcbacae09dd640d21
Jonathan Maron <jonathan.maron@oracle.com>
2022-09-01 10:14:17 -0400

    handle prod profile claim number

--- tests/e2e/verify-install/keycloak/keycloak_test.go
+++ tests/e2e/verify-install/keycloak/keycloak_test.go
@@ -177,7 +177,11 @@ var _ = t.Describe("Verify", Label("f:platform-lcm.install"), func() {
 		} else if pkg.IsProdProfile() {
 			t.It("Prod install profile", func() {
 				// 50 GB Persistent Volume create for MySQL in a prod install
-				Expect(len(volumeClaims)).To(Equal(1))
+				expectedClaims := 1
+				if ok, _ := pkg.IsVerrazzanoMinVersion("1.4.0", kubeconfigPath); ok {
+					expectedClaims = 3
+				}
+				Expect(len(volumeClaims)).To(Equal(expectedClaims))
 				assertPersistentVolume(claimName, size)
 			})
 		}
