8e0586a4ab5fb66c42f40d7d51b912031a1c42cb
Anders Swanson <anders.swanson@oracle.com>
2022-09-01 07:25:19 -0700

    opensearch dynamic config

--- platform-operator/controllers/verrazzano/transform/merge.go
+++ platform-operator/controllers/verrazzano/transform/merge.go
@@ -175,7 +175,7 @@ func GetEffectiveCR(actualCR *v1alpha1.Verrazzano) (*v1alpha1.Verrazzano, error)
 	}
 	var profileFiles []string
 	for _, profile := range profiles {
-		profileFiles = append(profileFiles, config.GetProfile(actualCR.GroupVersionKind().GroupVersion(), profile))
+		profileFiles = append(profileFiles, config.GetProfile(v1alpha1.SchemeGroupVersion, profile))
 	}
 	// Merge the profile files into an effective profile YAML string
 	effectiveCR, err := MergeProfiles(actualCR, profileFiles...)
@@ -208,7 +208,7 @@ func GetEffectiveV1beta1CR(actualCR *v1beta1.Verrazzano) (*v1beta1.Verrazzano, e
 	}
 	var profileFiles []string
 	for _, profile := range profiles {
-		profileFiles = append(profileFiles, config.GetProfile(actualCR.GroupVersionKind().GroupVersion(), profile))
+		profileFiles = append(profileFiles, config.GetProfile(v1beta1.SchemeGroupVersion, profile))
 	}
 	// Merge the profile files into an effective profile YAML string
 	effectiveCR, err := MergeProfilesForV1beta1(actualCR, profileFiles...)
