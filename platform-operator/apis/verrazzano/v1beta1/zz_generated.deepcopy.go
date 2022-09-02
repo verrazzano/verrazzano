4e3a68169a7f003c4698a11211c62908b82f9e37
Vicky Sarkar <vicky.sarkar@oracle.com>
2022-09-02 13:06:15 +0530

    VZ-6867 Spec Ingress Params (#4000)
    * rename ingress params

--- platform-operator/apis/verrazzano/v1beta1/zz_generated.deepcopy.go
+++ platform-operator/apis/verrazzano/v1beta1/zz_generated.deepcopy.go
@@ -201,8 +201,8 @@ func (in *ComponentSpec) DeepCopyInto(out *ComponentSpec) {
 		*out = new(GrafanaComponent)
 		(*in).DeepCopyInto(*out)
 	}
-	if in.Ingress != nil {
-		in, out := &in.Ingress, &out.Ingress
+	if in.IngressNGINX != nil {
+		in, out := &in.IngressNGINX, &out.IngressNGINX
 		*out = new(IngressNginxComponent)
 		(*in).DeepCopyInto(*out)
 	}
