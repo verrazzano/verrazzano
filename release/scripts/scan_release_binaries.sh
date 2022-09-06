f47f8b4faf1ff63970550e4ff50cf186048caad5
Padmanabha Bhat <padmanabha.bhat@oracle.com>
2022-09-06 15:10:06 +0530

    Initial change to update the release pipeline

--- release/scripts/scan_release_binaries.sh
+++ release/scripts/scan_release_binaries.sh
@@ -14,10 +14,10 @@ usage() {
   Performs malware scan on release binaries.
 
   Usage:
-    $(basename $0) <release branch> <directory where the release artifacts need to be downloaded, defaults to the current directory> <release version>
+    $(basename $0) <release branch> <directory where the release artifacts need to be downloaded> <flat to indicate whether to scan already downloaded bundle> <directory to place the scan report, which is optional>
 
   Example:
-    $(basename $0) release-1.0 . 1.0.2
+    $(basename $0) release-1.0 release_bundle_dir true
 
   The script expects the OCI CLI is installed. It also expects the following environment variables -
     RELEASE_VERSION - release version (major.minor.patch format, e.g. 1.0.1)
@@ -38,12 +38,31 @@ EOM
 
 . $SCRIPT_DIR/common.sh
 
-BRANCH=${1}
-WORK_DIR=${2:-$SCRIPT_DIR}
+if [ -z "$1" ]; then
+  echo "Branch from where to download the release bundle, must be specified"
+  exit 1
+fi
+BRANCH="$1"
 
-SCAN_REPORT_DIR="$WORK_DIR/scan_report_dir"
+if [ -z "$2" ]; then
+  echo "Work directory must be specified"
+  exit 1
+fi
+WORK_DIR="$2"
+
+if [ -z "$3" ]; then
+  echo "A flag to indicate whether to scan the release bundle which is already downloaded and extracted"
+  exit 1
+fi
+USE_DOWNLOADED_BUNDLE="$3"
+
+DEFAULT_SCAN_REPORT_DIR="$WORK_DIR/scan_report_dir"
+SCAN_REPORT_DIR=${4:-$DEFAULT_SCAN_REPORT_DIR}
+
 SCANNER_HOME="$WORK_DIR/scanner_home"
 SCAN_REPORT="$SCAN_REPORT_DIR/scan_report.out"
+SCAN_SUMMARY_REPORT="${SCAN_REPORT_DIR}/scan_summary.out"
+
 VERRAZZANO_PREFIX="verrazzano-$RELEASE_VERSION"
 
 RELEASE_TAR_BALL="$VERRAZZANO_PREFIX-lite.zip"
@@ -64,6 +83,16 @@ function download_release_tarball() {
         -bn ${OBJECT_STORAGE_BUCKET} \
         --name "${BRANCH}/${RELEASE_TAR_BALL}" \
         --file "$RELEASE_BUNDLE_DIR/${RELEASE_TAR_BALL}"
+  SHA256_CMD="sha256sum -c"
+  if [ "$(uname)" == "Darwin" ]; then
+    SHA256_CMD="shasum -a 256 -c"
+  fi
+  ${SHA_CMD} ${_file}.256
+  unzip ${_file}
+  rm -f ${_file}
+  rm -f ${_file}.256
+  unzip $RELEASE_TAR_BALL
+  rm $RELEASE_TAR_BALL
 }
 
 function install_scanner() {
@@ -86,11 +115,6 @@ function scan_release_binaries() {
     rm -f $SCAN_REPORT
   fi
 
-  # Extract the release bundle to a directory, and scan that directory
-  cd $RELEASE_BUNDLE_DIR
-  unzip $RELEASE_TAR_BALL
-  rm $RELEASE_TAR_BALL
-
   cd $DIR_TO_SCAN
   count_files=$(ls -1q *.* | wc -l)
   ls
@@ -102,11 +126,10 @@ function scan_release_binaries() {
   ./uvscan $DIR_TO_SCAN --RPTALL --RECURSIVE --CLEAN --UNZIP --VERBOSE --SUB --SUMMARY --PROGRAM --RPTOBJECTS >> $SCAN_REPORT 2>&1
 
   # Extract only the last 25 lines from the scan report and create a file, which will be used for the validation
-  local scan_summary="${SCAN_REPORT_DIR}/scan_summary.out"
-  if [ -e "${scan_summary}" ]; then
-    rm -f $scan_summary
+  if [ -e "${SCAN_SUMMARY_REPORT}" ]; then
+    rm -f $SCAN_SUMMARY_REPORT
   fi
-  tail -25 ${SCAN_REPORT} > ${scan_summary}
+  tail -25 ${SCAN_REPORT} > ${SCAN_SUMMARY_REPORT}
 
   # The following set of lines from the summary in the scan report is used here for validation.
   declare -a expectedLines=("Total files:...................     $count_files"
@@ -133,7 +156,7 @@ function scan_release_binaries() {
         *)
       esac
     done
-  done < "$scan_summary"
+  done < "$SCAN_SUMMARY_REPORT"
   echo "Count of expected lines in the scan summary: ${result_count}"
   if [ "$result_count" == "$array_count" ];then
     echo "Found all the expected lines in the summary of the scan report."
@@ -146,7 +169,11 @@ function scan_release_binaries() {
 
 mkdir -p $SCANNER_HOME
 validate_oci_cli || exit 1
-download_release_tarball || exit 1
+
+if [[ "${USE_DOWNLOADED_BUNDLE}" == "false" ]]; then
+  download_release_tarball || exit 1
+fi
+
 install_scanner || exit 1
 update_virus_definition || exit 1
 scan_release_binaries || exit 1
