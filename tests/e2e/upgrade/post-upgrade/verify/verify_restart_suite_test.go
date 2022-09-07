df8413f80baecd20bf7a3e18be86cea51e8d3c7c
ben.mclean <ben.mclean@oracle.com>
2022-09-07 10:34:57 -0400

    add unit test

--- tests/e2e/upgrade/post-upgrade/verify/verify_restart_suite_test.go
+++ tests/e2e/upgrade/post-upgrade/verify/verify_restart_suite_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2021, Oracle and/or its affiliates.
+// Copyright (c) 2022, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 package verify
@@ -11,5 +11,5 @@ import (
 
 func TestVerifyAppRestart(t *testing.T) {
 	gomega.RegisterFailHandler(ginkgo.Fail)
-	ginkgo.RunSpecs(t, "Upgrade Verify App Restart Suite")
+	ginkgo.RunSpecs(t, "Verify Upgrade Suite")
 }
---
+++ tests/e2e/upgrade/post-upgrade/verify/verify_upgrade_flow_test.go
@@ -0,0 +1,33 @@
+// Copyright (c) 2022, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package verify
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	vzapi "github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1alpha1"
+	"github.com/verrazzano/verrazzano/tests/e2e/pkg"
+)
+
+var _ = t.Describe("After upgrade is complete", Label("f:platform-lcm.upgrade"), func() {
+
+	// GIVEN the verrazzano-system namespace
+	// WHEN the container images are retrieved
+	// THEN verify that each pod that uses istio has the correct istio proxy image
+	t.It("an install should not begin", func() {
+		Eventually(func() bool {
+			return upgradeCompleteIsLastCondition()
+		}, fiveMinutes, pollingInterval).Should(BeTrue(), "Expected UpgradeComplete to be the last condition")
+	})
+})
+
+func upgradeCompleteIsLastCondition() bool {
+	vz, err := pkg.GetVerrazzano()
+	if err != nil {
+		t.Logs.Errorf("Error getting Verrazzano CR: %v", err)
+		return false
+	}
+	lastCondtion := vz.Status.Conditions[len(vz.Status.Conditions)-1]
+	return lastCondtion.Type == vzapi.CondUpgradeComplete
+}
