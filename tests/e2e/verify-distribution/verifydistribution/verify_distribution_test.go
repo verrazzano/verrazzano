4b8c82fb98a1bb7bc3c1ed7ea02c5793851bcd85
Saidu Dosapati <saidu.babu.dosapati@oracle.com>
2022-09-12 07:40:01 -0400

    more changes; added support for Full bundle contents validation

--- tests/e2e/verify-distribution/verifydistribution/verify_distribution_test.go
+++ tests/e2e/verify-distribution/verifydistribution/verify_distribution_test.go
@@ -13,9 +13,12 @@ import (
 
 const SLASH = "/"
 
-var opensourcepaths = map[string]string{
+var variant string
+
+var allPaths = map[string]string{
 	"top":                          "",
 	"bin":                          SLASH + "bin",
+	"images":                       SLASH + "images",
 	"manifests":                    SLASH + "manifests",
 	"charts":                       SLASH + "manifests" + SLASH + "charts",
 	"verrazzano-platform-operator": SLASH + "manifests" + SLASH + "charts" + SLASH + "verrazzano-platform-operator",
@@ -23,9 +26,21 @@ var opensourcepaths = map[string]string{
 	"templates":                    SLASH + "manifests" + SLASH + "charts" + SLASH + "verrazzano-platform-operator" + SLASH + "templates",
 	"k8s":                          SLASH + "manifests" + SLASH + "k8s",
 }
+
 var opensourcefileslistbydir = map[string][]string{
 	"top":                          {"LICENSE", "README.md", "bin", "manifests"},
 	"bin":                          {"bom_utils.sh", "vz", "vz-registry-image-helper.sh"},
+	"manifests":                    {"charts", "k8s", "verrazzano-bom.json"},
+	"charts":                       {"verrazzano-platform-operator"},
+	"verrazzano-platform-operator": {"Chart.yaml", "NOTES.txt", "crds", "templates", "values.yaml"},
+	"crds":                         {"clusters.verrazzano.io_verrazzanomanagedclusters.yaml", "install.verrazzano.io_verrazzanos.yaml"},
+	"templates":                    {"clusterrole.yaml", "clusterrolebinding.yaml", "deployment.yaml", "namespace.yaml", "service.yaml", "serviceaccount.yaml", "validatingwebhookconfiguration.yaml"},
+	"k8s":                          {"verrazzano-platform-operator.yaml"},
+}
+
+var fullBundleFileslistbydir = map[string][]string{
+	"top":                          {"LICENSE", "README.md", "bin", "images", "manifests"},
+	"bin":                          {"bom_utils.sh", "darwin-amd64", "darwin-arm64", "linux-amd64", "linux-arm64", "vz-registry-image-helper.sh"},
 	"manifests":                    {"charts", "k8s", "verrazzano-bom.json"},
 	"charts":                       {"verrazzano-platform-operator"},
 	"verrazzano-platform-operator": {"Chart.yaml", "NOTES.txt", "crds", "templates", "values.yaml"},
@@ -39,21 +54,42 @@ var t = framework.NewTestFramework("verifydistribution")
 var _ = t.AfterEach(func() {})
 
 var _ = t.Describe("Verify VZ distribution", func() {
-	t.Describe("When provided Lite ", func() {
 
-		generatedPath := os.Getenv("TARBALL_DIR") ///home/opc/jenkins/workspace/sapat_vz-distribution-validation/vz-tarball/verrazzano-1.4.0
+	variant = os.Getenv("DISTRIBUTION_VARIANT")
+	generatedPath := os.Getenv("TARBALL_DIR") ///home/opc/jenkins/workspace/sapat_vz-distribution-validation/vz-tarball/verrazzano-1.4.0
 
-		t.It("Verify Lite bundle", func() {
-			verifyDisByDir(generatedPath+opensourcepaths["top"], "top")
-			verifyDisByDir(generatedPath+opensourcepaths["bin"], "bin")
-			verifyDisByDir(generatedPath+opensourcepaths["manifests"], "manifests")
-			verifyDisByDir(generatedPath+opensourcepaths["charts"], "charts")
-			verifyDisByDir(generatedPath+opensourcepaths["verrazzano-platform-operator"], "verrazzano-platform-operator")
-			verifyDisByDir(generatedPath+opensourcepaths["crds"], "crds")
-			verifyDisByDir(generatedPath+opensourcepaths["templates"], "templates")
-			verifyDisByDir(generatedPath+opensourcepaths["k8s"], "k8s")
+	if variant == "Lite" {
+		t.Describe("When provided Lite ", func() {
+
+			t.It("Verify Lite bundle", func() {
+				verifyDisByDir(generatedPath+allPaths["top"], "top")
+				verifyDisByDir(generatedPath+allPaths["bin"], "bin")
+				verifyDisByDir(generatedPath+allPaths["manifests"], "manifests")
+				verifyDisByDir(generatedPath+allPaths["charts"], "charts")
+				verifyDisByDir(generatedPath+allPaths["verrazzano-platform-operator"], "verrazzano-platform-operator")
+				verifyDisByDir(generatedPath+allPaths["crds"], "crds")
+				verifyDisByDir(generatedPath+allPaths["templates"], "templates")
+				verifyDisByDir(generatedPath+allPaths["k8s"], "k8s")
+			})
 		})
-	})
+	} else {
+		t.Describe("When provided full bundle", func() {
+
+			// generatedPath := os.Getenv("TARBALL_DIR") //+ "verrazzano-1.4.0"
+
+			t.It("Verify Full Bundle", func() {
+				verifyDisByDir(generatedPath+allPaths["top"], "top")
+				verifyDisByDir(generatedPath+allPaths["bin"], "bin")
+				verifyDisByDir(generatedPath+allPaths["manifests"], "manifests")
+				verifyDisByDir(generatedPath+allPaths["charts"], "charts")
+				verifyDisByDir(generatedPath+allPaths["verrazzano-platform-operator"], "verrazzano-platform-operator")
+				verifyDisByDir(generatedPath+allPaths["crds"], "crds")
+				verifyDisByDir(generatedPath+allPaths["templates"], "templates")
+				verifyDisByDir(generatedPath+allPaths["k8s"], "k8s")
+			})
+		})
+	}
+
 })
 
 func verifyDisByDir(inputDir string, key string) {
@@ -65,8 +101,13 @@ func verifyDisByDir(inputDir string, key string) {
 	gomega.Expect(err).To(gomega.BeNil())
 	for _, each := range filesInfo {
 		filesList = append(filesList, each.Name())
+		println(each.Name())
 	}
-	gomega.Expect(compareSlices(filesList, opensourcefileslistbydir[key])).To(gomega.BeTrue())
+	if variant == "Lite" {
+		gomega.Expect(compareSlices(filesList, opensourcefileslistbydir[key])).To(gomega.BeTrue())
+	} else {
+		gomega.Expect(compareSlices(filesList, fullBundleFileslistbydir[key])).To(gomega.BeTrue())
+	}
 	fmt.Printf("All files found for %s \n", key)
 }
 
