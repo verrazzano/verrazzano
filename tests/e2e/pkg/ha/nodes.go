ed735bbcd306bf0913620713070188a314c18f27
Fred Tibbitts <fred.tibbitts@oracle.com>
2022-09-12 14:06:28 -0400

    Fix file after rebase

--- tests/e2e/pkg/ha/nodes.go
+++ tests/e2e/pkg/ha/nodes.go
@@ -5,13 +5,11 @@ package ha
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"github.com/onsi/gomega"
 	"go.uber.org/zap"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
@@ -35,47 +33,6 @@ func EventuallyGetNodes(cs *kubernetes.Clientset, log *zap.SugaredLogger) *corev
 		return true
 	}, WaitTimeout, PollingInterval).Should(gomega.BeTrue())
 	return nodes
-}
-
-func EventuallySetNodeScheduling(cs *kubernetes.Clientset, name string, unschedulable bool, log *zap.SugaredLogger) {
-	gomega.Eventually(func() bool {
-		node, err := cs.CoreV1().Nodes().Get(context.TODO(), name, metav1.GetOptions{})
-		if err != nil {
-			log.Errorf("Failed to refresh node[%s]: %v", name, err)
-			return false
-		}
-		node.Spec.Unschedulable = unschedulable
-		if _, err := cs.CoreV1().Nodes().Update(context.TODO(), node, metav1.UpdateOptions{}); err != nil {
-			log.Errorf("Failed to update node[%s] scheduling: %v", name, err)
-			return false
-		}
-		return true
-	}).Should(gomega.BeTrue())
-	log.Infof("Set node[%s].spec.unschedulable=%v", name, unschedulable)
-}
-
-func EventuallyEvictNode(cs *kubernetes.Clientset, name string, log *zap.SugaredLogger) {
-	gomega.Eventually(func() bool {
-		pods, err := cs.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{
-			FieldSelector: fmt.Sprintf("spec.nodeName=%s", name),
-		})
-		if err != nil {
-			log.Errorf("Failed to get pods for node[%s]: %v", name, err)
-			return false
-		}
-
-		for i := range pods.Items {
-			pod := &pods.Items[i]
-			if err := cs.CoreV1().Pods(pod.Namespace).Delete(context.TODO(), pod.Name, metav1.DeleteOptions{}); err != nil {
-				if !errors.IsNotFound(err) {
-					log.Errorf("Failed to delete pod[%s] for node[%s]: %v", pod.Name, name, err)
-					return false
-				}
-			}
-		}
-		return true
-	}, WaitTimeout, PollingInterval).Should(gomega.BeTrue())
-	log.Infof("Evicted node[%s]", name)
 }
 
 func IsControlPlaneNode(node corev1.Node) bool {
