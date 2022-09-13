1d49ad3aa016abab92d6e96a6ef505f407d0e679
stshapir <steven.shapiro@oracle.com>
2022-09-13 09:29:29 -0400

    rebase

--- platform-operator/controllers/verrazzano/component/mysql/mysql_component.go
+++ platform-operator/controllers/verrazzano/component/mysql/mysql_component.go
@@ -14,7 +14,6 @@ import (
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/common"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/helm"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/istio"
-	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/mysqloperator"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/spi"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/secret"
 	"github.com/verrazzano/verrazzano/platform-operator/internal/config"
