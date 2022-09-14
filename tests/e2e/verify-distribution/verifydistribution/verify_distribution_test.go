12a3e2095fe19fa3c2c1e4cca8c3350bf930ed83
Saidu Dosapati <saidu.babu.dosapati@oracle.com>
2022-09-14 08:08:06 -0400

    renaming and cleanup

--- tests/e2e/verify-distribution/verifydistribution/verify_distribution_test.go
+++ tests/e2e/verify-distribution/verifydistribution/verify_distribution_test.go
@@ -44,12 +44,15 @@ var opensourcefileslistbydir = map[string][]string{
 }
 
 var fullBundleFileslistbydir = map[string][]string{
-	"top":       {"LICENSE", "README.md", "README.html", "bin", "images", "manifests"},
-	"bin":       {"bom_utils.sh", "darwin-amd64", "darwin-arm64", "linux-amd64", "linux-arm64", "vz-registry-image-helper.sh"},
-	"vz":        {"vz"},
-	"manifests": {"charts", "k8s", "profiles", "verrazzano-bom.json"},
-	"k8s":       {"verrazzano-platform-operator.yaml"},
-	"profiles":  {"dev.yaml", "managed-cluster.yaml", "prod.yaml"},
+	"top":          {"LICENSE", "README.md", "README.html", "bin", "images", "manifests"},
+	"bin":          {"bom_utils.sh", "darwin-amd64", "darwin-arm64", "linux-amd64", "linux-arm64", "vz-registry-image-helper.sh"},
+	"darwin-amd64": {"vz"},
+	"darwin-arm64": {"vz"},
+	"linux-amd64":  {"vz"},
+	"linux-arm64":  {"vz"},
+	"manifests":    {"charts", "k8s", "profiles", "verrazzano-bom.json"},
+	"k8s":          {"verrazzano-platform-operator.yaml"},
+	"profiles":     {"dev.yaml", "managed-cluster.yaml", "prod.yaml"},
 }
 
 var t = framework.NewTestFramework("verifydistribution")
@@ -100,10 +103,10 @@ var _ = t.Describe("Verify VZ distribution", func() {
 				verifyDistributionByDirectory(generatedPath+allPaths["top"], "top", variant)
 				verifyDistributionByDirectory(generatedPath+allPaths["bin"], "bin", variant)
 
-				verifyDistributionByDirectory(generatedPath+allPaths["bin"]+"/darwin-amd64", "vz", variant)
-				verifyDistributionByDirectory(generatedPath+allPaths["bin"]+"/darwin-arm64", "vz", variant)
-				verifyDistributionByDirectory(generatedPath+allPaths["bin"]+"/linux-amd64", "vz", variant)
-				verifyDistributionByDirectory(generatedPath+allPaths["bin"]+"/linux-arm64", "vz", variant)
+				verifyDistributionByDirectory(generatedPath+allPaths["bin"]+"/darwin-amd64", "darwin-amd64", variant)
+				verifyDistributionByDirectory(generatedPath+allPaths["bin"]+"/darwin-arm64", "darwin-arm64", variant)
+				verifyDistributionByDirectory(generatedPath+allPaths["bin"]+"/linux-amd64", "linux-amd64", variant)
+				verifyDistributionByDirectory(generatedPath+allPaths["bin"]+"/linux-arm64", "linux-arm64", variant)
 
 				verifyDistributionByDirectory(generatedPath+allPaths["manifests"], "manifests", variant)
 				verifyDistributionByDirectory(generatedPath+allPaths["k8s"], "k8s", variant)
