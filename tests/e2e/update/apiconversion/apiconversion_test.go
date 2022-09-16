e2036c90ab29b4a0eff622a7be9bcf44ad34b7a1
abnadago <abijitha.nadagouda@oracle.com>
2022-09-16 11:08:27 -0700

    ingressNginx tests

--- tests/e2e/update/apiconversion/apiconversion_test.go
+++ tests/e2e/update/apiconversion/apiconversion_test.go
@@ -1,8 +1,18 @@
+// Copyright (c) 2022, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
 package apiconversion
 
 import (
 	"fmt"
+	. "github.com/onsi/ginkgo/v2"
+	"github.com/verrazzano/verrazzano/pkg/constants"
+	"github.com/verrazzano/verrazzano/pkg/test/framework"
 	"github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1beta1"
+	"github.com/verrazzano/verrazzano/tests/e2e/pkg"
+	"github.com/verrazzano/verrazzano/tests/e2e/update"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	"sigs.k8s.io/yaml"
 )
 
 const (
@@ -10,16 +20,103 @@ const (
 	ingressNGINXLabelKey   = "app.kubernetes.io/name"
 )
 
-type IngressNGINXReplicasModifier struct {
+type IngressNGINXReplicasModifierV1beta1 struct {
 	replicas uint32
 }
 
-type IngressNGINXPodPerNodeAffinityModifier struct {
+type IngressNGINXPodPerNodeAffinityModifierV1beta1 struct {
 }
 
-func (u IngressNGINXReplicasModifier) ModifyCR(cr *v1beta1.Verrazzano) {
+type IngressNGINXDefaultModifierV1beta1 struct {
+}
+
+func (u IngressNGINXDefaultModifierV1beta1) ModifyCRV1beta1(cr *v1beta1.Verrazzano) {
+	cr.Spec.Components.IngressNGINX = &v1beta1.IngressNginxComponent{}
+}
+
+var t = framework.NewTestFramework("update ingressNginx")
+
+var nodeCount uint32
+
+var _ = t.BeforeSuite(func() {
+	var err error
+	nodeCount, err = pkg.GetNodeCount()
+	if err != nil {
+		Fail(err.Error())
+	}
+})
+
+var _ = t.AfterSuite(func() {
+	m := IngressNGINXDefaultModifierV1beta1{}
+	err := update.UpdateCRV1beta1(m)
+	if err != nil {
+		Fail(err.Error())
+	}
+
+	cr := update.GetCR()
+	expectedRunning := uint32(1)
+	if cr.Spec.Profile == "prod" || cr.Spec.Profile == "" {
+		expectedRunning = 2
+	}
+	update.ValidatePods(ingressNGINXLabelValue, ingressNGINXLabelKey, constants.IngressNamespace, expectedRunning, false)
+
+})
+
+func (u IngressNGINXReplicasModifierV1beta1) ModifyCRV1beta1(cr *v1beta1.Verrazzano) {
 	if cr.Spec.Components.IngressNGINX == nil {
 		cr.Spec.Components.IngressNGINX = &v1beta1.IngressNginxComponent{}
 	}
-	cr.Spec.Components.IngressNGINX.ValueOverrides = fmt.Sprintf(`replicas: %v`, u.replicas)
+	ingressNginxReplicaOverridesYaml := fmt.Sprintf(`controller:
+            defaultBackend:
+              replicaCount: %v`, u.replicas)
+	cr.Spec.Components.IngressNGINX.ValueOverrides = createOverridesOrDie(ingressNginxReplicaOverridesYaml)
 }
+
+func (u IngressNGINXPodPerNodeAffinityModifierV1beta1) ModifyCRV1beta1(cr *v1beta1.Verrazzano) {
+	if cr.Spec.Components.IngressNGINX == nil {
+		cr.Spec.Components.IngressNGINX = &v1beta1.IngressNginxComponent{}
+	}
+	ingressNginxReplicaOverridesYaml := fmt.Sprintf(`controller:
+              affinity:
+                podAntiAffinity:
+                  preferredDuringSchedulingIgnoredDuringExecution:
+                    - podAffinityTerm:
+                        labelSelector:
+                          matchLabels:
+                            app.kubernetes.io/component: controller
+                            app.kubernetes.io/name: %v
+                        topologyKey: kubernetes.io/hostname
+                      weight: 100`, ingressNGINXLabelValue)
+	cr.Spec.Components.IngressNGINX.ValueOverrides = createOverridesOrDie(ingressNginxReplicaOverridesYaml)
+}
+
+func createOverridesOrDie(yamlString string) []v1beta1.Overrides {
+	data, err := yaml.YAMLToJSON([]byte(yamlString))
+	if err != nil {
+		t.Logs.Errorf("Failed to convert yaml to JSON: %s", yamlString)
+		panic(err)
+	}
+	return []v1beta1.Overrides{
+		{
+			ConfigMapRef: nil,
+			SecretRef:    nil,
+			Values: &apiextensionsv1.JSON{
+				Raw: data,
+			},
+		},
+	}
+}
+
+var _ = t.Describe("Update ingressNGINX", Label("f:platform-lcm.update"), func() {
+	t.Describe("ingress-nginx update replicas with v1beta1 client", Label("f:platform-lcm.authproxy-update-replicas"), func() {
+		t.It("authproxy explicit replicas", func() {
+			m := IngressNGINXReplicasModifierV1beta1{replicas: nodeCount}
+			err := update.UpdateCRV1beta1(m)
+			if err != nil {
+				Fail(err.Error())
+			}
+			expectedRunning := nodeCount
+			update.ValidatePods(ingressNGINXLabelValue, ingressNGINXLabelKey, constants.IngressNamespace, expectedRunning, false)
+		})
+	})
+})
