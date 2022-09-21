22c009573bcaf58def52accbdf2ee2654940e103
Neeraj Singh <neeraj.n.singh@oracle.com>
2022-09-21 16:31:05 +0530

    make kiali installtion based on istio installation

--- platform-operator/controllers/verrazzano/component/kiali/kiali_component.go
+++ platform-operator/controllers/verrazzano/component/kiali/kiali_component.go
@@ -144,8 +144,9 @@ func (c kialiComponent) IsReady(context spi.ComponentContext) bool {
 }
 
 // IsEnabled Kiali-specific enabled check for installation
+// If Istio is enabled then only enabled kiali else it will be disabled always
 func (c kialiComponent) IsEnabled(effectiveCR runtime.Object) bool {
-	return vzconfig.IsKialiEnabled(effectiveCR)
+	return vzconfig.IsIstioEnabled(effectiveCR) && vzconfig.IsKialiEnabled(effectiveCR)
 }
 
 // createOrUpdateKialiResources create or update related Kiali resources
