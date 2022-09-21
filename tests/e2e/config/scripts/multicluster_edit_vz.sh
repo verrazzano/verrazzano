7a1f8877a204182444950d7257737165fdfd90f8
Anand Francis Joseph <anand.francis.joseph.augustin@oracle.com>
2022-09-21 09:27:03 +0530

    Using install overrides for both v1alpha1 and v1beta1 CRD API selection
    Signed-off-by: Anand Francis Joseph <anand.francis.joseph.augustin@oracle.com>

--- tests/e2e/config/scripts/multicluster_edit_vz.sh
+++ tests/e2e/config/scripts/multicluster_edit_vz.sh
@@ -15,14 +15,7 @@ yq -i eval '.spec.components.kubeStateMetrics.enabled = true' "${VZ_CR_FILE}"
 yq -i eval '.spec.components.prometheusPushgateway.enabled = true' "${VZ_CR_FILE}"
 yq -i eval '.spec.components.jaegerOperator.enabled = true' "${VZ_CR_FILE}"
 # For managed clusters, enable Jaeger operator and update the istio tracing configuration
-if [ "${CLUSTER_COUNT}" -gt 1 ] && [ "$CRD_API_VERSION" == "v1alpha1" ]; then
-    yq -i eval '.spec.components.istio.istioInstallArgs[0].name = "meshConfig.enableTracing"' "${VZ_CR_FILE}"
-    yq -i eval '.spec.components.istio.istioInstallArgs[0].value = "true"' "${VZ_CR_FILE}"
-    yq -i eval '.spec.components.istio.istioInstallArgs[1].name = "meshConfig.defaultConfig.tracing.sampling"' "${VZ_CR_FILE}"
-    yq -i eval '.spec.components.istio.istioInstallArgs[1].value = "90.0"' "${VZ_CR_FILE}"
-    yq -i eval '.spec.components.istio.istioInstallArgs[2].name = "meshConfig.defaultConfig.tracing.zipkin.address"' "${VZ_CR_FILE}"
-    yq -i eval '.spec.components.istio.istioInstallArgs[2].value = "jaeger-verrazzano-managed-cluster-collector.verrazzano-monitoring.svc.cluster.local.:9411"' "${VZ_CR_FILE}"
-elif [ "${CLUSTER_COUNT}" -gt 1  ] && [ $CRD_API_VERSION == "v1beta1" ]; then
+if [ "${CLUSTER_COUNT}" -gt 1  ]; then
   yq -i eval '.spec.components.istio.overrides.[0].values.apiVersion = "install.istio.io/v1alpha1"' ${VZ_CR_FILE}
   yq -i eval '.spec.components.istio.overrides.[0].values.kind = "IstioOperator"' ${VZ_CR_FILE}
   yq -i eval '.spec.components.istio.overrides.[0].values.spec.values.meshConfig.defaultConfig.tracing.sampling = 90' ${VZ_CR_FILE}
