c17042e0755f3c66ff98aa9567ef6800e676b0b6
Paul Mackin <paul.mackin@oracle.com>
2022-09-22 13:58:53 -0400

    add comment

--- platform-operator/controllers/verrazzano/component/networkpolicies/netpol_component.go
+++ platform-operator/controllers/verrazzano/component/networkpolicies/netpol_component.go
@@ -1,8 +1,9 @@
 // Copyright (c) 2022, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
+// This component is needed to apply network policies during install and upgrade before
+// any other components are installed or upgraded.
 // Note that there is no NetworkPolicy component in Verrazzano CR.
-// This component is needed to apply network policies during install and upgrade.
 
 package networkpolicies
 
