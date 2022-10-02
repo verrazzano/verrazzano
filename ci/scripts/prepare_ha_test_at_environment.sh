5c1d3af08af9db76b401c18addd5c7cbeacccfae
Michael Gianatassio <michael.gianatassio@oracle.com>
2022-10-02 11:51:50 -0400

    fix array yaml

--- ci/scripts/prepare_ha_test_at_environment.sh
+++ ci/scripts/prepare_ha_test_at_environment.sh
@@ -26,10 +26,10 @@ fi
 yq eval -i '.spec.components.opensearch.policies[0].policyName = "verrazzano-system"' "${INSTALL_CONFIG_FILE}"
 yq eval -i '.spec.components.opensearch.policies[0].indexPattern = "verrazzano-system*"' "${INSTALL_CONFIG_FILE}"
 yq eval -i '.spec.components.opensearch.policies[0].minIndexAge = "7d"' "${INSTALL_CONFIG_FILE}"
-yq eval -i '.spec.components.opensearch.policies[0].rollover[0].minIndexAge = "1d"' "${INSTALL_CONFIG_FILE}"
+yq eval -i '.spec.components.opensearch.policies[0].rollover.minIndexAge = "1d"' "${INSTALL_CONFIG_FILE}"
 yq eval -i '.spec.components.opensearch.policies[1].policyName = "verrazzano-application"' "${INSTALL_CONFIG_FILE}"
 yq eval -i '.spec.components.opensearch.policies[1].indexPattern = "verrazzano-application*"' "${INSTALL_CONFIG_FILE}"
 yq eval -i '.spec.components.opensearch.policies[1].minIndexAge = "7d"' "${INSTALL_CONFIG_FILE}"
-yq eval -i '.spec.components.opensearch.policies[1].rollover[0].minIndexAge = "1d"' "${INSTALL_CONFIG_FILE}"
+yq eval -i '.spec.components.opensearch.policies[1].rollover.minIndexAge = "1d"' "${INSTALL_CONFIG_FILE}"
 
 exit 0
