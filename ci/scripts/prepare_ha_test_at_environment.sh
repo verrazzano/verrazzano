cb10852b5d410a4f59cfcc2c01838d9480aa0754
Michael Gianatassio <michael.gianatassio@oracle.com>
2022-10-02 09:14:52 -0400

    clean up bash script

--- ci/scripts/prepare_ha_test_at_environment.sh
+++ ci/scripts/prepare_ha_test_at_environment.sh
@@ -23,13 +23,13 @@ if [ ! -f "$INSTALL_CONFIG_FILE" ]; then
 fi
 
 # Update the install configuration to include what the end-to-end tests require
-yq eval -i '.spec.components.opensearch.policies[0].policyName = "verrazzano-system"' ${INSTALL_CONFIG_FILE}
-yq eval -i '.spec.components.opensearch.policies[0].indexPattern = "verrazzano-system*"' ${INSTALL_CONFIG_FILE}
-yq eval -i '.spec.components.opensearch.policies[0].minIndexAge = "7d"' ${INSTALL_CONFIG_FILE}
-yq eval -i '.spec.components.opensearch.policies[0].rollover[0].minIndexAge = "1d"' ${INSTALL_CONFIG_FILE}
-yq eval -i '.spec.components.opensearch.policies[1].policyName = "verrazzano-application"' ${INSTALL_CONFIG_FILE}
-yq eval -i '.spec.components.opensearch.policies[1].indexPattern = "verrazzano-application*"' ${INSTALL_CONFIG_FILE}
-yq eval -i '.spec.components.opensearch.policies[1].minIndexAge = "7d"' ${INSTALL_CONFIG_FILE}
-yq eval -i '.spec.components.opensearch.policies[1].rollover[0].minIndexAge = "1d"' ${INSTALL_CONFIG_FILE}
+yq eval -i '.spec.components.opensearch.policies[0].policyName = "verrazzano-system"' "${INSTALL_CONFIG_FILE}"
+yq eval -i '.spec.components.opensearch.policies[0].indexPattern = "verrazzano-system*"' "${INSTALL_CONFIG_FILE}"
+yq eval -i '.spec.components.opensearch.policies[0].minIndexAge = "7d"' "${INSTALL_CONFIG_FILE}"
+yq eval -i '.spec.components.opensearch.policies[0].rollover[0].minIndexAge = "1d"' "${INSTALL_CONFIG_FILE}"
+yq eval -i '.spec.components.opensearch.policies[1].policyName = "verrazzano-application"' "${INSTALL_CONFIG_FILE}"
+yq eval -i '.spec.components.opensearch.policies[1].indexPattern = "verrazzano-application*"' "${INSTALL_CONFIG_FILE}"
+yq eval -i '.spec.components.opensearch.policies[1].minIndexAge = "7d"' "${INSTALL_CONFIG_FILE}"
+yq eval -i '.spec.components.opensearch.policies[1].rollover[0].minIndexAge = "1d"' "${INSTALL_CONFIG_FILE}"
 
 exit 0
