e59c356f7de997e7819f9d0ae2d3146584f40e57
Michael Gianatassio <michael.gianatassio@oracle.com>
2022-10-04 10:54:21 -0400

    incresae another timeout

--- tests/e2e/loggingtrait/weblogicworkload/weblogic_loggingtrait_test.go
+++ tests/e2e/loggingtrait/weblogicworkload/weblogic_loggingtrait_test.go
@@ -21,7 +21,7 @@ import (
 const (
 	shortWaitTimeout     = 10 * time.Minute
 	shortPollingInterval = 10 * time.Second
-	longWaitTimeout      = 15 * time.Minute
+	longWaitTimeout      = 20 * time.Minute
 	longPollingInterval  = 20 * time.Second
 	componentsPath       = "testdata/loggingtrait/weblogicworkload/weblogic-logging-components.yaml"
 	applicationPath      = "testdata/loggingtrait/weblogicworkload/weblogic-logging-application.yaml"
