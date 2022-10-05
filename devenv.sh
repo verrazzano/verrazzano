849c7915db6b0c52e8ff6db68f9b155f94e148e5
Michael Cico <46466506+michael-cico@users.noreply.github.com>
2022-10-05 17:44:22 -0400

    Get rid of need for .go-version

--- devenv.sh
+++ devenv.sh
@@ -1,8 +1,24 @@
 # Copyright (C) 2022, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
-unset GOVERSION
-. ./.go-version
-echo "Setting up go version ${GOVERSION}"
+
+scriptHome=$(dirname ${0})
+
+branch_version=$(grep verrazzano-development-version ${scriptHome}/.verrazzano-development-version | sed -e 's/verrazzano-development-version=//')
+case $branch_version in
+  1.4*|1.3*)
+    GOVER=1.17.8
+    ;;
+  1.2*)
+    GOVER=1.16.15
+    ;;
+  *)
+    GOVER=1.19.2
+    ;;
+esac
+unset branch_version
+
+GOVERSION=${1:-1.19.2}
+echo "Setting up go version ${GOVERSION}, GOPATH=${GOPATH}"
 
 GOCMD=go${GOVERSION}
 
@@ -27,4 +43,6 @@ Go commmand: $(which go)
 
 $(go version)
 """
+
+unset scriptHome
 
