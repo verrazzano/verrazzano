bbe20de3c07660a38e889f7e105ca8bc6c095a41
Ravi Singhal <ravi.r.singhal@oracle.com>
2022-10-07 15:33:55 +0530

    fix format

--- tests/e2e/verify-infra/restapi/rancher_url_test.go
+++ tests/e2e/verify-infra/restapi/rancher_url_test.go
@@ -291,9 +291,9 @@ func verifyUILogoSetting(settingName string, logoPath string, dynamicClient dyna
 // verifySettingValue verifies the value of a rancher setting
 // GIVEN a Verrazzano installation with setting specified by settingName populated
 //
-//  WHEN value field of the setting CR specified by settingName is extracted
-//  AND compared with input expectedValue
-//  THEN both the values are expected to be equal, otherwise the test scenario is deemed to have failed.
+//	WHEN value field of the setting CR specified by settingName is extracted
+//	AND compared with input expectedValue
+//	THEN both the values are expected to be equal, otherwise the test scenario is deemed to have failed.
 func verifySettingValue(settingName string, expectedValue string, k8sClient dynamic.Interface) {
 	start := time.Now()
 	t.Logs.Infof("Verify %s setting", settingName)
