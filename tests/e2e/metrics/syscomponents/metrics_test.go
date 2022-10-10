912b45c96eaac6eb85a059e112aa1fa1dff14030
Sukruth N Bhat <sukruth.n.bhat@oracle.com>
2022-10-10 16:37:17 +0530

    Misspelled words corrected

--- tests/e2e/metrics/syscomponents/metrics_test.go
+++ tests/e2e/metrics/syscomponents/metrics_test.go
@@ -34,7 +34,7 @@ const (
 	vmoFunctionMetric              = "vmo_reconcile_total"
 	vmoCounterMetric               = "vmo_deployment_update_total"
 	vmoGaugeMetric                 = "vmo_work_queue_size"
-	vmoTimestampMetric             = "vmo_configmap_last_succesful_timestamp"
+	vmoTimestampMetric             = "vmo_configmap_last_successful_timestamp"
 	vaoSuccessCountMetric          = "vao_appconfig_successful_reconcile_total"
 	vaoFailCountMetric             = "vao_appconfig_error_reconcile_total"
 	vaoDurationCountMetric         = "vao_appconfig_reconcile_duration_count"
