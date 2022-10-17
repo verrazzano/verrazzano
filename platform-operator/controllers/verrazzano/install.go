7a7ea4c3e3f9e331c7c946870e1cfd8eb4049ec9
ben.mclean <ben.mclean@oracle.com>
2022-10-17 17:23:30 -0400

    requeue

--- platform-operator/controllers/verrazzano/install.go
+++ platform-operator/controllers/verrazzano/install.go
@@ -6,7 +6,6 @@ package verrazzano
 import (
 	"fmt"
 	vzapi "github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1alpha1"
-	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/rancher"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/registry"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/spi"
 	vzcontext "github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/context"
@@ -130,6 +129,8 @@ func (r *Reconciler) reconcileComponents(vzctx vzcontext.VerrazzanoContext, preU
 				return ctrl.Result{Requeue: true}, err
 			}
 			tracker.vzState = vzStateInstallComponents
+			// since we updated the status, requeue to pick up new changesf
+			return ctrl.Result{Requeue: true}, nil
 
 		case vzStateInstallComponents:
 			res, err := r.installComponents(spiCtx, tracker, preUpgrade)
@@ -140,9 +141,9 @@ func (r *Reconciler) reconcileComponents(vzctx vzcontext.VerrazzanoContext, preU
 
 		case vzStatePostInstall:
 			if !preUpgrade {
-				if err := rancher.ConfigureAuthProviders(spiCtx); err != nil {
-					return ctrl.Result{Requeue: true}, err
-				}
+				//if err := rancher.ConfigureAuthProviders(spiCtx); err != nil {
+				//	return ctrl.Result{Requeue: true}, err
+				//}
 			}
 			tracker.vzState = vzStateReconcileEnd
 		}
