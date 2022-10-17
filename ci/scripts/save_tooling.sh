0fff0b30aba2e862f11eb7475f0587ee46e1911f
Robert Donat <robert.donat@oracle.com>
2022-10-17 17:52:50 +0000

    fix script

--- ci/scripts/save_tooling.sh
+++ ci/scripts/save_tooling.sh
@@ -23,7 +23,7 @@ fi
 
 cd out
 tar -czf ${WORKSPACE}/vz-linux-amd64.tar.gz -C linux_amd64 .
-if [ "CURRENT_BRANCH_NAME" == "master" ] || [[ "CURRENT_BRANCH_NAME" == "release-*" ]]; then
+if [[ $CURRENT_BRANCH_NAME == master ]] || [[ $CURRENT_BRANCH_NAME == release-* ]]; then
   tar -czf ${WORKSPACE}/vz-linux-arm64.tar.gz -C linux_arm64 .
   tar -czf ${WORKSPACE}/vz-darwin-amd64.tar.gz -C darwin_amd64 .
   tar -czf ${WORKSPACE}/vz-darwin-arm64.tar.gz -C darwin_arm64 .
@@ -36,7 +36,7 @@ oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} 
 oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_COMMIT_BUCKET} --name ephemeral/${CURRENT_BRANCH_NAME}/${SHORT_COMMIT_HASH_ENV}/vz-linux-amd64.tar.gz --file vz-linux-amd64.tar.gz
 oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_COMMIT_BUCKET} --name ephemeral/${CURRENT_BRANCH_NAME}/${SHORT_COMMIT_HASH_ENV}/vz-linux-amd64.tar.gz.sha256 --file vz-linux-amd64.tar.gz.sha256
 
-if [ "CURRENT_BRANCH_NAME" == "master" ] || [[ "CURRENT_BRANCH_NAME" == "release-*" ]]; then
+if [[ $CURRENT_BRANCH_NAME == master ]] || [[ $CURRENT_BRANCH_NAME == release-* ]]; then
   sha256sum vz-linux-arm64.tar.gz > vz-linux-arm64.tar.gz.sha256
   sha256sum vz-darwin-amd64.tar.gz > vz-darwin-amd64.tar.gz.sha256
   sha256sum vz-darwin-arm64.tar.gz > vz-darwin-arm64.tar.gz.sha256
