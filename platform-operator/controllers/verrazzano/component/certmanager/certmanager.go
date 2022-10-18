44637ecfc72d6da632ce42367baf9f77c8722570
Aditya Bharadwaj <49105292+adiforluls@users.noreply.github.com>
2022-10-18 09:33:16 +0530

    VZ-6932: Webhook, unit tests, refactoring (#4389) (#4408)

--- platform-operator/controllers/verrazzano/component/certmanager/certmanager.go
+++ platform-operator/controllers/verrazzano/component/certmanager/certmanager.go
@@ -12,6 +12,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1beta1"
+	"github.com/verrazzano/verrazzano/platform-operator/internal/vzconfig"
 	"io"
 	"k8s.io/apimachinery/pkg/runtime"
 	"net/mail"
@@ -57,6 +58,9 @@ const (
 	caCertCommonName            = "verrazzano-root-ca"
 	verrazzanoClusterIssuerName = "verrazzano-cluster-issuer"
 	clusterResourceNamespaceKey = "clusterResourceNamespace"
+
+	longestSystemURLPrefix = "elasticsearch.vmi.system"
+	preOccupiedspace       = len(longestSystemURLPrefix) + 2
 
 	crdDirectory  = "/cert-manager/"
 	crdInputFile  = "cert-manager.crds.yaml"
@@ -945,4 +949,58 @@ func GetOverrides(object runtime.Object) interface{} {
 		return effectiveCR.Spec.Components.CertManager.ValueOverrides
 	}
 	return []v1beta1.Overrides{}
+}
+
+// validateLongestHostName validates that the longest possible host name for a system endpoint
+// is not greater than 64 characters
+func validateLongestHostName(effectiveCR runtime.Object) error {
+	envName := getEnvironmentName(effectiveCR)
+	dnsSuffix, wildcard := getDNSSuffix(effectiveCR)
+	spaceOccupied := preOccupiedspace
+	longestHostName := fmt.Sprintf("%s.%s.%s", longestSystemURLPrefix, envName, dnsSuffix)
+	if len(longestHostName) > 64 {
+		if wildcard {
+			spaceOccupied = spaceOccupied + len(dnsSuffix)
+			return fmt.Errorf("spec.environmentName %s is too long. For the given configuration it must have at most %v characters", envName, 64-spaceOccupied)
+		}
+
+		return fmt.Errorf("spec.environmentName %s and DNS suffix %s are too long. For the given configuration they must have at most %v characters in combination", envName, dnsSuffix, 64-spaceOccupied)
+	}
+	return nil
+}
+
+func getEnvironmentName(effectiveCR runtime.Object) string {
+	if cr, ok := effectiveCR.(*vzapi.Verrazzano); ok {
+		return cr.Spec.EnvironmentName
+	}
+	cr := effectiveCR.(*v1beta1.Verrazzano)
+	return cr.Spec.EnvironmentName
+}
+
+func getDNSSuffix(effectiveCR runtime.Object) (string, bool) {
+	dnsSuffix, wildcard := "0.0.0.0", true
+	if cr, ok := effectiveCR.(*vzapi.Verrazzano); ok {
+		if cr.Spec.Components.DNS == nil || cr.Spec.Components.DNS.Wildcard != nil {
+			return fmt.Sprintf("%s.%s", dnsSuffix, vzconfig.GetWildcardDomain(cr.Spec.Components.DNS)), wildcard
+		} else if cr.Spec.Components.DNS.OCI != nil {
+			wildcard = false
+			dnsSuffix = cr.Spec.Components.DNS.OCI.DNSZoneName
+		} else if cr.Spec.Components.DNS.External != nil {
+			wildcard = false
+			dnsSuffix = cr.Spec.Components.DNS.External.Suffix
+		}
+		return dnsSuffix, wildcard
+	}
+
+	cr := effectiveCR.(*v1beta1.Verrazzano)
+	if cr.Spec.Components.DNS == nil || cr.Spec.Components.DNS.Wildcard != nil {
+		return fmt.Sprintf("%s.%s", dnsSuffix, vzconfig.GetWildcardDomain(cr.Spec.Components.DNS)), wildcard
+	} else if cr.Spec.Components.DNS.OCI != nil {
+		wildcard = false
+		dnsSuffix = cr.Spec.Components.DNS.OCI.DNSZoneName
+	} else if cr.Spec.Components.DNS.External != nil {
+		wildcard = false
+		dnsSuffix = cr.Spec.Components.DNS.External.Suffix
+	}
+	return dnsSuffix, wildcard
 }
