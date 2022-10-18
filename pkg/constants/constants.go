41f122e587f8d170c25ab1e4013e3b87a702e776
Sukruth N Bhat <sukruth.n.bhat@oracle.com>
2022-10-18 11:05:58 +0530

    Testing

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -9,7 +9,7 @@ import (
 	platformOperatorConstants "github.com/verrazzano/verrazzano/platform-operator/constants"
 )
 
-// RestartVersionAnnotation - the annotation used by user to tell Verrazzano applicaton to restart its components
+// RestartVersionAnnotation - the annotation used by user to tell Verrazzano application to restart its components
 const RestartVersionAnnotation = "verrazzano.io/restart-version"
 
 // VerrazzanoRestartAnnotation is the annotation used to restart platform workloads
