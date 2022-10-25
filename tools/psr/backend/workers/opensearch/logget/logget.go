065d80b58223fcdd887b3bcc36ca6bfb442e4dbd
ben.mclean <ben.mclean@oracle.com>
2022-10-25 09:51:35 -0400

    no body

--- tools/psr/backend/workers/opensearch/logget/logget.go
+++ tools/psr/backend/workers/opensearch/logget/logget.go
@@ -4,20 +4,18 @@
 package logget
 
 import (
-	"bytes"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/verrazzano/verrazzano/pkg/log/vzlog"
 	"github.com/verrazzano/verrazzano/tools/psr/backend/config"
 	"github.com/verrazzano/verrazzano/tools/psr/backend/spi"
-	"io"
 	"net/http"
 	"net/url"
 )
 
 const osIngestService = "vmi-system-es-ingest.verrazzano-system:9200"
 
-var bodyString = "{\"query\":{\"bool\":{\"filter\":[{\"match_phrase\":{\"kubernetes.container_name\":\"istio-proxy\"}}]}}}"
-var body = io.NopCloser(bytes.NewBuffer([]byte(bodyString)))
+//var bodyString = "{\"query\":{\"bool\":{\"filter\":[{\"match_phrase\":{\"kubernetes.container_name\":\"istio-proxy\"}}]}}}"
+//var body = io.NopCloser(bytes.NewBuffer([]byte(bodyString)))
 
 type LogGetter struct {
 	spi.Worker
@@ -55,7 +53,7 @@ func (w LogGetter) DoWork(conf config.CommonConfig, log vzlog.VerrazzanoLogger) 
 			Path:   "verrazzano-system",
 		},
 		Header: http.Header{"Content-Type": {"application/json"}},
-		Body:   body,
+		//Body:   body,
 	}
 	_, err := c.Do(&req)
 	if err == nil {
