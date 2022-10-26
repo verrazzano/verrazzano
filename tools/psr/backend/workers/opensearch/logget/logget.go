40a98a795936b24ae064ed287259a4aa725a56c1
ben.mclean <ben.mclean@oracle.com>
2022-10-26 10:07:14 -0400

    parse

--- tools/psr/backend/workers/opensearch/logget/logget.go
+++ tools/psr/backend/workers/opensearch/logget/logget.go
@@ -4,20 +4,18 @@
 package logget
 
 import (
-	"bytes"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/verrazzano/verrazzano/pkg/log/vzlog"
 	"github.com/verrazzano/verrazzano/tools/psr/backend/config"
 	"github.com/verrazzano/verrazzano/tools/psr/backend/spi"
-	"io"
 	"net/http"
 	"net/url"
 )
 
-const osIngestService = "vmi-system-es-ingest.verrazzano-system:9200"
+const osIngestService = "http://vmi-system-es-ingest.verrazzano-system:9200/verrazzano-system"
 
-var bodyString = "{\"query\":{\"bool\":{\"filter\":[{\"match_phrase\":{\"kubernetes.container_name\":\"istio-proxy\"}}]}}}"
-var body = io.NopCloser(bytes.NewBuffer([]byte(bodyString)))
+//var bodyString = "{\"query\":{\"bool\":{\"filter\":[{\"match_phrase\":{\"kubernetes.container_name\":\"istio-proxy\"}}]}}}"
+//var body = io.NopCloser(bytes.NewBuffer([]byte(bodyString)))
 
 type LogGetter struct {
 	spi.Worker
@@ -50,12 +48,17 @@ func (w LogGetter) DoWork(conf config.CommonConfig, log vzlog.VerrazzanoLogger) 
 	c := http.Client{}
 	req := http.Request{
 		URL: &url.URL{
-			Scheme: "http",
-			Host:   osIngestService,
-			Path:   "verrazzano-system",
+			//Scheme: "http",
+			//Host:   osIngestService,
+			//Path:   "verrazzano-system",
 		},
 		//Header: http.Header{"Content-Type": {"application/json"}},
 		//Body:   body,
+	}
+	u, err := url.Parse(osIngestService)
+	req.URL = u
+	if err != nil {
+		log.Error(err)
 	}
 	resp, err := c.Do(&req)
 	log.Infof("Resp: %V", resp == nil)
