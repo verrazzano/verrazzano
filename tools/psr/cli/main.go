5bd7bd4ddc4a0bcf5e6fe29c56c28762a6f24406
stshapir <steven.shapiro@oracle.com>
2022-10-26 15:49:21 -0400

    first commit for psrctl cli

--- tools/psr/cli/main.go
+++ tools/psr/cli/main.go
@@ -3,6 +3,22 @@
 
 package main
 
-func main() {
+import (
+	"os"
 
+	"github.com/spf13/pflag"
+	"github.com/verrazzano/verrazzano/tools/psr/cli/cmd/root"
+	"github.com/verrazzano/verrazzano/tools/vz/cmd/helpers"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+)
+
+func main() {
+	flags := pflag.NewFlagSet("vz", pflag.ExitOnError)
+	pflag.CommandLine = flags
+
+	rc := helpers.NewRootCmdContext(genericclioptions.IOStreams{In: os.Stdin, Out: os.Stdout, ErrOut: os.Stderr})
+	rootCmd := root.NewRootCmd(rc)
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
 }
