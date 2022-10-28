148bbbd7117d8136af61e1a1425166cdb888c775
davisgomes <49332517+davisgomes@users.noreply.github.com>
2022-10-28 10:01:17 -0400

    VZ-6465 add more logging to register multicluster

--- tests/e2e/config/scripts/register_managed_cluster.sh
+++ tests/e2e/config/scripts/register_managed_cluster.sh
@@ -116,16 +116,20 @@ fi
 
 # get the admin user token from the Rancher API
 RANCHER_URL=$(kubectl --kubeconfig ${ADMIN_KUBECONFIG} get vz -o jsonpath='{.items[0].status.instance.rancherUrl}')
+echo "RANCHER_URL: ${RANCHER_URL}"
 RANCHER_ADMIN_PASS=$(kubectl --kubeconfig ${ADMIN_KUBECONFIG} get secret -n cattle-system rancher-admin-secret -o jsonpath={.data.password} | base64 --decode)
-RANCHER_TOKEN=$(curl -k -X POST -H 'Content-Type: application/json' "${RANCHER_URL}/v3-public/localProviders/local?action=login"  -d "{\"username\":\"admin\", \"password\":\"${RANCHER_ADMIN_PASS}\"}"| jq -r ".token")
+echo "RANCHER_ADMIN_PASS: ${RANCHER_ADMIN_PASS}"
+RANCHER_TOKEN=$(curl -s -k -X POST -H 'Content-Type: application/json' "${RANCHER_URL}/v3-public/localProviders/local?action=login"  -d "{\"username\":\"admin\", \"password\":\"${RANCHER_ADMIN_PASS}\"}"| jq -r ".token")
+echo "RANCHER_TOKEN: ${RANCHER_TOKEN}"
 if [ -z "${RANCHER_TOKEN}" ] ; then
   echo "Rancher token for admin user not found"
   exit 1
 fi
 
 # Use the admin token to apply the manifest to the managed cluster
-RANCHER_CLUSTER_ID=$(curl -k -X GET -H "Authorization: Bearer ${RANCHER_TOKEN}" "${RANCHER_URL}/v3/clusters?name=${MANAGED_CLUSTER_NAME}" | jq '.data[0].id')
-curl -k -X GET -H "Authorization: Bearer ${RANCHER_TOKEN}" "${RANCHER_URL}/v3/import/${RANCHER_TOKEN}_${RANCHER_CLUSTER_ID}.yaml" > register-"${MANAGED_CLUSTER_NAME}".yaml
+RANCHER_CLUSTER_ID=$(curl -s -k -X GET -H "Authorization: Bearer ${RANCHER_TOKEN}" "${RANCHER_URL}/v3/clusters?name=${MANAGED_CLUSTER_NAME}" | jq '.data[0].id')
+echo "RANCHER_CLUSTER_ID: ${RANCHER_CLUSTER_ID}"
+curl -s -k -X GET -H "Authorization: Bearer ${RANCHER_TOKEN}" "${RANCHER_URL}/v3/import/${RANCHER_TOKEN}_${RANCHER_CLUSTER_ID}.yaml" > register-"${MANAGED_CLUSTER_NAME}".yaml
 
 echo "----------BEGIN register-${MANAGED_CLUSTER_NAME}.yaml contents----------"
 cat register-${MANAGED_CLUSTER_NAME}.yaml
