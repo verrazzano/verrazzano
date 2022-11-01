5b59d44d0c6a6049787890dfe532351f0665f6df
Rosemary Marano <rosemary.marano@oracle.com>
2022-11-01 15:01:00 -0400

    edit generated API docs

--- application-operator/apis/app/v1alpha1/metricsbinding_types.go
+++ application-operator/apis/app/v1alpha1/metricsbinding_types.go
@@ -39,7 +39,7 @@ type MetricsBindingSpec struct {
 	// +optional
 	PrometheusConfigMap NamespaceName `json:"prometheusConfigMap,omitempty"`
 
-	// Identifies a namespace, name, and key for a secret containing the Prometheus config.
+	// Identifies a namespace, name, and key for a secret containing the Prometheus configuration.
 	// +optional
 	PrometheusConfigSecret SecretKey `json:"prometheusConfigSecret,omitempty"`
 
@@ -49,10 +49,10 @@ type MetricsBindingSpec struct {
 
 // NamespaceName identifies a namespace and name pair for a resource.
 type NamespaceName struct {
-	// Name of resource.
+	// Name of the resource.
 	Name string `json:"name"`
 
-	// Namespace of resource.
+	// Namespace of the resource.
 	Namespace string `json:"namespace"`
 }
 
@@ -70,9 +70,9 @@ type SecretKey struct {
 
 // Workload identifies the name and type of workload.
 type Workload struct {
-	// Name of resource.
+	// Name of a resource.
 	Name string `json:"name"`
 
-	// TypeMeta of resource.
+	// TypeMeta of a resource.
 	TypeMeta metav1.TypeMeta `json:"typeMeta"`
 }
