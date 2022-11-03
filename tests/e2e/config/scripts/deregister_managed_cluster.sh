79a8dd8dcee688bfecdfb7ba969207e1edaf45dc
Fred Tibbitts III <44442080+fredtibbitts@users.noreply.github.com>
2022-11-03 14:36:04 -0400

    Revert "VZ-6468: Delete Rancher cluster when VMC is deleted (#4602)"
    This reverts commit 7ebb9ca68e8e41f16cf8e38e5da9c8a701437378.

--- tests/e2e/config/scripts/deregister_managed_cluster.sh
+++ tests/e2e/config/scripts/deregister_managed_cluster.sh
@@ -23,6 +23,26 @@ echo ADMIN_KUBECONFIG: ${ADMIN_KUBECONFIG}
 echo MANAGED_CLUSTER_NAME: ${MANAGED_CLUSTER_NAME}
 echo MANAGED_KUBECONFIG: ${MANAGED_KUBECONFIG}
 
+function deleteRancherCluster() {
+      # get the admin user token from the Rancher API
+      RANCHER_URL=$(kubectl --kubeconfig ${ADMIN_KUBECONFIG} get vz -o jsonpath='{.items[0].status.instance.rancherUrl}')
+      echo "RANCHER_URL: ${RANCHER_URL}"
+      RANCHER_ADMIN_PASS=$(kubectl --kubeconfig ${ADMIN_KUBECONFIG} get secret -n cattle-system rancher-admin-secret -o jsonpath={.data.password} | base64 --decode)
+      RANCHER_TOKEN=$(curl -s -k -X POST -H 'Content-Type: application/json' "${RANCHER_URL}/v3-public/localProviders/local?action=login"  -d "{\"username\":\"admin\", \"password\":\"${RANCHER_ADMIN_PASS}\"}"| jq -r ".token")
+      if [ -z "${RANCHER_TOKEN}" ] ; then
+        echo "Rancher token for admin user not found"
+        exit 1
+      fi
+
+      # Use the token to retrieve Rancher cluster id and delete the cluster from Rancher
+      RANCHER_CLUSTER_ID=$(curl -s -k -X GET -H "Authorization: Bearer ${RANCHER_TOKEN}" "${RANCHER_URL}/v3/clusters?name=${MANAGED_CLUSTER_NAME}" | jq -r '.data[0].id')
+      echo "deleting RANCHER_CLUSTER_ID: ${RANCHER_CLUSTER_ID}"
+      curl -s -k -X DELETE -H "Authorization: Bearer ${RANCHER_TOKEN}" "${RANCHER_URL}/v3/clusters/${RANCHER_CLUSTER_ID}"
+}
+
+# Delete the Rancher cluster explicitly here until VMC delete auto-triggers Rancher cluster delete (VZ-6451).
+echo "Deleting Rancher cluster"
+deleteRancherCluster
 echo "Deleting VMC on admin cluster ${MANAGED_CLUSTER_NAME}"
 kubectl --kubeconfig ${ADMIN_KUBECONFIG} -n verrazzano-mc delete vmc ${MANAGED_CLUSTER_NAME}
 echo "Deleting cluster registration secret on managed cluster"
