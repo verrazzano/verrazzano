883d8910b60970b5e5ab9b8d372da7704c26253b
Ryan Eberhard <ryan.eberhard@oracle.com>
2022-11-04 14:27:26 -0400

    Remove WebLogic types and replace with Unstructured

--- application-operator/apis/weblogic/v8/doc.go
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2020, 2021, Oracle and/or its affiliates.
-// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
-
-// Package v8 contains API Schema definitions for the weblogic v8 API group
-// +k8s:deepcopy-gen=package,register
-// +groupName=weblogic.oracle
-package v8
