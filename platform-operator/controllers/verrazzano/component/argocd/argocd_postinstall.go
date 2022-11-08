ebe820d6a8a9957d4bdcfe232f820a6ecd9e0c66
leliyin <lelia.yin@oracle.com>
2022-11-07 21:43:03 -0800

    2nd pass at keycloak integration

--- platform-operator/controllers/verrazzano/component/argocd/argocd_postinstall.go
+++ platform-operator/controllers/verrazzano/component/argocd/argocd_postinstall.go
@@ -4,8 +4,11 @@
 package argocd
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"text/template"
+
 	vzapi "github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1alpha1"
 	"github.com/verrazzano/verrazzano/platform-operator/constants"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/common"
@@ -15,9 +18,30 @@ import (
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	controllerruntime "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
+
+const (
+	ArgoCDIngressCAName = "tls-argocd-ingress"
+	ArgoCDCACert        = "ca.crt"
+)
+
+const oidcConfigTemplate = `
+     name: Keycloak
+     issuer: "{{.KeycloakUrl}}"
+     clientID: argocd
+     clientSecret: $oidc.keycloak.clientSecret
+     requestedScopes: ["openid", "profile", "email", "groups"]
+     rootCA: 
+       "{{.CaCert}}"
+`
+
+type oideConfigTemplateData struct {
+	KeycloakUrl string
+	CaCert      []byte
+}
 
 // patchArgoCDSecret
 func patchArgoCDSecret(ctx spi.ComponentContext) error {
@@ -46,9 +70,33 @@ func patchArgoCDSecret(ctx spi.ComponentContext) error {
 
 // patchArgoCDConfigMap
 func patchArgoCDConfigMap(ctx spi.ComponentContext) error {
-	argocdHostName, err := getArgoCDHostname(ctx.Client(), ctx.EffectiveCR())
+	dnsSubDomain, err := getDNSDomain(ctx.Client(), ctx.EffectiveCR())
 	if err != nil {
-		return ctx.Log().ErrorfNewErr("Failed getting ArgoCD host name: %v", err)
+		ctx.Log().Errorf("Component ArgoCD failed retrieving DNS sub domain: %v", err)
+		return err
+	}
+
+	keycloakHost := "keycloak." + dnsSubDomain
+	argocdHost := "argocd." + dnsSubDomain
+
+	ctx.Log().Debugf("Getting ArgoCD TLS root CA")
+	caCert, err := GetRootCA(ctx)
+	if err != nil {
+		ctx.Log().Errorf("Failed to get ArgoCD TLS root CA: %v", err)
+		return err
+	}
+
+	t, err := template.New("oidcconfig_template").Parse(oidcConfigTemplate)
+	if err != nil {
+		return err
+	}
+	var b bytes.Buffer
+	var data = oideConfigTemplateData{}
+	data.KeycloakUrl = fmt.Sprintf("https://%s/%s", keycloakHost, "auth/realms/verrazzano-system")
+	data.CaCert = caCert
+	err = t.Execute(&b, &data)
+	if err != nil {
+		return err
 	}
 
 	cm := &corev1.ConfigMap{
@@ -57,21 +105,13 @@ func patchArgoCDConfigMap(ctx spi.ComponentContext) error {
 			Namespace: constants.ArgoCDNamespace,
 		},
 	}
-
-	var oidcString = fmt.Sprintf(`|
-        name: Keycloak
-        issuer: "https://%s/%s"
-        clientID: argocd
-        clientSecret: $oidc.keycloak.clientSecret
-        requestedScopes: ["openid", "profile", "email", "groups"]`, argocdHostName, "auth/realms/verrazzano-system")
-
 	// Add the oidc configuration to enable our keycloak authentication.
 	if _, err := controllerruntime.CreateOrUpdate(context.TODO(), ctx.Client(), cm, func() error {
 		if cm.Data == nil {
 			cm.Data = make(map[string]string)
 		}
-		cm.Data["url"] = fmt.Sprintf("https://%s", argocdHostName)
-		cm.Data["oidc.config"] = oidcString
+		cm.Data["url"] = fmt.Sprintf("https://%s", argocdHost)
+		cm.Data["oidc.config"] = b.String()
 		return nil
 	}); err != nil {
 		return err
@@ -90,8 +130,7 @@ func patchArgoCDRbacConfigMap(ctx spi.ComponentContext) error {
 		},
 	}
 
-	var policyString = `|
-        g, verrazzano-admins, role:admin`
+	var policyString = `g, verrazzano-admins, role:admin`
 	var err error
 
 	// Disable the built-in admin user. Grant admin (role:admin) to verrazzano-admins group
@@ -109,6 +148,21 @@ func patchArgoCDRbacConfigMap(ctx spi.ComponentContext) error {
 	return err
 }
 
+// GetRootCA gets the root CA certificate from the argocd TLS secret. If the secret does not exist, we
+// return a nil slice.
+func GetRootCA(ctx spi.ComponentContext) ([]byte, error) {
+	secret := &corev1.Secret{}
+	nsName := types.NamespacedName{
+		Namespace: constants.ArgoCDNamespace,
+		Name:      ArgoCDIngressCAName}
+
+	if err := ctx.Client().Get(context.TODO(), nsName, secret); err != nil {
+		return nil, err
+	}
+
+	return secret.Data[ArgoCDCACert], nil
+}
+
 func getArgoCDHostname(c client.Client, vz *vzapi.Verrazzano) (string, error) {
 	dnsSuffix, err := vzconfig.GetDNSSuffix(c, vz)
 	if err != nil {
@@ -120,4 +174,14 @@ func getArgoCDHostname(c client.Client, vz *vzapi.Verrazzano) (string, error) {
 	}
 	argocdHostname := fmt.Sprintf("%s.%s.%s", common.ArgoCDName, env, dnsSuffix)
 	return argocdHostname, nil
+}
+
+// getDNSDomain returns the DNS Domain
+func getDNSDomain(c client.Client, vz *vzapi.Verrazzano) (string, error) {
+	dnsSuffix, err := vzconfig.GetDNSSuffix(c, vz)
+	if err != nil {
+		return "", err
+	}
+	dnsDomain := fmt.Sprintf("%s.%s", vz.Spec.EnvironmentName, dnsSuffix)
+	return dnsDomain, nil
 }
