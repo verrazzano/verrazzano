4c3421abf78a565475ebb84ccd2cf44173770a56
Deva Sagar <deva.sagar@oracle.com>
2022-11-09 15:19:48 -0500

    reduce client-go auth imports

--- cluster-operator/main.go
+++ cluster-operator/main.go
@@ -20,9 +20,7 @@ import (
 	"flag"
 	"os"
 
-	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
-	// to ensure that exec-entrypoint and run can make use of them.
-	_ "k8s.io/client-go/plugin/pkg/client/auth"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
