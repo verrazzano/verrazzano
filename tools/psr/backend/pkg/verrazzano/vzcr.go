54488eb935c0e3744c1cfa4a8e03527c29ab4ad0
Paul Mackin <paul.mackin@oracle.com>
2022-11-10 16:17:44 -0500

    add psr client

--- tools/psr/backend/pkg/verrazzano/vzcr.go
+++ tools/psr/backend/pkg/verrazzano/vzcr.go
@@ -2,10 +2,13 @@ package verrazzano
 
 import (
 	"context"
+	"fmt"
 	"github.com/verrazzano/verrazzano/pkg/k8sutil"
 	vzalpha1 "github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1alpha1"
+	vpoClient "github.com/verrazzano/verrazzano/platform-operator/clientset/versioned"
 	e2epkg "github.com/verrazzano/verrazzano/tests/e2e/pkg"
 	"github.com/verrazzano/verrazzano/tests/e2e/pkg/update"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -29,20 +32,23 @@ func UpdateCR(ctrlRuntimeClient client.Client, m update.CRModifier) error {
 	return err
 }
 
-// GetVzCr returns true if VZ CR is ready
-func GetVzCr() (*vzalpha1.Verrazzano, error) {
-	kubeconfigPath, err := k8sutil.GetKubeConfigLocation()
-	if err != nil {
-		return nil, err
-	}
-	// Get the CR
-	cr, err := e2epkg.GetVerrazzanoInstallResourceInCluster(kubeconfigPath)
-	if err != nil {
-		return nil, err
-	}
-	return cr, nil
-}
-
 func IsReady(cr *vzalpha1.Verrazzano) bool {
 	return cr.Status.State == vzalpha1.VzStateReady
+}
+
+// GetVerrazzanoResource returns the installed Verrazzano CR in the given cluster
+// (there should only be 1 per cluster)
+func GetVerrazzanoResource(client vpoClient.Interface) (*vzalpha1.Verrazzano, error) {
+	vzClient := client.VerrazzanoV1alpha1().Verrazzanos("")
+	vzList, err := vzClient.List(context.TODO(), metav1.ListOptions{})
+
+	if err != nil {
+		return nil, fmt.Errorf("error listing out Verrazzano instances: %v", err)
+	}
+	numVzs := len(vzList.Items)
+	if numVzs == 0 {
+		return nil, fmt.Errorf("did not find installed Verrazzano instance")
+	}
+	vz := vzList.Items[0]
+	return &vz, nil
 }
--- tools/psr/backend/workers/opensearch/scale/scale.go
+++ tools/psr/backend/workers/opensearch/scale/scale.go
@@ -6,10 +6,9 @@ package scale
 import (
 	"fmt"
 	"github.com/verrazzano/verrazzano/tests/e2e/pkg/update"
+	"github.com/verrazzano/verrazzano/tools/psr/backend/pkg/k8sclient"
 	psropensearch "github.com/verrazzano/verrazzano/tools/psr/backend/pkg/opensearch"
 	psrvz "github.com/verrazzano/verrazzano/tools/psr/backend/pkg/verrazzano"
-	controllerruntime "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 	"strconv"
 	"time"
 
@@ -35,9 +34,8 @@ type scaleWorker struct {
 	metricDescList []prometheus.Desc
 	*workerMetrics
 	*state
-
-	// ctrlRuntimeClient is a controller-runtime client
-	ctrlRuntimeClient client.Client
+	psrClient k8sclient.PsrClient
+	log       vzlog.VerrazzanoLogger
 }
 
 type state struct {
@@ -53,7 +51,13 @@ type workerMetrics struct {
 }
 
 func NewScaleWorker() (spi.Worker, error) {
+	c, err := k8sclient.NewPsrClient()
+	if err != nil {
+		return nil, err
+	}
 	w := scaleWorker{
+		psrClient: c,
+		log:       vzlog.DefaultLogger(),
 		workerMetrics: &workerMetrics{
 			scaleOutCountTotal: metrics.MetricItem{
 				Name: "scale_out_count_total",
@@ -73,27 +77,7 @@ func NewScaleWorker() (spi.Worker, error) {
 		*w.scaleInCountTotal.BuildMetricDesc(w.GetWorkerDesc().MetricsName),
 	}
 
-	c, err := createClient()
-	if err != nil {
-		return nil, err
-	}
-
-	w.ctrlRuntimeClient = c
-
 	return w, nil
-}
-
-func createClient() (client.Client, error) {
-	cfg, err := controllerruntime.GetConfig()
-	if err != nil {
-		return nil, fmt.Errorf("Failed to get controller-runtime config %v", err)
-	}
-	c, err := client.New(cfg, client.Options{})
-	if err != nil {
-		return nil, fmt.Errorf("Failed to create a controller-runtime client %v", err)
-	}
-	_ = vzv1alpha1.AddToScheme(c.Scheme())
-	return c, nil
 }
 
 // GetWorkerDesc returns the WorkerDesc for the worker
@@ -130,19 +114,19 @@ func (w scaleWorker) DoWork(_ config.CommonConfig, log vzlog.VerrazzanoLogger) e
 	}
 
 	// Wait until VZ is ready
-	_, err := waitReady(log, true)
+	_, err := w.waitReady(true)
 	if err != nil {
 		log.Progress("Failed to wait for Verrazzano to be ready after update.  The test results are not valid %v", err)
 		return err
 	}
 	// Get the current OpenSearch replica field for the tier in the VZ CR
-	pods, err := psropensearch.GetPodsForTier(w.ctrlRuntimeClient, tier)
+	pods, err := psropensearch.GetPodsForTier(w.psrClient.CrtlRuntime, tier)
 	if err != nil {
 		log.Progress("Failed to get the pods for tier %s: %v", tier, err)
 		return err
 	}
 	// Create a modifier that is used to update the Verrazzno CR opensearch replica field
-	m, desiredReplicas, err := getUpdateModifier(tier, len(pods))
+	m, desiredReplicas, err := w.getUpdateModifier(tier, len(pods))
 	if err != nil {
 		return err
 	}
@@ -150,13 +134,13 @@ func (w scaleWorker) DoWork(_ config.CommonConfig, log vzlog.VerrazzanoLogger) e
 	log.Infof("Updating Verrazzano CR OpenSearch %s tier, scaling to %v replicas", tier, desiredReplicas)
 
 	// Update the CR to change the replica count
-	err = updateCr(log, w.ctrlRuntimeClient, m)
+	err = w.updateCr(m)
 	if err != nil {
 		return err
 	}
 
 	// Wait until VZ is NOT ready, this means it started working on the change
-	_, err = waitReady(log, false)
+	_, err = w.waitReady(false)
 	if err != nil {
 		log.Progress("Failed to wait for Verrazzano to be NOT ready after update.  The test results are not valid %v", err)
 		return err
@@ -176,7 +160,7 @@ func (w scaleWorker) GetMetricList() []prometheus.Metric {
 	}
 }
 
-func getUpdateModifier(tier string, currentReplicas int) (*update.CRModifier, int, error) {
+func (w scaleWorker) getUpdateModifier(tier string, currentReplicas int) (*update.CRModifier, int, error) {
 	max, err := strconv.Atoi(config.PsrEnv.GetEnv(maxReplicaCount))
 	if err != nil {
 		return nil, 0, fmt.Errorf("maxReplicaCount can not be parsed to an integer: %f", err)
@@ -208,31 +192,15 @@ func getUpdateModifier(tier string, currentReplicas int) (*update.CRModifier, in
 	return &m, int(desiredReplicas), nil
 }
 
-// Get current replicas in the VZ CR elasticsearch component for the current tier
-func getCurrentReplicas(cr *vzv1alpha1.Verrazzano, tier string) int {
-	if cr.Spec.Components.Elasticsearch == nil || cr.Spec.Components.Elasticsearch.Nodes == nil {
-		return 1
-	}
-
-	for _, node := range cr.Spec.Components.Elasticsearch.Nodes {
-		for _, nodeRole := range node.Roles {
-			if string(nodeRole) == tier {
-				return int(node.Replicas)
-			}
-		}
-	}
-	return 1
-}
-
 // updateCr updates the Verrazzano CR and retries if there is a conflict error
-func updateCr(log vzlog.VerrazzanoLogger, ctrlRuntimeClient client.Client, m *update.CRModifier) error {
+func (w scaleWorker) updateCr(m *update.CRModifier) error {
 	for {
-		err := psrvz.UpdateCR(ctrlRuntimeClient, *m)
+		err := psrvz.UpdateCR(w.psrClient.CrtlRuntime, *m)
 		if err != nil {
 			if er.IsUpdateConflict(err) {
 				time.Sleep(3 * time.Second)
 				logMsg := fmt.Sprintf("VZ conflict error, retrying")
-				log.Infof(logMsg)
+				w.log.Infof(logMsg)
 				continue
 			} else {
 				return fmt.Errorf("Failed to scale update Verrazzano cr: %v", err)
@@ -244,9 +212,9 @@ func updateCr(log vzlog.VerrazzanoLogger, ctrlRuntimeClient client.Client, m *up
 }
 
 // Wait until Verrazzano is ready or not ready
-func waitReady(log vzlog.VerrazzanoLogger, desiredReady bool) (cr *vzv1alpha1.Verrazzano, err error) {
+func (w scaleWorker) waitReady(desiredReady bool) (cr *vzv1alpha1.Verrazzano, err error) {
 	for {
-		cr, err = psrvz.GetVzCr()
+		cr, err = psrvz.GetVerrazzanoResource(w.psrClient.VzInstall)
 		if err != nil {
 			return nil, err
 		}
@@ -257,7 +225,7 @@ func waitReady(log vzlog.VerrazzanoLogger, desiredReady bool) (cr *vzv1alpha1.Ve
 		if ready == desiredReady {
 			break
 		}
-		log.Progressf("Waiting for Verrazzano CR ready state to be %v", desiredReady)
+		w.log.Progressf("Waiting for Verrazzano CR ready state to be %v", desiredReady)
 		time.Sleep(1 * time.Second)
 	}
 	return cr, err
