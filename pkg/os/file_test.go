1045ea9b6e60d34c368acb27ccf64d1a64de18ef
alyssavlev <alyssa.l.levine@oracle.com>
2022-11-14 11:40:06 -0500

    alyslevi/VZ-7658 added unit tests in file_test.go

--- pkg/os/file_test.go
+++ pkg/os/file_test.go
@@ -3,11 +3,41 @@
 package os
 
 import (
-	"github.com/stretchr/testify/assert"
-	"go.uber.org/zap"
+	"fmt"
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
 )
+
+// TestCreateTempFile tests the CreateTempFile function
+// GIVEN a call to CreateTempFile
+//
+//	WHEN with a valid prefix and/or suffix
+//	THEN the temp file is created
+func TestCreateTempFile(t *testing.T) {
+	var tests = []struct {
+		name          string
+		createPattern string
+	}{
+		{name: "WithPrefixAndSuffix", createPattern: "test1-*.yaml"},
+		{name: "WithPrefixOnly", createPattern: "test2-*"},
+		{name: "WithSuffixOnly", createPattern: "*-test3"},
+		{name: "NoPrefixNorSuffix", createPattern: "test4"},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			assert := assert.New(t)
+			temp, err := CreateTempFile(test.createPattern, nil)
+			assert.FileExists(temp.Name())
+			fmt.Println(temp.Name())
+			assert.NoErrorf(err, "Unable to create temp file %s: %s", temp.Name(), err)
+		})
+	}
+
+}
 
 // TestRemoveTempFiles tests the RemoveTempFiles function
 // GIVEN a call to RemoveTempFiles
@@ -38,4 +68,35 @@ func TestRemoveTempFiles(t *testing.T) {
 	}
 	// Verify error returned on invalid regex pattern
 	assert.Error(t, RemoveTempFiles(zap.S(), "["))
+}
+
+// TestFileExists tests the FileExists function
+// GIVEN a call to FileExists
+//
+//	WHEN with a valid file path
+//	THEN the file name is checked
+func TestFileExists(t *testing.T) {
+	tests := []struct {
+		name          string
+		createPattern string
+	}{
+		{name: "WithPrefixAndSuffix", createPattern: "test1-*.yaml"},
+		{name: "WithPrefixOnly", createPattern: "test2-*"},
+		{name: "WithSuffixOnly", createPattern: "*-test3"},
+		{name: "NoPrefixNorSuffix", createPattern: "test"},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			assert := assert.New(t)
+			tmpFile, err := CreateTempFile(test.createPattern, nil)
+			assert.NoErrorf(err, "Unable to create temp file %s for testing: %s", tmpFile, err)
+			exists, err := FileExists(tmpFile.Name())
+			assert.True(exists)
+			assert.NoError(err)
+			//Returns false when file does not exist
+			exists, err = FileExists("nofile")
+			assert.False(exists)
+			assert.NoError(err)
+		})
+	}
 }
