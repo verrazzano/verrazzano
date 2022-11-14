e625129e394fef8cdf0d114fdba5511fd67ebb71
Michael Cico <46466506+michael-cico@users.noreply.github.com>
2022-11-14 15:12:20 -0500

    skip-at Debug save psr tooling script

--- ci/scripts/save_psr_tooling.sh
+++ ci/scripts/save_psr_tooling.sh
@@ -4,6 +4,9 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 #
 
+set -o pipefail
+set -xv
+
 if [ -z "$1" ]; then
   echo "Branch name must be specified"
   exit 1
@@ -32,19 +35,19 @@ sha256sum psrctl-linux-amd64.tar.gz > psrctl-linux-amd64.tar.gz.sha256
 sha256sum psrctl-linux-arm64.tar.gz > psrctl-linux-arm64.tar.gz.sha256
 sha256sum psrctl-darwin-amd64.tar.gz > psrctl-darwin-amd64.tar.gz.sha256
 sha256sum psrctl-darwin-arm64.tar.gz > psrctl-darwin-arm64.tar.gz.sha256
-oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_BUCKET} --name ${CURRENT_BRANCH_NAME}/psrctl-linux-amd64.tar.gz --file psrctl-linux-amd64.tar.gz
-oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_BUCKET} --name ${CURRENT_BRANCH_NAME}/psrctl-linux-amd64.tar.gz.sha256 --file psrctl-linux-amd64.tar.gz.sha256
-oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_BUCKET} --name ${CURRENT_BRANCH_NAME}/psrctl-linux-arm64.tar.gz --file psrctl-linux-arm64.tar.gz
-oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_BUCKET} --name ${CURRENT_BRANCH_NAME}/psrctl-linux-arm64.tar.gz.sha256 --file psrctl-linux-arm64.tar.gz.sha256
-oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_BUCKET} --name ${CURRENT_BRANCH_NAME}/psrctl-darwin-amd64.tar.gz --file psrctl-darwin-amd64.tar.gz
-oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_BUCKET} --name ${CURRENT_BRANCH_NAME}/psrctl-darwin-amd64.tar.gz.sha256 --file psrctl-darwin-amd64.tar.gz.sha256
-oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_BUCKET} --name ${CURRENT_BRANCH_NAME}/psrctl-darwin-arm64.tar.gz --file psrctl-darwin-arm64.tar.gz
-oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_BUCKET} --name ${CURRENT_BRANCH_NAME}/psrctl-darwin-arm64.tar.gz.sha256 --file psrctl-darwin-arm64.tar.gz.sha256
-oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_COMMIT_BUCKET} --name ephemeral/${CURRENT_BRANCH_NAME}/${SHORT_COMMIT_HASH_ENV}/psrctl-linux-amd64.tar.gz --file psrctl-linux-amd64.tar.gz
-oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_COMMIT_BUCKET} --name ephemeral/${CURRENT_BRANCH_NAME}/${SHORT_COMMIT_HASH_ENV}/psrctl-linux-amd64.tar.gz.sha256 --file psrctl-linux-amd64.tar.gz.sha256
-oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_COMMIT_BUCKET} --name ephemeral/${CURRENT_BRANCH_NAME}/${SHORT_COMMIT_HASH_ENV}/psrctl-linux-arm64.tar.gz --file psrctl-linux-arm64.tar.gz
-oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_COMMIT_BUCKET} --name ephemeral/${CURRENT_BRANCH_NAME}/${SHORT_COMMIT_HASH_ENV}/psrctl-linux-arm64.tar.gz.sha256 --file psrctl-linux-arm64.tar.gz.sha256
-oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_COMMIT_BUCKET} --name ephemeral/${CURRENT_BRANCH_NAME}/${SHORT_COMMIT_HASH_ENV}/psrctl-darwin-amd64.tar.gz --file psrctl-darwin-amd64.tar.gz
-oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_COMMIT_BUCKET} --name ephemeral/${CURRENT_BRANCH_NAME}/${SHORT_COMMIT_HASH_ENV}/psrctl-darwin-amd64.tar.gz.sha256 --file psrctl-darwin-amd64.tar.gz.sha256
-oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_COMMIT_BUCKET} --name ephemeral/${CURRENT_BRANCH_NAME}/${SHORT_COMMIT_HASH_ENV}/psrctl-darwin-arm64.tar.gz --file psrctl-darwin-arm64.tar.gz
-oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_COMMIT_BUCKET} --name ephemeral/${CURRENT_BRANCH_NAME}/${SHORT_COMMIT_HASH_ENV}/psrctl-darwin-arm64.tar.gz.sha256 --file psrctl-darwin-arm64.tar.gz.sha256
+#oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_BUCKET} --name ${CURRENT_BRANCH_NAME}/psrctl-linux-amd64.tar.gz --file psrctl-linux-amd64.tar.gz
+#oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_BUCKET} --name ${CURRENT_BRANCH_NAME}/psrctl-linux-amd64.tar.gz.sha256 --file psrctl-linux-amd64.tar.gz.sha256
+#oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_BUCKET} --name ${CURRENT_BRANCH_NAME}/psrctl-linux-arm64.tar.gz --file psrctl-linux-arm64.tar.gz
+#oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_BUCKET} --name ${CURRENT_BRANCH_NAME}/psrctl-linux-arm64.tar.gz.sha256 --file psrctl-linux-arm64.tar.gz.sha256
+#oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_BUCKET} --name ${CURRENT_BRANCH_NAME}/psrctl-darwin-amd64.tar.gz --file psrctl-darwin-amd64.tar.gz
+#oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_BUCKET} --name ${CURRENT_BRANCH_NAME}/psrctl-darwin-amd64.tar.gz.sha256 --file psrctl-darwin-amd64.tar.gz.sha256
+#oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_BUCKET} --name ${CURRENT_BRANCH_NAME}/psrctl-darwin-arm64.tar.gz --file psrctl-darwin-arm64.tar.gz
+#oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_BUCKET} --name ${CURRENT_BRANCH_NAME}/psrctl-darwin-arm64.tar.gz.sha256 --file psrctl-darwin-arm64.tar.gz.sha256
+#oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_COMMIT_BUCKET} --name ephemeral/${CURRENT_BRANCH_NAME}/${SHORT_COMMIT_HASH_ENV}/psrctl-linux-amd64.tar.gz --file psrctl-linux-amd64.tar.gz
+#oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_COMMIT_BUCKET} --name ephemeral/${CURRENT_BRANCH_NAME}/${SHORT_COMMIT_HASH_ENV}/psrctl-linux-amd64.tar.gz.sha256 --file psrctl-linux-amd64.tar.gz.sha256
+#oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_COMMIT_BUCKET} --name ephemeral/${CURRENT_BRANCH_NAME}/${SHORT_COMMIT_HASH_ENV}/psrctl-linux-arm64.tar.gz --file psrctl-linux-arm64.tar.gz
+#oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_COMMIT_BUCKET} --name ephemeral/${CURRENT_BRANCH_NAME}/${SHORT_COMMIT_HASH_ENV}/psrctl-linux-arm64.tar.gz.sha256 --file psrctl-linux-arm64.tar.gz.sha256
+#oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_COMMIT_BUCKET} --name ephemeral/${CURRENT_BRANCH_NAME}/${SHORT_COMMIT_HASH_ENV}/psrctl-darwin-amd64.tar.gz --file psrctl-darwin-amd64.tar.gz
+#oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_COMMIT_BUCKET} --name ephemeral/${CURRENT_BRANCH_NAME}/${SHORT_COMMIT_HASH_ENV}/psrctl-darwin-amd64.tar.gz.sha256 --file psrctl-darwin-amd64.tar.gz.sha256
+#oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_COMMIT_BUCKET} --name ephemeral/${CURRENT_BRANCH_NAME}/${SHORT_COMMIT_HASH_ENV}/psrctl-darwin-arm64.tar.gz --file psrctl-darwin-arm64.tar.gz
+#oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_COMMIT_BUCKET} --name ephemeral/${CURRENT_BRANCH_NAME}/${SHORT_COMMIT_HASH_ENV}/psrctl-darwin-arm64.tar.gz.sha256 --file psrctl-darwin-arm64.tar.gz.sha256
