2c98d85d5644c89cd00bdc6b26b8c954ea822c30
Michael Cico <46466506+michael-cico@users.noreply.github.com>
2022-11-15 10:32:31 -0500

    VZ-7423.  Initial build pipeline for PSR, inject worker image (#4731)
    Initial Jenkins pipeline to build the psr-backend worker image and psrctl binaries and push them to Github/ObjectStore accordingly.
    - Also injects the built worker image as the default image into the psrctl binary for now; we will likely need to revisit this and how we expect people to use it.

--- tools/psr/psrctl/pkg/scenario/start.go
+++ tools/psr/psrctl/pkg/scenario/start.go
@@ -6,6 +6,7 @@ package scenario
 import (
 	"fmt"
 	helmcli "github.com/verrazzano/verrazzano/pkg/helm"
+	"github.com/verrazzano/verrazzano/tools/psr/psrctl/cmd/constants"
 	"k8s.io/apimachinery/pkg/types"
 	"os"
 	"path/filepath"
@@ -41,8 +42,10 @@ func (m Manager) StartScenario(scman *ScenarioManifest) (string, error) {
 	// Helm install each use case
 	var i int
 	for _, uc := range scman.Usecases {
-		helmOverrides := []helmcli.HelmOverrides{}
-
+		// Create the set of HelmOverrides, starting with the worker image
+		helmOverrides := []helmcli.HelmOverrides{
+			m.buildWorkerImageOverride(),
+		}
 		// This is the usecase path, E.G. manifests/usecases/opensearch/getlogs/getlogs.yaml
 		ucOverride := filepath.Join(m.Manifest.UseCasesAbsDir, uc.UsecasePath)
 		helmOverrides = append(helmOverrides, helmcli.HelmOverrides{FileOverride: ucOverride})
@@ -70,7 +73,7 @@ func (m Manager) StartScenario(scman *ScenarioManifest) (string, error) {
 			Namespace: m.Namespace,
 			Name:      relname,
 		})
-		i = i + 1
+		i++
 	}
 
 	// Save the scenario in a ConfigMap
@@ -101,4 +104,8 @@ func readWorkerType(ucOverride string) (string, error) {
 		return "nil", fmt.Errorf("Failed to find global.envVars.PSR_WORKER_TYPE in %s", ucOverride)
 	}
 	return wt.Global.EnvVars.WorkerType, nil
+}
+
+func (m Manager) buildWorkerImageOverride() helmcli.HelmOverrides {
+	return helmcli.HelmOverrides{SetOverrides: fmt.Sprintf("%s=%s", constants.ImageNameKey, m.WorkerImage)}
 }
