24c8895a2238d52ed1dce418f3f5d35a5f563990
Abraham Hernandez <abraham.hernandez@oracle.com>
2022-11-15 11:25:42 -0800

    Corrected Jenkinsfile PUT

--- build/coverage-number.sh
+++ build/coverage-number.sh
@@ -4,6 +4,6 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 #
 
-#Extracts the line-rate number from
+#Extracts the line-rate number from coverage.xml
 grep -i '<coverage' coverage.xml | awk -F' ' '{print $2}' | \
 sed -E 's/line-rate=\"(.*)\"/\1/' > unit-test-coverage-number.txt
