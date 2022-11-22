1f49675ef9f4b3392d4727de4568569d1116aba6
Asha Yarangatta <asha.yarangatta@oracle.com>
2022-11-22 10:48:35 +0530

    VZ-7478 Add prerequisite validator webhook (#4698)
    A webhook has been added to check the Verrazzano prerequisites and
    present an appropriate warning to the user while applying the
    Verrazzano custom resource.

---
+++ platform-operator/apis/verrazzano/webhooks/requirements_webhook_v1beta1.go
@@ -0,0 +1,69 @@
+// Copyright (c) 2022, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package webhooks
+
+import (
+	"context"
+	vzlog "github.com/verrazzano/verrazzano/pkg/log"
+	"github.com/verrazzano/verrazzano/pkg/vzchecks"
+	"github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1beta1"
+	"go.uber.org/zap"
+	k8sadmission "k8s.io/api/admission/v1"
+	"net/http"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+)
+
+// RequirementsValidatorV1beta1 is a struct holding objects used during validation.
+type RequirementsValidatorV1beta1 struct {
+	client  client.Client
+	decoder *admission.Decoder
+}
+
+// InjectClient injects the client.
+func (v *RequirementsValidatorV1beta1) InjectClient(c client.Client) error {
+	v.client = c
+	return nil
+}
+
+// InjectDecoder injects the decoder.
+func (v *RequirementsValidatorV1beta1) InjectDecoder(d *admission.Decoder) error {
+	v.decoder = d
+	return nil
+}
+
+// Handle performs validation of the Verrazzano prerequisites based on the profiled used.
+func (v *RequirementsValidatorV1beta1) Handle(ctx context.Context, req admission.Request) admission.Response {
+	var log = zap.S().With(vzlog.FieldResourceNamespace, req.Namespace, vzlog.FieldResourceName, req.Name, vzlog.FieldWebhook, RequirementsWebhook)
+
+	log.Infof("Processing Requirements validator webhook")
+	vz := &v1beta1.Verrazzano{}
+	err := v.decoder.Decode(req, vz)
+	if err != nil {
+		return admission.Errored(http.StatusBadRequest, err)
+	}
+
+	if vz.ObjectMeta.DeletionTimestamp.IsZero() {
+		switch req.Operation {
+		case k8sadmission.Create, k8sadmission.Update:
+			return validateRequirementsV1beta1(log, v.client, vz)
+		}
+	}
+	return admission.Allowed("")
+}
+
+// validateRequirementsV1alpha1 presents the user with a warning if the prerequisite checks are not met.
+func validateRequirementsV1beta1(log *zap.SugaredLogger, client client.Client, vz *v1beta1.Verrazzano) admission.Response {
+	response := admission.Allowed("")
+	if errs := vzchecks.PrerequisiteCheck(client, vzchecks.ProfileType(vz.Spec.Profile)); len(errs) > 0 {
+		var warnings []string
+		for _, err := range errs {
+			log.Warnf(err.Error())
+			warnings = append(warnings, err.Error())
+		}
+		return admission.Allowed("").WithWarnings(warnings...)
+	}
+
+	return response
+}
