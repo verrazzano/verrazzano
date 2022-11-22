9b93c212a42d95b04bb9f47914a5c265cfe7ab18
Fred Tibbitts <fred.tibbitts@oracle.com>
2022-11-22 12:14:31 -0500

    More unit tests

--- cluster-operator/controllers/rancher/rancher_cluster_controller_test.go
+++ cluster-operator/controllers/rancher/rancher_cluster_controller_test.go
@@ -48,6 +48,104 @@ func TestReconcileCreateVMC(t *testing.T) {
 	asserts.Equal(clusterName, vmc.Status.RancherRegistration.ClusterID)
 }
 
+// GIVEN a Rancher cluster resource is created with labels
+// AND   the user has specified a cluster selector that matches the labels
+// WHEN  the reconciler runs
+// THEN  a VMC is created and the cluster id is set in the status
+func TestReconcileWithClusterSelector(t *testing.T) {
+	asserts := assert.New(t)
+
+	const (
+		labelName  = "test-label"
+		labelValue = "test-label-value"
+	)
+
+	cluster := newCattleCluster(clusterName, displayName)
+	cluster.SetLabels(map[string]string{labelName: labelValue})
+	fakeClient := fake.NewClientBuilder().WithScheme(newScheme()).WithObjects(cluster).Build()
+
+	reconciler := newRancherClusterReconciler(fakeClient)
+	reconciler.ClusterSelector = &metav1.LabelSelector{
+		MatchExpressions: []metav1.LabelSelectorRequirement{
+			{
+				Key:      labelName,
+				Operator: metav1.LabelSelectorOpIn,
+				Values:   []string{labelValue},
+			},
+		},
+	}
+	request := newRequest(clusterName)
+
+	_, err := reconciler.Reconcile(context.TODO(), request)
+	asserts.NoError(err)
+
+	// expect that a VMC was created and that the cluster id is set in the status
+	vmc := &clustersv1alpha1.VerrazzanoManagedCluster{}
+	err = fakeClient.Get(context.TODO(), types.NamespacedName{Name: displayName, Namespace: vzconst.VerrazzanoMultiClusterNamespace}, vmc)
+	asserts.NoError(err)
+	asserts.Equal(clusterName, vmc.Status.RancherRegistration.ClusterID)
+}
+
+// GIVEN a Rancher cluster resource is created with labels
+// AND   the user has specified a cluster selector that DOES NOT match the labels
+// WHEN  the reconciler runs
+// THEN  a VMC is not created
+func TestReconcileClusterSelectorMismatch(t *testing.T) {
+	asserts := assert.New(t)
+
+	const (
+		labelName  = "test-label"
+		labelValue = "test-label-value"
+	)
+
+	cluster := newCattleCluster(clusterName, displayName)
+	cluster.SetLabels(map[string]string{"label": "does-not-match"})
+	fakeClient := fake.NewClientBuilder().WithScheme(newScheme()).WithObjects(cluster).Build()
+
+	reconciler := newRancherClusterReconciler(fakeClient)
+	reconciler.ClusterSelector = &metav1.LabelSelector{
+		MatchExpressions: []metav1.LabelSelectorRequirement{
+			{
+				Key:      labelName,
+				Operator: metav1.LabelSelectorOpIn,
+				Values:   []string{labelValue},
+			},
+		},
+	}
+	request := newRequest(clusterName)
+
+	_, err := reconciler.Reconcile(context.TODO(), request)
+	asserts.NoError(err)
+
+	// expect that a VMC was not created
+	vmc := &clustersv1alpha1.VerrazzanoManagedCluster{}
+	err = fakeClient.Get(context.TODO(), types.NamespacedName{Name: displayName, Namespace: vzconst.VerrazzanoMultiClusterNamespace}, vmc)
+	asserts.Error(err)
+	asserts.True(errors.IsNotFound(err))
+}
+
+// GIVEN a Rancher cluster resource is created
+// AND   Rancher cluster sync is disabled
+// WHEN  the reconciler runs
+// THEN  a VMC is not created
+func TestReconcileClusterSyncDisabled(t *testing.T) {
+	asserts := assert.New(t)
+
+	fakeClient := fake.NewClientBuilder().WithScheme(newScheme()).WithObjects(newCattleCluster(clusterName, displayName)).Build()
+	reconciler := newRancherClusterReconciler(fakeClient)
+	reconciler.ClusterSyncEnabled = false
+	request := newRequest(clusterName)
+
+	_, err := reconciler.Reconcile(context.TODO(), request)
+	asserts.NoError(err)
+
+	// expect that a VMC was not created
+	vmc := &clustersv1alpha1.VerrazzanoManagedCluster{}
+	err = fakeClient.Get(context.TODO(), types.NamespacedName{Name: displayName, Namespace: vzconst.VerrazzanoMultiClusterNamespace}, vmc)
+	asserts.Error(err)
+	asserts.True(errors.IsNotFound(err))
+}
+
 // GIVEN a Rancher cluster resource is created and a VMC already exists for the cluster
 // WHEN  the reconciler runs
 // THEN  the VMC is updated and the cluster id is set in the status
@@ -81,7 +179,10 @@ func TestReconcileDeleteVMC(t *testing.T) {
 	vmc := newVMC(displayName)
 	vmc.Status.RancherRegistration.ClusterID = clusterName
 	fakeClient := fake.NewClientBuilder().WithScheme(newScheme()).WithObjects(cluster, vmc).Build()
+
 	reconciler := newRancherClusterReconciler(fakeClient)
+	// disable cluster sync, the VMC (if it exists) should be deleted even if this flag is false
+	reconciler.ClusterSyncEnabled = false
 	request := newRequest(clusterName)
 
 	_, err := reconciler.Reconcile(context.TODO(), request)
