c25160d78ae8035bcda5077bd0055da6a09cc090
Abhishek Mitra <abhishek.a.mitra@oracle.com>
2022-11-22 09:08:38 +0530

    VZ-7793: Update resource deletion

--- tests/e2e/config/scripts/delete_oke_cluster.sh
+++ tests/e2e/config/scripts/delete_oke_cluster.sh
@@ -59,7 +59,10 @@ do
     export KUBECONFIG=${KUBECONFIG_DIR}/$i/kube_config
     export VERRAZZANO_KUBECONFIG=$KUBECONFIG
   fi
-  cleanup_vz_resources
+  #cleanup_vz_resources
+  resName=$(kubectl get vz -o jsonpath='{.items[*].metadata.name}')
+  echo "Deleting verrazzano resource ${resName}"
+  kubectl delete vz ${resName}
 done
 
 # delete the OKE clusters
