6c19bb9cf8e53a6289d505c865fd32d3dc6491b9
Ravi Singhal <ravi.r.singhal@oracle.com>
2022-11-24 17:49:07 +0530

    fix test

--- platform-operator/controllers/verrazzano/component/rancher/rancher_component_test.go
+++ platform-operator/controllers/verrazzano/component/rancher/rancher_component_test.go
@@ -1007,11 +1007,11 @@ func prepareContexts() (spi.ComponentContext, spi.ComponentContext) {
 	k8sutilfake.PodExecResult = func(url *url.URL) (string, string, error) {
 		var commands []string
 		if commands = url.Query()["command"]; len(commands) == 3 {
-			if strings.Contains(commands[2], fmt.Sprintf("cat %s", SettingUILogoDarkLogoFilePath)) {
+			if strings.Contains(commands[2], fmt.Sprintf("base64 %s", SettingUILogoDarkLogoFilePath)) {
 				return base64.StdEncoding.EncodeToString([]byte("<svg>dark</svg>")), "", nil
 			}
 
-			if strings.Contains(commands[2], fmt.Sprintf("cat %s", SettingUILogoLightLogoFilePath)) {
+			if strings.Contains(commands[2], fmt.Sprintf("base64 %s", SettingUILogoLightLogoFilePath)) {
 				return base64.StdEncoding.EncodeToString([]byte("<svg>light</svg>")), "", nil
 			}
 
