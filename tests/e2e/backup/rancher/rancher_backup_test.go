e6eb70c9cd58b622505a1af75e77fe1516cd30dc
Abhishek Mitra <abhishek.a.mitra@oracle.com>
2022-11-28 09:49:01 +0530

    VZ-7993: go fmt

--- tests/e2e/backup/rancher/rancher_backup_test.go
+++ tests/e2e/backup/rancher/rancher_backup_test.go
@@ -376,7 +376,7 @@ var _ = t.Describe("Rancher Backup and Restore,", Label("f:platform-verrazzano.r
 		})
 
 	})
-	
+
 	t.Context("Rancher restore", func() {
 		WhenRancherBackupInstalledIt("Start restore after rancher backup is completed", func() {
 			Eventually(func() error {
