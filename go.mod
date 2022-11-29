a04b231272f5f3094b06a07dc2fda3676788f0f0
karan k <karan.k@oracle.com>
2022-11-29 19:52:36 +0530

    update crd

--- go.mod
+++ go.mod
@@ -27,7 +27,7 @@ require (
 	github.com/spf13/cobra v1.5.0
 	github.com/spf13/pflag v1.0.5
 	github.com/stretchr/testify v1.7.2
-	github.com/verrazzano/verrazzano-monitoring-operator v0.0.29-0.20220810134448-c6e8df57abb2
+	github.com/verrazzano/verrazzano-monitoring-operator v0.0.30-0.20221129103401-55e03f06a419
 	go.uber.org/zap v1.21.0
 	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616
 	golang.org/x/text v0.3.7
--- platform-operator/controllers/verrazzano/component/opensearch/vmi.go
+++ platform-operator/controllers/verrazzano/component/opensearch/vmi.go
@@ -5,12 +5,13 @@ package opensearch
 
 import (
 	"fmt"
-	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/common"
 
 	vmov1 "github.com/verrazzano/verrazzano-monitoring-operator/pkg/apis/vmcontroller/v1"
-	vzapi "github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1alpha1"
-	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/spi"
 	"k8s.io/apimachinery/pkg/api/resource"
+
+	vzapi "github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1alpha1"
+	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/common"
+	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/spi"
 )
 
 const (
@@ -88,6 +89,9 @@ func newOpenSearch(effectiveCR, actualCR *vzapi.Verrazzano, storage *common.Reso
 	for _, policy := range opensearchComponent.Policies {
 		opensearch.Policies = append(opensearch.Policies, *policy.DeepCopy())
 	}
+
+	// Set the InstallPlugins to the VMI
+	opensearch.InstallPlugins = opensearchComponent.InstallPlugins
 
 	// Set the values in the OpenSearch object from the Verrazzano component InstallArgs
 	if err := populateOpenSearchFromInstallArgs(opensearch, opensearchComponent); err != nil {
