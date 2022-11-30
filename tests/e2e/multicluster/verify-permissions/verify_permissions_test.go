9ef7b380858157cb8ff3b83369a2e3add9a2ecd4
Fred Tibbitts <fred.tibbitts@oracle.com>
2022-11-30 10:49:13 -0500

    WIP tests

--- tests/e2e/multicluster/verify-permissions/verify_permissions_test.go
+++ tests/e2e/multicluster/verify-permissions/verify_permissions_test.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 
 	v1alpha12 "github.com/verrazzano/verrazzano/cluster-operator/apis/clusters/v1alpha1"
+	covmc "github.com/verrazzano/verrazzano/cluster-operator/controllers/vmc"
 	"github.com/verrazzano/verrazzano/tests/e2e/pkg/test/framework"
 	"github.com/verrazzano/verrazzano/tests/e2e/pkg/test/framework/metrics"
 
@@ -16,18 +17,22 @@ import (
 	"time"
 
 	"github.com/verrazzano/verrazzano/pkg/k8s/resource"
+	"github.com/verrazzano/verrazzano/pkg/log/vzlog"
 
 	oamv1alpha2 "github.com/crossplane/oam-kubernetes-runtime/apis/core/v1alpha2"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	clustersv1alpha1 "github.com/verrazzano/verrazzano/application-operator/apis/clusters/v1alpha1"
 	"github.com/verrazzano/verrazzano/application-operator/apis/oam/v1alpha1"
+	"github.com/verrazzano/verrazzano/pkg/constants"
 	"github.com/verrazzano/verrazzano/pkg/k8sutil"
 	"github.com/verrazzano/verrazzano/tests/e2e/pkg"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -42,6 +47,7 @@ const permissionTest2Namespace = "permissions-test2-ns"
 var managedClusterName = os.Getenv("MANAGED_CLUSTER_NAME")
 var adminKubeconfig = os.Getenv("ADMIN_KUBECONFIG")
 var managedKubeconfig = os.Getenv("MANAGED_KUBECONFIG")
+var rancherProxyKubeconfig string
 
 const vpTest1 = "permissions-test1"
 const vpTest2 = "permissions-test2"
@@ -51,6 +57,12 @@ var t = framework.NewTestFramework("permissions_test")
 var beforeSuite = t.BeforeSuiteFunc(func() {
 	// Do set up for multi cluster tests
 	deployTestResources()
+
+	Eventually(func() error {
+		var err error
+		rancherProxyKubeconfig, err = getUserKubeconfigForManagedCluster()
+		return err
+	}).WithPolling(pollingInterval).WithTimeout(time.Minute).ShouldNot(HaveOccurred())
 })
 
 var _ = BeforeSuite(beforeSuite)
@@ -379,6 +391,26 @@ var _ = t.Describe("Multi Cluster Verify Kubeconfig Permissions", Label("f:multi
 				}
 				return errors.IsForbidden(err), nil
 			}, waitTimeout, pollingInterval).Should(BeTrue(), "Expected to get a forbidden error")
+		})
+	})
+
+	t.When("the Rancher MC user accesses Kubernetes resources on the managed cluster", func() {
+		var clientset *kubernetes.Clientset
+		t.BeforeEach(func() {
+			var err error
+			clientset, err = pkg.GetKubernetesClientsetForCluster(rancherProxyKubeconfig)
+			Expect(err).ShouldNot(HaveOccurred())
+		})
+
+		t.It("should be able to list secrets", func() {
+			Eventually(func() (*v1.SecretList, error) {
+				return clientset.CoreV1().Secrets(constants.VerrazzanoSystemNamespace).List(context.TODO(), metav1.ListOptions{})
+			}).WithPolling(pollingInterval).WithTimeout(waitTimeout).ShouldNot(BeNil())
+		})
+
+		t.It("should not be able to list pods", func() {
+			_, err := clientset.CoreV1().Pods(constants.VerrazzanoSystemNamespace).List(context.TODO(), metav1.ListOptions{})
+			Expect(errors.IsForbidden(err)).To(BeTrue(), "Expected forbidden error", err)
 		})
 	})
 })
@@ -760,4 +792,49 @@ func isStatusAsExpected(status clustersv1alpha1.MultiClusterResourceStatus,
 		}
 	}
 	return matchingConditionCount >= 1 && matchingClusterStatusCount == 1
+}
+
+func getUserKubeconfigForManagedCluster() (string, error) {
+	// get the Rancher cluster id from the VMC status
+	client, err := pkg.GetClusterOperatorClientset(adminKubeconfig)
+	if err != nil {
+		return "", err
+	}
+	vmc, err := client.ClustersV1alpha1().VerrazzanoManagedClusters(constants.VerrazzanoMultiClusterNamespace).Get(context.TODO(), managedClusterName, metav1.GetOptions{})
+	if err != nil {
+		return "", err
+	}
+	if vmc.Status.RancherRegistration.ClusterID == "" {
+		return "", fmt.Errorf("Rancher status cluster id is empty")
+	}
+
+	// get the Verrazzano cluster user password from the secret and create a Rancher config with a bearer token for the user
+	secret, err := pkg.GetSecretInCluster(constants.VerrazzanoMultiClusterNamespace, "verrazzano-cluster-user", adminKubeconfig)
+	if err != nil {
+		return "", err
+	}
+	config, err := pkg.CreateNewRancherConfigForUser(t.Logs, adminKubeconfig, "verrazzanocluster", string(secret.Data["password"]))
+	if err != nil {
+		return "", err
+	}
+
+	// get the managed cluster kubeconfig configured for the user from Rancher
+	kubeconfig, err := covmc.GetClusterKubeconfig(config, vmc.Status.RancherRegistration.ClusterID, vzlog.DefaultLogger())
+	Expect(err).ShouldNot(HaveOccurred())
+	if err != nil {
+		return "", err
+	}
+
+	// write the kubeconfig contents to a temp file
+	tmpFile, err := os.CreateTemp("", "")
+	if err != nil {
+		return "", err
+	}
+	defer tmpFile.Close()
+	_, err = tmpFile.WriteString(kubeconfig)
+	if err != nil {
+		return "", err
+	}
+
+	return tmpFile.Name(), nil
 }
