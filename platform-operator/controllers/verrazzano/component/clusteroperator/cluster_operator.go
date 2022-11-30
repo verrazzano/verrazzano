0a7f3360a1e252ad8bb1d4fbbf153567be5ba86f
davisgomes <49332517+davisgomes@users.noreply.github.com>
2022-11-30 12:15:09 -0800

    VZ-7503 add authorization header to the post request

--- platform-operator/controllers/verrazzano/component/clusteroperator/cluster_operator.go
+++ platform-operator/controllers/verrazzano/component/clusteroperator/cluster_operator.go
@@ -131,7 +131,7 @@ func createVZClusterUser(ctx spi.ComponentContext) error {
 	}
 	reqURL = rc.BaseURL + usersPath
 	payload := fmt.Sprintf(userPayload, vzconst.VerrazzanoClusterRancherName, pass, vzconst.VerrazzanoClusterRancherName)
-	response, _, err = rancherutil.SendRequest(http.MethodPost, reqURL, map[string]string{}, payload, rc, ctx.Log())
+	response, _, err = rancherutil.SendRequest(http.MethodPost, reqURL, headers, payload, rc, ctx.Log())
 	if err != nil {
 		return ctx.Log().ErrorfNewErr("Failed to create the Verrazzano cluster user in Rancher: %v", err)
 	}
