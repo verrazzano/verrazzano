3d211147e41059dbae442581191f0a7179c92300
davisgomes <49332517+davisgomes@users.noreply.github.com>
2022-12-06 12:17:25 -0500

    Populate wait4webhook script

--- cluster-operator/scripts/wait4webhook.sh
+++ cluster-operator/scripts/wait4webhook.sh
@@ -2,4 +2,6 @@
 # Copyright (c) 2022, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 # While loop for verrazzano-cluster-operator to wait for webhooks to be started before starting up
-while [[ "$(curl --insecure -s -o /dev/null -w '%{http_code}' https://verrazzano-cluster-operator-webhook:443//validate-clusters-verrazzano-io-v1alpha1-verrazzanomanagedcluster -H 'Content-Type: application/json')" != "200" ]]; do sleep 5; done
+while [[ "$(curl --insecure -s -o /dev/null -w '%{http_code}' https://verrazzano-cluster-operator-webhook:443//validate-clusters-verrazzano-io-v1alpha1-verrazzanomanagedcluster:q
+ :q
+ -H 'Content-Type: application/json')" != "200" ]]; do sleep 5; done
