c79952d8d89105d7a59cb4e7f44bcd50c436bfd7
stshapir <steven.shapiro@oracle.com>
2022-12-06 10:44:39 -0500

    test verbose

---
+++ tools/psr/psrctl/cmd/explain/explain_test.go
@@ -0,0 +1,21 @@
+package explain
+
+import (
+	"testing"
+)
+
+// TestExplainScenario tests the NewCmdExplain and RunCmdExplain functions
+//
+//	WHEN 'psr explain -s [scenario]' is called
+//	THEN ensure the output is correct for that scenario
+func TestExplainScenario(t *testing.T) {
+
+}
+
+// TestExplainNoScenario tests the NewCmdExplain and RunCmdExplain functions
+//
+//	WHEN 'psr explain' is called
+//	THEN ensure the output is correct for all listed scenarios
+func TestExplainNoScenario(t *testing.T) {
+
+}
