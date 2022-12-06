0558046f804490960e2c672b1c90a46950dfdf39
Robert Donat <robert.donat@oracle.com>
2022-12-06 16:35:30 +0000

    cleanup

--- tests/e2e/uninstall/verifyrancher/verify_rancher_crs_test.go
+++ tests/e2e/uninstall/verifyrancher/verify_rancher_crs_test.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"fmt"
 	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 	"github.com/verrazzano/verrazzano/pkg/k8sutil"
 	"github.com/verrazzano/verrazzano/tests/e2e/pkg"
 	"github.com/verrazzano/verrazzano/tests/e2e/pkg/test/framework"
@@ -19,48 +20,50 @@ import (
 var t = framework.NewTestFramework("uninstall verify Rancher CRs")
 
 // This test verifies that Rancher cluster scoped custom resources have been deleted from the cluster.
-var _ = t.Describe("Verify Rancher cluster scoped resources deleted after uninstall.", Label("f:platform-lcm.unnstall"), func() {
-	crds, err := pkg.ListCRDs()
-	if err != nil {
-		Fail(err.Error())
-	}
+var _ = t.Describe("Verify Rancher cluster scoped resources", Label("f:platform-lcm.unnstall"), func() {
+	t.It("Check for unexpected Rancher custom resources", func() {
+		crds, err := pkg.ListCRDs()
+		if err != nil {
+			Expect(err).To(Not(HaveOccurred()))
+		}
 
-	kubeconfigPath, err := k8sutil.GetKubeConfigLocation()
-	if err != nil {
-		Fail(fmt.Errorf("Failed getting kubeconfig: %s", err).Error())
-	}
+		kubeconfigPath, err := k8sutil.GetKubeConfigLocation()
+		if err != nil {
+			Expect(err).To(Not(HaveOccurred()), "Failed getting kubeconfig")
+		}
 
-	// Get the Dynamic clientset
-	clientset, err := pkg.GetDynamicClientInCluster(kubeconfigPath)
-	if err != nil {
-		Fail(fmt.Errorf("Failed getting Kubernetes clientset: %s", err).Error())
-	}
+		// Get the Dynamic clientset
+		clientset, err := pkg.GetDynamicClientInCluster(kubeconfigPath)
+		if err != nil {
+			Expect(err).To(Not(HaveOccurred()), "Failed getting Kubernetes clientset")
+		}
 
-	unexpectedCRs := false
+		unexpectedCRs := false
 
-	for _, crd := range crds.Items {
-		if strings.HasSuffix(crd.Name, ".cattle.io") && crd.Spec.Scope == v1.ClusterScoped {
-			for _, version := range crd.Spec.Versions {
-				rancherCRs, err := clientset.Resource(schema.GroupVersionResource{
-					Group:    crd.Spec.Group,
-					Version:  version.Name,
-					Resource: crd.Spec.Names.Plural,
-				}).List(context.TODO(), metav1.ListOptions{})
-				if err != nil {
-					Fail(fmt.Errorf("Failed listing custom resources: %s", err).Error())
-				}
-				if len(rancherCRs.Items) == 0 {
-					continue
-				}
-				for _, rancherCR := range rancherCRs.Items {
-					pkg.Log(pkg.Error, fmt.Sprintf("Unexpected custom resource %s was found for %s/%s", rancherCR.GetName(), crd.Spec.Group, version.Name))
-					unexpectedCRs = true
+		for _, crd := range crds.Items {
+			if strings.HasSuffix(crd.Name, ".cattle.io") && crd.Spec.Scope == v1.ClusterScoped {
+				for _, version := range crd.Spec.Versions {
+					rancherCRs, err := clientset.Resource(schema.GroupVersionResource{
+						Group:    crd.Spec.Group,
+						Version:  version.Name,
+						Resource: crd.Spec.Names.Plural,
+					}).List(context.TODO(), metav1.ListOptions{})
+					if err != nil {
+						Expect(err).To(Not(HaveOccurred()), fmt.Sprintf("Failed listing custom resources for %s", crd.Spec.Names.Kind))
+					}
+					if len(rancherCRs.Items) == 0 {
+						continue
+					}
+					for _, rancherCR := range rancherCRs.Items {
+						pkg.Log(pkg.Error, fmt.Sprintf("Unexpected custom resource %s was found for %s.%s/%s", rancherCR.GetName(), crd.Spec.Names.Singular, crd.Spec.Group, version.Name))
+						unexpectedCRs = true
+					}
 				}
 			}
 		}
-	}
 
-	if unexpectedCRs {
-		Fail("Failed to verify Rancher custom resources")
-	}
+		if unexpectedCRs {
+			Fail("Failed to verify Rancher custom resources")
+		}
+	})
 })
