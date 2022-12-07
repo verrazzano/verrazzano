a6468bb0f545fe1790a1d39865eb8429ac8bdb25
Michael Cico <46466506+michael-cico@users.noreply.github.com>
2022-12-07 01:14:14 -0500

    Enable parallel test execution; fix scenario start/stop utils for race conditions

--- tools/psr/tests/scenarios/common/scenario_common.go
+++ tools/psr/tests/scenarios/common/scenario_common.go
@@ -4,12 +4,14 @@
 package common
 
 import (
+	"fmt"
 	"github.com/onsi/gomega"
 	"github.com/verrazzano/verrazzano/pkg/log/vzlog"
 	"github.com/verrazzano/verrazzano/tests/e2e/pkg"
 	"github.com/verrazzano/verrazzano/tests/e2e/pkg/test/framework"
 	"github.com/verrazzano/verrazzano/tools/psr/tests/pkg/psrctlcli"
 	"github.com/verrazzano/verrazzano/tools/psr/tests/pkg/secrets"
+	"strings"
 )
 
 var (
@@ -33,11 +35,19 @@ func InitScenario(t *framework.TestFramework, log vzlog.VerrazzanoLogger, scenar
 		return
 	}
 
-	if !psrctlcli.IsScenarioRunning(log, scenarioID, namespace) {
-		stdout, stderr, err := psrctlcli.StartScenario(log, scenarioID, namespace)
-		t.Logs.Infof("StartScenario %s/%s, stdout: %s, stderr: %s", namespace, scenarioID, stdout, stderr)
-		gomega.Expect(err).To(gomega.Not(gomega.HaveOccurred()))
+	if psrctlcli.IsScenarioRunning(log, scenarioID, namespace) {
+		t.Logs.Infof("Scenario %s/%s is already running", namespace, scenarioID)
+		return
 	}
+	stdout, stderr, err := psrctlcli.StartScenario(log, scenarioID, namespace)
+	if err != nil {
+		// When running in parallel, the command may fail due to a race condition
+		if !strings.Contains(string(stderr), "release: already exists") {
+			t.Fail(fmt.Sprintf("Unexpected error starting scenario: %s", string(stderr)))
+			return
+		}
+	}
+	t.Logs.Infof("StartScenario %s/%s successful, stdout: %s, stderr: %s", namespace, scenarioID, stdout, stderr)
 }
 
 // StopScenario - Stops a scenario if it is running in the specified namespace; if skipStopScenario is true, we don't shut it down
@@ -46,9 +56,19 @@ func StopScenario(t *framework.TestFramework, log vzlog.VerrazzanoLogger, scenar
 		t.Logs.Info("Skip stop scenario")
 		return
 	}
-	if psrctlcli.IsScenarioRunning(log, scenarioID, namespace) {
-		stdout, stderr, err := psrctlcli.StopScenario(log, scenarioID, namespace)
-		t.Logs.Infof("StopScenario %s/%s, stdout: %s, stderr: %s", namespace, scenarioID, stdout, stderr)
-		gomega.Expect(err).To(gomega.Not(gomega.HaveOccurred()))
+	if !psrctlcli.IsScenarioRunning(log, scenarioID, namespace) {
+		t.Logs.Infof("Scenario %s/%s is not running", namespace, scenarioID)
+		return
 	}
+	stdout, stderr, err := psrctlcli.StopScenario(log, scenarioID, namespace)
+	if err != nil {
+		stderrString := string(stderr)
+		if !strings.Contains(stderrString, "Failed to purge the release: release: not found") &&
+			!strings.Contains(stderrString, "Failed to find ConfigMap for scenario") &&
+			!strings.Contains(stderrString, "Failed to purge the release: secrets") {
+			t.Fail(fmt.Sprintf("Unexpected error starting scenario: %s", stderrString))
+			return
+		}
+	}
+	t.Logs.Infof("StopScenario %s/%s succeeded, stdout: %s, stderr: %s", namespace, scenarioID, stdout, stderr)
 }
