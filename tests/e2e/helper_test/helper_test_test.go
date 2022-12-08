614f1e4f20db381af1f493b0ea686bf201dae6de
abnadago <abijitha.nadagouda@oracle.com>
2022-12-08 01:57:15 -0800

    test

--- tests/e2e/helper_test/helper_test_test.go
+++ tests/e2e/helper_test/helper_test_test.go
@@ -2,9 +2,6 @@ package helper_test_test
 
 import (
 	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
-
-	"github.com/verrazzano/verrazzano/tests/e2e/helper_test"
 )
 
 var _ = Describe("HelperTest", func() {
