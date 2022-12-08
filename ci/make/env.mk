7eb84d63767374c6a20402f3f13742edc18c97ef
Michael Cico <46466506+michael-cico@users.noreply.github.com>
2022-12-08 08:12:03 -0500

    Fix includes, and fix edge case in install script

--- ci/make/env.mk
+++ ci/make/env.mk
@@ -7,6 +7,7 @@ export GO_REPO_PATH ?= ${GOPATH}/src/github.com/verrazzano
 export VZ_ROOT := ${GO_REPO_PATH}/verrazzano
 export CI_ROOT ?= ${VZ_ROOT}/ci
 export CI_SCRIPTS_DIR ?= ${CI_ROOT}/scripts
+export TEST_SCRIPTS_DIR ?= ${VZ_ROOT}/tests/e2e/config/scripts
 
 export WORKSPACE ?= ${HOME}/verrazzano-workspace
 export DUMP_ROOT_DIRECTORY ?= ${WORKSPACE}/cluster-snapshots
