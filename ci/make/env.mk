8b409cd4874c601a7280833e43851b52781fe288
Michael Cico <46466506+michael-cico@users.noreply.github.com>
2022-12-07 22:29:09 -0500

    Fix test reports bug and install target regression - clean up make files a bit

--- ci/make/env.mk
+++ ci/make/env.mk
@@ -9,15 +9,6 @@ export CI_ROOT ?= ${VZ_ROOT}/ci
 export CI_SCRIPTS_DIR ?= ${CI_ROOT}/scripts
 
 export WORKSPACE ?= ${HOME}/verrazzano-workspace
+export DUMP_ROOT_DIRECTORY ?= ${WORKSPACE}/cluster-snapshots
 export KUBECONFIG ?= ${WORKSPACE}/test_kubeconfig
 
-export IMAGE_PULL_SECRET ?= verrazzano-container-registry
-export DOCKER_REPO ?= 'ghcr.io'
-export DOCKER_NAMESPACE ?= 'verrazzano'
-export TEST_ROOT ?= ${VZ_ROOT}/tests/e2e
-export TEST_SCRIPTS_DIR ?= ${TEST_ROOT}/config/scripts
-export DUMP_ROOT_DIRECTORY ?= ${WORKSPACE}/cluster-snapshots
-
-export OCI_OS_ARTIFACT_BUCKET=build-failure-artifacts
-export OCI_OS_COMMIT_BUCKET=verrazzano-builds-by-commit
-export OCI_CLI_PROFILE ?= DEFAULT
