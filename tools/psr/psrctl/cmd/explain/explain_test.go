54349d444c3033cdee3f2b0c75db37b34c756392
stshapir <steven.shapiro@oracle.com>
2022-12-08 10:15:48 -0500

    verbose

--- tools/psr/psrctl/cmd/explain/explain_test.go
+++ tools/psr/psrctl/cmd/explain/explain_test.go
@@ -5,7 +5,6 @@ import (
 	"encoding/base64"
 	"fmt"
 	"os"
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -23,27 +22,12 @@ import (
 
 const psrRoot = "../../.."
 
-// TestExplainScenario tests the NewCmdExplain and RunCmdExplain functions
-//
-//	WHEN 'psr explain -s ops-s1' is called
-//	THEN ensure the output is correct for that scenario
-func TestExplainScenario(t *testing.T) {
-
-	expectedName := "opensearch-s1"
-	expectedId := "ops-s1"
-	expectedDescription := "This is a scenario that writes logs to STDOUT and gets logs from OpenSearch at a moderated rate."
-
-	manifest.Manifests = &manifest.PsrManifests{
-		RootTmpDir:        psrRoot,
-		WorkerChartAbsDir: psrRoot + "/manifests/charts/worker",
-		UseCasesAbsDir:    psrRoot + "/manifests/usecases",
-		ScenarioAbsDir:    psrRoot + "/manifests/scenarios",
-	}
-
-	defer manifest.ResetManifests()
-
-	// create scenario ConfigMap
-	cm := &corev1.ConfigMap{
+var (
+	expectedName        = "opensearch-s1"
+	expectedId          = "ops-s1"
+	expectedDescription = "This is a scenario that writes logs to STDOUT and gets logs from OpenSearch at a moderated rate."
+	// ConfigMap for the ops-s1 scenario
+	cm = &corev1.ConfigMap{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "psr-ops-s1",
 			Namespace: "psr",
@@ -73,6 +57,21 @@ Usecases:
 `)),
 		},
 	}
+)
+
+// TestExplainScenario tests the NewCmdExplain and RunCmdExplain functions
+//
+//	WHEN 'psr explain -s ops-s1' is called
+//	THEN ensure the output is correct for that scenario
+func TestExplainScenario(t *testing.T) {
+	manifest.Manifests = &manifest.PsrManifests{
+		RootTmpDir:        psrRoot,
+		WorkerChartAbsDir: psrRoot + "/manifests/charts/worker",
+		UseCasesAbsDir:    psrRoot + "/manifests/usecases",
+		ScenarioAbsDir:    psrRoot + "/manifests/scenarios",
+	}
+
+	defer manifest.ResetManifests()
 
 	defer func() { k8sutil.GetCoreV1Func = k8sutil.GetCoreV1Client }()
 	k8sutil.GetCoreV1Func = func(log ...vzlog.VerrazzanoLogger) (corev1cli.CoreV1Interface, error) {
@@ -103,28 +102,38 @@ Usecases:
 //	WHEN 'psr explain -s ops-s2 -v' is called
 //	THEN ensure the output is correct for that scenario
 func TestExplainScenarioVerbose(t *testing.T) {
-	expectedName := "opensearch-s2"
-	expectedId := "ops-s2"
-	expectedDescription := "This is a scenario that writes logs to STDOUT and gets logs from OpenSearch at a moderated rate."
+	manifest.Manifests = &manifest.PsrManifests{
+		RootTmpDir:        psrRoot,
+		WorkerChartAbsDir: psrRoot + "/manifests/charts/worker",
+		UseCasesAbsDir:    psrRoot + "/manifests/usecases",
+		ScenarioAbsDir:    psrRoot + "/manifests/scenarios",
+	}
+
+	defer manifest.ResetManifests()
+
+	defer func() { k8sutil.GetCoreV1Func = k8sutil.GetCoreV1Client }()
+	k8sutil.GetCoreV1Func = func(log ...vzlog.VerrazzanoLogger) (corev1cli.CoreV1Interface, error) {
+		return k8sfake.NewSimpleClientset(cm).CoreV1(), nil
+	}
 
 	// Send the command output to a byte buffer
 	buf := new(bytes.Buffer)
 	errBuf := new(bytes.Buffer)
 	rc := helpers.NewFakeRootCmdContext(genericclioptions.IOStreams{In: os.Stdin, Out: buf, ErrOut: errBuf})
+
 	explainCmd := NewCmdExplain(rc)
 	explainCmd.PersistentFlags().Set(constants.FlagScenario, "ops-s1")
+	explainCmd.PersistentFlags().Set(constants.FlagNamespace, "psr")
+	explainCmd.PersistentFlags().Set(constants.FlagVerbose, "true")
 	assert.NotNil(t, explainCmd)
 
-	// Run version command, check for the expected status results to be displayed
+	// Run explain command, check for the expected status results to be displayed
 	err := explainCmd.Execute()
 	assert.NoError(t, err)
 	result := buf.String()
-	results := strings.Split(result, "\n")
-	actualName, actualId, actualDescription, useCases := results[3], results[4], results[5], results[6]
-	assert.Contains(t, fmt.Sprintf("Name: %s", expectedName), actualName)
-	assert.Contains(t, fmt.Sprintf("ID: %s", expectedId), actualId)
-	assert.Contains(t, fmt.Sprintf("Description: %s", expectedDescription), actualDescription)
-	assert.Contains(t, "Use cases:", useCases)
+	assert.Contains(t, fmt.Sprintf("Name: %s", expectedName), result)
+	assert.Contains(t, fmt.Sprintf("ID: %s", expectedId), result)
+	assert.Contains(t, fmt.Sprintf("Description: %s", expectedDescription), result)
 }
 
 // TestExplainNoScenario tests the NewCmdExplain and RunCmdExplain functions
