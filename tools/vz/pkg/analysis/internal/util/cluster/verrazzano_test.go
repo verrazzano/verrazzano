944c491e25d2982521b3256fe47aa6c72a81e8fb
Vicky Sarkar <vicky.sarkar@oracle.com>
2022-12-15 14:38:40 +0530

    vz analyze code coverage improvisations : t2

--- tools/vz/pkg/analysis/internal/util/cluster/verrazzano_test.go
+++ tools/vz/pkg/analysis/internal/util/cluster/verrazzano_test.go
@@ -2,4 +2,20 @@
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 package cluster
 
-// TODO: Add tests
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/verrazzano/verrazzano/tools/vz/pkg/analysis/internal/util/log"
+	"github.com/verrazzano/verrazzano/tools/vz/pkg/analysis/internal/util/report"
+	"testing"
+)
+
+// Test Verrazzano Installation Status with a couple of cluster root
+// Expect No Error for each installation status
+func TestInstallationStatus(t *testing.T) {
+	logger := log.GetDebugEnabledLogger()
+	var issueReporter = report.IssueReporter{
+		PendingIssues: make(map[string]report.Issue),
+	}
+	assert.NoError(t, installationStatus(logger, "../../../test/cluster/problem-pods-install/cluster-snapshot", &issueReporter))
+	assert.NoError(t, installationStatus(logger, "../../../test/cluster/pending-pods/cluster-snapshot", &issueReporter))
+}
