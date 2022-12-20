16a7b2c3be8cdff05aa0a1ee38de9230049511af
Isha Girdhar <isha.girdhar@oracle.com>
2022-12-20 14:29:01 +0530

    Fix test

--- tests/e2e/workloads/weblogic/weblogic_workload_test.go
+++ tests/e2e/workloads/weblogic/weblogic_workload_test.go
@@ -244,7 +244,7 @@ var _ = t.Describe("Validate deployment of VerrazzanoWebLogicWorkload", Label("f
 			pkg.Concurrently(
 				func() {
 					Eventually(func() bool {
-						var scrapePools = []string{"weblogic-workload"}
+						var scrapePools = []string{namespace}
 						return pkg.ScrapeTargetsHealthy(scrapePools)
 					}, shortWaitTimeout, shortPollingInterval).Should(BeTrue())
 				},
