4fd8ebecd57bd9bd488bf3a23360b24789361a83
Kate Nelson <kate.s.nelson@oracle.com>
2023-01-03 12:25:17 -0800

    update metrics test

--- tests/e2e/metrics/syscomponents/metrics_test.go
+++ tests/e2e/metrics/syscomponents/metrics_test.go
@@ -168,10 +168,10 @@ var _ = t.Describe("Prometheus Metrics", Label("f:observability.monitoring.prom"
 			eventuallyMetricsContainLabels("vz_platform_operator_error_reconcile_total", map[string]string{})
 		})
 		t.ItMinimumVersion("Verify VPO install metrics can be queried from Prometheus", metricsVersion, kubeConfig, func() {
-			eventuallyMetricsContainLabels("vz_ingressNGINX_install_duration_seconds", map[string]string{})
+			eventuallyMetricsContainLabels("vz_platform_operator_component_install_duration_seconds", map[string]string{})
 		})
 		t.ItMinimumVersion("Verify VPO upgrade counter metrics can be queried from Prometheus", metricsVersion, kubeConfig, func() {
-			eventuallyMetricsContainLabels("vz_ingressNGINX_upgrade_duration_seconds", map[string]string{})
+			eventuallyMetricsContainLabels("vz_platform_operator_component_upgrade_duration_seconds", map[string]string{})
 		})
 
 		t.ItMinimumVersion("Verify VMO function metrics can be queried from Prometheus", metricsVersion, kubeConfig, func() {
