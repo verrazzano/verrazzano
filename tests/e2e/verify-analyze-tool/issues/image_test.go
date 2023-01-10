f8205f122fcaae40ada7ed41e9b7822dbc7288e4
Vicky Sarkar <vicky.sarkar@oracle.com>
2023-01-11 00:24:41 +0530

    changes to patch go client

--- tests/e2e/verify-analyze-tool/issues/image_test.go
+++ tests/e2e/verify-analyze-tool/issues/image_test.go
@@ -5,13 +5,11 @@ package issues
 
 import (
 	"context"
-	"fmt"
 	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 	k8util "github.com/verrazzano/verrazzano/pkg/k8sutil"
 	"github.com/verrazzano/verrazzano/tests/e2e/pkg/test/framework"
-	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/types"
 	"os/exec"
 	"strings"
 	"time"
@@ -23,80 +21,63 @@ var (
 )
 
 const (
-	ImagePullNotFound string = "ImagePullNotFound"
-	ImagePullBackOff  string = "ImagePullBackOff"
+	//ImagePullNotFound string = "ImagePullNotFound"
+	ImagePullBackOff string = "ImagePullBackOff"
 )
 
 var t = framework.NewTestFramework("Vz Tools Analysis Image Issues")
 var _ = BeforeSuite(beforeSuite)
 var _ = t.AfterEach(func() {})
-var clusterImageIssues = make(map[string]bool)
-var clusterPodStatus = make(map[string][]corev1.Pod)
 
 var beforeSuite = t.BeforeSuiteFunc(func() {
 })
 
 var _ = t.Describe("VZ Tools", Label("f:vz-tools-image-issues"), func() {
 	t.Context("During Analysis", func() {
-		out, err := RunVzAnalyze()
-		if err != nil {
-			Fail(err.Error())
-		}
-		fmt.Println("11111111 \n", out)
-		/*
-		out, err = InjectIssues()
-		if err != nil {
-			Fail(err.Error())
-		}
-		fmt.Println("22222222 \n", out)
-		out, err = RunVzAnalyze()
-		if err != nil {
-			Fail(err.Error())
-		}
-		fmt.Println("33333333 \n", out)
-		t.It("Doesn't Have Image Pull Back Off Issue", func() {
-			Eventually(func() bool {
-				return testIssues(out,"ImagePullBackOff")
-			}, waitTimeout, pollingInterval).Should(BeTrue())
-		})
-		out, err = RevertIssues()
-		if err != nil {
-			Fail(err.Error())
-		}
-		fmt.Println("44444444 \n", out)
-		*/
 		c, err := k8util.GetKubernetesClientset()
 		if err != nil {
 			Fail(err.Error())
 		}
-		//deploymentsClient := c.ExtensionsV1beta1().Deployments("verrazzano-system")
-		patch := []byte(`{"spec":{"template":{"spec":{"containers":[{"image":"ghcr.io/oracle/coherence-operator:3.YY","name":"coherence-operator"}]}}}}`)
-		res, err := c.AppsV1().Deployments("verrazzano-system").Patch(context.TODO(),"coherence-operator", types.MergePatchType, patch,  v1.PatchOptions{}, "")
-		if err != nil {
-			fmt.Println(err)
+		//patch := []byte(`{"spec":{"template":{"spec":{"containers":[{"image":"ghcr.io/oracle/coherence-operator:3.YY","name":"coherence-operator"}]}}}}`)
+		deploymentsClient := c.AppsV1().Deployments("verrazzano-system")
+		result, getErr := deploymentsClient.Get(context.TODO(), "verrazzano-console", v1.GetOptions{})
+		if getErr != nil {
+			Fail(err.Error())
 		}
-		fmt.Println(res)
+		//fmt.Println(result.Spec.Template.Spec.Containers[0].Image)
+		for _, i := range result.Spec.Template.Spec.Containers {
+			if i.Name == "verrazzano-console" {
+				i.Image = "ghcr.io/verrazzano/console:v1.5.X-20221118195745-5347193"
+			}
+		}
+		_, updateErr := deploymentsClient.Update(context.TODO(), result, v1.UpdateOptions{})
+		Fail(updateErr.Error())
+		out, err := RunVzAnalyze()
+		if err != nil {
+			Fail(err.Error())
+		}
+		Eventually(func() bool {
+			return testIssues(out, ImagePullBackOff)
+		}, waitTimeout, pollingInterval).Should(BeTrue())
+
+		time.Sleep(time.Second * waitTimeout)
+
+		for _, i := range result.Spec.Template.Spec.Containers {
+			if i.Name == "verrazzano-console" {
+				i.Image = "ghcr.io/verrazzano/console:v1.5.0-20221118195745-5347193"
+			}
+		}
+		_, updateErr = deploymentsClient.Update(context.TODO(), result, v1.UpdateOptions{})
+		Fail(updateErr.Error())
+		out, err = RunVzAnalyze()
+		if err != nil {
+			Fail(err.Error())
+		}
+		Eventually(func() bool {
+			return testIssues(out, ImagePullBackOff)
+		}, waitTimeout, pollingInterval).Should(BeFalse())
 	})
 })
-
-func InjectIssues() (string, error) {
-	cmd := exec.Command("kubectl", "apply", "-f", "issue.yaml")
-	out, err := cmd.Output()
-	return string(out), err
-}
-
-func RevertIssues() (string, error) {
-	cmd := exec.Command("kubectl", "apply", "-f", "revertIssues.yaml")
-	out, err := cmd.Output()
-	return string(out), err
-
-}
-func testIssues(out, issueType string) bool {
-	if strings.Contains(out, issueType) {
-		return true
-	}
-	return false
-}
 
 func RunVzAnalyze() (string, error) {
 	cmd := exec.Command("vz", "analyze")
@@ -104,3 +85,9 @@ func RunVzAnalyze() (string, error) {
 	return string(out), err
 }
 
+func testIssues(out, issueType string) bool {
+	if strings.Contains(out, issueType) {
+		return true
+	}
+	return false
+}
