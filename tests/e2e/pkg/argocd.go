e6d7b51516d2d662ee924c36995b0e2446ad83db
Preethi Gnanesh <preethi.gnanesh@oracle.com>
2023-01-10 15:29:20 -0600

    Added nosec for ignoring golangcli-lint

--- tests/e2e/pkg/argocd.go
+++ tests/e2e/pkg/argocd.go
@@ -150,6 +150,7 @@ func GetApplicationsWithClient(log *zap.SugaredLogger, argoCDURL string, token s
 	client := &http.Client{}
 	var bearer = "Bearer " + token
 
+	//nosec
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 	req, err := http.NewRequest("GET", argoCDLoginURL, nil)
 	if err != nil {
