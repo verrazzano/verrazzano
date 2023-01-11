cd59b0a6fb4702ce619dc0978870bc562aa756a0
Abraham Hernandez <abraham.hernandez@oracle.com>
2023-01-11 15:11:41 -0800

    Update validate_ocr_images.sh

--- release/scripts/validate_ocr_images.sh
+++ release/scripts/validate_ocr_images.sh
@@ -9,12 +9,14 @@ export DOCKER_CLI_EXPERIMENTAL=enabled
 VZ_IMAGE_TXT=verrazzano_images.txt
 IMAGES=$(cat "$VZ_IMAGE_TXT")
 
-for image in "$IMAGES"; do
-    IMAGE_NAME_AND_TAG=$(echo "$image" | awk -F '[/]' '{print $2}')
-    # docker manifest inspect "$IMAGE_NAME_AND_TAG"
-done
-
-echo Running manifest inspect . . .
+echo Logging into Docker ...
 echo "$OCR_CREDS_PSW" | docker login "$DOCKER_REPO" -u "$OCR_CREDS_USR" --password-stdin
 
-docker image inspect alertmanager:v0.24.0-20221206192620-fb73d30f
+echo Running docker image inspect ...
+for image in "$IMAGES"; do
+    IMAGE_NAME_AND_TAG=$(echo "$image")
+    # docker image inspect "$DOCKER_REPO"/"$IMAGE_NAME_AND_TAG"
+done
+
+docker image inspect "$DOCKER_REPO"/verrazzano/example-bobbys-coherence:1.0.0-1-20210728181814-eb1e622
+docker image inspect "$DOCKER_REPO"/verrazzano/example-bobbys-front-end:1.0.0-1-20220608153526-843e168
