8e7fa3c8ece6818a7f90e00d3b375235e4aa4eab
Abraham Hernandez <abraham.hernandez@oracle.com>
2023-01-11 17:13:58 -0800

    Fixed while loop to pull docker images

--- release/scripts/validate_ocr_images.sh
+++ release/scripts/validate_ocr_images.sh
@@ -4,17 +4,18 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 #
 
-echo Running OCR image checks ...
+# We enabled the experimental Docker CLI to be able to run 'docker pull'
 export DOCKER_CLI_EXPERIMENTAL=enabled
+
+echo Running OCR image checks ...
 VZ_IMAGE_TXT=verrazzano_images.txt
-IMAGES=$(cat "$VZ_IMAGE_TXT")
 
 echo Logging into Docker ...
 echo "$OCR_CREDS_PSW" | docker login "$DOCKER_REPO" -u "$OCR_CREDS_USR" --password-stdin
 
 echo Running docker image inspect ...
-for image in "$IMAGES"; do
-    IMAGE_NAME_AND_TAG=$(echo "$image")
-    docker pull "$DOCKER_REPO"/"$IMAGE_NAME_AND_TAG"
-    # docker image inspect "$DOCKER_REPO"/"$IMAGE_NAME_AND_TAG"
-done
+while IFS= read -r line
+do
+  IMAGE_NAME_AND_TAG=$(echo "$line")
+  docker pull "$DOCKER_REPO"/"$IMAGE_NAME_AND_TAG"
+done < "$LOCAL_IMAGES_TXT"
