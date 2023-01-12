a51975297e24ecd6487709946cfa8676d906fe34
abnadago <abijitha.nadagouda@oracle.com>
2023-01-12 04:25:31 -0800

    test

--- ci/tools/derive_upgrade_version_test.go
+++ ci/tools/derive_upgrade_version_test.go
@@ -19,12 +19,12 @@ func TestGetInterimRelease(t *testing.T) {
 	pwd, _ := os.Getwd()
 	parseCliArgs([]string{pwd, "interim-version"})
 	releaseTags := []string{"v1.0.0", "v1.0.1", "v1.0.2", "v1.0.3", "v1.0.4", "v1.1.0", "v1.1.1", "v1.1.2", "v1.2.0", "v1.2.1", "v1.2.2", "v1.3.0", "v1.3.1", "v1.3.2", "v1.3.3", "v1.3.4", "v1.3.5", "v1.3.6", "v1.3.7", "v1.3.8", "v1.4.0", "v1.4.1", "v1.4.2"}
-	assert.Equal(t, "v1.3.8\n", getInstallRelease(releaseTags))
+	assert.Equal(t, "v1.3.8\n", getInterimRelease(releaseTags))
 }
 
 func TestGetInterimReleaseForMajorReleaseCase(t *testing.T) {
 	pwd, _ := os.Getwd()
 	parseCliArgs([]string{pwd, "interim-version"})
 	releaseTags := []string{"v1.0.0", "v1.0.1", "v1.0.2", "v1.0.3", "v1.0.4", "v1.1.0", "v1.1.1", "v1.1.2", "v1.2.0", "v1.2.1", "v1.2.2", "v1.3.0", "v1.3.1", "v1.3.2", "v1.3.3", "v1.3.4", "v1.3.5", "v1.3.6", "v1.3.7", "v1.3.8", "v1.4.0", "v1.4.1", "v1.4.2", "v1.5.0", "2.0.0"}
-	assert.Equal(t, "v1.5.0\n", getInstallRelease(releaseTags))
+	assert.Equal(t, "v1.5.0\n", getInterimRelease(releaseTags))
 }
