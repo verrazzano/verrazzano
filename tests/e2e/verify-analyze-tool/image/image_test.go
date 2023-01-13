819c4aeda48bfcbffd6f7a8d954cdc78c293d195
Vicky Sarkar <vicky.sarkar@oracle.com>
2023-01-13 14:02:56 +0530

    code optmisations

--- tests/e2e/verify-analyze-tool/image/image_test.go
+++ tests/e2e/verify-analyze-tool/image/image_test.go
@@ -10,6 +10,7 @@ import (
 	k8util "github.com/verrazzano/verrazzano/pkg/k8sutil"
 	"github.com/verrazzano/verrazzano/tests/e2e/pkg/test/framework"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 	"os/exec"
 	"strings"
 	"time"
@@ -21,24 +22,46 @@ var (
 )
 
 const (
-	ImagePullNotFound string = "ISSUE (ImagePullNotFound)"
-	//ImagePullBackOff string = "ISSUE (ImagePullBackOff)"
-	NameSpace string = "verrazzano-system"
+	ImagePullNotFound     string = "ISSUE (ImagePullNotFound)"
+	NameSpace             string = "verrazzano-system"
 	DeploymentToBePatched string = "verrazzano-console" // tbd : this could be fetched dynamically from list of deployments
+	//ImagePullBackOff    string = "ISSUE (ImagePullBackOff)"
 )
 
 var err error
+var reportAnalysis []string
+var c = &kubernetes.Clientset{}
+
 var t = framework.NewTestFramework("Vz Tools Analysis Image Issues")
 var _ = BeforeSuite(beforeSuite)
-var _ = t.AfterEach(func() {})
 var beforeSuite = t.BeforeSuiteFunc(func() {
-})
-
-func patchImage(deploymentName, namespace string, patch bool) error {
-	c, err := k8util.GetKubernetesClientset()
+	c, err = k8util.GetKubernetesClientset()
 	if err != nil {
 		Fail(err.Error())
 	}
+})
+
+func feedAnalysisReport() []string {
+	out := make([]string, 2)
+	for i := 0; i < len(out); i++ {
+		patchErr := patchImage(DeploymentToBePatched, NameSpace, i == 0)
+		if patchErr != nil {
+			Fail(patchErr.Error())
+		}
+		time.Sleep(waitTimeout)
+		out[i], err = RunVzAnalyze()
+		if err != nil {
+			Fail(err.Error())
+		}
+		if i == 0 {
+			time.Sleep(time.Second * 30)
+		}
+	}
+	reportAnalysis = append(reportAnalysis, out[0], out[1])
+	return reportAnalysis
+}
+
+func patchImage(deploymentName, namespace string, patch bool) error {
 	deploymentsClient := c.AppsV1().Deployments(namespace)
 	result, getErr := deploymentsClient.Get(context.TODO(), deploymentName, v1.GetOptions{})
 	if getErr != nil {
@@ -64,30 +87,19 @@ func patchImage(deploymentName, namespace string, patch bool) error {
 
 var _ = t.Describe("VZ Tools", Label("f:vz-tools-image-issues"), func() {
 	t.Context("During Image Issue Analysis", func() {
-		out := make([]string, 2)
-		for i := 0; i < len(out); i++ {
-			patchErr := patchImage(DeploymentToBePatched, NameSpace, i==0)
-			if patchErr != nil {
-				Fail(patchErr.Error())
-			}
-			time.Sleep(waitTimeout)
-			out[i], err = RunVzAnalyze()
-			if err != nil {
-				Fail(err.Error())
-			}
-			if i == 0 {
-				time.Sleep(time.Second * 30)
-			}
-		}
+		t.It("First Inject/ Revert Issue and Feed Analysis Report", func() {
+			feedAnalysisReport()
+			Expect(len(reportAnalysis)).To(Equal(2))
+		})
 		t.It("Should Have ImagePullNotFound Issue Post Bad Image Inject", func() {
 			Eventually(func() bool {
-				return verifyIssue(out[0], ImagePullNotFound)
+				return verifyIssue(reportAnalysis[0], ImagePullNotFound)
 			}, waitTimeout, pollingInterval).Should(BeTrue())
 		})
 
 		t.It("Should Not Have ImagePullNotFound Issue Post Correct Image Inject", func() {
 			Eventually(func() bool {
-				return verifyIssue(out[1], ImagePullNotFound)
+				return verifyIssue(reportAnalysis[1], ImagePullNotFound)
 			}, waitTimeout, pollingInterval).Should(BeFalse())
 		})
 	})
