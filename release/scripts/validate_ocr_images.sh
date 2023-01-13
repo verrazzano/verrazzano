1d4147c0e446a992ebd337fcff50002fe5f4a354
Abraham Hernandez <abraham.hernandez@oracle.com>
2023-01-12 19:06:54 -0800

    Prints the list of images that were NOT found in OCR

--- release/scripts/validate_ocr_images.sh
+++ release/scripts/validate_ocr_images.sh
@@ -7,38 +7,28 @@
 # We enabled the experimental Docker CLI to be able to run 'docker pull'
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
-echo Running OCR image checks ...
+echo "Running OCR image checks ..."
 OBJ_STORAGE_VZ_IMAGE_TXT=verrazzano_images.txt
-SUCCESSFULLY_PULLED_IMAGES=("")
 
-echo Logging into Docker ...
+echo "Logging into Docker ..."
 echo "$OCR_CREDS_PSW" | docker login "$DOCKER_REPO" -u "$OCR_CREDS_USR" --password-stdin
 
-echo Pulling images from OCR ... 
+echo "Pulling images from OCR ..." 
 while IFS= read -r line
 do  
     VZ_IMAGE_NAME=$(echo "$line")
     docker pull "$DOCKER_REPO"/"$VZ_IMAGE_NAME"
 done < "$OBJ_STORAGE_VZ_IMAGE_TXT"
 
-# while IFS= read -r line
-# do  
-#     VZ_IMAGE_NAME=$(echo "$line")
-#     # IMAGE_NAME_AND_TAG=$(echo "$line" | awk -F '/' '{print $2}')
-#     docker image inspect "$DOCKER_REPO"/"$VZ_IMAGE_NAME"
+echo "The following Images were not found in OCR ..."
+while IFS= read -r line
+do  
+    VZ_IMAGE_NAME=$(echo "$line")
+    INSPECT_EXIT_CODE=$(docker image inspect "$DOCKER_REPO"/"$VZ_IMAGE_NAME")
+    if [[ $? -eq 1 ]]; then
+        echo "$VZ_IMAGE_NAME"
+    fi
+done < "$OBJ_STORAGE_VZ_IMAGE_TXT"
 
-# done < "$OBJ_STORAGE_VZ_IMAGE_TXT"
-echo Exit code of image inspect . . .
-EXIT_CODE=$(docker image inspect "$DOCKER_REPO"/verrazzano/velero:v1.9.1-20220928065349-147272cf)
-echo $?
-# echo List of images that were successfully pulled ...
-# for image_name in "${SUCCESSFULLY_PULLED_IMAGES[@]}"
-# do
-#     IMAGE_FOUND_OR_NOT=$(grep -i "$image_name" "$OBJ_STORAGE_VZ_IMAGE_TXT")
-#     if [[ $? -eq 0 ]]; then
-#         echo Image "$image_name" found
-#     else
-#         echo Image "$image_name" not found
-#     fi
-# done
+ echo "Done."
 
