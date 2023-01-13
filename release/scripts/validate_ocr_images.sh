7177a7bf26cc0f983f199e7b84c9855276909951
Abraham Hernandez <abraham.hernandez@oracle.com>
2023-01-12 18:51:33 -0800

    Update validate_ocr_images.sh

--- release/scripts/validate_ocr_images.sh
+++ release/scripts/validate_ocr_images.sh
@@ -21,13 +21,16 @@ do
     docker pull "$DOCKER_REPO"/"$VZ_IMAGE_NAME"
 done < "$OBJ_STORAGE_VZ_IMAGE_TXT"
 
-while IFS= read -r line
-do  
-    VZ_IMAGE_NAME=$(echo "$line")
-    # IMAGE_NAME_AND_TAG=$(echo "$line" | awk -F '/' '{print $2}')
-    docker image inspect "$DOCKER_REPO"/"$VZ_IMAGE_NAME"
-done < "$OBJ_STORAGE_VZ_IMAGE_TXT"
+# while IFS= read -r line
+# do  
+#     VZ_IMAGE_NAME=$(echo "$line")
+#     # IMAGE_NAME_AND_TAG=$(echo "$line" | awk -F '/' '{print $2}')
+#     docker image inspect "$DOCKER_REPO"/"$VZ_IMAGE_NAME"
 
+# done < "$OBJ_STORAGE_VZ_IMAGE_TXT"
+echo Exit code of image inspect . . .
+EXIT_CODE=$(docker image inspect "$DOCKER_REPO"/container-registry.oracle.com/verrazzano/velero:v1.9.1-20220928065349-147272cf)
+echo $?
 # echo List of images that were successfully pulled ...
 # for image_name in "${SUCCESSFULLY_PULLED_IMAGES[@]}"
 # do
