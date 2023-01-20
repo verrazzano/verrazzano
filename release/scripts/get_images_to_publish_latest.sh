3ac232cde041be158817737366df8b17b59790cf
Abraham Hernandez <abraham.hernandez@oracle.com>
2023-01-19 21:05:44 -0800

    Moving latest version logic to script

--- release/scripts/get_images_to_publish_latest.sh
+++ release/scripts/get_images_to_publish_latest.sh
@@ -4,7 +4,8 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 #
 
-echo Latest release number is ...
+echo "Latest release number is ..."
 LIST_OF_RELEASE_NUM=($(grep -i 'images.txt' object_storage_images.json))
-export LATEST_RELEASE_NUMBER=$(echo ${LIST_OF_RELEASE_NUM[${#LIST_OF_RELEASE_NUM[@]}-1]} | cut -d  '"' -f 2)
+LATEST_RELEASE_NUMBER=$(echo ${LIST_OF_RELEASE_NUM[${#LIST_OF_RELEASE_NUM[@]}-1]} | cut -d  '"' -f 2)
 echo $LATEST_RELEASE_NUMBER
+oci --region ${OCI_REGION} os object get --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_BUCKET} --name $LATEST_RELEASE_NUMBER --file verrazzano_images.txt
