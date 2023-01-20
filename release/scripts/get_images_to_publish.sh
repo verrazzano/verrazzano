d6cf79ceea37eac52dadac1bc7064c9d78c68ff5
Abraham Hernandez <abraham.hernandez@oracle.com>
2023-01-19 22:59:46 -0800

    Added dev comments

--- release/scripts/get_images_to_publish.sh
+++ release/scripts/get_images_to_publish.sh
@@ -5,18 +5,19 @@
 #
 
 # Try to download specified Object store file from, default value is NONE
-echo "Trying to download image.txt file for $OBJECT_STORE_FILE"
+printf "\nTrying to download image.txt file for $OBJECT_STORE_FILE"
 oci --region ${OCI_REGION} os object get --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_BUCKET} --name ${OBJECT_STORE_FILE} --file verrazzano_images.txt
 
 if [[ $? -gt 0 ]]; then
-    echo "Trying to downloading release-* object list ..."
+# This "object list" command grabs the list of all objects in the OCI_OS_NAMESPACE that is a release object. Then trys to download the most up-to-date images.txt file
+# This only runs if the "object get" for the specified version fails or does not exist
+    printf "\nTrying to downloading release-* object list ..."
     oci os object list --region us-phoenix-1 --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_BUCKET} --all --stream-output --prefix release- > object_storage_images.json
-    
-    echo "Latest release-* object image.txt file is ..."
+
     LIST_OF_RELEASE_NUM=($(grep -i 'images.txt' object_storage_images.json))
     LATEST_RELEASE_NUMBER=$(echo ${LIST_OF_RELEASE_NUM[${#LIST_OF_RELEASE_NUM[@]}-1]} | cut -d  '"' -f 2)
-    echo $LATEST_RELEASE_NUMBER
+    printf "\nLatest release-* object image.txt file is ... $LATEST_RELEASE_NUMBER"
 
-    echo "Trying to download image.txt file for $LATEST_RELEASE_NUMBER"
+    printf "\nTrying to download image.txt file for $LATEST_RELEASE_NUMBER"
     oci --region ${OCI_REGION} os object get --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_BUCKET} --name $LATEST_RELEASE_NUMBER --file verrazzano_images.txt
 fi
