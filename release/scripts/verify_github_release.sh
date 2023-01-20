3939f2315c302934f8fcdd609a0cc932f36ec041
Abraham Hernandez <abraham.hernandez@oracle.com>
2023-01-20 14:39:38 -0800

    Removed logic for NONE case of release_version

--- release/scripts/verify_github_release.sh
+++ release/scripts/verify_github_release.sh
@@ -44,18 +44,6 @@ VERSION_NUMBER_MINOR=$(echo "$VERSION" | tail -c4 | head -c1)
 wget -O latest "https://github.com/verrazzano/verrazzano/releases/latest"
 LATEST_RELEASE_VERSION=$(grep -i '<title>' latest | awk -F 'release ' '{print $2}' | head -c6 | tail -c5)
 
-# Release artifacts for latest
-declare -a releaseArtifactsLatest=("verrazzano-platform-operator.yaml"
-                             "verrazzano-platform-operator.yaml.sha256"
-                             "verrazzano-${LATEST_RELEASE_VERSION}-darwin-amd64.tar.gz"
-                             "verrazzano-${LATEST_RELEASE_VERSION}-darwin-amd64.tar.gz.sha256"
-                             "verrazzano-${LATEST_RELEASE_VERSION}-darwin-arm64.tar.gz"
-                             "verrazzano-${LATEST_RELEASE_VERSION}-darwin-arm64.tar.gz.sha256"
-                             "verrazzano-${LATEST_RELEASE_VERSION}-linux-amd64.tar.gz"
-                             "verrazzano-${LATEST_RELEASE_VERSION}-linux-amd64.tar.gz.sha256"
-                             "verrazzano-${LATEST_RELEASE_VERSION}-linux-arm64.tar.gz"
-                             "verrazzano-${LATEST_RELEASE_VERSION}-linux-arm64.tar.gz.sha256")
-
 function verify_released_artifacts() {
   local releaseVersionDir=${TMPDIR}/release
   mkdir -p $releaseVersionDir
@@ -63,29 +51,7 @@ function verify_released_artifacts() {
 
   # Iterate the array containing the release artifacts and download all of them
   echo "Downloading release artifacts for ${VERSION}"
-
-  if [[ "$VERSION" == 'NONE' ]]; then
-    printf "Version_Number is NONE, getting latest artifacts"
-    # Latest tag is automatic, do we really need to check ? If required, better compare the files from the two directories
-    local latestVersionDir=${TMPDIR}}/latest
-    mkdir -p $latestVersionDir
-    cd $latestVersionDir
-
-    # Iterate the array containing the release artifacts and download all of them
-    printf "Latest release version is: $LATEST_RELEASE_VERSION\n"
-    echo "Downloading release artifacts for latest"
-    for i in "${releaseArtifactsLatest[@]}"
-    do
-      local url="https://github.com/verrazzano/verrazzano/releases/download/v$LATEST_RELEASE_VERSION/$i"
-      curl -Ss -L --show-error --fail -o $i ${url} || { echo "Unable to download ${url}"; exit; }
-    done
-    ${SHA_CMD} verrazzano-platform-operator.yaml.sha256
-    ${SHA_CMD} verrazzano-${LATEST_RELEASE_VERSION}-darwin-amd64.tar.gz.sha256
-    ${SHA_CMD} verrazzano-${LATEST_RELEASE_VERSION}-darwin-arm64.tar.gz.sha256
-    ${SHA_CMD} verrazzano-${LATEST_RELEASE_VERSION}-linux-amd64.tar.gz.sha256
-    ${SHA_CMD} verrazzano-${LATEST_RELEASE_VERSION}-linux-arm64.tar.gz.sha256
-
-  elif [[ "$VERSION_NUMBER_MINOR" -lt 4 ]]; then
+  if [[ "$VERSION_NUMBER_MINOR" -lt 4 ]]; then
       printf "Version_Number is PRIOR to v1.4.0\n"
       for i in "${releaseArtifactsPriorToV140[@]}"
       do
@@ -108,6 +74,25 @@ function verify_released_artifacts() {
       ${SHA_CMD} verrazzano-${RELEASE_VERSION}-darwin-arm64.tar.gz.sha256
       ${SHA_CMD} verrazzano-${RELEASE_VERSION}-linux-amd64.tar.gz.sha256
       ${SHA_CMD} verrazzano-${RELEASE_VERSION}-linux-arm64.tar.gz.sha256
+
+
+      # Latest tag is automatic, do we really need to check ? If required, better compare the files from the two directories
+      local latestVersionDir=${TMPDIR}}/latest
+      mkdir -p $latestVersionDir
+      cd $latestVersionDir
+
+      # Iterate the array containing the release artifacts and download all of them
+      echo "Downloading release artifacts for latest"
+      for i in "${releaseArtifactsLatest[@]}"
+      do
+        local url="https://github.com/verrazzano/verrazzano/releases/latest/download/v$LATEST_RELEASE_VERSION/$i"
+        curl -Ss -L --show-error --fail -o $i ${url} || { echo "Unable to download ${url}"; exit; }
+      done
+      ${SHA_CMD} verrazzano-platform-operator.yaml.sha256
+    ${SHA_CMD} verrazzano-${LATEST_RELEASE_VERSION}-darwin-amd64.tar.gz.sha256
+    ${SHA_CMD} verrazzano-${LATEST_RELEASE_VERSION}-darwin-arm64.tar.gz.sha256
+    ${SHA_CMD} verrazzano-${LATEST_RELEASE_VERSION}-linux-amd64.tar.gz.sha256
+    ${SHA_CMD} verrazzano-${LATEST_RELEASE_VERSION}-linux-arm64.tar.gz.sha256
   fi
 }
 
