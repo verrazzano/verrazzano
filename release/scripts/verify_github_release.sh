900c76654b7aa611d6ad7884f1c80d321e5647a7
Abraham Hernandez <abraham.hernandez@oracle.com>
2023-01-24 10:26:56 -0800

    Revert verify github artifacts to master

--- release/scripts/verify_github_release.sh
+++ release/scripts/verify_github_release.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright (c) 2021, 2022, 2023, Oracle and/or its affiliates.
+# Copyright (c) 2021, 2022, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 #
 # Verifies GitHub release artifacts.
@@ -37,10 +37,6 @@ if [ "$(uname)" == "Darwin" ]; then
     SHA_CMD="shasum -a 256 -c"
 fi
 
-# Grabs the minor release number to determine which github artifacts to download and check
-VERSION_NUMBER_MAJOR=$(echo "$VERSION" | head -c1)
-VERSION_NUMBER_MINOR=$(echo "$VERSION" | tail -c4 | head -c1)
-
 function verify_released_artifacts() {
   local releaseVersionDir=${TMPDIR}/release
   mkdir -p $releaseVersionDir
@@ -48,30 +44,34 @@ function verify_released_artifacts() {
 
   # Iterate the array containing the release artifacts and download all of them
   echo "Downloading release artifacts for ${VERSION}"
-  if [[ "$VERSION_NUMBER_MAJOR" == 1 && "$VERSION_NUMBER_MINOR" -lt 4 ]]; then
-      printf "Version_Number is PRIOR to v1.4.0\n"
-      for i in "${releaseArtifactsPriorToV140[@]}"
-      do
-        local url="https://github.com/verrazzano/verrazzano/releases/download/v$VERSION/$i"
-        curl -Ss -L --show-error --fail -o $i ${url} || { echo "Unable to download ${url}"; exit; }
-      done
-      ${SHA_CMD} k8s-dump-cluster.sh.sha256
-      ${SHA_CMD} verrazzano-analysis-darwin-amd64.tar.gz.sha256
-      ${SHA_CMD} verrazzano-analysis-linux-amd64.tar.gz.sha256
+  for i in "${releaseArtifacts[@]}"
+  do
+    local url="https://github.com/verrazzano/verrazzano/releases/download/$VERSION/$i"
+    curl -Ss -L --show-error --fail -o $i ${url} || { echo "Unable to download ${url}"; exit; }
+  done
+  ${SHA_CMD} verrazzano-platform-operator.yaml.sha256
+  ${SHA_CMD} verrazzano-${RELEASE_VERSION}-darwin-amd64.tar.gz.sha256
+  ${SHA_CMD} verrazzano-${RELEASE_VERSION}-darwin-arm64.tar.gz.sha256
+  ${SHA_CMD} verrazzano-${RELEASE_VERSION}-linux-amd64.tar.gz.sha256
+  ${SHA_CMD} verrazzano-${RELEASE_VERSION}-linux-arm64.tar.gz.sha256
 
-  else
-      printf "Version_Number is POST v1.4.0\n"
-      for i in "${releaseArtifacts[@]}"
-      do
-        local url="https://github.com/verrazzano/verrazzano/releases/download/v$VERSION/$i"
-        curl -Ss -L --show-error --fail -o $i ${url} || { echo "Unable to download ${url}"; exit; }
-      done
-      ${SHA_CMD} verrazzano-platform-operator.yaml.sha256
-      ${SHA_CMD} verrazzano-${RELEASE_VERSION}-darwin-amd64.tar.gz.sha256
-      ${SHA_CMD} verrazzano-${RELEASE_VERSION}-darwin-arm64.tar.gz.sha256
-      ${SHA_CMD} verrazzano-${RELEASE_VERSION}-linux-amd64.tar.gz.sha256
-      ${SHA_CMD} verrazzano-${RELEASE_VERSION}-linux-arm64.tar.gz.sha256
-  fi
+  # Latest tag is automatic, do we really need to check ? If required, better compare the files from the two directories
+  local latestVersionDir=${TMPDIR}}/latest
+  mkdir -p $latestVersionDir
+  cd $latestVersionDir
+
+  # Iterate the array containing the release artifacts and download all of them
+  echo "Downloading release artifacts for latest"
+  for i in "${releaseArtifacts[@]}"
+  do
+    local url="https://github.com/verrazzano/verrazzano/releases/latest/download/$i"
+    curl -Ss -L --show-error --fail -o $i ${url} || { echo "Unable to download ${url}"; exit; }
+  done
+  ${SHA_CMD} verrazzano-platform-operator.yaml.sha256
+  ${SHA_CMD} verrazzano-${RELEASE_VERSION}-darwin-amd64.tar.gz.sha256
+  ${SHA_CMD} verrazzano-${RELEASE_VERSION}-darwin-arm64.tar.gz.sha256
+  ${SHA_CMD} verrazzano-${RELEASE_VERSION}-linux-amd64.tar.gz.sha256
+  ${SHA_CMD} verrazzano-${RELEASE_VERSION}-linux-arm64.tar.gz.sha256
 }
 
 verify_released_artifacts
