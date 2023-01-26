e79dd3ec875c3fb57318033a515fa950c1a35246
stshapir <steven.shapiro@oracle.com>
2023-01-26 11:58:12 -0500

    spring-boot

--- tests/e2e/examples/springboot/springboot_test.go
+++ tests/e2e/examples/springboot/springboot_test.go
@@ -5,7 +5,6 @@ package springboot
 
 import (
 	"fmt"
-	dump "github.com/verrazzano/verrazzano/tests/e2e/pkg/test/clusterdump"
 	"net/http"
 	"time"
 
@@ -13,22 +12,29 @@ import (
 	. "github.com/onsi/gomega"
 	"github.com/verrazzano/verrazzano/pkg/k8sutil"
 	"github.com/verrazzano/verrazzano/tests/e2e/pkg"
+	dump "github.com/verrazzano/verrazzano/tests/e2e/pkg/test/clusterdump"
 	"github.com/verrazzano/verrazzano/tests/e2e/pkg/test/framework"
 	"github.com/verrazzano/verrazzano/tests/e2e/pkg/test/framework/metrics"
 )
 
-var expectedPodsSpringBootApp = []string{"springboot-workload"}
-var pollingInterval = 30 * time.Second
-var shortPollingInterval = 10 * time.Second
-var shortWaitTimeout = 5 * time.Minute
-var longWaitTimeout = 15 * time.Minute
-var longPollingInterval = 20 * time.Second
-
-var (
-	t                        = framework.NewTestFramework("springboot")
-	generatedNamespace       = pkg.GenerateNamespace("springboot")
+const (
+	shortWaitTimeout         = 5 * time.Minute
+	shortPollingInterval     = 10 * time.Second
+	longWaitTimeout          = 20 * time.Minute
+	longPollingInterval      = 20 * time.Second
 	imagePullWaitTimeout     = 40 * time.Minute
 	imagePullPollingInterval = 30 * time.Second
+
+	springService = "springboot-workload"
+	ingress       = "springboot-ingress-rule"
+	appCreds      = "springboot-appconf"
+)
+
+var (
+	t                         = framework.NewTestFramework("springboot")
+	generatedNamespace        = pkg.GenerateNamespace("springboot")
+	expectedPodsSpringBootApp = []string{"springboot-workload"}
+	host                      = ""
 )
 
 var beforeSuite = t.BeforeSuiteFunc(func() {
@@ -41,19 +47,58 @@ var beforeSuite = t.BeforeSuiteFunc(func() {
 		// GIVEN springboot app is deployed
 		// WHEN the component and appconfig are created
 		// THEN the expected pod must be running in the test namespace
-		pkg.Log(pkg.Info, "Container image pull check")
+		t.Logs.Info("Container image pull check")
 		Eventually(func() bool {
 			return pkg.ContainerImagePullWait(namespace, expectedPodsSpringBootApp)
 		}, imagePullWaitTimeout, imagePullPollingInterval).Should(BeTrue())
 	}
 
+	t.Logs.Info("Spring Boot Application: check expected pods are running")
 	Eventually(func() bool {
 		result, err := pkg.PodsRunning(namespace, expectedPodsSpringBootApp)
 		if err != nil {
 			AbortSuite(fmt.Sprintf("One or more pods are not running in the namespace: %v, error: %v", namespace, err))
 		}
 		return result
-	}, longWaitTimeout, pollingInterval).Should(BeTrue())
+	}, longWaitTimeout, longPollingInterval).Should(BeTrue(), "Spring Boot Application Failed to Deploy: Pods are not ready")
+
+	t.Logs.Info("Spring Boot Application: check expected Service is running")
+	Eventually(func() bool {
+		result, err := pkg.DoesServiceExist(namespace, springService)
+		if err != nil {
+			AbortSuite(fmt.Sprintf("App Service %s is not running in the namespace: %v, error: %v", springService, namespace, err))
+		}
+		return result
+	}, longWaitTimeout, longPollingInterval).Should(BeTrue(), "Spring Boot Application Failed to Deploy: Services are not ready")
+
+	t.Logs.Info("Spring Boot Application: check expected VirtualService is ready")
+	Eventually(func() bool {
+		result, err := pkg.DoesVirtualServiceExist(namespace, ingress)
+		if err != nil {
+			AbortSuite(fmt.Sprintf("App VirtualService %s is not running in the namespace: %v, error: %v", ingress, namespace, err))
+		}
+		return result
+	}, shortWaitTimeout, longPollingInterval).Should(BeTrue(), "Spring Boot Application Failed to Deploy: VirtualService is not ready")
+
+	t.Logs.Info("Spring Boot Application: check expected Secrets exist")
+	Eventually(func() bool {
+		result, err := pkg.DoesSecretExist(namespace, appCreds)
+		if err != nil {
+			AbortSuite(fmt.Sprintf("App Secret %s does not exist in the namespace: %v, error: %v", appCreds, namespace, err))
+		}
+		return result
+	}, shortWaitTimeout, longPollingInterval).Should(BeTrue(), "Spring Boot Application Failed to Deploy: Secret does not exist")
+
+	var err error
+	// Get the host from the Istio gateway resource.
+	start := time.Now()
+	t.Logs.Info("Spring Boot Application: check expected Gateway is ready")
+	Eventually(func() (string, error) {
+		host, err = k8sutil.GetHostnameFromGateway(namespace, "")
+		return host, err
+	}, shortWaitTimeout, shortPollingInterval).Should(Not(BeEmpty()), "Spring Boot Application Failed to Deploy: Gateway is not ready")
+	metrics.Emit(t.Metrics.With("get_host_name_elapsed_time", time.Since(start).Milliseconds()))
+
 	beforeSuitePassed = true
 })
 
