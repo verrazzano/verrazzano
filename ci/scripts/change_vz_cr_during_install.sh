54eb7f2b1e85c6247faafa24b50e0298433aa89e
Marco Gellecanao <marco.m.gellecanao@oracle.com>
2023-02-01 15:46:59 -0800

    wrote down TODO list of how to change the VZ CR during install

---
+++ ci/scripts/change_vz_cr_during_install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2023, Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+#
+# TODO: write description for environment variables and script arguments here
+
+# TODO: wait for vz resource to exist
+
+# TODO: wait a specified amount of time. Maybe make this an argument to this script
+
+# TODO: change the Wildcard DNS domain. Maybe make this flexible depending on script arguments?
+
