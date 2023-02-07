35aaf411003ff887d451131fce4bb0ece59d285d
Jonathan Maron <jonathan.maron@oracle.com>
2023-02-07 10:26:52 -0500

    extending liveness and readiness probe failure periods

--- tests/e2e/config/scripts/install-metallb.sh
+++ tests/e2e/config/scripts/install-metallb.sh
@@ -11,16 +11,17 @@ ADDRESS_RANGE=${1:-"172.18.0.230-172.18.0.254"}
 # Apply the MetalLB manifest
 wget https://raw.githubusercontent.com/metallb/metallb/v0.13.7/config/manifests/metallb-native.yaml
 sed -i "s|log-level=info|log-level=debug|g" metallb-native.yaml
+sed -i "s|failureThreshold: 3|failureThreshold: 12|g" metallb-native.yaml
 kubectl apply -f metallb-native.yaml --wait=true
 # Wait for the controller. webhook, and speaker to become ready
 kubectl wait --namespace metallb-system \
                 --for=condition=ready pod \
                 --selector=component=controller \
-                --timeout=1200s
+                --timeout=600s
 kubectl wait --namespace metallb-system \
                 --for=condition=ready pod \
                 --selector=component=speaker \
-                --timeout=1200s
+                --timeout=600s
 
 # Create the IPAddressPool for the cluster
 kubectl apply -f - <<-EOF
