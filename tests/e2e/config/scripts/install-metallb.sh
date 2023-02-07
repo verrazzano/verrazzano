407817fbb477729de35b35dac38e59e9d03591be
Jonathan Maron <jonathan.maron@oracle.com>
2023-02-07 14:36:57 -0500

    sample for liveness and readiness every 3 seconds for 30 seconds

--- tests/e2e/config/scripts/install-metallb.sh
+++ tests/e2e/config/scripts/install-metallb.sh
@@ -11,7 +11,8 @@ ADDRESS_RANGE=${1:-"172.18.0.230-172.18.0.254"}
 # Apply the MetalLB manifest
 wget https://raw.githubusercontent.com/metallb/metallb/v0.13.7/config/manifests/metallb-native.yaml
 sed -i "s|log-level=info|log-level=debug|g" metallb-native.yaml
-sed -i "s|failureThreshold: 3|failureThreshold: 12|g" metallb-native.yaml
+sed -i "s|failureThreshold: 3|failureThreshold: 10|g" metallb-native.yaml
+sed -i "s|periodSeconds: 10|periodSeconds: 3|g" metallb-native.yaml
 kubectl apply -f metallb-native.yaml --wait=true
 # Wait for the controller. webhook, and speaker to become ready
 kubectl wait --namespace metallb-system \
