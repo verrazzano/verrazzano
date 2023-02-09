b91249c95e790eab9629d5e1dc98e00bc1806dd3
stshapir <steven.shapiro@oracle.com>
2023-02-09 10:17:17 -0500

    linter

--- platform-operator/controllers/verrazzano/component/rancher/rancher_component.go
+++ platform-operator/controllers/verrazzano/component/rancher/rancher_component.go
@@ -6,12 +6,6 @@ package rancher
 import (
 	"context"
 	"fmt"
-	"os"
-	"path/filepath"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	"strconv"
-	"strings"
-
 	"github.com/gertd/go-pluralize"
 	"github.com/verrazzano/verrazzano/application-operator/controllers"
 	"github.com/verrazzano/verrazzano/pkg/bom"
@@ -40,6 +34,11 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/dynamic"
 	corev1 "k8s.io/client-go/kubernetes/typed/core/v1"
+	"os"
+	"path/filepath"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"strconv"
+	"strings"
 )
 
 // ComponentName is the name of the component
