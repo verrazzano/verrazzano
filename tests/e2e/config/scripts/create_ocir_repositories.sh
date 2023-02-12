0c35f4939176720ec387baee4691134abe6ef233
Bryan Tidd <bryan.tidd@oracle.com>
2023-02-12 10:03:43 -0500

    Fixup manage ocir repositories for rancher image names4

--- tests/e2e/config/scripts/create_ocir_repositories.sh
+++ tests/e2e/config/scripts/create_ocir_repositories.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2021, 2022, Oracle and/or its affiliates.
+# Copyright (c) 2021, 2023, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 #
 # Create a Docker repository in a specific compartment using an exploded tarball of Verrazzano images; useful for
