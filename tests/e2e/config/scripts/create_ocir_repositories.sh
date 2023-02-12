78e56bb87b98a7383f05c27e0bba8631093145b1
tony.vlatas <tony.vlatas@oracle.com>
2023-02-12 10:01:25 -0500

    Another script needed adjusting as well

--- tests/e2e/config/scripts/create_ocir_repositories.sh
+++ tests/e2e/config/scripts/create_ocir_repositories.sh
@@ -179,7 +179,7 @@ function create_image_repos_from_archives() {
     if [ "$from_repository" == "rancher" ] || [ "$from_repository" == "verrazzano/rancher" ] \
       || [ "$from_image_name" == "verrazzano-platform-operator" ] \
       || [ "$from_image_name" == "fluentd-kubernetes-daemonset" ] || [ "$from_image_name" == "proxyv2" ] \
-      || [ "$from_image_name" == "weblogic-monitoring-exporter" ] && [ -z $OCIR_SCAN_TARGET_ID ]; then
+      || [ "$from_image_name" == "weblogic-monitoring-exporter" ] || [[ "$from_image_name" =~ *rancher-* ]] && [ -z $OCIR_SCAN_TARGET_ID ]; then
       # Rancher repos must be public
       is_public="true"
     fi
