353e3ffb740501944723fc5db80cced3f85da292
Kate Nelson <kate.s.nelson@oracle.com>
2023-02-14 16:39:00 +0000

    test cases work

--- tools/vz/cmd/uninstall/uninstall.go
+++ tools/vz/cmd/uninstall/uninstall.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2022, Oracle and/or its affiliates.
+// Copyright (c) 2022, 2023, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 package uninstall
@@ -65,33 +65,7 @@ var logsEnum = cmdhelpers.LogFormatSimple
 func NewCmdUninstall(vzHelper helpers.VZHelper) *cobra.Command {
 	cmd := cmdhelpers.NewCommand(vzHelper, CommandName, helpShort, helpLong)
 	cmd.RunE = func(cmd *cobra.Command, args []string) error {
-		err := runCmdUninstall(cmd, args, vzHelper)
-		autoanalyzeFlag, errFlag := cmd.Flags().GetBool(constants.AutoanalyzeFlag)
-		if errFlag != nil {
-			fmt.Fprintf(vzHelper.GetOutputStream(), "error fetching flags: %s", errFlag.Error())
-		}
-		// if runCmdUninstall() returned an err and auto-analyze is set to true
-		if err != nil && autoanalyzeFlag {
-			//print the runCmdUninstall err
-			fmt.Fprintf(vzHelper.GetOutputStream(), "Install error: %s", err.Error())
-			cmd2 := analyze.NewCmdAnalyze(vzHelper)
-			kubeconfigFlag, errFlag :=  cmd.Flags().GetString(constants.GlobalFlagKubeConfig)
-			if errFlag != nil {
-				fmt.Fprintf(vzHelper.GetOutputStream(), "error fetching flags: %s", errFlag.Error())
-			}
-			contextFlag, errFlag2 :=  cmd.Flags().GetString(constants.GlobalFlagContext)
-			if errFlag2 != nil {
-				fmt.Fprintf(vzHelper.GetOutputStream(), "error fetching flags: %s", errFlag2.Error())
-			}
-			cmd2.Flags().StringVar(&kubeconfig, constants.GlobalFlagKubeConfig, "", constants.GlobalFlagKubeConfigHelp)
-			cmd2.Flags().StringVar(&context, constants.GlobalFlagContext, "", constants.GlobalFlagContextHelp)
-			cmd2.Flags().Set(constants.GlobalFlagKubeConfig, kubeconfigFlag)
-			cmd2.Flags().Set(constants.GlobalFlagContext, contextFlag)
-			cmd2.PersistentFlags().Set(constants.ReportFormatFlagName, constants.SummaryReport)
-			return analyze.RunCmdAnalyze(cmd2, args, vzHelper)
-		}
-		//otherwise, just return the runCmdUninstall err
-		return err
+		return runCmdUninstall(cmd, args, vzHelper)
 	}
 	cmd.Example = helpExample
 
@@ -186,11 +160,36 @@ func runCmdUninstall(cmd *cobra.Command, args []string, vzHelper helpers.VZHelpe
 
 	// Wait for the Verrazzano uninstall to complete.
 	err = waitForUninstallToComplete(client, kubeClient, vzHelper, types.NamespacedName{Namespace: vz.Namespace, Name: vz.Name}, timeout, vpoTimeout, logFormat, useUninstallJob)
-	if err != nil {
-		return fmt.Errorf("Failed to uninstall Verrazzano: %s", err.Error())
+	if err == nil {
+		return nil
 	}
-
-	return nil
+	autoanalyzeFlag, errFlag := cmd.Flags().GetBool(constants.AutoanalyzeFlag)
+	if errFlag != nil {
+		fmt.Fprintf(vzHelper.GetOutputStream(), "Error fetching flags: %s", errFlag.Error())
+	}
+	// if waitForUninstallToComplete() returned an err and auto-analyze is set to true, call vz analyze
+	if autoanalyzeFlag {
+		cmd2 := analyze.NewCmdAnalyze(vzHelper)
+		kubeconfigFlag, errFlag :=  cmd.Flags().GetString(constants.GlobalFlagKubeConfig)
+		if errFlag != nil {
+			fmt.Fprintf(vzHelper.GetOutputStream(), "Error fetching flags: %s", errFlag.Error())
+		}
+		contextFlag, errFlag2 :=  cmd.Flags().GetString(constants.GlobalFlagContext)
+		if errFlag2 != nil {
+			fmt.Fprintf(vzHelper.GetOutputStream(), "Error fetching flags: %s", errFlag2.Error())
+		}
+		cmd2.Flags().StringVar(&kubeconfig, constants.GlobalFlagKubeConfig, "", constants.GlobalFlagKubeConfigHelp)
+		cmd2.Flags().StringVar(&context, constants.GlobalFlagContext, "", constants.GlobalFlagContextHelp)
+		cmd2.Flags().Set(constants.GlobalFlagKubeConfig, kubeconfigFlag)
+		cmd2.Flags().Set(constants.GlobalFlagContext, contextFlag)
+		cmd2.PersistentFlags().Set(constants.ReportFormatFlagName, constants.SummaryReport)
+		analyzeErr := analyze.RunCmdAnalyze(cmd2, vzHelper)
+		if analyzeErr != nil {
+			fmt.Fprintf(vzHelper.GetOutputStream(), "Error calling VZ analyze %s \n", analyzeErr.Error())
+		}
+	}
+	//return the waitForUninstallToComplete() err
+	return fmt.Errorf("Failed to uninstall Verrazzano: %s", err.Error())
 }
 
 // cleanupResources deletes remaining resources that remain after the Verrazzano resource in uninstalled
