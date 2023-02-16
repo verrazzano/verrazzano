62a2dee82410501222fd2f37ee1079097fbcff38
verrazzanobot <verrazzanobot@oracle.com>
2023-02-16 17:01:29 +0000

    Revert "vz 8248 disable resource requirements validation (#5371)"
    This reverts commit bc36cef2781d9d41469769f4c647a6ad029d9e7d.

--- platform-operator/scripts/wait4webhook.sh
+++ platform-operator/scripts/wait4webhook.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-# Copyright (c) 2020, 2023, Oracle and/or its affiliates.
+# Copyright (c) 2020, 2022, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 #While loop for verrazzano-platform-operator to wait for webhooks to be started before starting up
 while [[ "$(curl --insecure -s -o /dev/null -w '%{http_code}' https://verrazzano-platform-operator-webhook:443/validate-install-verrazzano-io-v1alpha1-verrazzano -H 'Content-Type: application/json')" != "200" ]]; do sleep 5; done
 while [[ "$(curl --insecure -s -o /dev/null -w '%{http_code}' https://verrazzano-platform-operator-webhook:443/validate-install-verrazzano-io-v1beta1-verrazzano -H 'Content-Type: application/json')" != "200" ]]; do sleep 5; done
 while [[ "$(curl --insecure -s -o /dev/null -w '%{http_code}' https://verrazzano-platform-operator-webhook:443/v1beta1-validate-mysql-install-override-values -H 'Content-Type: application/json')" != "200" ]]; do sleep 5; done
 while [[ "$(curl --insecure -s -o /dev/null -w '%{http_code}' https://verrazzano-platform-operator-webhook:443/v1alpha1-validate-mysql-install-override-values -H 'Content-Type: application/json')" != "200" ]]; do sleep 5; done
-if [[ "$ENABLE_RESOURCE_VALIDATION" != "false" ]]; then
-  while [[ "$(curl --insecure -s -o /dev/null -w '%{http_code}' https://verrazzano-platform-operator-webhook:443/v1beta1-validate-requirements -H 'Content-Type: application/json')" != "200" ]]; do sleep 5; done
-  while [[ "$(curl --insecure -s -o /dev/null -w '%{http_code}' https://verrazzano-platform-operator-webhook:443/v1alpha1-validate-requirements -H 'Content-Type: application/json')" != "200" ]]; do sleep 5; done
-fi
+while [[ "$(curl --insecure -s -o /dev/null -w '%{http_code}' https://verrazzano-platform-operator-webhook:443/v1beta1-validate-requirements -H 'Content-Type: application/json')" != "200" ]]; do sleep 5; done
+while [[ "$(curl --insecure -s -o /dev/null -w '%{http_code}' https://verrazzano-platform-operator-webhook:443/v1alpha1-validate-requirements -H 'Content-Type: application/json')" != "200" ]]; do sleep 5; done
 while [[ "$(curl --insecure -s -o /dev/null -w '%{http_code}' -XPOST https://verrazzano-platform-operator-webhook:443/convert -H 'Content-Type: application/json' -d '{"apiVersion":"apiextensions.k8s.io/v1", "kind":"ConversionReview", "request":{}}')" != "200" ]]; do sleep 5; done
