d494eb3a48589969711761c15065396f7a5e9f60
Preethi Gnanesh <preethi.gnanesh@oracle.com>
2023-02-21 13:58:19 -0600

    Adding argo cd only for admin cluster

--- tests/e2e/config/scripts/multicluster_edit_vz.sh
+++ tests/e2e/config/scripts/multicluster_edit_vz.sh
@@ -14,7 +14,6 @@ yq -i eval '.spec.components.prometheusAdapter.enabled = true' "${VZ_CR_FILE}"
 yq -i eval '.spec.components.kubeStateMetrics.enabled = true' "${VZ_CR_FILE}"
 yq -i eval '.spec.components.prometheusPushgateway.enabled = true' "${VZ_CR_FILE}"
 yq -i eval '.spec.components.jaegerOperator.enabled = true' "${VZ_CR_FILE}"
-yq -i eval '.spec.components.argoCD.enabled = true' "${VZ_CR_FILE}"
 # For managed clusters, enable Jaeger operator and update the istio tracing configuration
 if [ "${CLUSTER_COUNT}" -gt 1  ]; then
   yq -i eval '.spec.components.istio.overrides.[0].values.apiVersion = "install.istio.io/v1alpha1"' ${VZ_CR_FILE}
@@ -23,6 +22,9 @@ if [ "${CLUSTER_COUNT}" -gt 1  ]; then
   yq -i eval '.spec.components.istio.overrides.[0].values.spec.meshConfig.defaultConfig.tracing.zipkin.address = "jaeger-verrazzano-managed-cluster-collector.verrazzano-monitoring.svc.cluster.local.:9411"' ${VZ_CR_FILE}
   yq -i eval '.spec.components.istio.overrides.[0].values.spec.meshConfig.enableTracing = true' ${VZ_CR_FILE}
 fi
-
+#For admin cluster only, enable Argo CD
+if [ "${CLUSTER_COUNT}" -eq 1  ]; then
+  yq -i eval '.spec.components.argoCD.enabled = true' "${VZ_CR_FILE}"
+fi
 echo "VZ CR to be applied:"
 cat "${VZ_CR_FILE}"
