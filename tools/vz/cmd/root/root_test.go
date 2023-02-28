e1d9a8ffc2a193fc33dfc8423bb95cd4e8533a60
Kate Nelson <kate.s.nelson@oracle.com>
2023-02-28 12:44:38 -0800

    testing subcommand

--- tools/vz/cmd/root/root_test.go
+++ tools/vz/cmd/root/root_test.go
@@ -6,13 +6,17 @@ package root
 import (
 	"bytes"
 	"fmt"
-	"os"
-	"strings"
-	"testing"
-
+	"github.com/spf13/cobra"
 	"github.com/verrazzano/verrazzano/tools/vz/cmd/analyze"
 	"github.com/verrazzano/verrazzano/tools/vz/cmd/bugreport"
 	"github.com/verrazzano/verrazzano/tools/vz/cmd/cluster"
+	cmdhelpers "github.com/verrazzano/verrazzano/tools/vz/cmd/helpers"
+	testhelpers "github.com/verrazzano/verrazzano/tools/vz/test/helpers"
+	"os"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"strings"
+	"testing"
 
 	"github.com/verrazzano/verrazzano/tools/vz/cmd/install"
 	"github.com/verrazzano/verrazzano/tools/vz/cmd/uninstall"
@@ -22,7 +26,7 @@ import (
 	"github.com/verrazzano/verrazzano/tools/vz/cmd/status"
 	"github.com/verrazzano/verrazzano/tools/vz/cmd/version"
 	"github.com/verrazzano/verrazzano/tools/vz/pkg/constants"
-	"github.com/verrazzano/verrazzano/tools/vz/test/helpers"
+	"github.com/verrazzano/verrazzano/tools/vz/pkg/helpers"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 )
 
@@ -30,7 +34,7 @@ func TestNewRootCmd(t *testing.T) {
 
 	buf := new(bytes.Buffer)
 	errBuf := new(bytes.Buffer)
-	rc := helpers.NewFakeRootCmdContext(genericclioptions.IOStreams{In: os.Stdin, Out: buf, ErrOut: errBuf})
+	rc := testhelpers.NewFakeRootCmdContext(genericclioptions.IOStreams{In: os.Stdin, Out: buf, ErrOut: errBuf})
 	rootCmd := NewRootCmd(rc)
 	assert.NotNil(t, rootCmd)
 
@@ -75,7 +79,7 @@ func TestNewRootCmd(t *testing.T) {
 func TestClusterCommandIsHiddenInHelp(t *testing.T) {
 	buf := new(bytes.Buffer)
 	errBuf := new(bytes.Buffer)
-	rc := helpers.NewFakeRootCmdContext(genericclioptions.IOStreams{In: os.Stdin, Out: buf, ErrOut: errBuf})
+	rc := testhelpers.NewFakeRootCmdContext(genericclioptions.IOStreams{In: os.Stdin, Out: buf, ErrOut: errBuf})
 	rootCmd := NewRootCmd(rc)
 	assert.NotNil(t, rootCmd)
 	args := []string{"-h"}
@@ -86,4 +90,37 @@ func TestClusterCommandIsHiddenInHelp(t *testing.T) {
 
 	// The create command should be hidden in VZ CLI help
 	assert.NotContains(t, output, cluster.HelpShort)
+}
+
+// TestInstallCmdDefaultTimeout
+// GIVEN a CLI install command with all defaults and --timeout=2s
+//
+//	WHEN I call cmd.Execute for install
+//	THEN the CLI install command times out
+func TestInstallCmdDefaultTimeout(t *testing.T) {
+	c := fake.NewClientBuilder().WithScheme(helpers.NewScheme()).WithObjects(testhelpers.CreateTestVPOObjects()...).Build()
+	cmd, buf, errBuf, _ := createNewTestCommandAndBuffers(t, c)
+	cmdInstall, err := cmdhelpers.GetSubCommandInstall(cmd)
+
+	cmdInstall.PersistentFlags().Set(constants.TimeoutFlag, "2s")
+	cmdhelpers.SetDeleteFunc(cmdhelpers.FakeDeleteFunc)
+	defer cmdhelpers.SetDefaultDeleteFunc()
+
+	// Run install command
+	err = cmdInstall.Execute()
+	assert.Error(t, err)
+	assert.Equal(t, "Error: Timeout 2s exceeded waiting for install to complete\n", errBuf.String())
+	assert.Contains(t, buf.String(), "Installing Verrazzano version v1.3.1")
+}
+
+func createNewTestCommandAndBuffers(t *testing.T, c client.Client) (*cobra.Command, *bytes.Buffer, *bytes.Buffer, *testhelpers.FakeRootCmdContext) {
+	buf := new(bytes.Buffer)
+	errBuf := new(bytes.Buffer)
+	rc := testhelpers.NewFakeRootCmdContext(genericclioptions.IOStreams{In: os.Stdin, Out: buf, ErrOut: errBuf})
+	if c != nil {
+		rc.SetClient(c)
+	}
+	cmd := NewRootCmd(rc)
+	assert.NotNil(t, cmd)
+	return cmd, buf, errBuf, rc
 }
