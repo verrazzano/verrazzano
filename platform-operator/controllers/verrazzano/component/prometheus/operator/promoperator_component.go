2aeb36aa4ddef90df5248a1aa5b10d138d677e1a
Saidu Dosapati <saidu.babu.dosapati@oracle.com>
2023-02-28 16:48:21 -0500

    initial commit to remove hard dependencies for prometheus

--- platform-operator/controllers/verrazzano/component/prometheus/operator/promoperator_component.go
+++ platform-operator/controllers/verrazzano/component/prometheus/operator/promoperator_component.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2022, Oracle and/or its affiliates.
+// Copyright (c) 2022, 2023, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 package operator
@@ -189,6 +189,9 @@ func (c prometheusComponent) GetIngressNames(ctx spi.ComponentContext) []types.N
 
 	if vzcr.IsPrometheusEnabled(ctx.EffectiveCR()) {
 		ns := ComponentNamespace
+		if !vzcr.IsNGINXEnabled(ctx.EffectiveCR()) && !vzcr.IsAuthProxyEnabled(ctx.EffectiveCR()) {
+			return ingressNames
+		}
 		if vzcr.IsAuthProxyEnabled(ctx.EffectiveCR()) {
 			ns = authproxy.ComponentNamespace
 		}
