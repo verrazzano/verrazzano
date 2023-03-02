2396d52a893e05e46e920c76545059e2cf5dad56
Michael Cico <46466506+michael-cico@users.noreply.github.com>
2023-03-02 08:47:06 -0500

    Revert "API/chart experiments"
    This reverts commit aa97191f2b58aef5e4abf1ada0809867ee44c238.

--- platform-operator/poc/charts/thirdparty/post-install.sh
+++
@@ -1,5 +0,0 @@
-kubectl apply -f manual-keycloak-install/issuer-secret.yaml
-kubectl apply -f manual-keycloak-install/cluster-issuer.yaml
-kubectl apply -f manual-keycloak-install/cert.yaml
-kubectl apply -f manual-keycloak-install/ingress-cm.yaml
-kubectl apply -f manual-keycloak-install/ingress.yaml
