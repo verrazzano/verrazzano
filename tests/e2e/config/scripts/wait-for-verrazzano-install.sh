d878889e33b3570db332294901a364835c477503
Bryan Tidd <bryan.tidd@oracle.com>
2023-03-10 10:27:39 -0500

    NO JIRA - Extend install timeout to 45 from 30 in tests/e2e/config/scripts/wait-for-verrazzano-install.sh2

--- tests/e2e/config/scripts/wait-for-verrazzano-install.sh
+++ tests/e2e/config/scripts/wait-for-verrazzano-install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright (c) 2020, 2022, Oracle and/or its affiliates.
+# Copyright (c) 2020, 2023, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 #
 
@@ -12,7 +12,7 @@ i=0
 resName=$(kubectl get vz -o jsonpath='{.items[*].metadata.name}')
 echo "waiting for install of resource ${resName} to complete"
 
-while [[ $retval_success -ne 0 ]] && [[ $retval_failed -ne 0 ]]  && [[ $i -lt 30 ]]  ; do
+while [[ $retval_success -ne 0 ]] && [[ $retval_failed -ne 0 ]]  && [[ $i -lt 45 ]]  ; do
   sleep 60
   output=$(kubectl wait --for=condition=InstallFailed verrazzano/${resName} --timeout=0 2>&1)
   retval_failed=$?
