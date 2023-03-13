30c625e0b735f554d9dcc3572b9921d248782f70
Paul Mackin <paul.mackin@oracle.com>
2023-03-13 15:29:06 -0400

    checkpoint

--- tests/e2e/components/rancher/install/loop/rancher_install_uninstall_suite_test.go
+++ tests/e2e/components/rancher/install/loop/rancher_install_uninstall_suite_test.go
@@ -1,14 +1,15 @@
 // Copyright (c) 2022, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-package installuninstall
+package loop
 
 import (
-	"github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 	"testing"
 )
 
 func TestRancherLoopInstallUninstall(test *testing.T) {
-	t.RegisterFailHandler()
-	ginkgo.RunSpecs(test, "Test Rancher Install then Uninstall in a loop")
+	RegisterFailHandler(Fail)
+	RunSpecs(test, "Test Rancher Install then Uninstall in a loop")
 }
