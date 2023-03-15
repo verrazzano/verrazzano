084132406ee7ddb8f489c0a15ebbd17373fc181e
Paul Mackin <paul.mackin@oracle.com>
2023-03-15 16:50:19 -0400

    fix typo in script (#5588)

--- platform-operator/scripts/wait4webhook.sh
+++ platform-operator/scripts/wait4webhook.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 # Copyright (c) 2020, 2023, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
-#While loop for verrazzano-platform-operator to wait for webhooks to be started before starting up
-
-#!/bin/bash
-# Copyright (c) 2020, 2023, Oracle and/or its affiliates.
-# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
-#While loop for verrazzano-platform-operator to wait for webhooks to be started before starting up
 
 function poll-webhook {
     SECONDS=0
     MAX_SECONDS=120
     while [ $SECONDS -lt $MAX_SECONDS ]; do
-        status_code=$(curl --insecure --silent --output /tmp/out --write-out '%{http_code}' -H 'Content-Type: application/json' $1)
+        status_code=$(curl  -vvv --insecure --silent --output /tmp/out --write-out '%{http_code}' -H 'Content-Type: application/json' $1)
         echo "$1 returned HTTP $status_code."
         if [[ "$status_code" != "200" ]]; then
             cat /tmp/out
-            curl --insecure -v -H 'Content-Type: application/json' $1
             echo "waiting 5 seconds"
+            let SECONDS=SECONDS+5
             sleep 5
         else
             exit 0
