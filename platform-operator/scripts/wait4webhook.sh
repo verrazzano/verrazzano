ee8564e1951bb908c6ad31652ab4fe8e29476c31
Paul Mackin <paul.mackin@oracle.com>
2023-03-14 21:26:06 -0400

    increase webhook timeout to 120

--- platform-operator/scripts/wait4webhook.sh
+++ platform-operator/scripts/wait4webhook.sh
@@ -10,14 +10,15 @@
 
 function poll-webhook {
   SECONDS=0
-  MAX_SECONDS=60
+  MAX_SECONDS=120
   while [ $SECONDS -lt $MAX_SECONDS ]; do
       status_code=$(curl --insecure --silent --output /tmp/out --write-out '%{http_code}' -H 'Content-Type: application/json' $1)
       echo "$1 returned HTTP $status_code."
       if [[ "$status_code" != "200" ]]; then
           cat /tmp/out
-          echo "waiting"
+          echo "waiting 5 seconds"
           sleep 5
+          let SECONDS=SECONDS+5
       else
           exit 0
       fi
