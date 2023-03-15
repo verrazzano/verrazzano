a70333abb15e1953d8a1d3cdfab828b6a2680055
Paul Mackin <paul.mackin@oracle.com>
2023-03-15 08:49:53 -0400

    VZ-8959 fix wait4webhook.sh typo (#5582)
    Co-authored-by: Ravi Singhal <ravi.r.singhal@oracle.com>

--- platform-operator/scripts/wait4webhook.sh
+++ platform-operator/scripts/wait4webhook.sh
@@ -9,20 +9,22 @@
 #While loop for verrazzano-platform-operator to wait for webhooks to be started before starting up
 
 function poll-webhook {
-  SECONDS=0
-  MAX_SECONDS=30
-  while [ $SECONDS -lt $MAX_SECONDS ]; do
-      status_code=$(curl --insecure --silent --output /tmp/out --write-out '%{http_code}' -H 'Content-Type: application/json' $1)
-      echo "$1 returned HTTP $status_code."
-      if [[ "$status_code" != "200" ]]; then
-          cat /tmp/out
-          echo "waiting"
-          sleep 5
-      else
-          exit 0
-      fi
-  done
-+exit 1
+    SECONDS=0
+    MAX_SECONDS=120
+    while [ $SECONDS -lt $MAX_SECONDS ]; do
+        status_code=$(curl --insecure --silent --output /tmp/out --write-out '%{http_code}' -H 'Content-Type: application/json' $1)
+        echo "$1 returned HTTP $status_code."
+        if [[ "$status_code" != "200" ]]; then
+            cat /tmp/out
+            curl --insecure -v -H 'Content-Type: application/json' $1
+            echo "waiting 5 seconds"
+            sleep 5
+        else
+            exit 0
+        fi
+    done
+    echo "timeout waiting for VPO webhook"
+    exit 1
 }
 
-poll-webhook "https://verrazzano-platform-operator-webhook:443/validate-install-verrazzano-io-v1beta1-verrazzano"
+poll-webhook "https://verrazzano-platform-operator-webhook.verrazzano-install.svc.cluster.local:443/validate-install-verrazzano-io-v1beta1-verrazzano"
