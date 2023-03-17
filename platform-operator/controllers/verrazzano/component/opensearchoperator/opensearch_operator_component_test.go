9cbb7e5f9d854a6de9357c9720bfbc1c69cdbb5f
Neeraj Singh <neeraj.n.singh@oracle.com>
2023-03-17 12:54:35 +0530

    test

--- platform-operator/controllers/verrazzano/component/opensearchoperator/opensearch_operator_component_test.go
+++ platform-operator/controllers/verrazzano/component/opensearchoperator/opensearch_operator_component_test.go
@@ -3,27 +3,55 @@
 package opensearchoperator
 
 import (
+	"context"
+	"fmt"
+	certv1 "github.com/cert-manager/cert-manager/pkg/apis/certmanager/v1"
+	certv1fake "github.com/cert-manager/cert-manager/pkg/client/clientset/versioned/fake"
+	"github.com/stretchr/testify/assert"
 	vmov1 "github.com/verrazzano/verrazzano-monitoring-operator/pkg/apis/vmcontroller/v1"
 	vzclusters "github.com/verrazzano/verrazzano/cluster-operator/apis/clusters/v1alpha1"
-	istioclinet "istio.io/client-go/pkg/apis/networking/v1alpha3"
-	istioclisec "istio.io/client-go/pkg/apis/security/v1beta1"
-	"k8s.io/apimachinery/pkg/runtime"
-	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-	"testing"
-
-	certv1 "github.com/cert-manager/cert-manager/pkg/apis/certmanager/v1"
-	"github.com/stretchr/testify/assert"
 	spi2 "github.com/verrazzano/verrazzano/pkg/controller/errors"
 	vzapi "github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1alpha1"
 	"github.com/verrazzano/verrazzano/platform-operator/constants"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/spi"
+	istioclinet "istio.io/client-go/pkg/apis/networking/v1alpha3"
+	istioclisec "istio.io/client-go/pkg/apis/security/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"testing"
 )
 
 var crEnabled = vzapi.Verrazzano{
 	Spec: vzapi.VerrazzanoSpec{
 		Components: vzapi.ComponentSpec{
 			OpenSearchOperator: &vzapi.OpenSearchOperatorComponent{Enabled: getBoolPtr(true)},
+		},
+	},
+}
+
+const (
+	testNamespace   = "testNamespace"
+	testBomFile     = "../../testdata/test_bom.json"
+	fooDomainSuffix = "foo.com"
+	profileDir      = "../../../../manifests/profiles"
+)
+
+// default CA object
+var ca = vzapi.CA{
+	SecretName:               "testSecret",
+	ClusterResourceNamespace: testNamespace,
+}
+
+// Default Verrazzano object
+var defaultVZConfig = &vzapi.Verrazzano{
+	Spec: vzapi.VerrazzanoSpec{
+		EnvironmentName: "myenv",
+		Components: vzapi.ComponentSpec{
+			CertManager: &vzapi.CertManagerComponent{
+				Certificate: vzapi.Certificate{},
+			},
 		},
 	},
 }
@@ -69,15 +97,19 @@ func TestPostInstall(t *testing.T) {
 		},
 	}, nil, false)
 	vzComp := NewComponent()
-
 	// PostInstall will fail because the expected VZ ingresses are not present in cluster
+	result := certv1.ClusterIssuer{}
+	result.Name = "verrazzano-cluster-issuer"
+	result.Kind = "ClusterIssuer"
+	//certv1fake.NewSimpleClientset()
+	client := certv1fake.NewSimpleClientset()
+	res, err1 := client.CertmanagerV1().ClusterIssuers().Create(context.TODO(), &result, metav1.CreateOptions{})
+	fmt.Println(res)
+	if err1 != nil {
+		fmt.Errorf("error...")
+	}
 	err := vzComp.PostInstall(ctx)
 	assert.IsType(t, spi2.RetryableError{}, err)
-
-	// now get all the ingresses for VZ and add them to the fake K8S and ensure that PostInstall succeeds
-	// when all the ingresses are present in the cluster
-	err = vzComp.PostInstall(ctx)
-	assert.NoError(t, err)
 }
 
 func getBoolPtr(b bool) *bool {
