3b3f14c75e887add3704620ece3532c662b4d3e8
Ben McLean <mbenmclean@gmail.com>
2023-03-28 18:01:01 -0600

    VZ-9133-experimental-module (#5672)

--- platform-operator/main.go
+++ platform-operator/main.go
@@ -93,6 +93,7 @@ func main() {
 		"MySQL check period seconds; set to 0 to disable MySQL checks")
 	flag.Int64Var(&config.MySQLRepairTimeoutSeconds, "mysql-repair-timeout", config.MySQLRepairTimeoutSeconds,
 		"MySQL repair timeout seconds")
+	flag.BoolVar(&config.ExperimentalModules, "experimental-modules", config.ExperimentalModules, "enable experimental modules")
 
 	// Add the zap logger flag set to the CLI.
 	opts := kzap.Options{}
