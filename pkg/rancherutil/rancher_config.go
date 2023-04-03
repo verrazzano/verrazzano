95be53a0d6d437908650810e612ba8a1e76bbb3e
davisgomes <49332517+davisgomes@users.noreply.github.com>
2023-04-03 16:25:30 -0400

    Fix host

--- pkg/rancherutil/rancher_config.go
+++ pkg/rancherutil/rancher_config.go
@@ -41,7 +41,7 @@ const (
 	tokensPath = "/v3/tokens" //nolint:gosec
 
 	// this host resolves to the cluster IP
-	nginxIngressHostName = "rancher.default.172.18.0.231.nip.io"
+	nginxIngressHostName = "ingress-controller-ingress-nginx-controller.ingress-nginx"
 )
 
 type RancherConfig struct {
