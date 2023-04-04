6bcacb7d59293fd8e307ff6a913110b30d0f4c83
Fred Tibbitts III <44442080+fredtibbitts@users.noreply.github.com>
2023-04-04 09:43:21 -0400

    VZ-8840: Grafana datasource support for Thanos (#5707)

--- platform-operator/controllers/verrazzano/component/grafana/grafana.go
+++ platform-operator/controllers/verrazzano/component/grafana/grafana.go
@@ -1,14 +1,21 @@
-// Copyright (c) 2022, Oracle and/or its affiliates.
+// Copyright (c) 2022, 2023, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 package grafana
 
 import (
 	"fmt"
+	"path"
 
+	"github.com/pkg/errors"
 	"github.com/verrazzano/verrazzano/pkg/k8s/ready"
+	"github.com/verrazzano/verrazzano/pkg/k8sutil"
+	"github.com/verrazzano/verrazzano/pkg/vzcr"
+	"github.com/verrazzano/verrazzano/platform-operator/constants"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/common"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/spi"
+	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/thanos"
+	"github.com/verrazzano/verrazzano/platform-operator/internal/config"
 	"k8s.io/apimachinery/pkg/types"
 )
 
@@ -48,4 +55,67 @@ func newDeployments() []types.NamespacedName {
 			Namespace: ComponentNamespace,
 		},
 	}
+}
+
+// applyDatasourcesConfigmap applies a configmap containing Grafana datasources. If Thanos Query is enabled
+// we set Thanos as the default datasource, otherwise Prometheus is the default datasource.
+func applyDatasourcesConfigmap(ctx spi.ComponentContext) error {
+	// create template key/value map
+	args := make(map[string]interface{})
+	args["namespace"] = constants.VerrazzanoSystemNamespace
+	args["name"] = datasourcesConfigMapName
+
+	cr := ctx.EffectiveCR()
+	promEnabled := vzcr.IsPrometheusEnabled(cr) && vzcr.IsPrometheusOperatorEnabled(cr)
+	args["isPrometheusEnabled"] = promEnabled
+	if promEnabled {
+		args["prometheusURL"] = "http://prometheus-operator-kube-p-prometheus.verrazzano-monitoring"
+		args["prometheusPort"] = 9090
+	}
+
+	thanosQueryEnabled, err := isThanosQueryFrontendEnabled(ctx)
+	if err != nil {
+		return err
+	}
+	args["isThanosQueryEnabled"] = thanosQueryEnabled
+	if thanosQueryEnabled {
+		args["thanosQueryURL"] = "http://thanos-query-frontend.verrazzano-monitoring"
+		args["thanosQueryPort"] = 9090
+	}
+
+	// substitute template values in the datasources configmap template and apply the resulting YAML
+	fpath := path.Join(config.GetThirdPartyManifestsDir(), "grafana", "datasources-configmap.yaml")
+	yamlApplier := k8sutil.NewYAMLApplier(ctx.Client(), "")
+	err = yamlApplier.ApplyFT(fpath, args)
+	if err != nil {
+		return ctx.Log().ErrorfNewErr("Failed to substitute template values in Grafana datasources configmap: %v", err)
+	}
+	return nil
+}
+
+// isThanosQueryFrontendEnabled returns true if the Thanos component is enabled and Thanos Query Frontend is
+// enabled in the Helm chart
+func isThanosQueryFrontendEnabled(ctx spi.ComponentContext) (bool, error) {
+	const queryFrontendEnabledHelmKey = "queryFrontend.enabled"
+
+	if !vzcr.IsThanosEnabled(ctx.EffectiveCR()) {
+		return false, nil
+	}
+
+	thanosComp := thanos.NewComponent().(thanos.ThanosComponent)
+	vals, err := thanosComp.GetComputedValues(ctx)
+	if err != nil {
+		return false, errors.Errorf("Unable to fetch computed Helm values for Thanos: %v", err)
+	}
+	enabledVal, err := vals.PathValue(queryFrontendEnabledHelmKey)
+	if err != nil {
+		return false, errors.Errorf("Unable to find Helm key %s in Thanos chart: %v", queryFrontendEnabledHelmKey, err)
+	}
+
+	enabled, ok := enabledVal.(bool)
+	if !ok {
+		return false, errors.Errorf("Thanos chart value %s expected to be of type bool", queryFrontendEnabledHelmKey)
+	}
+
+	return enabled, nil
 }
