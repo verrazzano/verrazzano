1a502929823665c7a8fbff8445900b9bf3d81ab2
Kate Nelson <kate.s.nelson@oracle.com>
2023-04-17 07:31:22 -0700

    add watch for oci secret

--- platform-operator/controllers/verrazzano/reconcile/controller.go
+++ platform-operator/controllers/verrazzano/reconcile/controller.go
@@ -868,7 +868,12 @@ func (r *Reconciler) watchResources(namespace string, name string, log vzlog.Ver
 	}
 
 	log.Debugf("Watching the Rancher global data namespace to create admin cloud credential")
-	return r.watchRancherGlobalDataNamespace(namespace, name)
+	if err := r.watchRancherGlobalDataNamespace(namespace, name); err != nil {
+		return err
+	}
+
+	log.Debugf("Watching for OCI secret to create admin cloud credential")
+	return r.watchOCISecret(namespace, name)
 }
 
 func (r *Reconciler) watchManagedClusterRegistrationSecret(namespace string, name string) error {
@@ -931,6 +936,22 @@ func (r *Reconciler) isCattleGlobalDataNamespace(o client.Object) bool {
 	return true
 }
 
+func (r *Reconciler) watchOCISecret(namespace string, name string) error {
+	return r.Controller.Watch(
+		&source.Kind{Type: &corev1.Secret{}},
+		createReconcileEventHandler(namespace, name),
+		// Reconcile if there is an event related to the registration secret
+		predicate.Funcs{
+			CreateFunc: func(e event.CreateEvent) bool {
+				return r.isOCISecret(e.Object)
+			},
+			UpdateFunc: func(e event.UpdateEvent) bool {
+				return r.isOCISecret(e.ObjectNew)
+			},
+		},
+	)
+}
+
 func (r *Reconciler) isManagedClusterRegistrationSecret(o client.Object) bool {
 	secret := o.(*corev1.Secret)
 	if secret.Namespace != vzconst.VerrazzanoSystemNamespace || secret.Name != vzconst.MCRegistrationSecret {
@@ -946,6 +967,15 @@ func (r *Reconciler) isThanosInternalUserSecret(o client.Object) bool {
 		return false
 	}
 	r.AddWatch(keycloak.ComponentJSONName)
+	return true
+}
+
+func (r *Reconciler) isOCISecret(o client.Object) bool {
+	secret := o.(*corev1.Secret)
+	if secret.Namespace != vzconst.VerrazzanoInstallNamespace || secret.Name != vzconst.OCISecretName {
+		return false
+	}
+	r.AddWatch(rancher.ComponentJSONName)
 	return true
 }
 
