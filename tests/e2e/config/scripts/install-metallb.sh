341109845d8973117e52b1864ec6c1c049283c7b
davisgomes <49332517+davisgomes@users.noreply.github.com>
2023-04-19 16:54:46 -0400

    VZ-9439 look for metallb in the home directory

--- tests/e2e/config/scripts/install-metallb.sh
+++ tests/e2e/config/scripts/install-metallb.sh
@@ -9,10 +9,14 @@ set -e
 ADDRESS_RANGE=${1:-"172.18.0.230-172.18.0.254"}
 
 # Apply the MetalLB manifest
-wget https://raw.githubusercontent.com/metallb/metallb/v0.13.7/config/manifests/metallb-native.yaml
-sed -i -e "s|log-level=info|log-level=debug|g" metallb-native.yaml
-sed -i -e "s|failureThreshold: 3|failureThreshold: 6|g" metallb-native.yaml
-kubectl apply -f metallb-native.yaml --wait=true
+if [ -f "$HOME"/metallb-native.yaml ] ; then
+  kubectl apply -f "$HOME"/metallb-native.yaml --wait=true
+else
+  wget https://raw.githubusercontent.com/metallb/metallb/v0.13.7/config/manifests/metallb-native.yaml
+  sed -i -e "s|log-level=info|log-level=debug|g" metallb-native.yaml
+  sed -i -e "s|failureThreshold: 3|failureThreshold: 6|g" metallb-native.yaml
+  kubectl apply -f metallb-native.yaml --wait=true
+fi
 
 sleep 5 # wait a few before checking the status, sometimes we get some resource errors on MacOS if we check too soon
 kubectl rollout status -n metallb-system deployment controller --timeout=600s
