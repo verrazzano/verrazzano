8ca5d06211b7e63e732d4e0b4a2afa4489fa3bd9
Marco Gellecanao <marco.m.gellecanao@oracle.com>
2023-04-20 12:56:18 -0700

    Successfully made an HTTP GET request for the admin cluster

--- tests/e2e/capi/ocne-driver/ocne_cluster_driver_test.go
+++ tests/e2e/capi/ocne-driver/ocne_cluster_driver_test.go
@@ -4,6 +4,12 @@
 package ocnedriver
 
 import (
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/Jeffail/gabs/v2"
+	"github.com/hashicorp/go-retryablehttp"
 	"github.com/verrazzano/verrazzano/pkg/k8sutil"
 	"github.com/verrazzano/verrazzano/tests/e2e/pkg"
 	"github.com/verrazzano/verrazzano/tests/e2e/pkg/test/framework"
@@ -12,25 +18,42 @@ import (
 	. "github.com/onsi/gomega"
 )
 
+var (
+	httpClient *retryablehttp.Client
+	rancherURL string
+	adminToken string
+)
+
 var t = framework.NewTestFramework("install")
 
 var beforeSuite = t.BeforeSuiteFunc(func() {
 	kubeconfigPath, err := k8sutil.GetKubeConfigLocation()
 	Expect(err).ShouldNot(HaveOccurred())
-	httpClient := pkg.EventuallyVerrazzanoRetryableHTTPClient()
+	httpClient = pkg.EventuallyVerrazzanoRetryableHTTPClient()
 	api := pkg.EventuallyGetAPIEndpoint(kubeconfigPath)
-	rancherURL := pkg.EventuallyGetURLForIngress(t.Logs, api, "cattle-system", "rancher", "https")
-	adminToken := pkg.GetRancherAdminToken(t.Logs, httpClient, rancherURL)
-	_ = adminToken
-	// TODO: authenticate using Bearer Token
+	rancherURL = pkg.EventuallyGetURLForIngress(t.Logs, api, "cattle-system", "rancher", "https")
+	adminToken = pkg.GetRancherAdminToken(t.Logs, httpClient, rancherURL)
+	fmt.Println("Token: " + adminToken)
 })
 var _ = BeforeSuite(beforeSuite)
 
 var _ = t.Describe("OCNE Cluster Driver", Label("TODO: appropriate label"), Serial, func() {
 	t.Context("Cluster Creation", func() {
 		t.It("creates an active cluster", func() {
-			Expect(1+1).Should(Equal(2))
 			// TODO: create a cluster with an HTTP POST request, then verify that the cluster is eventually active
+
+			// doing GET request as a test
+			clusterName := "local"
+			requestURL := rancherURL + "/v3/cluster?name=" + clusterName
+			request, _ := retryablehttp.NewRequest(http.MethodGet, requestURL, nil)
+			request.Header.Set("Authorization", fmt.Sprintf("Bearer %v", adminToken))
+			response, _ := httpClient.Do(request)
+			defer response.Body.Close()
+			body, _ := io.ReadAll(response.Body)
+			jsonBody, _ := gabs.ParseJSON(body)
+			fmt.Println(jsonBody)
+
+			Expect(1+1).Should(Equal(2))
 		})
 	})
 })
