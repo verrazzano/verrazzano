d49c56f8fb3856a530baf9f4780cab8f7dfb703b
Fred Tibbitts III <44442080+fredtibbitts@users.noreply.github.com>
2023-04-21 14:34:57 -0400

    VZ-8749: Add update validators for Prometheus components (#5840)

--- platform-operator/controllers/verrazzano/component/prometheus/kubestatemetrics/kubestatemetrics_component_test.go
+++ platform-operator/controllers/verrazzano/component/prometheus/kubestatemetrics/kubestatemetrics_component_test.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	vzapi "github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1alpha1"
+	"github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1beta1"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/spi"
 )
 
@@ -161,4 +162,111 @@ func TestAppendNGINXOverrides(t *testing.T) {
 	kvs, err := AppendOverrides(spi.NewFakeContext(nil, vz, nil, false), ComponentName, ComponentNamespace, "", []bom.KeyValue{})
 	assert.NoError(t, err)
 	assert.Len(t, kvs, 1)
+}
+
+// TestValidateUpdate tests the validate update functions
+func TestValidateUpdate(t *testing.T) {
+	falseValue := false
+	trueValue := true
+	tests := []struct {
+		name    string
+		old     *vzapi.Verrazzano
+		new     *vzapi.Verrazzano
+		wantErr bool
+	}{
+		{
+			// GIVEN the component is disabled
+			// WHEN the component is enabled and we call the validate update function
+			// THEN no error is returned
+			name: "enable",
+			old: &vzapi.Verrazzano{
+				Spec: vzapi.VerrazzanoSpec{
+					Components: vzapi.ComponentSpec{
+						KubeStateMetrics: &vzapi.KubeStateMetricsComponent{
+							Enabled: &falseValue,
+						},
+					},
+				},
+			},
+			new: &vzapi.Verrazzano{
+				Spec: vzapi.VerrazzanoSpec{
+					Components: vzapi.ComponentSpec{
+						KubeStateMetrics: &vzapi.KubeStateMetricsComponent{
+							Enabled: &trueValue,
+						},
+					},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			// GIVEN the component is enabled
+			// WHEN the component is disabled and we call the validate update function
+			// THEN an error is returned
+			name: "disable",
+			old: &vzapi.Verrazzano{
+				Spec: vzapi.VerrazzanoSpec{
+					Components: vzapi.ComponentSpec{
+						KubeStateMetrics: &vzapi.KubeStateMetricsComponent{
+							Enabled: &trueValue,
+						},
+					},
+				},
+			},
+			new: &vzapi.Verrazzano{
+				Spec: vzapi.VerrazzanoSpec{
+					Components: vzapi.ComponentSpec{
+						KubeStateMetrics: &vzapi.KubeStateMetricsComponent{
+							Enabled: &falseValue,
+						},
+					},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			// GIVEN the component is enabled
+			// WHEN the component is not changed and we call the validate update function
+			// THEN no error is returned
+			name: "no change",
+			old: &vzapi.Verrazzano{
+				Spec: vzapi.VerrazzanoSpec{
+					Components: vzapi.ComponentSpec{
+						KubeStateMetrics: &vzapi.KubeStateMetricsComponent{
+							Enabled: &trueValue,
+						},
+					},
+				},
+			},
+			new: &vzapi.Verrazzano{
+				Spec: vzapi.VerrazzanoSpec{
+					Components: vzapi.ComponentSpec{
+						KubeStateMetrics: &vzapi.KubeStateMetricsComponent{
+							Enabled: &trueValue,
+						},
+					},
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := NewComponent()
+			if err := c.ValidateUpdate(tt.old, tt.new); (err != nil) != tt.wantErr {
+				t.Errorf("ValidateUpdate() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			v1beta1New := &v1beta1.Verrazzano{}
+			v1beta1Old := &v1beta1.Verrazzano{}
+			err := tt.new.ConvertTo(v1beta1New)
+			assert.NoError(t, err)
+			err = tt.old.ConvertTo(v1beta1Old)
+			assert.NoError(t, err)
+
+			if err := c.ValidateUpdateV1Beta1(v1beta1Old, v1beta1New); (err != nil) != tt.wantErr {
+				t.Errorf("ValidateUpdateV1Beta1() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
 }
--- platform-operator/controllers/verrazzano/component/prometheus/nodeexporter/nodeexporter_component_test.go
+++ platform-operator/controllers/verrazzano/component/prometheus/nodeexporter/nodeexporter_component_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2022, Oracle and/or its affiliates.
+// Copyright (c) 2022, 2023, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 package nodeexporter
@@ -9,6 +9,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/verrazzano/verrazzano/pkg/bom"
 	vzapi "github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1alpha1"
+	"github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1beta1"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/spi"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
@@ -251,4 +252,111 @@ func TestPreInstallcomponent(t *testing.T) {
 	c := fake.NewClientBuilder().Build()
 	ctx := spi.NewFakeContext(c, &vzapi.Verrazzano{}, nil, true)
 	assert.Nil(t, NewComponent().PreInstall(ctx))
+}
+
+// TestValidateUpdate tests the validate update functions
+func TestValidateUpdate(t *testing.T) {
+	falseValue := false
+	trueValue := true
+	tests := []struct {
+		name    string
+		old     *vzapi.Verrazzano
+		new     *vzapi.Verrazzano
+		wantErr bool
+	}{
+		{
+			// GIVEN the component is disabled
+			// WHEN the component is enabled and we call the validate update function
+			// THEN no error is returned
+			name: "enable",
+			old: &vzapi.Verrazzano{
+				Spec: vzapi.VerrazzanoSpec{
+					Components: vzapi.ComponentSpec{
+						PrometheusNodeExporter: &vzapi.PrometheusNodeExporterComponent{
+							Enabled: &falseValue,
+						},
+					},
+				},
+			},
+			new: &vzapi.Verrazzano{
+				Spec: vzapi.VerrazzanoSpec{
+					Components: vzapi.ComponentSpec{
+						PrometheusNodeExporter: &vzapi.PrometheusNodeExporterComponent{
+							Enabled: &trueValue,
+						},
+					},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			// GIVEN the component is enabled
+			// WHEN the component is disabled and we call the validate update function
+			// THEN an error is returned
+			name: "disable",
+			old: &vzapi.Verrazzano{
+				Spec: vzapi.VerrazzanoSpec{
+					Components: vzapi.ComponentSpec{
+						PrometheusNodeExporter: &vzapi.PrometheusNodeExporterComponent{
+							Enabled: &trueValue,
+						},
+					},
+				},
+			},
+			new: &vzapi.Verrazzano{
+				Spec: vzapi.VerrazzanoSpec{
+					Components: vzapi.ComponentSpec{
+						PrometheusNodeExporter: &vzapi.PrometheusNodeExporterComponent{
+							Enabled: &falseValue,
+						},
+					},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			// GIVEN the component is enabled
+			// WHEN the component is not changed and we call the validate update function
+			// THEN no error is returned
+			name: "no change",
+			old: &vzapi.Verrazzano{
+				Spec: vzapi.VerrazzanoSpec{
+					Components: vzapi.ComponentSpec{
+						PrometheusNodeExporter: &vzapi.PrometheusNodeExporterComponent{
+							Enabled: &trueValue,
+						},
+					},
+				},
+			},
+			new: &vzapi.Verrazzano{
+				Spec: vzapi.VerrazzanoSpec{
+					Components: vzapi.ComponentSpec{
+						PrometheusNodeExporter: &vzapi.PrometheusNodeExporterComponent{
+							Enabled: &trueValue,
+						},
+					},
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := NewComponent()
+			if err := c.ValidateUpdate(tt.old, tt.new); (err != nil) != tt.wantErr {
+				t.Errorf("ValidateUpdate() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			v1beta1New := &v1beta1.Verrazzano{}
+			v1beta1Old := &v1beta1.Verrazzano{}
+			err := tt.new.ConvertTo(v1beta1New)
+			assert.NoError(t, err)
+			err = tt.old.ConvertTo(v1beta1Old)
+			assert.NoError(t, err)
+
+			if err := c.ValidateUpdateV1Beta1(v1beta1Old, v1beta1New); (err != nil) != tt.wantErr {
+				t.Errorf("ValidateUpdateV1Beta1() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
 }
