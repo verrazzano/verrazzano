0b284e9b766c660eb80d3cb7f04a962ef1d04abe
Michael Cico <46466506+michael-cico@users.noreply.github.com>
2023-04-25 19:10:05 -0400

    Fix up after rebase

--- platform-operator/controllers/verrazzano/component/certmanagerconfig/certmanager_config.go
+++ platform-operator/controllers/verrazzano/component/certmanagerconfig/certmanager_config.go
@@ -10,8 +10,6 @@ import (
 	"encoding/pem"
 	"errors"
 	"fmt"
-	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/common"
-	apiextensionsv1client "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset/typed/apiextensions/v1"
 	"net/mail"
 	"strings"
 	"text/template"
