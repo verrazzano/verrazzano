3c3c9006102f9ed727d466b0e15748143a2116e5
davisgomes <49332517+davisgomes@users.noreply.github.com>
2023-04-28 15:51:39 -0400

    REmove quotes from for loop

--- ci/scripts/generate_vz_distribution.sh
+++ ci/scripts/generate_vz_distribution.sh
@@ -322,7 +322,7 @@ loadExampleTarFiles() {
     local generatedDir="$3"
 
   IMAGE_LIST=$(grep -r 'image:' ~/go/src/github.com/verrazzano/verrazzano/examples | grep -E '(ghcr\.io|container\-registry\.oracle\.com){1}(/.+)+:[^"]+')
-  for image in "${IMAGE_LIST[@]}"; do
+  for image in ${IMAGE_LIST[@]}; do
     docker pull "$image"
   done
 
