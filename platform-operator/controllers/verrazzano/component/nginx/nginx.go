e2dcde88dbbae35f0539855fde8775b4aa573df0
Jonathan Maron <jonathan.maron@oracle.com>
2023-05-01 09:37:32 -0400

    copyright

--- platform-operator/controllers/verrazzano/component/nginx/nginx.go
+++ platform-operator/controllers/verrazzano/component/nginx/nginx.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2021, 2022, Oracle and/or its affiliates.
+// Copyright (c) 2021, 2023, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 package nginx
@@ -62,7 +62,7 @@ func AppendOverrides(context spi.ComponentContext, _ string, _ string, _ string,
 	newKvs = append(newKvs, helm.GetInstallArgs(getInstallArgs(cr))...)
 
 	newKvs = append(newKvs, bom.KeyValue{Key: "controller.allowSnippetAnnotations", Value: "true"})
-	
+
 	return newKvs, nil
 }
 
