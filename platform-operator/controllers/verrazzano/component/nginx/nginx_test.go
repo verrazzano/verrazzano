34aced7e3d1ba0303f96c14cae78beea0b991c22
Jonathan Maron <67696706+jmaron99@users.noreply.github.com>
2023-05-03 08:59:43 -0400

    Revert "JIRA: VZ-5060 add streaming directives to nginx configuration to address websocket errors in Rancher console (#5880)"
    This reverts commit c3cde21ab5b663ce1a50f00d66f9c84b8f69170f.

--- platform-operator/controllers/verrazzano/component/nginx/nginx_test.go
+++ platform-operator/controllers/verrazzano/component/nginx/nginx_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2021, 2023, Oracle and/or its affiliates.
+// Copyright (c) 2021, 2022, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 package nginx
@@ -39,7 +39,7 @@ func TestAppendNGINXOverrides(t *testing.T) {
 	vz := &vzapi.Verrazzano{}
 	kvs, err := AppendOverrides(spi.NewFakeContext(nil, vz, nil, false), ComponentName, nginxutil.IngressNGINXNamespace(), "", []bom.KeyValue{})
 	assert.NoError(t, err)
-	assert.Len(t, kvs, 2)
+	assert.Len(t, kvs, 1)
 }
 
 // TestAppendNGINXOverridesWithInstallArgs tests the AppendOverrides fn
@@ -63,7 +63,7 @@ func TestAppendNGINXOverridesWithInstallArgs(t *testing.T) {
 	}
 	kvs, err := AppendOverrides(spi.NewFakeContext(nil, vz, nil, false), ComponentName, nginxutil.IngressNGINXNamespace(), "", []bom.KeyValue{})
 	assert.NoError(t, err)
-	assert.Len(t, kvs, 5)
+	assert.Len(t, kvs, 4)
 }
 
 // TestAppendNGINXOverridesExtraKVs tests the AppendOverrides fn
@@ -93,7 +93,7 @@ func TestAppendNGINXOverridesWithExternalDNS(t *testing.T) {
 	}
 	kvs, err := AppendOverrides(spi.NewFakeContext(nil, vz, nil, false), ComponentName, nginxutil.IngressNGINXNamespace(), "", []bom.KeyValue{})
 	assert.NoError(t, err)
-	assert.Len(t, kvs, 7)
+	assert.Len(t, kvs, 6)
 }
 
 // TestAppendNGINXOverridesExtraKVs tests the AppendOverrides fn
@@ -112,7 +112,7 @@ func TestAppendNGINXOverridesExtraKVs(t *testing.T) {
 	}
 	kvs, err := AppendOverrides(spi.NewFakeContext(nil, vz, nil, false), ComponentName, nginxutil.IngressNGINXNamespace(), "", kvs)
 	assert.NoError(t, err)
-	assert.Len(t, kvs, 3)
+	assert.Len(t, kvs, 2)
 }
 
 // TestNGINXPreInstall tests the PreInstall fn
