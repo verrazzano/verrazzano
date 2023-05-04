a62c8816c82ef7be4bf5246800df39b2e6371a3f
Abijitha Nadagouda <29689969+Abhijithang@users.noreply.github.com>
2023-05-04 08:35:43 -0700

    Fix imports

--- platform-operator/controllers/verrazzano/component/capi/capi_component.go
+++ platform-operator/controllers/verrazzano/component/capi/capi_component.go
@@ -11,6 +11,7 @@ import (
 	"github.com/verrazzano/verrazzano/pkg/vzcr"
 	"github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1alpha1"
 	"github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1beta1"
+	vpoconstants "github.com/verrazzano/verrazzano/platform-operator/constants"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/certmanager"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/spi"
 	appsv1 "k8s.io/api/apps/v1"
@@ -114,7 +115,7 @@ func (c capiComponent) IsEnabled(effectiveCR runtime.Object) bool {
 
 // GetMinVerrazzanoVersion returns the minimum Verrazzano version required by the component
 func (c capiComponent) GetMinVerrazzanoVersion() string {
-	return constants.VerrazzanoVersion1_6_0
+	return vpoconstants.VerrazzanoVersion1_6_0
 }
 
 // GetIngressNames returns the list of ingress names associated with the component
