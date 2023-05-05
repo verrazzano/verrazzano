293888f3afbc76c67b12d0f2e367338cfc985652
Michael Cico <46466506+michael-cico@users.noreply.github.com>
2023-05-05 15:11:17 -0400

    Backport newline fix to release-1.3 (#5931)
    * Prepend/append newline to compartment field when copying the OCI DNS secret
    
    * Fix copyright

--- platform-operator/controllers/verrazzano/component/externaldns/external_dns.go
+++ platform-operator/controllers/verrazzano/component/externaldns/external_dns.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2021, 2022, Oracle and/or its affiliates.
+// Copyright (c) 2021, 2023, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 package externaldns
@@ -74,7 +74,7 @@ func preInstall(compContext spi.ComponentContext) error {
 
 		// Extract data and create secret in the external DNS namespace
 		for k := range dnsSecret.Data {
-			externalDNSSecret.Data[ociSecretFileName] = append(dnsSecret.Data[k], []byte(fmt.Sprintf("compartment: %s", dns.OCI.DNSZoneCompartmentOCID))...)
+			externalDNSSecret.Data[ociSecretFileName] = append(dnsSecret.Data[k], []byte(fmt.Sprintf("\ncompartment: %s\n", dns.OCI.DNSZoneCompartmentOCID))...)
 		}
 
 		return nil
