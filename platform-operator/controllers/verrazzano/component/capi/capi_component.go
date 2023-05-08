5c9b3835203a0f8cdf258a8c07835733eb44e81e
Robert Donat <robert.donat@oracle.com>
2023-05-08 13:48:02 +0000

    fix fmt

--- platform-operator/controllers/verrazzano/component/capi/capi_component.go
+++ platform-operator/controllers/verrazzano/component/capi/capi_component.go
@@ -248,10 +248,10 @@ func (c capiComponent) Upgrade(ctx spi.ComponentContext) error {
 
 	// Set up the upgrade options for the CAPI apply upgrade.
 	applyUpgradeOptions := clusterapi.ApplyUpgradeOptions{
-		CoreProvider:            fmt.Sprintf("%S/%s:%s", ComponentNamespace, clusterAPIProviderName, overrides.APIVersion),
-		BootstrapProviders:      []string{fmt.Sprintf("%S/%s:%s", ComponentNamespace, ocneProviderName, overrides.OCNEBootstrapVersion)},
-		ControlPlaneProviders:   []string{fmt.Sprintf("%S/%s:%s", ComponentNamespace, ocneProviderName, overrides.OCNEControlPlaneVersion)},
-		InfrastructureProviders: []string{fmt.Sprintf("%S/%s:%s", ComponentNamespace, ociProviderName, overrides.OCIVersion)},
+		CoreProvider:            fmt.Sprintf("%s/%s:%s", ComponentNamespace, clusterAPIProviderName, overrides.APIVersion),
+		BootstrapProviders:      []string{fmt.Sprintf("%s/%s:%s", ComponentNamespace, ocneProviderName, overrides.OCNEBootstrapVersion)},
+		ControlPlaneProviders:   []string{fmt.Sprintf("%s/%s:%s", ComponentNamespace, ocneProviderName, overrides.OCNEControlPlaneVersion)},
+		InfrastructureProviders: []string{fmt.Sprintf("%s/%s:%s", ComponentNamespace, ociProviderName, overrides.OCIVersion)},
 	}
 
 	return capiClient.ApplyUpgrade(applyUpgradeOptions)
