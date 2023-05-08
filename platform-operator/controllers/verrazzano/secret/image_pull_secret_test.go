d05841c81596a11080fca46fd175661c36ea22ac
Fred Tibbitts III <44442080+fredtibbitts@users.noreply.github.com>
2023-05-08 12:49:47 -0400

    VZ-9531: Support installing VPO image from private registry with credentials (#5941)

--- platform-operator/controllers/verrazzano/secret/image_pull_secret_test.go
+++ platform-operator/controllers/verrazzano/secret/image_pull_secret_test.go
@@ -1,13 +1,14 @@
-// Copyright (c) 2021, 2022, Oracle and/or its affiliates.
+// Copyright (c) 2021, 2023, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 package secret
 
 import (
 	"context"
 	"fmt"
+	"testing"
+
 	"github.com/verrazzano/verrazzano/pkg/bom"
 	"github.com/verrazzano/verrazzano/pkg/log/vzlog"
-	"testing"
 
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
@@ -228,4 +229,22 @@ func TestAddImagePullSecretTargetSecretCopied(t *testing.T) {
 		assert.Containsf(t, []string{"key1", "key2", "helmKey"}, kv.Key, "Did not have Key %s", kv.Key)
 		assert.Containsf(t, []string{"value1", "value2", constants.GlobalImagePullSecName}, kv.Value, "Did not have Value", kv.Value)
 	}
+}
+
+// TestCheckImagePullSecretInstallNamespace tests the CheckImagePullSecret function
+// GIVEN a call to CheckImagePullSecret
+// WHEN the pull secret exists in the verrazzano-install namespace
+// THEN true is returned and the secret is copied to the target namespace
+func TestCheckImagePullSecretInstallNamespace(t *testing.T) {
+	name := types.NamespacedName{Name: constants.GlobalImagePullSecName, Namespace: constants.VerrazzanoInstallNamespace}
+	fakeClient := fake.NewClientBuilder().WithScheme(k8scheme.Scheme).WithObjects(
+		&corev1.Secret{ObjectMeta: metav1.ObjectMeta{Name: name.Name, Namespace: name.Namespace}},
+	).Build()
+	copied, err := CheckImagePullSecret(fakeClient, constants.VerrazzanoSystemNamespace)
+	assert.NoError(t, err)
+	assert.True(t, copied)
+
+	secret := &corev1.Secret{}
+	err = fakeClient.Get(context.TODO(), types.NamespacedName{Namespace: constants.VerrazzanoSystemNamespace, Name: constants.GlobalImagePullSecName}, secret)
+	assert.NoError(t, err)
 }
