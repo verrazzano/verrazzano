2e9a0dfa9108bf4686c4e1ca16bed07a95c604e0
Paul Mackin <paul.mackin@oracle.com>
2023-05-12 09:37:57 -0400

    VZ-9644 verrazzano argocd component must not overwrite policy.csv (#5987)

--- platform-operator/controllers/verrazzano/component/argocd/argocd_postinstall.go
+++ platform-operator/controllers/verrazzano/component/argocd/argocd_postinstall.go
@@ -6,6 +6,7 @@ package argocd
 import (
 	"context"
 	"fmt"
+	"strings"
 	"time"
 
 	vzconst "github.com/verrazzano/verrazzano/pkg/constants"
@@ -138,7 +139,7 @@ func patchArgoCDRbacConfigMap(ctx spi.ComponentContext) error {
 		},
 	}
 
-	var policyString = `g, verrazzano-admins, role:admin`
+	var adminPolicy = `g, verrazzano-admins, role:admin`
 	var err error
 
 	// Disable the built-in admin user. Grant admin (role:admin) to verrazzano-admins group
@@ -146,7 +147,17 @@ func patchArgoCDRbacConfigMap(ctx spi.ComponentContext) error {
 		if rbaccm.Data == nil {
 			rbaccm.Data = make(map[string]string)
 		}
-		rbaccm.Data["policy.csv"] = policyString
+		// Make sure the policy.csv has the verrazzano admin policy
+		policyCSV, ok := rbaccm.Data["policy.csv"]
+		if !ok || len(policyCSV) == 0 {
+			// There is no policy.csv override, Add the verrazzano admin
+			rbaccm.Data["policy.csv"] = adminPolicy
+		} else if !strings.Contains(policyCSV, adminPolicy) {
+			// The policy.csv exists, but doesn't have the verrazzano admin policy.  Add it.
+			trim := strings.TrimSpace(policyCSV)
+			s := fmt.Sprintf("%s\n%s", trim, adminPolicy)
+			rbaccm.Data["policy.csv"] = s
+		}
 		return nil
 	}); err != nil {
 		ctx.Log().ErrorfNewErr("Failed to patch the Argo CD configmap argocd-rbac-cm: %s", err)
