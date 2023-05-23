b4c2fcb85d2c2ef23816c7c6f194061e3af4b68b
Neeraj Singh <neeraj.n.singh@oracle.com>
2023-05-23 11:30:44 +0530

    VZ:9063: Create new kind images. (#5967)
    * New Kind Images.

--- ci/scripts/create_kind_clusters.sh
+++ ci/scripts/create_kind_clusters.sh
@@ -41,18 +41,20 @@ create_kind_cluster() {
     return 0
   fi
 
-  if [ ${K8S_VERSION} == 1.21 ]; then
-    KIND_IMAGE="v1.21.12@sha256:f316b33dd88f8196379f38feb80545ef3ed44d9197dca1bfd48bcb1583210207"
-  elif [ ${K8S_VERSION} == 1.22 ]; then
-    KIND_IMAGE="v1.22.9@sha256:8135260b959dfe320206eb36b3aeda9cffcb262f4b44cda6b33f7bb73f453105"
-  elif [ ${K8S_VERSION} == 1.23 ]; then
-    KIND_IMAGE="v1.23.6@sha256:b1fa224cc6c7ff32455e0b1fd9cbfd3d3bc87ecaa8fcb06961ed1afb3db0f9ae"
-  elif [ ${K8S_VERSION} == 1.24 ]; then
-    KIND_IMAGE="v1.24.0@sha256:0866296e693efe1fed79d5e6c7af8df71fc73ae45e3679af05342239cdc5bc8e"
-  else
-    echo "ERROR: Invalid value for Kubernetes Version ${K8S_VERSION}."
-    exit 1
-  fi
+   if [ "${K8S_VERSION}" == "1.21" ]; then
+      KIND_IMAGE="ghcr.io/verrazzano/kind:v1.21.14-20230510140100-7d934451"
+    elif [ "${K8S_VERSION}" == "1.22" ]; then
+      KIND_IMAGE="ghcr.io/verrazzano/kind:v1.22.15-20230510133529-7d934451"
+    elif [ "${K8S_VERSION}" == "1.23" ]; then
+      KIND_IMAGE="ghcr.io/verrazzano/kind:v1.23.13-20230510133509-7d934451"
+    elif [ "${K8S_VERSION}" == "1.24" ]; then
+      KIND_IMAGE="ghcr.io/verrazzano/kind:v1.24.10-20230510133459-7d934451"
+    elif [ "${K8S_VERSION}" == "1.25" ]; then
+      KIND_IMAGE="ghcr.io/verrazzano/kind:v1.25.8-20230510133540-7d934451"
+    else
+      echo "ERROR: Invalid value for Kubernetes Version ${K8S_VERSION}."
+      exit 1
+   fi
 
   if [ $SETUP_CALICO == true ] ; then
     CALICO_SUFFIX="-calico"
@@ -98,9 +100,9 @@ create_kind_cluster() {
   for (( n=2; n<=${NODE_COUNT}; n++ ))
   do
     echo "  - role: worker" >> ${KIND_CONFIG_FILE}
-    echo "    image: kindest/node:KIND_IMAGE" >> ${KIND_CONFIG_FILE}
+    echo "    image: KIND_IMAGE" >> ${KIND_CONFIG_FILE}
   done
-  sed -i -e "s/KIND_IMAGE/${KIND_IMAGE}/g" ${KIND_CONFIG_FILE}
+  sed -i -e "s|KIND_IMAGE|${KIND_IMAGE}|g" ${KIND_CONFIG_FILE}
 
   # Use a custom API server address if specified; defaults to 127.0.0.1
   echo "Setting the cluster API server address to ${KIND_API_SERVER_ADDRESS}"
