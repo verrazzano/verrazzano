764e4a034e53c6774db5258c4d661943d2ae0d65
Michael Cico <46466506+michael-cico@users.noreply.github.com>
2023-05-24 10:34:08 -0400

    Issuer utils and unit tests

---
+++ platform-operator/apis/verrazzano/v1beta1/issuer_utils_test.go
@@ -0,0 +1,156 @@
+// Copyright (c) 2023, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package v1beta1
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/verrazzano/verrazzano/pkg/constants"
+	"testing"
+)
+
+// TestNewDefaultClusterIssuer Tests the TestNewDefaultClusterIssuer constructor
+// GIVEN a call to NewDefaultClusterIssuer()
+// THEN a valid ClusterIssuerComponent is returned with the default self-signed CA issuer configuration
+func TestNewDefaultClusterIssuer(t *testing.T) {
+	asserts := assert.New(t)
+	issuer := NewDefaultClusterIssuer()
+
+	asserts.Equal(constants.CertManagerNamespace, issuer.ClusterResourceNamespace)
+	asserts.Equal(constants.DefaultVerrazzanoCASecretName, issuer.CA.SecretName)
+	asserts.Nil(issuer.LetsEncrypt)
+
+	isDefaultIssuer, err := issuer.IsDefaultIssuer()
+	asserts.True(isDefaultIssuer)
+	asserts.NoError(err)
+
+	isCAIssuer, err := issuer.IsCAIssuer()
+	asserts.True(isCAIssuer)
+	asserts.NoError(err)
+
+	isACMEIssuer, err := issuer.IsLetsEncryptIssuer()
+	asserts.False(isACMEIssuer)
+	asserts.NoError(err)
+}
+
+// TestClusterIssuerComponent_IsCAIssuer_IsLEIssuer Tests the IsCAIssuer and IsLetsEncryptIssuer methods
+// GIVEN a call to IsCAIssuer() or IsLetsEncryptIssuer() for various configurations
+// THEN the functions behave as expected
+func TestClusterIssuerComponent_IsCAIssuer_IsLEIssuer(t *testing.T) {
+	asserts := assert.New(t)
+
+	caIssuer := ClusterIssuerComponent{
+		Enabled:                  newBool(true),
+		ClusterResourceNamespace: constants.CertManagerNamespace,
+		IssuerConfig: IssuerConfig{
+			CA: &CAIssuer{SecretName: "myCA"},
+		},
+	}
+
+	isCAIssuer, err := caIssuer.IsCAIssuer()
+	asserts.True(isCAIssuer)
+	asserts.NoError(err)
+
+	isACMEIssuer, err := caIssuer.IsLetsEncryptIssuer()
+	asserts.False(isACMEIssuer)
+	asserts.NoError(err)
+
+	leIssuer := ClusterIssuerComponent{
+		Enabled:                  newBool(true),
+		ClusterResourceNamespace: constants.CertManagerNamespace,
+		IssuerConfig: IssuerConfig{
+			LetsEncrypt: &LetsEncryptACMEIssuer{
+				EmailAddress: "foo@bar.com",
+				Environment:  "staging",
+			},
+		},
+	}
+
+	isCAIssuer, err = leIssuer.IsCAIssuer()
+	asserts.False(isCAIssuer)
+	asserts.NoError(err)
+
+	isACMEIssuer, err = leIssuer.IsLetsEncryptIssuer()
+	asserts.True(isACMEIssuer)
+	asserts.NoError(err)
+}
+
+// TestClusterIssuerComponent_NotDefaultIssuer Tests the IsDefaultIssuer method
+// GIVEN a call to IsDefaultIssuer()
+// WHEN the issuer configuration is not a default self-signed configuration
+// THEN the function returns false, or an error if the issuer is misconfigured
+func TestClusterIssuerComponent_NotDefaultIssuer(t *testing.T) {
+	asserts := assert.New(t)
+
+	// Test default clusterResourceNamespace, non-default secret name
+	caIssuer1 := ClusterIssuerComponent{
+		Enabled:                  newBool(true),
+		ClusterResourceNamespace: constants.CertManagerNamespace,
+		IssuerConfig: IssuerConfig{
+			CA: &CAIssuer{SecretName: "myCA"},
+		},
+	}
+
+	isDefIssuer1, err := caIssuer1.IsDefaultIssuer()
+	asserts.False(isDefIssuer1)
+	asserts.NoError(err)
+
+	// Test default secret name, non-default cluster resource namespace
+	caIssuer2 := ClusterIssuerComponent{
+		Enabled:                  newBool(true),
+		ClusterResourceNamespace: "mycluserResourceNamespace",
+		IssuerConfig: IssuerConfig{
+			CA: &CAIssuer{SecretName: constants.DefaultVerrazzanoCASecretName},
+		},
+	}
+	isDefIssuer2, err := caIssuer2.IsDefaultIssuer()
+	asserts.False(isDefIssuer2)
+	asserts.NoError(err)
+
+	// Test LE issuer
+	leIssuer := ClusterIssuerComponent{
+		Enabled:                  newBool(true),
+		ClusterResourceNamespace: constants.CertManagerNamespace,
+		IssuerConfig: IssuerConfig{
+			LetsEncrypt: &LetsEncryptACMEIssuer{
+				EmailAddress: "foo@bar.com",
+				Environment:  "staging",
+			},
+		},
+	}
+	isDefIssuer3, err := leIssuer.IsDefaultIssuer()
+	asserts.False(isDefIssuer3)
+	asserts.NoError(err)
+}
+
+// TestClusterIssuerComponent_BadIssuerConfig Tests the various IsXXX method
+// GIVEN a call to IsXXXXIssuer()
+// WHEN the issuer configuration is invalid
+// THEN the functions returns false and an error
+func TestClusterIssuerComponent_BadIssuerConfig(t *testing.T) {
+	asserts := assert.New(t)
+
+	badConfig := ClusterIssuerComponent{
+		Enabled:                  newBool(true),
+		ClusterResourceNamespace: constants.CertManagerNamespace,
+		IssuerConfig: IssuerConfig{
+			CA: &CAIssuer{SecretName: "myCA"},
+			LetsEncrypt: &LetsEncryptACMEIssuer{
+				EmailAddress: "foo@bar.com",
+				Environment:  "staging",
+			},
+		},
+	}
+
+	isCAIssuer, err := badConfig.IsCAIssuer()
+	asserts.Error(err)
+	asserts.False(isCAIssuer)
+
+	isLEIssuer, err := badConfig.IsLetsEncryptIssuer()
+	asserts.Error(err)
+	asserts.False(isLEIssuer)
+
+	isDefIssuer, err := badConfig.IsDefaultIssuer()
+	asserts.Error(err)
+	asserts.False(isDefIssuer)
+}
