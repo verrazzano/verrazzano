f2cb68d3e0250913ce1a05525b0691c30b05e3bb
Deva Sagar <deva.sagar@oracle.com>
2023-06-05 13:46:40 -0400

    Fix relative path in VMC doc

--- cluster-operator/apis/clusters/v1alpha1/verrazzanomanagedcluster_types.go
+++ cluster-operator/apis/clusters/v1alpha1/verrazzanomanagedcluster_types.go
@@ -16,7 +16,7 @@ type VerrazzanoManagedClusterSpec struct {
 	// The name of a Secret that contains the CA certificate of the managed cluster. This is used to configure the
 	// admin cluster to scrape metrics from the Prometheus endpoint on the managed cluster. If Rancher is enabled on
 	// the admin cluster, this will be automatically populated by Verrazzano. Otherwise, if you disabled Rancher, see the pre-registration
-	// <a href="../../../../docs/setup/mc-install/advanced-mc-install/#preregistration-setup">instructions</a>
+	// <a href="../../../docs/setup/mc-install/advanced-mc-install/#preregistration-setup">instructions</a>
 	// for how to create this Secret.
 	CASecret string `json:"caSecret,omitempty"`
 
