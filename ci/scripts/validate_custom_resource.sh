5b278aafaecbc930d3a5ada2e40976af45159f05
Abraham Hernandez <abraham.hernandez@oracle.com>
2023-06-07 17:00:56 -0700

    Update validate_custom_resource.sh

--- ci/scripts/validate_custom_resource.sh
+++ ci/scripts/validate_custom_resource.sh
@@ -4,23 +4,25 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 #
 
-OPERATOR_YAML=$1
+INVALID_OPERATOR_YAML=$1
 VPO_YAML=$2
 VALIDATION_ERROR="ValidationError"
-cd "$WORKSPACE"
 
-ERROR1=$(./vz install --filename "$OPERATOR_YAML" --manifests "$VPO_YAML" 2>&1 >/dev/null)  
+cd "$WORKSPACE"
+ERROR1=$(./vz install --filename "$INVALID_OPERATOR_YAML" --manifests "$VPO_YAML" 2>&1 >/dev/null)  
 if [[ "$ERROR1" =~ .*"$VALIDATION_ERROR".* ]]; then
-    echo "Error: $VALIDATION_ERROR was caught as expected"
+    echo "Expected Error: $VALIDATION_ERROR, Actual Error: $VALIDATION_ERROR"
+    echo "Error: $VALIDATION_ERROR was caught"
 else 
-    echo "Expected $VALIDATION_ERROR in invalidCR.yaml" 
+    echo "Expected Error: $VALIDATION_ERROR from $INVALID_OPERATOR_YAML, Actual Error: $ERROR1" 
     exit 1
 fi
 
 ERROR2=$(./vz install --set trash=foo --manifests "$VPO_YAML" 2>&1 >/dev/null)
 if [[ "$ERROR2" =~ .*"$VALIDATION_ERROR".* ]]; then
-    echo "Error was caught as expected"
+    echo "Expected Error: $VALIDATION_ERROR, Actual Error: $VALIDATION_ERROR"
+    echo "Error: $VALIDATION_ERROR was caught"
 else
-    echo "Expected $VALIDATION_ERROR from field(s) trash=foo"
+    echo "Expected Error: $VALIDATION_ERROR from field(s) trash=foo, Actual Error: $ERROR2"
     exit 1
 fi
