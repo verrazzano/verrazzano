8bbc2a6d33234397712ef14eab20b36911ba9883
ben.mclean <ben.mclean@oracle.com>
2023-06-22 12:06:48 -0400

    create the namespace

--- ci/scripts/install_third_party_components.sh
+++ ci/scripts/install_third_party_components.sh
@@ -7,6 +7,11 @@
 echo "Installing cert-manager via helm chart"
 echo "Setting clusterResourceNamespace to $CLUSTER_RESOURCE_NAMESPACE"
 
+if [ -z "$(kubectl get ns | grep my-cert-manager)" ]
+then
+  kubectl create ns my-cert-manager
+fi
+
 if [ $CLUSTER_RESOURCE_NAMESPACE != my-cert-manager ]
 then
   if [ -z "$(kubectl get ns | grep $CLUSTER_RESOURCE_NAMESPACE)" ]
@@ -23,7 +28,7 @@ helm upgrade cert-manager -n my-cert-manager platform-operator/thirdparty/charts
 --set cainjector.image.repository=ghcr.io/verrazzano/cert-manager-cainjector --set cainjector.image.tag=${cainjectorTag}  \
 --set webhook.image.repository=ghcr.io/verrazzano/cert-manager-webhook --set webhook.image.tag=${webhookTag} \
 --set startupapicheck.enabled=false --set clusterResourceNamespace=${CLUSTER_RESOURCE_NAMESPACE} \
---set installCRDs=true --install --create-namespace
+--set installCRDs=true --install
 
 echo "ensure cert-manager using ghcr.io images"
 if [ ! -z "$(kubectl get po -n my-cert-manager -o yaml | grep quay.io)" ]
@@ -36,6 +41,10 @@ kubectl get pods -n my-cert-manager
 
 echo "Installing ingress-nginx via helm chart"
 
+if [ -z "$(kubectl get ns | grep ingress-nginx)" ]
+then
+  kubectl create ns ingress-nginx
+fi
 
 controllerTag=$(cat platform-operator/verrazzano-bom.json | jq '.components[] | select(.name=="ingress-nginx")' | jq '.subcomponents[0].images[] | select(.image=="nginx-ingress-controller")' | jq .tag -r)
 defaultBackendTag=$(cat platform-operator/verrazzano-bom.json | jq '.components[] | select(.name=="ingress-nginx")' | jq '.subcomponents[0].images[] | select(.image=="nginx-ingress-default-backend")' | jq .tag -r)
@@ -43,7 +52,7 @@ defaultBackendTag=$(cat platform-operator/verrazzano-bom.json | jq '.components[
 helm upgrade ingress-controller -n ingress-nginx platform-operator/thirdparty/charts/ingress-nginx \
 --set controller.image.digest="" --set controller.image.repository=ghcr.io/verrazzano/nginx-ingress-controller --set controller.image.tag=${controllerTag}  \
 --set defaultBackend.image.repository=ghcr.io/verrazzano/nginx-ingress-default-backend --set defaultBackend.image.tag=${defaultBackendTag} \
---set defaultBackend.enabled=true --install  --create-namespace
+--set defaultBackend.enabled=true --install
 
 echo "ensure cert-manager using ghcr.io images"
 if [ ! -z "$(kubectl get po -n cert-manager -o yaml | grep registry.k8s.io)" ]
