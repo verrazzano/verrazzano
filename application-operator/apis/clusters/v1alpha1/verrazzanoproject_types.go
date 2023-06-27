1c1618a7a87544ae7ceb841be6010a826756f6b1
Robert Donat <robert.donat@oracle.com>
2023-06-27 09:56:13 -0400

    Fix network policies link in API docs (#6343)

--- application-operator/apis/clusters/v1alpha1/verrazzanoproject_types.go
+++ application-operator/apis/clusters/v1alpha1/verrazzanoproject_types.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2021, 2022, Oracle and/or its affiliates.
+// Copyright (c) 2021, 2023, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 package v1alpha1
@@ -25,7 +25,7 @@ type NamespaceTemplate struct {
 // NetworkPolicyTemplate contains the metadata and specification of a Kubernetes NetworkPolicy.
 // <div class="alert alert-warning" role="alert">
 // <h4 class="alert-heading">NOTE</h4>
-// To add an application NetworkPolicy, see <a href="../../../../docs/networking/security/net-security/#networkpolicies-for-applications">NetworkPolicies for applications</a>.
+// To add an application NetworkPolicy, see <a href="../../../docs/networking/security/#networkpolicies-for-applications">NetworkPolicies for applications</a>.
 // </div>
 type NetworkPolicyTemplate struct {
 	// +kubebuilder:pruning:PreserveUnknownFields
