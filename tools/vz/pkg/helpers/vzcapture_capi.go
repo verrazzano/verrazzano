f74c5e345b3aa2e7f8bd264a5f3b82ed0886d4cc
Michael Gianatassio <michael.gianatassio@oracle.com>
2023-07-04 06:10:32 -0400

    add more capi resources, change list to by namespace

--- tools/vz/pkg/helpers/vzcapture_capi.go
+++ tools/vz/pkg/helpers/vzcapture_capi.go
@@ -14,11 +14,18 @@ import (
 )
 
 const (
-	v1Alpha1       = "v1alpha1"
-	v1Beta1        = "v1beta1"
-	addonsGroup    = "addons.cluster.x-k8s.io"
-	bootstrapGroup = "bootstrap.cluster.x-k8s.io"
-	clusterGroup   = "cluster.x-k8s.io"
+	v1Alpha1            = "v1alpha1"
+	v1Alpha3            = "v1alpha3"
+	v1Beta1             = "v1beta1"
+	v1Beta2             = "v1beta2"
+	addonsGroup         = "addons.cluster.x-k8s.io"
+	bootstrapGroup      = "bootstrap.cluster.x-k8s.io"
+	clusterGroup        = "cluster.x-k8s.io"
+	clusterctlGroup     = "clusterctl.cluster.x-k8s.io"
+	controlPlaneGroup   = "controlplane.cluster.x-k8s.io"
+	infrastructureGroup = "infrastructure.cluster.x-k8s.io"
+	ipamGroup           = "ipam.cluster.x-k8s.io"
+	runtimeGroup        = "runtime.cluster.x-k8s.io"
 )
 
 type capiResource struct {
@@ -38,6 +45,24 @@ var capiResources = []capiResource{
 	{GVR: schema.GroupVersionResource{Group: clusterGroup, Version: v1Beta1, Resource: "machinepools"}, Kind: "MachinePools"},
 	{GVR: schema.GroupVersionResource{Group: clusterGroup, Version: v1Beta1, Resource: "machines"}, Kind: "Machines"},
 	{GVR: schema.GroupVersionResource{Group: clusterGroup, Version: v1Beta1, Resource: "machinesets"}, Kind: "MachineSets"},
+	{GVR: schema.GroupVersionResource{Group: clusterctlGroup, Version: v1Alpha3, Resource: "providers"}, Kind: "Providers"},
+	{GVR: schema.GroupVersionResource{Group: controlPlaneGroup, Version: v1Alpha1, Resource: "ocnecontrolplanes"}, Kind: "OCNEControlPlanes"},
+	{GVR: schema.GroupVersionResource{Group: controlPlaneGroup, Version: v1Alpha1, Resource: "ocnecontrolplanetemplates"}, Kind: "OCNEControlPlaneTemplates"},
+	{GVR: schema.GroupVersionResource{Group: infrastructureGroup, Version: v1Beta2, Resource: "ociclusteridentities"}, Kind: "OCIClusterIdentities"},
+	{GVR: schema.GroupVersionResource{Group: infrastructureGroup, Version: v1Beta2, Resource: "ociclusters"}, Kind: "OCIClusters"},
+	{GVR: schema.GroupVersionResource{Group: infrastructureGroup, Version: v1Beta2, Resource: "ociclustertemplates"}, Kind: "OCIClusterTemplates"},
+	{GVR: schema.GroupVersionResource{Group: infrastructureGroup, Version: v1Beta2, Resource: "ocimachinepools"}, Kind: "OCIMachinePools"},
+	{GVR: schema.GroupVersionResource{Group: infrastructureGroup, Version: v1Beta2, Resource: "ocimachines"}, Kind: "OCIMachines"},
+	{GVR: schema.GroupVersionResource{Group: infrastructureGroup, Version: v1Beta2, Resource: "ocimachinetemplates"}, Kind: "OCIMachineTemplates"},
+	{GVR: schema.GroupVersionResource{Group: infrastructureGroup, Version: v1Beta2, Resource: "ocimanagedclusters"}, Kind: "OCIManagedClusters"},
+	{GVR: schema.GroupVersionResource{Group: infrastructureGroup, Version: v1Beta2, Resource: "ocimanagedclustertemplates"}, Kind: "OCIManagedClusterTemplates"},
+	{GVR: schema.GroupVersionResource{Group: infrastructureGroup, Version: v1Beta2, Resource: "ocimanagedcontrolplanes"}, Kind: "OCIManagedControlPlanes"},
+	{GVR: schema.GroupVersionResource{Group: infrastructureGroup, Version: v1Beta2, Resource: "ocimanagedcontrolplanetemplates"}, Kind: "OCIManagedControlPlaneTemplates"},
+	{GVR: schema.GroupVersionResource{Group: infrastructureGroup, Version: v1Beta2, Resource: "ocimanagedmachinepools"}, Kind: "OCIManagedMachinePools"},
+	{GVR: schema.GroupVersionResource{Group: infrastructureGroup, Version: v1Beta2, Resource: "ocimanagedmachinepooltemplates"}, Kind: "OCIManagedMachinePoolTemplates"},
+	{GVR: schema.GroupVersionResource{Group: ipamGroup, Version: v1Alpha1, Resource: "ipaddressclaims"}, Kind: "IPAddressClaims"},
+	{GVR: schema.GroupVersionResource{Group: ipamGroup, Version: v1Alpha1, Resource: "ipaddresses"}, Kind: "IPAddresses"},
+	{GVR: schema.GroupVersionResource{Group: runtimeGroup, Version: v1Alpha1, Resource: "extensionconfigs"}, Kind: "ExtensionConfigs"},
 }
 
 // captureCapiResources captures resources related to ClusterAPI
@@ -52,7 +77,7 @@ func captureCapiResources(dynamicClient dynamic.Interface, namespace, captureDir
 }
 
 func captureResource(dynamicClient dynamic.Interface, gvr schema.GroupVersionResource, kind string, namespace, captureDir string, vzHelper VZHelper) error {
-	list, err := dynamicClient.Resource(gvr).List(context.TODO(), metav1.ListOptions{})
+	list, err := dynamicClient.Resource(gvr).Namespace(namespace).List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
 		LogError(fmt.Sprintf("An error occurred while getting the %s in namespace %s: %s\n", kind, namespace, err.Error()))
 	}
