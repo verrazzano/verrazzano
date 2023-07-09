b674cbc0db939c605f460bd2c30663180323b783
Michael Gianatassio <michael.gianatassio@oracle.com>
2023-07-09 05:54:39 -0400

    save

--- tests/e2e/clusterapi/capi-overrides/capi_overrides_test.go
+++ tests/e2e/clusterapi/capi-overrides/capi_overrides_test.go
@@ -6,6 +6,7 @@ package capi_overrides
 import (
 	"context"
 	"fmt"
+	"strings"
 	"time"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -16,6 +17,7 @@ import (
 	"github.com/verrazzano/verrazzano/tests/e2e/pkg"
 	capipkg "github.com/verrazzano/verrazzano/tests/e2e/pkg/clusterapi"
 	"github.com/verrazzano/verrazzano/tests/e2e/pkg/test/framework"
+	appsv1 "k8s.io/api/apps/v1"
 	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/dynamic"
@@ -26,7 +28,16 @@ const (
 	pollingInterval = 10 * time.Second
 )
 
-const versionOverrides = `
+const (
+	globalRegistryName     = "testreg.io"
+	globalRegistryOverride = `
+{
+  "global": {
+    "registry": "%s"
+  }
+}
+`
+	versionOverrides = `
 {
   "defaultProviders": {
     "oci": {
@@ -43,6 +54,7 @@ const versionOverrides = `
     }
   }
 }`
+)
 
 var t = framework.NewTestFramework("capi_overrides")
 
@@ -64,24 +76,37 @@ var _ = t.Describe("Cluster API", Label("f:platform-lcm.install"), func() {
 
 	t.Context("initial state", func() {
 		// GIVEN the Cluster API is installed
-		// WHEN we check the kontainerdrivers
-		// THEN we successfully find them all active
+		// WHEN we check the initial state of the Verrazzano install
+		// THEN we successfully find it ready
 		capipkg.WhenClusterAPIInstalledIt(t, "kontainerdrivers are active", func() {
-			Eventually(capipkg.IsAllDriversActive(t, dynClient), waitTimeout, pollingInterval).Should(BeTrue())
-		})
-	})
-
-	t.Context("override oci, core, ocneBootstrap and ocneControlPlane versions", func() {
-		// GIVEN the CAPI environment is ready
-		// WHEN we override versions
-		// THEN the overrides get successfully applied
-		capipkg.WhenClusterAPIInstalledIt(t, "and wait for reconcile to complete", func() {
-			applyOverrides(fmt.Sprintf(versionOverrides, ociComp.Version, ocneComp.Version, ocneComp.Version, coreComp.Version))
-			Eventually(isStatusReconciling, waitTimeout, pollingInterval).Should(BeTrue())
 			Eventually(isStatusReady, waitTimeout, pollingInterval).Should(BeTrue())
 		})
 	})
 
+	t.Context("override global registry", func() {
+		// GIVEN a CAPI environment
+		// WHEN we override the global registry
+		// THEN the overrides get successfully applied
+		capipkg.WhenClusterAPIInstalledIt(t, "and wait for deployments to use it", func() {
+			applyOverrides(fmt.Sprintf(globalRegistryOverride, globalRegistryName))
+			Eventually(isStatusReconciling, waitTimeout, pollingInterval).Should(BeTrue())
+			// The CAPI pods are now in a broken state because the registry name does not exist.
+			// Verify the deployments get updated to use the new value.
+			Eventually(isGlobalRegUsed, waitTimeout, pollingInterval).Should(BeTrue())
+		})
+	})
+	/*
+		t.Context("override oci, core, ocneBootstrap and ocneControlPlane versions", func() {
+			// GIVEN the CAPI environment is ready
+			// WHEN we override versions
+			// THEN the overrides get successfully applied
+			capipkg.WhenClusterAPIInstalledIt(t, "and wait for reconcile to complete", func() {
+				applyOverrides(fmt.Sprintf(versionOverrides, ociComp.Version, ocneComp.Version, ocneComp.Version, coreComp.Version))
+				Eventually(isStatusReconciling, waitTimeout, pollingInterval).Should(BeTrue())
+				Eventually(isStatusReady, waitTimeout, pollingInterval).Should(BeTrue())
+			})
+		})
+	*/
 	t.Context("restore VZ to default values for clusterAPI", func() {
 		// GIVEN the CAPI environment is ready
 		// WHEN we remove the overrides
@@ -169,4 +194,41 @@ func getComponents() (*bom.BomComponent, *bom.BomComponent, *bom.BomComponent) {
 	Expect(capiComp).ToNot(BeNil())
 	Expect(coreComp).ToNot(BeNil())
 	return ociComp, capiComp, coreComp
+}
+
+// isGlobalRegUsed - determine if the global registry override is being used in the CAPI deployments
+func isGlobalRegUsed() bool {
+	return isSubstringInDeploymentImages(globalRegistryName)
+}
+
+func isSubstringInDeploymentImages(substring string) bool {
+	matchCount := 0
+	deployments := getCAPIDeployments()
+	for _, deployment := range deployments {
+		switch deployment.Name {
+		case "capi-controller-manager":
+		case "capi-ocne-bootstrap-controller-manager":
+		case "capi-ocne-control-plane-controller-manager":
+		case "capoci-controller-manager":
+			for _, container := range deployment.Spec.Template.Spec.Containers {
+				if strings.Contains(container.Image, substring) {
+					matchCount++
+				}
+			}
+		}
+	}
+	// Expect four deployments to match
+	return matchCount == 4
+}
+
+func getCAPIDeployments() []appsv1.Deployment {
+	v1client, err := k8sutil.GetKubernetesClientset()
+	if err != nil {
+		return []appsv1.Deployment{}
+	}
+	deployments, err := v1client.AppsV1().Deployments("verrazzano-capi").List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		return []appsv1.Deployment{}
+	}
+	return deployments.Items
 }
