760ab72fcfc678701ff1d20bc61ac334038a7795
Michael Gianatassio <michael.gianatassio@oracle.com>
2023-07-12 10:21:50 -0400

    save

--- tools/vz/pkg/helpers/vzsanitize_test.go
+++ tools/vz/pkg/helpers/vzsanitize_test.go
@@ -10,14 +10,16 @@ import (
 )
 
 var (
-	testIP   = "127.255.255.255"
-	testOCID = "ocid1.tenancy.oc1..a763cu5f3m7qpzwnvr2so2655cpzgxmglgtui3v7q"
-	testSSH  = "ssh-rsa AAAAB3NzaCDo798PWwYniRpZ/DEKAapLQDfrHeR/OO59T4ZUr4ln/5EoUGYu1HRVWmvQx4wsKZRwl4u8pi9gYOW1pL/IYp3cumJef9Y99+/ foo@foo-mac"
+	testIP       = "127.255.255.255"
+	testOCID     = "ocid1.tenancy.oc1..a763cu5f3m7qpzwnvr2so2655cpzgxmglgtui3v7q"
+	testSSH      = "ssh-rsa AAAAB3NzaCDo798PWwYniRpZ/DEKAapLQDfrHeR/OO59T4ZUr4ln/5EoUGYu1HRVWmvQx4wsKZRwl4u8pi9gYOW1pL/IYp3cumJef9Y99+/ foo@foo-mac"
+	testUserData = "\"user_data\": \"abcABC012=+\""
 )
 
 func TestSanitizeALine(t *testing.T) {
 	assert.NotContains(t, SanitizeString(testIP), testIP)
 	assert.NotContains(t, SanitizeString(testOCID), testOCID)
 	assert.NotContains(t, SanitizeString(testSSH), testSSH)
+	assert.NotContains(t, SanitizeString(testUserData), testUserData)
 	assert.Contains(t, SanitizeString("test.me.test.me"), "test")
 }
