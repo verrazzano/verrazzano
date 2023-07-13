ed8e41a43c689b63984eb773c65d1f20da624a4c
Michael Gianatassio <michael.gianatassio@oracle.com>
2023-07-13 07:17:45 -0400

    save

--- tools/vz/pkg/helpers/vzcapture_capi.go
+++ tools/vz/pkg/helpers/vzcapture_capi.go
@@ -8,11 +8,10 @@ import (
 	"fmt"
 	"strings"
 
-	"k8s.io/client-go/dynamic"
-
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
 )
 
 const (
