05a0a94bc860095e3169e9588ca6f97331b38fb3
Abijitha Nadagouda <29689969+Abhijithang@users.noreply.github.com>
2023-07-14 10:46:16 -0700

    VZ-9955: Refactor upgrade resiliency pipeline scripts (#6484)

--- ci/tools/derive_upgrade_version_test.go
+++ ci/tools/derive_upgrade_version_test.go
@@ -15,7 +15,7 @@ func TestGetInstallReleaseWithoutMajorVersion(t *testing.T) {
 	pwd, _ := os.Getwd()
 	parseCliArgs([]string{pwd, "install-version"})
 	releaseTags := []string{"v1.0.0", "v1.0.1", "v1.0.2", "v1.0.3", "v1.0.4", "v1.1.0", "v1.1.1", "v1.1.2", "v1.2.0", "v1.2.1", "v1.2.2", "v1.3.0", "v1.3.1", "v1.3.2", "v1.3.3", "v1.3.4", "v1.3.5", "v1.3.6", "v1.3.7", "v1.3.8", "v1.4.0", "v1.4.1", "v1.4.2"}
-	assert.Equal(t, "v1.2.2\n", getInstallRelease(releaseTags))
+	assert.Equal(t, "v1.2.2", getInstallRelease(releaseTags))
 }
 
 // TestGetInstallReleaseWithMajorRelease Tests the getInstallRelease function
@@ -25,7 +25,7 @@ func TestGetInstallReleaseWithMajorRelease(t *testing.T) {
 	pwd, _ := os.Getwd()
 	parseCliArgs([]string{pwd, "install-version"})
 	releaseTags := []string{"v1.0.0", "v1.0.1", "v1.0.2", "v1.0.3", "v1.0.4", "v1.1.0", "v1.1.1", "v1.1.2", "v1.2.0", "v1.2.1", "v1.2.2", "v1.3.0", "v1.3.1", "v1.3.2", "v1.3.3", "v1.3.4", "v1.3.5", "v1.3.6", "v1.3.7", "v1.3.8", "v1.4.0", "v1.4.1", "v1.4.2", "v1.5.0", "v2.0.0", "v2.1.0"}
-	assert.Equal(t, "v1.5.0\n", getInstallRelease(releaseTags))
+	assert.Equal(t, "v1.5.0", getInstallRelease(releaseTags))
 }
 
 // TestGetInstallReleaseNotMoreThanTwoMinorVersions Tests the getInstallRelease function
@@ -35,7 +35,7 @@ func TestGetInstallReleaseNotMoreThanTwoMinorVersions(t *testing.T) {
 	pwd, _ := os.Getwd()
 	parseCliArgs([]string{pwd, "install-version"})
 	releaseTags := []string{"v1.4.0", "v1.4.1", "v1.4.2", "v1.5.0", "v1.5.1"}
-	assert.Equal(t, "v1.4.2\n", getInstallRelease(releaseTags))
+	assert.Equal(t, "v1.4.0", getInstallRelease(releaseTags))
 }
 
 // TestGetInterimReleaseWithoutMajorVersion Tests the getInterimRelease function
@@ -45,7 +45,7 @@ func TestGetInterimReleaseWithoutMajorVersion(t *testing.T) {
 	pwd, _ := os.Getwd()
 	parseCliArgs([]string{pwd, "interim-version"})
 	releaseTags := []string{"v1.0.0", "v1.0.1", "v1.0.2", "v1.0.3", "v1.0.4", "v1.1.0", "v1.1.1", "v1.1.2", "v1.2.0", "v1.2.1", "v1.2.2", "v1.3.0", "v1.3.1", "v1.3.2", "v1.3.3", "v1.3.4", "v1.3.5", "v1.3.6", "v1.3.7", "v1.3.8", "v1.4.0", "v1.4.1", "v1.4.2"}
-	assert.Equal(t, "v1.3.8\n", getInterimRelease(releaseTags))
+	assert.Equal(t, "v1.3.8", getInterimRelease(releaseTags))
 }
 
 // TestGetInterimReleaseWithMajorVersion Tests the getInterimRelease function
@@ -55,7 +55,7 @@ func TestGetInterimReleaseWithMajorVersion(t *testing.T) {
 	pwd, _ := os.Getwd()
 	parseCliArgs([]string{pwd, "interim-version"})
 	releaseTags := []string{"v1.0.0", "v1.0.1", "v1.0.2", "v1.0.3", "v1.0.4", "v1.1.0", "v1.1.1", "v1.1.2", "v1.2.0", "v1.2.1", "v1.2.2", "v1.3.0", "v1.3.1", "v1.3.2", "v1.3.3", "v1.3.4", "v1.3.5", "v1.3.6", "v1.3.7", "v1.3.8", "v1.4.0", "v1.4.1", "v1.4.2", "v1.5.0", "2.0.0"}
-	assert.Equal(t, "v1.5.0\n", getInterimRelease(releaseTags))
+	assert.Equal(t, "v1.5.0", getInterimRelease(releaseTags))
 }
 
 // TestGetInterimReleaseNotMoreThanTwoMinorVersions Tests the getInterimRelease function
@@ -63,9 +63,9 @@ func TestGetInterimReleaseWithMajorVersion(t *testing.T) {
 // THEN interim release version with the first patch release of the last minor release is expected
 func TestGetInterimReleaseNotMoreThanTwoMinorVersions(t *testing.T) {
 	pwd, _ := os.Getwd()
-	parseCliArgs([]string{pwd, "install-version"})
+	parseCliArgs([]string{pwd, "interim-version"})
 	releaseTags := []string{"v1.4.0", "v1.4.1", "v1.4.2", "v1.5.0", "v1.5.1"}
-	assert.Equal(t, "v1.5.0\n", getInterimRelease(releaseTags))
+	assert.Equal(t, "v1.4.2", getInterimRelease(releaseTags))
 }
 
 // TestGetLatestReleaseForBranch tests the getLatestReleaseForCurrentBranch function
