6bfc0ffc3c6785580e6599bdc7c406c6e1985c86
Michael Gianatassio <31552226+mgianatagh@users.noreply.github.com>
2023-07-18 15:44:21 -0400

    VZ-10281: Add analyze of clustergroups, clusterregistration and fleet clusters (#6545)

---
+++ tools/vz/pkg/analysis/internal/util/cluster/rancher/clustergroups.go
@@ -0,0 +1,88 @@
+// Copyright (c) 2023, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package rancher
+
+import (
+	"fmt"
+
+	"github.com/verrazzano/verrazzano/tools/vz/pkg/analysis/internal/util/files"
+	"github.com/verrazzano/verrazzano/tools/vz/pkg/analysis/internal/util/report"
+	"go.uber.org/zap"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// Minimal definition of object that only contains the fields that will be analyzed
+type clusterGroupList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []clusterGroup `json:"items"`
+}
+type clusterGroup struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+	Status            clusterGroupStatus `json:"status,omitempty"`
+}
+type clusterGroupStatus struct {
+	NonReadyClusterCount int               `json:"nonReadyClusterCount,omitempty"`
+	Conditions           []cattleCondition `json:"conditions,omitempty"`
+}
+
+// AnalyzeClusterGroups - analyze the status of ClusterGruop objects
+func AnalyzeClusterGroups(log *zap.SugaredLogger, clusterRoot string, issueReporter *report.IssueReporter) error {
+	list := &clusterGroupList{}
+	err := files.UnmarshallFileInClusterRoot(clusterRoot, "clustergroup.fleet.cattle.io.json", list)
+	if err != nil {
+		return err
+	}
+
+	for _, clusterGroup := range list.Items {
+		err = analyzeClusterGroup(clusterRoot, clusterGroup, issueReporter)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// analyzeClusterGroup - analyze a single ClusterGroup and report any issues
+func analyzeClusterGroup(clusterRoot string, clusterGroup clusterGroup, issueReporter *report.IssueReporter) error {
+
+	var messages []string
+	var subMessage string
+	for _, condition := range clusterGroup.Status.Conditions {
+		if condition.Status != corev1.ConditionTrue {
+			switch condition.Type {
+			case "Processed":
+				subMessage = "is not processed"
+			case "Ready":
+				subMessage = "is not ready"
+			default:
+				continue
+			}
+			// Add a message for the issue
+			reason := ""
+			msg := ""
+			if len(condition.Reason) > 0 {
+				reason = fmt.Sprintf(", reason is %q", condition.Reason)
+			}
+			if len(condition.Message) > 0 {
+				msg = fmt.Sprintf(", message is %q", condition.Message)
+			}
+			message := fmt.Sprintf("Rancher ClusterGroup resource %q %s %s%s", clusterGroup.Name, subMessage, reason, msg)
+			messages = append([]string{message}, messages...)
+		}
+	}
+
+	if clusterGroup.Status.NonReadyClusterCount > 0 {
+		message := fmt.Sprintf("Rancher Bundle resource %q in namespace %s has %d clusters not ready", clusterGroup.Name, clusterGroup.Namespace, clusterGroup.Status.NonReadyClusterCount)
+		messages = append([]string{message}, messages...)
+	}
+	if len(messages) > 0 {
+		issueReporter.AddKnownIssueMessagesFiles(report.RancherIssues, clusterRoot, messages, []string{})
+	}
+
+	return nil
+}
