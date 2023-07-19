e0d6248211f34f02d43b70b234b2cc41ab1947bf
Paul Mackin <paul.mackin@oracle.com>
2023-07-19 09:46:54 -0400

    VZ-10309 Add VPO module integration first-pass, controlled by feature gate. (#6463)

---
+++ platform-operator/experimental/module-integration/component-handler/delete/delete_handler.go
@@ -0,0 +1,98 @@
+// Copyright (c) 2023, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package delete
+
+import (
+	moduleapi "github.com/verrazzano/verrazzano-modules/module-operator/apis/platform/v1alpha1"
+	"github.com/verrazzano/verrazzano-modules/module-operator/controllers/module/handlers/common"
+	"github.com/verrazzano/verrazzano-modules/module-operator/controllers/module/status"
+	"github.com/verrazzano/verrazzano-modules/pkg/controller/handlerspi"
+	"github.com/verrazzano/verrazzano-modules/pkg/controller/result"
+	"github.com/verrazzano/verrazzano-modules/pkg/helm"
+)
+
+type ComponentHandler struct {
+	common.BaseHandler
+}
+
+var (
+	_ handlerspi.StateMachineHandler = &ComponentHandler{}
+)
+
+func NewHandler() handlerspi.StateMachineHandler {
+	return &ComponentHandler{}
+}
+
+// GetWorkName returns the work name
+func (h ComponentHandler) GetWorkName() string {
+	return "uninstall"
+}
+
+// IsWorkNeeded returns true if install is needed
+func (h ComponentHandler) IsWorkNeeded(ctx handlerspi.HandlerContext) (bool, result.Result) {
+	return true, result.NewResult()
+}
+
+// PreWorkUpdateStatus does the lifecycle pre-Work status update
+func (h ComponentHandler) PreWorkUpdateStatus(ctx handlerspi.HandlerContext) result.Result {
+	return result.NewResult()
+}
+
+// PreWork does the pre-work
+func (h ComponentHandler) PreWork(ctx handlerspi.HandlerContext) result.Result {
+	return result.NewResult()
+}
+
+// DoWorkUpdateStatus does the work status update
+func (h ComponentHandler) DoWorkUpdateStatus(ctx handlerspi.HandlerContext) result.Result {
+	module := ctx.CR.(*moduleapi.Module)
+	return status.UpdateReadyConditionReconciling(ctx, module, moduleapi.ReadyReasonUninstallStarted)
+}
+
+// DoWork uninstalls the module using Helm
+func (h ComponentHandler) DoWork(ctx handlerspi.HandlerContext) result.Result {
+	installed, err := helm.IsReleaseInstalled(ctx.HelmRelease.Name, ctx.HelmRelease.Namespace)
+	if err != nil {
+		ctx.Log.ErrorfThrottled("Error checking if Helm release installed for %s/%s", ctx.HelmRelease.Namespace, ctx.HelmRelease.Name)
+		return result.NewResult()
+	}
+	if !installed {
+		return result.NewResult()
+	}
+
+	err = helm.Uninstall(ctx.Log, ctx.HelmRelease.Name, ctx.HelmRelease.Namespace, ctx.DryRun)
+	return result.NewResultShortRequeueDelayIfError(err)
+}
+
+// IsWorkDone Indicates whether a module is uninstalled
+func (h ComponentHandler) IsWorkDone(ctx handlerspi.HandlerContext) (bool, result.Result) {
+	if ctx.DryRun {
+		ctx.Log.Debugf("IsReady() dry run for %s", ctx.HelmRelease.Name)
+		return true, result.NewResult()
+	}
+
+	deployed, err := helm.IsReleaseDeployed(ctx.HelmRelease.Name, ctx.HelmRelease.Namespace)
+	if err != nil {
+		ctx.Log.ErrorfThrottled("Error occurred checking release deployment: %v", err.Error())
+		return false, result.NewResultShortRequeueDelayIfError(err)
+	}
+
+	return !deployed, result.NewResult()
+}
+
+// PostWorkUpdateStatus does the post-work status update
+func (h ComponentHandler) PostWorkUpdateStatus(ctx handlerspi.HandlerContext) result.Result {
+	return result.NewResult()
+}
+
+// PostWork does installation pre-work
+func (h ComponentHandler) PostWork(ctx handlerspi.HandlerContext) result.Result {
+	return result.NewResult()
+}
+
+// WorkCompletedUpdateStatus does the lifecycle completed Work status update
+func (h ComponentHandler) WorkCompletedUpdateStatus(ctx handlerspi.HandlerContext) result.Result {
+	module := ctx.CR.(*moduleapi.Module)
+	return status.UpdateReadyConditionSucceeded(ctx, module, moduleapi.ReadyReasonUninstallSucceeded)
+}
