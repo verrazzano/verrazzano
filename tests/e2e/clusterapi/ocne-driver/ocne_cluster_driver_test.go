6041a2751fc058e42737060ed8f847e2da3f7e7b
Prasad Shirodkar <prasad.shirodkar@oracle.com>
2023-07-20 09:50:41 -0700

    renamed methods for clarity

--- tests/e2e/clusterapi/ocne-driver/ocne_cluster_driver_test.go
+++ tests/e2e/clusterapi/ocne-driver/ocne_cluster_driver_test.go
@@ -31,7 +31,7 @@ var (
 )
 
 // Part of SynchronizedBeforeSuite, run by only one process
-func sbsProcess1Func() []byte {
+func synchronizedBeforeSuiteProcess1Func() []byte {
 	kubeconfigPath, err := k8sutil.GetKubeConfigLocation()
 	Expect(err).ShouldNot(HaveOccurred())
 	if !pkg.IsRancherEnabled(kubeconfigPath) || !pkg.IsClusterAPIEnabled(kubeconfigPath) {
@@ -68,7 +68,7 @@ func sbsProcess1Func() []byte {
 }
 
 // Part of SynchronizedBeforeSuite, run by all processes
-func sbsAllProcessesFunc(credentialIDBytes []byte) {
+func synchronizedBeforeSuiteAllProcessesFunc(credentialIDBytes []byte) {
 	// Define global variables for all processes
 	cloudCredentialID = string(credentialIDBytes)
 
@@ -95,10 +95,10 @@ func sbsAllProcessesFunc(credentialIDBytes []byte) {
 	clusterNameNodePool = fmt.Sprintf("strudel-pool-%s", ocneClusterNameSuffix)
 }
 
-var _ = t.SynchronizedBeforeSuite(sbsProcess1Func, sbsAllProcessesFunc)
+var _ = t.SynchronizedBeforeSuite(synchronizedBeforeSuiteProcess1Func, synchronizedBeforeSuiteAllProcessesFunc)
 
 // Part of SynchronizedAfterSuite, run by only one process
-func sasProcess1Func() {
+func synchronizedAfterSuiteProcess1Func() {
 	// Delete the clusters concurrently
 	clusterNames := [...]string{clusterNameSingleNode, clusterNameNodePool}
 	var wg sync.WaitGroup
@@ -128,7 +128,7 @@ func sasProcess1Func() {
 		BeTrue(), fmt.Sprintf("cloud credential %s is not deleted", cloudCredentialID))
 }
 
-var _ = t.SynchronizedAfterSuite(func() {}, sasProcess1Func)
+var _ = t.SynchronizedAfterSuite(func() {}, synchronizedAfterSuiteProcess1Func)
 
 var _ = t.Describe("OCNE Cluster Driver", Label("f:rancher-capi:ocne-cluster-driver"), func() {
 	t.Context("OCNE cluster creation with single node", Ordered, func() {
