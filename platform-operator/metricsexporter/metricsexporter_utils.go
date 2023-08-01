116adb2f4211ae1c429aa79df1c5962b22485cc8
Raaghav Wadhawan <raaghav.w.wadhawan@oracle.com>
2023-08-01 11:47:06 -0700

    VZ-10390: metricsexporter spamming errors in the dev lre (#6677)

--- platform-operator/metricsexporter/metricsexporter_utils.go
+++ platform-operator/metricsexporter/metricsexporter_utils.go
@@ -286,8 +286,7 @@ func AnalyzeVerrazzanoResourceMetrics(log vzlog.VerrazzanoLogger, cr vzapi.Verra
 		}
 		found, component := registry.FindComponent(componentName)
 		if !found {
-			log.Errorf("No component %s found", componentName)
-			return
+			continue
 		}
 		componentJSONName := component.GetJSONName()
 		if installStartTime != "" && installCompletionTime != "" {
