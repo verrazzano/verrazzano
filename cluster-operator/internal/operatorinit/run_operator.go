822a51b1300c2681b599d6d29c971b3510e83ab5
Jonathan Maron <jonathan.maron@oracle.com>
2023-08-11 07:34:16 -0400

    fix managed cluster crash

--- cluster-operator/internal/operatorinit/run_operator.go
+++ cluster-operator/internal/operatorinit/run_operator.go
@@ -85,7 +85,7 @@ func StartClusterOperator(metricsAddr string, enableLeaderElection bool, probeAd
 		}
 	}
 
-	crdInstalled, err = isCRDInstalled(apiextv1Client, capiClustersCRDName)
+	capiCrdInstalled, err := isCRDInstalled(apiextv1Client, capiClustersCRDName)
 	if err != nil {
 		log.Error(err, "unable to determine if CAPI CRD is installed")
 		os.Exit(1)
@@ -96,7 +96,7 @@ func StartClusterOperator(metricsAddr string, enableLeaderElection bool, probeAd
 	}
 
 	// only start the Rancher cluster sync controller if the cattle clusters CRD is installed
-	if crdInstalled && rancherRegistrationEnabled {
+	if capiCrdInstalled && rancherRegistrationEnabled {
 		log.Infof("Starting CAPI Cluster controller")
 		if err = (&capi.CAPIClusterReconciler{
 			Client:             mgr.GetClient(),
