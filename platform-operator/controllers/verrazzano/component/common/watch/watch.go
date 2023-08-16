3bfb9a475fe5cf04ff219c2de26379f3f576cfd2
Paul Mackin <paul.mackin@oracle.com>
2023-08-16 18:27:43 -0400

    remove unused code

--- platform-operator/controllers/verrazzano/component/common/watch/watch.go
+++ platform-operator/controllers/verrazzano/component/common/watch/watch.go
@@ -9,7 +9,6 @@ import (
 	"github.com/verrazzano/verrazzano-modules/pkg/controller/base/controllerspi"
 	vzstring "github.com/verrazzano/verrazzano/pkg/string"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/source"
 )
@@ -58,28 +57,4 @@ func GetModuleReadyWatches(moduleNames []string) []controllerspi.WatchDescriptor
 	})
 
 	return watches
-}
-
-// ShouldReconcile returns true if reconcile should be done in response to the Module status changing to ready
-func ShouldReconcile(_ types.NamespacedName, newWatchedObject client.Object, oldWatchedObject client.Object, event controllerspi.WatchEventType) bool {
-	// Get new module Ready condition and return false if not ready
-	var newModule = newWatchedObject.(*moduleapi.Module)
-	newCond := status.GetReadyCondition(newModule)
-	if newCond == nil {
-		return false
-	}
-	if newCond.Status != corev1.ConditionTrue {
-		return false
-	}
-
-	// The new module is ready. get old module Ready condition
-	var oldModule = oldWatchedObject.(*moduleapi.Module)
-	oldCond := status.GetReadyCondition(oldModule)
-	if oldCond == nil {
-		return false
-	}
-
-	// Return false if the old module condition reason matches the new module AND the old condition was ready.
-	// In that case we don't need to reconcile
-	return !(oldCond.Reason == newCond.Reason && oldCond.Status == corev1.ConditionTrue)
 }
