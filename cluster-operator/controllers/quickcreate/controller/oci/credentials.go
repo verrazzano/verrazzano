f2fc80ee22e9a2d9c9b6ac8522f15cc525233f1c
Anders Swanson <anders.swanson@oracle.com>
2023-08-24 10:22:50 -0700

    Cluster Identity Support

--- cluster-operator/controllers/quickcreate/controller/oci/credentials.go
+++ cluster-operator/controllers/quickcreate/controller/oci/credentials.go
@@ -5,13 +5,54 @@ package oci
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
-	capociv1beta2 "github.com/oracle/cluster-api-provider-oci/api/v1beta2"
-	"github.com/oracle/cluster-api-provider-oci/cloud/util"
 	vmcv1alpha1 "github.com/verrazzano/verrazzano/cluster-operator/apis/clusters/v1alpha1"
 	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
+	"reflect"
 	clipkg "sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+type (
+	CredentialsLoader interface {
+		LoadCredentialsIfAllowed(ctx context.Context, cli clipkg.Client, identityRef vmcv1alpha1.NamespacedRef, namespace string) (*Credentials, error)
+	}
+	CredentialsLoaderImpl struct{}
+	Credentials           struct {
+		Region               string
+		Tenancy              string
+		User                 string
+		PrivateKey           string
+		Fingerprint          string
+		Passphrase           string
+		UseInstancePrincipal string
+	}
+	CAPIIdentity struct {
+		Spec struct {
+			Namespaces      *AllowedNamespaces `json:"allowedNamespaces"`
+			PrincipalSecret struct {
+				Name      string `json:"name"`
+				Namespace string `json:"namespace"`
+			} `json:"principalSecret"`
+		} `json:"spec"`
+	}
+	AllowedNamespaces struct {
+		List     []string              `json:"list"`
+		Selector *metav1.LabelSelector `json:"selector"`
+	}
+)
+
+var (
+	_                     CredentialsLoader = CredentialsLoaderImpl{}
+	gvkOCIClusterIdentity                   = schema.GroupVersionKind{
+		Group:   "infrastructure.cluster.x-k8s.io",
+		Version: "v1beta2",
+		Kind:    "ociclusteridentity",
+	}
 )
 
 const (
@@ -24,39 +65,76 @@ const (
 	ociUseInstancePrincipalField = "useInstancePrincipal"
 )
 
-type (
-	Credentials struct {
-		Region               string
-		Tenancy              string
-		User                 string
-		PrivateKey           string
-		Fingerprint          string
-		Passphrase           string
-		UseInstancePrincipal string
-	}
-)
-
-func LoadCredentials(ctx context.Context, cli clipkg.Client, identityRef vmcv1alpha1.NamespacedRef, clusterNamespace string) (*Credentials, error) {
+// LoadCredentialsIfAllowed fetches the OCI Credentials for an OCIClusterIdentity, if that OCIClusterIdentity exists, has a principal secret,
+// and allows access from a given namespace.
+func (c CredentialsLoaderImpl) LoadCredentialsIfAllowed(ctx context.Context, cli clipkg.Client, identityRef vmcv1alpha1.NamespacedRef, namespace string) (*Credentials, error) {
 	nsn := types.NamespacedName{
 		Name:      identityRef.Name,
 		Namespace: identityRef.Namespace,
 	}
-	identity, ok := IsAllowedNamespace(ctx, cli, nsn, clusterNamespace)
-	if !ok {
+	identity, err := getIdentity(ctx, cli, nsn)
+	if err != nil {
+		return nil, err
+	}
+	if !IsAllowedNamespace(ctx, cli, identity, namespace) {
 		return nil, fmt.Errorf("cannot access OCI identity %s/%s", identityRef.Namespace, identityRef.Name)
 	}
 	return getCredentialsFromIdentity(ctx, cli, identity)
 }
 
-func IsAllowedNamespace(ctx context.Context, cli clipkg.Client, nsn types.NamespacedName, namespace string) (*capociv1beta2.OCIClusterIdentity, bool) {
-	identity := &capociv1beta2.OCIClusterIdentity{}
-	if err := cli.Get(ctx, nsn, identity); err != nil {
-		return nil, false
+// IsAllowedNamespace checks if a given identity allows access from a given namespace.
+func IsAllowedNamespace(ctx context.Context, cli clipkg.Client, identity *CAPIIdentity, namespace string) bool {
+	// No allowed namespaces means nothing allowed
+	if identity.Spec.Namespaces == nil {
+		return false
 	}
-	return identity, util.IsClusterNamespaceAllowed(ctx, cli, identity.Spec.AllowedNamespaces, namespace)
+	// Empty allowed namespaces means all namespaces allowed
+	if reflect.DeepEqual(*identity.Spec.Namespaces, AllowedNamespaces{}) {
+		return true
+	}
+	// If namespace is in the allowed namespaces list, access is permitted
+	for _, ns := range identity.Spec.Namespaces.List {
+		if ns == namespace {
+			return true
+		}
+	}
+	// Deny access for invalid or empty selectors
+	selector, err := metav1.LabelSelectorAsSelector(identity.Spec.Namespaces.Selector)
+	if err != nil {
+		return false
+	}
+	if selector.Empty() {
+		return false
+	}
+	// Allow access if the namespace matches any namespace from the selectors
+	namespaces := &v1.NamespaceList{}
+	if err := cli.List(ctx, namespaces, clipkg.MatchingLabelsSelector{Selector: selector}); err != nil {
+		return false
+	}
+	for _, ns := range namespaces.Items {
+		if ns.Name == namespace {
+			return true
+		}
+	}
+	return false
 }
 
-func getCredentialsFromIdentity(ctx context.Context, cli clipkg.Client, identity *capociv1beta2.OCIClusterIdentity) (*Credentials, error) {
+func getIdentity(ctx context.Context, cli clipkg.Client, nsn types.NamespacedName) (*CAPIIdentity, error) {
+	unstructuredIdentity := &unstructured.Unstructured{}
+	unstructuredIdentity.SetGroupVersionKind(gvkOCIClusterIdentity)
+	if err := cli.Get(ctx, nsn, unstructuredIdentity); err != nil {
+		return nil, err
+	}
+	identityBytes, err := json.Marshal(unstructuredIdentity.Object)
+	if err != nil {
+		return nil, err
+	}
+	identity := &CAPIIdentity{}
+	err = json.Unmarshal(identityBytes, identity)
+	return identity, err
+}
+
+func getCredentialsFromIdentity(ctx context.Context, cli clipkg.Client, identity *CAPIIdentity) (*Credentials, error) {
 	nsn := types.NamespacedName{
 		Namespace: identity.Spec.PrincipalSecret.Namespace,
 		Name:      identity.Spec.PrincipalSecret.Name,
--- cluster-operator/controllers/quickcreate/controller/oci/credentials_test.go
+++ cluster-operator/controllers/quickcreate/controller/oci/credentials_test.go
@@ -5,77 +5,124 @@ package oci
 
 import (
 	"context"
-	capociv1beta2 "github.com/oracle/cluster-api-provider-oci/api/v1beta2"
+	_ "embed"
+	"encoding/json"
 	"github.com/stretchr/testify/assert"
 	vmcv1alpha1 "github.com/verrazzano/verrazzano/cluster-operator/apis/clusters/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	apiyaml "k8s.io/apimachinery/pkg/util/yaml"
 	clipkg "sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 	"testing"
 )
 
-var testRef = vmcv1alpha1.NamespacedRef{
-	Name:      "test",
-	Namespace: "test",
+var (
+	testRef = vmcv1alpha1.NamespacedRef{
+		Name:      "test",
+		Namespace: "test",
+	}
+	scheme *runtime.Scheme
+	//go:embed testdata/identity-base.yaml
+	testIdentityBase []byte
+	//go:embed testdata/patch-allow-all.yaml
+	testPatchAllowAll []byte
+	//go:embed testdata/patch-allow-default-ns.yaml
+	testPatchAllowDefaultNs []byte
+	//go:embed testdata/patch-allow-test-ns.yaml
+	testPatchAllowTestNs []byte
+	//go:embed testdata/patch-allow-test-ns-by-selector.yaml
+	testPatchAllowTestNsBySelector []byte
+)
+
+func init() {
+	scheme = runtime.NewScheme()
+	_ = corev1.AddToScheme(scheme)
+	_ = vmcv1alpha1.AddToScheme(scheme)
 }
 
 func TestLoadCredentials(t *testing.T) {
-	emptyClient := fake.NewClientBuilder().WithScheme().Build()
+	selectorNamespace := &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: testRef.Name,
+			Labels: map[string]string{
+				"foo": "bar",
+			},
+		},
+	}
+	clusterNamespace := testRef.Namespace
 	var tests = []struct {
-		name             string
-		cli              clipkg.Client
-		hasError         bool
-		clusterNamespace string
+		name     string
+		cli      clipkg.Client
+		hasError bool
 	}{
 		{
-			"errors when identity not found",
-			fake.
+			"access when identity allows test namespace by selector",
+			fake.NewClientBuilder().WithScheme(scheme).WithObjects(newTestIdentity(testPatchAllowTestNsBySelector), newTestSecret(), selectorNamespace).Build(),
+			false,
+		},
+		{
+			"access when identity allows test namespace",
+			fake.NewClientBuilder().WithScheme(scheme).WithObjects(newTestIdentity(testPatchAllowTestNs), newTestSecret()).Build(),
+			false,
+		},
+		{
+			"deny when identity allows a namespace that isn't the test namespaces",
+			fake.NewClientBuilder().WithScheme(scheme).WithObjects(newTestIdentity(testPatchAllowDefaultNs), newTestSecret()).Build(),
+			true,
+		},
+		{
+			"allow when identity allows all namespaces",
+			fake.NewClientBuilder().WithScheme(scheme).WithObjects(newTestIdentity(testPatchAllowAll), newTestSecret()).Build(),
+			false,
+		},
+		{
+			"deny when identity has no allowedNamespaces",
+			fake.NewClientBuilder().WithScheme(scheme).WithObjects(newTestIdentity(nil)).Build(),
+			true,
+		},
+		{
+			"deny when identity not found",
+			fake.NewClientBuilder().WithScheme(scheme).Build(),
+			true,
 		},
 	}
 
 	for _, tt := range tests {
-		c, err := LoadCredentials(context.TODO(), tt.cli, testRef, tt.clusterNamespace)
-		if tt.hasError {
-			assert.Error(t, err)
-			assert.Nil(t, c)
-		} else {
-			assert.NoError(t, err)
-			assert.NotNil(t, c)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			c, err := CredentialsLoaderImpl{}.LoadCredentialsIfAllowed(context.TODO(), tt.cli, testRef, clusterNamespace)
+			if tt.hasError {
+				assert.Error(t, err)
+				assert.Nil(t, c)
+			} else {
+				assert.NoError(t, err)
+				assert.NotNil(t, c)
+			}
+		})
 	}
 }
 
-func testPrincipalSecret(creds *Credentials) *corev1.Secret {
+func newTestIdentity(allowedNamespaces []byte) *unstructured.Unstructured {
+	j, _ := apiyaml.ToJSON(testIdentityBase)
+	obj, _ := runtime.Decode(unstructured.UnstructuredJSONScheme, j)
+	identity := obj.(*unstructured.Unstructured)
+	if len(allowedNamespaces) > 0 {
+		allowedNamespacesJson, _ := apiyaml.ToJSON(allowedNamespaces)
+		m := map[string]interface{}{}
+		_ = json.Unmarshal(allowedNamespacesJson, &m)
+		identity.Object["spec"].(map[string]interface{})["allowedNamespaces"] = m
+	}
+	return identity
+}
+
+func newTestSecret() *corev1.Secret {
 	return &corev1.Secret{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      testRef.Name,
 			Namespace: testRef.Namespace,
 		},
-		Data: map[string][]byte{
-			ociUseInstancePrincipalField: []byte(creds.UseInstancePrincipal),
-			ociPassphraseField:           []byte(creds.Passphrase),
-			ociFingerprintField:          []byte(creds.Fingerprint),
-			ociKeyField:                  []byte(creds.PrivateKey),
-			ociUserField:                 []byte(creds.User),
-			ociTenancyField:              []byte(creds.Tenancy),
-			ociRegionField:               []byte(creds.Region),
-		},
-	}
-}
-
-func testIdentity(namespaces *capociv1beta2.AllowedNamespaces) *capociv1beta2.OCIClusterIdentity {
-	return &capociv1beta2.OCIClusterIdentity{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      testRef.Name,
-			Namespace: testRef.Namespace,
-		},
-		Spec: capociv1beta2.OCIClusterIdentitySpec{
-			AllowedNamespaces: namespaces,
-			PrincipalSecret: corev1.SecretReference{
-				Name:      testRef.Name,
-				Namespace: testRef.Namespace,
-			},
-		},
+		Data: map[string][]byte{},
 	}
 }
