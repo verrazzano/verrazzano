b78f176adb1f6dced77678adc3a0c6885386bcae
Paul Mackin <paul.mackin@oracle.com>
2023-08-29 10:54:55 -0400

    checkpoint - fix unit tests

---
+++ platform-operator/controllers/verrazzano/component/prometheus/pushgateway/module_integration.go
@@ -0,0 +1,15 @@
+// Copyright (c) 2023, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package pushgateway
+
+import (
+	"github.com/verrazzano/verrazzano-modules/pkg/controller/spi/controllerspi"
+	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/common/watch"
+	promoperator "github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/prometheus/operator"
+)
+
+// GetWatchDescriptors returns the list of WatchDescriptors for objects being watched by the component
+func (c prometheusPushgatewayComponent) GetWatchDescriptors() []controllerspi.WatchDescriptor {
+	return watch.GetModuleReadyWatches([]string{promoperator.ComponentName})
+}
