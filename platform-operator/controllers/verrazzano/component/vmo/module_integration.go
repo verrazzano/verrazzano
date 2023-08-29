11960d0dc61258fc19ca07ec945d9aa385f61527
Paul Mackin <paul.mackin@oracle.com>
2023-08-29 11:11:35 -0400

    checkpoint - finish getwatchdescriptors

---
+++ platform-operator/controllers/verrazzano/component/vmo/module_integration.go
@@ -0,0 +1,15 @@
+// Copyright (c) 2023, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package vmo
+
+import (
+	"github.com/verrazzano/verrazzano-modules/pkg/controller/spi/controllerspi"
+	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/common/watch"
+	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/nginx"
+)
+
+// GetWatchDescriptors returns the list of WatchDescriptors for objects being watched by the component
+func (c vmoComponent) GetWatchDescriptors() []controllerspi.WatchDescriptor {
+	return watch.GetModuleReadyWatches([]string{nginx.ComponentName})
+}
