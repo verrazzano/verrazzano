5bd0fd20c3c7bc6bfe019a4a9146761463b1b82f
raaghav.w.wadhawan@oracle.com <raagwadh@raagwadh-1.subnet2ad3phx.devweblogicphx.oraclevcn.com>
2023-08-31 22:38:27 +0000

    copyright

--- ci/scripts/check_image_sizes.sh
+++ ci/scripts/check_image_sizes.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-
+#
+# Copyright (c) 2022, Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+#
 oci --region us-ashburn-1 os object get --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_BUCKET} --name ${CLEAN_BRANCH_NAME}/image-list --file ${WORKSPACE}/image-sizes-objectstore.txt
 if [ $? -ne  0 ] ; then
   echo "image-sizes.txt not found"
