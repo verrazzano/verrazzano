9aa3bbf9a5b4eca718dab1a02f9de485cad67327
raaghav.w.wadhawan@oracle.com <raagwadh@raagwadh-1.subnet2ad3phx.devweblogicphx.oraclevcn.com>
2023-08-31 21:22:15 +0000

    get diff for image sizes sh update

--- ci/scripts/check_image_sizes.sh
+++ ci/scripts/check_image_sizes.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-oci --region us-ashburn-1 os object get --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_BUCKET} --name ${CLEAN_BRANCH_NAME}/image-list --file ${WORKSPACE}/image-sizes.txt
+oci --region us-ashburn-1 os object get --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_BUCKET} --name ${CLEAN_BRANCH_NAME}/image-list --file ${WORKSPACE}/image-sizes-objectstore.txt
 if [ $? -ne  0 ] ; then
   echo "image-sizes.txt not found"
   oci --region us-ashburn-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_BUCKET} --name ${CLEAN_BRANCH_NAME}/image-list --file ${WORKSPACE}/image-sizes.txt
-  if [ $? -ne 0 ] ; then
-     echo "os object put --file image-sizes.txt failed"
-     exit
+  if [ $? -eq 0 ] ; then
+      exit
   fi
+  echo "os object put --file image-sizes.txt failed"
+  exit
 fi
 
-diff $IMAGE_LIST 1x.txt > out.txt }
+diff ${WORKSPACE}/image-sizes-objectstore.txt ${WORKSPACE}/image-sizes.txt > returnvalue.txt
 if [ $? -eq 0 ] ; then
-  rm out.txt
+  rm returnvalue.txt
 fi
