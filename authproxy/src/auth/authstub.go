e384d58d34fd0d18d4cf0548c58194e62cb5cbbb
Deva Sagar <deva.sagar@oracle.com>
2023-09-06 11:14:30 -0400

    export fake authenticator

--- authproxy/src/auth/authstub.go
+++ authproxy/src/auth/authstub.go
@@ -11,7 +11,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-var _ Authenticator = fakeAuthenticator{nil, nil, nil}
+var _ Authenticator = FakeAuthenticator{nil, nil, nil}
 
 type Authenticator interface {
 	AuthenticateRequest(req *http.Request, rw http.ResponseWriter) (bool, error)
@@ -24,25 +24,25 @@ type OIDCConfiguration struct {
 	CallbackURL string
 }
 
-type fakeAuthenticator struct {
+type FakeAuthenticator struct {
 	oidcConfig *OIDCConfiguration
 	Log        *zap.SugaredLogger
 	K8sClient  client.Client
 }
 
-func NewFakeAuthenticator(oidcConfig *OIDCConfiguration, log *zap.SugaredLogger, client client.Client) *fakeAuthenticator {
-	return &fakeAuthenticator{oidcConfig: oidcConfig, Log: log, K8sClient: client}
+func NewFakeAuthenticator(oidcConfig *OIDCConfiguration, log *zap.SugaredLogger, client client.Client) *FakeAuthenticator {
+	return &FakeAuthenticator{oidcConfig: oidcConfig, Log: log, K8sClient: client}
 }
 
 // Authenticate authenticates the given request. If a redirect or error has been processed, then
 // return true to indicate the request has been fully processed. Otherwise return false to indicate
 // that request processing should continue
-func (a fakeAuthenticator) AuthenticateRequest(req *http.Request, rw http.ResponseWriter) (bool, error) {
+func (a FakeAuthenticator) AuthenticateRequest(req *http.Request, rw http.ResponseWriter) (bool, error) {
 	// request is not processed
 	return false, nil
 }
 
 // AuthenticateToken authenticates the given token
-func (a fakeAuthenticator) AuthenticateToken(ctx context.Context, token string) (bool, error) {
+func (a FakeAuthenticator) AuthenticateToken(ctx context.Context, token string) (bool, error) {
 	return true, nil
 }
