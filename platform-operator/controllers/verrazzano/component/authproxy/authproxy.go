399cfb10fd660d3a3d075bd7d7ec9d8ef0772793
Fred Tibbitts III <44442080+fredtibbitts@users.noreply.github.com>
2023-09-06 08:38:21 -0400

    VZ-10836: Updates to Auth Proxy v2 configuration (#6954)

--- platform-operator/controllers/verrazzano/component/authproxy/authproxy.go
+++ platform-operator/controllers/verrazzano/component/authproxy/authproxy.go
@@ -6,13 +6,15 @@ package authproxy
 import (
 	"context"
 	"fmt"
+	"io/fs"
+	"os"
+
+	vzconst "github.com/verrazzano/verrazzano/pkg/constants"
 	"github.com/verrazzano/verrazzano/pkg/k8s/ready"
 	"github.com/verrazzano/verrazzano/pkg/vzcr"
 	"github.com/verrazzano/verrazzano/platform-operator/constants"
-	"io/fs"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"os"
 
 	"github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1beta1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -87,8 +89,9 @@ func AppendOverrides(ctx spi.ComponentContext, _ string, _ string, _ string, kvs
 		mgdClusterOidcClient = fmt.Sprintf("verrazzano-%s", clusterName)
 	}
 
+	oidcProviderHost := fmt.Sprintf("keycloak.%s.%s", overrides.Config.EnvName, dnsSuffix)
 	overrides.Proxy = &proxyValues{
-		OidcProviderHost:          fmt.Sprintf("keycloak.%s.%s", overrides.Config.EnvName, dnsSuffix),
+		OidcProviderHost:          oidcProviderHost,
 		OidcProviderHostInCluster: keycloakInClusterURL,
 		PKCEClientID:              adminClusterOidcID,
 	}
@@ -128,6 +131,9 @@ func AppendOverrides(ctx spi.ComponentContext, _ string, _ string, _ string, kvs
 
 	// Append any installArgs overrides in vzkvs after the file overrides to ensure precedence of those
 	kvs = append(kvs, bom.KeyValue{Value: overridesFileName, IsFile: true})
+
+	// Append auth proxy v2 overrides
+	kvs = appendOIDCOverrides(kvs, keycloakInClusterURL, oidcProviderHost, vzconst.VerrazzanoOIDCSystemRealm)
 
 	return appendAuthProxyImageOverrides(kvs), nil
 }
@@ -335,5 +341,16 @@ func appendAuthProxyImageOverrides(kvs []bom.KeyValue) []bom.KeyValue {
 			Value: envImageOverride,
 		})
 	}
+	return kvs
+}
+
+// appendOIDCOverrides appends overrides related to OIDC configuration
+func appendOIDCOverrides(kvs []bom.KeyValue, oidcServiceHost, oidcExternalHost, realm string) []bom.KeyValue {
+	oidcServiceURL := fmt.Sprintf("http://%s/auth/realms/%s", oidcServiceHost, realm)
+	kvs = append(kvs, bom.KeyValue{Key: "v2.oidcServiceURL", Value: oidcServiceURL})
+
+	oidcExternalURL := fmt.Sprintf("https://%s/auth/realms/%s", oidcExternalHost, realm)
+	kvs = append(kvs, bom.KeyValue{Key: "v2.oidcExternalURL", Value: oidcExternalURL})
+
 	return kvs
 }
