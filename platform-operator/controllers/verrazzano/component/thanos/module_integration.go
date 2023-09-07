0b013f0d1af3187267c0f494422adf065ee7519e
Ben McLean <mbenmclean@gmail.com>
2023-09-07 15:42:20 -0400

    add dns to thanos (#6983)

--- platform-operator/controllers/verrazzano/component/thanos/module_integration.go
+++ platform-operator/controllers/verrazzano/component/thanos/module_integration.go
@@ -6,7 +6,7 @@ package thanos
 import (
 	"github.com/verrazzano/verrazzano-modules/pkg/controller/spi/controllerspi"
 	"github.com/verrazzano/verrazzano/pkg/vzcr"
-	"github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1alpha1"
+	vzapi "github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1alpha1"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/common/watch"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/fluentoperator"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/nginx"
@@ -16,17 +16,40 @@ import (
 )
 
 type valuesConfig struct {
-	IstioInjectionEnabled bool `json:"istioInjectionEnabled"`
+	IstioInjectionEnabled bool                         `json:"istioInjectionEnabled"`
+	Ingress               *vzapi.IngressNginxComponent `json:"ingress,omitempty"`
+	DNS                   *vzapi.DNSComponent          `json:"dns,omitempty"`
+	EnvironmentName       string                       `json:"environmentName,omitempty"`
 }
 
 // GetModuleConfigAsHelmValues returns an unstructured JSON snippet representing the portion of the Verrazzano CR that corresponds to the module
-func (c ThanosComponent) GetModuleConfigAsHelmValues(effectiveCR *v1alpha1.Verrazzano) (*apiextensionsv1.JSON, error) {
+func (c ThanosComponent) GetModuleConfigAsHelmValues(effectiveCR *vzapi.Verrazzano) (*apiextensionsv1.JSON, error) {
 	if effectiveCR == nil {
 		return nil, nil
 	}
 
 	configSnippet := valuesConfig{
 		IstioInjectionEnabled: vzcr.IsIstioInjectionEnabled(effectiveCR),
+	}
+
+	dns := effectiveCR.Spec.Components.DNS
+	if dns != nil {
+		configSnippet.DNS = &vzapi.DNSComponent{
+			External:         dns.External,
+			InstallOverrides: vzapi.InstallOverrides{},
+			OCI:              dns.OCI,
+			Wildcard:         dns.Wildcard,
+		}
+	}
+
+	nginx := effectiveCR.Spec.Components.Ingress
+	if nginx != nil {
+		configSnippet.Ingress = nginx.DeepCopy()
+		configSnippet.Ingress.InstallOverrides.ValueOverrides = []vzapi.Overrides{}
+	}
+
+	if len(effectiveCR.Spec.EnvironmentName) > 0 {
+		configSnippet.EnvironmentName = effectiveCR.Spec.EnvironmentName
 	}
 
 	return spi.NewModuleConfigHelmValuesWrapper(configSnippet)
