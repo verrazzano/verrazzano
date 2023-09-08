29890b321c6238760091881b43c9d92a5419f72f
Paul Mackin <paul.mackin@oracle.com>
2023-09-08 12:29:37 -0400

    label namespaces after istio installed

--- platform-operator/controllers/verrazzano/component/istio/istio_component.go
+++ platform-operator/controllers/verrazzano/component/istio/istio_component.go
@@ -456,12 +456,21 @@ func (i istioComponent) PreUpgrade(context spi.ComponentContext) error {
 	return webhook.DeleteMutatingWebhookConfiguration(context.Log(), context.Client(), webhooks)
 }
 
-func (i istioComponent) PostUpgrade(context spi.ComponentContext) error {
-	err := deleteIstioCoreDNS(context)
+func (i istioComponent) PostUpgrade(compContext spi.ComponentContext) error {
+	// Make sure namespaces get updated with Istio Enabled
+	common.CreateAndLabelNamespaces(compContext)
+
+	// During upgrade there is a window where the latest Istio envoy sidecar container is not included in a pod.
+	// Restart system components that are missing the sidecar.
+	if err := restart.RestartComponents(compContext.Log(), config.GetInjectedSystemNamespaces(), compContext.ActualCR().Generation, &restart.OutdatedSidecarPodMatcher{}); err != nil {
+		return err
+	}
+
+	err := deleteIstioCoreDNS(compContext)
 	if err != nil {
 		return err
 	}
-	err = removeIstioHelmSecrets(context)
+	err = removeIstioHelmSecrets(compContext)
 	if err != nil {
 		return err
 	}
