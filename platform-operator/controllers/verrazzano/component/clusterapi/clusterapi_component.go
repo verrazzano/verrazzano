5395ec2b2c37fe71b38d0d60572e7e2309adb4c0
ben.mclean <ben.mclean@oracle.com>
2023-09-15 09:39:35 -0400

    add rancher as capi dependency

--- platform-operator/controllers/verrazzano/component/clusterapi/clusterapi_component.go
+++ platform-operator/controllers/verrazzano/component/clusterapi/clusterapi_component.go
@@ -13,6 +13,7 @@ import (
 	"github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1beta1"
 	vpoconstants "github.com/verrazzano/verrazzano/platform-operator/constants"
 	cmconstants "github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/certmanager/constants"
+	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/rancher"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/spi"
 	"github.com/verrazzano/verrazzano/platform-operator/internal/config"
 	appsv1 "k8s.io/api/apps/v1"
@@ -109,7 +110,7 @@ func (c clusterAPIComponent) GetModuleConfigAsHelmValues(effectiveCR *v1alpha1.V
 
 // GetDependencies returns the dependencies of this component.
 func (c clusterAPIComponent) GetDependencies() []string {
-	return []string{cmconstants.CertManagerComponentName, cmconstants.ClusterIssuerComponentName}
+	return []string{cmconstants.CertManagerComponentName, cmconstants.ClusterIssuerComponentName, rancher.ComponentName}
 }
 
 // IsReady indicates whether a component is Ready for dependency components.
