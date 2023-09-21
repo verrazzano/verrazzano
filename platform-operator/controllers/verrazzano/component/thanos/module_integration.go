edfcf881220d91149675886050bb9c38bb8d1036
Michael Cico <46466506+michael-cico@users.noreply.github.com>
2023-09-21 18:35:23 -0400

    Clean up thanos module integration code

--- platform-operator/controllers/verrazzano/component/thanos/module_integration.go
+++ platform-operator/controllers/verrazzano/component/thanos/module_integration.go
@@ -5,32 +5,12 @@ package thanos
 
 import (
 	"github.com/verrazzano/verrazzano-modules/pkg/controller/spi/controllerspi"
-	vzapi "github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1alpha1"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/common/watch"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/fluentoperator"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/istio"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/nginx"
 	promoperator "github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/prometheus/operator"
-	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 )
-
-//type valuesConfig struct {
-//	IstioInjectionEnabled bool `json:"istioInjectionEnabled"`
-//}
-
-// GetModuleConfigAsHelmValues returns an unstructured JSON snippet representing the portion of the Verrazzano CR that corresponds to the module
-func (c ThanosComponent) GetModuleConfigAsHelmValues(effectiveCR *vzapi.Verrazzano) (*apiextensionsv1.JSON, error) {
-	return nil, nil
-	//if effectiveCR == nil {
-	//	return nil, nil
-	//}
-	//
-	//configSnippet := valuesConfig{
-	//	IstioInjectionEnabled: vzcr.IsIstioInjectionEnabled(effectiveCR),
-	//}
-	//
-	//return spi.NewModuleConfigHelmValuesWrapper(configSnippet)
-}
 
 // GetWatchDescriptors returns the list of WatchDescriptors for objects being watched by the component
 func (c ThanosComponent) GetWatchDescriptors() []controllerspi.WatchDescriptor {
