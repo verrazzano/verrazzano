670f7c9c1cff80b8e8bf7c0c2b64ea43a5335485
ben.mclean <ben.mclean@oracle.com>
2023-09-25 18:13:53 -0400

    update bom

--- platform-operator/controllers/verrazzano/catalog/catalog_test.go
+++ platform-operator/controllers/verrazzano/catalog/catalog_test.go
@@ -14,7 +14,9 @@ import (
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/registry"
 	"github.com/verrazzano/verrazzano/platform-operator/internal/config"
 	"io"
+	"os/exec"
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -111,80 +113,88 @@ func TestGetVersionForAllRegistryComponents(t *testing.T) {
 }
 
 func TestCompareBOMWithRemote(t *testing.T) {
-	config.SetDefaultBomFilePath(bomPath)
+	if checkBOMModifiedInBranch(t) {
+		config.SetDefaultBomFilePath(bomPath)
 
-	// get the local bom
-	localBOM, err := bom.NewBom(bomPath)
-	assert.NoError(t, err)
-	assert.NotNil(t, localBOM)
+		// get the local bom
+		localBOM, err := bom.NewBom(bomPath)
+		assert.NoError(t, err)
+		assert.NotNil(t, localBOM)
 
-	// get the remote bom from master
-	req, err := retryablehttp.NewRequest("GET", remoteBOMPath, nil)
-	assert.NoError(t, err)
-	client := retryablehttp.NewClient()
-	resp, err := client.Do(req)
-	assert.NoError(t, err)
-	assert.NotNil(t, resp)
-	bodyRaw, err := io.ReadAll(resp.Body)
-	assert.NoError(t, err)
-	assert.NotEmpty(t, bodyRaw)
-	remoteBOM, err := bom.NewBOMFromJSON(bodyRaw)
-	assert.NoError(t, err)
-	assert.NotNil(t, remoteBOM)
+		// get the remote bom from master
+		req, err := retryablehttp.NewRequest("GET", remoteBOMPath, nil)
+		assert.NoError(t, err)
+		client := retryablehttp.NewClient()
+		resp, err := client.Do(req)
+		assert.NoError(t, err)
+		assert.NotNil(t, resp)
+		bodyRaw, err := io.ReadAll(resp.Body)
+		assert.NoError(t, err)
+		assert.NotEmpty(t, bodyRaw)
+		remoteBOM, err := bom.NewBOMFromJSON(bodyRaw)
+		assert.NoError(t, err)
+		assert.NotNil(t, remoteBOM)
 
-	// get the local catalog
-	localCatalog, err := NewCatalog(catalogPath)
-	assert.NoError(t, err)
-	assert.NotNil(t, localCatalog)
+		// get the local catalog
+		localCatalog, err := NewCatalog(catalogPath)
+		assert.NoError(t, err)
+		assert.NotNil(t, localCatalog)
 
-	// get the remote catalog from master
-	req, err = retryablehttp.NewRequest("GET", remoteCatalogPath, nil)
-	assert.NoError(t, err)
-	resp, err = client.Do(req)
-	assert.NoError(t, err)
-	assert.NotNil(t, resp)
-	bodyRaw, err = io.ReadAll(resp.Body)
-	assert.NoError(t, err)
-	assert.NotEmpty(t, bodyRaw)
-	remoteCatalog, err := NewCatalogfromYAML(bodyRaw)
-	assert.NoError(t, err)
-	assert.NotNil(t, remoteCatalog)
+		// get the remote catalog from master
+		req, err = retryablehttp.NewRequest("GET", remoteCatalogPath, nil)
+		assert.NoError(t, err)
+		resp, err = client.Do(req)
+		assert.NoError(t, err)
+		assert.NotNil(t, resp)
+		bodyRaw, err = io.ReadAll(resp.Body)
+		assert.NoError(t, err)
+		assert.NotEmpty(t, bodyRaw)
+		remoteCatalog, err := NewCatalogfromYAML(bodyRaw)
+		assert.NoError(t, err)
+		assert.NotNil(t, remoteCatalog)
 
-	for _, module := range localCatalog.Modules {
-		if vzString.SliceContainsString(modulesNotInBom, module.Name) ||
-			module.Version == constants.BomVerrazzanoVersion {
-			continue
-		}
-		overridedModule, ok := subcomponentOverrides[module.Name]
-		updated := false
-		if ok {
-			for _, override := range overridedModule {
-				localBOMImage, err := localBOM.FindImage(override.subcomponentName, override.imageName)
+		for _, module := range localCatalog.Modules {
+			if vzString.SliceContainsString(modulesNotInBom, module.Name) ||
+				module.Version == constants.BomVerrazzanoVersion {
+				continue
+			}
+			overridedModule, ok := subcomponentOverrides[module.Name]
+			updated := false
+			if ok {
+				for _, override := range overridedModule {
+					localBOMImage, err := localBOM.FindImage(override.subcomponentName, override.imageName)
+					assert.NoError(t, err)
+					remoteBOMImage, err := remoteBOM.FindImage(override.subcomponentName, override.imageName)
+					assert.NoError(t, err)
+					if localBOMImage != remoteBOMImage {
+						updated = true
+					}
+				}
+			} else {
+				localComponent, err := localBOM.GetComponent(module.Name)
 				assert.NoError(t, err)
-				remoteBOMImage, err := remoteBOM.FindImage(override.subcomponentName, override.imageName)
+				remoteComponent, err := remoteBOM.GetComponent(module.Name)
 				assert.NoError(t, err)
-				if localBOMImage != remoteBOMImage {
+				if !reflect.DeepEqual(localComponent, remoteComponent) {
 					updated = true
 				}
 			}
-		} else {
-			localComponent, err := localBOM.GetComponent(module.Name)
-			assert.NoError(t, err)
-			remoteComponent, err := remoteBOM.GetComponent(module.Name)
-			assert.NoError(t, err)
-			if !reflect.DeepEqual(localComponent, remoteComponent) {
-				updated = true
+
+			if updated {
+				localVersion, err := semver.NewSemVersion(localCatalog.GetVersion(module.Name))
+				assert.NoError(t, err)
+				remoteVersion, err := semver.NewSemVersion(remoteCatalog.GetVersion(module.Name))
+				assert.NoError(t, err)
+				assert.Truef(t, localVersion.IsGreatherThan(remoteVersion),
+					"BOM entry for module %s has been updated so local catalog version %s should be greater than remote catalog version %s",
+					module.Name, localVersion.ToString(), remoteVersion.ToString())
 			}
 		}
-
-		if updated {
-			localVersion, err := semver.NewSemVersion(localCatalog.GetVersion(module.Name))
-			assert.NoError(t, err)
-			remoteVersion, err := semver.NewSemVersion(remoteCatalog.GetVersion(module.Name))
-			assert.NoError(t, err)
-			assert.Truef(t, localVersion.IsGreatherThan(remoteVersion),
-				"BOM entry for module %s has been updated so local catalog version %s should be greater than remote catalog version %s",
-				module.Name, localVersion.ToString(), remoteVersion.ToString())
-		}
 	}
+}
+
+func checkBOMModifiedInBranch(t *testing.T) bool {
+	out, err := exec.Command("git", "diff", "--name-only", "origin/master...").Output()
+	assert.NoError(t, err)
+	return strings.Contains(string(out), "platform-operator/verrazzano-bom.json")
 }
