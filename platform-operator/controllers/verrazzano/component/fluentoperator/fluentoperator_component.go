d861350df0f89d5b248a2d3d1bc512cf424c0fd4
Prasad Shirodkar <prasad.shirodkar@oracle.com>
2023-09-25 13:06:04 -0700

    added the missing dependency of istio on fluent operator

--- platform-operator/controllers/verrazzano/component/fluentoperator/fluentoperator_component.go
+++ platform-operator/controllers/verrazzano/component/fluentoperator/fluentoperator_component.go
@@ -20,6 +20,7 @@ import (
 	"github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1alpha1"
 	"github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1beta1"
 	vzconst "github.com/verrazzano/verrazzano/platform-operator/constants"
+	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/common"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/helm"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/spi"
 	"github.com/verrazzano/verrazzano/platform-operator/internal/config"
@@ -66,7 +67,7 @@ func NewComponent() spi.Component {
 			InstallBeforeUpgrade:      true,
 			ImagePullSecretKeyname:    "operator.imagePullSecrets[0].name",
 			AppendOverridesFunc:       appendOverrides,
-			Dependencies:              []string{"verrazzano-network-policies"},
+			Dependencies:              []string{"verrazzano-network-policies", common.IstioComponentName},
 			GetInstallOverridesFunc:   getOverrides,
 			AvailabilityObjects: &ready.AvailabilityObjects{
 				DeploymentNames: []types.NamespacedName{
