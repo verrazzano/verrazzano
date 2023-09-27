3984f5216d7022dfe03465484ec32caaabfecf3d
ben.mclean <ben.mclean@oracle.com>
2023-09-27 12:56:21 -0400

    update message

--- platform-operator/controllers/verrazzano/catalog/catalog_test.go
+++ platform-operator/controllers/verrazzano/catalog/catalog_test.go
@@ -190,10 +190,11 @@ func TestCompareBOMWithRemote(t *testing.T) {
 				remoteVersion, err := semver.NewSemVersion(remoteCatalog.GetVersion(module.Name))
 				assert.NoError(t, err)
 				assert.Truef(t, localVersion.IsGreatherThan(remoteVersion),
-					"BOM entry for module %s has been updated so local catalog version %s should be greater than remote catalog version %s on target branch %s."+
-						"Increment the prerelease version for module %s in the catalog (platform-operator/manifests/catalog/catalog.yaml)"+
+					"BOM entry for module %s on this branch has been modified from the one on %s.\n"+
+						"The catalog module version %s must also be modifed to be greater than remote catalog module version %s on target branch %s.\n"+
+						"Increment the prerelease version for module %s in the catalog k (platform-operator/manifests/catalog/catalog.yaml)"+
 						"and update the corresponding BOM component version.",
-					module.Name, localVersion.ToString(), remoteVersion.ToString(), targetBranch, module.Name)
+					module.Name, targetBranch, localVersion.ToString(), remoteVersion.ToString(), targetBranch, module.Name)
 			}
 		}
 	}
