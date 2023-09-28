0ed272cc17f357b0b2b82de4e26509ae8dd5c5d9
Marco Gellecanao <marco.m.gellecanao@oracle.com>
2023-09-28 11:17:26 -0700

    VZ-11009: Extend VMC states for CAPI Clusters (#7191)

--- cluster-operator/controllers/vmc/vmc_controller.go
+++ cluster-operator/controllers/vmc/vmc_controller.go
@@ -16,6 +16,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	clustersv1alpha1 "github.com/verrazzano/verrazzano/cluster-operator/apis/clusters/v1alpha1"
+	"github.com/verrazzano/verrazzano/cluster-operator/internal/capi"
 	vzconstants "github.com/verrazzano/verrazzano/pkg/constants"
 	vzctrl "github.com/verrazzano/verrazzano/pkg/controller"
 	"github.com/verrazzano/verrazzano/pkg/log/vzlog"
@@ -28,6 +29,7 @@ import (
 	rbacv1 "k8s.io/api/rbac/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -257,7 +259,7 @@ func (r *VerrazzanoManagedClusterReconciler) doReconcile(ctx context.Context, lo
 	statusErr := r.updateStatus(ctx, vmc)
 
 	if statusErr != nil {
-		log.Errorf("Failed to update status to ready for VMC %s: %v", vmc.Name, err)
+		log.Errorf("Failed to update status to ready for VMC %s: %v", vmc.Name, statusErr)
 	}
 
 	if err := r.syncManagedMetrics(ctx, log, vmc); err != nil {
@@ -623,19 +625,8 @@ func (r *VerrazzanoManagedClusterReconciler) setStatusCondition(vmc *clustersv1a
 
 // updateStatus updates the status of the VMC in the cluster, with all provided conditions, after setting the vmc.Status.State field for the cluster
 func (r *VerrazzanoManagedClusterReconciler) updateStatus(ctx context.Context, vmc *clustersv1alpha1.VerrazzanoManagedCluster) error {
-	if vmc.Status.LastAgentConnectTime != nil {
-		currentTime := metav1.Now()
-		// Using the current plus added time to find the difference with lastAgentConnectTime to validate
-		// if it exceeds the max allowed time before changing the state of the vmc resource.
-		maxPollingTime := currentTime.Add(vzconstants.VMCAgentPollingTimeInterval * vzconstants.MaxTimesVMCAgentPollingTime)
-		timeDiff := maxPollingTime.Sub(vmc.Status.LastAgentConnectTime.Time)
-		if int(timeDiff.Minutes()) > vzconstants.MaxTimesVMCAgentPollingTime {
-			vmc.Status.State = clustersv1alpha1.StateInactive
-		} else if vmc.Status.State == "" {
-			vmc.Status.State = clustersv1alpha1.StatePending
-		} else {
-			vmc.Status.State = clustersv1alpha1.StateActive
-		}
+	if err := r.updateState(vmc); err != nil {
+		return err
 	}
 
 	// Fetch the existing VMC to avoid conflicts in the status update
@@ -654,6 +645,87 @@ func (r *VerrazzanoManagedClusterReconciler) updateStatus(ctx context.Context, v
 
 	r.log.Debugf("Updating Status of VMC %s: %v", vmc.Name, vmc.Status.Conditions)
 	return r.Status().Update(ctx, existingVMC)
+}
+
+// updateState sets the vmc.Status.State for the given VMC.
+// The state field functions differently according to whether this VMC references an underlying ClusterAPI cluster.
+func (r *VerrazzanoManagedClusterReconciler) updateState(vmc *clustersv1alpha1.VerrazzanoManagedCluster) error {
+	// If there is no underlying CAPI cluster, set the state field based on the lastAgentConnectTime
+	if vmc.Status.ClusterRef == nil {
+		r.updateStateFromLastAgentConnectTime(vmc)
+		return nil
+	}
+
+	// If there is an underlying CAPI cluster, set the state field according to the phase of the CAPI cluster.
+	capiClusterPhase, err := r.getCAPIClusterPhase(vmc.Status.ClusterRef)
+	if err != nil {
+		return err
+	}
+	if capiClusterPhase != "" {
+		vmc.Status.State = capiClusterPhase
+	}
+	return nil
+}
+
+// updateStateFromLastAgentConnectTime sets the vmc.Status.State according to the lastAgentConnectTime,
+// setting possible values of Active, Inactive, or Pending.
+func (r *VerrazzanoManagedClusterReconciler) updateStateFromLastAgentConnectTime(vmc *clustersv1alpha1.VerrazzanoManagedCluster) {
+	if vmc.Status.LastAgentConnectTime != nil {
+		currentTime := metav1.Now()
+		// Using the current plus added time to find the difference with lastAgentConnectTime to validate
+		// if it exceeds the max allowed time before changing the state of the vmc resource.
+		maxPollingTime := currentTime.Add(vzconstants.VMCAgentPollingTimeInterval * vzconstants.MaxTimesVMCAgentPollingTime)
+		timeDiff := maxPollingTime.Sub(vmc.Status.LastAgentConnectTime.Time)
+		if int(timeDiff.Minutes()) > vzconstants.MaxTimesVMCAgentPollingTime {
+			vmc.Status.State = clustersv1alpha1.StateInactive
+		} else if vmc.Status.State == "" {
+			vmc.Status.State = clustersv1alpha1.StatePending
+		} else {
+			vmc.Status.State = clustersv1alpha1.StateActive
+		}
+	}
+}
+
+// getCAPIClusterPhase returns the phase reported by the CAPI Cluster CR which is referenced by clusterRef.
+func (r *VerrazzanoManagedClusterReconciler) getCAPIClusterPhase(clusterRef *clustersv1alpha1.ClusterReference) (clustersv1alpha1.StateType, error) {
+	// Get the CAPI Cluster CR
+	cluster := &unstructured.Unstructured{}
+	cluster.SetGroupVersionKind(capi.GVKCAPICluster)
+	clusterNamespacedName := types.NamespacedName{
+		Name:      clusterRef.Name,
+		Namespace: clusterRef.Namespace,
+	}
+	if err := r.Get(context.TODO(), clusterNamespacedName, cluster); err != nil {
+		if errors.IsNotFound(err) {
+			return "", nil
+		}
+		return "", err
+	}
+
+	// Get the state
+	phase, found, err := unstructured.NestedString(cluster.Object, "status", "phase")
+	if !found {
+		r.log.Progressf("could not find status.phase field inside cluster %s: %v", clusterNamespacedName, err)
+		return "", nil
+	}
+	if err != nil {
+		r.log.Progressf("error while looking for status.phase field for cluster %s: %v", clusterNamespacedName, err)
+		return "", nil
+	}
+
+	// Validate that the CAPI Phase is a proper StateType for the VMC
+	switch state := clustersv1alpha1.StateType(phase); state {
+	case clustersv1alpha1.StatePending,
+		clustersv1alpha1.StateProvisioning,
+		clustersv1alpha1.StateProvisioned,
+		clustersv1alpha1.StateDeleting,
+		clustersv1alpha1.StateUnknown,
+		clustersv1alpha1.StateFailed:
+		return state, nil
+	default:
+		r.log.Progressf("retrieved an invalid ClusterAPI Cluster phase of %s", state)
+		return clustersv1alpha1.StateUnknown, nil
+	}
 }
 
 // getVerrazzanoResource gets the installed Verrazzano resource in the cluster (of which only one is expected)
