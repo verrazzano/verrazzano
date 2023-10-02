34e93e593d5d384e4a0461cd6486d524ece003fa
Abijitha Nadagouda <29689969+Abhijithang@users.noreply.github.com>
2023-10-01 23:32:38 -0700

    print pod status

--- tests/e2e/pkg/common.go
+++ tests/e2e/pkg/common.go
@@ -19,7 +19,6 @@ import (
 	"sync"
 	"time"
 
-	"github.com/hashicorp/go-retryablehttp"
 	"github.com/onsi/ginkgo/v2"
 	v12 "github.com/verrazzano/verrazzano-monitoring-operator/pkg/apis/vmcontroller/v1"
 	"github.com/verrazzano/verrazzano/pkg/constants"
@@ -307,6 +306,9 @@ func PodsRunningInClusterWithClient(namespace string, namePrefixes []string, cli
 // SpecificPodsRunningInClusterWithClient checks if all the pods identified by labels and are ready and running in the given cluster
 func SpecificPodsRunningInClusterWithClient(namespace, labels string, client *kubernetes.Clientset) (bool, error) {
 	pods, err := ListPodsWithLabelsInCluster(namespace, labels, client)
+	Log(Info, fmt.Sprintf("POD Name: %v,  Pod NS %v, Pod Status Message %v , Pod Status %v, %v, %v, %v, %v, %v", pods.Items[0].Name, pods.Items[0].Namespace, pods.Items[0].Status.Message, pods.Items[0].Status.ContainerStatuses[0].Image, pods.Items[0].Status.ContainerStatuses[1].State, pods.Items[0].Status.Conditions, pods.Items[0].Status.ContainerStatuses, pods.Items[0].Status.Reason, pods.Items[0].Status.Phase))
+	Log(Info, fmt.Sprintf("POD Name: %v,  Pod NS %v, Pod Status Message %v , Pod Status %v, %v, %v, %v, %v, %v", pods.Items[1].Name, pods.Items[1].Namespace, pods.Items[1].Status.Message, pods.Items[1].Status.ContainerStatuses[1].Image, pods.Items[1].Status.ContainerStatuses[1].State, pods.Items[1].Status.Conditions, pods.Items[1].Status.ContainerStatuses, pods.Items[1].Status.Reason, pods.Items[1].Status.Phase))
+
 	if err != nil {
 		Log(Error, fmt.Sprintf(podListingErrorFmt, namespace, err))
 		return false, fmt.Errorf(podListingErrorFmt, namespace, err)
