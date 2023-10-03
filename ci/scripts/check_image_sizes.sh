9a78126281ea9fb7631b93c2c852f04208bef6b3
Raaghav Wadhawan <raaghav.w.wadhawan@oracle.com>
2023-10-03 22:15:42 +0000

    add ons added

--- ci/scripts/check_image_sizes.sh
+++ ci/scripts/check_image_sizes.sh
@@ -39,7 +39,8 @@ oci --region us-phoenix-1 os object get --namespace ${OCI_OS_NAMESPACE} -bn ${OC
 if [ $? -ne  0 ] ; then
  echo "${CLEAN_BRANCH_NAME}/image-list not found"
  oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_BUCKET} --name ${CLEAN_BRANCH_NAME}/image-list --file ${WORKSPACE}/image-sizes.txt
- touch ${WORKSPACE}/commitID.txt
+echo "${GIT_COMMIT}">${WORKSPACE}/commitID.txt
+echo " ">${WORKSPACE}/newimagefound.txt
  if [ $? -eq 0 ] ; then
      exit
  fi
@@ -70,7 +71,9 @@ done <<< "$IMAGE_DATA_GENERATED"
 for IMAGENAME in "${!IMAGENAME_SIZES_FILE_GENERATED[@]}"; do
   if [[ ! "${IMAGENAME_SIZES_FILE_OS[$IMAGENAME]}" ]]; then
     NEW_IMAGE_FOUND="true"
-    echo "The image-sizes.txt base file contains an image with image name: $IMAGENAME that is not in the newly generated image-sizes.txt." >> ${WORKSPACE}/newimagefound.txt
+    if [[ "$IMAGENAME" != *"Commit ID-"* ]]; then
+        echo "The image-sizes.txt base file contains an image with image name: $IMAGENAME that is not in the newly generated image-sizes.txt." >> ${WORKSPACE}/newimagefound.txt
+    fi
   fi
 done
 
@@ -80,7 +83,7 @@ for IMAGENAME in "${!IMAGENAME_SIZES_FILE_OS[@]}"; do
   FILE_SIZE_OS="${IMAGENAME_SIZES_FILE_OS[$IMAGENAME]}"
   FILE_SIZE_GENERATED="${IMAGENAME_SIZES_FILE_GENERATED[$IMAGENAME]}"
 
-# Check if image size has increased by 0.1 MB or more
+# Check if image size has increased by the threshold or more
   if [ -n "$FILE_SIZE_OS" ] && [ -n "$FILE_SIZE_GENERATED" ] && [ "$FILE_SIZE_GENERATED" -gt 0 ] && [ "$FILE_SIZE_GENERATED" -gt "$((FILE_SIZE_OS+IMAGE_SIZE_INCREASE_THRESHOLD))" ]; then
         IMAGE_SIZE_DIFF_FOUND="true"
     echo "Image size for $IMAGENAME has increased from $((FILE_SIZE_OS/1000000))MB to $((FILE_SIZE_GENERATED/1000000))MB " >> ${WORKSPACE}/result.txt
