25927975ee7587ab487d3bf35070085adfe3127a
Marco Gellecanao <marco.m.gellecanao@oracle.com>
2023-10-03 16:27:48 -0700

    remaining unit tests for internal/capi/cluster.go

--- cluster-operator/internal/capi/cluster_test.go
+++ cluster-operator/internal/capi/cluster_test.go
@@ -14,29 +14,95 @@ import (
 )
 
 const (
-	clusterName = "capi-test-cluster"
-	testNamespace = "test-namespace"
+	clusterName      = "test-cluster"
+	clusterClassName = "test-cluster-class"
+	testNamespace    = "test-namespace"
 )
 
 func TestGetCluster(t *testing.T) {
 	a := assert.New(t)
 	ctx := context.TODO()
 	fakeClient := fake.NewClientBuilder().WithObjects(newCAPICluster(clusterName, testNamespace)).Build()
-	cluster, err := GetCluster(ctx, fakeClient, types.NamespacedName{Name: clusterName, Namespace: testNamespace})
 
+	// get the CAPI cluster
+	cluster, err := GetCluster(ctx, fakeClient, types.NamespacedName{Name: clusterName, Namespace: testNamespace})
 	a.NoError(err)
 	a.Equal(clusterName, cluster.GetName())
 	a.Equal(testNamespace, cluster.GetNamespace())
 	a.Equal(GVKCAPICluster, cluster.GetObjectKind().GroupVersionKind())
 
+	// attempt to get a CAPI cluster that does not exist
 	cluster, err = GetCluster(ctx, fakeClient, types.NamespacedName{Name: "nonexistent-cluster", Namespace: testNamespace})
 	a.Error(err)
 	a.Nil(cluster)
 }
 
+func TestGetClusterClass(t *testing.T) {
+	a := assert.New(t)
+	ctx := context.TODO()
+	fakeClient := fake.NewClientBuilder().WithObjects(newCAPIClusterClass(clusterClassName, testNamespace)).Build()
+
+	// get the CAPI ClusterClass
+	clusterClass, err := GetClusterClass(ctx, fakeClient, types.NamespacedName{Name: clusterClassName, Namespace: testNamespace})
+	a.NoError(err)
+	a.Equal(clusterClassName, clusterClass.GetName())
+	a.Equal(testNamespace, clusterClass.GetNamespace())
+	a.Equal(GVKCAPIClusterClass, clusterClass.GetObjectKind().GroupVersionKind())
+
+	// attempt to get a CAPI ClusterClass that does not exist
+	clusterClass, err = GetClusterClass(ctx, fakeClient, types.NamespacedName{Name: "nonexistent-cluster-class", Namespace: testNamespace})
+	a.Error(err)
+	a.Nil(clusterClass)
+}
+
+func TestGetClusterClassFromCluster(t *testing.T) {
+	a := assert.New(t)
+	ctx := context.TODO()
+	fakeClient := fake.NewClientBuilder().
+		WithObjects(newCAPIClusterWithClassReference(clusterName, clusterClassName, testNamespace), newCAPIClusterClass(clusterClassName, testNamespace)).
+		Build()
+
+	cluster, err := GetCluster(ctx, fakeClient, types.NamespacedName{Name: clusterName, Namespace: testNamespace})
+	a.NoError(err)
+
+	// Get the ClusterClass associated with the Cluster
+	clusterClass, err := GetClusterClassFromCluster(ctx, fakeClient, cluster)
+	a.NoError(err)
+	a.Equal(clusterClassName, clusterClass.GetName())
+	a.Equal(testNamespace, clusterClass.GetNamespace())
+	a.Equal(GVKCAPIClusterClass, clusterClass.GetObjectKind().GroupVersionKind())
+
+	// Shouldn't work if the CAPI Cluster has no reference to a ClusterClass
+	fakeClient = fake.NewClientBuilder().
+		WithObjects(newCAPICluster(clusterName, testNamespace), newCAPIClusterClass(clusterClassName, testNamespace)).
+		Build()
+	cluster, err = GetCluster(ctx, fakeClient, types.NamespacedName{Name: clusterName, Namespace: testNamespace})
+	a.NoError(err)
+	clusterClass, err = GetClusterClassFromCluster(ctx, fakeClient, cluster)
+	a.Error(err)
+	a.Nil(clusterClass)
+}
+
 func newCAPICluster(name, namespace string) *unstructured.Unstructured {
 	cluster := &unstructured.Unstructured{}
 	cluster.SetGroupVersionKind(GVKCAPICluster)
+	cluster.SetName(name)
+	cluster.SetNamespace(namespace)
+	return cluster
+}
+
+func newCAPIClusterWithClassReference(name, className, namespace string) *unstructured.Unstructured {
+	cluster := &unstructured.Unstructured{}
+	cluster.SetGroupVersionKind(GVKCAPICluster)
+	cluster.SetName(name)
+	cluster.SetNamespace(namespace)
+	unstructured.SetNestedField(cluster.Object, className, "spec", "topology", "class")
+	return cluster
+}
+
+func newCAPIClusterClass(name, namespace string) *unstructured.Unstructured {
+	cluster := &unstructured.Unstructured{}
+	cluster.SetGroupVersionKind(GVKCAPIClusterClass)
 	cluster.SetName(name)
 	cluster.SetNamespace(namespace)
 	return cluster
