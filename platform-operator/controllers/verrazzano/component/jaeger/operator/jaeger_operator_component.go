1802148d5f73c9b0f1a70462d7e305ea22c1b115
Zaid A <zaid.abdulrehman85@gmail.com>
2023-10-03 15:40:07 -0700

    Move whhere crd is created

--- platform-operator/controllers/verrazzano/component/jaeger/operator/jaeger_operator_component.go
+++ platform-operator/controllers/verrazzano/component/jaeger/operator/jaeger_operator_component.go
@@ -255,6 +255,13 @@ func (c jaegerOperatorComponent) PreUpgrade(ctx spi.ComponentContext) error {
 		}
 	}
 
+	ctx.Log().Debugf("Applying jaeger crd yaml with dir: %s", c.ChartDir) //debug
+	err = common.ApplyCRDYaml(ctx, c.ChartDir)
+	if err != nil {
+		return err
+	}
+	ctx.Log().Debugf("Done applying jaeger crd yaml") //debug
+
 	createInstance, err := isCreateDefaultJaegerInstance(ctx)
 	if err != nil {
 		return err
@@ -263,13 +270,6 @@ func (c jaegerOperatorComponent) PreUpgrade(ctx spi.ComponentContext) error {
 		// Create Jaeger secret with the OpenSearch credentials
 		return createJaegerSecret(ctx)
 	}
-
-	ctx.Log().Debugf("Applying jaeger crd yaml with dir: %s", c.ChartDir) //debug
-	err = common.ApplyCRDYaml(ctx, c.ChartDir)
-	if err != nil {
-		return err
-	}
-	ctx.Log().Debugf("Done applying jaeger crd yaml") //debug
 
 	return c.HelmComponent.PreUpgrade(ctx)
 }
