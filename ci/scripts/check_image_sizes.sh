90ed829c9a2f0dd864154094c8fb362f269e2ee9
Raaghav Wadhawan <raaghav.w.wadhawan@oracle.com>
2023-10-04 20:40:49 +0000

    new image found

--- ci/scripts/check_image_sizes.sh
+++ ci/scripts/check_image_sizes.sh
@@ -40,7 +40,6 @@ if [ $? -ne  0 ] ; then
  echo "${CLEAN_BRANCH_NAME}/image-list not found"
  oci --region us-phoenix-1 os object put --force --namespace ${OCI_OS_NAMESPACE} -bn ${OCI_OS_BUCKET} --name ${CLEAN_BRANCH_NAME}/image-list --file ${WORKSPACE}/image-sizes.txt
 echo "${GIT_COMMIT}">${WORKSPACE}/commitID.txt
-echo " ">${WORKSPACE}/newimagefound.txt
  if [ $? -eq 0 ] ; then
      exit
  fi
@@ -70,9 +69,9 @@ done <<< "$IMAGE_DATA_GENERATED"
 # Check if imagenames in generated file are not in object store
 for IMAGENAME in "${!IMAGENAME_SIZES_FILE_GENERATED[@]}"; do
   if [[ ! "${IMAGENAME_SIZES_FILE_OS[$IMAGENAME]}" ]]; then
-    NEW_IMAGE_FOUND="true"
     if [[ "$IMAGENAME" != *"Commit ID-"* ]]; then
-    echo "The image-sizes.txt base file contains an image with image name: $IMAGENAME that is not in the newly generated image-sizes.txt." >> ${WORKSPACE}/newimagefound.txt
+      NEW_IMAGE_FOUND="true"
+      echo "The image-sizes.txt base file contains an image with image name: $IMAGENAME that is not in the newly generated image-sizes.txt." >> ${WORKSPACE}/newimagefound.txt
     fi
   fi
 done
