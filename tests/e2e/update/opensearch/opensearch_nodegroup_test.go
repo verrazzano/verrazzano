7597aa7eddca52d64a2c1af18a0523f8163a4b7c
karan k <karan.k@oracle.com>
2023-10-04 21:11:29 +0530

    remove check for downgrading replicas

--- tests/e2e/update/opensearch/opensearch_nodegroup_test.go
+++ tests/e2e/update/opensearch/opensearch_nodegroup_test.go
@@ -25,9 +25,6 @@ var _ = t.Describe("Update opensearch", Label("f:platform-lcm.update"), func() {
 		update.ValidatePods(string(vmov1.MasterRole), nodePoolLabel, constants.VerrazzanoLoggingNamespace, 3, false)
 		update.ValidatePodMemoryRequest(map[string]string{nodePoolLabel: string(vmov1.MasterRole)},
 			constants.VerrazzanoLoggingNamespace, "opensearch", "512Mi")
-		// disabling the master node-pool once testing is done.
-		m = OpensearchMasterNodeGroupModifier{NodeReplicas: 0, NodeMemory: "512Mi", NodeStorage: "2Gi"}
-		update.UpdateCRWithRetries(m, pollingInterval, waitTimeout)
 	})
 
 	// GIVEN a VZ custom resource in dev profile,
@@ -39,9 +36,6 @@ var _ = t.Describe("Update opensearch", Label("f:platform-lcm.update"), func() {
 		update.ValidatePods(string(vmov1.IngestRole), nodePoolLabel, constants.VerrazzanoLoggingNamespace, 3, false)
 		update.ValidatePodMemoryRequest(map[string]string{nodePoolLabel: string(vmov1.IngestRole)},
 			constants.VerrazzanoLoggingNamespace, "opensearch", "512Mi")
-		// disabling this node-pool once testing is done.
-		m = OpensearchIngestNodeGroupModifier{NodeReplicas: 0, NodeMemory: "512Mi", NodeStorage: "2Gi"}
-		update.UpdateCRWithRetries(m, pollingInterval, waitTimeout)
 	})
 
 	// GIVEN a VZ custom resource in dev profile,
