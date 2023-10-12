cacd990fde8a1a3172df24896ab129a8571d8eb2
Jonathan Maron <jonathan.maron@oracle.com>
2023-10-12 13:57:46 -0400

    removed unused code

--- cluster-operator/controllers/capi/capi_cluster_controller.go
+++ cluster-operator/controllers/capi/capi_cluster_controller.go
@@ -13,30 +13,19 @@ import (
 	vzstring "github.com/verrazzano/verrazzano/pkg/string"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/common"
 	"go.uber.org/zap"
-	appsv1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
-	netv1 "k8s.io/api/networking/v1"
-	rbacv1 "k8s.io/api/rbac/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
 
 const (
-	finalizerName                = "verrazzano.io/capi-cluster"
-	clusterProvisionerLabel      = "cluster.verrazzano.io/provisioner"
-	clusterStatusSuffix          = "-cluster-status"
-	clusterIDKey                 = "clusterId"
-	clusterRegistrationStatusKey = "clusterRegistration"
-	registrationRetrieved        = "retrieved"
-	registrationInitiated        = "initiated"
+	finalizerName = "verrazzano.io/capi-cluster"
 )
 
 type CAPIClusterReconciler struct {
@@ -216,122 +205,4 @@ func (r *CAPIClusterReconciler) getVMCName(cluster *unstructured.Unstructured) s
 	}
 	// return the display Name
 	return rancherMgmtCluster.UnstructuredContent()["spec"].(map[string]interface{})["displayName"].(string)
-}
-
-// getClusterClient returns a controller runtime client configured for the workload cluster
-func getClusterClient(restConfig *rest.Config) (client.Client, error) {
-	scheme := runtime.NewScheme()
-	_ = rbacv1.AddToScheme(scheme)
-	_ = v1.AddToScheme(scheme)
-	_ = netv1.AddToScheme(scheme)
-	_ = appsv1.AddToScheme(scheme)
-	_ = clustersv1alpha1.AddToScheme(scheme)
-
-	return client.New(restConfig, client.Options{Scheme: scheme})
-}
-
-// getClusterID returns the cluster ID assigned by Rancher for the given cluster
-func getClusterID(ctx context.Context, client client.Client, cluster *unstructured.Unstructured) string {
-	clusterID := ""
-
-	regStatusSecret, err := getClusterRegistrationStatusSecret(ctx, client, cluster)
-	if err != nil {
-		return clusterID
-	}
-	clusterID = string(regStatusSecret.Data[clusterIDKey])
-
-	return clusterID
-}
-
-// getClusterRegistrationStatus returns the Rancher registration status for the cluster
-func getClusterRegistrationStatus(ctx context.Context, c client.Client, cluster *unstructured.Unstructured) string {
-	clusterStatus := registrationRetrieved
-
-	regStatusSecret, err := getClusterRegistrationStatusSecret(ctx, c, cluster)
-	if err != nil {
-		return clusterStatus
-	}
-	clusterStatus = string(regStatusSecret.Data[clusterRegistrationStatusKey])
-
-	return clusterStatus
-}
-
-// getClusterRegistrationStatusSecret returns the secret that stores cluster status information
-func getClusterRegistrationStatusSecret(ctx context.Context, c client.Client, cluster *unstructured.Unstructured) (*v1.Secret, error) {
-	clusterIDSecret := &v1.Secret{}
-	secretName := types.NamespacedName{
-		Namespace: constants.VerrazzanoCAPINamespace,
-		Name:      cluster.GetName() + clusterStatusSuffix,
-	}
-	err := c.Get(ctx, secretName, clusterIDSecret)
-	if err != nil {
-		return nil, err
-	}
-	return clusterIDSecret, err
-}
-
-// persistClusterStatus stores the cluster status in the cluster status secret
-func persistClusterStatus(ctx context.Context, client client.Client, cluster *unstructured.Unstructured, log *zap.SugaredLogger, clusterID string, status string) error {
-	log.Debugf("Persisting cluster %s cluster id: %s", cluster.GetName(), clusterID)
-	clusterRegistrationStatusSecret := &v1.Secret{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      cluster.GetName() + clusterStatusSuffix,
-			Namespace: constants.VerrazzanoCAPINamespace,
-		},
-	}
-	_, err := ctrl.CreateOrUpdate(ctx, client, clusterRegistrationStatusSecret, func() error {
-		// Build the secret data
-		if clusterRegistrationStatusSecret.Data == nil {
-			clusterRegistrationStatusSecret.Data = make(map[string][]byte)
-		}
-		clusterRegistrationStatusSecret.Data[clusterIDKey] = []byte(clusterID)
-		clusterRegistrationStatusSecret.Data[clusterRegistrationStatusKey] = []byte(status)
-
-		return nil
-	})
-	if err != nil {
-		log.Errorf("Unable to persist status for cluster %s: %v", cluster.GetName(), err)
-		return err
-	}
-
-	return nil
-}
-
-// getWorkloadClusterKubeconfig returns a kubeconfig for accessing the workload cluster
-func getWorkloadClusterKubeconfig(client client.Client, cluster *unstructured.Unstructured, log *zap.SugaredLogger) ([]byte, error) {
-	// get the cluster kubeconfig
-	kubeconfigSecret := &v1.Secret{}
-	err := client.Get(context.TODO(), types.NamespacedName{Name: fmt.Sprintf("%s-kubeconfig", cluster.GetName()), Namespace: cluster.GetNamespace()}, kubeconfigSecret)
-	if err != nil {
-		log.Warn(err, "failed to obtain workload cluster kubeconfig resource. Re-queuing...")
-		return nil, err
-	}
-	kubeconfig, ok := kubeconfigSecret.Data["value"]
-	if !ok {
-		log.Error(err, "failed to read kubeconfig from resource")
-		return nil, fmt.Errorf("Unable to read kubeconfig from retrieved cluster resource")
-	}
-
-	return kubeconfig, nil
-}
-
-func getWorkloadClusterClient(client client.Client, log *zap.SugaredLogger, cluster *unstructured.Unstructured) (client.Client, error) {
-	// identify whether the workload cluster is using "untrusted" certs
-	kubeconfig, err := getWorkloadClusterKubeconfig(client, cluster, log)
-	if err != nil {
-		// requeue since we're waiting for cluster
-		return nil, err
-	}
-	// create a workload cluster client
-	// create workload cluster client
-	restConfig, err := clientcmd.RESTConfigFromKubeConfig(kubeconfig)
-	if err != nil {
-		log.Warnf("Failed getting rest config from workload kubeconfig")
-		return nil, err
-	}
-	workloadClient, err := getClusterClient(restConfig)
-	if err != nil {
-		return nil, err
-	}
-	return workloadClient, nil
 }
