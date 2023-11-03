c242e27f39ca481ae3b129ba17dd16c5cc9c7968
manvi-prog <manvi.g.gupta@oracle.com>
2023-11-03 11:01:26 +0000

    Added unit test case in vzcapture_test.go

--- tools/vz/pkg/helpers/vzcapture.go
+++ tools/vz/pkg/helpers/vzcapture.go
@@ -865,8 +865,8 @@ func isCaExpired(client clipkg.Client, cert v1.Certificate, namespace string) (*
 	return &caCrtInfoForCert, true, nil
 }
 
-// AddEffCr converts the effective verrazzano resouce configmap into a Json file
-func AddEffCr(c clipkg.Client, captureDir string, vz *v1beta1.Verrazzano) error {
+// AddEffectiveCr converts the effective verrazzano resource configmap into a Json file
+func AddEffectiveCr(c clipkg.Client, captureDir string, vz *v1beta1.Verrazzano) error {
 	var effCRConfigmap corev1.ConfigMap
 	err := c.Get(context.Background(), clipkg.ObjectKey{
 		Namespace: vz.ObjectMeta.Namespace,
