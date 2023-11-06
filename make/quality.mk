1a14e8661584547df0bc66c5e12d936110d33d89
Ravi Singhal <ravi.r.singhal@oracle.com>
2023-11-06 16:34:28 +0530

    exclude tests

--- make/quality.mk
+++ make/quality.mk
@@ -16,7 +16,7 @@ check: golangci-lint word-linter url-linter ## run all linters
 
 .PHONY: golangci-lint
 golangci-lint: check-golangci-lint
-	golangci-lint --color never run --max-same-issues 25 --timeout 300s
+	golangci-lint --color never run --max-same-issues 25 --timeout 300s --tests false
 
 .PHONY: check-golangci-lint
 check-golangci-lint: install-golangci-lint ## run Go linters
