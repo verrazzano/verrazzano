b78f409964b2e48731b671159d0adcdd38e0c9ae
Ravi Singhal <ravi.r.singhal@oracle.com>
2023-11-06 16:06:59 +0530

    fixed lint errors

--- pkg/os/shell.go
+++ pkg/os/shell.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2021, 2022, Oracle and/or its affiliates.
+// Copyright (c) 2021, 2023, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 package os
@@ -13,9 +13,8 @@ var bashRunner CmdRunner = DefaultRunner{}
 // RunBash runs a bash script
 func RunBash(inArgs ...string) (string, string, error) {
 	args := []string{}
-	for i := range inArgs {
-		args = append(args, inArgs[i])
-	}
+	args = append(args, inArgs...)
+
 	cmd := osexec.Command("bash", args...)
 	stdout, stderr, err := bashRunner.Run(cmd)
 	if err != nil {
