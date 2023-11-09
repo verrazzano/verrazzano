eda6d7902ffb4695dbd066a079845ef8d353d233
Deva Sagar <deva.sagar@oracle.com>
2023-11-09 13:43:26 -0500

    VZ-11441 - Switch the Verrazzano application operator image to use the ol8-static image as its base image. (#7443)

--- platform-operator/helm_config/charts/verrazzano-application-operator/wait4webhook.sh
+++ platform-operator/helm_config/charts/verrazzano-application-operator/wait4webhook.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2020, 2022, Oracle and/or its affiliates.
+# Copyright (c) 2020, 2023, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 #While loop for verrazzano-platform-operator to wait for webhooks to be started before starting up
 while [[ "$(curl --insecure -s -o /dev/null -w '%{http_code}' https://verrazzano-application-operator-webhook:443/validate-clusters-verrazzano-io-v1alpha1-multiclusterapplicationconfiguration -H 'Content-Type: application/json')" != "200" ]]; do sleep 5; done
