b0a4d4f7881d5b06fc8564958561b99d9d50163e
Ravi Singhal <ravi.r.singhal@oracle.com>
2023-11-20 15:09:43 +0530

    revert manifest

--- platform-operator/clientset/versioned/clientset.go
+++ platform-operator/clientset/versioned/clientset.go
@@ -22,7 +22,8 @@ type Interface interface {
 	VerrazzanoV1alpha1() verrazzanov1alpha1.VerrazzanoV1alpha1Interface
 }
 
-// Clientset contains the clients for groups.
+// Clientset contains the clients for groups. Each group has exactly one
+// version included in a Clientset.
 type Clientset struct {
 	*discovery.DiscoveryClient
 	verrazzanoV1beta1  *verrazzanov1beta1.VerrazzanoV1beta1Client
