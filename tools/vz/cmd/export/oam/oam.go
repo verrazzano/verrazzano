3837fc8f7cc83b81c6a4c46d34fef38ae09f6621
Michael Gianatassio <michael.gianatassio@oracle.com>
2023-12-06 10:07:29 -0500

    save

--- tools/vz/cmd/export/oam/oam.go
+++ tools/vz/cmd/export/oam/oam.go
@@ -4,9 +4,19 @@
 package oam
 
 import (
+	"context"
 	"fmt"
 
+	"k8s.io/apimachinery/pkg/api/errors"
+
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"k8s.io/client-go/dynamic"
+
 	"github.com/verrazzano/verrazzano/tools/vz/pkg/constants"
+	"k8s.io/utils/strings/slices"
 
 	"github.com/spf13/cobra"
 	cmdhelpers "github.com/verrazzano/verrazzano/tools/vz/cmd/helpers"
@@ -22,6 +32,9 @@ const (
 TBD
 `
 )
+
+// apiIgnoreList - list of APIs to ignore for export
+var apiIgnoreList = []string{"events", "pods", "poddisruptionbudgets", "localsubjectaccessreviews", "controllerrevisions", "csistoragecapacities", "leases"}
 
 func NewCmdExportOAM(vzHelper helpers.VZHelper) *cobra.Command {
 	cmd := cmdhelpers.NewCommand(vzHelper, CommandName, helpShort, helpLong)
@@ -42,11 +55,11 @@ func NewCmdExportOAM(vzHelper helpers.VZHelper) *cobra.Command {
 
 func RunCmdExportOAM(cmd *cobra.Command, vzHelper helpers.VZHelper) error {
 	// Get the OAM application name
-	name, err := cmd.PersistentFlags().GetString(constants.AppNameFlag)
+	appName, err := cmd.PersistentFlags().GetString(constants.AppNameFlag)
 	if err != nil {
 		return fmt.Errorf(flagErrorStr, err.Error())
 	}
-	if len(name) == 0 {
+	if len(appName) == 0 {
 		return fmt.Errorf("A value for --%s is required", constants.AppNameFlag)
 	}
 
@@ -55,23 +68,12 @@ func RunCmdExportOAM(cmd *cobra.Command, vzHelper helpers.VZHelper) error {
 	if err != nil {
 		return fmt.Errorf(flagErrorStr, err.Error())
 	}
-	fmt.Fprintf(vzHelper.GetOutputStream(), "Namespace = %q\n", namespace)
-
-	// Get the controller runtime client
-	/*
-		client, err := vzHelper.GetClient(cmd)
-		if err != nil {
-			return err
-		}
-	*/
 
 	// Get the dynamic client
-	/*
-		dynamicClient, err := vzHelper.GetDynamicClient(cmd)
-		if err != nil {
-			return err
-		}
-	*/
+	dynamicClient, err := vzHelper.GetDynamicClient(cmd)
+	if err != nil {
+		return err
+	}
 
 	// Get the list of API namespaced resources
 	disco, err := vzHelper.GetDiscoveryClient(cmd)
@@ -85,9 +87,34 @@ func RunCmdExportOAM(cmd *cobra.Command, vzHelper helpers.VZHelper) error {
 
 	for _, list := range lists {
 		for _, resource := range list.APIResources {
-			fmt.Fprintf(vzHelper.GetErrorStream(), "Resource: %q Group: %q Version: %q \n", resource.Name, resource.Group, resource.Version)
+			// Skip the following resource types
+			if slices.Contains(apiIgnoreList, resource.Name) {
+				continue
+			}
+			if err = exportResource(dynamicClient, vzHelper, resource, namespace, appName); err != nil {
+				return err
+			}
 		}
 	}
 
+	return nil
+}
+
+func exportResource(client dynamic.Interface, vzHelper helpers.VZHelper, resource metav1.APIResource, namespace string, appName string) error {
+	fmt.Fprintf(vzHelper.GetOutputStream(), "Processing resource %q in namespace %q for OAM app %q \n", resource.Name, namespace, appName)
+
+	foo := schema.GroupVersionResource{Group: resource.Group, Version: resource.Version, Resource: resource.Name}
+	list, err := client.Resource(foo).Namespace(namespace).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return nil
+		}
+		return err
+	}
+
+	// Export each resource that matches the OAM filters
+	for _, item := range list.Items {
+		fmt.Fprintf(vzHelper.GetOutputStream(), "Found item %s", item.GetName())
+	}
 	return nil
 }
