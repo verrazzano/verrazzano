58cd4edfcc93bb965fc8bd79aa17d85e61f14735
pfmackin <paul.mackin@oracle.com>
2023-12-06 20:06:49 -0500

    checkpoint

--- tools/psr/backend/workers/weblogic/todo/common.go
+++ tools/psr/backend/workers/weblogic/todo/common.go
@@ -6,33 +6,8 @@ package todo
 import (
 	"github.com/verrazzano/verrazzano/pkg/log/vzlog"
 	"github.com/verrazzano/verrazzano/tools/psr/backend/metrics"
+	"io"
 	"net/http"
-	"sync/atomic"
-)
-
-type httpFunc func(url string) (resp *http.Response, err error)
-
-var httpGetFunc httpFunc = http.Get
-
-const (
-	// metricsPrefix is the prefix that is automatically pre-pended to all metrics exported by this worker.
-	metricsPrefix = "http_get"
-
-	// ServiceName specifies the name of the service in the local cluster
-	// By default, the ServiceName is not specified
-	ServiceName = "SERVICE_NAME"
-
-	// ServiceNamespace specifies the namespace of the service in the local cluster
-	// By default, the ServiceNamespace is not specified
-	ServiceNamespace = "SERVICE_NAMESPACE"
-
-	// ServicePort specifies the port of the service in the local cluster
-	// By default, the ServicePort is not specified
-	ServicePort = "SERVICE_PORT"
-
-	// Path specifies the path in the URL
-	// By default, the path is not specified
-	Path = "URL_PATH"
 )
 
 type HttpMetricDef struct {
@@ -43,23 +18,23 @@ type HttpMetricDef struct {
 }
 
 // HandleResponse processes the HTTP response and updates metrics
-func HandleResponse(log vzlog.VerrazzanoLogger, URL string, metricDef *HttpMetricDef, resp *http.Response, err error) error {
+func HandleResponse(log vzlog.VerrazzanoLogger, URL string, metricDef *HttpMetricDef, resp *http.Response, err error) ([]byte, error) {
 	if err != nil {
-		atomic.AddInt64(&metricDef.RequestsFailedCountTotal.Val, 1)
-		return log.ErrorfNewErr("HTTP request %s returned error %v", URL, err)
+		return nil, log.ErrorfNewErr("HTTP request %s returned error %v", URL, err)
 	}
 	if resp == nil {
-		atomic.AddInt64(&metricDef.RequestsFailedCountTotal.Val, 1)
-		return log.ErrorfNewErr("HTTP request %s returned nil response", URL)
+		return nil, log.ErrorfNewErr("HTTP request %s returned nil response", URL)
 	}
-	resp.Body.Read()
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, log.ErrorfNewErr("HTTP request body ReadAll for URL %s returned error %v", URL, err)
+	}
 
 	if resp.StatusCode != 200 {
-		atomic.AddInt64(&metricDef.RequestsFailedCountTotal.Val, 1)
-		return log.ErrorfNewErr("HTTP request %s returned StatusCode &v", URL, resp.StatusCode)
+		return nil, log.ErrorfNewErr("HTTP request %s returned StatusCode &v", URL, resp.StatusCode)
 	}
 	// Success
-	atomic.AddInt64(&metricDef.RequestsSucceededCountTotal.Val, 1)
 	log.Progressf("Http request to URL %s succeeded", URL)
-	return nil
+	return body, nil
 }
