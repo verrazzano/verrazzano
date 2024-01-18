c6920600398b8d39c88e255251595b95b91dedad
Tony Vlatas <37378381+agv426@users.noreply.github.com>
2024-01-18 14:26:52 -0500

    VZ-11667 metrics service is going away (#7671)
    * VZ-11667 disable metrics Remove SEARCH_HTTP_ENDPOINT settings from pipeline envs, should get rid of auth failure attempts

--- ci/scripts/metric_del.sh
+++ ci/scripts/metric_del.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright (c) 2021, Oracle and/or its affiliates.
+# Copyright (c) 2021, 2024, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 #
 PROMETHEUS_GW_URL=$1
@@ -12,4 +12,4 @@ if [ -z "$4" ]
   then
     URL="${PROMETHEUS_GW_URL}/metrics/job/$JOB"
 fi
-curl -i -X DELETE "$URL" -u $PROMETHEUS_CRED
+# curl -i -X DELETE "$URL" -u $PROMETHEUS_CRED
