e944aec7d16c1d2320096ad5ecfca6f0da6870d3
bhatpmk <padmanabha.bhat@oracle.com>
2024-01-19 10:29:00 +0530

    Don't return on error from special release case

--- ci/scripts/get_branch_scan_results.sh
+++ ci/scripts/get_branch_scan_results.sh
@@ -207,6 +207,7 @@ if [[ "${CLEAN_BRANCH_NAME}" == release-* ]]; then
 
     GIT_COMMIT=$(git rev-list -n 1 ${VERSION})
     echo "Fetching BOM for ${VERSION}"
+
     export SCAN_BOM_FILE=${BOM_DIR}/${VERSION}-bom.json
     get_bom_from_release ${VERSION} ${SCAN_BOM_FILE}
 
@@ -214,8 +215,8 @@ if [[ "${CLEAN_BRANCH_NAME}" == release-* ]]; then
     mkdir -p ${SCAN_RESULTS_DIR}
 
     echo "Fetching scan results for BOM: ${SCAN_BOM_FILE}"
-    ${RELEASE_SCRIPT_DIR}/scan_bom_images.sh  -b ${SCAN_BOM_FILE} -o ${SCAN_RESULTS_DIR} -r ${OCIR_SCAN_REGISTRY} -x ${OCIR_REPOSITORY_BASE} || exit 1
-    ${RELEASE_SCRIPT_DIR}/get_ocir_scan_results.sh ${SCAN_BOM_FILE} || exit 1
+    ${RELEASE_SCRIPT_DIR}/scan_bom_images.sh  -b ${SCAN_BOM_FILE} -o ${SCAN_RESULTS_DIR} -r ${OCIR_SCAN_REGISTRY} -x ${OCIR_REPOSITORY_BASE}
+    ${RELEASE_SCRIPT_DIR}/get_ocir_scan_results.sh ${SCAN_BOM_FILE}
     ${RELEASE_SCRIPT_DIR}/generate_vulnerability_report.sh ${SCAN_RESULTS_DIR} ${GIT_COMMIT} ${CLEAN_BRANCH_NAME} ${VERSION} ${SCAN_DATETIME} ${BUILD_NUMBER}
     ${RELEASE_SCRIPT_DIR}/generate_upload_file.sh ${SCAN_RESULTS_DIR}/consolidated.csv "${VERSION}" > ${SCAN_RESULTS_DIR}/consolidated-upload.json
     publish_results ${VERSION} ${SCAN_BOM_FILE} ${SCAN_RESULTS_DIR}
