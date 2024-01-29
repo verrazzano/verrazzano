dac4218e9ac51f9221f30442405346aca0ac5b3b
George Aeillo <george.f.aeillo@oracle.com>
2024-01-29 11:39:11 -0500

    Change permissions from 0644 to 0600

--- tools/vz/pkg/helpers/vzcapture.go
+++ tools/vz/pkg/helpers/vzcapture.go
@@ -148,7 +148,7 @@ func CaptureMetadata(captureDir string) error {
 		return err
 	}
 	sanitizedDataInBytes := []byte(SanitizeString(string(metadataJSON)))
-	err = os.WriteFile(metadataFilename, sanitizedDataInBytes, 0644)
+	err = os.WriteFile(metadataFilename, sanitizedDataInBytes, 0600)
 	if err != nil {
 		LogError(fmt.Sprintf(writeFileError, metadataFilename, err.Error()))
 		return err
