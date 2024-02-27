e8ae71938f13f2eccc59aff4b7f5bd38cfa36765
George Aeillo <george.f.aeillo@oracle.com>
2024-02-27 12:01:28 -0500

    VZ-11703: Refactor CLI Test Code (#7760)
    * Add helper function to generate a VZ Helper
    
    * Save initial struct progress
    
    * Save progress on refactoring
    
    * Save progress
    
    * New struct, save progress
    
    * Finish refactoring for root, version, and analyze command files
    
    * Refactor sanitize command
    
    * Make progress on refactoring, currently working on upgrade command, install command and bug-report have not been worked on yet, the rest of the cmd folder seems to be done with the refactoring
    
    * Have everything working except for two tests on upgrade
    
    * Upgrade test working, working through install tests
    
    * Install tests on working, working through bugreport tests
    
    * Finish bug-report test file
    
    * Fix copyright
    
    * Further fix copyright
    
    * Update functions to allow cleaning of sanitization map
    
    * Add additional defer statements to further clean up files on bugreport_test.go file
    
    * Fix race condition issues
    
    * Fix merged changes to stay u to date with refactor
    
    * Add client race condition check
    
    * Fix go ineffectual assign error
    
    * Save progress
    
    * Further add assert to helper function

--- tools/vz/cmd/version/version_test.go
+++ tools/vz/cmd/version/version_test.go
@@ -1,17 +1,15 @@
-// Copyright (c) 2022, Oracle and/or its affiliates.
+// Copyright (c) 2022, 2024, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 package version
 
 import (
-	"bytes"
 	"os"
 	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/verrazzano/verrazzano/tools/vz/test/helpers"
-	"k8s.io/cli-runtime/pkg/genericclioptions"
 )
 
 // TestVersionCmd - check that command reports not implemented yet
@@ -22,17 +20,17 @@ func TestVersionCmd(t *testing.T) {
 	gitCommit = "9dbc916b58ab9781f7b4c25e51748fb31ec940f8"
 
 	// Send the command output to a byte buffer
-	buf := new(bytes.Buffer)
-	errBuf := new(bytes.Buffer)
-	rc := helpers.NewFakeRootCmdContext(genericclioptions.IOStreams{In: os.Stdin, Out: buf, ErrOut: errBuf})
+	rc := helpers.NewFakeRootCmdContextWithFiles(t)
+	defer helpers.CleanUpNewFakeRootCmdContextWithFiles(rc)
 	versionCmd := NewCmdVersion(rc)
 	assert.NotNil(t, versionCmd)
 
 	// Run version command, check for the expected status results to be displayed
 	err := versionCmd.Execute()
 	assert.NoError(t, err)
-	result := buf.String()
-	results := strings.Split(result, "\n")
+	result, err := os.ReadFile(rc.Out.Name())
+	assert.Nil(t, err)
+	results := strings.Split(string(result), "\n")
 	version, build, commit := results[1], results[2], results[3]
 	assert.Regexp(t, `^(Version: )?(v)?(\d+\.)?(\d+\.)?(\d+)$`, version)
 	assert.Regexp(t, `^(BuildDate: )?(\d+\-)?(\d+\-)?(\d+T)?(\d+\:)?(\d+\:)?(\d+Z)$`, build)
